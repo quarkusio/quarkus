2db48dae33c8c1d38b4255c91b4327db73bcdcd6
Sanne Grinovero <sanne@hibernate.org>
2018-06-14 14:02:58 +0100

    Step1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,32 +1,62 @@
 package com.example;
 
+import java.util.List;
+import java.util.UUID;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class Main {
 
-    public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+		System.out.println( "Hibernate EntityManagerFactory: booted" );
 
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        Person person = new Person( 1L, "Bob", new Address( 1L, "Main Street" ) );
-        entityManager.persist( person );
-        entityManager.getTransaction().commit();
-        entityManager.close();
+		doStuffWithHibernate( entityManagerFactory );
 
-        entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        Person bob = entityManager.find( Person.class, 1L );
+		entityManagerFactory.close();
+		System.out.println( "Hibernate EntityManagerFactory: shut down" );
+	}
 
-        assert bob != null;
-        assert bob.getName().equals( "Bob" );
-        assert bob.getAddress().getName().equals( "Main Street" );
+	private static void doStuffWithHibernate(EntityManagerFactory entityManagerFactory) {
+		EntityManager em = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
 
-        entityManager.getTransaction().commit();
-        entityManager.close();
+		persistNewPerson( em );
 
-        entityManagerFactory.close();
-    }
+		listExistingPersons( em );
+
+		transaction.commit();
+		em.close();
+	}
+
+	private static void listExistingPersons(EntityManager em) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<Person> cq = cb.createQuery( Person.class );
+		Root<Person> from = cq.from( Person.class );
+		cq.select( from );
+		TypedQuery<Person> q = em.createQuery( cq );
+		List<Person> allpersons = q.getResultList();
+		StringBuilder sb = new StringBuilder( "list of stored Person names:\n\t" );
+		for ( Person p : allpersons ) {
+			sb.append( p.id ).append( ":\t" ).append( p.name ).append( "\n\t" );
+		}
+		sb.append( "\nList complete.\n" );
+		System.out.print( sb );
+	}
+
+	private static void persistNewPerson(EntityManager entityManager) {
+		UUID uuid = UUID.randomUUID();
+		Person person = new Person();
+		person.name = uuid.toString();
+		entityManager.persist( person );
+	}
+
 }

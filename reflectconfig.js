a1ad1b97645d9b78bade5f437140dace05858134
Sanne Grinovero <sanne@hibernate.org>
2018-07-09 17:45:42 +0100

    Switch to Hibernate ORM 5.4.0-SNAPSHOT and use a JDK8 compatible version of JTA

---
+++ reflectconfig.js
@@ -0,0 +1,10 @@
+[
+   {
+      "name" : "testing.graal.service.a.ServiceImpl",
+      "allPublicConstructors" : true
+   },
+   {
+      "name" : "testing.graal.service.b.ServiceImpl",
+      "allPublicConstructors" : true
+   }
+]

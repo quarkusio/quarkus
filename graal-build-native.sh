334639adcd5ee89013bf6f6186fd3119ce84e3d3
Sanne Grinovero <sanne@hibernate.org>
2018-07-09 15:05:39 +0100

    Scripts to create native-image

--- graal-build-native.sh
+++ graal-build-native.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 
-# Expects JDK 10+ on classpath
+echo "Expects GraalVM on classpath"
+echo ""
+
+echo "Starting mvn build..."
+mvn clean install > maven-build.log
 
 ## Update the classpath definition used by this script:
-mvn dependency:build-classpath -Dmdep.outputFile=cp.txt > /dev/null
+mvn dependency:build-classpath -Dmdep.outputFile=cp.txt >> maven-build.log
 CLASSPATH=`cat cp.txt`
 
-echo "Listing `jdeps --print-module-deps` for each dependency individually:"
-IFS=':'; CLASSPATH_ARRAY=($CLASSPATH); unset IFS;
-for dep in ${CLASSPATH_ARRAY[@]}
-do
-	echo "  "
-    echo "Dependency: $dep"
-    jdeps --print-module-deps $dep
-done
-echo "  "
-# TODO : combined report not supported by jdeps?
-#echo "Listing `jdeps --print-module-deps` for all dependencies combined:"
-#jdeps --print-module-deps -cp $CLASSPATH
+echo "Starting native-image :"
 
+native-image -cp "$CLASSPATH":./target/classes com.example.Main com.example.Main
 
+echo ""
+echo "Starting the native app:"
 
+./com.example.Main

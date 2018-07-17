988b5e9c57f5c00e3462e5e51da6e2b88eaf29e9
Sanne Grinovero <sanne@hibernate.org>
2018-07-17 10:50:14 +0100

    Disable the Oracle8i Dialects

--- src/main/java/org/hibernate/protean/substitutions/RemoveOracle.java
+++ src/main/java/org/hibernate/protean/substitutions/RemoveOracle.java
@@ -1,4 +1,13 @@
 package org.hibernate.protean.substitutions;
 
-public class RemoveOracle {
+import com.oracle.svm.core.annotate.Delete;
+import com.oracle.svm.core.annotate.TargetClass;
+
+@TargetClass(className = "org.hibernate.dialect.Oracle8iDialect")
+@Delete
+public final class RemoveOracle {
+
+	//The Oracle8iDialect presence gets SVM to initialize the org.hibernate.dialect.Oracle8iDialect,
+	//which in turn pulls in the Oracle JDBC driver - and requires it to be on classpath.
+
 }

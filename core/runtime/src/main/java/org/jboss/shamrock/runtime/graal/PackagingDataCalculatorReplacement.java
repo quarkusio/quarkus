976d91f72e4cc0a1894303bf02be6c50cb80b2ad
Stuart Douglas <stuart.w.douglas@gmail.com>
2018-07-25 09:57:52 +1000

    Fix up logging

---
+++ core/runtime/src/main/java/org/jboss/shamrock/runtime/graal/PackagingDataCalculatorReplacement.java
@@ -0,0 +1,21 @@
+package org.jboss.shamrock.runtime.graal;
+
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+import ch.qos.logback.classic.spi.StackTraceElementProxy;
+
+@TargetClass(className = "ch.qos.logback.classic.spi.PackagingDataCalculator")
+final class PackagingDataCalculatorReplacement {
+
+    @Substitute
+    String getCodeLocation(Class type) {
+        return "na";
+    }
+
+    @Substitute
+    void populateFrames(StackTraceElementProxy[] stepArray) {
+
+    }
+
+
+}

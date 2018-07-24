473fe2195bd250847ded87ef13b1552908f653c3
Stuart Douglas <stuart.w.douglas@gmail.com>
2018-07-25 09:57:32 +1000

    Add support for debug symbols to the graal runner

--- test-framework/graal/src/main/java/org/jboss/shamrock/junit/GraalTest.java
+++ test-framework/graal/src/main/java/org/jboss/shamrock/junit/GraalTest.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 
@@ -43,6 +45,7 @@ public class GraalTest extends BlockJUnit4ClassRunner {
 
     private void runInternal(RunNotifier notifier) {
         boolean reportAtRuntime = Boolean.getBoolean("graal.reportAtRuntime");
+        boolean debugSymbols = Boolean.getBoolean("graal.debugSymbols");
         if (first) {
             first = false;
             String graal = System.getenv("GRAALVM_HOME");
@@ -67,14 +70,18 @@ public class GraalTest extends BlockJUnit4ClassRunner {
 
             try {
 
-                String[] command;
-                if (reportAtRuntime) {
-                    command = new String[]{nativeImage, "-jar", path, "-H:+ReportUnsupportedElementsAtRuntime", "-H:IncludeResources=META-INF/.*"};
-                } else {
-                    command = new String[]{nativeImage, "-jar", path, "-H:IncludeResources=META-INF/.*"};
+                List<String> command = new ArrayList<>();
+                command.add(nativeImage);
+                command.add("-jar");
+                command.add(path);
+                command.add("-H:IncludeResources=META-INF/.*");
+                if(reportAtRuntime) {
+                    command.add("-H:+ReportUnsupportedElementsAtRuntime");
                 }
-
-                Process process = Runtime.getRuntime().exec(command, new String[]{}, new File(path.substring(0, path.lastIndexOf(File.separator))));
+                if(debugSymbols) {
+                    command.add("-g");
+                }
+                Process process = Runtime.getRuntime().exec(command.toArray(new String[0]), new String[]{}, new File(path.substring(0, path.lastIndexOf(File.separator))));
                 new Thread(new ProcessReader(process.getInputStream())).start();
                 new Thread(new ProcessReader(process.getErrorStream())).start();
                 if (process.waitFor() != 0) {

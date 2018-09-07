26f432664969657e312c5f0fe9bebe592ca29d4b
Stuart Douglas <stuart.w.douglas@gmail.com>
2018-09-07 12:24:50 +1000

    Travis CI work

---
+++ get-graal.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ -d .graal-install/graalvm-ce-1.0.0-rc6 ]; then
+echo "Graal already present, exiting"
+exit
+fi
+mkdir .graal-install
+cd .graal-install
+wget https://github.com/oracle/graal/releases/download/vm-1.0.0-rc6/graalvm-ce-1.0.0-rc6-linux-amd64.tar.gz
+tar -xf graalvm-ce-1.0.0-rc6-linux-amd64.tar.gz

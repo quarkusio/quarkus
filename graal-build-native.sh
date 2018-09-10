5367f175a8e7e62917cb9c4f7946f67cda1fa26a
Sanne Grinovero <sanne@hibernate.org>
2018-09-10 11:28:46 +0100

    Adjust bash scripts to new directory layout

--- graal-build-native.sh
+++ graal-build-native.sh
@@ -15,6 +15,9 @@ echo ""
 echo "Starting mvn build..."
 mvn clean package > maven-build.log
 
+echo "Jump into example directory"
+cd hibernate-orm-protean-example
+
 ## Update the classpath definition used by this script:
 mvn dependency:build-classpath -DexcludeArtifactIds=graal-annotations -Dmdep.outputFile=cp.txt >> maven-build.log
 CLASSPATH=`cat cp.txt`
@@ -24,7 +27,7 @@ rm com.example.Main
 
 echo "Starting native-image :"
 #--verbose --shared -ea -H:+ReportUnsupportedElementsAtRuntime -H:+PrintAnalysisCallTree -R:±LSRAOptimization -R:AnalysisSizeCutoff=8 -H:-RuntimeAssertions -H:+PrintImageElementSizes -H:+PrintImageHeapPartitionSizes -H:+PrintImageObjectTree -H:+PrintAnalysisCallTree -H:+ReportUnsupportedElementsAtRuntime
-timer native-image -g --no-server -O1 --verbose -H:IncludeResources=META-INF/services/.* -H:+PrintImageElementSizes -H:+PrintImageHeapPartitionSizes -H:+PrintImageObjectTree -H:+PrintAnalysisCallTree -H:ReflectionConfigurationFiles=reflectconfig.json -cp "$CLASSPATH":./target/classes com.example.Main com.example.Main
+timer native-image --no-server -O0 --verbose -H:IncludeResources=META-INF/services/.* -H:+PrintImageElementSizes -H:+PrintImageHeapPartitionSizes -H:+PrintImageObjectTree -H:+PrintAnalysisCallTree -H:ReflectionConfigurationFiles=../reflectconfig.json -cp "$CLASSPATH":./target/classes com.example.Main com.example.Main
 
 echo ""
 echo "Disk size, before strip:"
@@ -45,3 +48,6 @@ timer ./com.example.Main -Xmx1M -Xmn1M
 
 nm -t d -S --size-sort ./*.debug  | sort -k 4 | sed 's/\.[_a-zA-Z0-9<>$]*(.*//g' | sed 's/\.[a-zA-Z0-9$_]*$//g' | sed 's/\.[^\.]*\$.*$//g' | sort -k 4 | awk '{if (package != $4) {print subtotal/1024 "K " package; package=$4; subtotal=0} else {subtotal+=$2}}' | grep -v "0K " | sort -k1hr > reports/package-sizes.txt
 echo "Updated reports/package-sizes.txt"
+
+# Return to root of project:
+cd ..

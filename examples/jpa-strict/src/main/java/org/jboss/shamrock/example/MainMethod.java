47d09dad408edd0954681c267d04d81a9257394e
Sanne Grinovero <sanne@hibernate.org>
2018-09-14 10:15:40 +0100

    Introducing JPA integration tests, progress with the Hibernate integration

---
+++ examples/jpa-strict/src/main/java/org/jboss/shamrock/example/MainMethod.java
@@ -0,0 +1,9 @@
+package org.jboss.shamrock.example;
+
+public class MainMethod {
+
+    public static void main(String... args) throws Exception {
+        Class.forName("org.jboss.shamrock.runtime.RuntimeRunner");
+    }
+
+}
---
+++ examples/jpa-strict/src/test/java/org/jboss/shamrock/example/test/JPAReflectionInGraalITCase.java
@@ -0,0 +1,23 @@
+package org.jboss.shamrock.example.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.jboss.shamrock.example.testutils.URLTester;
+import org.jboss.shamrock.junit.GraalTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Test reflection around JPA entities
+ *
+ * @author Emmanuel Bernard emmanuel@hibernate.org
+ */
+@RunWith(GraalTest.class)
+public class JPAReflectionInGraalITCase {
+
+    @Test
+    public void testFieldAndGetterReflectionOnEntityFromServlet() throws Exception {
+        assertEquals("OK", URLTester.relative("jpa/testreflection").invokeURL().asString());
+    }
+
+}

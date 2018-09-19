19725695247f249dfb01bba183b34b71d7dabd3e
Stuart Douglas <stuart.w.douglas@gmail.com>
2018-09-19 17:31:44 +1000

    Wire up request scope

---
+++ examples/strict/src/test/java/org/jboss/shamrock/example/test/RequestScopeTestCase.java
@@ -0,0 +1,19 @@
+package org.jboss.shamrock.example.test;
+
+import org.jboss.shamrock.example.testutils.URLTester;
+import org.jboss.shamrock.junit.ShamrockTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(ShamrockTest.class)
+public class RequestScopeTestCase {
+
+    @Test
+    public void testRequestScope() {
+        Assert.assertEquals("3", URLTester.relative("rest/request-scoped").invokeURL().asString());
+        Assert.assertEquals("3", URLTester.relative("rest/request-scoped").invokeURL().asString());
+    }
+
+
+}

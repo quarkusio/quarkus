d040d96dd2455da999e5e36294ca47636d0592b5
Stuart Douglas <stuart.w.douglas@gmail.com>
2018-10-16 16:16:54 +1100

    Don't parse XML on startup, parse at build time

--- transactions/runtime/src/main/java/org/jboss/shamrock/transactions/runtime/TransactionTemplate.java
+++ transactions/runtime/src/main/java/org/jboss/shamrock/transactions/runtime/TransactionTemplate.java
@@ -1,12 +1,18 @@
 package org.jboss.shamrock.transactions.runtime;
 
+import java.lang.reflect.Field;
 import java.util.Properties;
 
+import org.jboss.logging.Logger;
+
 import com.arjuna.ats.jta.UserTransaction;
+import com.arjuna.common.util.propertyservice.PropertiesFactory;
 
 public class TransactionTemplate {
 
     private static Properties defaultProperties;
+
+    private static final Logger log = Logger.getLogger(TransactionTemplate.class);
 
     public void forceInit() {
         try {
@@ -18,6 +24,17 @@ public class TransactionTemplate {
     }
 
     public void setDefaultProperties(Properties properties) {
+        //TODO: this is a huge hack to avoid loading XML parsers
+        //this needs a proper SPI
+        try {
+            Field field = PropertiesFactory.class.getDeclaredField("delegatePropertiesFactory");
+            field.setAccessible(true);
+            field.set(null, new ShamrockPropertiesFactory(properties));
+
+        } catch (Exception e) {
+            log.error("Could not override transaction properties factory", e);
+        }
+
         defaultProperties = properties;
     }
 

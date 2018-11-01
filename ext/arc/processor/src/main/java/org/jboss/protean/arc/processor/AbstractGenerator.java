cda2eddf8c0c0dde237764fcfbe6326b41e62959
Martin Kouba <mkouba@redhat.com>
2018-11-01 19:33:08 +0100

    Arc - various stuff, mostly perf optimizations
    - cache resolved beans in Instance<>
    - cache last notifier in Event<>
    - fix AbstractSharedContext#get(Contextual<T> contextual)
    - add AsyncProducerTest to test producer with CompletionStage return
    type

--- ext/arc/processor/src/main/java/org/jboss/protean/arc/processor/AbstractGenerator.java
+++ ext/arc/processor/src/main/java/org/jboss/protean/arc/processor/AbstractGenerator.java
@@ -4,8 +4,11 @@ import java.lang.reflect.Modifier;
 
 import org.jboss.jandex.FieldInfo;
 import org.jboss.jandex.MethodInfo;
+import org.jboss.protean.arc.Arc;
 
 abstract class AbstractGenerator {
+
+    static final String DEFAULT_PACKAGE = Arc.class.getPackage().getName() + ".generator";
 
     protected String providerName(String name) {
         // TODO we can do better
@@ -42,5 +45,20 @@ abstract class AbstractGenerator {
     protected boolean isPackagePrivate(int mod) {
         return !(Modifier.isPrivate(mod) || Modifier.isProtected(mod) || Modifier.isPublic(mod));
     }
+
+    protected String getPackageName(BeanInfo bean) {
+        String packageName;
+        if (bean.isProducerMethod() || bean.isProducerField()) {
+            packageName = DotNames.packageName(bean.getDeclaringBean().getProviderType().name());
+        } else {
+            packageName = DotNames.packageName(bean.getProviderType().name());
+        }
+        if (packageName.startsWith("java.")) {
+            // It is not possible to place a class in a JDK package
+            packageName = DEFAULT_PACKAGE;
+        }
+        return packageName;
+    }
+
 
 }
--- ext/arc/runtime/src/main/java/org/jboss/protean/arc/InstanceImpl.java
+++ ext/arc/runtime/src/main/java/org/jboss/protean/arc/InstanceImpl.java
@@ -28,6 +28,8 @@ class InstanceImpl<T> implements Instance<T> {
 
     private final CreationalContextImpl<?> creationalContext;
 
+    private final Set<InjectableBean<?>> beans;
+
     InstanceImpl(Type type, Set<Annotation> qualifiers, CreationalContextImpl<?> creationalContext) {
         if (type instanceof ParameterizedType) {
             this.type = ((ParameterizedType) type).getActualTypeArguments()[0];
@@ -36,17 +38,24 @@ class InstanceImpl<T> implements Instance<T> {
         }
         this.qualifiers = qualifiers != null ? qualifiers : Collections.emptySet();
         this.creationalContext = creationalContext;
+
+        if (qualifiers.isEmpty() && Object.class.equals(type)) {
+            // Do not prefetch the beans for Instance<Object> with no qualifiers
+            this.beans = null;
+        } else {
+            this.beans = resolve();
+        }
     }
 
     @Override
     public Iterator<T> iterator() {
-        return new InstanceIterator(getBeans());
+        return new InstanceIterator(beans());
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public T get() {
-        Set<InjectableBean<?>> beans = getBeans();
+        Set<InjectableBean<?>> beans = beans();
         if (beans.isEmpty()) {
             throw new UnsatisfiedResolutionException();
         } else if (beans.size() > 1) {
@@ -78,12 +87,12 @@ class InstanceImpl<T> implements Instance<T> {
 
     @Override
     public boolean isUnsatisfied() {
-        return getBeans().isEmpty();
+        return beans().isEmpty();
     }
 
     @Override
     public boolean isAmbiguous() {
-        return getBeans().size() > 1;
+        return beans().size() > 1;
     }
 
     @Override
@@ -102,7 +111,11 @@ class InstanceImpl<T> implements Instance<T> {
         return instance;
     }
 
-    private Set<InjectableBean<?>> getBeans() {
+    private Set<InjectableBean<?>> beans() {
+        return beans != null ? beans : resolve();
+    }
+
+    private Set<InjectableBean<?>> resolve() {
         return ArcContainerImpl.instance().getResolvedBeans(type, qualifiers.toArray(EMPTY_ANNOTATION_ARRAY));
     }
 

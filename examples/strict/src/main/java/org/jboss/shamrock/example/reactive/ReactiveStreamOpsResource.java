4c47a51985c228dd5090a40164f640e379b0fc8e
Clement Escoffier <clement.escoffier@gmail.com>
2018-11-15 19:43:53 +0100

    Implement simple tests for the SmallRye Reactive Streams Operators support

---
+++ examples/strict/src/main/java/org/jboss/shamrock/example/reactive/ReactiveStreamOpsResource.java
@@ -0,0 +1,39 @@
+package org.jboss.shamrock.example.reactive;
+
+import io.reactivex.Flowable;
+import org.eclipse.microprofile.reactive.streams.ReactiveStreams;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/reactive")
+public class ReactiveStreamOpsResource {
+
+    @GET
+    public String foo() {
+        return "hello";
+    }
+
+    @GET
+    @Path("/stream-regular")
+    public String stream1() {
+        StringBuilder builder = new StringBuilder();
+        ReactiveStreams.of("a", "b", "c")
+                .map(String::toUpperCase)
+                .forEach(builder::append)
+                .run();
+        return builder.toString();
+    }
+
+    @GET
+    @Path("/stream-rx")
+    public String stream2() {
+        StringBuilder builder = new StringBuilder();
+        ReactiveStreams.fromPublisher(Flowable.fromArray("d", "e", "f"))
+                .map(String::toUpperCase)
+                .forEach(builder::append)
+                .run();
+        return builder.toString();
+    }
+
+}
---
+++ examples/strict/src/test/java/org/jboss/shamrock/example/test/ReactiveStreamsOperatorsTestCase.java
@@ -0,0 +1,24 @@
+package org.jboss.shamrock.example.test;
+
+import org.jboss.shamrock.example.testutils.URLTester;
+import org.jboss.shamrock.junit.ShamrockTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(ShamrockTest.class)
+public class ReactiveStreamsOperatorsTestCase {
+
+    @Test
+    public void testReactiveStreams() {
+        String result = URLTester.relative("rest/reactive/stream-regular").invokeURL().asString();
+        Assert.assertEquals("ABC", result);
+    }
+
+    @Test
+    public void testRxJava2() {
+        String result = URLTester.relative("rest/reactive/stream-rx").invokeURL().asString();
+        Assert.assertEquals("DEF", result);
+    }
+
+}

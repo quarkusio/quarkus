9e60e131a493c9dc6a869531b0d159c892abf42b
Sanne Grinovero <sanne@hibernate.org>
2018-11-22 12:39:28 +0000

    Include the PostgreSQL JDBC driver patches in the form of Substitutions

---
+++ jdbc/jdbc-postgresql/jdbc-postgresql-runtime/src/main/java/org/jboss/shamrock/jdbc/postresql/runtime/graal/Remove_NTDSAPIWrapper.java
@@ -0,0 +1,12 @@
+package org.jboss.shamrock.jdbc.postresql.runtime.graal;
+
+import org.postgresql.sspi.NTDSAPIWrapper;
+
+import com.oracle.svm.core.annotate.Delete;
+import com.oracle.svm.core.annotate.TargetClass;
+
+@TargetClass(NTDSAPIWrapper.class)
+@Delete
+public final class Remove_NTDSAPIWrapper {
+
+}

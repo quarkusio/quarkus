d885a3783baf689ff8751702f489f63f95d4a328
Sanne Grinovero <sanne@hibernate.org>
2018-11-23 12:59:18 +0000

    Introduce support for H2 databases

---
+++ jdbc/jdbc-h2/jdbc-h2-runtime/src/main/java/org/jboss/shamrock/jdbc/h2/runtime/graalsubstitutions/Engine.java
@@ -0,0 +1,23 @@
+package org.jboss.shamrock.jdbc.h2.runtime.graalsubstitutions;
+
+import org.h2.engine.ConnectionInfo;
+import org.h2.engine.Session;
+
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+
+@TargetClass(className = "org.h2.engine.Engine")
+@Substitute
+public final class Engine {
+
+    @Substitute
+    public Session createSession(ConnectionInfo ci) {
+        throw new UnsupportedOperationException("H2 database compiled into a native-image is only functional as a client: can't create an Embedded Database Session");
+    }
+
+    @Substitute
+    void close(String name) {
+        //no-op
+    }
+
+}

48430afaacb402467d9e3ecd5ea46421075db84a
Guillaume Smet <guillaume.smet@gmail.com>
2018-11-30 15:36:57 +0100

    Initialize resource bundles for messages eagerly

--- bean-validation/runtime/src/main/java/org/jboss/shamrock/beanvalidation/runtime/ValidatorTemplate.java
+++ bean-validation/runtime/src/main/java/org/jboss/shamrock/beanvalidation/runtime/ValidatorTemplate.java
@@ -17,6 +17,8 @@
 package org.jboss.shamrock.beanvalidation.runtime;
 
 import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.validation.Validation;
@@ -33,16 +35,20 @@ public class ValidatorTemplate {
         PredefinedScopeHibernateValidatorConfiguration configuration = Validation.byProvider(PredefinedScopeHibernateValidator.class)
                 .configure();
 
+        Set<Locale> localesToInitialize = Collections.singleton(Locale.getDefault());
+
         try {
             Class<?> cl = Class.forName("javax.el.ELManager");
             Method method = cl.getDeclaredMethod("getExpressionFactory");
             method.invoke(null);
         } catch (Throwable t) {
             //if EL is not on the class path we use the parameter message interpolator
-            configuration.messageInterpolator(new ParameterMessageInterpolator());
+            configuration.messageInterpolator(new ParameterMessageInterpolator(localesToInitialize));
         }
 
-        configuration.initializeBeanMetaData(classesToBeValidated);
+        configuration
+                .initializeBeanMetaData(classesToBeValidated)
+                .initializeLocales(localesToInitialize);
 
         ValidatorHolder.initialize(configuration.buildValidatorFactory());
     }

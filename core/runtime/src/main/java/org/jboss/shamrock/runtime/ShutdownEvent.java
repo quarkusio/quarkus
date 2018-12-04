e0c9a307770f326173f33d9f30ab728a66d0fd14
Clement Escoffier <clement.escoffier@gmail.com>
2018-12-04 18:28:55 +0100

    Add content for the application lifecycle events Add a bit of javadoc on these event classes.

--- core/runtime/src/main/java/org/jboss/shamrock/runtime/ShutdownEvent.java
+++ core/runtime/src/main/java/org/jboss/shamrock/runtime/ShutdownEvent.java
@@ -17,7 +17,17 @@
 package org.jboss.shamrock.runtime;
 
 /**
- * Event that is fired before shutdown
+ * Event that is fired before shutdown.
+ *
+ * This event is observed as follows:
+ *
+ * <code><pre>
+ *     void onStop(@Observes ShutdownEvent ev) {
+ *         LOGGER.info("The application is stopping...");
+ *     }
+ * </pre></code>
+ *
+ * The annotated method can access other injected beans.
  */
 public class ShutdownEvent {
 }

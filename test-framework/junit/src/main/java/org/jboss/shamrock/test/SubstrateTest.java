210369b56c6b2f77620794a56e0640e42dae0543
Stuart Douglas <stuart.w.douglas@gmail.com>
2018-12-07 08:41:20 +1100

    Fix test issue

--- test-framework/junit/src/main/java/org/jboss/shamrock/test/SubstrateTest.java
+++ test-framework/junit/src/main/java/org/jboss/shamrock/test/SubstrateTest.java
@@ -41,6 +41,7 @@ public class SubstrateTest extends BlockJUnit4ClassRunner {
     private static final long IMAGE_WAIT_TIME = 60000;
 
     private static boolean first = true;
+    private static boolean started = false;
 
     /**
      * Creates a BlockJUnit4ClassRunner to run {@code klass}
@@ -67,76 +68,79 @@ public class SubstrateTest extends BlockJUnit4ClassRunner {
 
                 @Override
                 public void testStarted(Description description) throws Exception {
-                    String path = System.getProperty("native.image.path");
-                    if (path == null) {
-                        //ok, lets make a guess
-                        //this is a horrible hack, but it is intended to make this work in IDE's
+                    if (!started) {
+                        started = true;
+                        String path = System.getProperty("native.image.path");
+                        if (path == null) {
+                            //ok, lets make a guess
+                            //this is a horrible hack, but it is intended to make this work in IDE's
 
-                        ClassLoader cl = getClass().getClassLoader();
-                        String guessedPath = null;
-                        if (cl instanceof URLClassLoader) {
-                            URL[] urls = ((URLClassLoader) cl).getURLs();
-                            for (URL url : urls) {
-                                if (url.getProtocol().equals("file") && url.getPath().endsWith("test-classes/")) {
-                                    //we have the test classes dir
-                                    File testClasses = new File(url.getPath());
-                                    for (File file : testClasses.getParentFile().listFiles()) {
-                                        if (file.getName().endsWith("-runner")) {
-                                            guessedPath = file.getAbsolutePath();
-                                            break;
+                            ClassLoader cl = getClass().getClassLoader();
+                            String guessedPath = null;
+                            if (cl instanceof URLClassLoader) {
+                                URL[] urls = ((URLClassLoader) cl).getURLs();
+                                for (URL url : urls) {
+                                    if (url.getProtocol().equals("file") && url.getPath().endsWith("test-classes/")) {
+                                        //we have the test classes dir
+                                        File testClasses = new File(url.getPath());
+                                        for (File file : testClasses.getParentFile().listFiles()) {
+                                            if (file.getName().endsWith("-runner")) {
+                                                guessedPath = file.getAbsolutePath();
+                                                break;
+                                            }
                                         }
                                     }
+                                    if (guessedPath != null) {
+                                        break;
+                                    }
                                 }
-                                if (guessedPath != null) {
+                            }
+
+                            if (guessedPath == null) {
+                                notifier.fireTestFailure(new Failure(Description.createSuiteDescription(SubstrateTest.class), new RuntimeException("Unable to find native image, make sure native.image.path is set")));
+                                return;
+                            } else {
+                                String errorString = "\n=native.image.path was not set, making a guess that  " + guessedPath + " is the correct native image=";
+                                for (int i = 0; i < errorString.length(); ++i) {
+                                    System.err.print("=");
+                                }
+                                System.err.println(errorString);
+                                for (int i = 0; i < errorString.length(); ++i) {
+                                    System.err.print("=");
+                                }
+                                System.err.println();
+                                path = guessedPath;
+                            }
+                        }
+                        System.out.println("Executing " + path);
+                        final Process testProcess = Runtime.getRuntime().exec(path);
+                        notifier.addListener(new RunListener() {
+                            @Override
+                            public void testRunFinished(Result result) throws Exception {
+                                super.testRunFinished(result);
+                                testProcess.destroy();
+                            }
+                        });
+                        new Thread(new ProcessReader(testProcess.getInputStream())).start();
+                        new Thread(new ProcessReader(testProcess.getErrorStream())).start();
+
+                        int port = Integer.getInteger("http.port", 8080);
+                        long bailout = System.currentTimeMillis() + IMAGE_WAIT_TIME;
+                        boolean ok = false;
+                        while (System.currentTimeMillis() < bailout) {
+                            try {
+                                Thread.sleep(100);
+                                try (Socket s = new Socket()) {
+                                    s.connect(new InetSocketAddress("localhost", port));
+                                    ok = true;
                                     break;
                                 }
+                            } catch (Exception expected) {
                             }
                         }
-
-                        if (guessedPath == null) {
-                            notifier.fireTestFailure(new Failure(Description.createSuiteDescription(SubstrateTest.class), new RuntimeException("Unable to find native image, make sure native.image.path is set")));
-                            return;
-                        } else {
-                            String errorString = "\n=native.image.path was not set, making a guess that  " + guessedPath + " is the correct native image=";
-                            for (int i = 0; i < errorString.length(); ++i) {
-                                System.err.print("=");
-                            }
-                            System.err.println(errorString);
-                            for (int i = 0; i < errorString.length(); ++i) {
-                                System.err.print("=");
-                            }
-                            System.err.println();
-                            path = guessedPath;
+                        if (!ok) {
+                            throw new RuntimeException("Unable to start native image in " + IMAGE_WAIT_TIME + "ms");
                         }
-                    }
-                    System.out.println("Executing " + path);
-                    final Process testProcess = Runtime.getRuntime().exec(path);
-                    notifier.addListener(new RunListener() {
-                        @Override
-                        public void testRunFinished(Result result) throws Exception {
-                            super.testRunFinished(result);
-                            testProcess.destroy();
-                        }
-                    });
-                    new Thread(new ProcessReader(testProcess.getInputStream())).start();
-                    new Thread(new ProcessReader(testProcess.getErrorStream())).start();
-
-                    int port = Integer.getInteger("http.port", 8080);
-                    long bailout = System.currentTimeMillis() + IMAGE_WAIT_TIME;
-                    boolean ok = false;
-                    while (System.currentTimeMillis() < bailout) {
-                        try {
-                            Thread.sleep(100);
-                            try (Socket s = new Socket()) {
-                                s.connect(new InetSocketAddress("localhost", port));
-                                ok = true;
-                                break;
-                            }
-                        } catch (Exception expected) {
-                        }
-                    }
-                    if (!ok) {
-                        throw new RuntimeException("Unable to start native image in " + IMAGE_WAIT_TIME + "ms");
                     }
                 }
             });

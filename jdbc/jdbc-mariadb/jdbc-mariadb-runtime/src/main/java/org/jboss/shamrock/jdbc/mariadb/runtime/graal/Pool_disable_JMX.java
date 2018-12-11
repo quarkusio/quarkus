6b1bbd56446ae9c6a9061ffd2b888dcc1b4a72b0
Sanne Grinovero <sanne@hibernate.org>
2018-12-11 10:36:40 +0000

    MariaDB - Avoid registering via JMX

---
+++ jdbc/jdbc-mariadb/jdbc-mariadb-runtime/src/main/java/org/jboss/shamrock/jdbc/mariadb/runtime/graal/Pool_disable_JMX.java
@@ -0,0 +1,25 @@
+package org.jboss.shamrock.jdbc.mariadb.runtime.graal;
+
+import org.mariadb.jdbc.internal.logging.Logger;
+
+import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+
+@TargetClass(org.mariadb.jdbc.internal.util.pool.Pool.class)
+public final class Pool_disable_JMX {
+
+    @Alias
+    private static final Logger logger = null;
+
+    @Substitute
+    private void registerJmx() throws Exception {
+        logger.warn("MariaDB driver can't register to JMX MBeans in GraalVM: request to register ignored");
+    }
+
+    @Substitute
+    private void unRegisterJmx() throws Exception {
+        //no-op
+    }
+
+}

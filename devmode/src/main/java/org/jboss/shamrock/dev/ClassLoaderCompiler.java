76ba2903df4bbd87ae7d625d8e47b0f936e22970
Stuart Douglas <stuart.w.douglas@gmail.com>
2018-12-17 22:28:17 +1100

    Improve error checking

--- devmode/src/main/java/org/jboss/shamrock/dev/ClassLoaderCompiler.java
+++ devmode/src/main/java/org/jboss/shamrock/dev/ClassLoaderCompiler.java
@@ -74,7 +74,7 @@ public class ClassLoaderCompiler {
             if (!parsedFiles.contains(s)) {
                 parsedFiles.add(s);
                 File file = new File(s);
-                if (file.exists()) {
+                if (file.exists() && file.getName().endsWith(".jar")) {
                     classPathElements.add(file);
                     if(!file.isDirectory()) {
                         try (JarFile jar = new JarFile(file)) {
@@ -91,6 +91,8 @@ public class ClassLoaderCompiler {
                                     }
                                 }
                             }
+                        } catch (Exception e) {
+                            throw new RuntimeException("Failed to open class path file " + file, e);
                         }
                     }
                 }

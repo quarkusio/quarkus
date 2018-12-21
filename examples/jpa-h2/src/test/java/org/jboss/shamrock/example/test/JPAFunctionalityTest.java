b0f906d9363f93fcc42736a432e8f1c39dd77d06
Guillaume Smet <guillaume.smet@gmail.com>
2018-12-21 16:09:35 +0100

    Use H2 to run the JPA integration tests and the strict example
    For the strict example, it's still possible to test with PostgreSQL by
    using -Dtest-postgresql (and -Ddocker if you want to start a Docker
    container).

--- examples/jpa-h2/src/test/java/org/jboss/shamrock/example/test/JPAFunctionalityTest.java
+++ examples/jpa-h2/src/test/java/org/jboss/shamrock/example/test/JPAFunctionalityTest.java
@@ -2,10 +2,8 @@ package org.jboss.shamrock.example.test;
 
 import static org.junit.Assert.assertEquals;
 
-import org.jboss.shamrock.example.testutils.H2DatabaseLifecycleManager;
 import org.jboss.shamrock.test.ShamrockTest;
 import org.jboss.shamrock.test.URLTester;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,9 +14,6 @@ import org.junit.runner.RunWith;
  */
 @RunWith(ShamrockTest.class)
 public class JPAFunctionalityTest {
-
-    @ClassRule
-    public static final H2DatabaseLifecycleManager h2 = new H2DatabaseLifecycleManager();
 
     @Test
     public void testJPAFunctionalityFromServlet() throws Exception {

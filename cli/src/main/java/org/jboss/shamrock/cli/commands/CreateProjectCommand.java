a92e1879a8064a3f8b6905c69f60d0ae67d65327
Ståle Pedersen <stalep@gmail.com>
2019-01-08 15:15:09 +0100

    added functionality to create a pom.xml. atm fairly static.

--- cli/src/main/java/org/jboss/shamrock/cli/commands/CreateProjectCommand.java
+++ cli/src/main/java/org/jboss/shamrock/cli/commands/CreateProjectCommand.java
@@ -8,6 +8,8 @@ import org.aesh.command.invocation.CommandInvocation;
 import org.aesh.command.option.Option;
 import org.aesh.io.Resource;
 
+import java.io.File;
+
 /**
  * @author <a href="mailto:stalep@gmail.com">Ståle Pedersen</a>
  */
@@ -16,6 +18,15 @@ public class CreateProjectCommand implements Command<CommandInvocation>{
 
     @Option(shortName = 'h', hasValue = false)
     private boolean help;
+
+    @Option(shortName = 'g', defaultValue = "com.acme")
+    private String groupid;
+
+    @Option(shortName = 'a', defaultValue = "shamrocks")
+    private String artifactid;
+
+    @Option(shortName = 'v', defaultValue = "1.0.0-SNAPSHOT")
+    private String version;
 
     @Option(shortName = 'p', description = "path for the project")
     private Resource path;
@@ -26,7 +37,17 @@ public class CreateProjectCommand implements Command<CommandInvocation>{
             return CommandResult.SUCCESS;
         }
 
-        commandInvocation.println("here we'll create a project in: "+path);
+        if(path != null) {
+            CreateProject createProject = new CreateProject(groupid, artifactid, version);
+
+            boolean status = createProject.doCreateProject(new File(path.getAbsolutePath()));
+            if(status)
+                commandInvocation.println("Project "+artifactid+" created successfully.");
+            else
+                commandInvocation.println("Failed to create project");
+        }
+        else
+            commandInvocation.println("You need to set the path for the project");
 
         return CommandResult.SUCCESS;
     }

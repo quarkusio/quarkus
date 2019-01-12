6a8486a4cca5b177d013bfc9844ff5ef3bea5bae
Clement Escoffier <clement.escoffier@gmail.com>
2019-01-12 14:51:11 +0100

    update copyright year

--- extensions/reactive-messaging/deployment/src/main/java/org/jboss/shamrock/reactivemessaging/deployment/ReactiveMessagingProcessor.java
+++ extensions/reactive-messaging/deployment/src/main/java/org/jboss/shamrock/reactivemessaging/deployment/ReactiveMessagingProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Red Hat, Inc.
+ * Copyright 2019 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

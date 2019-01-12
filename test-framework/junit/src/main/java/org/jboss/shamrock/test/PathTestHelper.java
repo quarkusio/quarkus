1668204db93d9d7786f3396ff372728d4000bb92
Jeff MAURY <jmaury@redhat.com>
2019-01-12 09:08:38 +0100

    Update PathTestHelper to be Windows friendly
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- test-framework/junit/src/main/java/org/jboss/shamrock/test/PathTestHelper.java
+++ test-framework/junit/src/main/java/org/jboss/shamrock/test/PathTestHelper.java
@@ -1,28 +1,34 @@
 package org.jboss.shamrock.test;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 final class PathTestHelper {
 
-    private static final String TEST_CLASSES_FRAGMENT = "/test-classes";
-    private static final String CLASSES_FRAGMENT = "/classes";
+    private static final String TEST_CLASSES_FRAGMENT = File.separator + "test-classes";
+    private static final String CLASSES_FRAGMENT = File.separator + "classes";
 
     private PathTestHelper() {
     }
 
     public static final Path getTestClassesLocation(Class<?> testClass) {
-        String classFileName = testClass.getName().replace('.', '/') + ".class";
+        String classFileName = testClass.getName().replace('.', File.separatorChar) + ".class";
         URL resource = testClass.getClassLoader().getResource(classFileName);
 
-        String path = resource.getPath();
+		try {
+			Path path = Paths.get(resource.toURI());
+	        if (!path.toString().contains(TEST_CLASSES_FRAGMENT)) {
+	            throw new RuntimeException("The test class " + testClass + " is not located in the " + TEST_CLASSES_FRAGMENT + " directory.");
+	        }
 
-        if (!path.contains(TEST_CLASSES_FRAGMENT)) {
-            throw new RuntimeException("The test class " + testClass + " is not located in the " + TEST_CLASSES_FRAGMENT + " directory.");
-        }
+	        return path.subpath(0, path.getNameCount() - Paths.get(classFileName).getNameCount());
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
 
-        return Paths.get(resource.getPath().substring(0, resource.getPath().length() - classFileName.length()));
     }
 
     public static final Path getAppClassLocation(Class<?> testClass) {

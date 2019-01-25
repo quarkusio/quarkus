983ad5ba829dd37d9ab909441a8aec77be8a0d51
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-01-25 13:48:36 +1100

    Fixes #635, metrics processors are not unregistered on undeployment

--- extensions/metrics/runtime/src/main/java/org/jboss/shamrock/metrics/runtime/MetricsDeploymentTemplate.java
+++ extensions/metrics/runtime/src/main/java/org/jboss/shamrock/metrics/runtime/MetricsDeploymentTemplate.java
@@ -20,6 +20,7 @@ import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.ThreadMXBean;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.microprofile.metrics.Metadata;
@@ -27,6 +28,7 @@ import org.eclipse.microprofile.metrics.MetricRegistry;
 import org.eclipse.microprofile.metrics.MetricType;
 import org.jboss.logging.Logger;
 import org.jboss.shamrock.arc.runtime.BeanContainer;
+import org.jboss.shamrock.runtime.ShutdownContext;
 import org.jboss.shamrock.runtime.Template;
 
 import io.smallrye.metrics.MetricRegistries;
@@ -39,6 +41,9 @@ import io.smallrye.metrics.app.CounterImpl;
 public class MetricsDeploymentTemplate {
 
     private static final Logger log = Logger.getLogger("org.jboss.shamrock.metrics");
+    private static final String MEMORY_HEAP_USAGE = "memory.heap.usage";
+    private static final String MEMORY_NON_HEAP_USAGE = "memory.nonHeap.usage";
+    private static final String THREAD_COUNT = "thread.count";
 
     /*
             public <E extends Member & AnnotatedElement> void registerCounted(Class<?> topClass, E element) {
@@ -50,49 +55,77 @@ public class MetricsDeploymentTemplate {
                 //registry.register(meta, new CounterImpl());
             }
             */
-    public void registerCounted(String topClassName, String elementName) {
+    public void registerCounted(String topClassName, String elementName, ShutdownContext shutdown) {
         MetricRegistry registry = MetricRegistries.get(MetricRegistry.Type.APPLICATION);
 
         String name = MetricRegistry.name(topClassName, elementName);
         Metadata meta = new Metadata(name, MetricType.COUNTER);
         log.debugf("Register: %s", name);
         registry.register(meta, new CounterImpl());
+        shutdown.addShutdownTask(new Runnable() {
+            @Override
+            public void run() {
+                registry.remove(name);
+            }
+        });
     }
 
-    public void registerBaseMetrics() {
+    public void registerBaseMetrics(ShutdownContext shutdown) {
         MetricRegistry registry = MetricRegistries.get(MetricRegistry.Type.BASE);
         List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
+        List<String> names = new ArrayList<>();
         for (GarbageCollectorMXBean gc : gcs) {
             Metadata meta = new Metadata("gc." + gc.getName() + ".count", MetricType.COUNTER);
             meta.setDisplayName("Garbage Collection Time");
             meta.setUnit("none");
             meta.setDescription("Displays the total number of collections that have occurred. This attribute lists -1 if the collection count is undefined for this collector.");
             registry.register(meta, new LambdaCounter(() -> gc.getCollectionCount()));
+            names.add(meta.getName());
 
             meta = new Metadata("gc." + gc.getName() + ".time", MetricType.COUNTER);
             meta.setDisplayName("Garbage Collection Time");
             meta.setUnit("milliseconds");
             meta.setDescription("machine implementation may use a high resolution timer to measure the elapsed time. This attribute may display the same value even if the collection count has been incremented if the collection elapsed time is very short.");
             registry.register(meta, new LambdaCounter(() -> gc.getCollectionTime()));
+            names.add(meta.getName());
+
         }
+        shutdown.addShutdownTask(new Runnable() {
+            @Override
+            public void run() {
+                for(String i : names) {
+                    registry.remove(i);
+                }
+            }
+        });
     }
 
-    public void registerVendorMetrics() {
+    public void registerVendorMetrics(ShutdownContext shutdown) {
         MetricRegistry registry = MetricRegistries.get(MetricRegistry.Type.VENDOR);
         MemoryMXBean mem = ManagementFactory.getMemoryMXBean();
 
-        Metadata meta = new Metadata("memory.heap.usage", MetricType.GAUGE);
+        Metadata meta = new Metadata(MEMORY_HEAP_USAGE, MetricType.GAUGE);
         meta.setUnit("bytes");
         registry.register(meta, new LambdaGauge(() -> mem.getHeapMemoryUsage().getUsed()));
 
-        meta = new Metadata("memory.nonHeap.usage", MetricType.GAUGE);
+        meta = new Metadata(MEMORY_NON_HEAP_USAGE, MetricType.GAUGE);
         meta.setUnit("bytes");
         registry.register(meta, new LambdaGauge(() -> mem.getNonHeapMemoryUsage().getUsed()));
 
         ThreadMXBean thread = ManagementFactory.getThreadMXBean();
 
-        meta = new Metadata("thread.count", MetricType.COUNTER);
+        meta = new Metadata(THREAD_COUNT, MetricType.COUNTER);
         registry.register(meta, new LambdaCounter( ()-> (long) thread.getThreadCount() ) );
+
+        shutdown.addShutdownTask(new Runnable() {
+            @Override
+            public void run() {
+                registry.remove(MEMORY_HEAP_USAGE);
+                registry.remove(MEMORY_NON_HEAP_USAGE);
+                registry.remove(THREAD_COUNT);
+
+            }
+        });
 
         /*
         meta = new Metadata("thread.cpuTime", MetricType.COUNTER);

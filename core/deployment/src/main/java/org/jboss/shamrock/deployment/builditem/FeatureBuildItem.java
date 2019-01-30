3edb7f67b69d6956552799a120a139195c7c769a
Guillaume Smet <guillaume.smet@gmail.com>
2019-01-30 19:19:11 +0100

    Fix Bean Validation extension feature name

--- core/deployment/src/main/java/org/jboss/shamrock/deployment/builditem/FeatureBuildItem.java
+++ core/deployment/src/main/java/org/jboss/shamrock/deployment/builditem/FeatureBuildItem.java
@@ -22,13 +22,13 @@ public final class FeatureBuildItem extends MultiBuildItem {
     public static final String MP_REACTIVE_OPERATORS = "mp-reactive-streams-operators";
     public static final String MP_REACTIVE_MESSAGING = "mp-reactive-streams-messaging";
     public static final String REACTIVE_CONVERTERS = "reactive-type-converters";
-    public static final String BEAN_VALIDATION = "mp-bean-validation";
+    public static final String BEAN_VALIDATION = "bean-validation";
     public static final String JPA = "jpa";
     public static final String SCHEDULER = "scheduler";
     public static final String VERTX = "vertx";
-    
+
     private final String info;
-    
+
     public FeatureBuildItem(String info) {
         this.info = Objects.requireNonNull(info);
     }

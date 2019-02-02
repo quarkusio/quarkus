ed216d457fcbf9b21096143f8f1ba82c0fcc12cd
Clement Escoffier <clement.escoffier@gmail.com>
2019-02-02 16:12:14 +0100

    Improve mail client

---
+++ independent-projects/vertx-axle-clients/vertx-axle-mail-client/src/test/java/io/vertx/axle/mail/MailClientTest.java
@@ -0,0 +1,38 @@
+package io.vertx.axle.mail;
+
+import io.vertx.axle.core.Vertx;
+import io.vertx.axle.ext.mail.MailClient;
+import io.vertx.ext.mail.MailConfig;
+import io.vertx.ext.mail.MailMessage;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class MailClientTest {
+
+    @Rule
+    public GenericContainer container = new GenericContainer("digiplant/fake-smtp")
+            .withExposedPorts(25)
+            .withFileSystemBind("target", "/tmp/fakemail", BindMode.READ_WRITE);
+
+    @Test
+    public void testAxleAPI() {
+        Vertx vertx = Vertx.vertx();
+        assertThat(vertx, is(notNullValue()));
+        MailClient client = MailClient.createShared(vertx, new MailConfig()
+                .setPort(container.getMappedPort(25))
+                .setHostname(container.getContainerIpAddress())
+        );
+        assertThat(client, is(notNullValue()));
+        client.sendMail(new MailMessage().setText("hello Axle")
+                .setSubject("test email")
+                .setTo("clement@apache.org")
+                .setFrom("clement@apache.org"))
+                .toCompletableFuture().join();
+    }
+}

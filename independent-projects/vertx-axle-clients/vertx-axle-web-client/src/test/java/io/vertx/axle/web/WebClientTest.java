f93fe8bde6511538d8575f4f71bea487e86fb96c
Clement Escoffier <clement.escoffier@gmail.com>
2019-02-02 16:13:27 +0100

    Add web client
    Note that the web client needs 2 modules: vertx-web-client and vertx-web-common

---
+++ independent-projects/vertx-axle-clients/vertx-axle-web-client/src/test/java/io/vertx/axle/web/WebClientTest.java
@@ -0,0 +1,39 @@
+package io.vertx.axle.web;
+
+import io.vertx.axle.core.Vertx;
+import io.vertx.axle.ext.web.client.HttpResponse;
+import io.vertx.axle.ext.web.client.WebClient;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClientOptions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class WebClientTest {
+
+    @Rule
+    public GenericContainer container = new GenericContainer("kennethreitz/httpbin")
+            .withExposedPorts(80)
+            .withFileSystemBind("target", "/tmp/fakemail", BindMode.READ_WRITE);
+
+    @Test
+    public void testWebClient() {
+        Vertx vertx = Vertx.vertx();
+        assertThat(vertx, is(notNullValue()));
+        WebClient client = WebClient.create(vertx, new WebClientOptions()
+                .setDefaultPort(container.getMappedPort(80))
+                .setDefaultHost(container.getContainerIpAddress())
+        );
+        assertThat(client, is(notNullValue()));
+
+        JsonObject object = client.get("/get?msg=hello").send()
+                .thenApply(HttpResponse::bodyAsJsonObject)
+                .toCompletableFuture().join();
+        assertThat(object.getJsonObject("args").getString("msg"), is("hello"));
+    }
+}

2544cc1b4a63cae5f73af4ecd60e93b402f41d17
Clement Escoffier <clement.escoffier@gmail.com>
2019-02-02 16:13:37 +0100

    Add MongoDB client

---
+++ independent-projects/vertx-axle-clients/vertx-axle-mongo-client/src/test/java/io/vertx/axle/mongo/MongoClientTest.java
@@ -0,0 +1,38 @@
+package io.vertx.axle.mongo;
+
+import io.vertx.axle.core.Vertx;
+import io.vertx.axle.ext.mongo.MongoClient;
+import io.vertx.core.json.JsonObject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MongoClientTest {
+
+    @Rule
+    public GenericContainer container = new GenericContainer("mongo")
+            .withExposedPorts(27017);
+
+    @Test
+    public void testAxleAPI() {
+        Vertx vertx = Vertx.vertx();
+        assertThat(vertx).isNotNull();
+        MongoClient client = MongoClient.createShared(vertx, new JsonObject()
+                .put("db_name", "axle-test")
+                .put("connection_string", "mongodb://" + container.getContainerIpAddress()
+                        + ":" + container.getMappedPort(27017)));
+
+        JsonObject document = new JsonObject().put("title", "The Hobbit");
+        List<JsonObject> list = client.save("books", document)
+                .thenCompose(x -> client.find("books", new JsonObject().put("title", "The Hobbit")))
+                .toCompletableFuture()
+                .join();
+
+        assertThat(list).hasSize(1)
+                .allMatch(json -> json.getString("title").equalsIgnoreCase("The Hobbit"));
+    }
+}

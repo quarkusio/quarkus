1bb5d9b8453db4b7b98cd26b4dede1a70b3c12c7
Gary Brown <gary@brownuk.com>
2019-02-05 15:58:12 +0000

    Jaeger SSL/TrustManager fix

--- core/deployment/src/main/java/org/jboss/shamrock/deployment/builditem/FeatureBuildItem.java
+++ core/deployment/src/main/java/org/jboss/shamrock/deployment/builditem/FeatureBuildItem.java
@@ -26,6 +26,7 @@ public final class FeatureBuildItem extends MultiBuildItem {
     public static final String REACTIVE_CONVERTERS = "reactive-type-converters";
     public static final String HIBERNATE_VALIDATOR = "hibernate-validator";
     public static final String JPA = "jpa";
+    public static final String JAEGER = "jaeger";
     public static final String SCHEDULER = "scheduler";
     public static final String SECURITY = "security";
     public static final String VERTX = "vertx";

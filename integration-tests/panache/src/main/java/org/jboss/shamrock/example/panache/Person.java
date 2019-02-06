b0c6fcaeef7958d038f19dc70481c9ce4e6968f6
Stephane Epardaud <stef@epardaud.fr>
2019-02-06 12:21:42 +0100

    Add integration tests for the Panache extension

---
+++ integration-tests/panache/src/main/java/org/jboss/shamrock/example/panache/Person.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.shamrock.example.panache;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.jboss.panache.jpa.Model;
+
+@Entity
+public class Person extends Model {
+
+    public String name;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    public Address address;
+    
+    public Status status;
+    
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    public List<Dog> dogs = new ArrayList<>();
+}

180d2a41f44166b64962cead20657a2f251cd231
Guillaume Smet <guillaume.smet@gmail.com>
2019-02-11 17:29:02 +0100

    Reorder extensions in FeatureBuildItem following the renaming

--- core/deployment/src/main/java/org/jboss/shamrock/deployment/builditem/FeatureBuildItem.java
+++ core/deployment/src/main/java/org/jboss/shamrock/deployment/builditem/FeatureBuildItem.java
@@ -11,25 +11,25 @@ public final class FeatureBuildItem extends MultiBuildItem {
 
     public static final String AGROAL = "agroal";
     public static final String CDI = "cdi";
-    public static final String NARAYANA_JTA = "narayana-jta";
-    public static final String UNDERTOW_WEBSOCKETS = "undertow-websockets";
+    public static final String HIBERNATE_VALIDATOR = "hibernate-validator";
+    public static final String JAEGER = "jaeger";
     public static final String JAXRS = "jaxrs";
     public static final String JAXRS_JSON = "jaxrs-json";
+    public static final String JPA = "jpa";
+    public static final String MP_REST_CLIENT = "mp-rest-client";
+    public static final String MP_REACTIVE_OPERATORS = "mp-reactive-streams-operators";
+    public static final String MP_REACTIVE_MESSAGING = "mp-reactive-streams-messaging";
+    public static final String NARAYANA_JTA = "narayana-jta";
+    public static final String REACTIVE_CONVERTERS = "reactive-type-converters";
+    public static final String SCHEDULER = "scheduler";
+    public static final String SECURITY = "security";
     public static final String SMALLRYE_HEALTH = "smallrye-health";
     public static final String SMALLRYE_OPENAPI = "smallrye-openapi";
     public static final String SMALLRYE_METRICS = "smallrye-metrics";
     public static final String SMALLRYE_FAULT_TOLERANCE = "smallrye-fault-tolerance";
     public static final String SMALLRYE_OPENTRACING = "smallrye-opentracing";
-    public static final String MP_REST_CLIENT = "mp-rest-client";
-    public static final String MP_REACTIVE_OPERATORS = "mp-reactive-streams-operators";
-    public static final String MP_REACTIVE_MESSAGING = "mp-reactive-streams-messaging";
-    public static final String REACTIVE_CONVERTERS = "reactive-type-converters";
-    public static final String HIBERNATE_VALIDATOR = "hibernate-validator";
-    public static final String JPA = "jpa";
-    public static final String JAEGER = "jaeger";
-    public static final String SCHEDULER = "scheduler";
-    public static final String SECURITY = "security";
     public static final String SPRING_DI = "spring-di";
+    public static final String UNDERTOW_WEBSOCKETS = "undertow-websockets";
     public static final String VERTX = "vertx";
 
     private final String info;

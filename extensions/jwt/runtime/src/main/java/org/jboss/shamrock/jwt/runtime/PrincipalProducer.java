ae3517a0467a45a4360b468072bcb167f1433c32
Scott Stark <starksm64@gmail.com>
2019-02-11 13:16:11 -0800

    Update to RequestScoped

--- extensions/jwt/runtime/src/main/java/org/jboss/shamrock/jwt/runtime/PrincipalProducer.java
+++ extensions/jwt/runtime/src/main/java/org/jboss/shamrock/jwt/runtime/PrincipalProducer.java
@@ -2,13 +2,15 @@ package org.jboss.shamrock.jwt.runtime;
 
 import javax.annotation.Priority;
 import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Alternative;
 import javax.enterprise.inject.Produces;
 
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
 /**
- * Override the default CDI Principal bean to allow the injection of a Principal to be a JsonWebToken
+ * Override the default CDI Principal bean to allow the injection of a Principal to be both a
+ * {@linkplain JsonWebToken} and a {@linkplain java.security.Principal}.
  */
 @Priority(1)
 @Alternative
@@ -20,7 +22,7 @@ public class PrincipalProducer {
      * @return
      */
     @Produces
-    @Dependent
+    @RequestScoped
     JsonWebToken currentJWTPrincipalOrNull() {
         return MPJWTProducer.getJWTPrincpal();
     }

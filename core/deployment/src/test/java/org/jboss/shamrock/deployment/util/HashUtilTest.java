fd8cbc1d27026d78a61591198bdad7b8137106be
Guillaume Smet <guillaume.smet@gmail.com>
2019-02-17 12:49:08 +0100

    Centralize sha1 implementation
    And make it more general purpose: using Base64 might work for file names
    but it doesn't work for Java identifiers (typically method) as it can
    contain characters that are not valid.
    
    Also modified Arc implementation.

---
+++ core/deployment/src/test/java/org/jboss/shamrock/deployment/util/HashUtilTest.java
@@ -0,0 +1,14 @@
+package org.jboss.shamrock.deployment.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class HashUtilTest {
+
+    @Test
+    public void testSha1() {
+        assertEquals("a94a8fe5ccb19ba61c4c0873d391e987982fbbd3", HashUtil.sha1("test"));
+    }
+}
--- independent-projects/arc/processor/src/main/java/org/jboss/protean/arc/processor/Hashes.java
+++ independent-projects/arc/processor/src/main/java/org/jboss/protean/arc/processor/Hashes.java
@@ -18,15 +18,18 @@ package org.jboss.protean.arc.processor;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 
 final class Hashes {
 
     static String sha1(String value) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
-            return Base64.getUrlEncoder()
-                    .encodeToString(md.digest(value.getBytes(StandardCharsets.UTF_8)));
+            byte[] digest = md.digest(value.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder(40);
+            for (int i = 0; i < digest.length; ++i) {
+                sb.append(Integer.toHexString((digest[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
         } catch (NoSuchAlgorithmException e) {
             throw new IllegalStateException(e);
         }

b2f4e444c4cc187371087ac4a419fbc8d90150d9
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-02-20 21:48:58 +1100

    Add Kafka extension

---
+++ integration-tests/main/src/test/java/org/jboss/shamrock/example/test/KafkaProducerITCase.java
@@ -0,0 +1,10 @@
+package org.jboss.shamrock.example.test;
+
+import org.jboss.shamrock.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class KafkaProducerITCase extends KafkaProducerTest {
+
+
+
+}

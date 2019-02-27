861d391e7e015ed6465202d134207d1a652fdefd
Stephane Epardaud <stef@epardaud.fr>
2019-02-27 12:04:01 +0100

    Added Panache javadocs

--- extensions/panache/panache-common/runtime/src/main/java/io/quarkus/panache/common/Sort.java
+++ extensions/panache/panache-common/runtime/src/main/java/io/quarkus/panache/common/Sort.java
@@ -3,9 +3,41 @@ package io.quarkus.panache.common;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <p>
+ * Utility class to build and represent SQL sorting specifications. A {@link Sort} instance represents
+ * a list of columns to sort on, each with a direction to use for sorting.
+ * </p>
+ * 
+ * <p>
+ * Usage:
+ * </p>
+ * 
+ * <code><pre>
+ * Sort sort = Sort.by("name").and("age", Direction.Descending);
+ * Sort sort2 = Sort.ascending("name", "age");
+ * Sort sort3 = Sort.descending("name", "age");
+ * </pre></code>
+ *
+ * @author Stéphane Épardaud
+ * @see Direction
+ */
 public class Sort {
+
+    /**
+     * Represents an SQL direction in which to sort results.
+     *
+     * @author Stéphane Épardaud
+     */
     public enum Direction {
-        Ascending, Descending;
+        /**
+         * Sort in ascending order (the default).
+         */
+        Ascending,
+        /**
+         * Sort in descending order (opposite from the default).
+         */
+        Descending;
     }
 
     private class Column {
@@ -27,14 +59,41 @@ public class Sort {
     private Sort() {
     }
 
+    /**
+     * Sort by the given column, in ascending order.
+     * 
+     * @param column the column to sort on, in ascending order.
+     * @return a new Sort instance which sorts on the given column in ascending order.
+     * @see #by(String, Direction)
+     * @see #by(String...)
+     */
     public static Sort by(String column) {
         return new Sort().and(column);
     }
 
+    /**
+     * Sort by the given column, in the given order.
+     * 
+     * @param column the column to sort on, in the given order.
+     * @param direction the direction to sort in
+     * @return a new Sort instance which sorts on the given column in the given order.
+     * @see #by(String)
+     * @see #by(String...)
+     */
     public static Sort by(String column, Direction direction) {
         return new Sort().and(column, direction);
     }
 
+    /**
+     * Sort by the given columns, in ascending order. Equivalent to {@link #ascending(String...)}.
+     * 
+     * @param columns the columns to sort on, in ascending order.
+     * @return a new Sort instance which sorts on the given columns in ascending order.
+     * @see #by(String, Direction)
+     * @see #by(String)
+     * @see #ascending(String...)
+     * @see #descending(String...)
+     */
     public static Sort by(String... columns) {
         Sort sort = new Sort();
         for (String column : columns) {
@@ -43,10 +102,29 @@ public class Sort {
         return sort;
     }
 
+    /**
+     * Sort by the given columns, in ascending order. Equivalent to {@link #by(String...)}.
+     * 
+     * @param columns the columns to sort on, in ascending order.
+     * @return a new Sort instance which sorts on the given columns in ascending order.
+     * @see #by(String, Direction)
+     * @see #by(String)
+     * @see #by(String...)
+     * @see #descending(String...)
+     */
     public static Sort ascending(String... columns) {
         return by(columns);
     }
 
+    /**
+     * Sort by the given columns, in descending order.
+     * 
+     * @param columns the columns to sort on, in descending order.
+     * @return a new Sort instance which sorts on the given columns in descending order.
+     * @see #by(String, Direction)
+     * @see #by(String)
+     * @see #descending(String...)
+     */
     public static Sort descending(String... columns) {
         Sort sort = new Sort();
         for (String column : columns) {
@@ -55,14 +133,36 @@ public class Sort {
         return sort;
     }
 
+    /**
+     * Sets the order to descending for all current sort columns.
+     * 
+     * @return this instance, modified.
+     * @see #ascending()
+     * @see #direction(Direction)
+     */
     public Sort descending() {
         return direction(Direction.Descending);
     }
 
+    /**
+     * Sets the order to ascending for all current sort columns.
+     * 
+     * @return this instance, modified.
+     * @see #descending()
+     * @see #direction(Direction)
+     */
     public Sort ascending() {
         return direction(Direction.Ascending);
     }
 
+    /**
+     * Sets the order to all current sort columns.
+     * 
+     * @param direction the direction to use for all current sort columns.
+     * @return this instance, modified.
+     * @see #descending()
+     * @see #ascending()
+     */
     public Sort direction(Direction direction) {
         for (Column column : columns) {
             column.direction = direction;
@@ -70,16 +170,35 @@ public class Sort {
         return this;
     }
 
+    /**
+     * Adds a sort column, in ascending order.
+     * 
+     * @param name the new column to sort on, in ascending order.
+     * @return this instance, modified.
+     * @see #and(String, Direction)
+     */
     public Sort and(String name) {
         columns.add(new Column(name));
         return this;
     }
 
+    /**
+     * Adds a sort column, in the given order.
+     * 
+     * @param name the new column to sort on, in the given order.
+     * @return this instance, modified.
+     * @see #and(String)
+     */
     public Sort and(String name, Direction direction) {
         columns.add(new Column(name, direction));
         return this;
     }
 
+    /**
+     * Returns an SQL "order by" clause for the current sort columns and directions.
+     * 
+     * @return an SQL "order by" clause for the current sort columns and directions.
+     */
     public String toOrderBy() {
         StringBuilder sb = new StringBuilder(" ORDER BY ");
         for (int i = 0; i < columns.size(); i++) {
--- extensions/panache/panache-jpa/runtime/src/main/java/io/quarkus/panache/jpa/PanacheQuery.java
+++ extensions/panache/panache-jpa/runtime/src/main/java/io/quarkus/panache/jpa/PanacheQuery.java
@@ -5,11 +5,26 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 import javax.persistence.Query;
 
 import io.quarkus.panache.common.Page;
 import io.quarkus.panache.jpa.impl.JpaOperations;
 
+/**
+ * <p>
+ * Class representing an entity query, which abstracts the use of paging, getting the number of results, and
+ * operating on {@link List} or {@link Stream}.
+ * </p>
+ * <p>
+ * Instances of this class cannot mutate the query itself or its parameters: only paging information can be
+ * modified, and instances of this class can be reused to obtain multiple pages of results.
+ * </p>
+ *
+ * @author Stéphane Épardaud
+ * @param <Entity> The entity type being queried
+ */
 public class PanacheQuery<Entity> {
 
     private Query jpaQuery;
@@ -34,6 +49,13 @@ public class PanacheQuery<Entity> {
 
     // Builder
 
+    /**
+     * Sets the current page.
+     * @param page the new page
+     * @return this query, modified
+     * @see #page(int, int)
+     * @see #page()
+     */
     @SuppressWarnings("unchecked")
     public <T extends Entity> PanacheQuery<T> page(Page page) {
         this.page = page;
@@ -41,34 +63,80 @@ public class PanacheQuery<Entity> {
         return (PanacheQuery<T>) this;
     }
 
+    /**
+     * Sets the current page.
+     * @param pageIndex the page index
+     * @param pageSize the page size
+     * @return this query, modified
+     * @see #page(Page)
+     * @see #page()
+     */
     public <T extends Entity> PanacheQuery<T> page(int pageIndex, int pageSize) {
         return page(Page.of(pageIndex, pageSize));
     }
 
+    /**
+     * Sets the current page to the next page
+     * @return this query, modified
+     * @see #previousPage()
+     */
     public <T extends Entity> PanacheQuery<T> nextPage() {
         return page(page.next());
     }
 
+    /**
+     * Sets the current page to the previous page (or the first page if there is no previous page)
+     * @return this query, modified
+     * @see #nextPage()
+     */
     public <T extends Entity> PanacheQuery<T> previousPage() {
         return page(page.previous());
     }
 
+    /**
+     * Sets the current page to the first page
+     * @return this query, modified
+     * @see #lastPage()
+     */
     public <T extends Entity> PanacheQuery<T> firstPage() {
         return page(page.first());
     }
 
+    /**
+     * Sets the current page to the last page. This will cause reading of the entity count.
+     * @return this query, modified
+     * @see #firstPage()
+     * @see #count()
+     */
     public <T extends Entity> PanacheQuery<T> lastPage() {
         return page(page.index(pageCount() - 1));
     }
 
+    /**
+     * Returns true if there is another page to read after the current one.
+     * This will cause reading of the entity count.
+     * @return true if there is another page to read
+     * @see #hasPreviousPage()
+     * @see #count()
+     */
     public boolean hasNextPage() {
         return page.index < (pageCount() - 1);
     }
 
+    /**
+     * Returns true if there is a page to read before the current one.
+     * @return true if there is a previous page to read
+     * @see #hasNextPage()
+     */
     public boolean hasPreviousPage() {
         return page.index > 0;
     }
 
+    /**
+     * Returns the total number of pages to be read using the current page size.
+     * This will cause reading of the entity count.
+     * @return the total number of pages to be read using the current page size.
+     */
     public int pageCount() {
         long count = count();
         if (count == 0)
@@ -76,12 +144,24 @@ public class PanacheQuery<Entity> {
         return (int) Math.ceil((double) count / (double) page.size);
     }
 
+    /**
+     * Returns the current page.
+     * @return the current page
+     * @see #page(Page)
+     * @see #page(int,int)
+     */
     public Page page() {
         return page;
     }
 
     // Results
 
+    /**
+     * Reads and caches the total number of entities this query operates on. This causes a database
+     * query with <code>SELECT COUNT(*)</code> and a query equivalent to the current query, minus
+     * ordering.
+     * @return the total number of entities this query operates on, cached.
+     */
     @SuppressWarnings("unchecked")
     public long count() {
         if (count == null) {
@@ -100,24 +180,51 @@ public class PanacheQuery<Entity> {
         return count;
     }
 
+    /**
+     * Returns the current page of results as a {@link List}.
+     * @return the current page of results as a {@link List}.
+     * @see #stream()
+     * @see #page(Page)
+     * @see #page()
+     */
     @SuppressWarnings("unchecked")
     public <T extends Entity> List<T> list() {
         jpaQuery.setMaxResults(page.size);
         return jpaQuery.getResultList();
     }
 
+    /**
+     * Returns the current page of results as a {@link Stream}.
+     * @return the current page of results as a {@link Stream}.
+     * @see #list()
+     * @see #page(Page)
+     * @see #page()
+     */
     @SuppressWarnings("unchecked")
     public <T extends Entity> Stream<T> stream() {
         jpaQuery.setMaxResults(page.size);
         return jpaQuery.getResultStream();
     }
 
+    /**
+     * Returns the first result of the current page index. This ignores the current page size to fetch
+     * a single result.
+     * @return the first result of the current page index, or null if there are no results.
+     * @see #singleResult()
+     */
     public <T extends Entity> T firstResult() {
         List<T> list = list();
         jpaQuery.setMaxResults(1);
         return list.isEmpty() ? null : list.get(0);
     }
 
+    /**
+     * Executes this query for the current page and return a single result.
+     * @return the single result (throws if there is not exactly one)
+     * @throws NoResultException if there is no result
+     * @throws NonUniqueResultException if there are more than one result
+     * @see #firstResult()
+     */
     @SuppressWarnings("unchecked")
     public <T extends Entity> T singleResult() {
         jpaQuery.setMaxResults(page.size);

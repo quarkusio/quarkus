23b502670dc70e11ac5272e61c7f84f39ed2c06f
Martin Kouba <mkouba@redhat.com>
2019-02-27 09:33:55 +0100

    Detect private no-arg constructor on normal scoped beans
    - and fail the deployment
    - resolves #1069

---
+++ independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/validation/NormalScopedPrivateNoArgsConstructorTest.java
@@ -0,0 +1,36 @@
+package io.quarkus.arc.test.validation;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.spi.DefinitionException;
+import javax.enterprise.inject.spi.DeploymentException;
+
+import io.quarkus.arc.test.ArcTestContainer;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class NormalScopedPrivateNoArgsConstructorTest {
+
+    @Rule
+    public ArcTestContainer container = ArcTestContainer.builder().beanClasses(Unproxyable.class).shouldFail().build();
+
+    @Test
+    public void testFailure() {
+        Throwable error = container.getFailure();
+        assertNotNull(error);
+        assertTrue(error instanceof DeploymentException);
+        assertNotNull(error.getCause());
+        assertTrue(error.getCause() instanceof DefinitionException);
+    }
+
+    @ApplicationScoped
+    static class Unproxyable {
+
+        private Unproxyable() {
+        }
+        
+    }
+
+}

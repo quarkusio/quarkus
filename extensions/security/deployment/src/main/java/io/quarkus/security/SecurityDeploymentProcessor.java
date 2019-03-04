25d1ceb7bc81f11f5875599108b65cf57ac2aebb
Scott Stark <starksm64@gmail.com>
2019-03-03 22:56:44 -0800

    No reason to do anything if there are no identity managers

--- extensions/security/deployment/src/main/java/io/quarkus/security/SecurityDeploymentProcessor.java
+++ extensions/security/deployment/src/main/java/io/quarkus/security/SecurityDeploymentProcessor.java
@@ -235,23 +235,26 @@ class SecurityDeploymentProcessor {
     void addIdentityManager(SecurityTemplate template, BuildProducer<ServletExtensionBuildItem> extension,
             SecurityDomainBuildItem securityDomain, List<IdentityManagerBuildItem> identityManagers,
             List<AuthConfigBuildItem> authConfigs) {
-        // Validate the at most one IdentityManagerBuildItem was created
+        // If there are no identityManagers, exit
+        if (identityManagers.size() == 0) {
+            return;
+        }
+
+        // Validate that at most one IdentityManagerBuildItem was created
         if (identityManagers.size() > 1) {
             throw new IllegalStateException("Multiple IdentityManagerBuildItem seen: " + identityManagers);
         }
-        // Only create an identityManager if one was configured
-        if (identityManagers.size() > 0) {
-            IdentityManagerBuildItem identityManager = identityManagers.get(0);
-            // Collect all of the authentication mechanisms and create a ServletExtension to register the Undertow identity manager
-            ArrayList<AuthConfig> allAuthConfigs = new ArrayList<>();
-            for (AuthConfigBuildItem authConfigExt : authConfigs) {
-                AuthConfig ac = authConfigExt.getAuthConfig();
-                allAuthConfigs.add(ac);
-            }
-            ServletExtension idmExt = template.configureUndertowIdentityManager(securityDomain.getSecurityDomain(),
-                    identityManager.getIdentityManager(), allAuthConfigs);
-            extension.produce(new ServletExtensionBuildItem(idmExt));
+        // Create the configured identity manager
+        IdentityManagerBuildItem identityManager = identityManagers.get(0);
+        // Collect all of the authentication mechanisms and create a ServletExtension to register the Undertow identity manager
+        ArrayList<AuthConfig> allAuthConfigs = new ArrayList<>();
+        for (AuthConfigBuildItem authConfigExt : authConfigs) {
+            AuthConfig ac = authConfigExt.getAuthConfig();
+            allAuthConfigs.add(ac);
         }
+        ServletExtension idmExt = template.configureUndertowIdentityManager(securityDomain.getSecurityDomain(),
+                identityManager.getIdentityManager(), allAuthConfigs);
+        extension.produce(new ServletExtensionBuildItem(idmExt));
     }
 
     /**

3357d7aefe2904a574de5d659341bcefce6b44b2
Michael Vorburger <vorburger@redhat.com>
2019-03-11 18:34:03 +0100

    Gradle IT - working better now, with separate Gradle invocations for building plugin and running IT (#1396)

---
+++ devtools/gradle/src/it/quickstart/src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -0,0 +1,35 @@
+package org.acme.quickstart;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
+    }
+
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .pathParam("name", uuid)
+                .when().get("/hello/greeting/{name}")
+                .then()
+                .statusCode(200)
+                .body(is("hello " + uuid));
+    }
+
+}

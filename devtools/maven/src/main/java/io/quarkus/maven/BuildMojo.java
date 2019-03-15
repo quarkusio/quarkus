7802843933cb1019fb6b55aacb0267989bb9755d
George Gastaldi <gegastaldi@gmail.com>
2019-03-15 01:11:13 -0300

    chore: add quarkus.uberJar maven property
    This allows to create a Uber Jar using `mvn clean package -Dquarkus.uberJar=true`

--- devtools/maven/src/main/java/io/quarkus/maven/BuildMojo.java
+++ devtools/maven/src/main/java/io/quarkus/maven/BuildMojo.java
@@ -122,7 +122,7 @@ public class BuildMojo extends AbstractMojo {
     @Parameter(defaultValue = "true")
     private boolean useStaticInit;
 
-    @Parameter(defaultValue = "false")
+    @Parameter(property = "quarkus.uberJar", defaultValue = "false")
     private boolean uberJar;
 
     public BuildMojo() {

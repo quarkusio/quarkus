bac2b84c339b0fd1adb5981d5ca3168824db8cbe
Andrea Cosentino <ancosen@gmail.com>
2019-03-16 13:00:13 +0100

    Fixes #1469 Add Extension for Camel-AWS S3

---
+++ integration-tests/camel-aws-s3/src/main/java/io/quarkus/camel/it/aws/CamelApplication.java
@@ -0,0 +1,8 @@
+package io.quarkus.camel.it.aws;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/test")
+public class CamelApplication extends Application {
+}
---
+++ integration-tests/camel-aws-s3/src/main/java/io/quarkus/camel/it/aws/CamelRoute.java
@@ -0,0 +1,20 @@
+package io.quarkus.camel.it.aws;
+
+import org.apache.camel.builder.RouteBuilder;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class CamelRoute extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        from("timer:quarkus?repeatCount=1")
+                .setHeader("CamelAwsS3Key", constant("testquarkus"))
+                .setBody(constant("Quarkus is great!"))
+                .to("aws-s3://devvox1")
+                .to("log:sf?showAll=true");
+
+    }
+
+}

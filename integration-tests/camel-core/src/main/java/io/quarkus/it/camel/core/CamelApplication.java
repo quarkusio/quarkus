788a18a367158f940dba0992c246ae3372f88cb8
Rostislav Svoboda <rsvoboda@redhat.com>
2019-04-05 23:12:39 +0200

    io.quarkus.it packages prefix for integration-tests module

---
+++ integration-tests/camel-core/src/main/java/io/quarkus/it/camel/core/CamelApplication.java
@@ -0,0 +1,8 @@
+package io.quarkus.it.camel.core;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/test")
+public class CamelApplication extends Application {
+}
---
+++ integration-tests/main/src/main/java/io/quarkus/it/rest/ProgrammaticRestInterface.java
@@ -0,0 +1,37 @@
+package io.quarkus.it.rest;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
+
+/**
+ * A version of {@link RestInterface} that doesn't have {@link org.eclipse.microprofile.rest.client.inject.RegisterRestClient}
+ * and can be used only programmatically, i.e. with the builder.
+ */
+@Path("/test")
+@RegisterClientHeaders
+public interface ProgrammaticRestInterface {
+
+    @GET
+    String get();
+
+    @GET
+    @Path("/jackson")
+    @Produces("application/json")
+    TestResource.MyData getData();
+
+    @GET
+    @Path("/complex")
+    @Produces("application/json")
+    List<ComponentType> complex();
+
+    @GET
+    @Path("/headers")
+    @Produces("application/json")
+    Map<String, String> getAllHeaders();
+}

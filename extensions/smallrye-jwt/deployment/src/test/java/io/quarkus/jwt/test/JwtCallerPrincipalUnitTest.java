a839823d95c2380a1224b0226cb1c96b8c6ac170
Sergey Beryozkin <sberyozkin@gmail.com>
2019-04-09 17:56:35 +0100

    Restoring the original unknown claim conversion rule

--- extensions/smallrye-jwt/deployment/src/test/java/io/quarkus/jwt/test/JwtCallerPrincipalUnitTest.java
+++ extensions/smallrye-jwt/deployment/src/test/java/io/quarkus/jwt/test/JwtCallerPrincipalUnitTest.java
@@ -9,6 +9,7 @@ import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonNumber;
 import javax.json.JsonObject;
+import javax.json.JsonString;
 
 import org.eclipse.microprofile.jwt.Claims;
 import org.jose4j.jwt.JwtClaims;
@@ -61,8 +62,8 @@ public class JwtCallerPrincipalUnitTest {
         Assertions.assertEquals(Json.createValue(4.4), customDoubleArray.getJsonNumber(4));
 
         // "customString": "customStringValue",
-        String customString = principal.getClaim("customString");
-        Assertions.assertEquals("customStringValue", customString);
+        JsonString customString = principal.getClaim("customString");
+        Assertions.assertEquals(Json.createValue("customStringValue"), customString);
         // "customInteger": 123456789,
         JsonNumber customInteger = principal.getClaim("customInteger");
         Assertions.assertEquals(Json.createValue(123456789), customInteger);

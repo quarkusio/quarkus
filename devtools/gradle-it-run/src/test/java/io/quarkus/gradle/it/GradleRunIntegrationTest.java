c3984bbb5f56f7c508e3586a8c63d94a453d146b
Michael Vorburger <mike@vorburger.ch>
2019-04-10 17:55:37 +0200

    add GradleRunIntegrationTest (fixes #1623)

---
+++ devtools/gradle-it-run/src/test/java/io/quarkus/gradle/it/GradleRunIntegrationTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.quarkus.gradle.it;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Integration Test which runs the sample app built by Gradle.
+ *
+ * @author <a href="mailto:mike@vorburger.ch">Michael Vorburger.ch</a>
+ */
+public class GradleRunIntegrationTest {
+    // do not rename this to *IT because we want it to run on regular mvn test without requiring failsafe
+
+    @Test
+    public void testHelloEndpoint() throws IOException {
+        Process process = new ProcessBuilder()
+                // .directory("../")
+                .command("java", "-jar", "../gradle-it/build/gradle-it-runner.jar")
+                .start();
+        try {
+            given()
+                    .when().get("/hello")
+                    .then()
+                    .statusCode(200)
+                    .body(is("hello"));
+
+        } finally {
+            process.destroy();
+        }
+    }
+}

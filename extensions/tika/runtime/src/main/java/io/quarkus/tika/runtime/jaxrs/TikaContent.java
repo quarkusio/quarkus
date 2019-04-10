4c54d227e337f25966e3e5d0fa168909090777f2
Sergey Beryozkin <sberyozkin@gmail.com>
2019-04-10 13:56:29 +0100

    Adding beans.xml to get TikaContent augmented

--- extensions/tika/runtime/src/main/java/io/quarkus/tika/runtime/jaxrs/TikaContent.java
+++ extensions/tika/runtime/src/main/java/io/quarkus/tika/runtime/jaxrs/TikaContent.java
@@ -2,4 +2,13 @@ package io.quarkus.tika.runtime.jaxrs;
 
 public class TikaContent {
 
+    private String content;
+
+    public TikaContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
--- extensions/tika/runtime/src/main/java/io/quarkus/tika/runtime/jaxrs/TikaMessageBodyReader.java
+++ extensions/tika/runtime/src/main/java/io/quarkus/tika/runtime/jaxrs/TikaMessageBodyReader.java
@@ -1,9 +1,11 @@
 package io.quarkus.tika.runtime.jaxrs;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -23,7 +25,16 @@ public class TikaMessageBodyReader implements MessageBodyReader<TikaContent> {
     public TikaContent readFrom(Class<TikaContent> type, Type genericType, Annotation[] annotations, MediaType mediaType,
             MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
             throws IOException, WebApplicationException {
-        return new TikaContent();
+        return new TikaContent(new String(readBytes(entityStream), StandardCharsets.UTF_8));
     }
 
+    private static byte[] readBytes(InputStream is) throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[4096];
+        int len;
+        while ((len = is.read(buffer)) != -1) {
+            os.write(buffer, 0, len);
+        }
+        return os.toByteArray();
+    }
 }

5ce30cabb988b7e03e37fe21e9c5cfa3b5ddc724
Georgios Andrianakis <geoand@gmail.com>
2019-04-10 14:17:11 +0300

    Fix issue where web.xml broke augmentation
    This happened because an empty list of additional beans is being passed
    by the Undertow build step
    
    Fixes: #1976

---
+++ extensions/undertow/deployment/src/test/java/io/quarkus/undertow/test/ServletEmptyWebXmlTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.quarkus.undertow.test;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+import io.restassured.RestAssured;
+
+public class ServletEmptyWebXmlTestCase {
+
+    static final String WEB_XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "\n" +
+            "<web-app version=\"3.0\"\n" +
+            "         xmlns=\"http://java.sun.com/xml/ns/javaee\"\n" +
+            "         xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+            "         xsi:schemaLocation=\"http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd\"\n"
+            +
+            "         metadata-complete=\"false\">\n" +
+            "</web-app>";
+
+    @RegisterExtension
+    static QuarkusUnitTest runner = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(WebXmlServlet.class)
+                    .addAsManifestResource(new StringAsset(WEB_XML), "web.xml"));
+
+    @Test
+    public void testWebXmlServlet() {
+        RestAssured.when().get("/").then()
+                .statusCode(403);
+    }
+
+}

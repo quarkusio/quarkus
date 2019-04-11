b6752a22417f5c34e166916cb2e16c0d09e37e9f
Michael Vorburger <mike@vorburger.ch>
2019-04-11 15:45:25 +0200

    make GradleRunIntegrationTest wait for "Listening on: http"
    This should fix the test failure on slow CI servers such as Azure Build.

--- devtools/gradle-it-run/src/test/java/io/quarkus/gradle/it/GradleRunIntegrationTest.java
+++ devtools/gradle-it-run/src/test/java/io/quarkus/gradle/it/GradleRunIntegrationTest.java
@@ -17,6 +17,7 @@ package io.quarkus.gradle.it;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -32,11 +33,16 @@ import ch.vorburger.exec.ManagedProcessException;
 public class GradleRunIntegrationTest {
     // do not rename this to *IT because we want it to run on regular mvn test without requiring failsafe
 
+    private static final String EXPECTED_CONSOLE_MESSAGE = "Listening on: http";
+    private static final int MAX_WAIT_IN_MS = 13000;
+
     @Test
     public void testHelloEndpoint() throws ManagedProcessException {
         ManagedProcess process = new ManagedProcessBuilder("java")
                 .addArgument("-jar").addArgument("../gradle-it/build/gradle-it-runner.jar").build();
-        process.start();
+        assertTrue(
+                process.startAndWaitForConsoleMessageMaxMs(EXPECTED_CONSOLE_MESSAGE, MAX_WAIT_IN_MS),
+                EXPECTED_CONSOLE_MESSAGE + " did not appear?!");
 
         try {
             given()

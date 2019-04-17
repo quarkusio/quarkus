297ee4e855dda12cbd1c8782a8fe67c3f4f56d0c
Sergey Beryozkin <sberyozkin@gmail.com>
2019-04-17 13:54:49 +0100

    Producing some tika-core resources

--- extensions/tika/deployment/src/main/java/io/quarkus/tika/deployment/TikaProcessor.java
+++ extensions/tika/deployment/src/main/java/io/quarkus/tika/deployment/TikaProcessor.java
@@ -2,13 +2,21 @@ package io.quarkus.tika.deployment;
 
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.substrate.SubstrateResourceBuildItem;
 import io.quarkus.resteasy.common.deployment.ResteasyJaxrsProviderBuildItem;
 import io.quarkus.tika.runtime.jaxrs.TikaMessageBodyReader;
 
 public class TikaProcessor {
 
     @BuildStep
-    void setupTikaProvider(BuildProducer<ResteasyJaxrsProviderBuildItem> providers) {
+    void produceTikaProvider(BuildProducer<ResteasyJaxrsProviderBuildItem> providers) {
         providers.produce(new ResteasyJaxrsProviderBuildItem(TikaMessageBodyReader.class.getName()));
     }
+
+    @BuildStep
+    public void produceTikaResources(BuildProducer<SubstrateResourceBuildItem> resource) throws Exception {
+        resource.produce(new SubstrateResourceBuildItem("org/apache/tika/mime/tika-mimetypes.xml"));
+        resource.produce(new SubstrateResourceBuildItem("org/apache/tika/parser/external/tika-external-parsers.xml"));
+    }
+
 }

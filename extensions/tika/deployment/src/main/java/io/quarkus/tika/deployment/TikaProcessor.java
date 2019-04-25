7654f216299abec7bef6622534cdb0e71a77f9dd
Sergey Beryozkin <sberyozkin@gmail.com>
2019-04-25 12:50:26 +0100

    Restructuring the processor a bit

--- extensions/tika/deployment/src/main/java/io/quarkus/tika/deployment/TikaProcessor.java
+++ extensions/tika/deployment/src/main/java/io/quarkus/tika/deployment/TikaProcessor.java
@@ -1,8 +1,17 @@
 package io.quarkus.tika.deployment;
 
+import java.util.Set;
+
+import org.apache.tika.detect.Detector;
+import org.apache.tika.detect.EncodingDetector;
+import org.apache.tika.parser.Parser;
+
+import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.substrate.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.substrate.SubstrateResourceBuildItem;
+import io.quarkus.deployment.util.ServiceUtil;
 import io.quarkus.resteasy.common.deployment.ResteasyJaxrsProviderBuildItem;
 import io.quarkus.tika.runtime.jaxrs.TikaMessageBodyReader;
 
@@ -20,10 +29,48 @@ public class TikaProcessor {
     }
 
     @BuildStep
-    public void produceTikaParsersResources(BuildProducer<SubstrateResourceBuildItem> resource) throws Exception {
-        resource.produce(new SubstrateResourceBuildItem("META-INF/services/org.apache.tika.parser.Parser"));
-        resource.produce(new SubstrateResourceBuildItem("META-INF/services/org.apache.tika.detect.Detector"));
-        resource.produce(new SubstrateResourceBuildItem("META-INF/services/org.apache.tika.detect.EncodingDetector"));
+    public void produceTikaParsersParsers(BuildProducer<SubstrateResourceBuildItem> resource,
+            BuildProducer<UnremovableBeanBuildItem> unremovableBeans,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClass) throws Exception {
+        resource.produce(new SubstrateResourceBuildItem("META-INF/services/" + Parser.class.getName()));
+        Set<String> availableParsers = ServiceUtil.classNamesNamedIn(getClass().getClassLoader(),
+                "META-INF/services/" + Parser.class.getName());
+        for (String parser : availableParsers) {
+            reflectiveClass.produce(new ReflectiveClassBuildItem(false, false, parser));
+        }
+        unremovableBeans.produce(new UnremovableBeanBuildItem(
+                b -> availableParsers.contains(b.getBeanClass().toString())));
+
+    }
+
+    @BuildStep
+    public void produceTikaParsersDetectors(BuildProducer<SubstrateResourceBuildItem> resource,
+            BuildProducer<UnremovableBeanBuildItem> unremovableBeans,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClass) throws Exception {
+        resource.produce(new SubstrateResourceBuildItem("META-INF/services/" + Detector.class.getName()));
+        Set<String> availableDetectors = ServiceUtil.classNamesNamedIn(getClass().getClassLoader(),
+                "META-INF/services/" + Detector.class.getName());
+        for (String detector : availableDetectors) {
+            reflectiveClass.produce(new ReflectiveClassBuildItem(false, false, detector));
+        }
+        unremovableBeans.produce(new UnremovableBeanBuildItem(
+                b -> availableDetectors.contains(b.getBeanClass().toString())));
+
+    }
+
+    @BuildStep
+    public void produceTikaParsersEncodingDetectors(BuildProducer<SubstrateResourceBuildItem> resource,
+            BuildProducer<UnremovableBeanBuildItem> unremovableBeans,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClass) throws Exception {
+        resource.produce(new SubstrateResourceBuildItem("META-INF/services/" + EncodingDetector.class.getName()));
+        Set<String> availableDetectors = ServiceUtil.classNamesNamedIn(getClass().getClassLoader(),
+                "META-INF/services/" + EncodingDetector.class.getName());
+        for (String detector : availableDetectors) {
+            reflectiveClass.produce(new ReflectiveClassBuildItem(false, false, detector));
+        }
+        unremovableBeans.produce(new UnremovableBeanBuildItem(
+                b -> availableDetectors.contains(b.getBeanClass().toString())));
+
     }
 
 }

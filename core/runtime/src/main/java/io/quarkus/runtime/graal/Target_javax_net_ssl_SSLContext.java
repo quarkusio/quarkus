6c91b7607587833862355386f86f895ac4058ada
Guillaume Smet <guillaume.smet@gmail.com>
2019-04-29 12:35:32 +0200

    Limit the SSL context substitutions as sun.security.jca not available in JDK 11

--- core/runtime/src/main/java/io/quarkus/runtime/graal/Target_javax_net_ssl_SSLContext.java
+++ core/runtime/src/main/java/io/quarkus/runtime/graal/Target_javax_net_ssl_SSLContext.java
@@ -6,10 +6,8 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.security.Provider;
 import java.security.SecureRandom;
-import java.util.Objects;
 
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.SSLContext;
@@ -25,10 +23,8 @@ import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 
 import io.quarkus.runtime.ssl.SslContextConfiguration;
-import sun.security.jca.GetInstance;
 
 @TargetClass(className = "javax.net.ssl.SSLContext")
-@SuppressWarnings("restriction")
 public final class Target_javax_net_ssl_SSLContext {
 
     @Alias
@@ -51,47 +47,50 @@ public final class Target_javax_net_ssl_SSLContext {
         return defaultContext;
     }
 
-    @Substitute
-    public static Target_javax_net_ssl_SSLContext getInstance(String protocol)
-            throws NoSuchAlgorithmException {
-        Objects.requireNonNull(protocol, "null protocol name");
-
-        if (!SslContextConfiguration.isSslNativeEnabled()) {
-            return (Target_javax_net_ssl_SSLContext) (Object) getDefault();
-        }
-
-        GetInstance.Instance instance = GetInstance.getInstance("SSLContext", SSLContextSpi.class, protocol);
-        return new Target_javax_net_ssl_SSLContext((SSLContextSpi) instance.impl, instance.provider,
-                protocol);
-    }
-
-    @Substitute
-    public static Target_javax_net_ssl_SSLContext getInstance(String protocol, String provider)
-            throws NoSuchAlgorithmException, NoSuchProviderException {
-        Objects.requireNonNull(protocol, "null protocol name");
-
-        if (!SslContextConfiguration.isSslNativeEnabled()) {
-            return (Target_javax_net_ssl_SSLContext) (Object) getDefault();
-        }
-
-        GetInstance.Instance instance = GetInstance.getInstance("SSLContext", SSLContextSpi.class, protocol, provider);
-        return new Target_javax_net_ssl_SSLContext((SSLContextSpi) instance.impl, instance.provider,
-                protocol);
-    }
-
-    @Substitute
-    public static Target_javax_net_ssl_SSLContext getInstance(String protocol, Provider provider)
-            throws NoSuchAlgorithmException {
-        Objects.requireNonNull(protocol, "null protocol name");
-
-        if (!SslContextConfiguration.isSslNativeEnabled()) {
-            return (Target_javax_net_ssl_SSLContext) (Object) getDefault();
-        }
-
-        GetInstance.Instance instance = GetInstance.getInstance("SSLContext", SSLContextSpi.class, protocol, provider);
-        return new Target_javax_net_ssl_SSLContext((SSLContextSpi) instance.impl, instance.provider,
-                protocol);
-    }
+    //    TODO sun.security.jca.GetInstance is not accessible in JDK 11. We cannot add an export
+    //    as we still compile with a JDK 8 target. So for now, we will have to leave with this
+    //    and only override getDefault().
+    //    @Substitute
+    //    public static Target_javax_net_ssl_SSLContext getInstance(String protocol)
+    //            throws NoSuchAlgorithmException {
+    //        Objects.requireNonNull(protocol, "null protocol name");
+    //
+    //        if (!SslContextConfiguration.isSslNativeEnabled()) {
+    //            return (Target_javax_net_ssl_SSLContext) (Object) getDefault();
+    //        }
+    //
+    //        GetInstance.Instance instance = GetInstance.getInstance("SSLContext", SSLContextSpi.class, protocol);
+    //        return new Target_javax_net_ssl_SSLContext((SSLContextSpi) instance.impl, instance.provider,
+    //                protocol);
+    //    }
+    //
+    //    @Substitute
+    //    public static Target_javax_net_ssl_SSLContext getInstance(String protocol, String provider)
+    //            throws NoSuchAlgorithmException, NoSuchProviderException {
+    //        Objects.requireNonNull(protocol, "null protocol name");
+    //
+    //        if (!SslContextConfiguration.isSslNativeEnabled()) {
+    //            return (Target_javax_net_ssl_SSLContext) (Object) getDefault();
+    //        }
+    //
+    //        GetInstance.Instance instance = GetInstance.getInstance("SSLContext", SSLContextSpi.class, protocol, provider);
+    //        return new Target_javax_net_ssl_SSLContext((SSLContextSpi) instance.impl, instance.provider,
+    //                protocol);
+    //    }
+    //
+    //    @Substitute
+    //    public static Target_javax_net_ssl_SSLContext getInstance(String protocol, Provider provider)
+    //            throws NoSuchAlgorithmException {
+    //        Objects.requireNonNull(protocol, "null protocol name");
+    //
+    //        if (!SslContextConfiguration.isSslNativeEnabled()) {
+    //            return (Target_javax_net_ssl_SSLContext) (Object) getDefault();
+    //        }
+    //
+    //        GetInstance.Instance instance = GetInstance.getInstance("SSLContext", SSLContextSpi.class, protocol, provider);
+    //        return new Target_javax_net_ssl_SSLContext((SSLContextSpi) instance.impl, instance.provider,
+    //                protocol);
+    //    }
 
     private static class DisabledSSLContext extends SSLContext {
 

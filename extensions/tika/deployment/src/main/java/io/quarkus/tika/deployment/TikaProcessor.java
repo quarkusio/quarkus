2a74cb26a61ad4d4335824485233d1099d28aaec
Sergey Beryozkin <sberyozkin@gmail.com>
2019-05-01 12:56:48 +0100

    Only registering the native ready parsers

--- extensions/tika/deployment/src/main/java/io/quarkus/tika/deployment/TikaProcessor.java
+++ extensions/tika/deployment/src/main/java/io/quarkus/tika/deployment/TikaProcessor.java
@@ -1,7 +1,8 @@
 package io.quarkus.tika.deployment;
 
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.tika.detect.Detector;
 import org.apache.tika.detect.EncodingDetector;
@@ -10,7 +11,6 @@ import org.apache.tika.parser.Parser;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.substrate.ReflectiveClassBuildItem;
-import io.quarkus.deployment.builditem.substrate.RuntimeInitializedClassBuildItem;
 import io.quarkus.deployment.builditem.substrate.SubstrateResourceBuildItem;
 import io.quarkus.deployment.util.ServiceUtil;
 import io.quarkus.resteasy.common.deployment.ResteasyJaxrsProviderBuildItem;
@@ -18,11 +18,10 @@ import io.quarkus.tika.runtime.jaxrs.TikaMessageBodyReader;
 
 public class TikaProcessor {
 
-    private static final Set<String> RUNTIME_INITIALIZED_CLASSES;
-    static {
-        RUNTIME_INITIALIZED_CLASSES = new HashSet<>();
-        RUNTIME_INITIALIZED_CLASSES.add("org.apache.tika.parser.pdf.PDFParser");
-    }
+    private static final Set<String> NATIVE_READY_PARSERS = Arrays.stream(new String[] {
+            "org.apache.tika.parser.txt.TXTParser",
+            "org.apache.tika.parser.odf.OpenDocumentParser"
+    }).collect(Collectors.toSet());
 
     @BuildStep
     void produceTikaJaxrsProvider(BuildProducer<ResteasyJaxrsProviderBuildItem> providers) {
@@ -43,13 +42,6 @@ public class TikaProcessor {
         produceTikaServiceProviders(resource, reflectiveClass, EncodingDetector.class.getName());
     }
 
-    @BuildStep
-    public void produceRuntimeInitializedClasses(BuildProducer<RuntimeInitializedClassBuildItem> resource) {
-        for (String className : RUNTIME_INITIALIZED_CLASSES) {
-            resource.produce(new RuntimeInitializedClassBuildItem(className));
-        }
-    }
-
     private void produceTikaServiceProviders(BuildProducer<SubstrateResourceBuildItem> resource,
             BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
             String serviceProviderName) throws Exception {
@@ -57,7 +49,7 @@ public class TikaProcessor {
         Set<String> availableProviderClasses = ServiceUtil.classNamesNamedIn(getClass().getClassLoader(),
                 "META-INF/services/" + serviceProviderName);
         for (String providerClass : availableProviderClasses) {
-            if (!RUNTIME_INITIALIZED_CLASSES.contains(providerClass)) {
+            if (!Parser.class.getName().equals(serviceProviderName) || NATIVE_READY_PARSERS.contains(providerClass)) {
                 reflectiveClass.produce(new ReflectiveClassBuildItem(false, false, providerClass));
             }
         }

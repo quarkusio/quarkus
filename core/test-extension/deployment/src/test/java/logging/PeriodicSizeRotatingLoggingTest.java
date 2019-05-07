ea432a0ba34a03e7b7f2b68bdfac06aa839a52c9
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2019-05-07 17:23:25 +0200

    feat(#2086): Add log files rotation

---
+++ core/test-extension/deployment/src/test/java/logging/PeriodicSizeRotatingLoggingTest.java
@@ -0,0 +1,51 @@
+package logging;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import org.jboss.logmanager.formatters.PatternFormatter;
+import org.jboss.logmanager.handlers.DelayedHandler;
+import org.jboss.logmanager.handlers.PeriodicSizeRotatingFileHandler;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.runtime.logging.InitialConfigurator;
+import io.quarkus.test.QuarkusUnitTest;
+
+public class PeriodicSizeRotatingLoggingTest {
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addAsResource("application-periodic-size-file-log-rotating.properties", "application.properties"));
+
+    @Test
+    public void periodicSizeRotatingConfigurationTest() {
+        LogManager logManager = LogManager.getLogManager();
+        assertThat(logManager).isInstanceOf(org.jboss.logmanager.LogManager.class);
+
+        DelayedHandler delayedHandler = InitialConfigurator.DELAYED_HANDLER;
+        assertThat(Logger.getLogger("").getHandlers()).contains(delayedHandler);
+
+        Handler handler = Arrays.asList(delayedHandler.getHandlers()).stream()
+                .filter(h -> (h instanceof PeriodicSizeRotatingFileHandler))
+                .findFirst().get();
+        assertThat(handler).isNotNull();
+        assertThat(handler.getLevel()).isEqualTo(Level.INFO);
+
+        Formatter formatter = handler.getFormatter();
+        assertThat(formatter).isInstanceOf(PatternFormatter.class);
+        PatternFormatter patternFormatter = (PatternFormatter) formatter;
+        assertThat(patternFormatter.getPattern()).isEqualTo("%d{HH:mm:ss} %-5p [%c{2.}]] (%t) %s%e%n");
+
+        PeriodicSizeRotatingFileHandler periodicSizeRotatingFileHandler = (PeriodicSizeRotatingFileHandler) handler;
+        assertThat(periodicSizeRotatingFileHandler.isRotateOnBoot()).isFalse();
+    }
+}

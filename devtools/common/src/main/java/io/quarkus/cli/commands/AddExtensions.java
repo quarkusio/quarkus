7f2e9fe37dbd6cd5a6091f6dd27417d130f68268
Emmanuel Bernard <emmanuel@hibernate.org>
2019-05-09 08:56:03 -0400

    issue 2385 - Improve add-extension with error in case of multiple matches

--- devtools/common/src/main/java/io/quarkus/cli/commands/AddExtensions.java
+++ devtools/common/src/main/java/io/quarkus/cli/commands/AddExtensions.java
@@ -7,8 +7,8 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
@@ -18,6 +18,9 @@ import io.quarkus.dependencies.Extension;
 import io.quarkus.maven.utilities.MojoUtils;
 
 public class AddExtensions {
+    private static String OK = "\u2705";
+    private static String NOK = "\u274c";
+    private static String NOOP = "\uD83D\uDC4D";
     private Model model;
     private String pom;
     private ProjectWriter writer;
@@ -37,35 +40,48 @@ public class AddExtensions {
         List<Dependency> dependenciesFromBom = getDependenciesFromBom();
 
         for (String dependency : extensions) {
-            Optional<Extension> optional = MojoUtils.loadExtensions().stream()
+            List<Extension> matches = MojoUtils.loadExtensions().stream()
                     .filter(d -> {
                         boolean hasTag = d.labels().contains(dependency.trim().toLowerCase());
                         boolean machName = d.getName()
                                 .toLowerCase()
                                 .contains(dependency.trim().toLowerCase());
-                        return hasTag || machName;
+                        boolean matchArtifactId = d.getArtifactId()
+                                .toLowerCase()
+                                .contains(dependency.trim().toLowerCase());
+                        return hasTag || machName || matchArtifactId;
                     })
-                    .findAny();
+                    .collect(Collectors.toList());
 
-            if (optional.isPresent()) {
-                final Extension extension = optional.get();
+            if (matches.size() > 1) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(NOK)
+                        .append(" Multiple extensions matching '" + dependency + "'");
+
+                matches.stream()
+                        .forEach(extension -> sb.append(System.lineSeparator()).append("     * ")
+                                .append(extension.managementKey()));
+                sb.append(System.lineSeparator()).append("     Be more specific e.g using the exact name or the full gav.");
+                System.out.println(sb);
+            } else if (matches.size() == 1) {
+                final Extension extension = matches.get(0);
 
                 if (!MojoUtils.hasDependency(model, extension.getGroupId(), extension.getArtifactId())) {
-                    System.out.println("Adding extension " + extension.managementKey());
+                    System.out.println(OK + " Adding extension " + extension.managementKey());
                     model.addDependency(extension
                             .toDependency(containsBOM(model) &&
                                     isDefinedInBom(dependenciesFromBom, extension)));
                     updated = true;
                 } else {
-                    System.out.println("Skipping extension " + extension.managementKey() + ": already present");
+                    System.out.println(NOOP + " Skipping extension " + extension.managementKey() + ": already present");
                 }
             } else if (dependency.contains(":")) {
                 Dependency parsed = MojoUtils.parse(dependency);
-                System.out.println("Adding dependency " + parsed.getManagementKey());
+                System.out.println(OK + " Adding dependency " + parsed.getManagementKey());
                 model.addDependency(parsed);
                 updated = true;
             } else {
-                System.out.println("Cannot find a dependency matching '" + dependency + "'");
+                System.out.println(NOK + " Cannot find a dependency matching '" + dependency + "', maybe a typo?");
             }
         }
 
--- devtools/common/src/test/java/io/quarkus/cli/commands/AddExtensionsTest.java
+++ devtools/common/src/test/java/io/quarkus/cli/commands/AddExtensionsTest.java
@@ -15,6 +15,7 @@ import io.quarkus.cli.commands.writer.FileProjectWriter;
 import io.quarkus.maven.utilities.MojoUtils;
 
 public class AddExtensionsTest {
+
     @Test
     public void addExtension() throws IOException {
         final File pom = new File("target/extensions-test", "pom.xml");
@@ -28,16 +29,45 @@ public class AddExtensionsTest {
 
         File pomFile = new File(pom.getAbsolutePath());
         new AddExtensions(new FileProjectWriter(pomFile.getParentFile()), pomFile.getName())
-                .addExtensions(new HashSet<>(asList("agroal", "arc", " hibernate-validator")));
+                .addExtensions(new HashSet<>(asList("jdbc-postgre", "agroal", "arc", " hibernate-validator")));
 
         Model model = MojoUtils.readPom(pom);
         hasDependency(model, "quarkus-agroal");
         hasDependency(model, "quarkus-arc");
         hasDependency(model, "quarkus-hibernate-validator");
+        hasDependency(model, "quarkus-jdbc-postgresql");
+    }
+
+    @Test
+    public void addDuplicatedExtension() throws IOException {
+        final File pom = new File("target/extensions-test", "pom.xml");
+
+        CreateProjectTest.delete(pom.getParentFile());
+        new CreateProject(new FileProjectWriter(pom.getParentFile()))
+                .groupId("org.acme")
+                .artifactId("add-extension-test")
+                .version("0.0.1-SNAPSHOT")
+                .doCreateProject(new HashMap<>());
+
+        File pomFile = new File(pom.getAbsolutePath());
+        new AddExtensions(new FileProjectWriter(pomFile.getParentFile()), pomFile.getName())
+                .addExtensions(new HashSet<>(asList("agroal", "jdbc", "non-exist-ent")));
+
+        Model model = MojoUtils.readPom(pom);
+        hasDependency(model, "quarkus-agroal");
+        doesNotHaveDependency(model, "quarkus-jdbc-postgresql");
+        doesNotHaveDependency(model, "quarkus-jdbc-h2");
     }
 
     private void hasDependency(final Model model, final String artifactId) {
         Assertions.assertTrue(model.getDependencies()
+                .stream()
+                .anyMatch(d -> d.getGroupId().equals(MojoUtils.getPluginGroupId()) &&
+                        d.getArtifactId().equals(artifactId)));
+    }
+
+    private void doesNotHaveDependency(final Model model, final String artifactId) {
+        Assertions.assertFalse(model.getDependencies()
                 .stream()
                 .anyMatch(d -> d.getGroupId().equals(MojoUtils.getPluginGroupId()) &&
                         d.getArtifactId().equals(artifactId)));

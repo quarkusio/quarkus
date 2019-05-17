29c2b09b4e38852bac73e996d6141094012d4b82
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2019-05-17 17:24:43 +0200

    enable access to @BeforeDestroyed methods for native mode
    fixes #2495

--- extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/LifecycleEventsBuildStep.java
+++ extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/LifecycleEventsBuildStep.java
@@ -4,13 +4,22 @@ import static io.quarkus.deployment.annotations.ExecutionTime.RUNTIME_INIT;
 
 import java.util.List;
 
+import javax.enterprise.context.BeforeDestroyed;
+
 import io.quarkus.arc.runtime.ArcDeploymentTemplate;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.ServiceStartBuildItem;
 import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
+import io.quarkus.deployment.builditem.substrate.ReflectiveClassBuildItem;
 
 public class LifecycleEventsBuildStep {
+
+    @BuildStep
+    public void build(BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
+        reflectiveClass.produce(new ReflectiveClassBuildItem(true, false, BeforeDestroyed.class.getName()));
+    }
 
     @BuildStep
     @Record(RUNTIME_INIT)

bf7a86480db43c068d46afbcfe3bfd708e1f606c
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2019-05-21 21:24:42 +0200

    feat(#2518): add a default toString() implementation for PanacheEntities

--- extensions/panache/hibernate-orm-panache/deployment/src/test/java/io/quarkus/hibernate/orm/panache/test/NoConfigTest.java
+++ extensions/panache/hibernate-orm-panache/deployment/src/test/java/io/quarkus/hibernate/orm/panache/test/NoConfigTest.java
@@ -5,6 +5,7 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.test.QuarkusUnitTest;
 
 public class NoConfigTest {
--- extensions/panache/hibernate-orm-panache/runtime/src/main/java/io/quarkus/hibernate/orm/panache/PanacheEntity.java
+++ extensions/panache/hibernate-orm-panache/runtime/src/main/java/io/quarkus/hibernate/orm/panache/PanacheEntity.java
@@ -34,5 +34,10 @@ public abstract class PanacheEntity extends PanacheEntityBase {
     @GeneratedValue
     public Long id;
 
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "<" + id + ">";
+    }
+
     // FIXME: VERSION?
 }

71a9a139404a6eb66e51dee1b67e131a74ac8ffe
Michael Vorburger <mike@vorburger.ch>
2019-06-10 22:26:23 +0200

    Revert "remove gradle-it, need a better approach"
    This reverts commit 20ecd1b3b043954e58ed60449cd036c1de3c2ed6.

---
+++ devtools/gradle-it/src/main/java/org/acme/quickstart/GreetingService.java
@@ -0,0 +1,12 @@
+package org.acme.quickstart;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+
+}

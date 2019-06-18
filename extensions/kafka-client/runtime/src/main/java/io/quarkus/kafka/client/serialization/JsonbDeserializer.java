c6a6e9d3dca0dd87e650494109e80e08547cbec6
Gunnar Morling <gunnar.morling@googlemail.com>
2019-06-18 18:06:44 +0200

    #2663 JSON-B based (de-)serialization support for Kafka client module

---
+++ extensions/kafka-client/runtime/src/main/java/io/quarkus/kafka/client/serialization/JsonbDeserializer.java
@@ -0,0 +1,65 @@
+package io.quarkus.kafka.client.serialization;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import org.apache.kafka.common.serialization.Deserializer;
+
+/**
+ * A {@link Deserializer} that deserializes JSON using JSON-B.
+ */
+public class JsonbDeserializer<T> implements Deserializer<T> {
+
+    private final Jsonb jsonb;
+    private final Class<T> type;
+    private final boolean jsonbNeedsClosing;
+
+    public JsonbDeserializer(Class<T> type) {
+        this(type, JsonbBuilder.create(), true);
+    }
+
+    public JsonbDeserializer(Class<T> type, Jsonb jsonb) {
+        this(type, jsonb, false);
+    }
+
+    private JsonbDeserializer(Class<T> type, Jsonb jsonb, boolean jsonbNeedsClosing) {
+        this.type = type;
+        this.jsonb = jsonb;
+        this.jsonbNeedsClosing = jsonbNeedsClosing;
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+    }
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+
+        try (InputStream is = new ByteArrayInputStream(data)) {
+            return jsonb.fromJson(is, type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() {
+        if (!jsonbNeedsClosing) {
+            return;
+        }
+
+        try {
+            jsonb.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ extensions/kafka-client/runtime/src/test/java/io/quarkus/kafka/client/serde/JsonbSerdeTest.java
@@ -0,0 +1,48 @@
+package io.quarkus.kafka.client.serde;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.kafka.client.serialization.JsonbSerde;
+
+public class JsonbSerdeTest {
+
+    @Test
+    public void shouldSerializeAndDeserializeEntity() {
+        MyEntity entity = new MyEntity();
+        entity.id = 42L;
+        entity.name = "Bob";
+
+        try (JsonbSerde<MyEntity> serde = new JsonbSerde<>(MyEntity.class)) {
+            byte[] serialized = serde.serializer().serialize("my-topic", entity);
+            MyEntity deserialized = serde.deserializer().deserialize("my-topic", serialized);
+
+            assertThat(deserialized.id).isEqualTo(42L);
+            assertThat(deserialized.name).isEqualTo("Bob");
+        }
+    }
+
+    @Test
+    public void shouldSerializeAndDeserializeEntityWithGivenJsonb() throws Exception {
+        MyEntity entity = new MyEntity();
+        entity.id = 42L;
+        entity.name = "Bob";
+
+        try (Jsonb jsonb = JsonbBuilder.create(); JsonbSerde<MyEntity> serde = new JsonbSerde<>(MyEntity.class, jsonb)) {
+            byte[] serialized = serde.serializer().serialize("my-topic", entity);
+            MyEntity deserialized = serde.deserializer().deserialize("my-topic", serialized);
+
+            assertThat(deserialized.id).isEqualTo(42L);
+            assertThat(deserialized.name).isEqualTo("Bob");
+        }
+    }
+
+    public static class MyEntity {
+        public long id;
+        public String name;
+    }
+}

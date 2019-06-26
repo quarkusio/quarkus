db8d9cd80ab9ad61f5d0be5fff3e13a324d02e7c
Guillaume Smet <guillaume.smet@gmail.com>
2019-06-26 12:06:37 +0200

    Move the initialization of the delayed logger handler up in the RuntimeRunner
    We used to have a workaround for dev mode but, in fact, we also have the
    issue in the QuarkusTestExtension and, basically, in everything using
    the RuntimeRunner.
    
    Typically, for some test failures, you didn't have anything logged
    because the delayed handler was never initialized.
    
    So let's move the forced initialization of the logger in the
    RuntimeRunner itself.

--- core/deployment/src/main/java/io/quarkus/runner/RuntimeRunner.java
+++ core/deployment/src/main/java/io/quarkus/runner/RuntimeRunner.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.logging.Handler;
 
 import org.objectweb.asm.ClassVisitor;
 
@@ -25,6 +26,7 @@ import io.quarkus.deployment.builditem.LiveReloadBuildItem;
 import io.quarkus.runtime.Application;
 import io.quarkus.runtime.LaunchMode;
 import io.quarkus.runtime.configuration.ProfileManager;
+import io.quarkus.runtime.logging.InitialConfigurator;
 
 /**
  * Class that can be used to run quarkus directly, executing the build and runtime
@@ -124,11 +126,15 @@ public class RuntimeRunner implements Runnable, Closeable {
                     application.stop();
                 }
             };
-
         } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
+        } finally {
+            // if the log handler is not activated, activate it with a default configuration to flush the messages
+            if (!InitialConfigurator.DELAYED_HANDLER.isActivated()) {
+                InitialConfigurator.DELAYED_HANDLER.setHandlers(new Handler[] { InitialConfigurator.createDefaultHandler() });
+            }
         }
     }
 

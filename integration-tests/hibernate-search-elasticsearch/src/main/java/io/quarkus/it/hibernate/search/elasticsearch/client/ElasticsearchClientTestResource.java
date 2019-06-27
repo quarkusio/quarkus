a91fa50c816a5b14753b3b51b37b3f87462583ea
Rostislav Svoboda <rsvoboda@redhat.com>
2019-06-27 14:32:03 +0200

    Use io.quarkus.it package in integration-tests

--- integration-tests/hibernate-search-elasticsearch/src/main/java/io/quarkus/it/hibernate/search/elasticsearch/client/ElasticsearchClientTestResource.java
+++ integration-tests/hibernate-search-elasticsearch/src/main/java/io/quarkus/it/hibernate/search/elasticsearch/client/ElasticsearchClientTestResource.java
@@ -1,4 +1,4 @@
-package io.quarkus.test.hibernate.search.elasticsearch.client;
+package io.quarkus.it.hibernate.search.elasticsearch.client;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;

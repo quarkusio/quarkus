0ca8c43cc6aecc0315e89af4d80ee82afb95e46b
Martin Kouba <mkouba@redhat.com>
2019-07-07 10:57:46 +0200

    Dev mode - notify extensions about file changes without app restart
    - resolves #2704

--- core/deployment/src/main/java/io/quarkus/deployment/devmode/HotReplacementContext.java
+++ core/deployment/src/main/java/io/quarkus/deployment/devmode/HotReplacementContext.java
@@ -2,6 +2,10 @@ package io.quarkus.deployment.devmode;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import io.quarkus.deployment.builditem.HotDeploymentWatchedFileBuildItem;
 
 public interface HotReplacementContext {
 
@@ -21,4 +25,12 @@ public interface HotReplacementContext {
     boolean doScan(boolean userInitiated) throws Exception;
 
     void addPreScanStep(Runnable runnable);
+
+    /**
+     * The consumer is invoked if only files which don't require restart are modified.
+     * 
+     * @param consumer The input is a set of chaned file paths
+     * @see HotDeploymentWatchedFileBuildItem#isRestartNeeded()
+     */
+    void consumeNoRestartChanges(Consumer<Set<String>> consumer);
 }

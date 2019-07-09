89ef5833606150e7741421631949710a47607589
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-07-09 17:50:30 +1000

    Minor Javadoc fix

--- core/deployment/src/main/java/io/quarkus/deployment/annotations/BuildStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/annotations/BuildStep.java
@@ -32,7 +32,7 @@ import io.quarkus.runtime.annotations.Recorder;
  * <li>{@link Consumer} of any concrete subclass of {@link BuildItem}</li>
  * <li>{@link Supplier} of any concrete subclass of {@link SimpleBuildItem}</li>
  * <li>{@link Optional} instances whose value type is a subclass of {@link SimpleBuildItem}</li>
- * <li>Recorder template classes, which are annotated with {@link Recorder} (method parameters only, if the method is annotated
+ * <li>Recorder classes, which are annotated with {@link Recorder} (method parameters only, if the method is annotated
  * {@link Record})</li>
  * <li>{@link BytecodeRecorderImpl} (method parameters only, if the method is annotated {@link Record})</li>
  * </ul>
--- core/runtime/src/main/java/io/quarkus/runtime/annotations/Recorder.java
+++ core/runtime/src/main/java/io/quarkus/runtime/annotations/Recorder.java
@@ -11,10 +11,11 @@ import java.lang.annotation.Target;
  * <p>
  * Recorder classes must be non final and have a public no-arg constructor.
  * <p>
- * At deployment time proxies of the templates can be injected into BuildStep methods that
+ * At deployment time proxies of the recorder can be injected into BuildStep methods that
  * have been annotated with {@code @Record}. Any invocations made on these proxies will be
  * recorded, and bytecode will be written out to be executed at runtime to make the same
  * sequence of invocations with the same parameters on the actual recorder objects.
+ *
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)

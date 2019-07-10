a75ebdf43bc598cf4f44c1abac1d177def4c5561
Loïc Mathieu <loikeseke@gmail.com>
2019-07-10 14:35:45 +0200

    doc: add the `@TransactionConfig` to the transactional guide

--- extensions/narayana-jta/runtime/src/main/java/io/quarkus/narayana/jta/runtime/TransactionConfiguration.java
+++ extensions/narayana-jta/runtime/src/main/java/io/quarkus/narayana/jta/runtime/TransactionConfiguration.java
@@ -7,21 +7,23 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * This annotation can be used to configure a different transaction timeout that the default one for a method or a class.
+ * This annotation can be used to configure a different transaction timeout than the default one for a method or a class.
  * It needs to be used on the entry method of the transaction.
  */
 @Inherited
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface TransactionConfiguration {
-    /** The value is used to specify that no transaction timeout is configured */
+    /**
+     * This value is used to specify that no transaction timeout is configured
+     */
     int UNSET_TIMEOUT = -1;
 
     /**
-     * The transaction timeout in second.
+     * The transaction timeout in seconds.
      * Defaults to UNSET_TIMEOUT: no timeout configured.
      * 
-     * @return The transaction timeout in second.
+     * @return The transaction timeout in seconds.
      */
     int timeout() default UNSET_TIMEOUT;
 }

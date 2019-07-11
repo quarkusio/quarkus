b8f17029a08292c63ae1fcc9a79d66b2294c539d
Ken Finnigan <ken@kenfinnigan.me>
2019-07-11 14:15:31 -0400

    Switch REST Client implementation to RESTEasy
    - Updates to RESTEasy 4.1.1.Final
    - Removes SmallRye REST Client extension and implementation
    - Adds REST Client extension using RESTEasy
    - Fixes #3055

---
+++ extensions/rest-client/runtime/src/main/java/io/quarkus/restclient/runtime/RestClientBase.java
@@ -0,0 +1,60 @@
+package io.quarkus.restclient.runtime;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Optional;
+
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+
+public class RestClientBase {
+
+    public static final String MP_REST = "mp-rest";
+    public static final String REST_URL_FORMAT = "%s/" + MP_REST + "/url";
+    public static final String REST_URI_FORMAT = "%s/" + MP_REST + "/uri";
+
+    private final Class<?> proxyType;
+    private final String baseUriFromAnnotation;
+
+    private final Config config;
+
+    public RestClientBase(Class<?> proxyType, String baseUriFromAnnotation) {
+        this.proxyType = proxyType;
+        this.baseUriFromAnnotation = baseUriFromAnnotation;
+        this.config = ConfigProvider.getConfig();
+    }
+
+    public Object create() {
+        RestClientBuilder builder = RestClientBuilder.newBuilder();
+        String baseUrl = getBaseUrl();
+        try {
+            return builder.baseUrl(new URL(baseUrl)).build(proxyType);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("The value of URL was invalid " + baseUrl, e);
+        } catch (Exception e) {
+            if ("com.oracle.svm.core.jdk.UnsupportedFeatureError".equals(e.getClass().getCanonicalName())) {
+                throw new IllegalArgumentException(baseUrl
+                        + " requires SSL support but it is disabled. You probably have set quarkus.ssl.native to false.");
+            }
+            throw e;
+        }
+    }
+
+    private String getBaseUrl() {
+        String propertyName = String.format(REST_URI_FORMAT, proxyType.getName());
+        Optional<String> propertyOptional = config.getOptionalValue(propertyName, String.class);
+        if (!propertyOptional.isPresent()) {
+            propertyName = String.format(REST_URL_FORMAT, proxyType.getName());
+            propertyOptional = config.getOptionalValue(propertyName, String.class);
+        }
+        if (((baseUriFromAnnotation == null) || baseUriFromAnnotation.isEmpty())
+                && !propertyOptional.isPresent()) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            "Unable to determine the proper baseUrl/baseUri. Consider registering using @RegisterRestClient(baseUri=\"someuri\"), or by adding '%s' to your Quarkus configuration",
+                            propertyName));
+        }
+        return propertyOptional.orElse(baseUriFromAnnotation);
+    }
+}
---
+++ extensions/rest-client/runtime/src/main/java/io/quarkus/restclient/runtime/graal/ClientBuilderReplacement.java
@@ -0,0 +1,17 @@
+package io.quarkus.restclient.runtime.graal;
+
+import javax.ws.rs.client.ClientBuilder;
+
+import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
+
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+
+@TargetClass(ClientBuilder.class)
+final class ClientBuilderReplacement {
+
+    @Substitute
+    public static ClientBuilder newBuilder() {
+        return new ResteasyClientBuilderImpl();
+    }
+}

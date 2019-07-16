a0b4b4dff2339447ccacad7a996be7e5d2275a68
atzoum <atzoum@gmail.com>
2019-07-16 22:17:09 +0300

    Enable Hibernate Envers compatibility in JVM mode
    Solves #3231

---
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/envers/EnversValidationTestCase.java
@@ -0,0 +1,31 @@
+package io.quarkus.hibernate.orm.envers;
+
+import static org.hamcrest.Matchers.is;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+import io.restassured.RestAssured;
+
+public class EnversValidationTestCase {
+
+    @RegisterExtension
+    static QuarkusUnitTest runner = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(MyAuditedEntity.class, MyRevisionEntity.class, MyRevisionListener.class,
+                            EnversTestValidationResource.class)
+                    .addAsResource("application.properties")
+                    .addAsResource(new StringAsset(""), "import.sql")); // define an empty import.sql file
+
+    @Test
+    public void testInsert() {
+        String entityName = "audited";
+        RestAssured.given().body(entityName).when().post("/envers").then()
+                .body(is("OK"));
+    }
+
+}

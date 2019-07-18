e7343f5fafb3d289ba8aa35d21534f7788a6ecea
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2019-07-18 13:04:31 +0200

    refactor(mailer): completely re-initialise MailerClient on hot reload

--- extensions/mailer/runtime/src/main/java/io/quarkus/mailer/impl/MailConfigRecorder.java
+++ extensions/mailer/runtime/src/main/java/io/quarkus/mailer/impl/MailConfigRecorder.java
@@ -14,19 +14,15 @@ import io.vertx.ext.mail.StartTLSOptions;
 public class MailConfigRecorder {
 
     private static volatile MailClient client;
-    private static volatile MailConfig config;
 
     public RuntimeValue<MailClient> configureTheClient(RuntimeValue<Vertx> vertx, BeanContainer container,
-            MailConfig config, LaunchMode launchMode, ShutdownContext shutdown) {
+            MailConfig config, ShutdownContext shutdown) {
 
         initialize(vertx.getValue(), config);
-
         MailClientProducer producer = container.instance(MailClientProducer.class);
         producer.initialize(client);
 
-        if (!launchMode.isDevOrTest()) {
-            shutdown.addShutdownTask(this::close);
-        }
+        shutdown.addShutdownTask(this::close);
         return new RuntimeValue<>(client);
     }
 
@@ -42,13 +38,6 @@ public class MailConfigRecorder {
     }
 
     void initialize(Vertx vertx, MailConfig config) {
-        if (client != null && config.equals(MailConfigRecorder.config)) {
-            // Already configured
-            return;
-        }
-
-        this.close();
-        MailConfigRecorder.config = config;
         io.vertx.ext.mail.MailConfig cfg = toVertxMailConfig(config);
         client = MailClient.createNonShared(vertx, cfg);
     }

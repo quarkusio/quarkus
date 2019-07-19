fc9b7079efd81c8d891bffea76d2e110257d47aa
Guillaume Smet <guillaume.smet@gmail.com>
2019-07-19 15:30:56 +0200

    Properly destroy @Dependent ConstraintValidator beans
    Per suggestion from Aris Tzoumas.

--- extensions/hibernate-validator/runtime/src/main/java/io/quarkus/hibernate/validator/runtime/ArcConstraintValidatorFactoryImpl.java
+++ extensions/hibernate-validator/runtime/src/main/java/io/quarkus/hibernate/validator/runtime/ArcConstraintValidatorFactoryImpl.java
@@ -2,7 +2,10 @@ package io.quarkus.hibernate.validator.runtime;
 
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.IdentityHashMap;
+import java.util.Map;
 
+import javax.enterprise.context.Dependent;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorFactory;
 
@@ -13,19 +16,33 @@ import io.quarkus.arc.InstanceHandle;
 
 public class ArcConstraintValidatorFactoryImpl implements ConstraintValidatorFactory {
 
+    /**
+     * In the case of a predefined scope {@code ValidatorFactory} as the one used by Quarkus, all the constraint validators are
+     * created at bootstrap in a single thread so we can use an {@link IdentityHashMap}.
+     * <p>
+     * Note that this is not the case when using a standard {@code ValidatorFactory}.
+     */
+    private final Map<ConstraintValidator<?, ?>, InstanceHandle<?>> destroyableConstraintValidators = new IdentityHashMap<>();
+
     @Override
-    public final <T extends ConstraintValidator<?, ?>> T getInstance(Class<T> key) {
-        InstanceHandle<T> instanceHandle = Arc.container().instance(key);
-        if (instanceHandle.isAvailable()) {
-            return instanceHandle.get();
-        } else {
-            return run(NewInstance.action(key, "ConstraintValidator"));
+    public <T extends ConstraintValidator<?, ?>> T getInstance(Class<T> key) {
+        InstanceHandle<T> handle = Arc.container().instance(key);
+        if (handle.isAvailable()) {
+            T instance = handle.get();
+            if (handle.getBean().getScope().equals(Dependent.class)) {
+                destroyableConstraintValidators.put(instance, handle);
+            }
+            return instance;
         }
+        return run(NewInstance.action(key, "ConstraintValidator"));
     }
 
     @Override
     public void releaseInstance(ConstraintValidator<?, ?> instance) {
-        // we let ArC release the bean instances on shutdown as they were instantiated by ArC.
+        InstanceHandle<?> destroyableHandle = destroyableConstraintValidators.remove(instance);
+        if (destroyableHandle != null) {
+            destroyableHandle.destroy();
+        }
     }
 
     /**

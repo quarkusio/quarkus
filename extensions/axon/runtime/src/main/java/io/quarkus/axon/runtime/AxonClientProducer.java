3cc5a3949ad7ad027e56b6bc545520a6d7ec31c1
Jasper Huzen <feaster83@gmail.com>
2019-07-22 23:49:27 +0200

    Fix DI problem and change default serializer to XStreamSerializer (Axon's default)

--- extensions/axon/runtime/src/main/java/io/quarkus/axon/runtime/AxonClientProducer.java
+++ extensions/axon/runtime/src/main/java/io/quarkus/axon/runtime/AxonClientProducer.java
@@ -26,21 +26,12 @@ import org.axonframework.queryhandling.*;
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.json.JacksonSerializer;
 import org.axonframework.serialization.upcasting.event.NoOpEventUpcaster;
+import org.axonframework.serialization.xml.XStreamSerializer;
 
 @ApplicationScoped
 public class AxonClientProducer {
 
-    private Configuration configuration;
-    private Configurer configurer;
     private AxonRuntimeConfig axonRuntimeConfig;
-    private AxonServerConnectionManager axonServerConnectionManager;
-    private AxonServerConfiguration axonServerConfiguration;
-    private EventBus eventBus;
-    private CommandBus commandBus;
-    private QueryBus queryBus;
-    private Serializer serializer;
-    private DefaultQueryGateway queryGateway;
-    private DefaultCommandGateway commandGateway;
 
     private List<Object> sagas = new ArrayList<>();
     private List<Object> aggregates = new ArrayList<>();
@@ -53,158 +44,121 @@ public class AxonClientProducer {
     }
 
     @Produces
-    public Configurer quarkusDefaultConfigurer() {
-        if (configurer == null) {
-            configurer = DefaultConfigurer.defaultConfiguration()
-                    .configureEventBus(c -> eventBus())
-                    .configureCommandBus(c -> commandBus())
-                    .configureQueryBus(c -> queryBus())
-                    .configureSerializer(c -> serializer());
-
-            // When registering a Saga or Aggregate also the EventSourcingHandlers inside
-            // of that class are registered
-            sagas.forEach(saga -> configurer.eventProcessing().registerSaga(saga.getClass()));
-            aggregates.forEach(aggregate -> configurer.configureAggregate(aggregate.getClass()));
-
-            // Command handlers, Event handlers and Query handlers must be registered by itself.
-            commandHandlers.forEach(commandHandler -> configurer.registerCommandHandler(conf -> commandHandler));
-            eventHandlers.forEach(eventHandler -> configurer.registerEventHandler(conf -> eventHandler));
-            queryHandlers.forEach(queryHandler -> configurer.registerQueryHandler(conf -> queryHandler));
-        }
-        return configurer;
-    }
-
-    @Produces
     @ApplicationScoped
-    public Configuration initializeConfiguration() {
-        if (configuration == null) {
-            configuration = quarkusDefaultConfigurer().buildConfiguration();
-            if (axonRuntimeConfig.autostart) {
-                configuration.start();
-            }
+    public Configuration initializeConfiguration(Configurer configurer) {
+        Configuration configuration = configurer.buildConfiguration();
+        if (axonRuntimeConfig.autostart) {
+            configuration.start();
         }
         return configuration;
     }
 
     @Produces
     @Dependent
+    public Configurer quarkusDefaultConfigurer(EventBus eventBus, CommandBus commandBus, QueryBus queryBus,
+                                                Serializer serializer) {
+
+        Configurer configurer = DefaultConfigurer.defaultConfiguration(false)
+                .configureEventBus(c -> eventBus)
+                .configureCommandBus(c -> commandBus)
+                .configureQueryBus(c -> queryBus)
+                .configureSerializer(c -> serializer);
+
+        // When registering a Saga or Aggregate also the EventSourcingHandlers inside
+        // of that class are registered
+        sagas.forEach(saga -> configurer.eventProcessing().registerSaga(saga.getClass()));
+        aggregates.forEach(aggregate -> configurer.configureAggregate(aggregate.getClass()));
+
+        // Command handlers, Event handlers and Query handlers must be registered by itself.
+        commandHandlers.forEach(commandHandler -> configurer.registerCommandHandler(conf -> commandHandler));
+        eventHandlers.forEach(eventHandler -> configurer.registerEventHandler(conf -> eventHandler));
+        queryHandlers.forEach(queryHandler -> configurer.registerQueryHandler(conf -> queryHandler));
+
+        return configurer;
+    }
+
+    @Produces
+    @Dependent
     public AxonServerConfiguration axonServerConfiguration() {
-        if (axonServerConfiguration == null) {
+        AxonServerConfiguration.Builder builder = AxonServerConfiguration.builder();
+        axonRuntimeConfig.clientId.ifPresent(builder::clientId);
+        axonRuntimeConfig.componentName.ifPresent(builder::componentName);
 
-            AxonServerConfiguration.Builder builder = AxonServerConfiguration.builder();
-            axonRuntimeConfig.clientId.ifPresent(builder::clientId);
-            axonRuntimeConfig.componentName.ifPresent(builder::componentName);
-
-            axonRuntimeConfig.servers.ifPresent(builder::servers);
-            if (axonRuntimeConfig.sslEnabled) {
-                axonRuntimeConfig.certFile.ifPresent(builder::ssl);
-            }
-
-            axonRuntimeConfig.token.ifPresent(builder::token);
-            axonRuntimeConfig.eventSecretKey.ifPresent(builder::setEventSecretKey);
-
-            axonRuntimeConfig.context.ifPresent(builder::context);
-            axonRuntimeConfig.maxMessageSize.ifPresent(builder::maxMessageSize);
-            axonRuntimeConfig.snapshotPrefetch.ifPresent(builder::snapshotPrefetch);
-
-            axonServerConfiguration = builder.build();
+        axonRuntimeConfig.servers.ifPresent(builder::servers);
+        if (axonRuntimeConfig.sslEnabled) {
+            axonRuntimeConfig.certFile.ifPresent(builder::ssl);
         }
-        return axonServerConfiguration;
+
+        axonRuntimeConfig.token.ifPresent(builder::token);
+        axonRuntimeConfig.eventSecretKey.ifPresent(builder::setEventSecretKey);
+
+        axonRuntimeConfig.context.ifPresent(builder::context);
+        axonRuntimeConfig.maxMessageSize.ifPresent(builder::maxMessageSize);
+        axonRuntimeConfig.snapshotPrefetch.ifPresent(builder::snapshotPrefetch);
+
+        return builder.build();
     }
 
     @Produces
     @Dependent
-    public AxonServerConnectionManager axonServerConnectionManager() {
-        if (axonServerConnectionManager == null) {
-            axonServerConnectionManager = new AxonServerConnectionManager(axonServerConfiguration());
-        }
-        return axonServerConnectionManager;
+    public AxonServerConnectionManager axonServerConnectionManager(AxonServerConfiguration axonServerConfiguration) {
+       return new AxonServerConnectionManager(axonServerConfiguration);
     }
 
     @Produces
     @Dependent
-    public EventBus eventBus() {
-        if (eventBus == null) {
-            eventBus = AxonServerEventStore.builder()
-                    .eventSerializer(serializer())
-                    .snapshotSerializer(serializer())
-                    .configuration(axonServerConfiguration())
-                    .platformConnectionManager(axonServerConnectionManager())
-                    .upcasterChain(NoOpEventUpcaster.INSTANCE)
-                    .build();
-        }
-        return eventBus;
+    public EventBus eventBus(Serializer serializer, AxonServerConfiguration axonServerConfiguration,
+                                AxonServerConnectionManager axonServerConnectionManager) {
+
+        return AxonServerEventStore.builder()
+                .eventSerializer(serializer)
+                .snapshotSerializer(serializer)
+                .configuration(axonServerConfiguration)
+                .platformConnectionManager(axonServerConnectionManager)
+                .upcasterChain(NoOpEventUpcaster.INSTANCE)
+                .build();
+}
+
+    @Produces
+    @Dependent
+    public CommandBus commandBus(AxonServerConnectionManager axonServerConnectionManager,
+                                    AxonServerConfiguration axonServerConfiguration, Serializer serializer) {
+
+        return new AxonServerCommandBus(axonServerConnectionManager, axonServerConfiguration, SimpleCommandBus.builder().build(),
+                                            serializer, new AnnotationRoutingStrategy());
     }
 
     @Produces
     @Dependent
-    public CommandBus commandBus() {
-        if (commandBus == null) {
-            CommandBus localSegment = SimpleCommandBus.builder().build();
-            commandBus = new AxonServerCommandBus(
-                    axonServerConnectionManager(),
-                    axonServerConfiguration(),
-                    localSegment,
-                    serializer(),
-                    new AnnotationRoutingStrategy());
-
-        }
-        return commandBus;
-    }
-
-    @Produces
-    @Dependent
-    public QueryBus queryBus() {
-        if (queryBus == null) {
-            SimpleQueryBus localQueryBus = SimpleQueryBus.builder().build();
-            SimpleQueryUpdateEmitter queryUpdateEmitter = SimpleQueryUpdateEmitter.builder().build();
-            queryBus = new AxonServerQueryBus(axonServerConnectionManager(),
-                    axonServerConfiguration(),
-                    queryUpdateEmitter,
-                    localQueryBus,
-                    serializer(),
-                    serializer(),
-                    new QueryPriorityCalculator() {
+    public QueryBus queryBus(AxonServerConnectionManager axonServerConnectionManager, AxonServerConfiguration axonServerConfiguration,
+                             Serializer serializer) {
+        
+        return new AxonServerQueryBus(axonServerConnectionManager, axonServerConfiguration,
+                SimpleQueryUpdateEmitter.builder().build(), SimpleQueryBus.builder().build(), serializer, serializer,
+                new QueryPriorityCalculator() {
                         @Override
                         public int determinePriority(QueryMessage<?, ?> queryMessage) {
                             return 0;
                         }
                     });
-        }
-        return queryBus;
     }
 
     @Produces
     @Dependent
-    public QueryGateway queryGateway() {
-        if (queryGateway == null) {
-            queryGateway = DefaultQueryGateway
-                    .builder()
-                    .queryBus(queryBus())
-                    .build();
-        }
-        return queryGateway;
+    public QueryGateway queryGateway(QueryBus queryBus) {
+        return DefaultQueryGateway.builder().queryBus(queryBus).build();
     }
 
     @Produces
     @Dependent
-    public CommandGateway commandGateway() {
-        if (commandGateway == null) {
-            commandGateway = DefaultCommandGateway
-                    .builder()
-                    .commandBus(commandBus())
-                    .build();
-        }
-        return commandGateway;
+    public CommandGateway commandGateway(CommandBus commandBus) {
+        return DefaultCommandGateway.builder().commandBus(commandBus).build();
     }
 
     @Produces
     @Dependent
     public Serializer serializer() {
-        if (serializer == null) {
-            serializer = JacksonSerializer.builder().build();
-        }
-        return serializer;
+        return XStreamSerializer.builder().build();
     }
 
     void registerSaga(Object annotatedBean) {

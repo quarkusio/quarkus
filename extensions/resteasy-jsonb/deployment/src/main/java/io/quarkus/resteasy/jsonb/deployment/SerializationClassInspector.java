218fd13a413007d69bb1a97dc6b1f8bd06a98e21
Georgios Andrianakis <geoand@gmail.com>
2019-07-24 00:33:55 +0300

    Provide ability to generate serializer for subclasses

--- extensions/resteasy-jsonb/deployment/src/main/java/io/quarkus/resteasy/jsonb/deployment/SerializationClassInspector.java
+++ extensions/resteasy-jsonb/deployment/src/main/java/io/quarkus/resteasy/jsonb/deployment/SerializationClassInspector.java
@@ -1,16 +1,20 @@
 package io.quarkus.resteasy.jsonb.deployment;
 
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.AnnotationTarget;
 import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.ClassType;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.FieldInfo;
 import org.jboss.jandex.IndexView;
@@ -21,7 +25,6 @@ import org.jboss.jandex.Type;
 public class SerializationClassInspector {
 
     private final Map<DotName, Result> classInspectionResultMap = new HashMap<>();
-
     private final IndexView index;
 
     public SerializationClassInspector(IndexView index) {
@@ -29,6 +32,10 @@ public class SerializationClassInspector {
     }
 
     public Result inspect(DotName classDotName) {
+        return inspect(classDotName, true);
+    }
+
+    private Result inspect(DotName classDotName, boolean checkSubtypes) {
         ClassInfo classInfo = index.getClassByName(classDotName);
         if (classInfo == null) {
             return SerializationClassInspector.Result.notPossible(classInfo);
@@ -44,19 +51,19 @@ public class SerializationClassInspector {
                 // when the type is an interface than we can correctly generate a serializer at build time
                 // if there is a single implementation
                 // TODO investigate if this can possible be relaxed by checking and comparing all fields, getters and
-                //  class annotations of the implementations
+                //  class annotations of the implementations or perhaps by generating serializers for all implementations?
                 return SerializationClassInspector.Result.notPossible(classInfo);
             } else {
                 return inspect(allKnownImplementors.iterator().next().name());
             }
         }
 
-        if (!index.getAllKnownSubclasses(classDotName).isEmpty()) {
-            // if the class is an interface or is subclassed  we ignore it because json-b
+        if (checkSubtypes && !index.getAllKnownSubclasses(classDotName).isEmpty()) {
+            // if the class is subclassed  we ignore it because json-b
             // adds all the properties of the implementation or subclasses (which we can't know)
             // TODO investigate if we could relax these constraints by checking if there
-            //  there are no implementations or subclasses that contain properties other than those
-            //  of the interface or class
+            //  there are no subclasses that contain properties other than those
+            //  of the interface or class. Another idea is to generate serializers for all subclasses
             return SerializationClassInspector.Result.notPossible(classInfo);
         }
 
@@ -64,9 +71,13 @@ public class SerializationClassInspector {
             return classInspectionResultMap.get(classInfo.name());
         }
 
+        Result superClassResult = null;
         if (!DotNames.OBJECT.equals(classInfo.superName())) {
-            // for now don't handle classes with super types other than object because there are too many corner cases
-            return SerializationClassInspector.Result.notPossible(classInfo);
+            superClassResult = inspect(classInfo.superName(), false);
+            if (!superClassResult.isPossible()) {
+                return SerializationClassInspector.Result.notPossible(classInfo);
+            }
+            classInspectionResultMap.put(classInfo.superName(), superClassResult);
         }
 
         if (JandexUtil.containsInterfacesWithDefaultMethods(classInfo, index)) {
@@ -94,25 +105,25 @@ public class SerializationClassInspector {
         if (removeTransientGetters(getters)) {
             return SerializationClassInspector.Result.notPossible(classInfo);
         }
-        for (MethodInfo methodInfo : getters.keySet()) {
-            // currently we don't support generating serializers that contains items of the same class
-            if (hasReferenceToClassType(methodInfo.returnType(), classDotName)) {
-                return Result.notPossible(classInfo);
-            }
-        }
 
         List<FieldInfo> fields = PropertyUtil.getPublicFieldsWithoutGetters(classInfo, getters.keySet());
         if (removeTransientFields(fields)) {
             return SerializationClassInspector.Result.notPossible(classInfo);
         }
-        for (FieldInfo field : fields) {
-            if (hasReferenceToClassType(field.type(), classDotName)) {
-                return Result.notPossible(classInfo);
-            }
+
+        // we don't support generating a serializer when the class to be serialized is referenced as a field
+        // or getter
+        if (hasCyclicReferences(getters.keySet(), fields, classDotName)) {
+            return SerializationClassInspector.Result.notPossible(classInfo);
         }
 
         SerializationClassInspector.Result result = SerializationClassInspector.Result.possible(classInfo,
                 effectiveClassAnnotations, getters, fields);
+
+        if (superClassResult != null) {
+            result = result.merge(superClassResult);
+        }
+
         classInspectionResultMap.put(classInfo.name(), result);
         return result;
     }
@@ -197,19 +208,60 @@ public class SerializationClassInspector {
         return false;
     }
 
-    // check if type is the same as the class type or is a generic type that references it
-    private boolean hasReferenceToClassType(Type type, DotName classDotName) {
-        if (type.name().equals(classDotName)) {
-            // we don't support serializing types that contain serializable items of the same class
-            return true;
+    // checks whether the methods or fields contain any references back to the class
+    // this check is recursive meaning that the methods and fields are also checked in turn for cyclic references
+    private boolean hasCyclicReferences(Collection<MethodInfo> methods, Collection<FieldInfo> fields, DotName classDotName) {
+        return hasCyclicReferences(methods, fields, Collections.singleton(classDotName));
+    }
+
+    private boolean hasCyclicReferences(Collection<MethodInfo> methods, Collection<FieldInfo> fields, Set<DotName> candidates) {
+        Set<DotName> additionalTypesToCheck = new HashSet<>();
+        // first check if any of the fields or methods contain direct references to the candidates
+        for (MethodInfo method : methods) {
+            if (containedInCandidates(method.returnType(), candidates, additionalTypesToCheck)) {
+                return true;
+            }
+        }
+        for (FieldInfo field : fields) {
+            if (containedInCandidates(field.type(), candidates, additionalTypesToCheck)) {
+                return true;
+            }
+        }
+        // now recursively check the class types of fields and methods to see if any of their
+        // fields or methods contain references
+        for (DotName dotName : additionalTypesToCheck) {
+            ClassInfo classInfo = index.getClassByName(dotName);
+            if (classInfo == null) {
+                continue;
+            }
+            Set<MethodInfo> newMethods = PropertyUtil.getGetterMethods(classInfo).keySet();
+            List<FieldInfo> newFields = PropertyUtil.getPublicFieldsWithoutGetters(classInfo, newMethods);
+            Set<DotName> newCandidates = new HashSet<>(candidates);
+            newCandidates.add(classInfo.name());
+            if (hasCyclicReferences(newMethods, newFields, newCandidates)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean containedInCandidates(Type type, Set<DotName> candidates, Set<DotName> additionalTypesToCheck) {
+        if (type instanceof ClassType) {
+            if (candidates.contains(type.name())) {
+                return true;
+            } else {
+                additionalTypesToCheck.add(type.name());
+            }
         } else if (type instanceof ParameterizedType) {
-            ParameterizedType parameterizedType = type.asParameterizedType();
-            for (Type argumentType : parameterizedType.arguments()) {
-                if (hasReferenceToClassType(argumentType, classDotName)) {
+            List<Type> argumentTypes = type.asParameterizedType().arguments();
+            for (Type argumentType : argumentTypes) {
+                if (containedInCandidates(argumentType, candidates, additionalTypesToCheck)) {
                     return true;
                 }
             }
         }
+
         return false;
     }
 
@@ -262,6 +314,49 @@ public class SerializationClassInspector {
 
         public Collection<FieldInfo> getVisibleFieldsWithoutGetters() {
             return visibleFieldsWithoutGetters;
+        }
+
+        /**
+         * Merge info from other result with lower priority, which means that info from this object
+         * takes precedence if conflicting data exists
+         */
+        public Result merge(Result lowerPriorityResult) {
+            if (!(this.isPossible && lowerPriorityResult.isPossible)) {
+                throw new IllegalArgumentException("merge can only be used on Result objects who have isPossible = true");
+            }
+
+            Map<DotName, AnnotationInstance> finalEffectiveClassAnnotations = new HashMap<>(effectiveClassAnnotations);
+            for (DotName dotName : lowerPriorityResult.getEffectiveClassAnnotations().keySet()) {
+                if (!finalEffectiveClassAnnotations.containsKey(dotName)) {
+                    finalEffectiveClassAnnotations.put(dotName,
+                            lowerPriorityResult.getEffectiveClassAnnotations().get(dotName));
+                }
+            }
+
+            Map<MethodInfo, FieldInfo> finalGetters = new HashMap<>(getters);
+            Set<String> getterNames = new HashSet<>(getters.size());
+            for (MethodInfo methodInfo : getters.keySet()) {
+                getterNames.add(methodInfo.name());
+            }
+            for (MethodInfo methodInfo : lowerPriorityResult.getGetters().keySet()) {
+                if (!getterNames.contains(methodInfo.name())) {
+                    finalGetters.put(methodInfo, lowerPriorityResult.getGetters().get(methodInfo));
+                }
+            }
+
+            Collection<FieldInfo> finalVisibleFieldsWithoutGetters = new ArrayList<>(visibleFieldsWithoutGetters);
+            Set<String> fieldNames = new HashSet<>(visibleFieldsWithoutGetters.size());
+            for (FieldInfo fieldInfo : visibleFieldsWithoutGetters) {
+                fieldNames.add(fieldInfo.name());
+            }
+            for (FieldInfo fieldInfo : lowerPriorityResult.getVisibleFieldsWithoutGetters()) {
+                if (!fieldNames.contains(fieldInfo.name())) {
+                    finalVisibleFieldsWithoutGetters.add(fieldInfo);
+                }
+            }
+
+            return new Result(this.classInfo, true, finalEffectiveClassAnnotations, finalGetters,
+                    finalVisibleFieldsWithoutGetters);
         }
     }
 }

5b40259d0f4eb755c39fb9007d9856ea124b160e
Gwenneg Lepage <gwenneg@gmail.com>
2019-08-19 23:58:45 +0200

    Update native Narayana ConfigurationInfo substitutions

--- extensions/narayana-jta/runtime/src/main/java/io/quarkus/narayana/jta/runtime/graal/ConfigurationInfoSubstitution.java
+++ extensions/narayana-jta/runtime/src/main/java/io/quarkus/narayana/jta/runtime/graal/ConfigurationInfoSubstitution.java
@@ -1,32 +1,27 @@
 package io.quarkus.narayana.jta.runtime.graal;
 
 import com.arjuna.common.util.ConfigurationInfo;
-import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 
 @TargetClass(ConfigurationInfo.class)
 final class ConfigurationInfoSubstitution {
 
-    @Alias
-    private static String sourceId;
-    @Alias
-    private static String propertiesFile;
-    @Alias
-    private static String buildId;
-    @Alias
-    private static boolean isInitialized = false;
-
     // initialize build time properties from data in the jar's META-INF/MANIFEST.MF
     //TODO: actually implement this somehow so these values are baked in at build time
+
     @Substitute
-    private static synchronized void getBuildTimeProperties() {
-        if (isInitialized) {
-            return;
-        }
-        propertiesFile = "jbossts-properties.xml";
-        sourceId = "4d505";
-        buildId = "JBoss Inc. [ochaloup] Linux 4.15.4-200.fc26.x86_64 2018/Feb/26 19:35";
-        isInitialized = true;
+    public static String getSourceId() {
+        return "a66d5";
+    }
+
+    @Substitute
+    public static String getPropertiesFile() {
+        return "jbossts-properties.xml";
+    }
+
+    @Substitute
+    public static String getBuildId() {
+        return "JBoss Inc. [ochaloup] Linux 5.1.20-300.fc30.x86_64 201";
     }
 }

9336c8b829ac67ea9be41f011fb4f97e7575aedc
Guillaume Smet <guillaume.smet@gmail.com>
2019-08-21 11:43:03 +0200

    Upgrade to GraalVM 19.2.0
    Fixes #3604

--- core/creator/src/main/java/io/quarkus/creator/phase/nativeimage/NativeImagePhase.java
+++ core/creator/src/main/java/io/quarkus/creator/phase/nativeimage/NativeImagePhase.java
@@ -103,7 +103,7 @@ public class NativeImagePhase implements AppCreationPhase<NativeImagePhase>, Nat
 
     private String nativeImageXmx;
 
-    private String builderImage = "quay.io/quarkus/ubi-quarkus-native-image:19.1.1";
+    private String builderImage = "quay.io/quarkus/ubi-quarkus-native-image:19.2.0";
 
     private String containerRuntime = "";
 
@@ -561,15 +561,13 @@ public class NativeImagePhase implements AppCreationPhase<NativeImagePhase>, Nat
         }
     }
 
-    //FIXME remove after transition period
     private boolean isThisGraalVMVersionObsolete() {
         final String vmName = System.getProperty("java.vm.name");
         log.info("Running Quarkus native-image plugin on " + vmName);
-        final List<String> obsoleteGraalVmVersions = Arrays.asList("-rc9", "-rc10", "-rc11", "-rc12", "-rc13", "-rc14",
-                "-rc15", "-rc16", "19.0.", "19.1.0");
+        final List<String> obsoleteGraalVmVersions = Arrays.asList("1.0.0", "19.0.", "19.1.");
         final boolean vmVersionIsObsolete = obsoleteGraalVmVersions.stream().anyMatch(vmName::contains);
         if (vmVersionIsObsolete) {
-            log.error("Out of date build of GraalVM detected! Please upgrade to GraalVM 19.1.1.");
+            log.error("Out of date build of GraalVM detected! Please upgrade to GraalVM 19.2.0.");
             return true;
         }
         return false;

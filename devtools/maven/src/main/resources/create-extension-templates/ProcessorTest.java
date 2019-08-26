3e08ec268c23bbe68555aaed252c0462c2b9672b
Daniel Petisme <daniel.petisme@gmail.com>
2019-08-26 13:21:13 +0200

    comments with Sample test

--- devtools/maven/src/main/resources/create-extension-templates/ProcessorTest.java
+++ devtools/maven/src/main/resources/create-extension-templates/ProcessorTest.java
@@ -9,18 +9,39 @@ import io.quarkus.test.QuarkusUnitTest;
 
 class [=artifactIdBaseCamelCase]ProcessorTest {
 
-    // Starting the Quarkus Application
-    @RegisterExtension
-    static final QuarkusUnitTest config = new QuarkusUnitTest()
-        .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+//    @RegisterExtension // Starting the Quarkus Application with your extension loaded
+//    final static final QuarkusUnitTest config = new QuarkusUnitTest()
+//        .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
 //                .addClasses(MyServlet, MyService.class) // Adding custom classes
 //                .addAsResource("custom-application.properties", "application.properties") // Adding custom resources
-//                .addAsResource("data.sql")
-    );
+//                .addAsResource("data.sql"));
+
+//    @RegisterExtension // Starting the Quarkus Application with your extension loaded and hot reload enabled
+//      final static QuarkusDevModeTest test = new QuarkusDevModeTest()
+//          .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+//                 .addClasses(MyServlet, MyService.class) // Adding custom classes
+//                 .addAsResource("custom-application.properties", "application.properties") // Adding custom resources
+//                  .addAsResource("data.sql"));
 
 //    @Test
 //    public void testSomething() {
 //        RestAssured.when().get("/").then() // You can easily test your HTTP enpoints with RestAssured
 //                .statusCode(200).body(equalTo("Hello, World!"));
 //    }
+
+//    @Test
+//    public void testHotRealoading() { // You can test you extension hot reloading strategy
+//        RestAssured.when().get("/new").then()
+//                .statusCode(404);
+//        test.addSourceFile(NewServlet.class);
+//
+//        RestAssured.when().get("/new").then()
+//                .statusCode(200).body(is("A new Servlet"));
+//
+//        test.modifySourceFile("NewServlet.java", (source) -> s.replace("A new Servlet", "Hello Quarkus"));
+//
+//        RestAssured.when().get("/new").then()
+//                .statusCode(200).body(is("Hello Quarkus"));
+//    }
+
 }

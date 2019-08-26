f2d801be18a456d6c0024ea9b7ca291159efd9e2
Daniel Petisme <daniel.petisme@gmail.com>
2019-08-26 13:12:11 +0200

    comments with Sample test

--- devtools/maven/src/main/resources/create-extension-templates/ProcessorTest.java
+++ devtools/maven/src/main/resources/create-extension-templates/ProcessorTest.java
@@ -9,12 +9,18 @@ import io.quarkus.test.QuarkusUnitTest;
 
 class [=artifactIdBaseCamelCase]ProcessorTest {
 
+    // Starting the Quarkus Application
     @RegisterExtension
     static final QuarkusUnitTest config = new QuarkusUnitTest()
-        .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
+        .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+//                .addClasses(MyServlet, MyService.class) // Adding custom classes
+//                .addAsResource("custom-application.properties", "application.properties") // Adding custom resources
+//                .addAsResource("data.sql")
+    );
 
-    @Test
-    public void should() {
-
-    }
+//    @Test
+//    public void testSomething() {
+//        RestAssured.when().get("/").then() // You can easily test your HTTP enpoints with RestAssured
+//                .statusCode(200).body(equalTo("Hello, World!"));
+//    }
 }

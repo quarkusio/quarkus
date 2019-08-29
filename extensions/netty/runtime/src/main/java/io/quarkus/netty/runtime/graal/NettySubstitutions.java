60f499f49ba609d846c5b50de90ae05f64e0e1b7
Clement Escoffier <clement.escoffier@gmail.com>
2019-08-29 10:07:52 +0200

    Remove substitution contained in the upstream netty artifact

--- extensions/netty/runtime/src/main/java/io/quarkus/netty/runtime/graal/NettySubstitutions.java
+++ extensions/netty/runtime/src/main/java/io/quarkus/netty/runtime/graal/NettySubstitutions.java
@@ -11,8 +11,6 @@ import javax.net.ssl.TrustManagerFactory;
 
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.Delete;
-import com.oracle.svm.core.annotate.RecomputeFieldValue;
-import com.oracle.svm.core.annotate.RecomputeFieldValue.Kind;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 import com.oracle.svm.core.jdk.JDK11OrLater;
@@ -45,42 +43,6 @@ final class Target_io_netty_util_internal_logging_InternalLoggerFactory {
     private static InternalLoggerFactory newDefaultFactory(String name) {
         return JdkLoggerFactory.INSTANCE;
     }
-}
-
-/**
- * This substitution allows the usage of platform specific code to do low level
- * buffer related tasks
- */
-@TargetClass(className = "io.netty.util.internal.CleanerJava6")
-final class Target_io_netty_util_internal_CleanerJava6 {
-
-    @Alias
-    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.FieldOffset, declClassName = "java.nio.DirectByteBuffer", name = "cleaner")
-    private static long CLEANER_FIELD_OFFSET;
-}
-
-/**
- * This substitution allows the usage of platform specific code to do low level
- * buffer related tasks
- */
-@TargetClass(className = "io.netty.util.internal.PlatformDependent0")
-final class Target_io_netty_util_internal_PlatformDependent0 {
-
-    @Alias
-    @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.FieldOffset, declClassName = "java.nio.Buffer", name = "address")
-    private static long ADDRESS_FIELD_OFFSET;
-}
-
-/**
- * This substitution allows the usage of platform specific code to do low level
- * buffer related tasks
- */
-@TargetClass(className = "io.netty.util.internal.shaded.org.jctools.util.UnsafeRefArrayAccess")
-final class Target_io_netty_util_internal_shaded_org_jctools_util_UnsafeRefArrayAccess {
-
-    @Alias
-    @RecomputeFieldValue(kind = Kind.ArrayIndexShift, declClass = Object[].class)
-    public static int REF_ELEMENT_SHIFT;
 }
 
 // SSL

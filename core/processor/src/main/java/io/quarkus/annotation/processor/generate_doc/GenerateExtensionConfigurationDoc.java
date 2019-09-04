1647da639c6fffb503016fa825c330d0bfa13c67
Stephane Epardaud <stef@epardaud.fr>
2019-09-04 17:59:31 +0200

    Config docuemntation generator: Backtick enum values

--- core/processor/src/main/java/io/quarkus/annotation/processor/generate_doc/GenerateExtensionConfigurationDoc.java
+++ core/processor/src/main/java/io/quarkus/annotation/processor/generate_doc/GenerateExtensionConfigurationDoc.java
@@ -398,7 +398,9 @@ final public class GenerateExtensionConfigurationDoc {
             if (field.getKind() == ElementKind.ENUM_CONSTANT) {
                 if (sb.length() > 0)
                     sb.append(", ");
+                sb.append("`");
                 sb.append(DocGeneratorUtil.hyphenateEnumValue(field.getSimpleName().toString()));
+                sb.append("`");
             }
         }
         return sb.toString();

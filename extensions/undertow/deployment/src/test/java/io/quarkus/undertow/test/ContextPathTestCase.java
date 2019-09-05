b111d3dad06935c45b57ea9f23d779e5790a4f14
Gwenneg Lepage <gwenneg@gmail.com>
2019-09-05 22:44:07 +0200

    Make RestAssured aware of configured servlet context path

--- extensions/undertow/deployment/src/test/java/io/quarkus/undertow/test/ContextPathTestCase.java
+++ extensions/undertow/deployment/src/test/java/io/quarkus/undertow/test/ContextPathTestCase.java
@@ -1,6 +1,8 @@
 package io.quarkus.undertow.test;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
@@ -9,19 +11,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.test.QuarkusUnitTest;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.restassured.RestAssured;
 
 public class ContextPathTestCase {
+
+    private static final String CONTEXT_PATH = "/foo";
 
     @RegisterExtension
     static QuarkusUnitTest runner = new QuarkusUnitTest()
             .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
                     .addClasses(TestServlet.class)
-                    .addAsResource(new StringAsset("quarkus.servlet.context-path=/foo"), "application.properties"));
+                    .addAsResource(new StringAsset("quarkus.servlet.context-path=" + CONTEXT_PATH), "application.properties"));
+
+    @TestHTTPResource
+    String url;
 
     @Test
     public void testServlet() {
-        RestAssured.when().get("/foo/test").then()
+        assertTrue(System.getProperty("test.url").endsWith(CONTEXT_PATH));
+        assertTrue(url.endsWith(CONTEXT_PATH + "/"));
+        assertEquals(CONTEXT_PATH, RestAssured.basePath);
+        RestAssured.when().get("/test").then()
                 .statusCode(200)
                 .body(is("test servlet"));
     }

6fef6a4af5f1c65081252068e83e7938ffcce76e
Vincent Sevel <vincent@sevel.eu>
2019-09-05 11:40:46 +0200

    NPE in ArcTestRequestScopeProvider.setup() in native mode with dependency on deployment test jar

--- extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/ArcTestRequestScopeProvider.java
+++ extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/ArcTestRequestScopeProvider.java
@@ -1,21 +1,33 @@
 package io.quarkus.arc.deployment;
 
+import org.jboss.logging.Logger;
+
 import io.quarkus.arc.Arc;
 import io.quarkus.arc.ArcContainer;
 import io.quarkus.deployment.test.TestScopeSetup;
 
 public class ArcTestRequestScopeProvider implements TestScopeSetup {
 
+    private static final Logger LOGGER = Logger.getLogger(ArcTestRequestScopeProvider.class);
+
     @Override
     public void setup() {
         ArcContainer container = Arc.container();
-        container.requestContext().activate();
+        if (container == null) {
+            LOGGER.warn("ArcTestRequestScopeProvider: container is null, ignoring setup");
+        } else {
+            container.requestContext().activate();
+        }
 
     }
 
     @Override
     public void tearDown() {
         ArcContainer container = Arc.container();
-        container.requestContext().terminate();
+        if (container == null) {
+            LOGGER.warn("ArcTestRequestScopeProvider: container is null, ignoring tearDown");
+        } else {
+            container.requestContext().terminate();
+        }
     }
 }

35dea7f15a7ceff2d8559ad259c645fc7f956862
Guillaume Smet <guillaume.smet@gmail.com>
2019-09-06 10:37:50 +0200

    Unify EOL on all platforms when returning the original Asciidoc
    The Javadoc parser we use uses platform specific EOL characters.
    
    This should fix the Windows CI failure.

--- core/processor/src/main/java/io/quarkus/annotation/processor/generate_doc/JavaDocParser.java
+++ core/processor/src/main/java/io/quarkus/annotation/processor/generate_doc/JavaDocParser.java
@@ -20,6 +20,8 @@ import io.quarkus.annotation.processor.Constants;
 final class JavaDocParser {
 
     private static final Pattern REMOVE_LEADING_SPACE = Pattern.compile("^ ", Pattern.MULTILINE);
+    private static final Pattern REPLACE_WINDOWS_EOL = Pattern.compile("\r\n");
+    private static final Pattern REPLACE_MACOS_EOL = Pattern.compile("\r");
 
     private static final String HASH = "#";
     private static final String STAR = "*";
@@ -67,7 +69,12 @@ final class JavaDocParser {
         if (isAsciidoc(javadoc)) {
             // it's Asciidoc so we just pass through
             // unfortunately, the Javadoc parser keeps the leading spaces so let's remove them
-            return REMOVE_LEADING_SPACE.matcher(javadoc.getDescription().toText()).replaceAll("");
+            // it also uses platform specific EOL so we need to convert them back to \n
+            String asciidoc = javadoc.getDescription().toText();
+            asciidoc = REMOVE_LEADING_SPACE.matcher(asciidoc).replaceAll("");
+            asciidoc = REPLACE_WINDOWS_EOL.matcher(asciidoc).replaceAll("\n");
+            asciidoc = REPLACE_MACOS_EOL.matcher(asciidoc).replaceAll("\n");
+            return asciidoc;
         }
 
         return htmlJavadocToAsciidoc(javadoc.getDescription());

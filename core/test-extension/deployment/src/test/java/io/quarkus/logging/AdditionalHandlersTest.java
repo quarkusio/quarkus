23a68710f5f6831caf3b7e0fc196c71c67161d6d
ia3andy <adamevin@redhat.com>
2019-09-23 10:24:46 +0200

    Add tests for json formatter and custom log handlers

---
+++ core/test-extension/deployment/src/test/java/io/quarkus/logging/AdditionalHandlersTest.java
@@ -0,0 +1,43 @@
+package io.quarkus.logging;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import org.jboss.logmanager.handlers.DelayedHandler;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.extest.runtime.logging.AdditionalLogHandlerValueFactory.TestHandler;
+import io.quarkus.runtime.logging.InitialConfigurator;
+import io.quarkus.test.QuarkusUnitTest;
+
+public class AdditionalHandlersTest {
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withConfigurationResource("application-additional-handlers.properties");
+
+    @Test
+    public void additionalHandlersConfigurationTest() {
+        LogManager logManager = LogManager.getLogManager();
+        assertThat(logManager).isInstanceOf(org.jboss.logmanager.LogManager.class);
+
+        DelayedHandler delayedHandler = InitialConfigurator.DELAYED_HANDLER;
+        assertThat(Logger.getLogger("").getHandlers()).contains(delayedHandler);
+        assertThat(delayedHandler.getLevel()).isEqualTo(Level.ALL);
+
+        Handler handler = Arrays.stream(delayedHandler.getHandlers())
+                .filter(h -> (h instanceof TestHandler))
+                .findFirst().orElse(null);
+        assertThat(handler).isNotNull();
+        assertThat(handler.getLevel()).isEqualTo(Level.FINE);
+
+        TestHandler testHandler = (TestHandler) handler;
+        assertThat(testHandler.records).isNotEmpty();
+    }
+
+}

c9c30d0d49714d04281eb9f1931d0801c03b0da9
Georgios Andrianakis <geoand@gmail.com>
2019-09-25 17:54:34 +0300

    Provide better integration of RESTEasy with JSON-B
    * Replace ContextResolver generation with a default (low priority) ContextResolver
    * Add the ability to customize JsonbConfig bean
    without replacing it
    
    Co-Authored-By: Guillaume Smet <guillaume.smet@gmail.com>

--- extensions/jsonb/deployment/src/main/java/io/quarkus/jsonb/deployment/JsonbProcessor.java
+++ extensions/jsonb/deployment/src/main/java/io/quarkus/jsonb/deployment/JsonbProcessor.java
@@ -1,14 +1,23 @@
 package io.quarkus.jsonb.deployment;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
 
+import javax.inject.Singleton;
+import javax.json.bind.JsonbConfig;
 import javax.json.bind.adapter.JsonbAdapter;
+import javax.json.bind.serializer.JsonbDeserializer;
+import javax.json.bind.serializer.JsonbSerializer;
 
 import org.eclipse.yasson.JsonBindingProvider;
 import org.eclipse.yasson.spi.JsonbComponentInstanceCreator;
 import org.jboss.jandex.DotName;
 
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.arc.deployment.BeanArchiveIndexBuildItem;
+import io.quarkus.arc.deployment.GeneratedBeanBuildItem;
 import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
 import io.quarkus.arc.processor.BeanInfo;
 import io.quarkus.deployment.annotations.BuildProducer;
@@ -16,6 +25,14 @@ import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.substrate.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.substrate.ServiceProviderBuildItem;
 import io.quarkus.deployment.builditem.substrate.SubstrateResourceBundleBuildItem;
+import io.quarkus.gizmo.ClassCreator;
+import io.quarkus.gizmo.ClassOutput;
+import io.quarkus.gizmo.MethodCreator;
+import io.quarkus.gizmo.MethodDescriptor;
+import io.quarkus.gizmo.ResultHandle;
+import io.quarkus.json.CustomSerDeserBuildItem;
+import io.quarkus.jsonb.JsonbConfigCustomizer;
+import io.quarkus.jsonb.JsonbProducer;
 import io.quarkus.jsonb.QuarkusJsonbComponentInstanceCreator;
 
 public class JsonbProcessor {
@@ -25,7 +42,8 @@ public class JsonbProcessor {
     @BuildStep
     void build(BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
             BuildProducer<SubstrateResourceBundleBuildItem> resourceBundle,
-            BuildProducer<ServiceProviderBuildItem> serviceProvider) {
+            BuildProducer<ServiceProviderBuildItem> serviceProvider,
+            BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
         reflectiveClass.produce(new ReflectiveClassBuildItem(false, false,
                 JsonBindingProvider.class.getName()));
 
@@ -33,6 +51,9 @@ public class JsonbProcessor {
 
         serviceProvider.produce(new ServiceProviderBuildItem(JsonbComponentInstanceCreator.class.getName(),
                 QuarkusJsonbComponentInstanceCreator.class.getName()));
+
+        // this needs to be registered manually since the runtime module is not indexed by Jandex
+        additionalBeans.produce(new AdditionalBeanBuildItem(JsonbProducer.class));
     }
 
     @BuildStep
@@ -45,6 +66,77 @@ public class JsonbProcessor {
                 return bean.isClassBean() && bean.hasType(JSONB_ADAPTER_NAME);
             }
         }));
+    }
+
+    // Generate a JsonbConfigCustomizer bean that registers each serializer / deserializer with JsonbConfig
+    @BuildStep
+    void generateCustomizer(BuildProducer<GeneratedBeanBuildItem> generatedBeans,
+            List<CustomSerDeserBuildItem> customSerDesers) {
+
+        if (customSerDesers.isEmpty()) {
+            return;
+        }
+
+        final Set<String> customSerializerClasses = new HashSet<>();
+        final Set<String> customDeserializerClasses = new HashSet<>();
+        for (CustomSerDeserBuildItem customSerDeser : customSerDesers) {
+            if (customSerDeser.getJsonProvider() != CustomSerDeserBuildItem.JsonProvider.JSONB) {
+                continue;
+            }
+            customSerializerClasses.addAll(customSerDeser.getSerializerClasses());
+            customDeserializerClasses.addAll(customSerDeser.getDeserializerClasses());
+        }
+        if (customSerializerClasses.isEmpty() && customDeserializerClasses.isEmpty()) {
+            return;
+        }
+
+        ClassOutput classOutput = new ClassOutput() {
+            @Override
+            public void write(String name, byte[] data) {
+                generatedBeans.produce(new GeneratedBeanBuildItem(name, data));
+            }
+        };
+
+        try (ClassCreator classCreator = ClassCreator.builder().classOutput(classOutput)
+                .className("io.quarkus.jsonb.customizer.RegisterSerializersAndDeserializersCustomizer")
+                .interfaces(JsonbConfigCustomizer.class.getName())
+                .build()) {
+            classCreator.addAnnotation(Singleton.class);
+
+            try (MethodCreator customize = classCreator.getMethodCreator("customize", void.class, JsonbConfig.class)) {
+                ResultHandle jsonbConfig = customize.getMethodParam(0);
+                if (!customSerializerClasses.isEmpty()) {
+                    ResultHandle serializersArray = customize.newArray(JsonbSerializer.class,
+                            customize.load(customSerializerClasses.size()));
+                    int i = 0;
+                    for (String customSerializerClass : customSerializerClasses) {
+                        customize.writeArrayValue(serializersArray, i,
+                                customize.newInstance(MethodDescriptor.ofConstructor(customSerializerClass)));
+                        i++;
+                    }
+                    customize.invokeVirtualMethod(
+                            MethodDescriptor.ofMethod(JsonbConfig.class, "withSerializers", JsonbConfig.class,
+                                    JsonbSerializer[].class),
+                            jsonbConfig, serializersArray);
+                }
+                if (!customDeserializerClasses.isEmpty()) {
+                    ResultHandle deserializersArray = customize.newArray(JsonbDeserializer.class,
+                            customize.load(customDeserializerClasses.size()));
+                    int i = 0;
+                    for (String customDeserializerClass : customDeserializerClasses) {
+                        customize.writeArrayValue(deserializersArray, i,
+                                customize.newInstance(MethodDescriptor.ofConstructor(customDeserializerClass)));
+                        i++;
+                    }
+                    customize.invokeVirtualMethod(
+                            MethodDescriptor.ofMethod(JsonbConfig.class, "withDeserializers", JsonbConfig.class,
+                                    JsonbDeserializer[].class),
+                            jsonbConfig, deserializersArray);
+                }
+
+                customize.returnValue(null);
+            }
+        }
     }
 
 }

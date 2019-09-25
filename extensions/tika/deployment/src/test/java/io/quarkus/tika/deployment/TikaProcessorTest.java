9b37272676c78bd8f72a1f9edd54cb6557a7cb04
Sergey Beryozkin <sberyozkin@gmail.com>
2019-09-25 12:10:26 +0100

    Support for the parser configuration in the properties

--- extensions/tika/deployment/src/test/java/io/quarkus/tika/deployment/TikaProcessorTest.java
+++ extensions/tika/deployment/src/test/java/io/quarkus/tika/deployment/TikaProcessorTest.java
@@ -4,8 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
@@ -14,6 +17,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.runtime.configuration.QuarkusConfigFactory;
+import io.quarkus.tika.runtime.TikaParserParameter;
 import io.smallrye.config.SmallRyeConfig;
 import io.smallrye.config.SmallRyeConfigBuilder;
 
@@ -45,27 +49,55 @@ public class TikaProcessorTest {
     }
 
     @Test
-    public void testSupportedParserNames() throws Exception {
-        Optional<String> parserNames = Optional.of("pdf");
-        List<String> names = TikaProcessor.getSupportedParserNames(parserNames);
+    public void testPDFParserName() throws Exception {
+        Set<String> names = getParserNames(null, "pdf");
         assertEquals(1, names.size());
-        assertEquals("org.apache.tika.parser.pdf.PDFParser", names.get(0));
+        assertTrue(names.contains("org.apache.tika.parser.pdf.PDFParser"));
     }
 
     @Test
-    public void testResolvableCustomAbbreviation() throws Exception {
-        Optional<String> parserNames = Optional.of("pdf,opendoc");
-        List<String> names = TikaProcessor.getSupportedParserNames(parserNames);
+    public void testODFParserName() throws Exception {
+        Set<String> names = getParserNames(null, "odf");
+        assertEquals(1, names.size());
+        assertTrue(names.contains("org.apache.tika.parser.odf.OpenDocumentParser"));
+    }
+
+    @Test
+    public void testSupportedParserNames() throws Exception {
+        Set<String> names = getParserNames(null, "pdf,odf");
         assertEquals(2, names.size());
         assertTrue(names.contains("org.apache.tika.parser.pdf.PDFParser"));
         assertTrue(names.contains("org.apache.tika.parser.odf.OpenDocumentParser"));
     }
 
     @Test
+    public void testResolvableCustomAbbreviation() throws Exception {
+        Set<String> names = getParserConfig(null, "pdf,opendoc", Collections.emptyMap(),
+                Collections.singletonMap("opendoc",
+                        "org.apache.tika.parser.odf.OpenDocumentParser")).keySet();
+        assertEquals(2, names.size());
+        assertTrue(names.contains("org.apache.tika.parser.pdf.PDFParser"));
+        assertTrue(names.contains("org.apache.tika.parser.odf.OpenDocumentParser"));
+    }
+
+    @Test
+    public void testPdfParserConfig() throws Exception {
+        Map<String, List<TikaParserParameter>> parserConfig = getParserConfig(null, "pdf",
+                Collections.singletonMap("pdf",
+                        Collections.singletonMap("sort-by-position", "true")),
+                Collections.emptyMap());
+        assertEquals(1, parserConfig.size());
+
+        String pdfParserFullName = "org.apache.tika.parser.pdf.PDFParser";
+        assertEquals(1, parserConfig.get(pdfParserFullName).size());
+        assertEquals("sortByPosition", parserConfig.get(pdfParserFullName).get(0).getName());
+        assertEquals("true", parserConfig.get(pdfParserFullName).get(0).getValue());
+    }
+
+    @Test
     public void testUnresolvableCustomAbbreviation() throws Exception {
-        Optional<String> parserNames = Optional.of("classparser");
         try {
-            TikaProcessor.getSupportedParserNames(parserNames);
+            getParserNames(null, "classparser");
             fail("'classparser' is not resolvable");
         } catch (IllegalStateException ex) {
             // expected
@@ -74,8 +106,26 @@ public class TikaProcessorTest {
 
     @Test
     public void testAllSupportedParserNames() throws Exception {
-        Optional<String> parserNames = Optional.ofNullable(null);
-        List<String> names = TikaProcessor.getSupportedParserNames(parserNames);
+        assertEquals(69, getParserNames(null, null).size());
+    }
+
+    @Test
+    public void testSupportedParserNamesWithTikaConfigPath() throws Exception {
+        Set<String> names = getParserNames("tika-config.xml", "pdf");
         assertEquals(69, names.size());
+    }
+
+    private Set<String> getParserNames(String tikaConfigPath, String parsers) throws Exception {
+        return TikaProcessor.getSupportedParserConfig(
+                Optional.ofNullable(tikaConfigPath), Optional.ofNullable(parsers),
+                Collections.emptyMap(), Collections.emptyMap()).keySet();
+    }
+
+    private Map<String, List<TikaParserParameter>> getParserConfig(String tikaConfigPath, String parsers,
+            Map<String, Map<String, String>> parserParamMaps,
+            Map<String, String> parserAbbreviations) throws Exception {
+        return TikaProcessor.getSupportedParserConfig(
+                Optional.ofNullable(tikaConfigPath), Optional.ofNullable(parsers),
+                parserParamMaps, parserAbbreviations);
     }
 }
---
+++ extensions/tika/runtime/src/main/java/io/quarkus/tika/runtime/TikaParserParameter.java
@@ -0,0 +1,41 @@
+package io.quarkus.tika.runtime;
+
+public class TikaParserParameter {
+    private String name;
+    private String value;
+    private String type;
+
+    public TikaParserParameter() {
+
+    }
+
+    public TikaParserParameter(String name, String value, String type) {
+        this.name = name;
+        this.value = value;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}

9f7460e6df0ab4de99de5dcb96525ad6e53a9090
Jason T. Greene <jason.greene@redhat.com>
2019-09-27 14:32:28 -0500

    Limit arena size on JVM mode

--- extensions/netty/deployment/src/main/java/io/quarkus/netty/deployment/NettyProcessor.java
+++ extensions/netty/deployment/src/main/java/io/quarkus/netty/deployment/NettyProcessor.java
@@ -16,6 +16,7 @@ import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.JniBuildItem;
+import io.quarkus.deployment.builditem.SystemPropertyBuildItem;
 import io.quarkus.deployment.builditem.substrate.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.substrate.SubstrateConfigBuildItem;
 import io.quarkus.deployment.builditem.substrate.SubstrateSystemPropertyBuildItem;
@@ -39,6 +40,13 @@ class NettyProcessor {
         //in native mode we limit the size of the epoll array
         //if the array overflows the selector just moves the overflow to a map
         return new SubstrateSystemPropertyBuildItem("sun.nio.ch.maxUpdateArraySize", "100");
+    }
+
+    @BuildStep
+    public SystemPropertyBuildItem limitArenaSize() {
+        //in native mode we limit the size of the epoll array
+        //if the array overflows the selector just moves the overflow to a map
+        return new SystemPropertyBuildItem("io.netty.allocator.maxOrder", "1");
     }
 
     @BuildStep

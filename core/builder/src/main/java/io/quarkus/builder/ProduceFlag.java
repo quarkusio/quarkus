fcdc0b9b873cd7a64b6438db521e6ddd4a35a1bf
David M. Lloyd <david.lloyd@redhat.com>
2019-10-01 11:23:47 -0500

    Add ability to produce overridable build items
    Fixes #4270

--- core/builder/src/main/java/io/quarkus/builder/ProduceFlag.java
+++ core/builder/src/main/java/io/quarkus/builder/ProduceFlag.java
@@ -1,5 +1,7 @@
 package io.quarkus.builder;
 
+import io.quarkus.builder.item.SimpleBuildItem;
+
 /**
  */
 public enum ProduceFlag {
@@ -7,4 +9,8 @@ public enum ProduceFlag {
      * Only produce this item weakly: if only weak items produced by a build step are consumed, the step will not be included.
      */
     WEAK,
+    /**
+     * Only produce this {@link SimpleBuildItem} if no other build steps produce it.
+     */
+    OVERRIDABLE,
 }

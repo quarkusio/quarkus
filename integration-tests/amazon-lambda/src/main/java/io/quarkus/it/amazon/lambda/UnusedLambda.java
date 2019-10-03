6633b50b28ff16237c047a2447c3a07dccb3beb3
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-10-03 12:03:49 +1000

    Add support for named lambda request handlers
    Fixes #3099

--- integration-tests/amazon-lambda/src/main/java/io/quarkus/it/amazon/lambda/UnusedLambda.java
+++ integration-tests/amazon-lambda/src/main/java/io/quarkus/it/amazon/lambda/UnusedLambda.java
@@ -6,14 +6,14 @@ import javax.inject.Named;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-@Named("test")
-public class TestLambda implements RequestHandler<InputObject, OutputObject> {
+@Named("unused")
+public class UnusedLambda implements RequestHandler<InputObject, OutputObject> {
 
     @Inject
     ProcessingService service;
 
     @Override
     public OutputObject handleRequest(InputObject input, Context context) {
-        return service.proces(input).setRequestId(context.getAwsRequestId());
+        throw new RuntimeException("Should be unused");
     }
 }

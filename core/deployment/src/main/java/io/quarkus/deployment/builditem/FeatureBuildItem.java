78fd3dcbb174b518cf71eff5ee0e9958d7348321
Guillaume Smet <guillaume.smet@gmail.com>
2019-10-07 10:11:08 +0200

    Add a resteasy-jaxb extension
    This should solve the problems of Apache Camel: they have JAXB
    annotations in their model and it triggers JAXB inclusion even if they
    don't want to use it.

--- core/deployment/src/main/java/io/quarkus/deployment/builditem/FeatureBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/builditem/FeatureBuildItem.java
@@ -43,6 +43,7 @@ public final class FeatureBuildItem extends MultiBuildItem {
     public static final String NEO4J = "neo4j";
     public static final String RESTEASY = "resteasy";
     public static final String RESTEASY_JACKSON = "resteasy-jackson";
+    public static final String RESTEASY_JAXB = "resteasy-jaxb";
     public static final String RESTEASY_JSONB = "resteasy-jsonb";
     public static final String REST_CLIENT = "rest-client";
     public static final String SCALA = "scala";

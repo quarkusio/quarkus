3640a8e02633e2de8ab143587ba39856a7c2fc62
Yoann Rodière <yoann@hibernate.org>
2019-10-08 12:24:55 +0200

    Test enums annotated with Hibernate Validator annotations
    Fails with the current version of Hibernate Validator.

---
+++ integration-tests/hibernate-validator/src/main/java/io/quarkus/it/hibernate/validator/enums/MyEnumWithValidatorAnnotation.java
@@ -0,0 +1,17 @@
+package io.quarkus.it.hibernate.validator.enums;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Simply adding this class to the code, without even referencing it from anywhere,
+ * used to make Quarkus startup fail because of an error in Hibernate Validator.
+ * See https://github.com/quarkusio/quarkus/issues/3284
+ */
+public enum MyEnumWithValidatorAnnotation {
+    VALUE1,
+    VALUE2;
+
+    public static MyEnumWithValidatorAnnotation fromCode(@NotNull String code) {
+        return MyEnumWithValidatorAnnotation.valueOf(code);
+    }
+}

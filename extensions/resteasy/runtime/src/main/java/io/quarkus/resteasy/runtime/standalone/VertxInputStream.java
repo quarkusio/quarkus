fdba4453fa01f78b5fac6a41b8a578931d36236a
Georgios Andrianakis <geoand@gmail.com>
2019-10-09 14:18:12 +0300

    Make VertxBlockingInput static

--- extensions/resteasy/runtime/src/main/java/io/quarkus/resteasy/runtime/standalone/VertxInputStream.java
+++ extensions/resteasy/runtime/src/main/java/io/quarkus/resteasy/runtime/standalone/VertxInputStream.java
@@ -109,7 +109,7 @@ public class VertxInputStream extends InputStream {
         }
     }
 
-    public class VertxBlockingInput implements Handler<Buffer> {
+    public static class VertxBlockingInput implements Handler<Buffer> {
         protected final HttpServerRequest request;
         protected Buffer input1;
         protected Deque<Buffer> inputOverflow;

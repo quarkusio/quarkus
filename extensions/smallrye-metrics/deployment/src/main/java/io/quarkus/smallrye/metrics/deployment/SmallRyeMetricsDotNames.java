02539195e81bce8b8f65f446c51631bb0523a6d0
Jan Martiska <jmartisk@redhat.com>
2019-10-21 09:30:44 +0200

    Metrics extension should not add Dependent scope to JAX-RS provider classes

--- extensions/smallrye-metrics/deployment/src/main/java/io/quarkus/smallrye/metrics/deployment/SmallRyeMetricsDotNames.java
+++ extensions/smallrye-metrics/deployment/src/main/java/io/quarkus/smallrye/metrics/deployment/SmallRyeMetricsDotNames.java
@@ -57,9 +57,10 @@ public class SmallRyeMetricsDotNames {
         return METRICS_ANNOTATIONS.contains(instance.name());
     }
 
-    // these are needed for determining whether a class is a REST endpoint
+    // these are needed for determining whether a class is a REST endpoint or JAX-RS provider
     public static final DotName JAXRS_PATH = DotName.createSimple("javax.ws.rs.Path");
     public static final DotName REST_CONTROLLER = DotName
             .createSimple("org.springframework.web.bind.annotation.RestController");
+    public static final DotName JAXRS_PROVIDER = DotName.createSimple("javax.ws.rs.ext.Provider");
 
 }

1005fed0ac9afb2f878b7a54d03a1381dc95ce6f
Stephane Epardaud <stef@epardaud.fr>
2019-10-22 15:41:30 +0200

    Some tweaks to security guides
    Added generated config tables, some examples

--- extensions/elytron-security-properties-file/runtime/src/main/java/io/quarkus/elytron/security/runtime/SecurityUsersConfig.java
+++ extensions/elytron-security-properties-file/runtime/src/main/java/io/quarkus/elytron/security/runtime/SecurityUsersConfig.java
@@ -1,5 +1,6 @@
 package io.quarkus.elytron.security.runtime;
 
+import io.quarkus.runtime.annotations.ConfigDocSection;
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
@@ -10,14 +11,16 @@ import io.quarkus.runtime.annotations.ConfigRoot;
 @ConfigRoot(name = "security.users", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
 public final class SecurityUsersConfig {
     /**
-     * The configuration for the {@linkplain org.wildfly.security.auth.realm.LegacyPropertiesSecurityRealm}
+     * Property Files Realm Configuration
      */
     @ConfigItem
+    @ConfigDocSection
     public PropertiesRealmConfig file;
     /**
-     * The configuration for the {@linkplain org.wildfly.security.auth.realm.SimpleMapBackedSecurityRealm}
+     * Embedded Realm Configuration
      */
     @ConfigItem
+    @ConfigDocSection
     public MPRealmConfig embedded;
 
 }

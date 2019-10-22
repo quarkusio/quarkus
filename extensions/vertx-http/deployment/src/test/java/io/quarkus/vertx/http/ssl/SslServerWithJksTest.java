6fdf268277b2daa551dbada34d46988d6e1cf515
cstdev <pietomb00@hotmail.com>
2019-10-22 16:15:36 +0000

    Add test for Mutual TLS
    Also allow the SSL URL to be injected into tests that need it.

--- extensions/vertx-http/deployment/src/test/java/io/quarkus/vertx/http/ssl/SslServerWithJksTest.java
+++ extensions/vertx-http/deployment/src/test/java/io/quarkus/vertx/http/ssl/SslServerWithJksTest.java
@@ -3,7 +3,6 @@ package io.quarkus.vertx.http.ssl;
 import static org.hamcrest.core.Is.is;
 
 import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -18,10 +17,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.test.QuarkusUnitTest;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.restassured.RestAssured;
 import io.vertx.ext.web.Router;
 
 public class SslServerWithJksTest {
+
+    @TestHTTPResource(value = "/ssl", ssl = true)
+    URL url;
 
     @RegisterExtension
     static final QuarkusUnitTest config = new QuarkusUnitTest()
@@ -41,8 +44,7 @@ public class SslServerWithJksTest {
     }
 
     @Test
-    public void testSslServerWithJKS() throws MalformedURLException {
-        URL url = new URL("https://localhost:8444/ssl");
+    public void testSslServerWithJKS() {
         RestAssured.get(url).then().statusCode(200).body(is("ssl"));
     }
 
--- test-framework/common/src/main/java/io/quarkus/test/common/http/TestHTTPResourceManager.java
+++ test-framework/common/src/main/java/io/quarkus/test/common/http/TestHTTPResourceManager.java
@@ -20,6 +20,14 @@ public class TestHTTPResourceManager {
         return "http://" + host + ":" + port + contextPath;
     }
 
+    public static String getSslUri() {
+        Config config = ConfigProvider.getConfig();
+        String host = config.getOptionalValue("quarkus.http.host", String.class).orElse("localhost");
+        String port = config.getOptionalValue("quarkus.http.test-ssl-port", String.class).orElse("8444");
+        String contextPath = config.getOptionalValue("quarkus.servlet.context-path", String.class).orElse("");
+        return "https://" + host + ":" + port + contextPath;
+    }
+
     public static void inject(Object testCase) {
         Map<Class<?>, TestHTTPResourceProvider<?>> providers = getProviders();
         Class<?> c = testCase.getClass();
@@ -34,10 +42,18 @@ public class TestHTTPResourceManager {
                     }
                     String path = resource.value();
                     String val;
-                    if (path.startsWith("/")) {
-                        val = getUri() + path;
+                    if (resource.ssl()) {
+                        if (path.startsWith("/")) {
+                            val = getSslUri() + path;
+                        } else {
+                            val = getSslUri() + "/" + path;
+                        }
                     } else {
-                        val = getUri() + "/" + path;
+                        if (path.startsWith("/")) {
+                            val = getUri() + path;
+                        } else {
+                            val = getUri() + "/" + path;
+                        }
                     }
                     f.setAccessible(true);
                     try {

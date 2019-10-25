fc4ff44ad4777e901a3e06b3fcf867c1dc334c8b
Jaikiran Pai <jaikiran.pai@gmail.com>
2019-10-25 20:38:19 +0530

    issue-4830 Don't let org.jgroups.protocols.DISCARD use Java Swing or AWT classes in native mode

--- extensions/infinispan-embedded/runtime/src/main/java/io/quarkus/infinispan/embedded/runtime/graal/SubstituteJGroups.java
+++ extensions/infinispan-embedded/runtime/src/main/java/io/quarkus/infinispan/embedded/runtime/graal/SubstituteJGroups.java
@@ -6,6 +6,7 @@ import org.jgroups.util.UUID;
 
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.RecomputeFieldValue;
+import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 
 public class SubstituteJGroups {
@@ -17,4 +18,32 @@ final class SubstituteUUID {
     // Force it to null - so it can be reinitialized
     @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset)
     protected static volatile SecureRandom numberGenerator;
+}
+
+// DISCARD protocol uses swing classes
+@TargetClass(className = "org.jgroups.protocols.DISCARD")
+final class SubstituteDiscardProtocol {
+
+    @Substitute
+    public void startGui() {
+        // do nothing
+    }
+
+    @Substitute
+    public void stopGui() {
+        // do nothing
+    }
+
+    @Substitute
+    public void start() throws Exception {
+        // should call super.start() but the "super" Protocol.start() does nothing,
+        // so this empty impl is OK
+    }
+
+    @Substitute
+    public void stop() {
+        // should call super.stop() but the "super" Protocol.stop() does nothing,
+        // so this empty impl is OK
+
+    }
 }

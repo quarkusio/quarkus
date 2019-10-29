57caa1b605f232d48d2a844c8168c879b1a5ea0e
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-10-29 13:36:08 +1100

    Implement web.xml based permissions
    Fixes #4349

--- integration-tests/elytron-undertow/src/main/java/io/quarkus/it/undertow/elytron/GreetingServlet.java
+++ integration-tests/elytron-undertow/src/main/java/io/quarkus/it/undertow/elytron/GreetingServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "ServletGreeting", urlPatterns = "/")
+@WebServlet(name = "ServletGreeting", urlPatterns = "/*")
 public class GreetingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {

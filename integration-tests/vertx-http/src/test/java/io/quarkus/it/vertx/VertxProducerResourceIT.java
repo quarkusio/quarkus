58d9c6259dbe19652aaacc63efed1d872fe95d11
cstdev <pietomb00@hotmail.com>
2019-10-29 12:34:05 +0000

    Use correct ports for MTLS native test
    Native runs in prod profile and so doesn't use the set test ports,
    need to explicitly use the same port as would be used in prod. Currently hardcoded though.
    
    Use different method to include resources, specify all jks rather than using resource file.
    
    Fix import order
    
    Correct algorithm used in keystores, can't use DSA in Java 11+

--- integration-tests/vertx-http/src/test/java/io/quarkus/it/vertx/VertxProducerResourceIT.java
+++ integration-tests/vertx-http/src/test/java/io/quarkus/it/vertx/VertxProducerResourceIT.java
@@ -1,8 +1,25 @@
 package io.quarkus.it.vertx;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.jupiter.api.Test;
+
 import io.quarkus.test.junit.NativeImageTest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 
 @NativeImageTest
 public class VertxProducerResourceIT extends VertxProducerResourceTest {
 
+    @Test
+    public void testRouteRegistrationMTLS() {
+        RequestSpecification spec = new RequestSpecBuilder()
+                .setBaseUri(String.format("%s://%s", url.getProtocol(), url.getHost()))
+                .setPort(8443)
+                .setKeyStore("client-keystore.jks", "password")
+                .setTrustStore("client-truststore.jks", "password")
+                .build();
+        given().spec(spec).get("/my-path").then().body(containsString("OK"));
+    }
 }

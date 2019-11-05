d81e508b6fb543724bd526c01304c28ebedc3d8f
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2019-11-05 14:46:09 +0100

    fix: Dev mode - 404 Page - Live reloaded list of static files

--- extensions/resteasy/deployment/src/test/java/io/quarkus/resteasy/test/NotFoundExceptionMapperTestCase.java
+++ extensions/resteasy/deployment/src/test/java/io/quarkus/resteasy/test/NotFoundExceptionMapperTestCase.java
@@ -1,7 +1,11 @@
 package io.quarkus.resteasy.test;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+
 import org.hamcrest.Matchers;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -11,10 +15,13 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 public class NotFoundExceptionMapperTestCase {
+    private static final String META_INF_RESOURCES = "META-INF/resources/";
+
     @RegisterExtension
     static QuarkusDevModeTest test = new QuarkusDevModeTest()
             .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
-                    .addClasses(RootResource.class));
+                    .addClasses(RootResource.class)
+                    .addAsResource(new StringAsset("index content"), META_INF_RESOURCES + "index.html"));
 
     @Test
     public void testResourceNotFound() {
@@ -32,6 +39,7 @@ public class NotFoundExceptionMapperTestCase {
                 .then()
                 .statusCode(404)
                 .contentType(ContentType.HTML)
+                .body(containsString("index.html")) // check that index.html is displayed
                 .body(Matchers.containsString("<h1 class=\"container\">404 - Resource Not Found</h1>"));
     }
 
@@ -45,4 +53,46 @@ public class NotFoundExceptionMapperTestCase {
                 .contentType(ContentType.JSON);
     }
 
+    @Test
+    public void shouldDisplayNewAddedFileIn404ErrorPage() {
+        String CONTENT = "html content";
+        test.addResourceFile(META_INF_RESOURCES + "index2.html", CONTENT);
+
+        RestAssured.get("/index2.html")
+                .then()
+                .statusCode(200)
+                .body(containsString(CONTENT)); // check that index2.html is live reloaded
+
+        RestAssured.given()
+                .accept(ContentType.HTML)
+                .when()
+                .get("/api")
+                .then() // try to load unknown path
+                .statusCode(404)
+                .body(containsString("index2.html")); // check that index2.html is displayed
+    }
+
+    @Test
+    public void shouldNotDisplayDeletedFileIn404ErrorPage() {
+        String TEST_CONTENT = "test html content";
+        test.addResourceFile(META_INF_RESOURCES + "test.html", TEST_CONTENT);
+
+        RestAssured
+                .get("/test.html")
+                .then()
+                .statusCode(200)
+                .body(containsString(TEST_CONTENT)); // check that test.html is live reloaded
+
+        test.deleteResourceFile(META_INF_RESOURCES + "test.html"); // delete test.html file
+
+        RestAssured
+                .given()
+                .accept(ContentType.HTML)
+                .when()
+                .get("/test.html")
+                .then() // try to load static file
+                .statusCode(404)
+                .body(not(containsString("test.html"))); // check that test.html is not displayed
+
+    }
 }

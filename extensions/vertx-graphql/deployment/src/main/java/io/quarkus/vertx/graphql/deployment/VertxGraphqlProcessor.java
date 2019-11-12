c5d04d838276733618b87f1d5d0ee0993501d87f
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2019-11-12 20:04:08 +0100

    fix(vertx-graphql): make the extension work in native mode
    fixes #5248

--- extensions/vertx-graphql/deployment/src/main/java/io/quarkus/vertx/graphql/deployment/VertxGraphqlProcessor.java
+++ extensions/vertx-graphql/deployment/src/main/java/io/quarkus/vertx/graphql/deployment/VertxGraphqlProcessor.java
@@ -1,11 +1,18 @@
 package io.quarkus.vertx.graphql.deployment;
 
+import java.util.Arrays;
+import java.util.List;
+
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImagePackageResourceBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.vertx.http.deployment.WebsocketSubProtocolsBuildItem;
+import io.vertx.ext.web.handler.graphql.impl.GraphQLBatch;
+import io.vertx.ext.web.handler.graphql.impl.GraphQLInputDeserializer;
+import io.vertx.ext.web.handler.graphql.impl.GraphQLQuery;
 
 class VertxGraphqlProcessor {
-
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FeatureBuildItem.VERTX_GRAPHQL);
@@ -14,5 +21,18 @@ class VertxGraphqlProcessor {
     @BuildStep
     WebsocketSubProtocolsBuildItem websocketSubProtocols() {
         return new WebsocketSubProtocolsBuildItem("graphql-ws");
+    }
+
+    @BuildStep
+    List<ReflectiveClassBuildItem> registerForReflection() {
+        return Arrays.asList(
+                new ReflectiveClassBuildItem(true, true, GraphQLInputDeserializer.class.getName()),
+                new ReflectiveClassBuildItem(true, true, GraphQLBatch.class.getName()),
+                new ReflectiveClassBuildItem(true, true, GraphQLQuery.class.getName()));
+    }
+
+    @BuildStep
+    NativeImagePackageResourceBuildItem registerNativeImageResources() {
+        return new NativeImagePackageResourceBuildItem("io/vertx/ext/web/handler/graphiql");
     }
 }

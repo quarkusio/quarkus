2bdf7b9b663248624bcd395a1628ac07214d5fdd
Fabian Martinez <famartin@redhat.com>
2019-11-13 18:05:46 +0100

    Use same ResumingRouter in CDI as in Observes events.

--- integration-tests/vertx-http/src/main/java/io/quarkus/it/vertx/VertxProducerResource.java
+++ integration-tests/vertx-http/src/main/java/io/quarkus/it/vertx/VertxProducerResource.java
@@ -1,7 +1,9 @@
 package io.quarkus.it.vertx;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -28,6 +30,19 @@ public class VertxProducerResource {
             throw new NullPointerException("router instance should have been injected");
         }
         return "vert.x has been injected";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public String testBody(String body) {
+        if (vertx == null) {
+            throw new NullPointerException("vert.x instance should have been injected");
+        }
+        if (router == null) {
+            throw new NullPointerException("router instance should have been injected");
+        }
+        return body;
     }
 
 }
--- integration-tests/vertx-http/src/test/java/io/quarkus/it/vertx/VertxProducerResourceTest.java
+++ integration-tests/vertx-http/src/test/java/io/quarkus/it/vertx/VertxProducerResourceTest.java
@@ -2,10 +2,12 @@ package io.quarkus.it.vertx;
 
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
 
 @QuarkusTest
 public class VertxProducerResourceTest {
@@ -13,6 +15,12 @@ public class VertxProducerResourceTest {
     @Test
     public void testInjection() {
         get("/").then().body(containsString("vert.x has been injected"));
+    }
+
+    @Test
+    public void testInjectedRouter() {
+        RestAssured.given().contentType("text/plain").body("Hello world!")
+                .post("/").then().body(is("Hello world!"));
     }
 
     @Test

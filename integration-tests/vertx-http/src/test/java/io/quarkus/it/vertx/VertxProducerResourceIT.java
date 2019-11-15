4aa4e70c360eb9f0aa4e80ad7a4342a0dffc4a7c
cstdev <pietomb00@hotmail.com>
2019-11-15 10:04:07 +0000

    Work around SunEC warning

--- integration-tests/vertx-http/src/test/java/io/quarkus/it/vertx/VertxProducerResourceIT.java
+++ integration-tests/vertx-http/src/test/java/io/quarkus/it/vertx/VertxProducerResourceIT.java
@@ -3,6 +3,11 @@ package io.quarkus.it.vertx;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
+import java.security.Provider;
+import java.security.Security;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.NativeImageTest;
@@ -11,6 +16,20 @@ import io.restassured.specification.RequestSpecification;
 
 @NativeImageTest
 public class VertxProducerResourceIT extends VertxProducerResourceTest {
+
+    private static Provider sunECProvider;
+
+    @BeforeAll
+    public static void setupSecProvider() {
+        //Remove SunEC provider for the test as it's not being provided for tests.
+        sunECProvider = Security.getProvider("SunEC");
+        Security.removeProvider("SunEC");
+    }
+
+    @AfterAll
+    public static void restoreSecProvider() {
+        Security.addProvider(sunECProvider);
+    }
 
     @Test
     public void testRouteRegistrationMTLS() {

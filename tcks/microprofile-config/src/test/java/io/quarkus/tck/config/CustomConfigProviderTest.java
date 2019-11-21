6fd7e99b65a54beddd1c5ca48ccd9567bee43309
David M. Lloyd <david.lloyd@redhat.com>
2019-11-21 08:56:21 -0600

    TCK fixes to deal with property expansion

--- tcks/microprofile-config/src/test/java/io/quarkus/tck/config/CustomConfigProviderTest.java
+++ tcks/microprofile-config/src/test/java/io/quarkus/tck/config/CustomConfigProviderTest.java
@@ -3,12 +3,32 @@ package io.quarkus.tck.config;
 import org.eclipse.microprofile.config.tck.ConfigProviderTest;
 import org.testng.annotations.Test;
 
+import io.quarkus.runtime.configuration.ExpandingConfigSource;
+
 public class CustomConfigProviderTest extends ConfigProviderTest {
-    @Test(enabled = false)
+    @Test
     public void testEnvironmentConfigSource() {
+        // this test fails when there is a expression-like thing in an env prop
+        boolean old = ExpandingConfigSource.setExpanding(false);
+        try {
+            super.testPropertyConfigSource();
+        } finally {
+            ExpandingConfigSource.setExpanding(old);
+        }
     }
 
     @Test(enabled = false)
     public void testInjectedConfigSerializable() {
+    }
+
+    @Test
+    public void testPropertyConfigSource() {
+        // this test fails when there is a expression-like thing in a sys prop
+        boolean old = ExpandingConfigSource.setExpanding(false);
+        try {
+            super.testPropertyConfigSource();
+        } finally {
+            ExpandingConfigSource.setExpanding(old);
+        }
     }
 }

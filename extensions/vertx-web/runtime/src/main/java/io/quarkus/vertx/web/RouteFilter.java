ebcaaee3459538bbda334ef414b40374c192562c
Martin Kouba <mkouba@redhat.com>
2019-11-21 16:30:19 +0100

    Mention filter priority in javadoc

--- extensions/vertx-web/runtime/src/main/java/io/quarkus/vertx/web/RouteFilter.java
+++ extensions/vertx-web/runtime/src/main/java/io/quarkus/vertx/web/RouteFilter.java
@@ -5,12 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import io.quarkus.vertx.http.runtime.filters.Filter;
 import io.vertx.ext.web.RoutingContext;
 
 /**
  * This annotation is used to define a "filter", i.e. a route called on every HTTP request.
  * <p>
  * The target business method must return {@code void} and accept exactly one argument of type {@link RoutingContext}.
+ * <p>
+ * Filters with higher priority are called first. The default priority for all annotation-based filters is
+ * {@link #DEFAULT_PRIORITY}.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
@@ -22,6 +26,7 @@ public @interface RouteFilter {
      * Filters with higher priority are called first.
      * 
      * @return the priority
+     * @see Filter#getPriority()
      */
     int value() default DEFAULT_PRIORITY;
 

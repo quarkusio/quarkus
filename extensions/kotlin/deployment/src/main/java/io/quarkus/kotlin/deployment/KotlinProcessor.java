ac806ecfa174ebfe2b3246dca1e543caeee2272f
Georgios Andrianakis <geoand@gmail.com>
2019-11-29 17:27:34 +0200

    Ensure that the Kotlin and Scala Jackson modules are auto-registered if present

--- extensions/kotlin/deployment/src/main/java/io/quarkus/kotlin/deployment/KotlinProcessor.java
+++ extensions/kotlin/deployment/src/main/java/io/quarkus/kotlin/deployment/KotlinProcessor.java
@@ -1,12 +1,30 @@
 package io.quarkus.kotlin.deployment;
 
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.jackson.spi.ClassPathJacksonModuleBuildItem;
 
 public class KotlinProcessor {
+
+    private static final String KOTLIN_JACKSON_MODULE = "com.fasterxml.jackson.module.kotlin.KotlinModule";
 
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FeatureBuildItem.KOTLIN);
+    }
+
+    /*
+     * Register the Kotlin Jackson module if that has been added to the classpath
+     * Producing the BuildItem is entirely safe since if quarkus-jackson is not on the classpath
+     * the BuildItem will just be ignored
+     */
+    @BuildStep
+    void registerKotlinJacksonModule(BuildProducer<ClassPathJacksonModuleBuildItem> classPathJacksonModules) {
+        try {
+            Class.forName(KOTLIN_JACKSON_MODULE, false, Thread.currentThread().getContextClassLoader());
+            classPathJacksonModules.produce(new ClassPathJacksonModuleBuildItem(KOTLIN_JACKSON_MODULE));
+        } catch (Exception ignored) {
+        }
     }
 }

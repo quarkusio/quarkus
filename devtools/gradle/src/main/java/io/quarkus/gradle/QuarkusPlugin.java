0c83359916e5ca5e72627270efa0984c4ff65c94
George Gastaldi <gegastaldi@gmail.com>
2019-12-02 13:35:34 -0300

    Fix buildNative not being called in gradle build

--- devtools/gradle/src/main/java/io/quarkus/gradle/QuarkusPlugin.java
+++ devtools/gradle/src/main/java/io/quarkus/gradle/QuarkusPlugin.java
@@ -8,11 +8,11 @@ import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.plugins.BasePlugin;
+import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetContainer;
-import org.gradle.api.tasks.SourceSetOutput;
 import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.testing.Test;
 import org.gradle.util.GradleVersion;
@@ -26,10 +26,22 @@ import io.quarkus.gradle.tasks.QuarkusNative;
 import io.quarkus.gradle.tasks.QuarkusTestConfig;
 import io.quarkus.gradle.tasks.QuarkusTestNative;
 
-/**
- * @author <a href="mailto:stalep@gmail.com">Ståle Pedersen</a>
- */
 public class QuarkusPlugin implements Plugin<Project> {
+
+    public static final String LIST_EXTENSIONS_TASK_NAME = "listExtensions";
+    public static final String ADD_EXTENSION_TASK_NAME = "addExtension";
+    public static final String QUARKUS_BUILD_TASK_NAME = "quarkusBuild";
+    public static final String GENERATE_CONFIG_TASK_NAME = "generateConfig";
+    public static final String QUARKUS_DEV_TASK_NAME = "quarkusDev";
+    public static final String BUILD_NATIVE_TASK_NAME = "buildNative";
+    public static final String TEST_NATIVE_TASK_NAME = "testNative";
+    public static final String QUARKUS_TEST_CONFIG_TASK_NAME = "quarkusTestConfig";
+
+    // this name has to be the same as the directory in which the tests reside
+    public static final String NATIVE_TEST_SOURCE_SET_NAME = "native-test";
+
+    public static final String NATIVE_TEST_IMPLEMENTATION_CONFIGURATION_NAME = "nativeTestImplementation";
+    public static final String NATIVE_TEST_RUNTIME_ONLY_CONFIGURATION_NAME = "nativeTestRuntimeOnly";
 
     @Override
     public void apply(Project project) {
@@ -42,12 +54,14 @@ public class QuarkusPlugin implements Plugin<Project> {
 
     private void registerTasks(Project project) {
         TaskContainer tasks = project.getTasks();
-        tasks.create("listExtensions", QuarkusListExtensions.class);
-        tasks.create("addExtension", QuarkusAddExtension.class);
+        tasks.create(LIST_EXTENSIONS_TASK_NAME, QuarkusListExtensions.class);
+        tasks.create(ADD_EXTENSION_TASK_NAME, QuarkusAddExtension.class);
+        tasks.create(GENERATE_CONFIG_TASK_NAME, QuarkusGenerateConfig.class);
 
-        Task quarkusBuild = tasks.create("quarkusBuild", QuarkusBuild.class);
-        Task quarkusGenerateConfig = tasks.create("generateConfig", QuarkusGenerateConfig.class);
-        Task quarkusDev = tasks.create("quarkusDev", QuarkusDev.class);
+        Task quarkusBuild = tasks.create(QUARKUS_BUILD_TASK_NAME, QuarkusBuild.class);
+        Task quarkusDev = tasks.create(QUARKUS_DEV_TASK_NAME, QuarkusDev.class);
+        Task buildNative = tasks.create(BUILD_NATIVE_TASK_NAME, QuarkusNative.class);
+        Task quarkusTestConfig = tasks.create(QUARKUS_TEST_CONFIG_TASK_NAME, QuarkusTestConfig.class);
 
         project.getPlugins().withType(
                 BasePlugin.class,
@@ -57,40 +71,39 @@ public class QuarkusPlugin implements Plugin<Project> {
                 javaPlugin -> {
                     Task classesTask = tasks.getByName(JavaPlugin.CLASSES_TASK_NAME);
                     quarkusDev.dependsOn(classesTask);
-                    quarkusBuild.dependsOn(classesTask);
+                    quarkusBuild.dependsOn(classesTask, tasks.getByName(JavaPlugin.JAR_TASK_NAME));
 
-                    Task jarTask = tasks.getByName(JavaPlugin.JAR_TASK_NAME);
-                    quarkusBuild.dependsOn(jarTask);
+                    buildNative.dependsOn(tasks.getByName(BasePlugin.ASSEMBLE_TASK_NAME));
+
+                    SourceSetContainer sourceSets = project.getConvention().findPlugin(JavaPluginConvention.class)
+                            .getSourceSets();
+                    SourceSet nativeTestSourceSet = sourceSets.create(NATIVE_TEST_SOURCE_SET_NAME);
+                    SourceSet mainSourceSet = sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME);
+                    SourceSet testSourceSet = sourceSets.getByName(SourceSet.TEST_SOURCE_SET_NAME);
+
+                    nativeTestSourceSet.setCompileClasspath(
+                            nativeTestSourceSet.getCompileClasspath()
+                                    .plus(mainSourceSet.getCompileClasspath())
+                                    .plus(testSourceSet.getCompileClasspath()));
+
+                    nativeTestSourceSet.setRuntimeClasspath(
+                            nativeTestSourceSet.getRuntimeClasspath()
+                                    .plus(mainSourceSet.getRuntimeClasspath())
+                                    .plus(testSourceSet.getRuntimeClasspath()));
+
+                    // create a custom configuration to be used for the dependencies of the testNative task
+                    ConfigurationContainer configurations = project.getConfigurations();
+                    configurations.maybeCreate(NATIVE_TEST_IMPLEMENTATION_CONFIGURATION_NAME)
+                            .extendsFrom(configurations.findByName(JavaPlugin.TEST_IMPLEMENTATION_CONFIGURATION_NAME));
+                    configurations.maybeCreate(NATIVE_TEST_RUNTIME_ONLY_CONFIGURATION_NAME)
+                            .extendsFrom(configurations.findByName(JavaPlugin.TEST_RUNTIME_ONLY_CONFIGURATION_NAME));
+
+                    Task testNative = tasks.create(TEST_NATIVE_TASK_NAME, QuarkusTestNative.class);
+                    testNative.dependsOn(buildNative);
+                    testNative.setShouldRunAfter(Collections.singletonList(tasks.findByName(JavaPlugin.TEST_TASK_NAME)));
+                    tasks.getByName(JavaBasePlugin.CHECK_TASK_NAME).dependsOn(testNative);
                 });
 
-        Task buildNative = tasks.create("buildNative", QuarkusNative.class);
-
-        // set up the source set for the testNative
-        JavaPluginConvention javaPlugin = project.getConvention().findPlugin(JavaPluginConvention.class);
-        if (javaPlugin != null) {
-            buildNative.dependsOn(tasks.getByName(BasePlugin.ASSEMBLE_TASK_NAME));
-
-            SourceSetContainer sourceSets = javaPlugin.getSourceSets();
-            SourceSet nativeTestSourceSet = sourceSets.create("native-test"); // this name has to be the same as the directory in which the tests reside
-            SourceSetOutput mainSourceSetOutput = sourceSets.getByName("main").getOutput();
-            SourceSetOutput testSourceSetOutput = sourceSets.getByName("test").getOutput();
-            nativeTestSourceSet.setCompileClasspath(
-                    nativeTestSourceSet.getCompileClasspath().plus(mainSourceSetOutput).plus(testSourceSetOutput));
-            nativeTestSourceSet.setRuntimeClasspath(
-                    nativeTestSourceSet.getRuntimeClasspath().plus(mainSourceSetOutput).plus(testSourceSetOutput));
-
-            // create a custom configuration to be used for the dependencies of the testNative task
-            ConfigurationContainer configurations = project.getConfigurations();
-            configurations.maybeCreate("nativeTestImplementation").extendsFrom(configurations.findByName("implementation"));
-            configurations.maybeCreate("nativeTestRuntimeOnly").extendsFrom(configurations.findByName("runtimeOnly"));
-
-            Task testNative = tasks.create("testNative", QuarkusTestNative.class).dependsOn(buildNative);
-            testNative.setShouldRunAfter(Collections.singletonList(tasks.findByName("test")));
-
-            tasks.getByName("check").dependsOn(testNative);
-        }
-
-        final QuarkusTestConfig quarkusTestConfig = tasks.create("quarkusTestConfig", QuarkusTestConfig.class);
         tasks.withType(Test.class).forEach(t -> {
             // Quarkus test configuration task which should be executed before any Quarkus test
             t.dependsOn(quarkusTestConfig);

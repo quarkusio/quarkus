c38137acd439687fd80d682409eb84b1ec1aaa79
Rostislav Svoboda <rsvoboda@redhat.com>
2019-12-05 14:48:08 +0100

    Sentry tests - getSentryHandler method

--- extensions/logging-sentry/deployment/src/test/java/io/quarkus/logging/sentry/SentryLoggerCustomTest.java
+++ extensions/logging-sentry/deployment/src/test/java/io/quarkus/logging/sentry/SentryLoggerCustomTest.java
@@ -1,22 +1,14 @@
 package io.quarkus.logging.sentry;
 
+import static io.quarkus.logging.sentry.SentryLoggerTest.getSentryHandler;
 import static io.sentry.jvmti.ResetFrameCache.resetFrameCache;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
-
-import org.jboss.logmanager.handlers.DelayedHandler;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import io.quarkus.runtime.logging.InitialConfigurator;
 import io.quarkus.test.QuarkusUnitTest;
-import io.sentry.jul.SentryHandler;
 import io.sentry.jvmti.FrameCache;
 
 public class SentryLoggerCustomTest {
@@ -27,19 +19,7 @@ public class SentryLoggerCustomTest {
 
     @Test
     public void sentryLoggerCustomTest() {
-        LogManager logManager = LogManager.getLogManager();
-        assertThat(logManager).isInstanceOf(org.jboss.logmanager.LogManager.class);
-
-        DelayedHandler delayedHandler = InitialConfigurator.DELAYED_HANDLER;
-        assertThat(Logger.getLogger("").getHandlers()).contains(delayedHandler);
-        assertThat(delayedHandler.getLevel()).isEqualTo(Level.ALL);
-
-        Handler handler = Arrays.stream(delayedHandler.getHandlers())
-                .filter(h -> (h instanceof SentryHandler))
-                .findFirst().orElse(null);
-        SentryHandler sentryHandler = (SentryHandler) handler;
-        assertThat(sentryHandler).isNotNull();
-        assertThat(sentryHandler.getLevel()).isEqualTo(org.jboss.logmanager.Level.TRACE);
+        assertThat(getSentryHandler().getLevel()).isEqualTo(org.jboss.logmanager.Level.TRACE);
         assertThat(FrameCache.shouldCacheThrowable(new IllegalStateException("Test frame"), 1)).isTrue();
     }
 

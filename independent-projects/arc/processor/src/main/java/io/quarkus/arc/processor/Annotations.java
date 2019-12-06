bdb6e686a932f8798235c2bf027a6e1b28aec3e4
Matej Novotny <manovotn@redhat.com>
2019-12-06 16:00:42 +0100

    Log a warning if Arc finds observer for @Initialized(ApplicationScope.class).

--- independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/Annotations.java
+++ independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/Annotations.java
@@ -62,4 +62,23 @@ public final class Annotations {
         return annotations;
     }
 
+    /**
+     * Iterates over all annotations on a method and its parameters, filters out all non-parameter annotations
+     * and returns a first encountered {@link AnnotationInstance} with Annotation specified as {@link DotName}.
+     * Returns {@code null} if no such annotation exists.
+     *
+     * @param method MethodInfo to be searched for annotations
+     * @param annotation Annotation we are looking for, represented as DotName
+     * @return First encountered {@link AnnotationInstance} fitting the requirements, {@code null} if none is found
+     */
+    public static AnnotationInstance getParameterAnnotation(MethodInfo method, DotName annotation) {
+        for (AnnotationInstance annotationInstance : method.annotations()) {
+            if (annotationInstance.target().kind().equals(Kind.METHOD_PARAMETER) &&
+                    annotationInstance.name().equals(annotation)) {
+                return annotationInstance;
+            }
+        }
+        return null;
+    }
+
 }

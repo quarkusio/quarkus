803613d9dc8d896418c86df400ebe3cba12b3902
Johannes <johnnyt@gmx.at>
2019-12-07 09:28:50 +0100

    Add Flyway support for multiple datasources #3449

--- extensions/flyway/runtime/src/main/java/io/quarkus/flyway/runtime/FlywayRecorder.java
+++ extensions/flyway/runtime/src/main/java/io/quarkus/flyway/runtime/FlywayRecorder.java
@@ -1,9 +1,16 @@
 package io.quarkus.flyway.runtime;
 
+import java.lang.annotation.Annotation;
+import java.util.Map.Entry;
+
+import javax.enterprise.inject.Default;
+import javax.enterprise.util.AnnotationLiteral;
+
 import org.flywaydb.core.Flyway;
 
 import io.quarkus.arc.runtime.BeanContainer;
 import io.quarkus.arc.runtime.BeanContainerListener;
+import io.quarkus.flyway.FlywayDataSource;
 import io.quarkus.runtime.annotations.Recorder;
 
 @Recorder
@@ -21,13 +28,29 @@ public class FlywayRecorder {
     }
 
     public void doStartActions(FlywayRuntimeConfig config, BeanContainer container) {
-        if (config.cleanAtStart) {
-            Flyway flyway = container.instance(Flyway.class);
-            flyway.clean();
+        if (config.defaultDataSource.cleanAtStart) {
+            clean(container, Default.Literal.INSTANCE);
         }
-        if (config.migrateAtStart) {
-            Flyway flyway = container.instance(Flyway.class);
-            flyway.migrate();
+        if (config.defaultDataSource.migrateAtStart) {
+            migrate(container, Default.Literal.INSTANCE);
         }
+        for (Entry<String, FlywayDataSourceRuntimeConfig> configPerDataSource : config.namedDataSources.entrySet()) {
+            if (configPerDataSource.getValue().cleanAtStart) {
+                clean(container, FlywayDataSource.Literal.of(configPerDataSource.getKey()));
+            }
+            if (configPerDataSource.getValue().migrateAtStart) {
+                migrate(container, FlywayDataSource.Literal.of(configPerDataSource.getKey()));
+            }
+        }
+    }
+
+    private void clean(BeanContainer container, AnnotationLiteral<? extends Annotation> qualifier) {
+        Flyway flyway = container.instance(Flyway.class, qualifier);
+        flyway.clean();
+    }
+
+    private void migrate(BeanContainer container, AnnotationLiteral<? extends Annotation> qualifier) {
+        Flyway flyway = container.instance(Flyway.class, qualifier);
+        flyway.migrate();
     }
 }
--- extensions/flyway/runtime/src/main/java/io/quarkus/flyway/runtime/FlywayRuntimeConfig.java
+++ extensions/flyway/runtime/src/main/java/io/quarkus/flyway/runtime/FlywayRuntimeConfig.java
@@ -1,8 +1,7 @@
 package io.quarkus.flyway.runtime;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.OptionalInt;
+import java.util.Collections;
+import java.util.Map;
 
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
@@ -10,76 +9,36 @@ import io.quarkus.runtime.annotations.ConfigRoot;
 
 @ConfigRoot(name = "flyway", phase = ConfigPhase.RUN_TIME)
 public final class FlywayRuntimeConfig {
-    /**
-     * The maximum number of retries when attempting to connect to the database. After each failed attempt, Flyway will wait 1
-     * second before attempting to connect again, up to the maximum number of times specified by connectRetries.
+    /*
+     * Creates a {@link FlywayMultiDatasourceRuntimeConfig} with default settings.
+     * 
+     * @return {@link FlywayMultiDatasourceRuntimeConfig}
      */
-    @ConfigItem
-    public OptionalInt connectRetries;
-    /**
-     * Comma-separated case-sensitive list of schemas managed by Flyway.
-     * The first schema in the list will be automatically set as the default one during the migration.
-     * It will also be the one containing the schema history table.
-     */
-    @ConfigItem
-    public Optional<List<String>> schemas;
-    /**
-     * The name of Flyway's schema history table.
-     * By default (single-schema mode) the schema history table is placed in the default schema for the connection provided by
-     * the datasource.
-     * When the flyway.schemas property is set (multi-schema mode), the schema history table is placed in the first schema of
-     * the list.
-     */
-    @ConfigItem
-    public Optional<String> table;
+    public static final FlywayRuntimeConfig defaultConfig() {
+        return new FlywayRuntimeConfig();
+    }
 
     /**
-     * The file name prefix for versioned SQL migrations.
-     *
-     * Versioned SQL migrations have the following file name structure: prefixVERSIONseparatorDESCRIPTIONsuffix , which using
-     * the defaults translates to V1.1__My_description.sql
+     * Gets the {@link FlywayDataSourceRuntimeConfig} for the given datasource name.<br>
+     * The name of the default datasource is an empty {@link String}.
+     * 
+     * @param dataSourceName {@link String}
+     * @return {@link FlywayDataSourceRuntimeConfig}
+     * @throws NullPointerException if dataSourceName is null.
      */
-    @ConfigItem
-    public Optional<String> sqlMigrationPrefix;
+    public FlywayDataSourceRuntimeConfig getConfigForDataSourceName(String dataSourceName) {
+        return namedDataSources.getOrDefault(dataSourceName, FlywayDataSourceRuntimeConfig.defaultConfig());
+    }
 
     /**
-     * The file name prefix for repeatable SQL migrations.
-     *
-     * Repeatable SQL migrations have the following file name structure: prefixSeparatorDESCRIPTIONsuffix , which using the
-     * defaults translates to R__My_description.sql
+     * Flyway configuration for the default datasource.
      */
-    @ConfigItem
-    public Optional<String> repeatableSqlMigrationPrefix;
+    @ConfigItem(name = ConfigItem.PARENT)
+    public FlywayDataSourceRuntimeConfig defaultDataSource = FlywayDataSourceRuntimeConfig.defaultConfig();
 
     /**
-     * true to execute Flyway clean command automatically when the application starts, false otherwise.
-     *
+     * Flyway configurations for named datasources.
      */
-    @ConfigItem
-    public boolean cleanAtStart;
-
-    /**
-     * true to execute Flyway automatically when the application starts, false otherwise.
-     *
-     */
-    @ConfigItem
-    public boolean migrateAtStart;
-
-    /**
-     * Enable the creation of the history table if it does not exist already.
-     */
-    @ConfigItem
-    public boolean baselineOnMigrate;
-
-    /**
-     * The initial baseline version.
-     */
-    @ConfigItem
-    public Optional<String> baselineVersion;
-
-    /**
-     * The description to tag an existing schema with when executing baseline.
-     */
-    @ConfigItem
-    public Optional<String> baselineDescription;
+    @ConfigItem(name = ConfigItem.PARENT)
+    public Map<String, FlywayDataSourceRuntimeConfig> namedDataSources = Collections.emptyMap();
 }

94b59e305638a20d972b8ba5c5bfc0a4f67286a5
Guillaume Smet <guillaume.smet@gmail.com>
2019-12-08 12:11:22 +0100

    Various editorializations for the health checks documentation

--- extensions/kafka-client/deployment/src/main/java/io/quarkus/kafka/client/deployment/KafkaBuildTimeConfig.java
+++ extensions/kafka-client/deployment/src/main/java/io/quarkus/kafka/client/deployment/KafkaBuildTimeConfig.java
@@ -7,7 +7,8 @@ import io.quarkus.runtime.annotations.ConfigRoot;
 @ConfigRoot(name = "kafka", phase = ConfigPhase.BUILD_TIME)
 public class KafkaBuildTimeConfig {
     /**
-     * Whether or not an health check is published in case the smallrye-health extension is present (default to false).
+     * Whether or not an health check is published in case the smallrye-health extension is present.
+     * <p>
      * If you enable the health check, you must specify the `kafka.bootstrap.servers` property.
      */
     @ConfigItem(name = "health.enabled", defaultValue = "false")

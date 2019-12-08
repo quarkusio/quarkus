c777a8fca798121e722ed46ec8028000fe785ca9
Guillaume Smet <guillaume.smet@gmail.com>
2019-12-08 12:11:22 +0100

    Various editorializations for the health checks documentation

--- extensions/neo4j/deployment/src/main/java/io/quarkus/neo4j/deployment/Neo4jBuildTimeConfig.java
+++ extensions/neo4j/deployment/src/main/java/io/quarkus/neo4j/deployment/Neo4jBuildTimeConfig.java
@@ -7,7 +7,7 @@ import io.quarkus.runtime.annotations.ConfigRoot;
 @ConfigRoot(name = "neo4j", phase = ConfigPhase.BUILD_TIME)
 public class Neo4jBuildTimeConfig {
     /**
-     * Whether or not an health check is published in case the smallrye-health extension is present (default to true).
+     * Whether or not an health check is published in case the smallrye-health extension is present.
      */
     @ConfigItem(name = "health.enabled", defaultValue = "true")
     public boolean healthEnabled;

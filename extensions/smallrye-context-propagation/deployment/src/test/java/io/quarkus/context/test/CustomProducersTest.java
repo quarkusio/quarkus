0c331d98bf97dab94a47f2ffa0ead697aae7abd9
Matej Novotny <manovotn@redhat.com>
2019-12-13 14:03:31 +0100

    Provided ManagedExecutor and ThreadContext beans should be default so that users can override them easily.

---
+++ extensions/smallrye-context-propagation/deployment/src/test/java/io/quarkus/context/test/CustomProducersTest.java
@@ -0,0 +1,41 @@
+package io.quarkus.context.test;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.context.ManagedExecutor;
+import org.eclipse.microprofile.context.ThreadContext;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.arc.ClientProxy;
+import io.quarkus.test.QuarkusUnitTest;
+
+/**
+ * Tests that user can override default beans for {@code ManagedExecutor} and {@code ThreadContext}.
+ * Default beans are singletons (no proxy) whereas the newly defined beans here are application scoped.
+ * Therefore it is enough to check that the injected values are proxied.
+ */
+public class CustomProducersTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(ProducerBean.class));
+
+    @Inject
+    ManagedExecutor me;
+
+    @Inject
+    ThreadContext tc;
+
+    @Test
+    public void testDefaultBeansCanBeOverriden() {
+        Assertions.assertNotNull(me);
+        Assertions.assertNotNull(tc);
+        Assertions.assertTrue(me instanceof ClientProxy);
+        Assertions.assertTrue(tc instanceof ClientProxy);
+    }
+}

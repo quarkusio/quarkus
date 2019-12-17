3a35468e20e0290307996c4f8a3363ae208859e1
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-12-18 09:05:59 +1100

    Add CI support for code coverage

--- coverage-report/prepare.sh
+++ coverage-report/prepare.sh
@@ -5,14 +5,24 @@ mkdir -p  src/main/java
 
 for j in '../extensions' '../core' '../devtools' '../independent-projects/'
 do
-    for i in `find $j -regex .*target/classes `
+    for i in `find $j -regex .*target/classes`
     do
         cp -r  $i/* target/classes/
     done
-    for i in `find $j -regex .*src/main/java `
+    for i in `find $j -regex .*src/main/java`
     do
         cp -r  $i/* src/main/java/
     done
 done
-#needed to make sure the script always suceeds
+
+#we don't care about classes in the 'graal' package, because they are only used in native image generation
+find target/classes/ -name graal -exec rm -r {} \;
+
+#antlr generated code
+rm -r target/classes/io/quarkus/panacheql/internal
+
+#we don't care about the document processor
+rm -r target/classes/io/quarkus/annotation/processor/generate_doc
+
+#needed to make sure the script always succeeds
 echo "complete"

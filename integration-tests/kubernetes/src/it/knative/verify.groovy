62145a5f3de3cdc4ad2ee85d8a999b01fa034182
Ioannis Canellos <iocanel@gmail.com>
2019-12-18 17:16:26 +0200

    fix: handling of arrays in kubernetes properties.

--- integration-tests/kubernetes/src/it/knative/verify.groovy
+++ integration-tests/kubernetes/src/it/knative/verify.groovy
@@ -9,9 +9,10 @@ File knativeYml = new File(base, "target/kubernetes/knative.yml")
 assert knativeYml.exists()
 
 //Check that its parse-able
-KubernetesList list = Serialization.unmarshal(knativeYml.text, KubernetesList.class)
-assert list != null
-
+knativeYml.withInputStream { stream ->
+    KubernetesList list = Serialization.unmarshalAsList(stream)
+    assert list != null
+}
 // Un-comment once: https://github.com/fabric8io/kubernetes-client/issues/1838 is addressed.
 //Check that ti contains a DeploymentConfig named after the project
 //Service service = list.items.find{r -> r.kind == "Service"}
--- integration-tests/kubernetes/src/it/kubernetes-with-client/verify.groovy
+++ integration-tests/kubernetes/src/it/kubernetes-with-client/verify.groovy
@@ -8,20 +8,21 @@ import io.dekorate.deps.kubernetes.api.model.rbac.RoleBinding;
 String base = basedir
 File kubernetesYml = new File(base, "target/kubernetes/kubernetes.yml")
 assert kubernetesYml.exists()
+kubernetesYml.withInputStream { stream -> 
+    //Check that its parse-able
+    KubernetesList list = Serialization.unmarshalAsList(stream)
+    assert list != null
+    Deployment deployment = list.items.find{r -> r.kind == "Deployment"}
 
-//Check that its parse-able
-KubernetesList list = Serialization.unmarshal(kubernetesYml.text, KubernetesList.class)
-assert list != null
-Deployment deployment = list.items.find{r -> r.kind == "Deployment"}
+    //Check that ti contains a Deployment named after the project
+    assert deployment != null
+    assert deployment.metadata.name == "kubernetes-with-client"
 
-//Check that ti contains a Deployment named after the project
-assert deployment != null
-assert deployment.metadata.name == "kubernetes-with-client"
+    ServiceAccount serviceAccount = list.items.find{r -> r.kind == "ServiceAccount"}
+    assert serviceAccount != null
+    assert serviceAccount.metadata.name == "kubernetes-with-client"
 
-ServiceAccount serviceAccount = list.items.find{r -> r.kind == "ServiceAccount"}
-assert serviceAccount != null
-assert serviceAccount.metadata.name == "kubernetes-with-client"
-
-RoleBinding rolebinding = list.items.find{r -> r.kind == "RoleBinding"}
-assert rolebinding != null
-assert rolebinding.metadata.name == "kubernetes-with-client:view"
+    RoleBinding rolebinding = list.items.find{r -> r.kind == "RoleBinding"}
+    assert rolebinding != null
+    assert rolebinding.metadata.name == "kubernetes-with-client:view"
+}

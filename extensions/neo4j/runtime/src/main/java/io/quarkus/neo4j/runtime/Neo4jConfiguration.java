67f6073a90306051fd69ecf738e4f31e81d4ed20
Michael Simons <michael@simons.ac>
2019-12-19 12:18:43 +0100

    Make Neo4j transport encryption configurable.
    This adds the encrypted flag to the top level configuration and a new group that makes it possible to change the general trust settings. All settings have the same default as the underlying driver.
    
    Polishing.

--- extensions/neo4j/runtime/src/main/java/io/quarkus/neo4j/runtime/Neo4jConfiguration.java
+++ extensions/neo4j/runtime/src/main/java/io/quarkus/neo4j/runtime/Neo4jConfiguration.java
@@ -1,6 +1,11 @@
 package io.quarkus.neo4j.runtime;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.time.Duration;
+import java.util.Optional;
+
+import org.neo4j.driver.Config;
 
 import io.quarkus.runtime.annotations.ConfigDocSection;
 import io.quarkus.runtime.annotations.ConfigGroup;
@@ -29,6 +34,19 @@ public class Neo4jConfiguration {
     public Authentication authentication;
 
     /**
+     * If the driver should use encrypted traffic.
+     */
+    @ConfigItem
+    public boolean encrypted;
+
+    /**
+     * Configure trust settings for encrypted traffic.
+     */
+    @ConfigItem
+    @ConfigDocSection
+    public TrustSettings trustSettings;
+
+    /**
      * Connection pool.
      */
     @ConfigItem
@@ -54,7 +72,67 @@ public class Neo4jConfiguration {
          * Set this to true to disable authentication.
          */
         @ConfigItem
-        public boolean disabled = false;
+        public boolean disabled;
+    }
+
+    @ConfigGroup
+    static class TrustSettings {
+
+        public enum Strategy {
+
+            TRUST_ALL_CERTIFICATES,
+
+            TRUST_CUSTOM_CA_SIGNED_CERTIFICATES,
+
+            TRUST_SYSTEM_CA_SIGNED_CERTIFICATES
+        }
+
+        /**
+         * Configures which trust strategy to apply when using encrypted traffic.
+         */
+        @ConfigItem(defaultValue = "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES")
+        public Strategy strategy;
+
+        /**
+         * The file of the certificate to use.
+         */
+        @ConfigItem
+        public Optional<Path> certFile = Optional.empty();
+
+        /**
+         * If hostname verification is used.
+         */
+        @ConfigItem
+        public boolean hostnameVerificationEnabled;
+
+        Config.TrustStrategy toInternalRepresentation() {
+
+            Config.TrustStrategy internalRepresentation;
+            Strategy nonNullStrategy = strategy == null ? Strategy.TRUST_SYSTEM_CA_SIGNED_CERTIFICATES : strategy;
+            switch (nonNullStrategy) {
+                case TRUST_ALL_CERTIFICATES:
+                    internalRepresentation = Config.TrustStrategy.trustAllCertificates();
+                    break;
+                case TRUST_SYSTEM_CA_SIGNED_CERTIFICATES:
+                    internalRepresentation = Config.TrustStrategy.trustSystemCertificates();
+                    break;
+                case TRUST_CUSTOM_CA_SIGNED_CERTIFICATES:
+
+                    File certFile = this.certFile.map(Path::toFile).filter(File::isFile)
+                            .orElseThrow(() -> new RuntimeException("Configured trust strategy requires a certificate file."));
+                    internalRepresentation = Config.TrustStrategy.trustCustomCertificateSignedBy(certFile);
+                    break;
+                default:
+                    throw new RuntimeException("Unknown trust strategy: " + this.strategy.name());
+            }
+
+            if (hostnameVerificationEnabled) {
+                internalRepresentation.withHostnameVerification();
+            } else {
+                internalRepresentation.withoutHostnameVerification();
+            }
+            return internalRepresentation;
+        }
     }
 
     @ConfigGroup

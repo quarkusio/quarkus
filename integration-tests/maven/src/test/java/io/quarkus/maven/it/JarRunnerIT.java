51550b5d27a545ef1956f3a5ca6b7ff43d2e571d
Georgios Andrianakis <geoand@gmail.com>
2019-12-20 10:34:06 +0200

    Add tests for application name and version handling

--- integration-tests/maven/src/test/java/io/quarkus/maven/it/JarRunnerIT.java
+++ integration-tests/maven/src/test/java/io/quarkus/maven/it/JarRunnerIT.java
@@ -1,5 +1,6 @@
 package io.quarkus.maven.it;
 
+import static io.quarkus.maven.it.ApplicationNameAndVersionTestUtil.assertApplicationPropertiesSetCorrectly;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
@@ -55,6 +56,9 @@ public class JarRunnerIT extends MojoTestBase {
             String logs = FileUtils.readFileToString(output, "UTF-8");
 
             assertThatOutputWorksCorrectly(logs);
+
+            // test that the application name and version are properly set
+            assertApplicationPropertiesSetCorrectly();
         } finally {
             process.destroy();
         }

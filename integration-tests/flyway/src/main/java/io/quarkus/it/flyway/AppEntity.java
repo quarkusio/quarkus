672b44397a914ef26cbd9dbc18df815a9409375a
Jaikiran Pai <jaikiran.pai@gmail.com>
2019-12-20 09:45:36 +0530

    issue-5942 Introduce a deterministic ordering, between extensions, for database schema updates

---
+++ integration-tests/flyway/src/main/java/io/quarkus/it/flyway/AppEntity.java
@@ -0,0 +1,51 @@
+package io.quarkus.it.flyway;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity used within tests
+ */
+@Entity
+@Table(name = "quarkus_table2", schema = "TEST_SCHEMA")
+public class AppEntity {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        final AppEntity appEntity = (AppEntity) o;
+        return id == appEntity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

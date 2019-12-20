1625b4b062bc8c32b2506a3a4c2bd74475ed6fcc
Loïc Mathieu <loikeseke@gmail.com>
2019-12-20 14:55:04 +0100

    Implements @ProjectionFor

--- extensions/panache/mongodb-panache/deployment/src/main/java/io/quarkus/mongodb/panache/deployment/PanacheResourceProcessor.java
+++ extensions/panache/mongodb-panache/deployment/src/main/java/io/quarkus/mongodb/panache/deployment/PanacheResourceProcessor.java
@@ -1,13 +1,19 @@
 package io.quarkus.mongodb.panache.deployment;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import org.bson.codecs.pojo.annotations.BsonProperty;
 import org.bson.types.ObjectId;
+import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.CompositeIndex;
 import org.jboss.jandex.DotName;
+import org.jboss.jandex.FieldInfo;
 import org.jboss.jandex.Indexer;
+import org.jboss.jandex.MethodInfo;
 import org.jboss.jandex.Type;
 
 import io.quarkus.deployment.Capabilities;
@@ -27,6 +33,7 @@ import io.quarkus.mongodb.panache.PanacheMongoEntity;
 import io.quarkus.mongodb.panache.PanacheMongoEntityBase;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import io.quarkus.mongodb.panache.PanacheMongoRepositoryBase;
+import io.quarkus.mongodb.panache.ProjectionFor;
 import io.quarkus.panache.common.deployment.PanacheFieldAccessEnhancer;
 import io.quarkus.panache.common.deployment.PanacheRepositoryEnhancer;
 
@@ -36,7 +43,12 @@ public class PanacheResourceProcessor {
     static final DotName DOTNAME_PANACHE_ENTITY_BASE = DotName.createSimple(PanacheMongoEntityBase.class.getName());
     private static final DotName DOTNAME_PANACHE_ENTITY = DotName.createSimple(PanacheMongoEntity.class.getName());
 
+    private static final DotName DOTNAME_PROJECTION_FOR = DotName.createSimple(ProjectionFor.class.getName());
+    private static final DotName DOTNAME_BSON_PROPERTY = DotName.createSimple(BsonProperty.class.getName());
+
     private static final DotName DOTNAME_OBJECT_ID = DotName.createSimple(ObjectId.class.getName());
+
+    private static final DotName DOTNAME_OBJECT = DotName.createSimple(Object.class.getName());
 
     @BuildStep
     CapabilityBuildItem capability() {
@@ -129,6 +141,48 @@ public class PanacheResourceProcessor {
                     transformers.produce(new BytecodeTransformerBuildItem(className, panacheFieldAccessEnhancer));
                 }
             }
+        }
+
+        // manage @BsonProperty for the @ProjectionFor annotation
+        Map<ClassInfo, Map<String, String>> propertyMapping = new HashMap<>();
+        for (AnnotationInstance annotationInstance : index.getIndex().getAnnotations(DOTNAME_PROJECTION_FOR)) {
+            Type targetClass = annotationInstance.value().asClass();
+            ClassInfo target = index.getIndex().getClassByName(targetClass.name());
+            Map<String, String> classPropertyMapping = new HashMap<>();
+            extractMappings(classPropertyMapping, target, index);
+            propertyMapping.put(target, classPropertyMapping);
+        }
+        for (AnnotationInstance annotationInstance : index.getIndex().getAnnotations(DOTNAME_PROJECTION_FOR)) {
+            Type targetClass = annotationInstance.value().asClass();
+            ClassInfo target = index.getIndex().getClassByName(targetClass.name());
+            Map<String, String> targetPropertyMapping = propertyMapping.get(target);
+            if (targetPropertyMapping != null && !targetPropertyMapping.isEmpty()) {
+                ClassInfo info = annotationInstance.target().asClass();
+                ProjectionForEnhancer fieldEnhancer = new ProjectionForEnhancer(targetPropertyMapping);
+                transformers.produce(new BytecodeTransformerBuildItem(info.name().toString(), fieldEnhancer));
+            }
+        }
+    }
+
+    private void extractMappings(Map<String, String> classPropertyMapping, ClassInfo target, CombinedIndexBuildItem index) {
+        for (FieldInfo fieldInfo : target.fields()) {
+            if (fieldInfo.hasAnnotation(DOTNAME_BSON_PROPERTY)) {
+                AnnotationInstance bsonProperty = fieldInfo.annotation(DOTNAME_BSON_PROPERTY);
+                classPropertyMapping.put(fieldInfo.name(), bsonProperty.value().asString());
+            }
+        }
+        for (MethodInfo methodInfo : target.methods()) {
+            if (methodInfo.hasAnnotation(DOTNAME_BSON_PROPERTY)) {
+                AnnotationInstance bsonProperty = methodInfo.annotation(DOTNAME_BSON_PROPERTY);
+                classPropertyMapping.put(methodInfo.name(), bsonProperty.value().asString());
+            }
+        }
+
+        // climb up the hierarchy of types
+        if (!target.superClassType().name().equals(DOTNAME_OBJECT)) {
+            Type superType = target.superClassType();
+            ClassInfo superClass = index.getIndex().getClassByName(superType.name());
+            extractMappings(classPropertyMapping, superClass, index);
         }
     }
 }

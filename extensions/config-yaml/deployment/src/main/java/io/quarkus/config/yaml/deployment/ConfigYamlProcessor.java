a1e3e38643d41e843f88100c03030716f52516d7
Bruno Devaux <devaux.br@gmail.com>
2020-01-02 23:44:48 +0100

    enable hot reload on application.yaml

--- extensions/config-yaml/deployment/src/main/java/io/quarkus/config/yaml/deployment/ConfigYamlProcessor.java
+++ extensions/config-yaml/deployment/src/main/java/io/quarkus/config/yaml/deployment/ConfigYamlProcessor.java
@@ -1,12 +1,19 @@
 package io.quarkus.config.yaml.deployment;
 
+import io.quarkus.config.yaml.runtime.ApplicationYamlProvider;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.HotDeploymentWatchedFileBuildItem;
 
 public final class ConfigYamlProcessor {
 
     @BuildStep
     public FeatureBuildItem feature() {
         return new FeatureBuildItem(FeatureBuildItem.CONFIG_YAML);
+    }
+
+    @BuildStep
+    HotDeploymentWatchedFileBuildItem watchYamlConfig() {
+        return new HotDeploymentWatchedFileBuildItem(ApplicationYamlProvider.APPLICATION_YAML);
     }
 }

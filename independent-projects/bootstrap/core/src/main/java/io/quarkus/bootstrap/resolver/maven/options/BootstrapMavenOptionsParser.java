80f8c8b05343d6b4c4e2cc8411fdb1f0fb16d137
Georgios Andrianakis <geoand@gmail.com>
2020-01-02 18:49:06 +0200

    Properly initialize Maven profile options during bootstrap
    Fixes: #6383

--- independent-projects/bootstrap/core/src/main/java/io/quarkus/bootstrap/resolver/maven/options/BootstrapMavenOptionsParser.java
+++ independent-projects/bootstrap/core/src/main/java/io/quarkus/bootstrap/resolver/maven/options/BootstrapMavenOptionsParser.java
@@ -20,7 +20,7 @@ public class BootstrapMavenOptionsParser {
         final Map<String, Object> map = new HashMap<>();
         put(cmdLine, map, CLIManager.ALTERNATE_USER_SETTINGS);
         put(cmdLine, map, CLIManager.ALTERNATE_GLOBAL_SETTINGS);
-        put(cmdLine, map, CLIManager.ACTIVATE_PROFILES);
+        put(map, String.valueOf(CLIManager.ACTIVATE_PROFILES), cmdLine.getOptionValues(CLIManager.ACTIVATE_PROFILES));
 
         putBoolean(cmdLine, map, CLIManager.SUPRESS_SNAPSHOT_UPDATES);
         putBoolean(cmdLine, map, CLIManager.UPDATE_SNAPSHOTS);

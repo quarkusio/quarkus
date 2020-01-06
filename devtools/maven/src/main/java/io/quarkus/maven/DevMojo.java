f9be4d5172093e5ab94c5619ff3b212415a74350
Guillaume Smet <guillaume.smet@gmail.com>
2020-01-06 21:46:03 +0100

    Check that there is a pluginOptions child before iterating on it
    Fixes #6428

--- devtools/maven/src/main/java/io/quarkus/maven/DevMojo.java
+++ devtools/maven/src/main/java/io/quarkus/maven/DevMojo.java
@@ -684,8 +684,10 @@ public class DevMojo extends AbstractMojo {
             Xpp3Dom compilerPluginConfiguration = (Xpp3Dom) kotlinMavenPlugin.getConfiguration();
             if (compilerPluginConfiguration != null) {
                 Xpp3Dom compilerPluginArgsConfiguration = compilerPluginConfiguration.getChild("pluginOptions");
-                for (Xpp3Dom argConfiguration : compilerPluginArgsConfiguration.getChildren()) {
-                    options.add(argConfiguration.getValue());
+                if (compilerPluginArgsConfiguration != null) {
+                    for (Xpp3Dom argConfiguration : compilerPluginArgsConfiguration.getChildren()) {
+                        options.add(argConfiguration.getValue());
+                    }
                 }
             }
             devModeContext.setCompilerPluginsOptions(options);

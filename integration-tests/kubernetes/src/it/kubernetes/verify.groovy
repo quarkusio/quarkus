ac2c3f689a76040bf3d9679a85911dcbc75fd51c
Georgios Andrianakis <geoand@gmail.com>
2020-01-06 12:05:37 +0200

    Fix Kubernetes manifest port handling
    This is needed because of the config changes that went in 1.1.
    One thing that needed to be removed because of those changes was
    the warning we used to produce about potential mismatch between
    the port in the Kubernetes manifests and the port set at runtime
    
    Fixes: #6408

--- integration-tests/kubernetes/src/it/kubernetes/verify.groovy
+++ integration-tests/kubernetes/src/it/kubernetes/verify.groovy
@@ -1,6 +1,7 @@
 import io.dekorate.utils.Serialization
 import io.dekorate.deps.kubernetes.api.model.KubernetesList
 import io.dekorate.deps.kubernetes.api.model.apps.Deployment;
+import io.dekorate.deps.kubernetes.api.model.Service;
 
 //Check that file exits
 String base = basedir
@@ -11,7 +12,20 @@ assert kubernetesYml.exists()
 KubernetesList list = Serialization.unmarshal(kubernetesYml.text, KubernetesList.class)
 assert list != null
 Deployment deployment = list.items.find{r -> r.kind == "Deployment"}
+Service service = list.items.find{r -> r.kind == "Service"}
 
-//Check that ti contains a Deployment named after the project
+//Check that it contains a Deployment named after the project
 assert deployment != null
 assert deployment.metadata.name == "kubernetes"
+
+//Check the port of the Deployment
+assert deployment.spec.template.spec.containers[0].ports[0].containerPort == 8080
+
+//Check that it contains a Service named after the project
+assert service != null
+assert service.metadata.name == "kubernetes"
+
+//Check the port of the Service
+assert service.spec.ports[0].port == 8080
+
+

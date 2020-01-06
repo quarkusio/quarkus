e1482f9c077761d434e46b7667d9f00b74129347
dbaert <dennis.baerten@nike.com>
2020-01-06 10:10:34 +0100

    Add possibility to override the logging configuration of each handler on category level  A

---
+++ core/test-extension/deployment/src/test/java/io/quarkus/logging/CategoryConfiguredHandlerTest.java
@@ -0,0 +1,61 @@
+package io.quarkus.logging;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.logging.*;
+
+import org.jboss.logmanager.formatters.PatternFormatter;
+import org.jboss.logmanager.handlers.ConsoleHandler;
+import org.jboss.logmanager.handlers.DelayedHandler;
+import org.jboss.logmanager.handlers.FileHandler;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.runtime.logging.InitialConfigurator;
+import io.quarkus.test.QuarkusUnitTest;
+
+public class CategoryConfiguredHandlerTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withConfigurationResource("application-category-configured-handlers-output.properties")
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addAsManifestResource("application.properties", "microprofile-config.properties"));
+
+    @Test
+    public void consoleOutputTest() {
+        LogManager logManager = LogManager.getLogManager();
+        assertThat(logManager).isInstanceOf(org.jboss.logmanager.LogManager.class);
+
+        DelayedHandler delayedHandler = InitialConfigurator.DELAYED_HANDLER;
+        assertThat(Logger.getLogger("").getHandlers()).contains(delayedHandler);
+
+        Handler handler = Arrays.stream(delayedHandler.getHandlers()).filter(h -> (h instanceof ConsoleHandler))
+                .findFirst().get();
+        assertThat(handler).isNotNull();
+        assertThat(handler.getLevel()).isEqualTo(Level.WARNING);
+
+        Logger categoryLogger = logManager.getLogger("io.quarkus.category");
+        assertThat(categoryLogger).isNotNull();
+        assertThat(categoryLogger.getHandlers()).hasSize(2).extracting("class").containsExactlyInAnyOrder(ConsoleHandler.class,
+                FileHandler.class);
+
+        Logger otherCategoryLogger = logManager.getLogger("io.quarkus.othercategory");
+        assertThat(otherCategoryLogger).isNotNull();
+        assertThat(otherCategoryLogger.getHandlers()).hasSize(1).extracting("class")
+                .containsExactlyInAnyOrder(ConsoleHandler.class);
+
+        Logger anotherCategoryLogger = logManager.getLogger("io.quarkus.anothercategory");
+        assertThat(anotherCategoryLogger).isNotNull();
+        assertThat(anotherCategoryLogger.getHandlers()).isEmpty();
+
+        Formatter formatter = handler.getFormatter();
+        assertThat(formatter).isInstanceOf(PatternFormatter.class);
+        PatternFormatter patternFormatter = (PatternFormatter) formatter;
+        assertThat(patternFormatter.getPattern()).isEqualTo("%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c{3.}] (%t) %s%e%n");
+    }
+
+}

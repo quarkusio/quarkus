0503581c390e65e46f52533f09e83f520a2a2bda
Andrej Petras <andrej.petras@capgemini.com>
2020-01-17 09:35:18 +0100

    Add a Liquibase extension

---
+++ extensions/liquibase/deployment/src/test/java/io/quarkus/liquibase/test/LiquibaseExtensionConfigNamedDataSourceWithoutLiquibaseTest.java
@@ -0,0 +1,49 @@
+package io.quarkus.liquibase.test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import javax.inject.Inject;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.agroal.api.AgroalDataSource;
+import io.quarkus.agroal.DataSource;
+import io.quarkus.liquibase.LiquibaseDataSource;
+import io.quarkus.liquibase.LiquibaseFactory;
+import io.quarkus.test.QuarkusUnitTest;
+
+/**
+ * Assures, that liquibase can also be used without any configuration,
+ * provided, that at least a named datasource is configured.
+ */
+public class LiquibaseExtensionConfigNamedDataSourceWithoutLiquibaseTest {
+
+    @Inject
+    @LiquibaseDataSource("users")
+    LiquibaseFactory liquibaseFactory;
+
+    @Inject
+    LiquibaseExtensionConfigFixture fixture;
+
+    @Inject
+    @DataSource("users")
+    AgroalDataSource usersDataSource;
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClass(LiquibaseExtensionConfigFixture.class)
+                    .addAsResource("db/changeLog.xml", "db/changeLog.xml")
+                    .addAsResource("config-for-named-datasource-without-liquibase.properties", "application.properties"));
+
+    @Test
+    @DisplayName("Reads predefined default liquibase configuration for named datasource correctly")
+    public void testLiquibaseDefaultConfigInjection() throws Exception {
+        fixture.assertDefaultConfigurationSettings(liquibaseFactory.getConfiguration());
+        assertFalse(fixture.migrateAtStart("users"));
+    }
+}
---
+++ extensions/liquibase/deployment/src/test/java/io/quarkus/liquibase/test/LiquibaseExtensionMigrateAtStartSqlChangeLogTest.java
@@ -0,0 +1,45 @@
+package io.quarkus.liquibase.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.liquibase.LiquibaseFactory;
+import io.quarkus.test.QuarkusUnitTest;
+import liquibase.Liquibase;
+import liquibase.changelog.ChangeSetStatus;
+
+public class LiquibaseExtensionMigrateAtStartSqlChangeLogTest {
+    // Quarkus built object
+    @Inject
+    LiquibaseFactory liquibaseFactory;
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addAsResource("db/sql/changeLog.sql")
+                    .addAsResource("migrate-at-start-sql-config.properties", "application.properties"));
+
+    @Test
+    @DisplayName("Migrates at start with change log config correctly")
+    public void testLiquibaseSqlChangeLog() throws Exception {
+        try (Liquibase liquibase = liquibaseFactory.createLiquibase()) {
+            List<ChangeSetStatus> status = liquibase.getChangeSetStatuses(liquibaseFactory.createContexts(),
+                    liquibaseFactory.createLabels());
+            assertNotNull(status);
+            assertEquals(2, status.size());
+            assertFalse(status.get(0).getWillRun());
+            assertEquals(status.get(0).getChangeSet().getId(), "create-tables-1");
+            assertFalse(status.get(1).getWillRun());
+            assertEquals(status.get(1).getChangeSet().getId(), "test-1");
+        }
+    }
+}

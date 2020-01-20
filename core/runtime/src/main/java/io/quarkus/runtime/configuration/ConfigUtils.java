262fa3a684286cfc9bdfc78f005516fdc667ef9a
David M. Lloyd <david.lloyd@redhat.com>
2020-01-20 17:08:07 -0600

    Fix a problem where env vars do not have the right ordinal

--- core/runtime/src/main/java/io/quarkus/runtime/configuration/ConfigUtils.java
+++ core/runtime/src/main/java/io/quarkus/runtime/configuration/ConfigUtils.java
@@ -63,7 +63,7 @@ public final class ConfigUtils {
             sources.addAll(
                     new PropertiesConfigSourceProvider("WEB-INF/classes/META-INF/microprofile-config.properties", true,
                             classLoader).getConfigSources(classLoader));
-            sources.add(new EnvConfigSource());
+            sources.add(new EnvConfigSource(300));
             sources.add(new SysPropConfigSource());
             builder.withSources(sources.toArray(new ConfigSource[0]));
         }
@@ -87,6 +87,16 @@ public final class ConfigUtils {
 
     static final class EnvConfigSource implements ConfigSource {
         static final Pattern REP_PATTERN = Pattern.compile("[^a-zA-Z0-9_]");
+
+        private final int ordinal;
+
+        EnvConfigSource(final int ordinal) {
+            this.ordinal = ordinal;
+        }
+
+        public int getOrdinal() {
+            return ordinal;
+        }
 
         public Map<String, String> getProperties() {
             return Collections.emptyMap();

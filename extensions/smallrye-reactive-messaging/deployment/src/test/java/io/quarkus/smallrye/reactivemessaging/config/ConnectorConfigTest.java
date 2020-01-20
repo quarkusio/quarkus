0fdf3d396c1cc25bca2b2e44cda6dd76b161a7cb
Clement Escoffier <clement.escoffier@gmail.com>
2020-01-20 16:47:48 +0100

    Implement various tests for the Reactive Messaging provider
    * Check that all valid signatures are supported
    * Verify that profiles are supported
    * Check config and code live reload (including recovery after an issue)

---
+++ extensions/smallrye-reactive-messaging/deployment/src/test/java/io/quarkus/smallrye/reactivemessaging/config/ConnectorConfigTest.java
@@ -0,0 +1,53 @@
+package io.quarkus.smallrye.reactivemessaging.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class ConnectorConfigTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(DumbConnector.class, BeanUsingDummyConnector.class))
+            .overrideConfigKey("mp.messaging.incoming.a.values", "bonjour")
+            .overrideConfigKey("mp.messaging.incoming.a.connector", "dummy");
+
+    @Inject
+    BeanUsingDummyConnector bean;
+
+    @Test
+    public void test() {
+        await().until(() -> bean.getList().size() == 2);
+        assertThat(bean.getList()).containsExactly("bonjour", "BONJOUR");
+    }
+
+    @ApplicationScoped
+    public static class BeanUsingDummyConnector {
+
+        private List<String> list = new CopyOnWriteArrayList<>();
+
+        @Incoming("a")
+        public void consume(String s) {
+            list.add(s);
+        }
+
+        public List<String> getList() {
+            return list;
+        }
+
+    }
+}

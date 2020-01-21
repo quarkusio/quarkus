b2cc607087558e94baae0abe2e7e5479fe20d3e4
Georgios Andrianakis <geoand@gmail.com>
2020-01-21 09:25:56 +0200

    Add support for .yml in addition to .yaml
    Fixes: #6668

--- extensions/config-yaml/deployment/src/main/java/io/quarkus/config/yaml/deployment/ConfigYamlProcessor.java
+++ extensions/config-yaml/deployment/src/main/java/io/quarkus/config/yaml/deployment/ConfigYamlProcessor.java
@@ -1,6 +1,7 @@
 package io.quarkus.config.yaml.deployment;
 
 import io.quarkus.config.yaml.runtime.ApplicationYamlProvider;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.HotDeploymentWatchedFileBuildItem;
@@ -13,7 +14,8 @@ public final class ConfigYamlProcessor {
     }
 
     @BuildStep
-    HotDeploymentWatchedFileBuildItem watchYamlConfig() {
-        return new HotDeploymentWatchedFileBuildItem(ApplicationYamlProvider.APPLICATION_YAML);
+    void watchYamlConfig(BuildProducer<HotDeploymentWatchedFileBuildItem> items) {
+        items.produce(new HotDeploymentWatchedFileBuildItem(ApplicationYamlProvider.APPLICATION_YAML));
+        items.produce(new HotDeploymentWatchedFileBuildItem(ApplicationYamlProvider.APPLICATION_YML));
     }
 }

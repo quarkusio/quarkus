d881a31a928aeef236d858ab73cd3bcbb8c02478
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-01-29 10:29:34 +1100

    Make sure TCCL is correct for outer test class
    We still need to create an instance of the original test,
    make sure the TCCL is correct to avoid problems

---
+++ integration-tests/jsonb/src/test/java/io/quarkus/it/jsonb/JsonInStaticBlockTestCase.java
@@ -0,0 +1,19 @@
+package io.quarkus.it.jsonb;
+
+import javax.json.bind.spi.JsonbProvider;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class JsonInStaticBlockTestCase {
+    static {
+        JsonbProvider.provider().create();
+    }
+
+    @Test
+    void get() {
+
+    }
+}

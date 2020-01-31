563221cafc5c61968e7ed9a7be10d7fcf0326781
Rostislav Svoboda <rsvoboda@redhat.com>
2020-01-31 10:07:10 +0100

    Move comment text to @Disabled annotation text

--- integration-tests/main/src/test/java/io/quarkus/it/main/RestClientTestCase.java
+++ integration-tests/main/src/test/java/io/quarkus/it/main/RestClientTestCase.java
@@ -118,13 +118,8 @@ public class RestClientTestCase {
         Assertions.assertEquals("javax.enterprise.context.Dependent", responseWithDefaultScope);
     }
 
-    /**
-     * Disabled by default as it establishes external connections.
-     * <p>
-     * Uncomment when you want to test SSL support.
-     */
     @Test
-    @Disabled
+    @Disabled("Disabled by default as it establishes external connections, uncomment when you want to test SSL support")
     public void testDegradedSslSupport() {
         RestAssured.when().get("/ssl").then()
                 .statusCode(500)

f04e1fcb31082e5b7cdc880f068e267b985ca487
Martin Kouba <mkouba@redhat.com>
2020-02-04 12:04:58 +0100

    QuteProcessor - fix problem in param declaration validation
    - resolves #6957

---
+++ extensions/qute/deployment/src/test/java/io/quarkus/qute/deployment/ParamDeclarationTest.java
@@ -0,0 +1,36 @@
+package io.quarkus.qute.deployment;
+
+import javax.inject.Inject;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.qute.Template;
+import io.quarkus.test.QuarkusUnitTest;
+
+public class ParamDeclarationTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClass(Movie.class)
+                    .addAsResource(new StringAsset("{@io.quarkus.qute.deployment.Movie movie}"
+                            + "{movie.mainCharacters.size}: {#for character in movie.mainCharacters}"
+                            + "{character}"
+                            + "{#if hasNext}, {/}"
+                            + "{/}"), "templates/movie.html"));
+
+    @Inject
+    Template movie;
+
+    @Test
+    public void testValidationSuccess() {
+        Assertions.assertEquals("2: Michael Caine, John Cleese",
+                movie.data("movie", new Movie("Michael Caine", "John Cleese")).render());
+    }
+
+}

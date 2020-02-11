fe27eb72173d89d035f7831cc9f59f16647ed86b
Georgios Andrianakis <geoand@gmail.com>
2020-02-11 16:29:42 +0200

    Ensure that user provided ObjectMapperCustomizer objects always have higher priority
    Fixes: #7116

--- extensions/jackson/runtime/src/main/java/io/quarkus/jackson/ObjectMapperCustomizer.java
+++ extensions/jackson/runtime/src/main/java/io/quarkus/jackson/ObjectMapperCustomizer.java
@@ -10,7 +10,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * <p>
  * See also {@link ObjectMapperProducer#objectMapper}.
  */
-public interface ObjectMapperCustomizer {
+public interface ObjectMapperCustomizer extends Comparable<ObjectMapperCustomizer> {
+
+    int MINIMUM_PRIORITY = Integer.MIN_VALUE;
+    int DEFAULT_PRIORITY = 0;
 
     void customize(ObjectMapper objectMapper);
+
+    /**
+     * Defines the priority that the customizers are applied.
+     * A lower integer value means that the customizer will be applied after a customizer with a higher priority
+     */
+    default int priority() {
+        return DEFAULT_PRIORITY;
+    }
+
+    default int compareTo(ObjectMapperCustomizer o) {
+        return Integer.compare(o.priority(), priority());
+    }
 }
---
+++ extensions/resteasy-jackson/deployment/src/test/java/io/quarkus/resteasy/jackson/TimeCustomizer.java
@@ -0,0 +1,27 @@
+package io.quarkus.resteasy.jackson;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatterBuilder;
+
+import javax.inject.Singleton;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
+
+import io.quarkus.jackson.ObjectMapperCustomizer;
+
+@Singleton
+public class TimeCustomizer implements ObjectMapperCustomizer {
+
+    @Override
+    public void customize(ObjectMapper objectMapper) {
+        JavaTimeModule customDateModule = new JavaTimeModule();
+        customDateModule.addSerializer(ZonedDateTime.class, new ZonedDateTimeSerializer(
+                new DateTimeFormatterBuilder().appendInstant(0).toFormatter().withZone(ZoneId.of("Z"))));
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .registerModule(customDateModule);
+    }
+}

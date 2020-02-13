859cc28a7d30bffb4b5377ce90cec97b29efc63e
Clement Escoffier <clement.escoffier@gmail.com>
2020-02-13 12:30:56 +0100

    Add support for Mutiny Verticle
    Mutiny Verticle is a variant of Verticle using the Mutiny API.

---
+++ integration-tests/vertx/src/main/java/io/quarkus/it/vertx/verticles/MutinyAsyncVerticle.java
@@ -0,0 +1,16 @@
+package io.quarkus.it.vertx.verticles;
+
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+
+public class MutinyAsyncVerticle extends AbstractVerticle {
+
+    @Override
+    public Uni<Void> asyncStart() {
+        String address = config().getString("id");
+        return vertx.eventBus().consumer(address)
+                .handler(message -> message.replyAndForget("OK-" + address))
+                .completionHandler();
+    }
+
+}

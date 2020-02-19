ecb612ff6fefe07d4c254a03c7d893bae78920c0
Gwenneg Lepage <gwenneg@gmail.com>
2020-02-19 21:36:04 +0100

    Support GraalVM 20.0.0

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/NativeConfig.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/NativeConfig.java
@@ -133,7 +133,7 @@ public class NativeConfig {
     /**
      * The docker image to use to do the image build
      */
-    @ConfigItem(defaultValue = "quay.io/quarkus/ubi-quarkus-native-image:19.3.1-java8")
+    @ConfigItem(defaultValue = "quay.io/quarkus/ubi-quarkus-native-image:20.0.0-java8")
     public String builderImage;
 
     /**

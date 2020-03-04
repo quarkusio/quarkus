df70aedd4ca74d92ff4cad0def3b575009d90c65
Georgios Andrianakis <geoand@gmail.com>
2020-03-04 14:19:32 +0200

    Display the banner subtitle only when the default banner is not being used

--- core/deployment/src/main/java/io/quarkus/deployment/steps/BannerProcessor.java
+++ core/deployment/src/main/java/io/quarkus/deployment/steps/BannerProcessor.java
@@ -2,9 +2,19 @@ package io.quarkus.deployment.steps;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UncheckedIOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import org.jboss.logging.Logger;
@@ -39,9 +49,14 @@ public class BannerProcessor {
     }
 
     private String readBannerFile(BannerConfig config) {
-        URL resource = Thread.currentThread().getContextClassLoader().getResource(config.path);
-        if (resource != null) {
-            try (InputStream is = resource.openStream()) {
+        try {
+            Map.Entry<URL, Boolean> entry = getBanner(config);
+            URL bannerResourceURL = entry.getKey();
+            if (bannerResourceURL == null) {
+                logger.warn("Could not locate banner file");
+                return "";
+            }
+            try (InputStream is = bannerResourceURL.openStream()) {
                 byte[] content = FileUtil.readFileContents(is);
                 String bannerTitle = new String(content, StandardCharsets.UTF_8);
 
@@ -52,17 +67,56 @@ public class BannerProcessor {
                 }
 
                 String tagline = "\n";
-                if (!config.isDefaultPath()) {
+                Boolean isDefaultBanner = entry.getValue();
+                if (!isDefaultBanner) {
                     tagline = String.format("\n%" + width + "s\n", "Powered by Quarkus v" + Version.getVersion());
                 }
 
                 return bannerTitle + tagline;
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
             }
-        } else {
-            logger.warn("Could not read banner file");
+        } catch (IOException e) {
+            logger.warn("Unable to read banner file");
             return "";
+        }
+    }
+
+    /**
+     * @return an entry containing the text of the banner as the key and whether or not the default banner is being used as the
+     *         value. The default banner is used as a last report
+     */
+    private Map.Entry<URL, Boolean> getBanner(BannerConfig config) throws IOException {
+        Enumeration<URL> resources = Thread.currentThread().getContextClassLoader().getResources(config.path);
+        URL defaultBanner = null;
+        URL selectedBanner = null;
+        while (resources.hasMoreElements()) {
+            URL url = resources.nextElement();
+            if (defaultBanner == null && isQuarkusCoreBanner(url)) {
+                defaultBanner = url;
+            }
+            if (selectedBanner == null) {
+                selectedBanner = url;
+            }
+        }
+        return new AbstractMap.SimpleEntry<>(selectedBanner, defaultBanner == selectedBanner);
+    }
+
+    private boolean isQuarkusCoreBanner(URL url) throws IOException {
+        if (!"jar".equals(url.getProtocol())) {
+            return false;
+        }
+
+        // We determine whether the banner is the default by checking to see if the jar that contains it also
+        // contains this class. This way although somewhat complicated guarantees that any rename of artifacts
+        // won't affect the check
+        try (FileSystem fileSystem = FileSystems.newFileSystem(url.toURI(), Collections.emptyMap())) {
+            String thisClassName = this.getClass().getName();
+            String[] parts = thisClassName.split("\\.");
+            List<String> rest = new ArrayList<>(parts.length - 1);
+            rest.addAll(Arrays.asList(parts).subList(1, parts.length - 1));
+            rest.add(parts[parts.length - 1] + ".class");
+            return Files.exists(fileSystem.getPath(parts[0], rest.toArray(new String[] {})));
+        } catch (URISyntaxException e) {
+            return false;
         }
     }
 }

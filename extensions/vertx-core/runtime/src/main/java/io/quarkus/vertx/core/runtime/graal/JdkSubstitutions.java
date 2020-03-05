903ad1d05e0edc14d8077ea3a89f237911bed668
Gwenneg Lepage <gwenneg@gmail.com>
2020-03-05 01:02:50 +0100

    Initialize sun.nio.ch.Iocp at runtime on Windows with JDK 11

--- extensions/vertx-core/runtime/src/main/java/io/quarkus/vertx/core/runtime/graal/JdkSubstitutions.java
+++ extensions/vertx-core/runtime/src/main/java/io/quarkus/vertx/core/runtime/graal/JdkSubstitutions.java
@@ -2,9 +2,11 @@ package io.quarkus.vertx.core.runtime.graal;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.channels.spi.AsynchronousChannelProvider;
 import java.util.function.Function;
 
 import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.InjectAccessors;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 import com.oracle.svm.core.jdk.JDK11OrLater;
@@ -85,6 +87,47 @@ final class Package_jdk_internal_loader implements Function<TargetClass, String>
         }
 
         return "jdk.internal.loader." + annotation.className();
+    }
+}
+
+@Substitute
+@TargetClass(className = "sun.nio.ch.WindowsAsynchronousFileChannelImpl", innerClass = "DefaultIocpHolder", onlyWith = JDK11OrLater.class)
+final class Target_sun_nio_ch_WindowsAsynchronousFileChannelImpl_DefaultIocpHolder {
+
+    @Alias
+    @InjectAccessors(DefaultIocpAccessor.class)
+    static Target_sun_nio_ch_Iocp defaultIocp;
+}
+
+@TargetClass(className = "sun.nio.ch.Iocp", onlyWith = JDK11OrLater.class)
+final class Target_sun_nio_ch_Iocp {
+
+    @Alias
+    Target_sun_nio_ch_Iocp(AsynchronousChannelProvider provider, Target_sun_nio_ch_ThreadPool pool) throws IOException {
+    }
+
+    @Alias
+    Target_sun_nio_ch_Iocp start() {
+        return null;
+    }
+}
+
+@TargetClass(className = "sun.nio.ch.ThreadPool", onlyWith = JDK11OrLater.class)
+final class Target_sun_nio_ch_ThreadPool {
+
+    @Alias
+    static Target_sun_nio_ch_ThreadPool createDefault() {
+        return null;
+    }
+}
+
+final class DefaultIocpAccessor {
+    static Target_sun_nio_ch_Iocp get() {
+        try {
+            return new Target_sun_nio_ch_Iocp(null, Target_sun_nio_ch_ThreadPool.createDefault()).start();
+        } catch (IOException ioe) {
+            throw new InternalError(ioe);
+        }
     }
 }
 

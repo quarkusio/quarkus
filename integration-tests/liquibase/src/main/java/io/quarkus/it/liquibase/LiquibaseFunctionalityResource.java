769b25f33414c9b4f5310e4612ef9c058b50fbd0
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2020-03-06 20:05:29 +0100

    fix: allow Flyway and Liquibase to play together by making JdbcDataSourceSchemaReadyBuildItem a multiple build item
    The liquibase extension was merged in #6334 which means that we have multiple possibilities of making a DB schema ready.
    This makes the build item that controls whether DB schemas are ready a mutliple build item so that it can be produced by several extensions.
    
    Without this we have the following extensions when we try to cohabite Flyway and Liquibase
    ```
    io.quarkus.builder.ChainBuildException: Multiple producers of item class
    io.quarkus.agroal.deployment.JdbcDataSourceSchemaReadyBuildItem(io.quarkus.flyway.FlywayProcessor#configureRuntimeProperties)
    ```
    
    Also avoid the usage of star imports in liquibase integration tests

--- integration-tests/liquibase/src/main/java/io/quarkus/it/liquibase/LiquibaseFunctionalityResource.java
+++ integration-tests/liquibase/src/main/java/io/quarkus/it/liquibase/LiquibaseFunctionalityResource.java
@@ -5,7 +5,11 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
 import io.quarkus.liquibase.LiquibaseFactory;

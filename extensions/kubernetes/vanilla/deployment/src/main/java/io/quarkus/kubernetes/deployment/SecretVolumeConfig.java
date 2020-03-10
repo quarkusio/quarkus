f3736ab9609c8b9e599f4b981ed7f25079439153
Ladislav Thon <lthon@redhat.com>
2020-03-10 11:52:48 +0100

    fix how defaultMode in Kubernetes volumes is parsed
    Previously, the `defaultMode` config property of some Kubernetes
    volumes (ConfigMap, secret) was always treated as a decimal number,
    even though octal number is a common format (and we actually used
    an octal number when specifying the default value!).
    
    This commit changes the type of the config property to `String`,
    which is later parsed to an `int` when calling the Dekorate API.
    At that time, if the string begins with `0`, it's parsed as octal,
    otherwise as decimal.

--- extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/SecretVolumeConfig.java
+++ extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/SecretVolumeConfig.java
@@ -1,21 +1,25 @@
 package io.quarkus.kubernetes.deployment;
 
+import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigItem;
 
+@ConfigGroup
 public class SecretVolumeConfig {
 
     /**
      * The name of the secret to mount.
      */
+    @ConfigItem
     String secretName;
 
     /**
      * Default mode.
+     * When specifying an octal number, leading zero must be present.
      *
      * @return The default mode.
      */
     @ConfigItem(defaultValue = "0600")
-    Integer defaultMode;
+    String defaultMode;
 
     /**
      * Optional

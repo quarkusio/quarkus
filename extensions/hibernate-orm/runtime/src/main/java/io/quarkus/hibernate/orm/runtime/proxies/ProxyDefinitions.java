0321c6e727816d9f9f83a349f1d922638c4abba1
Sanne Grinovero <sanne@hibernate.org>
2020-03-11 15:50:49 +0000

    Allow Hibernate ORM to boot even when entity models are marked as final

--- extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/proxies/ProxyDefinitions.java
+++ extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/proxies/ProxyDefinitions.java
@@ -1,5 +1,7 @@
 package io.quarkus.hibernate.orm.runtime.proxies;
 
+import static org.objectweb.asm.Opcodes.ACC_FINAL;
+
 import java.lang.reflect.Constructor;
 import java.util.Collections;
 import java.util.HashMap;
@@ -14,6 +16,7 @@ import org.hibernate.internal.util.collections.ArrayHelper;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.proxy.pojo.ProxyFactoryHelper;
 import org.hibernate.proxy.pojo.bytebuddy.ByteBuddyProxyHelper;
+import org.jboss.logging.Logger;
 
 /**
  * Runtime proxies are used by Hibernate ORM to handle a number of corner cases;
@@ -25,10 +28,22 @@ import org.hibernate.proxy.pojo.bytebuddy.ByteBuddyProxyHelper;
  * Implementors of a custom {@link org.hibernate.bytecode.spi.ProxyFactoryFactory} are
  * then able to lookup such class definitions at runtime to create new instances of the
  * required enhanced proxies.
+ *
+ * Failure to generate such a proxy is not critical, but it implies that Hibernate ORM
+ * will not be able to use the enhanced proxy mechanism, possibly having to generate
+ * an additional round trip to the database in some circumstances.
+ * Most notably we'll fail to generate such a proxy when the entity has a "final" modifier;
+ * we'll also need a default constructor.
+ * Default constructors are required beyond proxy generation, so a lack of such a constructor
+ * will have us abort the bootstrap process with a critical error.
+ * On the other hand, having the entities marked as "final" is handled gracefully, as we
+ * can simply fallback to not use the enhanced proxy for the specific entity, and because
+ * it's a common case when writing entities in Kotlin.
  */
 public final class ProxyDefinitions {
 
     private final Map<Class<?>, ProxyClassDetailsHolder> proxyDefinitionMap;
+    private static final Logger LOGGER = Logger.getLogger(ProxyDefinitions.class.getName());
 
     private ProxyDefinitions(Map<Class<?>, ProxyClassDetailsHolder> proxyDefinitionMap) {
         this.proxyDefinitionMap = proxyDefinitionMap;
@@ -45,6 +60,9 @@ public final class ProxyDefinitions {
                     if (needsProxyGeneration(persistentClass)) {
                         final Class mappedClass = persistentClass.getMappedClass();
                         final Class proxyClassDefinition = generateProxyClass(persistentClass, byteBuddyProxyHelper);
+                        if (proxyClassDefinition == null) {
+                            continue;
+                        }
                         final boolean overridesEquals = ReflectHelper.overridesEquals(mappedClass);
                         try {
                             proxyDefinitionMap.put(mappedClass,
@@ -81,6 +99,12 @@ public final class ProxyDefinitions {
     private static Class generateProxyClass(PersistentClass persistentClass, ByteBuddyProxyHelper byteBuddyProxyHelper) {
         final String entityName = persistentClass.getEntityName();
         final Class mappedClass = persistentClass.getMappedClass();
+        if ((mappedClass.getModifiers() & ACC_FINAL) == ACC_FINAL) {
+            LOGGER.warn("Could not generate an enhanced proxy for entity '" + entityName + "' (class='"
+                    + mappedClass.getCanonicalName()
+                    + "') as it's final. Your application might perform better if we're allowed to extend it.");
+            return null;
+        }
         final Set<Class> proxyInterfaces = ProxyFactoryHelper.extractProxyInterfaces(persistentClass, entityName);
         Class proxyDef = byteBuddyProxyHelper.buildProxy(mappedClass, toArray(proxyInterfaces));
         return proxyDef;

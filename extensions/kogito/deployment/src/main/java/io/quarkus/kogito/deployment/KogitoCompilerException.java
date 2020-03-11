84abed8d2c48c004dcfc6edc27d6d30a41ca999c
mariofusco <mario.fusco@gmail.com>
2020-03-11 13:52:37 +0100

    remove kogito extension from quarkus codebase

--- extensions/kogito/deployment/src/main/java/io/quarkus/kogito/deployment/KogitoCompilerException.java
+++
@@ -1,8 +0,0 @@
-package io.quarkus.kogito.deployment;
-
-public class KogitoCompilerException extends RuntimeException {
-
-    public KogitoCompilerException(Throwable cause) {
-        super(cause);
-    }
-}
--- integration-tests/kogito/src/test/java/io/quarkus/it/kogito/jbpm/OrdersProcessTestIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkus.it.kogito.jbpm;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class OrdersProcessTestIT extends OrdersProcessTest {
-}

e9be655302ad702b28fd9e2ab93d676af47fe8d1
Guillaume Smet <guillaume.smet@gmail.com>
2020-03-12 19:14:21 +0100

    Make duration in Kubernetes extension config proper durations

--- extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/ProbeConverter.java
+++ extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/ProbeConverter.java
@@ -11,9 +11,9 @@ public class ProbeConverter {
         probe.httpActionPath.ifPresent(v -> b.withHttpActionPath(v));
         probe.execAction.ifPresent(v -> b.withExecAction(v));
         probe.tcpSocketAction.ifPresent(v -> b.withTcpSocketAction(v));
-        b.withInitialDelaySeconds(probe.initialDelaySeconds);
-        b.withPeriodSeconds(probe.periodSeconds);
-        b.withTimeoutSeconds(probe.timeoutSeconds);
+        b.withInitialDelaySeconds((int) probe.initialDelay.getSeconds());
+        b.withPeriodSeconds((int) probe.period.getSeconds());
+        b.withTimeoutSeconds((int) probe.timeout.getSeconds());
         b.withSuccessThreshold(probe.successThreshold);
         b.withFailureThreshold(probe.failureThreshold);
         return b.build();

9fa945e7a903aff8ef367d6402139c92b275ce39
Clement Escoffier <clement.escoffier@gmail.com>
2020-03-18 08:54:39 +0100

    Use the Mutiny reactive converter from SmallRye instead of our internal implementation.

--- extensions/mutiny/deployment/src/main/java/io/quarkus/mutiny/deployment/MutinyProcessor.java
+++ extensions/mutiny/deployment/src/main/java/io/quarkus/mutiny/deployment/MutinyProcessor.java
@@ -1,29 +1,12 @@
 package io.quarkus.mutiny.deployment;
 
-import org.jboss.jandex.DotName;
-
-import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
-import io.quarkus.mutiny.converters.MultiConverter;
-import io.quarkus.mutiny.converters.UniConverter;
-import io.smallrye.reactive.converters.ReactiveTypeConverter;
 
 public class MutinyProcessor {
 
     @BuildStep
     public FeatureBuildItem registerFeature() {
         return new FeatureBuildItem(FeatureBuildItem.MUTINY);
-    }
-
-    private static final DotName REACTIVE_TYPE_CONVERTER = DotName.createSimple(ReactiveTypeConverter.class.getName());
-
-    @BuildStep
-    public void registerReactiveConverters(BuildProducer<ServiceProviderBuildItem> serviceProvider) {
-        serviceProvider.produce(new ServiceProviderBuildItem(REACTIVE_TYPE_CONVERTER.toString(),
-                UniConverter.class.getName()));
-        serviceProvider.produce(new ServiceProviderBuildItem(REACTIVE_TYPE_CONVERTER.toString(),
-                MultiConverter.class.getName()));
     }
 }

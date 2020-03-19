0afc2766bb5dda183b8b4b8002da1ab1f1f88ca5
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-03-19 14:54:09 +1100

    Integrate vert.x routes with security
    Fixes #4544

--- extensions/vertx-web/runtime/src/main/java/io/quarkus/vertx/web/runtime/RouteHandler.java
+++ extensions/vertx-web/runtime/src/main/java/io/quarkus/vertx/web/runtime/RouteHandler.java
@@ -1,6 +1,9 @@
 package io.quarkus.vertx.web.runtime;
 
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.ManagedContext;
 import io.quarkus.arc.runtime.BeanInvoker;
+import io.quarkus.vertx.http.runtime.security.QuarkusHttpUser;
 import io.quarkus.vertx.web.Route;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.RoutingContext;
@@ -14,7 +17,24 @@ public interface RouteHandler extends Handler<RoutingContext>, BeanInvoker<Routi
 
     @Override
     default void handle(RoutingContext context) {
-        BeanInvoker.super.invoke(context);
+        QuarkusHttpUser user = (QuarkusHttpUser) context.user();
+        ManagedContext requestContext = Arc.container().requestContext();
+        if (requestContext.isActive()) {
+            if (user != null) {
+                Arc.container().beanManager().fireEvent(user.getSecurityIdentity());
+            }
+            invokeBean(context);
+        } else {
+            try {
+                requestContext.activate();
+                if (user != null) {
+                    Arc.container().beanManager().fireEvent(user.getSecurityIdentity());
+                }
+                invokeBean(context);
+            } finally {
+                requestContext.terminate();
+            }
+        }
     }
 
 }

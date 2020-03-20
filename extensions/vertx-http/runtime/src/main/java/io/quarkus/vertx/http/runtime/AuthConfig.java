53b3b483a1ac127a39e5563d8275818e0b5554fd
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-03-20 12:13:32 +1100

    Multiple authentication fixes
    - Use synthetic beans to configure form and basic auth
    - Allow multiple authentication mechanisms
    - Better default behaviour based on what is configured
    
    Fixes #7768
    Fixes #5284

--- extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/AuthConfig.java
+++ extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/AuthConfig.java
@@ -14,10 +14,7 @@ public class AuthConfig {
     /**
      * If basic auth should be enabled. If both basic and form auth is enabled then basic auth will be enabled in silent mode.
      *
-     * If no authentication mechanisms are configured basic auth is the default, unless an
-     * {@link io.quarkus.security.identity.IdentityProvider}
-     * is present that supports {@link io.quarkus.security.identity.request.TokenAuthenticationRequest} in which case
-     * form auth will be the default.
+     * If no authentication mechanisms are configured basic auth is the default.
      */
     @ConfigItem
     public boolean basic;

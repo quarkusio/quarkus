39006be9c85486d9d9f8b68b8a736845bf8f759d
Sanne Grinovero <sanne@hibernate.org>
2020-04-02 21:08:15 +0100

    Integration tests: use the same model for integration tests as well

--- extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/specialmappings/EarlyMetadaValidationTest.java
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/specialmappings/EarlyMetadaValidationTest.java
@@ -7,6 +7,13 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.test.QuarkusUnitTest;
 
+/**
+ * The particular model used in this test requires access to the ProxyFactoryFactory service
+ * during the metadata validation.
+ * This test verifies that such service is available in that phase: otherwise we
+ * wouldn't be able to bootstrap successfully.
+ * See also https://github.com/quarkusio/quarkus/issues/8350
+ */
 public class EarlyMetadaValidationTest {
 
     @RegisterExtension
---
+++ integration-tests/jpa-h2/src/main/java/io/quarkus/it/jpa/h2/DataIdentity.java
@@ -0,0 +1,36 @@
+package io.quarkus.it.jpa.h2;
+
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * This entity isn't directly referenced: its mere presence is
+ * useful to be able to verify bootstrap capabilities in the
+ * presence of abstract entities in the hierarchy.
+ */
+@MappedSuperclass
+@IdClass(IdVersionPK.class)
+public abstract class DataIdentity {
+    @Id
+    private String id;
+
+    @Id
+    private Long version;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+}

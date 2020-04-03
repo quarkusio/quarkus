cebc52386d3a3fd5114c9089650f2c5359aa8079
Georgios Andrianakis <geoand@gmail.com>
2020-04-03 13:47:41 +0300

    Make it easy to configure Kubernetes Mock Server before the Quarkus application starts

--- test-framework/kubernetes-client/src/main/java/io/quarkus/test/kubernetes/client/KubernetesMockServerTestResource.java
+++ test-framework/kubernetes-client/src/main/java/io/quarkus/test/kubernetes/client/KubernetesMockServerTestResource.java
@@ -27,7 +27,17 @@ public class KubernetesMockServerTestResource implements QuarkusTestResourceLife
             systemProps.put(Config.KUBERNETES_MASTER_SYSTEM_PROPERTY, client.getConfiguration().getMasterUrl());
         }
 
+        configureMockServer(mockServer);
+
         return systemProps;
+    }
+
+    /**
+     * Can be used by subclasses of {@code KubernetesMockServerTestResource} in order to
+     * setup the mock server before the Quarkus application starts
+     */
+    public void configureMockServer(KubernetesMockServer mockServer) {
+
     }
 
     @Override

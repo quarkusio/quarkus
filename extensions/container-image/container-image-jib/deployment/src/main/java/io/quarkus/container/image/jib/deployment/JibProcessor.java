7bb815802950019adee07ee863696829f6798962
Georgios Andrianakis <geoand@gmail.com>
2020-04-06 11:52:12 +0300

    Allow adding custom labels to container images created via Jib
    Fixes: #8406

--- extensions/container-image/container-image-jib/deployment/src/main/java/io/quarkus/container/image/jib/deployment/JibProcessor.java
+++ extensions/container-image/container-image-jib/deployment/src/main/java/io/quarkus/container/image/jib/deployment/JibProcessor.java
@@ -211,6 +211,7 @@ public class JibProcessor {
 
             return javaContainerBuilder.toContainerBuilder()
                     .setEnvironment(jibConfig.environmentVariables)
+                    .setLabels(jibConfig.labels)
                     .setCreationTime(Instant.now());
         } catch (IOException e) {
             throw new UncheckedIOException(e);
@@ -234,6 +235,7 @@ public class JibProcessor {
                     .setWorkingDirectory(workDirInContainer)
                     .setEntrypoint(entrypoint)
                     .setEnvironment(jibConfig.environmentVariables)
+                    .setLabels(jibConfig.labels)
                     .setCreationTime(Instant.now());
         } catch (InvalidImageReferenceException e) {
             throw new RuntimeException(e);

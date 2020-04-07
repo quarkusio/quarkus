6695c66edd4b140f5d55643f737662acb975b1f5
Matthias Harter <matthias.harter@ionos.com>
2020-04-07 10:59:33 +0200

    add elytron ldap authentication extension

---
+++ integration-tests/elytron-security-ldap/src/test/java/io/quarkus/elytron/security/ldap/it/ElytronLdapExtensionTestResources.java
@@ -0,0 +1,8 @@
+package io.quarkus.elytron.security.ldap.it;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.ldap.LdapServerTestResource;
+
+@QuarkusTestResource(LdapServerTestResource.class)
+public class ElytronLdapExtensionTestResources {
+}

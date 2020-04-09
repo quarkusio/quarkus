304783236df87f7b3f22da41e096a7dbf2584f72
Georgios Andrianakis <geoand@gmail.com>
2020-04-09 14:08:27 +0300

    Differentiate the username/password for pulling base image with Jib
    Fixes: #8498

--- extensions/container-image/container-image-jib/deployment/src/main/java/io/quarkus/container/image/jib/deployment/JibConfig.java
+++ extensions/container-image/container-image-jib/deployment/src/main/java/io/quarkus/container/image/jib/deployment/JibConfig.java
@@ -2,6 +2,7 @@ package io.quarkus.container.image.jib.deployment;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
@@ -44,5 +45,17 @@ public class JibConfig {
      * Custom labels to add to the generated image
      */
     @ConfigItem
-    Map<String, String> labels;
+    public Map<String, String> labels;
+
+    /**
+     * The username to use to authenticate with the registry used to pull the base JVM image
+     */
+    @ConfigItem
+    public Optional<String> baseRegistryUsername;
+
+    /**
+     * The password to use to authenticate with the registry used to pull the base JVM image
+     */
+    @ConfigItem
+    public Optional<String> baseRegistryPassword;
 }

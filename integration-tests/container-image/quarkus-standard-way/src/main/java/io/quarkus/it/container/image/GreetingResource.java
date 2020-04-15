0a2b90201ee80c21f7e48af14a5e1c4120284738
Georgios Andrianakis <geoand@gmail.com>
2020-04-15 14:49:12 +0300

    Introduce QuarkusProdTest module for container-image tests and add test for multiple container-image extensions

---
+++ integration-tests/container-image/quarkus-standard-way/src/main/java/io/quarkus/it/container/image/GreetingResource.java
@@ -0,0 +1,16 @@
+package io.quarkus.it.container.image;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/greeting")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}

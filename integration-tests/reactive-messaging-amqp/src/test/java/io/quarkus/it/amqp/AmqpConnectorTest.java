6aa4984d584a462792ec7304ec4f75a5813ca1cc
Clement Escoffier <clement.escoffier@gmail.com>
2020-04-19 15:33:11 +0200

    Disable the AMQP IT tests on Windows

--- integration-tests/reactive-messaging-amqp/src/test/java/io/quarkus/it/amqp/AmqpConnectorTest.java
+++ integration-tests/reactive-messaging-amqp/src/test/java/io/quarkus/it/amqp/AmqpConnectorTest.java
@@ -6,6 +6,8 @@ import static org.awaitility.Awaitility.await;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
@@ -13,6 +15,7 @@ import io.restassured.common.mapper.TypeRef;
 
 @QuarkusTest
 @QuarkusTestResource(AmqpBroker.class)
+@DisabledOnOs(OS.WINDOWS)
 public class AmqpConnectorTest {
 
     protected static final TypeRef<List<Person>> TYPE_REF = new TypeRef<List<Person>>() {

f4252631a328386157d4c2e0be1c65a3771f42ac
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-04-20 17:56:42 +1000

    Change dynodb to use config injection

--- extensions/amazon-dynamodb/runtime/src/main/java/io/quarkus/dynamodb/runtime/DynamodbRecorder.java
+++ extensions/amazon-dynamodb/runtime/src/main/java/io/quarkus/dynamodb/runtime/DynamodbRecorder.java
@@ -1,8 +1,6 @@
 package io.quarkus.dynamodb.runtime;
 
-import io.quarkus.arc.Arc;
 import io.quarkus.arc.runtime.BeanContainer;
-import io.quarkus.arc.runtime.BeanContainerListener;
 import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.ShutdownContext;
 import io.quarkus.runtime.annotations.Recorder;
@@ -11,17 +9,6 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Recorder
 public class DynamodbRecorder {
-
-    public BeanContainerListener setDynamodbBuildConfig(DynamodbBuildTimeConfig buildConfig) {
-        return beanContainer -> {
-            DynamodbClientProducer producer = beanContainer.instance(DynamodbClientProducer.class);
-            producer.setBuildTimeConfig(buildConfig);
-        };
-    }
-
-    public void configureRuntimeConfig(DynamodbConfig dynamodbConfig) {
-        Arc.container().instance(DynamodbClientProducer.class).get().setRuntimeConfig(dynamodbConfig);
-    }
 
     public RuntimeValue<DynamoDbClient> createClient(
             BeanContainer beanContainer, ShutdownContext shutdown) {

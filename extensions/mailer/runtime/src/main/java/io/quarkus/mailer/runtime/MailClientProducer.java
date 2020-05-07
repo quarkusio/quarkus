92fe692f1a6438645c0891d21455f7d6187d8395
Georgios Andrianakis <geoand@gmail.com>
2020-05-07 12:01:40 +0300

    Vastly simplify mailer CDI configuration

--- extensions/mailer/runtime/src/main/java/io/quarkus/mailer/runtime/MailClientProducer.java
+++ extensions/mailer/runtime/src/main/java/io/quarkus/mailer/runtime/MailClientProducer.java
@@ -1,12 +1,16 @@
 package io.quarkus.mailer.runtime;
 
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
 
 import org.jboss.logging.Logger;
 
+import io.vertx.core.Vertx;
+import io.vertx.ext.mail.LoginOption;
 import io.vertx.ext.mail.MailClient;
+import io.vertx.ext.mail.StartTLSOptions;
 
 /**
  * Beans producing the Vert.x Mail clients.
@@ -16,52 +20,74 @@ public class MailClientProducer {
 
     private static final Logger LOGGER = Logger.getLogger(MailClientProducer.class);
 
-    private io.vertx.axle.ext.mail.MailClient axleMailClient;
-    private io.vertx.reactivex.ext.mail.MailClient rxMailClient;
-    private io.vertx.mutiny.ext.mail.MailClient mutinyClient;
-    private MailClient client;
+    private final io.vertx.mutiny.ext.mail.MailClient mutinyClient;
+    private final MailClient client;
 
-    synchronized void initialize(MailClient client) {
-        this.client = client;
-        this.mutinyClient = io.vertx.mutiny.ext.mail.MailClient.newInstance(client);
+    public MailClientProducer(Vertx vertx, MailConfig config) {
+        this.client = mailClient(vertx, config);
+        this.mutinyClient = io.vertx.mutiny.ext.mail.MailClient.newInstance(this.client);
     }
 
     @Singleton
     @Produces
-    public synchronized MailClient mailClient() {
+    public MailClient mailClient() {
         return client;
     }
 
     @Singleton
     @Produces
-    public synchronized io.vertx.mutiny.ext.mail.MailClient mutinyClient() {
+    public io.vertx.mutiny.ext.mail.MailClient mutinyClient() {
         return mutinyClient;
     }
 
     @Singleton
     @Produces
     @Deprecated
-    public synchronized io.vertx.axle.ext.mail.MailClient axleMailClient() {
-        if (axleMailClient == null) {
-            LOGGER.warn(
-                    "`io.vertx.axle.ext.mail.MailClient` is deprecated and will be removed in a future version - it is "
-                            + "recommended to switch to `io.vertx.mutiny.ext.mail.MailClient`");
-            axleMailClient = io.vertx.axle.ext.mail.MailClient.newInstance(client);
-        }
-        return axleMailClient;
+    public io.vertx.axle.ext.mail.MailClient axleMailClient() {
+        LOGGER.warn(
+                "`io.vertx.axle.ext.mail.MailClient` is deprecated and will be removed in a future version - it is "
+                        + "recommended to switch to `io.vertx.mutiny.ext.mail.MailClient`");
+        return io.vertx.axle.ext.mail.MailClient.newInstance(client);
     }
 
     @Singleton
     @Produces
     @Deprecated
-    public synchronized io.vertx.reactivex.ext.mail.MailClient rxMailClient() {
-        if (rxMailClient == null) {
-            LOGGER.warn(
-                    "`io.vertx.reactivex.ext.mail.MailClient` is deprecated and will be removed in a future version - it is "
-                            + "recommended to switch to `io.vertx.mutiny.ext.mail.MailClient`");
-            rxMailClient = io.vertx.reactivex.ext.mail.MailClient.newInstance(client);
-        }
-        return rxMailClient;
+    public io.vertx.reactivex.ext.mail.MailClient rxMailClient() {
+        LOGGER.warn(
+                "`io.vertx.reactivex.ext.mail.MailClient` is deprecated and will be removed in a future version - it is "
+                        + "recommended to switch to `io.vertx.mutiny.ext.mail.MailClient`");
+        return io.vertx.reactivex.ext.mail.MailClient.newInstance(client);
+    }
+
+    @PreDestroy
+    public void stop() {
+        client.close();
+    }
+
+    private MailClient mailClient(Vertx vertx, MailConfig config) {
+        io.vertx.ext.mail.MailConfig cfg = toVertxMailConfig(config);
+        return MailClient.createShared(vertx, cfg);
+    }
+
+    private io.vertx.ext.mail.MailConfig toVertxMailConfig(MailConfig config) {
+        io.vertx.ext.mail.MailConfig cfg = new io.vertx.ext.mail.MailConfig();
+        config.authMethods.ifPresent(cfg::setAuthMethods);
+        cfg.setDisableEsmtp(config.disableEsmtp);
+        cfg.setHostname(config.host);
+        cfg.setKeepAlive(config.keepAlive);
+        config.keyStore.ifPresent(cfg::setKeyStore);
+        config.keyStorePassword.ifPresent(cfg::setKeyStorePassword);
+        config.login.ifPresent(s -> cfg.setLogin(LoginOption.valueOf(s.toUpperCase())));
+        config.maxPoolSize.ifPresent(cfg::setMaxPoolSize);
+        config.ownHostName.ifPresent(cfg::setOwnHostname);
+        config.username.ifPresent(cfg::setUsername);
+        config.password.ifPresent(cfg::setPassword);
+        config.port.ifPresent(cfg::setPort);
+        cfg.setSsl(config.ssl);
+        config.startTLS.ifPresent(s -> cfg.setStarttls(StartTLSOptions.valueOf(s.toUpperCase())));
+        cfg.setTrustAll(config.trustAll);
+        return cfg;
     }
 
 }

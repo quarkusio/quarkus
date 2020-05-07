015a57fff3372208e36fe266f655b9ff0bfeadab
Georgios Andrianakis <geoand@gmail.com>
2020-05-07 16:28:44 +0300

    Remove lambdas from MailClientProducer

--- extensions/mailer/runtime/src/main/java/io/quarkus/mailer/runtime/MailClientProducer.java
+++ extensions/mailer/runtime/src/main/java/io/quarkus/mailer/runtime/MailClientProducer.java
@@ -72,20 +72,40 @@ public class MailClientProducer {
 
     private io.vertx.ext.mail.MailConfig toVertxMailConfig(MailConfig config) {
         io.vertx.ext.mail.MailConfig cfg = new io.vertx.ext.mail.MailConfig();
-        config.authMethods.ifPresent(cfg::setAuthMethods);
+        if (config.authMethods.isPresent()) {
+            cfg.setAuthMethods(config.authMethods.get());
+        }
         cfg.setDisableEsmtp(config.disableEsmtp);
         cfg.setHostname(config.host);
         cfg.setKeepAlive(config.keepAlive);
-        config.keyStore.ifPresent(cfg::setKeyStore);
-        config.keyStorePassword.ifPresent(cfg::setKeyStorePassword);
-        config.login.ifPresent(s -> cfg.setLogin(LoginOption.valueOf(s.toUpperCase())));
-        config.maxPoolSize.ifPresent(cfg::setMaxPoolSize);
-        config.ownHostName.ifPresent(cfg::setOwnHostname);
-        config.username.ifPresent(cfg::setUsername);
-        config.password.ifPresent(cfg::setPassword);
-        config.port.ifPresent(cfg::setPort);
+        if (config.keyStore.isPresent()) {
+            cfg.setKeyStore(config.keyStore.get());
+        }
+        if (config.keyStorePassword.isPresent()) {
+            cfg.setKeyStorePassword(config.keyStorePassword.get());
+        }
+        if (config.login.isPresent()) {
+            cfg.setLogin(LoginOption.valueOf(config.login.get().toUpperCase()));
+        }
+        if (config.maxPoolSize.isPresent()) {
+            cfg.setMaxPoolSize(config.maxPoolSize.getAsInt());
+        }
+        if (config.ownHostName.isPresent()) {
+            cfg.setOwnHostname(config.ownHostName.get());
+        }
+        if (config.username.isPresent()) {
+            cfg.setUsername(config.username.get());
+        }
+        if (config.password.isPresent()) {
+            cfg.setPassword(config.password.get());
+        }
+        if (config.port.isPresent()) {
+            cfg.setPort(config.port.getAsInt());
+        }
         cfg.setSsl(config.ssl);
-        config.startTLS.ifPresent(s -> cfg.setStarttls(StartTLSOptions.valueOf(s.toUpperCase())));
+        if (config.startTLS.isPresent()) {
+            cfg.setStarttls(StartTLSOptions.valueOf(config.startTLS.get().toUpperCase()));
+        }
         cfg.setTrustAll(config.trustAll);
         return cfg;
     }

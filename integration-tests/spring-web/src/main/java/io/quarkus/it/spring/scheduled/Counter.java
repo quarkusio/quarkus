2f6fd58c9d89edca3a5c60b7fd7c64e56063424a
aurea munoz <amunozhe@redhat.com>
2020-05-07 12:17:07 +0200

    feature: add spring-scheduling extension

---
+++ integration-tests/spring-web/src/main/java/io/quarkus/it/spring/scheduled/Counter.java
@@ -0,0 +1,29 @@
+package io.quarkus.it.spring.scheduled;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+@ApplicationScoped
+public class Counter {
+
+    AtomicInteger counter;
+
+    @PostConstruct
+    void init() {
+        counter = new AtomicInteger();
+    }
+
+    public int get() {
+        return counter.get();
+    }
+
+    @Scheduled(cron = "0/1 * * * * ?")
+    void increment() {
+        counter.incrementAndGet();
+    }
+
+}

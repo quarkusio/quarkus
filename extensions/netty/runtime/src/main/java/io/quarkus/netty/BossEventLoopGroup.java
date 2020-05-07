00f0973647812bf087b315a92987d734ec4e40ce
Martin Kouba <mkouba@redhat.com>
2020-05-07 12:25:46 +0200

    Netty EventLoopGroup qualifiers - add annotation literals
    - to ease the programmatic lookup of EventLoopGroup beans

--- extensions/netty/runtime/src/main/java/io/quarkus/netty/BossEventLoopGroup.java
+++ extensions/netty/runtime/src/main/java/io/quarkus/netty/BossEventLoopGroup.java
@@ -3,9 +3,21 @@ package io.quarkus.netty;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import javax.enterprise.util.AnnotationLiteral;
 import javax.inject.Qualifier;
 
 @Qualifier
 @Retention(RetentionPolicy.RUNTIME)
 public @interface BossEventLoopGroup {
+
+    /**
+     * Supports inline instantiation of this qualifier.
+     */
+    public static final class Literal extends AnnotationLiteral<BossEventLoopGroup> implements BossEventLoopGroup {
+
+        public static final Literal INSTANCE = new Literal();
+
+        private static final long serialVersionUID = 1L;
+
+    }
 }
--- extensions/netty/runtime/src/main/java/io/quarkus/netty/MainEventLoopGroup.java
+++ extensions/netty/runtime/src/main/java/io/quarkus/netty/MainEventLoopGroup.java
@@ -3,9 +3,21 @@ package io.quarkus.netty;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import javax.enterprise.util.AnnotationLiteral;
 import javax.inject.Qualifier;
 
 @Qualifier
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MainEventLoopGroup {
+
+    /**
+     * Supports inline instantiation of this qualifier.
+     */
+    public static final class Literal extends AnnotationLiteral<MainEventLoopGroup> implements MainEventLoopGroup {
+
+        public static final Literal INSTANCE = new Literal();
+
+        private static final long serialVersionUID = 1L;
+
+    }
 }

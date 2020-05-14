5c5104e597c8cb24cfeeb11b28a38904d06311f5
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-05-14 13:37:35 +1000

    Add support for config based SameSite cookies
    Fixes #9286

--- extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/HttpConfiguration.java
+++ extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/HttpConfiguration.java
@@ -1,6 +1,7 @@
 package io.quarkus.vertx.http.runtime;
 
 import java.time.Duration;
+import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalInt;
 
@@ -182,13 +183,19 @@ public class HttpConfiguration {
 
     /**
      * If this is true then the request start time will be recorded to enable logging of total request time.
-     * 
+     *
      * This has a small performance penalty, so is disabled by default.
      */
     @ConfigItem
     public boolean recordRequestStartTime;
 
     AccessLogConfig accessLog;
+
+    /**
+     * Configuration that allows setting the same site attributes for cookies.
+     */
+    @ConfigItem
+    public Map<String, SameSiteCookieConfig> sameSiteCookie;
 
     public int determinePort(LaunchMode launchMode) {
         return launchMode == LaunchMode.TEST ? testPort : port;

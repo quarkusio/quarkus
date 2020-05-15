edd539f29c305ab908ed6bcd315821e1e5beb137
Guillaume Smet <guillaume.smet@gmail.com>
2020-05-15 15:15:25 +0200

    Upgrade GraalVM to 20.1.0
    Note that we are still supporting 19.3.1 and 19.3.2 for now.
    
    I'm recommending either 19.3.2 or 20.1.0 in the error message.

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/NativeConfig.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/NativeConfig.java
@@ -140,7 +140,7 @@ public class NativeConfig {
     /**
      * The docker image to use to do the image build
      */
-    @ConfigItem(defaultValue = "quay.io/quarkus/ubi-quarkus-native-image:19.3.1-java11")
+    @ConfigItem(defaultValue = "quay.io/quarkus/ubi-quarkus-native-image:20.1.0-java11")
     public String builderImage;
 
     /**

b406f7ac160389fdcf19ef95225afbdea7a48441
Georgios Andrianakis <geoand@gmail.com>
2020-06-02 17:12:37 +0300

    Make REST Client work whether or not SSL is enabled or not in native
    This is done in order to enable the use of Apache HTTP Client in all
    cases where the REST Client is used.
    This enables us to drop the fallback to URLConnection which is rather limited
    
    Fixes: #9342

---
+++ extensions/rest-client/runtime/src/main/java/io/quarkus/restclient/runtime/graal/ClientHttpEngineBuilder43Replacement.java
@@ -0,0 +1,36 @@
+package io.quarkus.restclient.runtime.graal;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+
+import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+
+/**
+ * This class is used to make sure that only the default SSLContext is requested when no SSLContext has been provided.
+ * The reason this is necessary is because by default, the code requests
+ * {@code SSLContext.getInstance(SSLConnectionSocketFactory.TLS)} which will fail in native when the SSL has been disabled
+ */
+@TargetClass(className = "org.jboss.resteasy.client.jaxrs.engines.ClientHttpEngineBuilder43")
+public final class ClientHttpEngineBuilder43Replacement {
+
+    @Alias
+    private ResteasyClientBuilder that;
+
+    @Substitute
+    public ClientHttpEngineBuilder43Replacement resteasyClientBuilder(ResteasyClientBuilder resteasyClientBuilder) {
+        that = resteasyClientBuilder;
+        if (that.getSSLContext() == null) {
+            try {
+                that.sslContext(SSLContext.getDefault());
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return this;
+    }
+}

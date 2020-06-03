c96108bbc10b76bb24f860526b0d7f8e83e62d48
Andrew Guibert <andy.guibert@gmail.com>
2020-06-03 09:27:45 -0500

    Address review comments from Clement

--- extensions/reactive-db2-client/runtime/src/main/java/io/quarkus/reactive/db2/client/runtime/health/ReactiveDB2DataSourceHealthCheck.java
+++ extensions/reactive-db2-client/runtime/src/main/java/io/quarkus/reactive/db2/client/runtime/health/ReactiveDB2DataSourceHealthCheck.java
@@ -1,43 +1,32 @@
 package io.quarkus.reactive.db2.client.runtime.health;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Readiness;
 
-import io.quarkus.arc.Arc;
-import io.vertx.db2client.DB2Pool;
+import io.vertx.mutiny.db2client.DB2Pool;
 
 @Readiness
 @ApplicationScoped
 public class ReactiveDB2DataSourceHealthCheck implements HealthCheck {
-    private DB2Pool db2Pool;
 
-    @PostConstruct
-    protected void init() {
-        db2Pool = Arc.container().instance(DB2Pool.class).get();
-    }
+    @Inject
+    DB2Pool db2Pool;
 
     @Override
     public HealthCheckResponse call() {
         HealthCheckResponseBuilder builder = HealthCheckResponse.named("Reactive DB2 connection health check").up();
 
         try {
-            CompletableFuture<Void> databaseConnectionAttempt = new CompletableFuture<>();
             db2Pool.query("SELECT 1 FROM SYSIBM.SYSDUMMY1")
-                    .execute(ar -> {
-                        if (ar.failed()) {
-                            builder.down();
-                        }
-                        databaseConnectionAttempt.complete(null);
-                    });
-            databaseConnectionAttempt.get(10, TimeUnit.SECONDS);
+                    .execute()
+                    .await().atMost(Duration.ofSeconds(10));
         } catch (Exception exception) {
             builder.down();
         }

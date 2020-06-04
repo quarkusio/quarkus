c7154dd9dec530d12d6fe6ede00eaad65971df80
Ladislav Thon <lthon@redhat.com>
2020-06-04 11:58:31 +0200

    Update to MicroProfile Fault Tolerance 3.0-RC2 and SmallRye Fault Tolerance 5.0.0-RC1

---
+++ extensions/smallrye-fault-tolerance/deployment/src/test/java/io/quarkus/smallrye/faulttolerance/test/asynchronous/additional/BlockingAsyncTest.java
@@ -0,0 +1,38 @@
+package io.quarkus.smallrye.faulttolerance.test.asynchronous.additional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.CompletionStage;
+
+import javax.inject.Inject;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class BlockingAsyncTest {
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class).addClasses(BlockingHelloService.class));
+
+    @Inject
+    BlockingHelloService service;
+
+    @Test
+    public void threadOffloadAndFallback() throws Exception {
+        Thread mainThread = Thread.currentThread();
+
+        CompletionStage<String> future = service.hello();
+        assertThat(future.toCompletableFuture().get()).isEqualTo("hello");
+
+        assertThat(service.getHelloThread()).isNotSameAs(mainThread);
+        assertThat(service.getHelloStackTrace()).anySatisfy(frame -> {
+            assertThat(frame.getClassName()).contains("io.smallrye.faulttolerance.core");
+        });
+
+        assertThat(service.getFallbackThread()).isNotSameAs(mainThread);
+    }
+}

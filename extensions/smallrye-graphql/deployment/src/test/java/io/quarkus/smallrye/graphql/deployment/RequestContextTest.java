100c1e9954c41e551745b655f3f0fb92b9e8edd0
Jan Martiska <jmartisk@redhat.com>
2020-06-05 13:12:03 +0200

    Activate request context for GraphQL API methods

---
+++ extensions/smallrye-graphql/deployment/src/test/java/io/quarkus/smallrye/graphql/deployment/RequestContextTest.java
@@ -0,0 +1,57 @@
+package io.quarkus.smallrye.graphql.deployment;
+
+import static org.hamcrest.Matchers.containsString;
+
+import javax.enterprise.context.ContextNotActiveException;
+
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.arc.Arc;
+import io.quarkus.test.QuarkusUnitTest;
+import io.restassured.RestAssured;
+
+/**
+ * Make sure that the request context is active while processing GraphQL API methods.
+ * This is necessary to get frameworks like JPA working.
+ */
+public class RequestContextTest extends AbstractGraphQLTest {
+
+    @RegisterExtension
+    static QuarkusUnitTest test = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClass(RequestContextApi.class)
+                    .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml"));
+
+    @Test
+    public void verifyRequestContextActive() {
+        String query = getPayload("{ foo }");
+        RestAssured.given()
+                .body(query)
+                .contentType(MEDIATYPE_JSON)
+                .post("/graphql")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(containsString("success"));
+    }
+
+    @GraphQLApi
+    public static class RequestContextApi {
+
+        @Query
+        public String foo() {
+            if (!Arc.container().requestContext().isActive()) {
+                throw new ContextNotActiveException();
+            }
+            return "success";
+        }
+
+    }
+
+}

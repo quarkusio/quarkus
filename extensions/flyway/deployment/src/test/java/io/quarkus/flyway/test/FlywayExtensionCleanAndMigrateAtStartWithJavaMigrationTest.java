54cd957f152f4bdb8cedb7570a334c535b68bae8
George Gastaldi <gegastaldi@gmail.com>
2020-06-05 12:26:25 -0300

    Index flyway dependency to look for all JavaMigration implementations

--- extensions/flyway/deployment/src/test/java/io/quarkus/flyway/test/FlywayExtensionCleanAndMigrateAtStartWithJavaMigrationTest.java
+++ extensions/flyway/deployment/src/test/java/io/quarkus/flyway/test/FlywayExtensionCleanAndMigrateAtStartWithJavaMigrationTest.java
@@ -1,6 +1,7 @@
 package io.quarkus.flyway.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -10,8 +11,10 @@ import java.sql.Statement;
 import javax.inject.Inject;
 
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationVersion;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
+import org.flywaydb.core.api.migration.JavaMigration;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.DisplayName;
@@ -32,7 +35,7 @@ public class FlywayExtensionCleanAndMigrateAtStartWithJavaMigrationTest {
     @RegisterExtension
     static final QuarkusUnitTest config = new QuarkusUnitTest()
             .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
-                    .addClass(V1_0_1__Update.class)
+                    .addClasses(V1_0_1__Update.class, V1_0_2__Update.class)
                     .addAsResource("db/migration/V1.0.0__Quarkus.sql")
                     .addAsResource("clean-and-migrate-at-start-config.properties", "application.properties"));
 
@@ -42,13 +45,14 @@ public class FlywayExtensionCleanAndMigrateAtStartWithJavaMigrationTest {
 
         try (Connection connection = defaultDataSource.getConnection(); Statement stat = connection.createStatement()) {
             try (ResultSet countQuery = stat.executeQuery("select count(1) from quarked_flyway")) {
-                countQuery.first();
-                int count = countQuery.getInt(1);
-                assertEquals(1, count, "Table 'quarked_flyway' does not contain the expected number of rows");
+                assertTrue(countQuery.first());
+                assertEquals(2,
+                        countQuery.getInt(1),
+                        "Table 'quarked_flyway' does not contain the expected number of rows");
             }
         }
         String currentVersion = flyway.info().current().getVersion().toString();
-        assertEquals("1.0.1", currentVersion, "Expected to be 1.0.1 as there is both a SQL and a Java migration script");
+        assertEquals("1.0.2", currentVersion, "Expected to be 1.0.2 as there is a SQL and two Java migration scripts");
     }
 
     public static class V1_0_1__Update extends BaseJavaMigration {
@@ -59,4 +63,39 @@ public class FlywayExtensionCleanAndMigrateAtStartWithJavaMigrationTest {
             }
         }
     }
+
+    public static class V1_0_2__Update implements JavaMigration {
+        @Override
+        public MigrationVersion getVersion() {
+            return MigrationVersion.fromVersion("1.0.2");
+        }
+
+        @Override
+        public String getDescription() {
+            return getClass().getSimpleName();
+        }
+
+        @Override
+        public Integer getChecksum() {
+            return null;
+        }
+
+        @Override
+        public boolean isUndo() {
+            return false;
+        }
+
+        @Override
+        public boolean canExecuteInTransaction() {
+            return true;
+        }
+
+        @Override
+        public void migrate(Context context) throws Exception {
+            try (Statement statement = context.getConnection().createStatement()) {
+                statement.executeUpdate("INSERT INTO quarked_flyway VALUES (1002, 'test')");
+            }
+        }
+    }
+
 }

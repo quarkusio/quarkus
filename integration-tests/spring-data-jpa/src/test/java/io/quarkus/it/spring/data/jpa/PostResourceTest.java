15d84c48405fdb92eeb89e8261df6a5716fcb883
Georgios Andrianakis <geoand@gmail.com>
2020-06-18 21:52:43 +0300

    Allow fields with "and" or "or" in Spring Data JPA repositories
    Fixes: #10100

--- integration-tests/spring-data-jpa/src/test/java/io/quarkus/it/spring/data/jpa/PostResourceTest.java
+++ integration-tests/spring-data-jpa/src/test/java/io/quarkus/it/spring/data/jpa/PostResourceTest.java
@@ -41,6 +41,17 @@ public class PostResourceTest {
     }
 
     @Test
+    void testByByOrganization() {
+        when().get("/post/organization/RH").then()
+                .statusCode(200)
+                .body("size()", is(3));
+
+        when().get("/post/organization/RHT").then()
+                .statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @Test
     void testPostCommentByPostId() {
         when().get("/post/postComment/postId/1").then()
                 .statusCode(200)

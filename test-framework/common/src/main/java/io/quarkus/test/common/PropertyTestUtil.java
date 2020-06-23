395ccad78d2d9e39b07047769420a1f2a52d5992
Georgios Andrianakis <geoand@gmail.com>
2020-06-24 00:09:08 +0300

    Ensure that a set quarkus.log.file.path system prop is used in tests

--- test-framework/common/src/main/java/io/quarkus/test/common/PropertyTestUtil.java
+++ test-framework/common/src/main/java/io/quarkus/test/common/PropertyTestUtil.java
@@ -8,16 +8,18 @@ import io.quarkus.runtime.logging.FileConfig;
 
 public class PropertyTestUtil {
 
+    private static final String LOG_FILE_PATH_PROPERTY = "quarkus.log.file.path";
+
     public static void setLogFileProperty() {
-        System.setProperty("quarkus.log.file.path", getLogFileLocation());
+        System.setProperty(LOG_FILE_PATH_PROPERTY, getLogFileLocation());
     }
 
     public static void setLogFileProperty(String logFileName) {
-        System.setProperty("quarkus.log.file.path", getLogFileLocation(logFileName));
+        System.setProperty(LOG_FILE_PATH_PROPERTY, getLogFileLocation(logFileName));
     }
 
     public static String getLogFileLocation() {
-        return getLogFileLocation(FileConfig.DEFAULT_LOG_FILE_NAME);
+        return getLogFileLocation(System.getProperty(LOG_FILE_PATH_PROPERTY, FileConfig.DEFAULT_LOG_FILE_NAME));
     }
 
     private static String getLogFileLocation(String logFileName) {

5b9b627a51a39014aa5c53e063d8bc0ff59838c5
Sanne Grinovero <sanne@hibernate.org>
2020-07-07 13:37:32 +0100

    Rebasing and adapting PR #3894 contributed by @dcdh

---
+++ integration-tests/hibernate-orm-envers/src/main/java/io/quarkus/it/envers/ProjectApplication.java
@@ -0,0 +1,9 @@
+package io.quarkus.it.envers;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/jpa-envers-test")
+public class ProjectApplication extends Application {
+
+}

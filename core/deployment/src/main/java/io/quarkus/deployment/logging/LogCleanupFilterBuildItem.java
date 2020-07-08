42637b83ad9dcaf8521f9b4ba8e83bc1781bb4a3
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-07-08 11:21:11 +1000

    Filter out Netty DEBUG level exceptions
    We have had various reports about these so this
    drops them to TRACE level.
    
    Fixes #5213

--- core/deployment/src/main/java/io/quarkus/deployment/logging/LogCleanupFilterBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/logging/LogCleanupFilterBuildItem.java
@@ -1,6 +1,7 @@
 package io.quarkus.deployment.logging;
 
 import java.util.Arrays;
+import java.util.logging.Level;
 
 import io.quarkus.builder.item.MultiBuildItem;
 import io.quarkus.runtime.logging.LogCleanupFilterElement;
@@ -19,8 +20,14 @@ public final class LogCleanupFilterBuildItem extends MultiBuildItem {
         if (messageStarts.length == 0) {
             throw new IllegalArgumentException("messageStarts cannot be null");
         }
-
         this.filterElement = new LogCleanupFilterElement(loggerName, Arrays.asList(messageStarts));
+    }
+
+    public LogCleanupFilterBuildItem(String loggerName, Level targetLevel, String... messageStarts) {
+        if (messageStarts.length == 0) {
+            throw new IllegalArgumentException("messageStarts cannot be null");
+        }
+        this.filterElement = new LogCleanupFilterElement(loggerName, targetLevel, Arrays.asList(messageStarts));
     }
 
     public LogCleanupFilterElement getFilterElement() {

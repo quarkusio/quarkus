c1d483ffccaee65289797d44fe3053a8587841b9
Daniel Zwicker <daniel.zwicker@authada.de>
2020-07-08 09:26:33 +0200

    Wait for graalVM version docker container because of posiible race condition

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/NativeImageBuildStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/NativeImageBuildStep.java
@@ -174,6 +174,7 @@ public class NativeImageBuildStep {
             Process versionProcess = new ProcessBuilder(versionCommand.toArray(new String[0]))
                     .redirectErrorStream(true)
                     .start();
+            versionProcess.waitFor();
             try (BufferedReader reader = new BufferedReader(
                     new InputStreamReader(versionProcess.getInputStream(), StandardCharsets.UTF_8))) {
                 graalVMVersion = reader.lines().filter((l) -> l.startsWith("GraalVM Version")).findFirst();

5ccfcbdb7c59ff97b515e0ad4b816190a9a9ae0c
Ивашин Алексей Витальевич <aivashin@bivgroup.com>
2020-07-11 21:10:39 +0300

    Allow for new lines in Panache queries regexp patterns
    Fixes #10617

--- extensions/panache/hibernate-orm-panache-common/runtime/src/test/java/io/quarkus/hibernate/orm/panache/common/runtime/QueryMatcherTest.java
+++ extensions/panache/hibernate-orm-panache-common/runtime/src/test/java/io/quarkus/hibernate/orm/panache/common/runtime/QueryMatcherTest.java
@@ -9,22 +9,28 @@ public class QueryMatcherTest {
 
     @Test
     public void testSelectMatcher() {
-        testMatch("from bar", null);
-        testMatch("select foo from bar", "from bar");
-        testMatch("  select   foo   from   bar  ", "from   bar  ");
-        testMatch("select foo as fff from bar", "from bar");
-        testMatch("select foo,bar from bar", "from bar");
-        testMatch("select foo , bar from bar", "from bar");
-        testMatch("select foo as f,bar as b from bar", "from bar");
-        testMatch("select foo as f , bar as b from bar", "from bar");
-
-        testMatch("select foo.bar.gee as f , bar.sup as b from bar", "from bar");
-
-        testMatch("select distinct foo from bar", "from bar");
-        testMatch("select distinct foo,bar from bar", "from bar");
+        testSelectMatch("from bar", null);
+        testSelectMatch("select foo from bar", "from bar");
+        testSelectMatch("  select   foo   from   bar  ", "from   bar  ");
+        testSelectMatch("select foo as fff from bar", "from bar");
+        testSelectMatch("select foo,bar from bar", "from bar");
+        testSelectMatch("select foo , bar from bar", "from bar");
+        testSelectMatch("select foo as f,bar as b from bar", "from bar");
+        testSelectMatch("select foo as f , bar as b from bar", "from bar");
+        testSelectMatch("select foo.bar.gee as f , bar.sup as b from bar", "from bar");
+        testSelectMatch("select distinct foo from bar \n where field = :value",
+                "from bar \n where field = :value");
+        testSelectMatch("select distinct foo from bar", "from bar");
+        testSelectMatch("select distinct foo,bar from bar", "from bar");
+        testSelectMatch("select foo from bar where field = :value", "from bar where field = :value");
+        testSelectMatch("select foo from bar \n where field = :value",
+                "from bar \n where field = :value");
+        testSelectMatch("select foo from bar \n where field = :value " +
+                "\n\r and fieldTwo :valueTwo \r\n :fieldThree = :valueThree",
+                "from bar \n where field = :value \n\r and fieldTwo :valueTwo \r\n :fieldThree = :valueThree");
     }
 
-    private void testMatch(String select, String result) {
+    private void testSelectMatch(String select, String result) {
         Matcher matcher = CommonPanacheQueryImpl.SELECT_PATTERN.matcher(select);
         if (result != null) {
             Assertions.assertTrue(matcher.matches());
@@ -33,4 +39,25 @@ public class QueryMatcherTest {
             Assertions.assertFalse(matcher.matches());
         }
     }
+
+    @Test
+    public void testFromMatcher() {
+        testFromMatch("from bar");
+        testFromMatch("   from   bar  ");
+        testFromMatch("from bar where field = :value");
+        testFromMatch("from bar where fieldOne = :valueOne and fieldTwo :valueTwo");
+        testFromMatch("from bar where fieldOne = :valueOne \n and fieldTwo :valueTwo");
+        testFromMatch("from bar where fieldOne = :valueOne \n\r and fieldTwo :valueTwo");
+        testFromMatch("from bar where fieldOne = :valueOne \r\n and fieldTwo :valueTwo \n" +
+                "\n :fieldThree = :valueThree");
+        testFromMatch("from \n bar where field = :value");
+        testFromMatch("from \n bar \n\r where \r\n field = :value");
+        testFromMatch("from \n bar b \n\r where \r\n b.field = :value");
+    }
+
+    private void testFromMatch(String query) {
+        Matcher matcher = CommonPanacheQueryImpl.FROM_PATTERN.matcher(query);
+        Assertions.assertTrue(matcher.matches());
+    }
+
 }

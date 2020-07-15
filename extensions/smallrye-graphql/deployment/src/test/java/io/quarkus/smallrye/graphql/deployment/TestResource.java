aec2eb1effbd909caa534427108bc1e1143d90f1
Phillip Kruger <phillip.kruger@gmail.com>
2020-07-15 19:22:40 +0200

    Upgrade MicroProfile GraphQL to 1.0.5
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- extensions/smallrye-graphql/deployment/src/test/java/io/quarkus/smallrye/graphql/deployment/TestResource.java
+++ extensions/smallrye-graphql/deployment/src/test/java/io/quarkus/smallrye/graphql/deployment/TestResource.java
@@ -1,15 +1,25 @@
 package io.quarkus.smallrye.graphql.deployment;
 
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Mutation;
 import org.eclipse.microprofile.graphql.Query;
 import org.eclipse.microprofile.graphql.Source;
+
+import graphql.schema.GraphQLEnumType;
+import graphql.schema.GraphQLSchema;
+import io.smallrye.graphql.api.Context;
 
 /**
  * Just a test endpoint
  */
 @GraphQLApi
 public class TestResource {
+
+    @Inject
+    Context context;
 
     @Query
     public TestPojo ping() {
@@ -26,6 +36,11 @@ public class TestResource {
         return new TestPojo[] { foo() };
     }
 
+    @Query("context")
+    public String getPathFromContext() {
+        return context.getPath();
+    }
+
     @Mutation
     public TestPojo moo(String name) {
         return new TestPojo(name);
@@ -35,5 +50,16 @@ public class TestResource {
 
     public TestRandom getRandomNumber(@Source TestPojo testPojo) {
         return new TestRandom(123);
+    }
+
+    public GraphQLSchema.Builder addMyOwnEnum(@Observes GraphQLSchema.Builder builder) {
+
+        GraphQLEnumType myOwnEnum = GraphQLEnumType.newEnum()
+                .name("SomeEnum")
+                .description("Adding some enum type")
+                .value("value1")
+                .value("value2").build();
+
+        return builder.additionalType(myOwnEnum);
     }
 }

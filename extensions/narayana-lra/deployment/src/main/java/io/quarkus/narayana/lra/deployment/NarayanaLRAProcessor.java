f01df0d8d108a94725ebc0d4944ae7ecd81748ea
Michael Musgrove <mmusgrov@redhat.com>
2020-07-17 18:11:15 +0100

    issue4399 LRA extension - add native mode support

--- extensions/narayana-lra/deployment/src/main/java/io/quarkus/narayana/lra/deployment/NarayanaLRAProcessor.java
+++ extensions/narayana-lra/deployment/src/main/java/io/quarkus/narayana/lra/deployment/NarayanaLRAProcessor.java
@@ -4,18 +4,21 @@ import static io.quarkus.deployment.annotations.ExecutionTime.RUNTIME_INIT;
 
 import javax.inject.Inject;
 
-import io.quarkus.deployment.Capability;
-import io.quarkus.deployment.Feature;
 import org.jboss.jandex.DotName;
+
+import com.arjuna.ats.internal.arjuna.coordinator.CheckedActionFactoryImple;
 
 import io.narayana.lra.client.internal.proxy.nonjaxrs.LRAParticipantRegistry;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.arc.deployment.BeanDefiningAnnotationBuildItem;
+import io.quarkus.deployment.Capability;
+import io.quarkus.deployment.Feature;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.CapabilityBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.narayana.lra.runtime.LRAConfiguration;
 import io.quarkus.narayana.lra.runtime.NarayanaLRAProducers;
 import io.quarkus.narayana.lra.runtime.NarayanaLRARecorder;
@@ -31,10 +34,14 @@ class NarayanaLRAProcessor {
 
     @BuildStep
     @Record(RUNTIME_INIT)
-    public void build(
-            NarayanaLRARecorder recorder, BuildProducer<FeatureBuildItem> feature, LRAConfiguration configuration) {
+    public void build(NarayanaLRARecorder recorder,
+            BuildProducer<FeatureBuildItem> feature,
+            LRAConfiguration configuration,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
         feature.produce(new FeatureBuildItem(Feature.NARAYANA_LRA));
-
+        reflectiveClass.produce(new ReflectiveClassBuildItem(false, false,
+                CheckedActionFactoryImple.class.getName()));
+        //                "org.apache.commons.logging.LogFactory"));
         additionalBeans.produce(new AdditionalBeanBuildItem(NarayanaLRAProducers.class));
 
         recorder.setConfig(configuration);
---
+++ integration-tests/narayana-lra/src/test/java/org/acme/quickstart/lra/LRATestNativeIT.java
@@ -0,0 +1,7 @@
+package org.acme.quickstart.lra;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class LRATestNativeIT extends LRATest {
+}

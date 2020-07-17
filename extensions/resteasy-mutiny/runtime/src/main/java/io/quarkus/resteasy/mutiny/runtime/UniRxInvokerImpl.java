6ff0c0af2f6a324c124ee04095953b00c2c6cb27
Clement Escoffier <clement.escoffier@gmail.com>
2020-07-17 13:49:39 +0200

    Re-subscription must re-trigger the operation.
    When an operation returns a Uni, resubscribing to this Uni must re-execute the underlying operation (except if the user explicitly uses cache()).
    As RestEasy uses CompletionStage behind the scene, this behavior was not working and was always returning the same CompletionStage (the initial one). As a consequence, resubscriptions and retries were not working.
    
    This commit changes how the Unis are created to re-execute the operation on every subscription. Note that it also delays the initial execution until the first subscription.

--- extensions/resteasy-mutiny/runtime/src/main/java/io/quarkus/resteasy/mutiny/runtime/UniRxInvokerImpl.java
+++ extensions/resteasy-mutiny/runtime/src/main/java/io/quarkus/resteasy/mutiny/runtime/UniRxInvokerImpl.java
@@ -1,5 +1,8 @@
 package io.quarkus.resteasy.mutiny.runtime;
 
+import java.util.concurrent.CompletionStage;
+import java.util.function.Supplier;
+
 import javax.ws.rs.client.CompletionStageRxInvoker;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.GenericType;
@@ -7,6 +10,7 @@ import javax.ws.rs.core.Response;
 
 import io.smallrye.mutiny.Uni;
 
+@SuppressWarnings("unchecked")
 public class UniRxInvokerImpl implements UniRxInvoker {
     private final CompletionStageRxInvoker completionStageRxInvoker;
     private final UniProvider UniProvider;
@@ -18,126 +22,251 @@ public class UniRxInvokerImpl implements UniRxInvoker {
 
     @Override
     public Uni<Response> get() {
-        return (Uni<Response>) UniProvider.fromCompletionStage(completionStageRxInvoker.get());
+        return (Uni<Response>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.get();
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> get(Class<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.get(responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.get(responseType);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> get(GenericType<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.get(responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.get(responseType);
+            }
+        });
     }
 
     @Override
     public Uni<Response> put(Entity<?> entity) {
-        return (Uni<Response>) UniProvider.fromCompletionStage(completionStageRxInvoker.put(entity));
+        return (Uni<Response>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.put(entity);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> put(Entity<?> entity, Class<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.put(entity, responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.put(entity, responseType);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> put(Entity<?> entity, GenericType<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.put(entity, responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.put(entity, responseType);
+            }
+        });
     }
 
     @Override
     public Uni<Response> post(Entity<?> entity) {
-        return (Uni<Response>) UniProvider.fromCompletionStage(completionStageRxInvoker.post(entity));
+        return (Uni<Response>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.post(entity);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> post(Entity<?> entity, Class<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.post(entity, responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.post(entity, responseType);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> post(Entity<?> entity, GenericType<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.post(entity, responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.post(entity, responseType);
+            }
+        });
     }
 
     @Override
     public Uni<Response> delete() {
-        return (Uni<Response>) UniProvider.fromCompletionStage(completionStageRxInvoker.delete());
+        return (Uni<Response>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.delete();
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> delete(Class<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.delete(responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.delete(responseType);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> delete(GenericType<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.delete(responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.delete(responseType);
+            }
+        });
     }
 
     @Override
     public Uni<Response> head() {
-        return (Uni<Response>) UniProvider.fromCompletionStage(completionStageRxInvoker.head());
+        return (Uni<Response>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.head();
+            }
+        });
     }
 
     @Override
     public Uni<Response> options() {
-        return (Uni<Response>) UniProvider.fromCompletionStage(completionStageRxInvoker.options());
+        return (Uni<Response>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.options();
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> options(Class<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.options(responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.options(responseType);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> options(GenericType<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.options(responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.options(responseType);
+            }
+        });
     }
 
     @Override
     public Uni<Response> trace() {
-        return (Uni<Response>) UniProvider.fromCompletionStage(completionStageRxInvoker.trace());
+        return (Uni<Response>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.trace();
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> trace(Class<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.trace(responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.trace(responseType);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> trace(GenericType<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.trace(responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.trace(responseType);
+            }
+        });
     }
 
     @Override
     public Uni<Response> method(String name) {
-        return (Uni<Response>) UniProvider.fromCompletionStage(completionStageRxInvoker.method(name));
+        return (Uni<Response>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.method(name);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> method(String name, Class<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.method(name, responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.method(name, responseType);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> method(String name, GenericType<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.method(name, responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.method(name, responseType);
+            }
+        });
     }
 
     @Override
     public Uni<Response> method(String name, Entity<?> entity) {
-        return (Uni<Response>) UniProvider.fromCompletionStage(completionStageRxInvoker.method(name, entity));
+        return (Uni<Response>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.method(name, entity);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> method(String name, Entity<?> entity, Class<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.method(name, entity, responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.method(name, entity, responseType);
+            }
+        });
     }
 
     @Override
     public <T> Uni<T> method(String name, Entity<?> entity, GenericType<T> responseType) {
-        return (Uni<T>) UniProvider.fromCompletionStage(completionStageRxInvoker.method(name, entity, responseType));
+        return (Uni<T>) UniProvider.fromCompletionStage(new Supplier<CompletionStage<?>>() {
+            @Override
+            public CompletionStage<?> get() {
+                return completionStageRxInvoker.method(name, entity, responseType);
+            }
+        });
     }
 }

9819d8fb6a5fa7a4abf22595b49a7a050b43a6c9
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-08-05 14:10:45 +1000

    Add more header delegates

---
+++ extensions/qrs/runtime/src/main/java/io/quarkus/qrs/runtime/headers/LocaleDelegate.java
@@ -0,0 +1,27 @@
+package io.quarkus.qrs.runtime.headers;
+
+import java.util.Locale;
+
+import javax.ws.rs.ext.RuntimeDelegate;
+
+import io.quarkus.qrs.runtime.util.LocaleHelper;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ */
+public class LocaleDelegate implements RuntimeDelegate.HeaderDelegate<Locale> {
+    public static final LocaleDelegate INSTANCE = new LocaleDelegate();
+
+    public Locale fromString(String value) throws IllegalArgumentException {
+        if (value == null)
+            throw new IllegalArgumentException("param was null");
+        return LocaleHelper.extractLocale(value);
+    }
+
+    public String toString(Locale value) {
+        if (value == null)
+            throw new IllegalArgumentException("param was null");
+        return LocaleHelper.toLanguageString(value);
+    }
+
+}
--- extensions/qrs/runtime/src/main/java/io/quarkus/qrs/runtime/jaxrs/QrsRuntimeDelegate.java
+++ extensions/qrs/runtime/src/main/java/io/quarkus/qrs/runtime/jaxrs/QrsRuntimeDelegate.java
@@ -1,13 +1,27 @@
 package io.quarkus.qrs.runtime.jaxrs;
 
+import java.util.Date;
+import java.util.Locale;
+
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.Link;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.Variant;
 import javax.ws.rs.ext.RuntimeDelegate;
 
+import io.quarkus.qrs.runtime.headers.CacheControlDelegate;
+import io.quarkus.qrs.runtime.headers.CookieHeaderDelegate;
+import io.quarkus.qrs.runtime.headers.DateDelegate;
+import io.quarkus.qrs.runtime.headers.EntityTagDelegate;
+import io.quarkus.qrs.runtime.headers.LocaleDelegate;
 import io.quarkus.qrs.runtime.headers.MediaTypeHeaderDelegate;
+import io.quarkus.qrs.runtime.headers.NewCookieHeaderDelegate;
 
 public class QrsRuntimeDelegate extends RuntimeDelegate {
 
@@ -34,7 +48,22 @@ public class QrsRuntimeDelegate extends RuntimeDelegate {
 
     @Override
     public <T> HeaderDelegate<T> createHeaderDelegate(Class<T> type) throws IllegalArgumentException {
-        return new MediaTypeHeaderDelegate<T>();
+        if (type.equals(MediaType.class)) {
+            return new MediaTypeHeaderDelegate<T>();
+        } else if (type.equals(Date.class)) {
+            return (HeaderDelegate<T>) new DateDelegate();
+        } else if (type.equals(CacheControl.class)) {
+            return (HeaderDelegate<T>) new CacheControlDelegate();
+        } else if (type.equals(NewCookie.class)) {
+            return (HeaderDelegate<T>) new NewCookieHeaderDelegate();
+        } else if (type.equals(Cookie.class)) {
+            return (HeaderDelegate<T>) new CookieHeaderDelegate();
+        } else if (type.equals(EntityTag.class)) {
+            return (HeaderDelegate<T>) new EntityTagDelegate();
+        } else if (type.equals(Locale.class)) {
+            return (HeaderDelegate<T>) new LocaleDelegate();
+        }
+        throw new IllegalArgumentException();
     }
 
     @Override

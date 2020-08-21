4adcd9a4f681e220852a1fdf8aa310a34bfcf210
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-08-21 15:11:16 +1000

    Don't fully parse accept header

--- extensions/qrs/runtime/src/main/java/io/quarkus/qrs/runtime/headers/FixedProducesHandler.java
+++ extensions/qrs/runtime/src/main/java/io/quarkus/qrs/runtime/headers/FixedProducesHandler.java
@@ -1,8 +1,5 @@
 package io.quarkus.qrs.runtime.headers;
 
-import java.util.Collections;
-import java.util.List;
-
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,7 +9,6 @@ import io.netty.handler.codec.http.HttpHeaderNames;
 import io.quarkus.qrs.runtime.core.QrsRequestContext;
 import io.quarkus.qrs.runtime.core.serialization.EntityWriter;
 import io.quarkus.qrs.runtime.handlers.RestHandler;
-import io.quarkus.qrs.runtime.util.MediaTypeHelper;
 
 /**
  * Handler that negotiates the content type for endpoints that
@@ -21,13 +17,15 @@ import io.quarkus.qrs.runtime.util.MediaTypeHelper;
 public class FixedProducesHandler implements RestHandler {
 
     final MediaType mediaType;
+    final String mediaTypeString;
+    final String mediaTypeSubstring;
     final EntityWriter writer;
-    final List<MediaType> mediaTypeList;
 
     public FixedProducesHandler(MediaType mediaType, EntityWriter writer) {
         this.mediaType = mediaType;
         this.writer = writer;
-        this.mediaTypeList = Collections.singletonList(mediaType);
+        this.mediaTypeString = mediaType.getType() + "/" + mediaType.getSubtype();
+        this.mediaTypeSubstring = mediaType.getType() + "/*";
     }
 
     @Override
@@ -38,13 +36,12 @@ public class FixedProducesHandler implements RestHandler {
             requestContext.setEntityWriter(writer);
         } else {
             //TODO: this needs to be optimised
-            List<MediaType> parsed = MediaTypeHelper.parseHeader(accept);
-            MediaType res = MediaTypeHelper.getBestMatch(parsed, mediaTypeList);
-            if (res == null) {
+            if (accept.contains(mediaTypeString) || accept.contains("*/*") || accept.contains(mediaTypeSubstring)) {
+                requestContext.setProducesMediaType(mediaType);
+                requestContext.setEntityWriter(writer);
+            } else {
                 throw new WebApplicationException(Response.notAcceptable(Variant.mediaTypes(mediaType).build()).build());
             }
-            requestContext.setProducesMediaType(res);
-            requestContext.setEntityWriter(writer);
         }
     }
 }

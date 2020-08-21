ddde44af225122442f56344e36ddc027365d641b
Guillaume Smet <guillaume.smet@gmail.com>
2020-08-21 12:44:53 +0200

    Only throw an error if two different storage engines are defined
    The storage engine is only used by MySQL/MariaDB so a setup with a MySQL
    PU using a storage engine and a PostgreSQL PU not using any is perfectly
    valid.

--- extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/HibernateOrmProcessor.java
+++ extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/HibernateOrmProcessor.java
@@ -552,7 +552,7 @@ public final class HibernateOrmProcessor {
                 .filter(i -> i.isDefault())
                 .findFirst();
 
-        Set<Optional<String>> storageEngines = new HashSet<>();
+        Set<String> storageEngines = new HashSet<>();
 
         if ((defaultJdbcDataSource.isPresent() && hibernateOrmConfig.persistenceUnits.isEmpty()) ||
                 hibernateOrmConfig.defaultPersistenceUnit.isAnyPropertySet()) {
@@ -564,7 +564,9 @@ public final class HibernateOrmProcessor {
                     jdbcDataSources, applicationArchivesBuildItem, launchMode,
                     systemProperties, nativeImageResources, hotDeploymentWatchedFiles, persistenceUnitDescriptors);
 
-            storageEngines.add(hibernateOrmConfig.defaultPersistenceUnit.dialect.storageEngine);
+            if (hibernateOrmConfig.defaultPersistenceUnit.dialect.storageEngine.isPresent()) {
+                storageEngines.add(hibernateOrmConfig.defaultPersistenceUnit.dialect.storageEngine.get());
+            }
         }
 
         for (Entry<String, HibernateOrmConfigPersistenceUnit> persistenceUnitEntry : hibernateOrmConfig.persistenceUnits
@@ -575,7 +577,9 @@ public final class HibernateOrmProcessor {
                     jdbcDataSources, applicationArchivesBuildItem, launchMode,
                     systemProperties, nativeImageResources, hotDeploymentWatchedFiles, persistenceUnitDescriptors);
 
-            storageEngines.add(persistenceUnitEntry.getValue().dialect.storageEngine);
+            if (persistenceUnitEntry.getValue().dialect.storageEngine.isPresent()) {
+                storageEngines.add(persistenceUnitEntry.getValue().dialect.storageEngine.get());
+            }
         }
 
         if (storageEngines.size() > 1) {

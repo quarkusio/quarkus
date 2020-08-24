cf9d122483b4d9868311bb2a2a013b378f5518d4
Georgios Andrianakis <geoand@gmail.com>
2020-08-24 16:41:03 +0300

    Don't create default mongo clients when not needed
    Relates to: #11491
    
    (cherry picked from commit 036d27fe9037f30f4d4ad004d770b6cafdd18a2c)

--- extensions/mongodb-client/deployment/src/test/java/io/quarkus/mongodb/NamedReactiveMongoClientConfigTest.java
+++ extensions/mongodb-client/deployment/src/test/java/io/quarkus/mongodb/NamedReactiveMongoClientConfigTest.java
@@ -2,14 +2,21 @@ package io.quarkus.mongodb;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.annotation.Annotation;
+
+import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.InjectableBean;
+import io.quarkus.arc.InstanceHandle;
 import io.quarkus.mongodb.reactive.ReactiveMongoClient;
 import io.quarkus.mongodb.runtime.MongoClientName;
 import io.quarkus.test.QuarkusUnitTest;
@@ -43,5 +50,21 @@ public class NamedReactiveMongoClientConfigTest extends MongoWithReplicasTestBas
     public void testNamedDataSourceInjection() {
         assertThat(client.listDatabases().collectItems().first().await().indefinitely()).isNotEmpty();
         assertThat(client2.listDatabases().collectItems().first().await().indefinitely()).isNotEmpty();
+
+        assertNoDefaultClient();
+    }
+
+    private void assertNoDefaultClient() {
+        boolean hasDefault = false;
+        for (InstanceHandle<ReactiveMongoClient> handle : Arc.container().select(ReactiveMongoClient.class).handles()) {
+            InjectableBean<ReactiveMongoClient> bean = handle.getBean();
+            for (Annotation qualifier : bean.getQualifiers()) {
+                if (qualifier.annotationType().equals(Default.class)) {
+                    hasDefault = true;
+                }
+            }
+        }
+        Assertions.assertFalse(hasDefault,
+                "The default reactive mongo client should not have been present as it is not used in any injection point");
     }
 }

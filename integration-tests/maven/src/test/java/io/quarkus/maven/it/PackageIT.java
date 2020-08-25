072da09ab6bcc13596eacd4e52c2ebd8ad7c01d6
Alexey Loubyansky <olubyans@redhat.com>
2020-08-25 18:17:58 +0200

    1.7.0.Final quarkus.package.output-directory causes NPE running tests

--- integration-tests/maven/src/test/java/io/quarkus/maven/it/PackageIT.java
+++ integration-tests/maven/src/test/java/io/quarkus/maven/it/PackageIT.java
@@ -64,6 +64,28 @@ public class PackageIT extends MojoTestBase {
     }
 
     @Test
+    public void testQuarkusPackageOutputDirectory()
+            throws MavenInvocationException, IOException, InterruptedException {
+        testDir = initProject("projects/quarkus.package.output-directory");
+
+        running = new RunningInvoker(testDir, false);
+        // we do want to run the tests too
+        final MavenProcessInvocationResult result = running.execute(Collections.singletonList("package"),
+                Collections.emptyMap());
+
+        assertThat(result.getProcess().waitFor()).isEqualTo(0);
+
+        File targetDir = getTargetDir();
+        List<File> jars = getFilesEndingWith(targetDir, ".jar");
+        assertThat(jars).hasSize(1);
+
+        targetDir = new File(targetDir, "custom-output-dir");
+        assertThat(targetDir).exists();
+        jars = getFilesEndingWith(targetDir, ".jar");
+        assertThat(jars).hasSize(1);
+    }
+
+    @Test
     public void testPackageWorksWhenUberjarIsTrue()
             throws MavenInvocationException, IOException, InterruptedException {
         testDir = initProject("projects/uberjar-check", "projects/project-uberjar-true");

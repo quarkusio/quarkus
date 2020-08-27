4f9dc06f2ba81f13812f3a6f0ea836540831d8e7
Ioannis Canellos <iocanel@gmail.com>
2020-08-27 13:35:33 +0300

    feat: Allow passing decorators via kubernetes spi

--- extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/KubernetesProcessor.java
+++ extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/KubernetesProcessor.java
@@ -97,6 +97,7 @@ import io.dekorate.kubernetes.decorator.ApplyCommandDecorator;
 import io.dekorate.kubernetes.decorator.ApplyImagePullPolicyDecorator;
 import io.dekorate.kubernetes.decorator.ApplyServiceAccountNamedDecorator;
 import io.dekorate.kubernetes.decorator.ApplyWorkingDirDecorator;
+import io.dekorate.kubernetes.decorator.Decorator;
 import io.dekorate.kubernetes.decorator.RemoveAnnotationDecorator;
 import io.dekorate.kubernetes.decorator.RemoveFromMatchingLabelsDecorator;
 import io.dekorate.kubernetes.decorator.RemoveFromSelectorDecorator;
@@ -115,6 +116,7 @@ import io.dekorate.s2i.decorator.AddBuilderImageStreamResourceDecorator;
 import io.dekorate.utils.Annotations;
 import io.dekorate.utils.Labels;
 import io.dekorate.utils.Maps;
+import io.dekorate.utils.Strings;
 import io.quarkus.container.image.deployment.util.ImageUtil;
 import io.quarkus.container.spi.BaseImageInfoBuildItem;
 import io.quarkus.container.spi.ContainerImageInfoBuildItem;
@@ -132,6 +134,7 @@ import io.quarkus.deployment.pkg.PackageConfig;
 import io.quarkus.deployment.pkg.builditem.OutputTargetBuildItem;
 import io.quarkus.deployment.util.FileUtil;
 import io.quarkus.kubernetes.deployment.Annotations.Prometheus;
+import io.quarkus.kubernetes.spi.DecoratorBuildItem;
 import io.quarkus.kubernetes.spi.KubernetesAnnotationBuildItem;
 import io.quarkus.kubernetes.spi.KubernetesCommandBuildItem;
 import io.quarkus.kubernetes.spi.KubernetesDeploymentTargetBuildItem;
@@ -282,6 +285,7 @@ class KubernetesProcessor {
             List<KubernetesRoleBindingBuildItem> kubernetesRoleBindings,
             List<KubernetesPortBuildItem> kubernetesPorts,
             EnabledKubernetesDeploymentTargetsBuildItem kubernetesDeploymentTargets,
+            List<DecoratorBuildItem> decorators,
             Optional<BaseImageInfoBuildItem> baseImage,
             Optional<ContainerImageInfoBuildItem> containerImage,
             Optional<KubernetesCommandBuildItem> command,
@@ -378,6 +382,16 @@ class KubernetesProcessor {
                     command,
                     kubernetesHealthLivenessPath,
                     kubernetesHealthReadinessPath);
+
+            decorators.stream().filter(d -> d.matches(Decorator.class)).forEach(i -> {
+                String group = i.getGroup();
+                Decorator decorator = (Decorator) i.getDecorator();
+                if (Strings.isNullOrEmpty(group)) {
+                    session.resources().decorate(decorator);
+                } else {
+                    session.resources().decorate(group, decorator);
+                }
+            });
 
             // write the generated resources to the filesystem
             generatedResourcesMap = session.close();

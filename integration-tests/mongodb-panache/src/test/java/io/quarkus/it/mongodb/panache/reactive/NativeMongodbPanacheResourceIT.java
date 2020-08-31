5171432658aae409c78c06fc6a2f688417c4db50
Georgios Andrianakis <geoand@gmail.com>
2020-08-31 17:25:13 +0300

    Also disable native versions of mongo tests

--- integration-tests/mongodb-panache/src/test/java/io/quarkus/it/mongodb/panache/reactive/NativeMongodbPanacheResourceIT.java
+++ integration-tests/mongodb-panache/src/test/java/io/quarkus/it/mongodb/panache/reactive/NativeMongodbPanacheResourceIT.java
@@ -1,8 +1,11 @@
 package io.quarkus.it.mongodb.panache.reactive;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
+@Disabled("See https://github.com/quarkusio/quarkus/issues/11711")
 class NativeMongodbPanacheResourceIT extends ReactiveMongodbPanacheResourceTest {
 
 }

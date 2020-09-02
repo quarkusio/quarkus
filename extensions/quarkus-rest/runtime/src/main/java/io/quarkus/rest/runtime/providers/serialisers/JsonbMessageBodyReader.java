20de6ecbd19f0ce2e88762ab7d03d17a8d237b82
Georgios Andrianakis <geoand@gmail.com>
2020-09-02 17:47:16 +0300

    Apply minor improvements to Jsonb reader/writer
    * Use Jsonb from CDI if available
    * Declare media types

--- extensions/quarkus-rest/runtime/src/main/java/io/quarkus/rest/runtime/providers/serialisers/JsonbMessageBodyReader.java
+++ extensions/quarkus-rest/runtime/src/main/java/io/quarkus/rest/runtime/providers/serialisers/JsonbMessageBodyReader.java
@@ -7,16 +7,30 @@ import java.lang.reflect.Type;
 
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.Provider;
 
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.InstanceHandle;
+
 @Provider
+@Consumes({ "application/json", "application/*+json", "text/json" })
 public class JsonbMessageBodyReader implements MessageBodyReader<Object> {
 
-    Jsonb json = JsonbBuilder.create();
+    private final Jsonb json;
+
+    public JsonbMessageBodyReader() {
+        InstanceHandle<Jsonb> jsonbInstanceHandle = Arc.container().instance(Jsonb.class);
+        if (jsonbInstanceHandle.isAvailable()) {
+            this.json = jsonbInstanceHandle.get();
+        } else {
+            this.json = JsonbBuilder.create();
+        }
+    }
 
     @Override
     public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
--- extensions/quarkus-rest/runtime/src/main/java/io/quarkus/rest/runtime/providers/serialisers/JsonbMessageBodyWriter.java
+++ extensions/quarkus-rest/runtime/src/main/java/io/quarkus/rest/runtime/providers/serialisers/JsonbMessageBodyWriter.java
@@ -7,17 +7,31 @@ import java.lang.reflect.Type;
 
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.InstanceHandle;
+
 // NOTE: currently disabled to benchmark the vertx one
 @Provider
+@Produces({ "application/json", "application/*+json", "text/json" })
 public class JsonbMessageBodyWriter implements MessageBodyWriter<Object> {
 
-    Jsonb json = JsonbBuilder.create();
+    private final Jsonb json;
+
+    public JsonbMessageBodyWriter() {
+        InstanceHandle<Jsonb> jsonbInstanceHandle = Arc.container().instance(Jsonb.class);
+        if (jsonbInstanceHandle.isAvailable()) {
+            this.json = jsonbInstanceHandle.get();
+        } else {
+            this.json = JsonbBuilder.create();
+        }
+    }
 
     @Override
     public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {

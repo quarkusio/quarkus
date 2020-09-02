c5e7af302c7dcc9fca33a71af6d1fbe6ae9634ea
Georgios Andrianakis <geoand@gmail.com>
2020-09-02 09:46:20 +0300

    Add support for doing injecting HttpServerRequest with @Context

--- extensions/quarkus-rest/runtime/src/main/java/io/quarkus/rest/runtime/injection/ContextProducers.java
+++ extensions/quarkus-rest/runtime/src/main/java/io/quarkus/rest/runtime/injection/ContextProducers.java
@@ -11,7 +11,13 @@ import javax.ws.rs.sse.Sse;
 import io.quarkus.rest.runtime.core.QuarkusRestRequestContext;
 import io.quarkus.rest.runtime.jaxrs.QuarkusRestSse;
 import io.quarkus.vertx.http.runtime.CurrentVertxRequest;
+import io.vertx.core.http.HttpServerRequest;
 
+/**
+ * Provides CDI producers for objects that can be injected via @Context
+ * In quarkus-rest this works because @Context is considered an alias for @Inject
+ * through the use of {@code AutoInjectAnnotationBuildItem}
+ */
 @Singleton
 public class ContextProducers {
 
@@ -34,6 +40,15 @@ public class ContextProducers {
     @Produces
     Sse sse() {
         return QuarkusRestSse.INSTANCE;
+    }
+
+    // HttpServerRequest is a Vert.x type so it's not necessary to have it injectable via @Context,
+    // however we do use it in the Quickstarts so let's make it work
+
+    @RequestScoped
+    @Produces
+    HttpServerRequest httpServerRequest() {
+        return currentVertxRequest.getCurrent().request();
     }
 
     private QuarkusRestRequestContext getContext() {

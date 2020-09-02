96a0d579ab3509b83c38e32d26034ff7a8cb9f42
Alexey Loubyansky <olubyans@redhat.com>
2020-09-02 17:45:17 +0200

    Default platform version range should be [major.minor-alpha,major.minor+1]

--- devtools/maven/src/main/java/io/quarkus/maven/CreateUtils.java
+++ devtools/maven/src/main/java/io/quarkus/maven/CreateUtils.java
@@ -85,9 +85,8 @@ public final class CreateUtils {
                 DefaultArtifactVersion pluginVersion = new DefaultArtifactVersion(baseVersion);
                 int majorVer = pluginVersion.getMajorVersion();
                 int minorVer = pluginVersion.getMinorVersion();
-                version = "[" + majorVer + "." + minorVer + "-snapshot, " + majorVer + "." + (minorVer + 1) + ")";
+                version = "[" + majorVer + "." + minorVer + "-alpha, " + majorVer + "." + (minorVer + 1) + ")";
             }
-
         }
 
         final QuarkusPlatformDescriptor platform;

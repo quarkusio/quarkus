b7c9c4dcc288c76786ea3bf2a243529f8e8fb340
Ken Finnigan <ken@kenfinnigan.me>
2020-09-04 11:49:51 -0400

    Re-enable tests that should now work

--- integration-tests/mongodb-panache-kotlin/src/test/kotlin/io/quarkus/it/mongodb/panache/reactive/NativeReactiveMongodbPanacheResourceIT.kt
+++ integration-tests/mongodb-panache-kotlin/src/test/kotlin/io/quarkus/it/mongodb/panache/reactive/NativeReactiveMongodbPanacheResourceIT.kt
@@ -1,8 +1,6 @@
 package io.quarkus.it.mongodb.panache.reactive
 
 import io.quarkus.test.junit.NativeImageTest
-import org.junit.jupiter.api.Disabled
 
 @NativeImageTest
-@Disabled("See https://github.com/quarkusio/quarkus/issues/11711")
 internal class NativeReactiveMongodbPanacheResourceIT : ReactiveMongodbPanacheResourceTest()

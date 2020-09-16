b4852888bc4743e4845203cccbaa4cdc929b52e5
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2020-09-16 15:18:09 +0200

    Reproducer for enum pattern issue with ARC encountered in OptaPlanner extension

--- independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/injection/assignability/generics/AssignabilityWithGenericsTest.java
+++ independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/injection/assignability/generics/AssignabilityWithGenericsTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.quarkus.arc.Arc;
+import io.quarkus.arc.DefaultBean;
 import io.quarkus.arc.InstanceHandle;
 import io.quarkus.arc.test.ArcTestContainer;
 import java.util.ArrayList;
@@ -18,6 +19,8 @@ import javax.enterprise.event.ObservesAsync;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.util.TypeLiteral;
 import javax.inject.Inject;
+import javax.inject.Singleton;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -29,7 +32,11 @@ public class AssignabilityWithGenericsTest {
             StringListConsumer.class, ListConsumer.class, ProducerBean.class, DefinitelyNotBar.class,
             Bar.class, GenericInterface.class, AlmostCompleteBean.class, ActualBean.class,
             BetaFace.class, GammaFace.class, GammaImpl.class, AbstractAlpha.class, AlphaImpl.class,
-            BeanInjectingActualType.class, FooTyped.class);
+            BeanInjectingActualType.class, FooTyped.class,
+            ScoreManager1.class, MyScore1.class, ScoreManagerBeanProvider1.class,
+            ScoreManager2.class, Score2.class, MyScore2.class, ScoreManagerBeanProvider2.class
+//            , ScoreManager3.class, Score3.class, MyScore3.class, ScoreManagerBeanProvider3.class
+    );
 
     @Test
     public void testSelectingInstanceOfCar() {
@@ -188,5 +195,84 @@ public class AssignabilityWithGenericsTest {
     static class AlphaImpl extends AbstractAlpha<GammaFace> {
 
     }
+
+
+
+
+    // This succeeds
+
+    @Test
+    public void testOptaPlanner_object() {
+        InstanceHandle<ScoreManager1<MyScore1>> instance = Arc.container().instance(
+                new TypeLiteral<ScoreManager1<MyScore1>>() {});
+        assertTrue(instance.isAvailable());
+    }
+
+    static class ScoreManager1<S> {}
+
+    static class MyScore1 {}
+
+    static class ScoreManagerBeanProvider1 {
+
+        @DefaultBean
+        @Singleton
+        @Produces
+        // SUCCESS: This matches with ScoreManager1<MySolution>
+        <S> ScoreManager1<S> scoreManager() {
+            return new ScoreManager1<>();
+        }
+    }
+
+    // This succeeds
+
+    @Test
+    public void testOptaPlanner_score_untyped() {
+        InstanceHandle<ScoreManager2<MyScore2>> instance = Arc.container().instance(
+                new TypeLiteral<ScoreManager2<MyScore2>>() {});
+        assertTrue(instance.isAvailable());
+    }
+
+    static class ScoreManager2<S extends Score2> {}
+
+    interface Score2 {}
+
+    static class MyScore2 implements Score2 {}
+
+    static class ScoreManagerBeanProvider2 {
+
+        @DefaultBean
+        @Singleton
+        @Produces
+        // SUCCESS: This matches with ScoreManager2<MyScore2>
+        <S extends Score2> ScoreManager2<S> scoreManager() {
+            return new ScoreManager2<>();
+        }
+    }
+
+    // This fails
+
+//    @Test
+//    public void testOptaPlanner_score_typed_too() {
+//        InstanceHandle<ScoreManager3<MyScore3>> instance = Arc.container().instance(
+//                new TypeLiteral<ScoreManager3<MyScore3>>() {});
+//        assertTrue(instance.isAvailable());
+//    }
+//
+//    static class ScoreManager3<S extends Score3<S>> {}
+//
+//    interface Score3<S extends Score3<S>> {}
+//
+//    static class MyScore3 implements Score3<MyScore3> {}
+//
+//    static class ScoreManagerBeanProvider3 {
+//
+//        @DefaultBean
+//        @Singleton
+//        @Produces
+//        // FAILURE (BUG): This should match with ScoreManager3<MyScore3>
+//        <S extends Score3<S>> ScoreManager3<S> scoreManager() {
+//            return new ScoreManager3<>();
+//        }
+//    }
 
 }

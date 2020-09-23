6ad6ec9b4a7cee7147321e075441e6cab8d25274
Roberto Cortez <radcortez@yahoo.com>
2020-09-23 17:05:47 +0100

    MP Config 2.0 Support.

---
+++ extensions/arc/deployment/src/test/java/io/quarkus/arc/test/config/NullConverterTest.java
@@ -0,0 +1,49 @@
+package io.quarkus.arc.test.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.spi.DeploymentException;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.config.spi.Converter;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class NullConverterTest {
+    @RegisterExtension
+    static final QuarkusUnitTest TEST = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClass(NullConverterBean.class)
+                    .addAsServiceProvider(Converter.class, CustomTypeConverter.class)
+                    .addAsResource(new StringAsset("my.prop=1234\n"), "application.properties"))
+            .setExpectedException(DeploymentException.class);
+
+    @Test
+    void nullProperty() {
+
+    }
+
+    @ApplicationScoped
+    static class NullConverterBean {
+        @Inject
+        @ConfigProperty(name = "my.prop", defaultValue = "1234")
+        CustomType customType;
+    }
+
+    static class CustomType {
+
+    }
+
+    public static class CustomTypeConverter implements Converter<CustomTypeConverter> {
+        @Override
+        public CustomTypeConverter convert(final String value)
+                throws IllegalArgumentException, NullPointerException {
+            return null;
+        }
+    }
+}
--- integration-tests/smallrye-config/src/main/java/io/quarkus/it/smallrye/config/ServerResource.java
+++ integration-tests/smallrye-config/src/main/java/io/quarkus/it/smallrye/config/ServerResource.java
@@ -5,13 +5,24 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+
 @Path("/server")
 public class ServerResource {
     @Inject
     Server server;
+    @Inject
+    @ConfigProperties
+    ServerProperties serverProperties;
 
     @GET
     public Response getServer() {
         return Response.ok(server).build();
+    }
+
+    @GET
+    @Path("/properties")
+    public Response getServerProperties() {
+        return Response.ok(serverProperties).build();
     }
 }

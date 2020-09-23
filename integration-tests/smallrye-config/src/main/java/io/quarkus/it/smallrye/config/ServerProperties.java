4fd893262a124377597b189110e1b42f90bae699
Roberto Cortez <radcortez@yahoo.com>
2020-09-23 17:05:47 +0100

    MP Config 2.0 Support.

---
+++ integration-tests/smallrye-config/src/main/java/io/quarkus/it/smallrye/config/ServerProperties.java
@@ -0,0 +1,28 @@
+package io.quarkus.it.smallrye.config;
+
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+@ConfigProperties(prefix = "server")
+public class ServerProperties {
+    private String host;
+    private int port;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(final String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(final int port) {
+        this.port = port;
+    }
+}

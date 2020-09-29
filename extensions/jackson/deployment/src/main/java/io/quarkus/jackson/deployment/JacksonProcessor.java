753a6e1b66deae4ef6c319e5bb7e3b792bcc203d
Georgios Andrianakis <geoand@gmail.com>
2020-09-29 08:33:51 +0300

    Fix reflection registration issue with Jackson's @JsonSerialize
    Fixes: #12345

--- extensions/jackson/deployment/src/main/java/io/quarkus/jackson/deployment/JacksonProcessor.java
+++ extensions/jackson/deployment/src/main/java/io/quarkus/jackson/deployment/JacksonProcessor.java
@@ -1,7 +1,6 @@
 package io.quarkus.jackson.deployment;
 
 import static org.jboss.jandex.AnnotationTarget.Kind.CLASS;
-import static org.jboss.jandex.AnnotationTarget.Kind.FIELD;
 import static org.jboss.jandex.AnnotationTarget.Kind.METHOD;
 
 import java.util.Arrays;
@@ -124,13 +123,10 @@ public class JacksonProcessor {
 
         // handle the various @JsonSerialize cases
         for (AnnotationInstance serializeInstance : index.getAnnotations(JSON_SERIALIZE)) {
-            AnnotationTarget annotationTarget = serializeInstance.target();
-            if (FIELD.equals(annotationTarget.kind()) || METHOD.equals(annotationTarget.kind())) {
-                AnnotationValue usingValue = serializeInstance.value("using");
-                if (usingValue != null) {
-                    // the Deserializers are constructed internally by Jackson using a no-args constructor
-                    reflectiveClass.produce(new ReflectiveClassBuildItem(false, false, usingValue.asClass().name().toString()));
-                }
+            AnnotationValue usingValue = serializeInstance.value("using");
+            if (usingValue != null) {
+                // the Serializers are constructed internally by Jackson using a no-args constructor
+                reflectiveClass.produce(new ReflectiveClassBuildItem(false, false, usingValue.asClass().name().toString()));
             }
         }
 

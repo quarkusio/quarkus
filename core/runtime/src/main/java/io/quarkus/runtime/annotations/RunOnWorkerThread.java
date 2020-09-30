92e6fe1902b8c0454640b5879ea6464e31c77a25
Clement Escoffier <clement.escoffier@gmail.com>
2020-09-30 10:32:37 +0200

    Define an annotation indicating that the invocation/processing must be done on a worker thread and not on the I/O thread

---
+++ core/runtime/src/main/java/io/quarkus/runtime/annotations/RunOnWorkerThread.java
@@ -0,0 +1,24 @@
+package io.quarkus.runtime.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+/**
+ * Annotation used to indicate that the annotated element should not be executed on the IO thread but on
+ * a worker thread.
+ * This annotation is used to allow <em>blocking</em> processing in a reactive application.
+ * For example, use this annotation if your reactive route or gRPC service need to use blocking APIs such
+ * as <em>blocking</em> database access.
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ TYPE, FIELD, METHOD })
+public @interface RunOnWorkerThread {
+}

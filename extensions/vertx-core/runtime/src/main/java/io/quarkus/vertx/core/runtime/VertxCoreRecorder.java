dea5539e88092e87328f9b707f441080c832680b
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-10-27 12:29:26 +1100

    Improve pom.xml changes in remote dev mode
    Fixes #12497

--- extensions/vertx-core/runtime/src/main/java/io/quarkus/vertx/core/runtime/VertxCoreRecorder.java
+++ extensions/vertx-core/runtime/src/main/java/io/quarkus/vertx/core/runtime/VertxCoreRecorder.java
@@ -9,6 +9,7 @@ import static io.vertx.core.file.impl.FileResolver.CACHE_DIR_BASE_PROP_NAME;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -372,6 +373,11 @@ public class VertxCoreRecorder {
                 return threads;
             }
         };
+    }
+
+    public static Supplier<Vertx> recoverFailedStart(VertxConfiguration config) {
+        return vertx = new VertxSupplier(config, Collections.emptyList());
+
     }
 
     static class VertxSupplier implements Supplier<Vertx> {

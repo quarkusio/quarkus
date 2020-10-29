d4c5fc6a7b54279f5dc6b1c00c28540a0927e618
Roberto Cortez <radcortez@yahoo.com>
2020-10-29 00:49:02 +0000

    Add native service providers for SR Config.

---
+++ integration-tests/smallrye-config/src/test/java/io/quarkus/it/smallrye/config/ConfigurableSourceTest.java
@@ -0,0 +1,21 @@
+package io.quarkus.it.smallrye.config;
+
+import static io.restassured.RestAssured.given;
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class ConfigurableSourceTest {
+    @Test
+    void configurableSource() {
+        given()
+                .get("/config/{name}", "database.user.naruto")
+                .then()
+                .statusCode(OK.getStatusCode())
+                .body("value", equalTo("uzumaki"));
+    }
+}

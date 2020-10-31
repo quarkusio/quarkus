c9b7140eb95538c534f9139ffd360c9e5cddbdc0
Matthias Harter <matthias.harter@ionos.com>
2020-10-31 10:26:37 +0100

    added searchRecursive property in ldap config

--- extensions/elytron-security-ldap/runtime/src/main/java/io/quarkus/elytron/security/ldap/LdapRecorder.java
+++ extensions/elytron-security-ldap/runtime/src/main/java/io/quarkus/elytron/security/ldap/LdapRecorder.java
@@ -29,19 +29,25 @@ public class LdapRecorder {
      * @return runtime value wrapper for the SecurityRealm
      */
     public RuntimeValue<SecurityRealm> createRealm(LdapSecurityRealmRuntimeConfig runtimeConfig) {
-        LdapSecurityRealmBuilder builder = LdapSecurityRealmBuilder.builder()
+        LdapSecurityRealmBuilder.IdentityMappingBuilder identityMappingBuilder = LdapSecurityRealmBuilder.builder()
                 .setDirContextSupplier(createDirContextSupplier(runtimeConfig.dirContext))
-                .identityMapping()
+                .identityMapping();
+
+        if (runtimeConfig.identityMapping.searchRecursive) {
+            identityMappingBuilder.searchRecursive();
+        }
+
+        LdapSecurityRealmBuilder ldapSecurityRealmBuilder = identityMappingBuilder
                 .map(createAttributeMappings(runtimeConfig.identityMapping))
                 .setRdnIdentifier(runtimeConfig.identityMapping.rdnIdentifier)
                 .setSearchDn(runtimeConfig.identityMapping.searchBaseDn)
                 .build();
 
         if (runtimeConfig.directVerification) {
-            builder.addDirectEvidenceVerification(false);
+            ldapSecurityRealmBuilder.addDirectEvidenceVerification(false);
         }
 
-        return new RuntimeValue<>(builder.build());
+        return new RuntimeValue<>(ldapSecurityRealmBuilder.build());
     }
 
     private ExceptionSupplier<DirContext, NamingException> createDirContextSupplier(DirContextConfig dirContext) {

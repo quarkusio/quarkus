d6fc10c3554d7e0f0a75319addd2d7968858f43b
Clement Escoffier <clement.escoffier@gmail.com>
2020-11-09 19:09:36 +0100

    The 'throttled' commit strategy requires substituting the built-in rebalance listener.

--- extensions/smallrye-reactive-messaging-kafka/deployment/src/main/java/io/quarkus/smallrye/reactivemessaging/kafka/deployment/SmallRyeReactiveMessagingKafkaProcessor.java
+++ extensions/smallrye-reactive-messaging-kafka/deployment/src/main/java/io/quarkus/smallrye/reactivemessaging/kafka/deployment/SmallRyeReactiveMessagingKafkaProcessor.java
@@ -1,8 +1,11 @@
 package io.quarkus.smallrye.reactivemessaging.kafka.deployment;
 
 import io.quarkus.deployment.Feature;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.vertx.kafka.client.consumer.impl.KafkaReadStreamImpl;
 
 public class SmallRyeReactiveMessagingKafkaProcessor {
 
@@ -10,4 +13,17 @@ public class SmallRyeReactiveMessagingKafkaProcessor {
     FeatureBuildItem feature() {
         return new FeatureBuildItem(Feature.SMALLRYE_REACTIVE_MESSAGING_KAFKA);
     }
+
+    @BuildStep
+    public void build(BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
+        // Required for the throttled commit strategy
+        reflectiveClass.produce(
+                ReflectiveClassBuildItem.builder(KafkaReadStreamImpl.class)
+                        .fields(true)
+                        .methods(true)
+                        .constructors(true)
+                        .finalFieldsWritable(true)
+                        .build());
+    }
+
 }

5787e11361e76ec1e7a00be768503218361cfbd4
Matthias Harter <matthias.harter@ionos.com>
2020-11-10 00:28:01 +0100

    added possibility to configure ldap referral mode

--- extensions/elytron-security-ldap/runtime/src/main/java/io/quarkus/elytron/security/ldap/config/DirContextConfig.java
+++ extensions/elytron-security-ldap/runtime/src/main/java/io/quarkus/elytron/security/ldap/config/DirContextConfig.java
@@ -2,6 +2,8 @@ package io.quarkus.elytron.security.ldap.config;
 
 import java.util.Optional;
 
+import org.wildfly.security.auth.realm.ldap.DirContextFactory;
+
 import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigItem;
 
@@ -26,12 +28,19 @@ public class DirContextConfig {
     @ConfigItem
     public Optional<String> password;
 
+    /**
+     * how ldap redirects are handled
+     */
+    @ConfigItem(defaultValue = "ignore")
+    public DirContextFactory.ReferralMode referralMode;
+
     @Override
     public String toString() {
         return "DirContextConfig{" +
                 "url='" + url + '\'' +
-                ", principal='" + principal + '\'' +
-                ", password='" + password + '\'' +
+                ", principal=" + principal +
+                ", password=" + password +
+                ", referralMode=" + referralMode +
                 '}';
     }
 }

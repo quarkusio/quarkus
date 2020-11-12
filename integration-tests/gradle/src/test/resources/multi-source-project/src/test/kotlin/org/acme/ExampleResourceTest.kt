c51fb2909ef13f941b0c7583c131eec084a92c49
Guillaume Le Floch <glfloch@gmail.com>
2020-11-12 09:29:43 +0100

    Fix gradle test assertion

--- integration-tests/gradle/src/test/resources/multi-source-project/src/test/kotlin/org/acme/ExampleResourceTest.kt
+++ integration-tests/gradle/src/test/resources/multi-source-project/src/test/kotlin/org/acme/ExampleResourceTest.kt
@@ -14,7 +14,7 @@ class ExampleResourceTest {
           .`when`().get("/hello")
           .then()
              .statusCode(200)
-             .body(`is`("hello"))
+             .body(`is`("hello from JavaComponent"))
     }
 
 }

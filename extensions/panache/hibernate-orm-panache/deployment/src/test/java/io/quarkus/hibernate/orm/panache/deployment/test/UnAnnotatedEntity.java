823c5eaa67fef90d988a1c54d2b72bc26625fa36
Georgios Andrianakis <geoand@gmail.com>
2020-11-17 13:43:37 +0200

    Provide actionable information when EntityManager is not available in Hibernate Panache
    Fixes: #13324
    Fixes: #13338

---
+++ extensions/panache/hibernate-orm-panache/deployment/src/test/java/io/quarkus/hibernate/orm/panache/deployment/test/UnAnnotatedEntity.java
@@ -0,0 +1,10 @@
+package io.quarkus.hibernate.orm.panache.deployment.test;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+//import javax.persistence.Entity;
+
+//@Entity
+public class UnAnnotatedEntity extends PanacheEntity {
+    public String name;
+}

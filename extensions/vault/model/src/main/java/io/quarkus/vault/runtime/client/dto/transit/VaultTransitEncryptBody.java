3e0a58f61a9cf120e96ce1cfd428e9df88d7a094
Vincent Sevel <vincent@sevel.eu>
2020-11-17 20:58:05 +0100

    Upgrade Vault to 1.6.0

--- extensions/vault/model/src/main/java/io/quarkus/vault/runtime/client/dto/transit/VaultTransitEncryptBody.java
+++ extensions/vault/model/src/main/java/io/quarkus/vault/runtime/client/dto/transit/VaultTransitEncryptBody.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.quarkus.vault.runtime.Base64String;
 import io.quarkus.vault.runtime.client.dto.VaultModel;
 
 public class VaultTransitEncryptBody implements VaultModel {
 
+    public String name;
+    public Base64String plaintext;
+    public Base64String context;
+    public Base64String nonce;
+    @JsonProperty("key_version")
+    public Integer keyVersion;
     public String type;
     @JsonProperty("convergent_encryption")
     public String convergentEncryption;
--- extensions/vault/model/src/main/java/io/quarkus/vault/runtime/client/dto/transit/VaultTransitEncryptData.java
+++ extensions/vault/model/src/main/java/io/quarkus/vault/runtime/client/dto/transit/VaultTransitEncryptData.java
@@ -8,6 +8,8 @@ import io.quarkus.vault.runtime.client.dto.VaultModel;
 
 public class VaultTransitEncryptData implements VaultModel {
 
+    public String ciphertext;
+    public String error;
     @JsonProperty("batch_results")
     public List<VaultTransitEncryptDataBatchResult> batchResults;
 

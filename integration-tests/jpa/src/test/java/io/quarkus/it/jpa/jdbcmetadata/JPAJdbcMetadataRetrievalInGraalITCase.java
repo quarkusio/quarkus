fe4377be7a3c32dd3e6bb3dc2dc15fdb40422433
Yoann Rodière <yoann@hibernate.org>
2020-11-23 14:52:18 +0100

    Test that Hibernate ORM correctly detects "get_generated_keys" support
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

---
+++ integration-tests/jpa/src/test/java/io/quarkus/it/jpa/jdbcmetadata/JPAJdbcMetadataRetrievalInGraalITCase.java
@@ -0,0 +1,7 @@
+package io.quarkus.it.jpa.jdbcmetadata;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class JPAJdbcMetadataRetrievalInGraalITCase extends JPAJdbcMetadataRetrievalTest {
+}

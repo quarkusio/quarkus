517021fc9006fc3230369f905bde0386a00a7f11
Loïc Mathieu <loikeseke@gmail.com>
2020-11-27 13:14:51 +0100

    Add a logging-gelf integration test with ELK

--- integration-tests/logging-gelf/src/test/java/io/quarkus/logging/gelf/it/GelfLogHandlerTest.java
+++ integration-tests/logging-gelf/src/test/java/io/quarkus/logging/gelf/it/GelfLogHandlerTest.java
@@ -1,35 +1,47 @@
 package io.quarkus.logging.gelf.it;
 
-import static org.hamcrest.Matchers.hasItem;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 /**
  * This test is disabled by default as it needs a central log management system up and running to be able to be launched.
  * Check the README.md, it contains info of how to launch one prior to this test.
  *
- * This test is designed to be launched with Graylog as the central management solution as the RestAssured assertion
- * check that a log events is received using the Graylog search API. Launching the test with another solution will
+ * This test is designed to be launched with ELK as the central management solution as the RestAssured assertion
+ * check that a log events is received using the Elasticsearch search API. Launching the test with another solution will
  * fail the test.
  */
 @QuarkusTest
 public class GelfLogHandlerTest {
 
     @Test
-    public void test() throws InterruptedException {
-        RestAssured.given().when().get("/gelf-log-handler").then().statusCode(204);
+    public void test() {
+        //we need to await for a certain time as logstash needs to create the index template,
+        // then elasticsearch create the index
+        // then some logs being indexed.
+        await().atMost(10, TimeUnit.SECONDS)
+                .untilAsserted(
+                        () -> {
+                            RestAssured.given().when().get("/gelf-log-handler").then().statusCode(204);
 
-        //wait two seconds for the log events to be processed
-        Thread.sleep(2000);
+                            Response response = RestAssured.given()
+                                    .when()
+                                    .get("http://127.0.0.1:9200/_search?q=message")
+                                    .prettyPeek()
+                                    .andReturn();
 
-        RestAssured.given()
-                .when()
-                .auth().basic("admin", "admin")
-                .get("http://127.0.0.1:9000/api/search/universal/relative?query=")
-                .then().statusCode(200)
-                .body("messages.message.message", hasItem("Some useful log message"));
+                            assertEquals(200, response.statusCode());
+                            assertNotNull(response.body().path("hits.hits[0]._source"));
+                            assertEquals("Some useful log message", response.body().path("hits.hits[0]._source.message"));
+                        });
     }
 }

d5d4ba62912b1f6fab7c0fa3147464ea3ebb9f1d
Jan Martiška <jmartisk@redhat.com>
2020-11-27 14:06:05 +0100

    SmallRye GraphQL 1.0.19 + tests for context propagation

---
+++ extensions/smallrye-graphql/deployment/src/test/java/io/quarkus/smallrye/graphql/deployment/GraphQLCDIContextPropagationTest.java
@@ -0,0 +1,188 @@
+package io.quarkus.smallrye.graphql.deployment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.context.ManagedExecutor;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Name;
+import org.eclipse.microprofile.graphql.Query;
+import org.eclipse.microprofile.graphql.Source;
+import org.hamcrest.Matchers;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+/**
+ * Testing scenarios which require CDI context propagation to work under the hood.
+ */
+public class GraphQLCDIContextPropagationTest extends AbstractGraphQLTest {
+
+    @RegisterExtension
+    static QuarkusUnitTest test = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(TestPojo.class, ResourceThatNeedsCdiContextPropagation.class)
+                    .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml"));
+
+    /**
+     * Call a query which needs the CDI context to be propagated, because it is on a RequestScoped bean
+     * and involves retrieving a batch source field (these are retrieved asynchronously).
+     */
+    @Test
+    public void testCdiContextPropagationForBatchSources() {
+        String pingRequest = getPayload("{\n" +
+                "  pojos {\n" +
+                "    duplicatedMessage\n" +
+                "  }\n" +
+                "}");
+
+        RestAssured.given().when()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(pingRequest)
+                .post("/graphql")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body("data.pojos.duplicatedMessage", Matchers.contains("AA", "BB"));
+    }
+
+    /**
+     * Same as above, but the batch source returns a list of CompletionStages.
+     */
+    @Test
+    public void testCdiContextPropagationForBatchSourcesAsync() {
+        String pingRequest = getPayload("{\n" +
+                "  pojos {\n" +
+                "    duplicatedMessageAsync\n" +
+                "  }\n" +
+                "}");
+
+        RestAssured.given().when()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(pingRequest)
+                .post("/graphql")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body("data.pojos.duplicatedMessageAsync", Matchers.contains("AA", "BB"));
+    }
+
+    /**
+     * In this case, there is an asynchronous query and the application launches a CompletableFuture,
+     * therefore the application is responsible for propagating the context to that future.
+     */
+    @Test
+    public void testManualContextPropagation() {
+        String pingRequest = getPayload("{\n" +
+                "  pojo_manual_propagation {\n" +
+                "    message\n" +
+                "  }\n" +
+                "}");
+
+        RestAssured.given().when()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(pingRequest)
+                .post("/graphql")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body("data.pojo_manual_propagation.message", Matchers.equalTo("A"));
+    }
+
+    @GraphQLApi
+    @ApplicationScoped
+    public static class ResourceThatNeedsCdiContextPropagation {
+
+        /**
+         * This is to make sure that `getPojos` and `duplicatedMessage` are called on the same class instance - they
+         * need to share the same CDI request context rather than create a new one for calling `duplicatedMessage`.
+         */
+        private volatile Long injectedBeanId;
+
+        @Inject
+        InjectedBean injectedBean;
+
+        @Inject
+        ManagedExecutor executor;
+
+        @Query("pojos")
+        public List<TestPojo> getPojos() {
+            this.injectedBeanId = injectedBean.getId();
+            List<TestPojo> pojos = new ArrayList<>();
+            pojos.add(new TestPojo("A"));
+            pojos.add(new TestPojo("B"));
+            return pojos;
+        }
+
+        @Query("pojo_manual_propagation")
+        public CompletionStage<TestPojo> getPojoWithProgrammaticContextPropagation() {
+            Long id = injectedBean.getId();
+            return executor.supplyAsync(
+                    () -> {
+                        if (!injectedBean.getId().equals(id)) {
+                            throw new IllegalStateException("The future was not executed in the correct request context");
+                        }
+                        return new TestPojo("A");
+                    });
+        }
+
+        /**
+         * This source field duplicates the message of a TestPojo (repeats it twice).
+         */
+        @Name("duplicatedMessage")
+        public List<String> duplicatedMessage(@Source List<TestPojo> pojos) {
+            if (!this.injectedBean.getId().equals(this.injectedBeanId)) {
+                throw new IllegalStateException("duplicatedMessage must be executed in the same request context as getPojos");
+            }
+            return pojos.stream()
+                    .map(pojo -> pojo.getMessage() + pojo.getMessage())
+                    .collect(Collectors.toList());
+        }
+
+        /**
+         * This source field duplicates the message of a TestPojo (repeats it twice) and does it asynchronously.
+         */
+        @Name("duplicatedMessageAsync")
+        public CompletionStage<List<String>> duplicatedMessageAsync(@Source List<TestPojo> pojos) {
+            if (!this.injectedBean.getId().equals(this.injectedBeanId)) {
+                throw new IllegalStateException(
+                        "duplicatedMessageAsync must be executed in the same request context as getPojos");
+            }
+            return CompletableFuture.completedFuture(pojos.stream()
+                    .map(pojo -> pojo.getMessage() + pojo.getMessage())
+                    .collect(Collectors.toList()));
+        }
+
+    }
+
+    @RequestScoped
+    public static class InjectedBean {
+
+        private Long id = ThreadLocalRandom.current().nextLong();
+
+        public Long getId() {
+            return id;
+        }
+    }
+
+}
---
+++ extensions/smallrye-graphql/deployment/src/test/java/io/quarkus/smallrye/graphql/deployment/SmallRyeGraphQLContextTestCase.java
@@ -0,0 +1,256 @@
+package io.quarkus.smallrye.graphql.deployment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.context.ManagedExecutor;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Name;
+import org.eclipse.microprofile.graphql.Query;
+import org.eclipse.microprofile.graphql.Source;
+import org.hamcrest.Matchers;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.smallrye.graphql.api.Context;
+
+public class SmallRyeGraphQLContextTestCase extends AbstractGraphQLTest {
+
+    @RegisterExtension
+    static QuarkusUnitTest test = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(Dummy.class, ContextPropagationResource.class));
+
+    @Test
+    public void testAsyncQuery() {
+        String request = getPayload("{\n" +
+                "  testAsyncQuery {\n" +
+                "    stringField\n" +
+                "  }\n" +
+                "}");
+
+        RestAssured.given().when()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(request)
+                .post("/graphql")
+                .then()
+                .log()
+                .body(true)
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body("data.testAsyncQuery.stringField", Matchers.equalTo("OK"));
+    }
+
+    @Test
+    public void testAsyncQueryWithAsyncSource() {
+        String request = getPayload("{\n" +
+                "  testAsyncQuery {\n" +
+                "    stringField\n" +
+                "    stringBatchSourceAsync\n" +
+                "  }\n" +
+                "}");
+
+        RestAssured.given().when()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(request)
+                .post("/graphql")
+                .then()
+                .log()
+                .body(true)
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body("data.testAsyncQuery.stringField", Matchers.equalTo("OK"))
+                .body("data.testAsyncQuery.stringBatchSourceAsync", Matchers.equalTo("hello"));
+    }
+
+    @Test
+    public void testManualPropagation() {
+        String request = getPayload("{\n" +
+                "  testManualPropagation {\n" +
+                "    stringField\n" +
+                "  }\n" +
+                "}");
+
+        RestAssured.given().when()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(request)
+                .post("/graphql")
+                .then()
+                .log()
+                .body(true)
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body("data.testManualPropagation.stringField", Matchers.equalTo("OK"));
+    }
+
+    @Test
+    public void testSourceMethods() {
+        String request = getPayload("{\n" +
+                "  testSourceMethods {\n" +
+                "    stringBatchSource,\n" +
+                "    numberBatchSource\n" +
+                "  }\n" +
+                "}");
+
+        RestAssured.given().when()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(request)
+                .post("/graphql")
+                .then()
+                .log()
+                .body(true)
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body("data.testSourceMethods.stringBatchSource", Matchers.equalTo("hello"));
+    }
+
+    @GraphQLApi
+    @ApplicationScoped
+    public static class ContextPropagationResource {
+
+        @Inject
+        Instance<Context> context;
+
+        @Inject
+        ManagedExecutor managedExecutor;
+
+        @Query
+        public CompletableFuture<Dummy> testAsyncQuery() {
+            final String executionId = context.get().getExecutionId();
+            assertNotNull(executionId);
+            this.executionId = context.get().getExecutionId();
+            assertEquals("testAsyncQuery", context.get().getFieldName());
+            Dummy result = new Dummy();
+            result.setStringField("OK");
+            return CompletableFuture.completedFuture(result);
+        }
+
+        @Query
+        public Dummy testManualPropagation() throws ExecutionException, InterruptedException {
+            final String executionId = context.get().getExecutionId();
+            assertNotNull(executionId);
+
+            CompletableFuture<Integer> numberFuture = CompletableFuture.supplyAsync(() -> {
+                assertNotNull(context);
+                assertEquals(executionId, context.get().getExecutionId());
+                assertEquals("testManualPropagation", context.get().getFieldName());
+                return 42;
+            }, managedExecutor);
+
+            CompletableFuture<String> stringFuture = CompletableFuture.supplyAsync(() -> {
+                assertNotNull(context, "Context must to be available inside an async task");
+                assertEquals(executionId, context.get().getExecutionId(), "Execution ID must be the same inside an async task");
+                assertEquals("testManualPropagation", context.get().getFieldName());
+                return "OK";
+            }, managedExecutor);
+
+            Dummy result = new Dummy();
+            result.setNumberField(numberFuture.get());
+            result.setStringField(stringFuture.get());
+            return result;
+        }
+
+        private volatile String executionId;
+
+        @Query
+        public Dummy testSourceMethods() {
+            this.executionId = context.get().getExecutionId();
+            assertEquals("testSourceMethods", context.get().getFieldName());
+            return new Dummy();
+        }
+
+        @Name("stringBatchSource")
+        public List<String> stringBatchSource(@Source List<Dummy> source) {
+            assertEquals("stringBatchSource", context.get().getFieldName());
+            assertEquals(this.executionId, context.get().getExecutionId(), "Wrong execution ID propagated from the root query");
+
+            List<String> result = new ArrayList<>();
+            for (Dummy dummy : source) {
+                result.add("hello");
+            }
+            return result;
+        }
+
+        @Name("stringBatchSourceAsync")
+        public CompletionStage<List<String>> stringBatchSourceAsync(@Source List<Dummy> source) {
+            assertEquals("stringBatchSourceAsync", context.get().getFieldName());
+            assertEquals(this.executionId, context.get().getExecutionId(), "Wrong execution ID propagated from the root query");
+
+            List<String> result = new ArrayList<>();
+            for (Dummy dummy : source) {
+                result.add("hello");
+            }
+            return CompletableFuture.completedFuture(result);
+        }
+
+        @Name("numberBatchSource")
+        public List<Integer> numberBatchSource(@Source List<Dummy> source) {
+            assertEquals("numberBatchSource", context.get().getFieldName());
+            assertEquals(this.executionId, context.get().getExecutionId(), "Wrong execution ID propagated from the root query");
+
+            List<Integer> result = new ArrayList<>();
+            for (Dummy dummy : source) {
+                result.add(123);
+            }
+            return result;
+        }
+
+    }
+
+    public static class Dummy {
+
+        private String stringField;
+
+        private Integer numberField;
+
+        private Boolean booleanField;
+
+        public String getStringField() {
+            return stringField;
+        }
+
+        public void setStringField(String stringField) {
+            this.stringField = stringField;
+        }
+
+        public Integer getNumberField() {
+            return numberField;
+        }
+
+        public void setNumberField(Integer numberField) {
+            this.numberField = numberField;
+        }
+
+        public Boolean getBooleanField() {
+            return booleanField;
+        }
+
+        public void setBooleanField(Boolean booleanField) {
+            this.booleanField = booleanField;
+        }
+    }
+
+}

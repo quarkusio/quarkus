368719cb9a6d88789f6c1350fee6a447fc924182
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-12-01 11:09:50 +1100

    Fix potential hang if exception is thrown from authenticator
    Fixes #13559

---
+++ extensions/vertx-http/deployment/src/test/java/io/quarkus/vertx/http/security/FailingIdentityProvider.java
@@ -0,0 +1,24 @@
+package io.quarkus.vertx.http.security;
+
+import javax.inject.Singleton;
+
+import io.quarkus.security.AuthenticationFailedException;
+import io.quarkus.security.identity.AuthenticationRequestContext;
+import io.quarkus.security.identity.IdentityProvider;
+import io.quarkus.security.identity.SecurityIdentity;
+import io.quarkus.security.identity.request.UsernamePasswordAuthenticationRequest;
+import io.smallrye.mutiny.Uni;
+
+@Singleton
+public class FailingIdentityProvider implements IdentityProvider<UsernamePasswordAuthenticationRequest> {
+    @Override
+    public Class<UsernamePasswordAuthenticationRequest> getRequestType() {
+        return UsernamePasswordAuthenticationRequest.class;
+    }
+
+    @Override
+    public Uni<SecurityIdentity> authenticate(UsernamePasswordAuthenticationRequest request,
+            AuthenticationRequestContext context) {
+        throw new AuthenticationFailedException("FAILED");
+    }
+}

154b038996e6710d566dc97d45c34c78258aae46
Alexey Loubyansky <olubyans@redhat.com>
2020-12-03 16:44:14 +0100

    Reset TCCL in QuarkusLauncher when the app terminates

--- core/launcher/src/main/java/io/quarkus/launcher/QuarkusLauncher.java
+++ core/launcher/src/main/java/io/quarkus/launcher/QuarkusLauncher.java
@@ -23,6 +23,7 @@ import io.quarkus.bootstrap.BootstrapConstants;
 public class QuarkusLauncher {
 
     public static void launch(String callingClass, String quarkusApplication, String... args) {
+        final ClassLoader originalCl = Thread.currentThread().getContextClassLoader();
         try {
             String classResource = callingClass.replace(".", "/") + ".class";
             URL resource = Thread.currentThread().getContextClassLoader().getResource(classResource);
@@ -57,6 +58,7 @@ public class QuarkusLauncher {
             throw new RuntimeException(e);
         } finally {
             System.clearProperty(BootstrapConstants.SERIALIZED_APP_MODEL);
+            Thread.currentThread().setContextClassLoader(originalCl);
         }
     }
 

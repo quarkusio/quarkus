d683556f76b2e0c873b6f2b6c683c4faca2df811
Sanne Grinovero <sanne@hibernate.org>
2020-12-04 16:32:16 +0000

    Wrong test structure to check 2LC can be disabled

---
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/JPACacheDisabledTest.java
@@ -0,0 +1,38 @@
+package io.quarkus.hibernate.orm;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.cache.spi.CacheImplementor;
+import org.hibernate.cache.spi.TimestampsCache;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.hibernate.orm.enhancer.Address;
+import io.quarkus.test.QuarkusUnitTest;
+
+public class JPACacheDisabledTest {
+
+    @RegisterExtension
+    static QuarkusUnitTest runner = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClass(Address.class))
+            .withConfigurationResource("application.properties")
+            .overrideConfigKey("quarkus.hibernate-orm.second-level-caching-enabled", "false");
+
+    @Inject
+    Session session;
+
+    @Test
+    @Transactional
+    public void testNTransaction() {
+        CacheImplementor cache = (CacheImplementor) session.getSessionFactory().getCache();
+        TimestampsCache timestampsCache = cache.getTimestampsCache();
+        Assertions.assertNull(timestampsCache);
+    }
+
+}

c641e7e68fafc285ff6cb5f49401843b80531a5a
Yoann Rodière <yoann@hibernate.org>
2020-12-04 08:55:38 +0100

    Test injection of SearchMapping/SearchSession
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

---
+++ extensions/hibernate-search-orm-elasticsearch/deployment/src/test/java/io/quarkus/hibernate/search/elasticsearch/test/multiplepersistenceunits/defaultpu/DefaultPUEntity.java
@@ -0,0 +1,44 @@
+package io.quarkus.hibernate.search.elasticsearch.test.multiplepersistenceunits.defaultpu;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+
+@Entity
+@Indexed
+public class DefaultPUEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @FullTextField
+    private String text;
+
+    public DefaultPUEntity() {
+    }
+
+    public DefaultPUEntity(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}

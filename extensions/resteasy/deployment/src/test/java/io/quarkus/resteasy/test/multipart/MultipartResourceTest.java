8ac06b12826a1cc489f305b6c7e6b3420faa351b
Guillaume Smet <guillaume.smet@gmail.com>
2020-12-04 14:44:28 +0100

    Register ServerFormUrlEncodedProvider manually
    It's registered manually in RESTEasy so it totally defeats our
    auto-detection mechanism.
    Add a version with a no-args constructor and add it manually to Quarkus.
    
    Fix #13667

---
+++ extensions/resteasy/deployment/src/test/java/io/quarkus/resteasy/test/multipart/MultipartResourceTest.java
@@ -0,0 +1,38 @@
+package io.quarkus.resteasy.test.multipart;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+import io.restassured.http.ContentType;
+
+public class MultipartResourceTest {
+
+    @RegisterExtension
+    static QuarkusUnitTest test = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(MultipartResource.class));
+
+    @Test
+    public void testHelloEndpoint() {
+        Map<String, String> map = new HashMap<>();
+        map.put("test", "value");
+
+        given()
+                .formParams(map)
+                .contentType(ContentType.URLENC)
+                .when().post("/multipart/")
+                .then()
+                .statusCode(200)
+                .body(is("[test:value]"));
+    }
+
+}

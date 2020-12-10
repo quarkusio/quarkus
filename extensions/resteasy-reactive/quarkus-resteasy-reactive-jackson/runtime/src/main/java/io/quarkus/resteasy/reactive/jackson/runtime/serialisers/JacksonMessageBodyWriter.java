a9a36bd1980a8d9ca53d8810f16cebf835a15936
Georgios Andrianakis <geoand@gmail.com>
2020-12-10 11:59:01 +0200

    Ensure than an exception during serialization of json is properly handled
    Fixes: #13797

--- extensions/resteasy-reactive/quarkus-resteasy-reactive-jackson/runtime/src/main/java/io/quarkus/resteasy/reactive/jackson/runtime/serialisers/JacksonMessageBodyWriter.java
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive-jackson/runtime/src/main/java/io/quarkus/resteasy/reactive/jackson/runtime/serialisers/JacksonMessageBodyWriter.java
@@ -16,16 +16,26 @@ import org.jboss.resteasy.reactive.server.spi.ServerMessageBodyWriter;
 import org.jboss.resteasy.reactive.server.spi.ServerRequestContext;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 public class JacksonMessageBodyWriter implements ServerMessageBodyWriter<Object> {
 
     private static final String JSON_VIEW_NAME = JsonView.class.getName();
-    private final ObjectMapper mapper;
+    private final ObjectWriter writer;
 
     @Inject
     public JacksonMessageBodyWriter(ObjectMapper mapper) {
-        this.mapper = mapper;
+        // we don't want the ObjectWriter to close the stream automatically, as we want to handle closing manually at the proper points
+        if (mapper.getFactory().isEnabled(JsonGenerator.Feature.AUTO_CLOSE_TARGET)) {
+            JsonFactory jsonFactory = mapper.getFactory().copy();
+            jsonFactory.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
+            this.writer = mapper.writer().with(jsonFactory);
+        } else {
+            this.writer = mapper.writer();
+        }
     }
 
     @Override
@@ -39,7 +49,7 @@ public class JacksonMessageBodyWriter implements ServerMessageBodyWriter<Object>
         if (o instanceof String) { // YUK: done in order to avoid adding extra quotes...
             entityStream.write(((String) o).getBytes());
         } else {
-            entityStream.write(mapper.writeValueAsBytes(o));
+            entityStream.write(writer.writeValueAsBytes(o));
         }
     }
 
@@ -50,22 +60,23 @@ public class JacksonMessageBodyWriter implements ServerMessageBodyWriter<Object>
 
     @Override
     public void writeResponse(Object o, ServerRequestContext context) throws WebApplicationException, IOException {
-        try (OutputStream stream = context.getOrCreateOutputStream()) {
-            if (o instanceof String) { // YUK: done in order to avoid adding extra quotes...
-                stream.write(((String) o).getBytes());
-            } else {
-                // First test the names to see if JsonView is used. We do this to avoid doing reflection for the common case
-                // where JsonView is not used
-                if (context.getResteasyReactiveResourceInfo().getMethodAnnotationNames().contains(JSON_VIEW_NAME)) {
-                    Method method = context.getResteasyReactiveResourceInfo().getMethod();
-                    JsonView jsonView = method.getAnnotation(JsonView.class);
-                    if ((jsonView != null) && (jsonView.value().length > 0)) {
-                        mapper.writerWithView(jsonView.value()[0]).writeValue(stream, o);
-                        return;
-                    }
+        OutputStream stream = context.getOrCreateOutputStream();
+        if (o instanceof String) { // YUK: done in order to avoid adding extra quotes...
+            stream.write(((String) o).getBytes());
+        } else {
+            // First test the names to see if JsonView is used. We do this to avoid doing reflection for the common case
+            // where JsonView is not used
+            if (context.getResteasyReactiveResourceInfo().getMethodAnnotationNames().contains(JSON_VIEW_NAME)) {
+                Method method = context.getResteasyReactiveResourceInfo().getMethod();
+                JsonView jsonView = method.getAnnotation(JsonView.class);
+                if ((jsonView != null) && (jsonView.value().length > 0)) {
+                    writer.withView(jsonView.value()[0]).writeValue(stream, o);
+                    return;
                 }
-                mapper.writeValue(stream, o);
             }
+            writer.writeValue(stream, o);
         }
+        // we don't use try-with-resources because that results in writing to the http output without the exception mapping coming into play
+        stream.close();
     }
 }

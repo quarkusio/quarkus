bf53da8eb377b06d5169e19ec27aa546a1f8a12c
Sergey Beryozkin <sberyozkin@gmail.com>
2020-12-14 17:54:09 +0000

    Register SignatureAlgorithm and KeyEncryptionAlgorithm for reflection in smallrye-jwt

--- extensions/smallrye-jwt-build/deployment/src/main/java/io/quarkus/smallrye/jwt/builder/deployment/SmallRyeJwtBuildProcessor.java
+++ extensions/smallrye-jwt-build/deployment/src/main/java/io/quarkus/smallrye/jwt/builder/deployment/SmallRyeJwtBuildProcessor.java
@@ -3,6 +3,7 @@ package io.quarkus.smallrye.jwt.builder.deployment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.smallrye.jwt.algorithm.KeyEncryptionAlgorithm;
 import io.smallrye.jwt.algorithm.SignatureAlgorithm;
 import io.smallrye.jwt.build.impl.JwtProviderImpl;
 
@@ -11,6 +12,7 @@ class SmallRyeJwtBuildProcessor {
     @BuildStep
     void addClassesForReflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClasses) {
         reflectiveClasses.produce(new ReflectiveClassBuildItem(true, true, SignatureAlgorithm.class));
+        reflectiveClasses.produce(new ReflectiveClassBuildItem(true, true, KeyEncryptionAlgorithm.class));
         reflectiveClasses.produce(new ReflectiveClassBuildItem(true, true, JwtProviderImpl.class));
     }
 }

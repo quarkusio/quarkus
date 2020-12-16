84d4fb5b755cef0e0d5dfd4ad108a2ef401f719b
Martin Kouba <mkouba@redhat.com>
2020-12-16 16:09:58 +0100

    Reactive routes - identify application handler classes correctly

--- extensions/vertx-web/deployment/src/main/java/io/quarkus/vertx/web/deployment/VertxWebProcessor.java
+++ extensions/vertx-web/deployment/src/main/java/io/quarkus/vertx/web/deployment/VertxWebProcessor.java
@@ -22,6 +22,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import javax.enterprise.context.ContextNotActiveException;
@@ -62,6 +63,7 @@ import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.ApplicationIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.GeneratedClassBuildItem;
 import io.quarkus.deployment.builditem.LaunchModeBuildItem;
@@ -225,9 +227,18 @@ class VertxWebProcessor {
             LaunchModeBuildItem launchMode,
             BuildProducer<RouteDescriptionBuildItem> descriptions,
             Capabilities capabilities,
-            Optional<BeanValidationAnnotationsBuildItem> beanValidationAnnotations) {
+            Optional<BeanValidationAnnotationsBuildItem> beanValidationAnnotations,
+            ApplicationIndexBuildItem applicationIndex) {
 
-        ClassOutput classOutput = new GeneratedClassGizmoAdaptor(generatedClass, true);
+        Predicate<String> appClassPredicate = new Predicate<String>() {
+            @Override
+            public boolean test(String name) {
+                int idx = name.lastIndexOf(HANDLER_SUFFIX);
+                String className = idx != -1 ? name.substring(0, idx) : name;
+                return applicationIndex.getIndex().getClassByName(DotName.createSimple(className.replace("/", "."))) != null;
+            }
+        };
+        ClassOutput classOutput = new GeneratedClassGizmoAdaptor(generatedClass, appClassPredicate);
         IndexView index = beanArchive.getIndex();
         Map<RouteMatcher, MethodInfo> matchers = new HashMap<>();
         boolean validatorAvailable = capabilities.isPresent(Capability.HIBERNATE_VALIDATOR);

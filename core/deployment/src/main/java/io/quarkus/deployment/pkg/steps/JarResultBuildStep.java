fe0879cfdf0e041ad08458adca94a1970cfa5321
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-12-16 14:16:56 +1100

    Make sure serialized app uses / seperators
    Fixes #13878

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/JarResultBuildStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/JarResultBuildStep.java
@@ -599,7 +599,8 @@ public class JarResultBuildStep {
                 Map<AppArtifactKey, List<String>> relativePaths = new HashMap<>();
                 for (Map.Entry<AppArtifactKey, List<Path>> e : copiedArtifacts.entrySet()) {
                     relativePaths.put(e.getKey(),
-                            e.getValue().stream().map(s -> buildDir.relativize(s).toString()).collect(Collectors.toList()));
+                            e.getValue().stream().map(s -> buildDir.relativize(s).toString().replace("\\", "/"))
+                                    .collect(Collectors.toList()));
                 }
 
                 //now we serialize the data needed to build up the reaugmentation class path

207f300a5550ded80e63034143b5b4189bee968a
Ioannis Canellos <iocanel@gmail.com>
2020-12-18 14:50:27 +0200

    fix (#13947) context root based so the builder used

--- extensions/container-image/container-image-openshift/deployment/src/main/java/io/quarkus/container/image/openshift/deployment/OpenshiftBaseNativeImage.java
+++ extensions/container-image/container-image-openshift/deployment/src/main/java/io/quarkus/container/image/openshift/deployment/OpenshiftBaseNativeImage.java
@@ -8,9 +8,10 @@ import io.quarkus.container.image.deployment.util.ImageUtil;
 public enum OpenshiftBaseNativeImage {
 
     //We only compare `repositories` so registries and tags are stripped
-    QUARKUS("quarkus/ubi-quarkus-native-binary-s2i:latest", "application", "QUARKUS_HOME", "QUARKUS_OPTS");
+    QUARKUS("quarkus/ubi-quarkus-native-binary-s2i:latest", "/home/quarkus/", "application", "QUARKUS_HOME", "QUARKUS_OPTS");
 
     private final String image;
+    private final String nativeBinaryDirectory;
     private final String fixedNativeBinaryName;
     private final String homeDirEnvVar;
     private final String optsEnvVar;
@@ -24,8 +25,10 @@ public enum OpenshiftBaseNativeImage {
         return Optional.empty();
     }
 
-    private OpenshiftBaseNativeImage(String image, String fixedNativeBinaryName, String homeDirEnvVar, String optsEnvVar) {
+    private OpenshiftBaseNativeImage(String image, String nativeBinaryDirectory, String fixedNativeBinaryName,
+            String homeDirEnvVar, String optsEnvVar) {
         this.image = image;
+        this.nativeBinaryDirectory = nativeBinaryDirectory;
         this.fixedNativeBinaryName = fixedNativeBinaryName;
         this.homeDirEnvVar = homeDirEnvVar;
         this.optsEnvVar = optsEnvVar;
@@ -33,6 +36,10 @@ public enum OpenshiftBaseNativeImage {
 
     public String getImage() {
         return image;
+    }
+
+    public String getNativeBinaryDirectory() {
+        return nativeBinaryDirectory;
     }
 
     public String getFixedNativeNinaryName() {
@@ -46,4 +53,5 @@ public enum OpenshiftBaseNativeImage {
     public String getOptsEnvVar() {
         return optsEnvVar;
     }
+
 }

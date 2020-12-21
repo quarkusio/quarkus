1fb47db670655e44ad174c907f5ee6cf53b7160c
Guillaume Smet <guillaume.smet@gmail.com>
2020-12-21 15:36:29 +0100

    Disable Image IO tests for now
    We will bring them back when upgrading to GraalVM 21 but for now they
    don't work with GraalVM 20.3.

--- integration-tests/main/src/main/java/io/quarkus/it/corestuff/ImageIOSupport.java
+++ integration-tests/main/src/main/java/io/quarkus/it/corestuff/ImageIOSupport.java
@@ -1,9 +1,7 @@
 package io.quarkus.it.corestuff;
 
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,9 +12,10 @@ public class ImageIOSupport extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        BufferedImage image = ImageIO.read(classLoader.getResource("META-INF/resources/1px.png"));
-        resp.getWriter().write(image.getHeight() + "x" + image.getWidth());
+        // Please uncomment this when we upgrade from GraalVM 20.3 to GraalVM 21
+        //        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        //        BufferedImage image = ImageIO.read(classLoader.getResource("META-INF/resources/1px.png"));
+        //        resp.getWriter().write(image.getHeight() + "x" + image.getWidth());
     }
 
 }

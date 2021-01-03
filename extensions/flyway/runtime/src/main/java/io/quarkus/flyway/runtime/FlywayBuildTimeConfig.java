d04550effe74a5ee8041a9c2ee83410ecb9a86a3
cristhiank <calovi86@gmail.com>
2021-01-03 10:48:27 -0500

    Removes dead code Encapsulates default datasource identification logic

--- extensions/flyway/runtime/src/main/java/io/quarkus/flyway/runtime/FlywayBuildTimeConfig.java
+++ extensions/flyway/runtime/src/main/java/io/quarkus/flyway/runtime/FlywayBuildTimeConfig.java
@@ -3,6 +3,7 @@ package io.quarkus.flyway.runtime;
 import java.util.Collections;
 import java.util.Map;
 
+import io.quarkus.datasource.common.runtime.DataSourceUtil;
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
@@ -10,16 +11,13 @@ import io.quarkus.runtime.annotations.ConfigRoot;
 @ConfigRoot(name = "flyway", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
 public final class FlywayBuildTimeConfig {
 
-    public static final FlywayBuildTimeConfig defaultConfig() {
-        FlywayBuildTimeConfig defaultConfig = new FlywayBuildTimeConfig();
-        defaultConfig.defaultDataSource = FlywayDataSourceBuildTimeConfig.defaultConfig();
-        return defaultConfig;
-    }
-
     /**
      * Gets the {@link FlywayDataSourceBuildTimeConfig} for the given datasource name.
      */
     public FlywayDataSourceBuildTimeConfig getConfigForDataSourceName(String dataSourceName) {
+        if (DataSourceUtil.isDefault(dataSourceName)) {
+            return defaultDataSource;
+        }
         return namedDataSources.getOrDefault(dataSourceName, FlywayDataSourceBuildTimeConfig.defaultConfig());
     }
 

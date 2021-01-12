bf28a6d2cda91c9ad6bf6efb81630899da78f4ea
Phillip Kruger <phillip.kruger@gmail.com>
2021-01-12 12:43:22 +0200

    Non-application path and Dev console fixes
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- extensions/smallrye-graphql/deployment/src/main/java/io/quarkus/smallrye/graphql/deployment/SmallRyeGraphQLProcessor.java
+++ extensions/smallrye-graphql/deployment/src/main/java/io/quarkus/smallrye/graphql/deployment/SmallRyeGraphQLProcessor.java
@@ -454,7 +454,7 @@ public class SmallRyeGraphQLProcessor {
 
             String graphQLPath = httpRootPath.adjustPath(graphQLConfig.rootPath);
             String graphQLUiPath = nonApplicationRootPathBuildItem
-                    .adjustPath(httpRootPath.adjustPath(graphQLConfig.ui.rootPath));
+                    .adjustPath(graphQLConfig.ui.rootPath);
 
             AppArtifact artifact = WebJarUtil.getAppArtifact(curateOutcomeBuildItem, GRAPHQL_UI_WEBJAR_GROUP_ID,
                     GRAPHQL_UI_WEBJAR_ARTIFACT_ID);
@@ -462,10 +462,11 @@ public class SmallRyeGraphQLProcessor {
                 Path tempPath = WebJarUtil.copyResourcesForDevOrTest(curateOutcomeBuildItem, launchMode, artifact,
                         GRAPHQL_UI_WEBJAR_PREFIX);
                 WebJarUtil.updateUrl(tempPath.resolve(FILE_TO_UPDATE), graphQLPath, LINE_TO_UPDATE, LINE_FORMAT);
-                WebJarUtil.updateUrl(tempPath.resolve(FILE_TO_UPDATE), graphQLUiPath, UI_LINE_TO_UPDATE, UI_LINE_FORMAT);
+                WebJarUtil.updateUrl(tempPath.resolve(FILE_TO_UPDATE), httpRootPath.adjustPath(graphQLUiPath),
+                        UI_LINE_TO_UPDATE, UI_LINE_FORMAT);
 
                 smallRyeGraphQLBuildProducer.produce(new SmallRyeGraphQLBuildItem(tempPath.toAbsolutePath().toString(),
-                        graphQLUiPath));
+                        httpRootPath.adjustPath(graphQLUiPath)));
                 notFoundPageDisplayableEndpointProducer
                         .produce(new NotFoundPageDisplayableEndpointBuildItem(graphQLUiPath + "/", "MicroProfile GraphQL UI"));
 
@@ -483,7 +484,8 @@ public class SmallRyeGraphQLProcessor {
                                         LINE_FORMAT)
                                 .getBytes(StandardCharsets.UTF_8);
                         content = WebJarUtil
-                                .updateUrl(new String(content, StandardCharsets.UTF_8), graphQLPath, UI_LINE_TO_UPDATE,
+                                .updateUrl(new String(content, StandardCharsets.UTF_8), httpRootPath.adjustPath(graphQLUiPath),
+                                        UI_LINE_TO_UPDATE,
                                         UI_LINE_FORMAT)
                                 .getBytes(StandardCharsets.UTF_8);
                     }
@@ -494,7 +496,7 @@ public class SmallRyeGraphQLProcessor {
                 }
 
                 smallRyeGraphQLBuildProducer.produce(new SmallRyeGraphQLBuildItem(GRAPHQL_UI_FINAL_DESTINATION,
-                        graphQLUiPath));
+                        httpRootPath.adjustPath(graphQLUiPath)));
             }
         }
     }

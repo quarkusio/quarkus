8e944416fbd5968333ee25d0677ad77de5e39c14
Yoann Rodière <yoann@hibernate.org>
2021-01-13 13:37:32 +0100

    Test the transaction lifecycle (session flushes, session closing, ...)
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

---
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/transaction/SimpleEntity.java
@@ -0,0 +1,36 @@
+package io.quarkus.hibernate.orm.transaction;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class SimpleEntity {
+
+    @Id
+    private long id;
+
+    private String name;
+
+    public SimpleEntity() {
+    }
+
+    public SimpleEntity(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/transaction/TransactionAnnotationLifecycleTest.java
@@ -0,0 +1,65 @@
+package io.quarkus.hibernate.orm.transaction;
+
+import java.util.function.Function;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+/**
+ * Check transaction lifecycle, including session flushes and the closing of the session.
+ */
+public class TransactionAnnotationLifecycleTest extends AbstractTransactionLifecycleTest {
+
+    @Inject
+    TransactionAnnotationCRUD transactionAnnotationCRUD;
+
+    @Override
+    protected TestCRUD crud() {
+        return transactionAnnotationCRUD;
+    }
+
+    @Override
+    protected boolean expectDoubleFlush() {
+        // FIXME: We expect double flushes in this case, but that's a bug
+        return true;
+    }
+
+    @ApplicationScoped
+    public static class TransactionAnnotationCRUD extends TestCRUD {
+        @Inject
+        EntityManager entityManager;
+
+        @Override
+        @Transactional
+        public ValueAndExecutionMetadata<Void> create(long id, String name) {
+            return super.create(id, name);
+        }
+
+        @Override
+        @Transactional
+        public ValueAndExecutionMetadata<String> retrieve(long id) {
+            return super.retrieve(id);
+        }
+
+        @Override
+        @Transactional
+        public ValueAndExecutionMetadata<Void> update(long id, String name) {
+            return super.update(id, name);
+        }
+
+        @Override
+        @Transactional
+        public ValueAndExecutionMetadata<Void> delete(long id) {
+            return super.delete(id);
+        }
+
+        @Override
+        public <T> ValueAndExecutionMetadata<T> inTransaction(Function<EntityManager, T> action) {
+            // We should already be in a transaction
+            return ValueAndExecutionMetadata.run(entityManager, action);
+        }
+    }
+
+}

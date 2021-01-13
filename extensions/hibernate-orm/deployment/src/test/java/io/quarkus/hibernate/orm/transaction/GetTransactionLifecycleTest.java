1deece0b5cac58511cbd7e34851f49e4f477d7e0
Yoann Rodière <yoann@hibernate.org>
2021-01-13 13:37:32 +0100

    Test the transaction lifecycle (session flushes, session closing, ...)
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

---
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/transaction/GetTransactionLifecycleTest.java
@@ -0,0 +1,59 @@
+package io.quarkus.hibernate.orm.transaction;
+
+import java.util.function.Function;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+public class GetTransactionLifecycleTest extends AbstractTransactionLifecycleTest {
+
+    @Inject
+    GetTransactionCRUD getTransactionCRUD;
+
+    @Override
+    protected TestCRUD crud() {
+        return getTransactionCRUD;
+    }
+
+    @Override
+    protected boolean expectDoubleFlush() {
+        // We expect double flushes in this case because EntityTransaction.commit() triggers a flush,
+        // and then the transaction synchronization will also trigger a flush before transaction completion.
+        // This may be a bug in ORM, but in any case there's nothing we can do about it.
+        return true;
+    }
+
+    @ApplicationScoped
+    public static class GetTransactionCRUD extends TestCRUD {
+        @Inject
+        EntityManagerFactory entityManagerFactory;
+
+        @Override
+        public <T> ValueAndExecutionMetadata<T> inTransaction(Function<EntityManager, T> action) {
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            try (AutoCloseable closeable = entityManager::close) {
+                EntityTransaction tx = entityManager.getTransaction();
+                tx.begin();
+                ValueAndExecutionMetadata<T> result;
+                try {
+                    result = ValueAndExecutionMetadata.run(entityManager, action);
+                } catch (Exception e) {
+                    try {
+                        tx.rollback();
+                    } catch (Exception e2) {
+                        e.addSuppressed(e2);
+                    }
+                    throw e;
+                }
+                tx.commit();
+                return result;
+            } catch (Exception e) {
+                throw new IllegalStateException("Unexpected exception: " + e.getMessage(), e);
+            }
+        }
+    }
+
+}

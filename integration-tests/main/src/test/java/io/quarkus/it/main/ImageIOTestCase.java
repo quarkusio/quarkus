eddd274fc9f9142b76d244b31bab37f7e785f5b3
Foivos Zakkak <fzakkak@redhat.com>
2021-01-14 16:22:24 +0200

    Revert "Disable Image IO tests for now"
    This reverts commit 1fb47db670655e44ad174c907f5ee6cf53b7160c.

--- integration-tests/main/src/test/java/io/quarkus/it/main/ImageIOTestCase.java
+++ integration-tests/main/src/test/java/io/quarkus/it/main/ImageIOTestCase.java
@@ -3,7 +3,6 @@ package io.quarkus.it.main;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -12,7 +11,6 @@ import io.quarkus.test.junit.QuarkusTest;
 public class ImageIOTestCase {
 
     @Test
-    @Disabled("Please bring this back when we upgrade from GraalVM 20.3 to GraalVM 21")
     public void testImageRead() {
         when().get("/core/imageio").then().body(equalTo("1x1"));
     }

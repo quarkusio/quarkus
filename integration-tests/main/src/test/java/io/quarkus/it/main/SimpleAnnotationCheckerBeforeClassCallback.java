900c90c5805928495679fd791acaebacec6a0898
Georgios Andrianakis <geoand@gmail.com>
2021-01-18 00:33:57 +0200

    Ensure that QuarkusTestBeforeClassCallback is run on the Quarkus Classloader

--- integration-tests/main/src/test/java/io/quarkus/it/main/SimpleAnnotationCheckerBeforeClassCallback.java
+++ integration-tests/main/src/test/java/io/quarkus/it/main/SimpleAnnotationCheckerBeforeClassCallback.java
@@ -2,6 +2,8 @@ package io.quarkus.it.main;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.junit.jupiter.api.Assertions;
+
 import io.quarkus.test.junit.callback.QuarkusTestBeforeClassCallback;
 
 public class SimpleAnnotationCheckerBeforeClassCallback implements QuarkusTestBeforeClassCallback {
@@ -10,11 +12,17 @@ public class SimpleAnnotationCheckerBeforeClassCallback implements QuarkusTestBe
 
     @Override
     public void beforeClass(Class<?> testClass) {
+        assertQuarkusClassLoader(Thread.currentThread().getContextClassLoader());
+        assertQuarkusClassLoader(testClass.getClassLoader());
         // make sure that this comes into play only for the test we care about
         if (!testClass.getName().endsWith("QuarkusTestCallbacksTestCase")) {
             return;
         }
 
         count.incrementAndGet();
+    }
+
+    private void assertQuarkusClassLoader(ClassLoader contextClassLoader) {
+        Assertions.assertTrue(contextClassLoader.getClass().getName().contains("QuarkusClassLoader"));
     }
 }

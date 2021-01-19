32eb1b15aa27d79e7d08dde70c316bf236fc4a67
SaumyaSingh1 <ssaumyas100@gmail.com>
2021-01-19 08:31:13 +0530

    Adding Additional Endpoints

--- extensions/resteasy-reactive/quarkus-resteasy-reactive/deployment/src/main/java/io/quarkus/resteasy/reactive/server/deployment/devconsole/DevConsoleProcessor.java
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive/deployment/src/main/java/io/quarkus/resteasy/reactive/server/deployment/devconsole/DevConsoleProcessor.java
@@ -1,15 +1,26 @@
 package io.quarkus.resteasy.reactive.server.deployment.devconsole;
 
+import java.util.List;
+
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.devconsole.spi.DevConsoleRuntimeTemplateInfoBuildItem;
+import io.quarkus.devconsole.spi.DevConsoleTemplateInfoBuildItem;
 import io.quarkus.resteasy.reactive.server.runtime.EndpointScoresSupplier;
+import io.quarkus.vertx.http.deployment.devmode.NotFoundPageDisplayableEndpointBuildItem;
 
 public class DevConsoleProcessor {
 
     @BuildStep(onlyIf = IsDevelopment.class)
     public DevConsoleRuntimeTemplateInfoBuildItem collectScores() {
         return new DevConsoleRuntimeTemplateInfoBuildItem("endpointScores", new EndpointScoresSupplier());
+    }
+
+    @BuildStep(onlyIf = IsDevelopment.class)
+    public DevConsoleTemplateInfoBuildItem collectAdditionalEndpoints(
+            List<NotFoundPageDisplayableEndpointBuildItem> additionalEndpoint) {
+
+        return new DevConsoleTemplateInfoBuildItem("additionalEndpointInfo", additionalEndpoint);
     }
 
 }

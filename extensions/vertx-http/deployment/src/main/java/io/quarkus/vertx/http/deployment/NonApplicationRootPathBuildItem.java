11141f5cb6ff140fe16e5e843b0fa0d2e238e428
Jan Martiška <jmartisk@redhat.com>
2021-01-22 14:11:58 +0100

    Fix the path to MetricsRequestHandler when quarkus.http.root-path is set

--- extensions/vertx-http/deployment/src/main/java/io/quarkus/vertx/http/deployment/NonApplicationRootPathBuildItem.java
+++ extensions/vertx-http/deployment/src/main/java/io/quarkus/vertx/http/deployment/NonApplicationRootPathBuildItem.java
@@ -3,14 +3,20 @@ package io.quarkus.vertx.http.deployment;
 import io.quarkus.builder.item.SimpleBuildItem;
 
 public final class NonApplicationRootPathBuildItem extends SimpleBuildItem {
+    private final String httpRootPath;
     private final String frameworkRootPath;
     private final boolean separateRoot;
 
     public NonApplicationRootPathBuildItem(String frameworkRootPath) {
+        this(frameworkRootPath, null);
+    }
+
+    public NonApplicationRootPathBuildItem(String frameworkRootPath, String httpRootPath) {
         this.frameworkRootPath = frameworkRootPath;
         this.separateRoot = frameworkRootPath != null
                 && !frameworkRootPath.equals("")
                 && !frameworkRootPath.equals("/");
+        this.httpRootPath = httpRootPath;
     }
 
     public String getFrameworkRootPath() {
@@ -21,6 +27,9 @@ public final class NonApplicationRootPathBuildItem extends SimpleBuildItem {
         return separateRoot;
     }
 
+    /**
+     * Adjusts a path by including the non-application root path.
+     */
     public String adjustPath(String path) {
         if (!path.startsWith("/")) {
             throw new IllegalArgumentException("Path must start with /");
@@ -29,5 +38,17 @@ public final class NonApplicationRootPathBuildItem extends SimpleBuildItem {
             return path;
         }
         return frameworkRootPath + path;
+    }
+
+    /**
+     * Adjusts a path by including both the non-application root path and
+     * the HTTP root path.
+     */
+    public String adjustPathIncludingHttpRootPath(String path) {
+        String withFrameWorkPath = adjustPath(path);
+        if (httpRootPath == null || httpRootPath.equals("/")) {
+            return withFrameWorkPath;
+        }
+        return httpRootPath + withFrameWorkPath;
     }
 }

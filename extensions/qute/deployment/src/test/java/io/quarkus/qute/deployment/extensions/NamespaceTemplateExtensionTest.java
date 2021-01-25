7cbd69e6b6dd824f76c8eda517427487bb59819d
Martin Kouba <mkouba@redhat.com>
2021-01-25 16:27:58 +0100

    Qute - support multiple namespace extension methods on the same class
    (cherry picked from commit feafa1cb606524e876f3b8dae617e0013517251a)

--- extensions/qute/deployment/src/test/java/io/quarkus/qute/deployment/extensions/NamespaceTemplateExtensionTest.java
+++ extensions/qute/deployment/src/test/java/io/quarkus/qute/deployment/extensions/NamespaceTemplateExtensionTest.java
@@ -3,6 +3,7 @@ package io.quarkus.qute.deployment.extensions;
 import static io.quarkus.qute.TemplateExtension.ANY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import javax.enterprise.event.TransactionPhase;
 import javax.inject.Inject;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -36,6 +37,8 @@ public class NamespaceTemplateExtensionTest {
                 engine.parse("{str:foolish('hello')}").render());
         assertEquals("ONE=ONE",
                 engine.parse("{MyEnum:ONE}={MyEnum:one}").render());
+        assertEquals("IN_PROGRESS=0",
+                engine.parse("{txPhase:IN_PROGRESS}={txPhase:IN_PROGRESS.ordinal}").render());
     }
 
     @TemplateExtension(namespace = "str")
@@ -68,6 +71,11 @@ public class NamespaceTemplateExtensionTest {
         @TemplateExtension(namespace = "MyEnum", matchName = ANY)
         static MyEnum getVal(String val) {
             return MyEnum.valueOf(val.toUpperCase());
+        }
+
+        @TemplateExtension(namespace = "txPhase", matchName = "*")
+        static TransactionPhase enumValue(String value) {
+            return TransactionPhase.valueOf(value);
         }
 
     }

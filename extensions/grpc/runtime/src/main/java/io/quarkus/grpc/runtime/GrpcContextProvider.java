270cab853bb7e506dbc5dd4643de4e4eec99d442
Gaétan Collaud <gaetancollaud@gmail.com>
2021-01-28 20:34:44 +0100

    add GrpcContextProvider

---
+++ extensions/grpc/runtime/src/main/java/io/quarkus/grpc/runtime/GrpcContextProvider.java
@@ -0,0 +1,43 @@
+package io.quarkus.grpc.runtime;
+
+import io.grpc.Context;
+import org.eclipse.microprofile.context.spi.ThreadContextProvider;
+import org.eclipse.microprofile.context.spi.ThreadContextSnapshot;
+
+import java.util.Map;
+
+public class GrpcContextProvider implements ThreadContextProvider {
+
+    private static final String GRPC_CONTEXT = "GRPC";
+
+    @Override
+    public ThreadContextSnapshot currentContext(Map<String, String> props) {
+        final Context currentContext = Context.current();
+        if (currentContext == null) {
+            return null;
+        }
+        return () -> {
+            // store the context before attaching the saved context
+            final Context previousContext = Context.current();
+
+            // attach the saved context
+            currentContext.attach();
+            return () -> {
+                // restore the previous context
+                previousContext.attach();
+            };
+        };
+    }
+
+    @Override
+    public ThreadContextSnapshot clearedContext(Map<String, String> props) {
+        // nothing to do here
+        return () -> () -> {
+        };
+    }
+
+    @Override
+    public String getThreadContextType() {
+        return GRPC_CONTEXT;
+    }
+}

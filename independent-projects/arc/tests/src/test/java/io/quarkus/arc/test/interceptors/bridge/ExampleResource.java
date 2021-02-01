1f51a8458276828e93ead510d5b53ad7c21817ac
Martin Kouba <mkouba@redhat.com>
2021-02-01 11:59:15 +0100

    ArC intercepted subclasses - fix bridge methods processing
    - skip methods overriden by bridge methods
    - do not skip a bridge method for which an "impl. method" is not
    declared on the class
    
    Co-authored-by: Matej Novotny <manovotn@redhat.com>
    (cherry picked from commit e42aff347e7d2053670a80a18e25f47b077ebfd4)

---
+++ independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/interceptors/bridge/ExampleResource.java
@@ -0,0 +1,15 @@
+package io.quarkus.arc.test.interceptors.bridge;
+
+import java.util.List;
+import javax.inject.Singleton;
+
+@Singleton
+@Simple
+public class ExampleResource extends AbstractResource<String> implements ExampleApi {
+
+    // Just to try to confuse our bridge method impl. algorithm 
+    public String create(List<Object> dtos) {
+        return dtos.toString();
+    }
+
+}

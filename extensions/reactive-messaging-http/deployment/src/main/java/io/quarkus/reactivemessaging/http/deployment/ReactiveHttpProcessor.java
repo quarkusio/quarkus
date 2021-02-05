6c42565e627ec6af74499ca01593e8c6bbcde05f
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2021-02-05 13:59:30 +0100

    Reactive Messaging HTTP: auto register payload classes for reflection

--- extensions/reactive-messaging-http/deployment/src/main/java/io/quarkus/reactivemessaging/http/deployment/ReactiveHttpProcessor.java
+++ extensions/reactive-messaging-http/deployment/src/main/java/io/quarkus/reactivemessaging/http/deployment/ReactiveHttpProcessor.java
@@ -1,17 +1,39 @@
 package io.quarkus.reactivemessaging.http.deployment;
 
+import static io.quarkus.arc.processor.DotNames.OBJECT;
+import static io.quarkus.arc.processor.DotNames.STRING;
+import static java.util.Arrays.asList;
+
 import java.lang.reflect.Modifier;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.streams.operators.ProcessorBuilder;
+import org.eclipse.microprofile.reactive.streams.operators.PublisherBuilder;
+import org.eclipse.microprofile.reactive.streams.operators.SubscriberBuilder;
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.MethodInfo;
+import org.jboss.jandex.ParameterizedType;
+import org.jboss.jandex.Type;
+import org.reactivestreams.Processor;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.arc.deployment.BeanArchiveIndexBuildItem;
 import io.quarkus.arc.deployment.GeneratedBeanBuildItem;
 import io.quarkus.arc.deployment.GeneratedBeanGizmoAdaptor;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.gizmo.ClassCreator;
 import io.quarkus.gizmo.ClassOutput;
 import io.quarkus.gizmo.MethodCreator;
@@ -33,10 +55,24 @@ import io.quarkus.reactivemessaging.http.runtime.serializers.Serializer;
 import io.quarkus.reactivemessaging.http.runtime.serializers.SerializerFactoryBase;
 import io.quarkus.vertx.http.deployment.BodyHandlerBuildItem;
 import io.quarkus.vertx.http.deployment.RouteBuildItem;
+import io.smallrye.mutiny.Multi;
 import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 
 public class ReactiveHttpProcessor {
+
+    private static final DotName JSON_ARRAY = DotName.createSimple(JsonArray.class.getName());
+    private static final DotName JSON_OBJECT = DotName.createSimple(JsonObject.class.getName());
+    private static final DotName MESSAGE = DotName.createSimple(Message.class.getName());
+    private static final DotName MULTI = DotName.createSimple(Multi.class.getName());
+    private static final DotName PROCESSOR = DotName.createSimple(Processor.class.getName());
+    private static final DotName PROCESSOR_BUILDER = DotName.createSimple(ProcessorBuilder.class.getName());
+    private static final DotName PUBLISHER = DotName.createSimple(Publisher.class.getName());
+    private static final DotName PUBLISHER_BUILDER = DotName.createSimple(PublisherBuilder.class.getName());
+    private static final DotName SUBSCRIBER = DotName.createSimple(Subscriber.class.getName());
+    private static final DotName SUBSCRIBER_BUILDER = DotName.createSimple(SubscriberBuilder.class.getName());
 
     @BuildStep
     @Record(ExecutionTime.RUNTIME_INIT)
@@ -80,6 +116,69 @@ public class ReactiveHttpProcessor {
         }
 
         initSerializers(ReactiveHttpConfig.readSerializers(), generatedBeanProducer);
+    }
+
+    @BuildStep
+    void registerMessagePayloadClassesForReflection(BeanArchiveIndexBuildItem index,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClasses) {
+
+        Set<String> payloadClasses = new HashSet<>();
+        for (AnnotationInstance incoming : index.getIndex().getAnnotations(DotName.createSimple(Incoming.class.getName()))) {
+            MethodInfo methodInfo = incoming.target().asMethod();
+            List<Type> parameters = methodInfo.parameters();
+
+            if (parameters.size() == 1) {
+                Type type = parameters.get(0);
+                // payload can be consumed as Publisher[Builder]<PayloadObject> or Publisher[Builder]<Message<PayloadObject>>
+                // or Multi<PayloadObject>, Multi<Message<PayloadObject>>
+                DotName typeName = type.name();
+                if (type.kind() == Type.Kind.PARAMETERIZED_TYPE
+                        && (typeName.equals(PUBLISHER_BUILDER) || typeName.equals(PUBLISHER) || typeName.equals(MULTI))) {
+                    List<Type> arguments = type.asParameterizedType().arguments();
+                    if (arguments.size() > 0) {
+                        collectPayloadType(payloadClasses, arguments.get(0));
+                    }
+                } else {
+                    collectPayloadType(payloadClasses, type);
+                }
+            } else if (parameters.size() == 0) {
+                // @Incoming method can also return a Subscriber[Builder] or Processor[Builder] for message payloads:
+                Type returnType = methodInfo.returnType();
+                if ((returnType.name().equals(SUBSCRIBER_BUILDER)
+                        || returnType.name().equals(PROCESSOR_BUILDER)
+                        || returnType.name().equals(SUBSCRIBER)
+                        || returnType.name().equals(PROCESSOR))
+                        && returnType.kind() == Type.Kind.PARAMETERIZED_TYPE) {
+                    ParameterizedType parameterizedType = returnType.asParameterizedType();
+                    List<Type> arguments = parameterizedType.arguments();
+                    if (arguments.size() > 0) {
+                        collectPayloadType(payloadClasses, arguments.get(0));
+                    }
+                }
+            }
+        }
+
+        payloadClasses.removeAll(asList(JSON_OBJECT.toString(), OBJECT.toString(), JSON_ARRAY.toString(), STRING.toString()));
+
+        reflectiveClasses.produce(new ReflectiveClassBuildItem(true, false, payloadClasses.toArray(new String[] {})));
+    }
+
+    private void collectPayloadType(Set<String> payloadClasses, Type type) {
+        if (type.kind() != Type.Kind.CLASS && type.kind() != Type.Kind.PARAMETERIZED_TYPE) {
+            return;
+        }
+        if (type.name().equals(MESSAGE)) {
+            // wrapped in a message
+            if (type.kind() == Type.Kind.PARAMETERIZED_TYPE) {
+                Type payloadType = type.asParameterizedType().arguments().get(0);
+                if (payloadType.kind() == Type.Kind.CLASS) {
+                    payloadClasses.add(payloadType.name().toString());
+                }
+            }
+        } else {
+            // or used directly
+            payloadClasses.add(type.name().toString());
+        }
     }
 
     private void initSerializers(List<String> serializers, BuildProducer<GeneratedBeanBuildItem> generatedBeans) {
---
+++ extensions/reactive-messaging-http/deployment/src/test/java/io/quarkus/reactivemessaging/http/RegistrationForReflectionTest.java
@@ -0,0 +1,236 @@
+package io.quarkus.reactivemessaging.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.eclipse.microprofile.reactive.streams.operators.ProcessorBuilder;
+import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
+import org.eclipse.microprofile.reactive.streams.operators.SubscriberBuilder;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.reactivestreams.Processor;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+import io.quarkus.builder.BuildChainBuilder;
+import io.quarkus.builder.BuildContext;
+import io.quarkus.builder.BuildStep;
+import io.quarkus.deployment.builditem.GeneratedResourceBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.quarkus.test.QuarkusUnitTest;
+import io.smallrye.mutiny.Multi;
+
+public class RegistrationForReflectionTest {
+
+    private static volatile List<ReflectiveClassBuildItem> registeredClasses;
+
+    @RegisterExtension
+    static QuarkusUnitTest runner = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class))
+            .addBuildChainCustomizer(buildCustomizer());
+
+    @Test
+    void shouldRegisterPayloadTypesForReflection() {
+        // if it gets there, it succeeded
+    }
+
+    private static void checkProperClassesAreRegistered() {
+        assertThat(registeredClasses)
+                .overridingErrorMessage("buildCustomizer failed to collect registered classes")
+                .isNotNull();
+
+        List<String> allRegisteredClasses = registeredClasses.stream()
+                .flatMap(c -> c.getClassNames().stream())
+                .collect(Collectors.toList());
+
+        List<String> expectedClasses = Stream
+                .of(PT1.class, PT2.class, PT3.class, PT4.class, PT5.class, PT6.class, PT7.class,
+                        PT8.class,
+                        PT9.class, PT10.class, PT11.class, PT12.class)
+                .map(Class::getName).collect(Collectors.toList());
+
+        assertThat(allRegisteredClasses).containsAll(expectedClasses);
+    }
+
+    private static Consumer<BuildChainBuilder> buildCustomizer() {
+        return new Consumer<BuildChainBuilder>() {
+            @Override
+            public void accept(BuildChainBuilder chainBuilder) {
+                chainBuilder.addBuildStep(
+                        new BuildStep() {
+                            @Override
+                            public void execute(BuildContext context) {
+                                registeredClasses = context.consumeMulti(ReflectiveClassBuildItem.class);
+                                checkProperClassesAreRegistered();
+                            }
+                        }).consumes(ReflectiveClassBuildItem.class)
+                        .produces(GeneratedResourceBuildItem.class).build();
+            }
+        };
+    }
+
+    @ApplicationScoped
+    static class ConsumerBean {
+        @Incoming("c1")
+        void consume(PT1 payload) {
+
+        }
+
+        @Incoming("c2")
+        CompletionStage<Void> consume(Message<PT2> payload) {
+            return new CompletableFuture<>();
+        }
+
+        @Incoming("c3")
+        @Outgoing("c5")
+        Multi<PT5> consume(Multi<PT3> payload) {
+            return payload.onItem().transform(o -> new PT5());
+        }
+
+        @Incoming("c4")
+        @Outgoing("c6")
+        Multi<Message<PT6>> consumeMultiWithMessage(Multi<Message<PT4>> payload) {
+            return payload.onItem().transform(msg -> Message.of(new PT6()));
+        }
+
+        @Incoming("c5")
+        Subscriber<PT5> consumeWithSubscriber() {
+            return createSubscriber();
+        }
+
+        @Incoming("c6")
+        Subscriber<Message<PT6>> consumeWithSubscriberWithMessage() {
+            return createSubscriber();
+        }
+
+        @Incoming("c7")
+        SubscriberBuilder<PT7, Void> consumeWithSubscriberBuilder() {
+            return ReactiveStreams.<PT7> builder().ignore();
+        }
+
+        @Incoming("c8")
+        SubscriberBuilder<Message<PT8>, Void> consumeWithSubscriberBuilderOfMsg() {
+            return ReactiveStreams.<Message<PT8>> builder().ignore();
+        }
+
+        @Incoming("c9")
+        @Outgoing("c1")
+        Processor<PT9, PT1> consumeWithProcessor() {
+            return createProcessor(new PT1());
+        }
+
+        @Incoming("c10")
+        @Outgoing("c2")
+        Processor<Message<PT10>, PT2> consumeWithProcessorOfMsg() {
+            return createProcessor(new PT2());
+        }
+
+        @Incoming("c11")
+        @Outgoing("c3")
+        ProcessorBuilder<PT11, PT3> consumeWithProcessorBuilder() {
+            return ReactiveStreams.<PT11> builder().map(o -> new PT3());
+        }
+
+        @Incoming("c12")
+        @Outgoing("c4")
+        ProcessorBuilder<Message<PT12>, Message<PT4>> consumeWithProcessorBuilderOfMsg() {
+            return ReactiveStreams.<Message<PT12>> builder().map(o -> Message.of(new PT4()));
+        }
+
+        @Outgoing("c7")
+        Publisher<PT7> producer7() {
+            return createPublisher(new PT7());
+        }
+
+        @Outgoing("c8")
+        Publisher<PT8> producer8() {
+            return createPublisher(new PT8());
+        }
+
+        @Outgoing("c9")
+        Publisher<PT9> producer9() {
+            return createPublisher(new PT9());
+        }
+
+        @Outgoing("c10")
+        Publisher<PT10> producer10() {
+            return createPublisher(new PT10());
+        }
+
+        @Outgoing("c11")
+        Publisher<PT11> producer11() {
+            return createPublisher(new PT11());
+        }
+
+        @Outgoing("c12")
+        Publisher<PT12> producer12() {
+            return createPublisher(new PT12());
+        }
+
+        <T> Publisher<T> createPublisher(T val) {
+            return ReactiveStreams.of(val)
+                    .buildRs();
+        }
+
+        <V, T> Processor<V, T> createProcessor(T val) {
+            return ReactiveStreams.<V> builder()
+                    .map(m -> val)
+                    .buildRs();
+        }
+
+        Subscriber createSubscriber() {
+            return ReactiveStreams.builder()
+                    .buildRs();
+        }
+    }
+
+    static class PT1 {
+    }
+
+    static class PT2 {
+    }
+
+    static class PT3 {
+    }
+
+    static class PT4 {
+    }
+
+    static class PT5 {
+    }
+
+    static class PT6 {
+    }
+
+    static class PT7 {
+    }
+
+    static class PT8 {
+    }
+
+    static class PT9 {
+    }
+
+    static class PT10 {
+    }
+
+    static class PT11 {
+    }
+
+    static class PT12 {
+    }
+
+}

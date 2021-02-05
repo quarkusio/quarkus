9a733bf7ae4b085e5c452aab793b7e47594e8fdb
Georgios Andrianakis <geoand@gmail.com>
2021-02-06 00:42:11 +0200

    Fix another issue with JDBC driver registration and test restart
    This fix is pretty terrible, but I don't see any other way around
    the problem
    
    Fixes: #14829

--- independent-projects/bootstrap/core/src/main/java/io/quarkus/bootstrap/classloading/DriverRemover.java
+++ independent-projects/bootstrap/core/src/main/java/io/quarkus/bootstrap/classloading/DriverRemover.java
@@ -1,5 +1,6 @@
 package io.quarkus.bootstrap.classloading;
 
+import java.lang.reflect.Field;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -29,5 +30,22 @@ public class DriverRemover implements Runnable {
                 log.error("Failed to deregister driver", t);
             }
         }
+
+        // this really sucks because it triggers an illegal access warning, but there is no other way
+        // to force the DriverManager to re-initialize drivers
+        try {
+            Field[] declaredFields = DriverManager.class.getDeclaredFields();
+            // go through the fields instead of accessing it immediately because in Java 8 this field does not exist
+            for (Field field : declaredFields) {
+                if (field.getName().equals("driversInitialized")) {
+                    field.setAccessible(true);
+                    field.set(null, false);
+                    break;
+                }
+            }
+        } catch (IllegalAccessException t) {
+            log.debug("Failed to clear initialization state of drivers", t);
+        }
+
     }
 }

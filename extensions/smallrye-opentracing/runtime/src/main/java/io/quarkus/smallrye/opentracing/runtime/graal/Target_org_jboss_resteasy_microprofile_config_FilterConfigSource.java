a66b331e34cecc0aeb750b99b9e193c081f48158
Georgios Andrianakis <geoand@gmail.com>
2021-02-10 09:38:31 +0200

    Remove field that can cause GraalVM to fail
    Related to: https://quarkusio.zulipchat.com/#narrow/stream/187038-dev/topic/camel.20.2B.20opentracing.20.2B.20quarkus.20snapshot

--- extensions/smallrye-opentracing/runtime/src/main/java/io/quarkus/smallrye/opentracing/runtime/graal/Target_org_jboss_resteasy_microprofile_config_FilterConfigSource.java
+++ extensions/smallrye-opentracing/runtime/src/main/java/io/quarkus/smallrye/opentracing/runtime/graal/Target_org_jboss_resteasy_microprofile_config_FilterConfigSource.java
@@ -9,13 +9,11 @@ import com.oracle.svm.core.annotate.TargetClass;
 /**
  * see {@link Target_org_jboss_resteasy_microprofile_config_ServletContextConfigSource}
  */
-@TargetClass(className = Target_org_jboss_resteasy_microprofile_config_FilterConfigSource.FILTER_CONFIG_SOURCE_NAME, onlyWith = {
+@TargetClass(className = "org.jboss.resteasy.microprofile.config.FilterConfigSource", onlyWith = {
         UndertowMissing.class,
         Target_org_jboss_resteasy_microprofile_config_FilterConfigSource.FilterConfigSourceIsLoaded.class
 })
 final class Target_org_jboss_resteasy_microprofile_config_FilterConfigSource {
-
-    static final String FILTER_CONFIG_SOURCE_NAME = "org.jboss.resteasy.microprofile.config.FilterConfigSource";
 
     @Alias
     @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.FromAlias)
@@ -30,7 +28,7 @@ final class Target_org_jboss_resteasy_microprofile_config_FilterConfigSource {
         @Override
         public boolean getAsBoolean() {
             try {
-                Class.forName(FILTER_CONFIG_SOURCE_NAME);
+                Class.forName("org.jboss.resteasy.microprofile.config.FilterConfigSource");
                 return true;
             } catch (ClassNotFoundException e) {
                 return false;

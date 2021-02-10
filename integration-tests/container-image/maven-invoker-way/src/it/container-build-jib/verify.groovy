3d6fed1e283e910c55cd5aa3bb661be690c19d3c
Georgios Andrianakis <geoand@gmail.com>
2021-02-10 20:14:52 +0200

    Write metadata file containing the information about the last built artifact

--- integration-tests/container-image/maven-invoker-way/src/it/container-build-jib/verify.groovy
+++ integration-tests/container-image/maven-invoker-way/src/it/container-build-jib/verify.groovy
@@ -1,5 +1,7 @@
 import io.quarkus.deployment.util.ExecUtil
 
+import java.nio.file.Paths
+
 try {
     ExecUtil.exec("docker", "version", "--format", "'{{.Server.Version}}'")
 } catch (Exception ignored) {
@@ -10,3 +12,17 @@ try {
 String group = System.getProperty("user.name")
 assert ExecUtil.exec("docker", "images", group + "/container-build-jib")
 assert ExecUtil.exec("docker", "rmi", group + "/container-build-jib:0.1-SNAPSHOT")
+
+File propertiesFile =
+        System.getProperty("user.dir").endsWith("maven-invoker-way")
+                ? Paths.get("target", "it", "container-build-jib", "target", "quarkus-artifact.properties").toFile()
+                : Paths.get("integration-tests", "container-image", "maven-invoker-way", "target", "it", "container-build-jib", "target", "quarkus-artifact.properties").toFile()
+Properties properties = new Properties()
+propertiesFile.withInputStream {
+    properties.load(it)
+}
+
+
+
+assert properties.type == 'jar-container'
+assert properties."metadata.container-image" == group + "/container-build-jib:0.1-SNAPSHOT"

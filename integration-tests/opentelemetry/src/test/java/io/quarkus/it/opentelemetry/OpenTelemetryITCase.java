9a652505fb10b88302dc8d6edbb94a0129548e05
Ken Finnigan <ken@kenfinnigan.me>
2021-02-11 08:32:03 -0500

    Add OpenTelemetry and Jaeger Exporter extensions with native support
    Resolves #10499

---
+++ integration-tests/opentelemetry/src/test/java/io/quarkus/it/opentelemetry/OpenTelemetryITCase.java
@@ -0,0 +1,19 @@
+package io.quarkus.it.opentelemetry;
+
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.OpenTelemetrySdkBuilder;
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class OpenTelemetryITCase extends OpenTelemetryTestCase {
+    @Override
+    protected void buildGlobalTelemetryInstance() {
+        // When running native tests the test class is outside the Quarkus application,
+        // so we need to set the propagator on the GlobalOpenTelemetry instance
+        OpenTelemetrySdkBuilder builder = OpenTelemetrySdk.builder();
+        builder.setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()));
+        builder.buildAndRegisterGlobal();
+    }
+}

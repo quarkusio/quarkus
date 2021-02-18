quarkusio/quarkus

commit 2b01ca7b0b3287db87ee98dd62fff2f426bff3e9

Qute - improve Java array support
- make it possible to get the length of the specified array and access
the elements directly via an index value
- support array-based template parameter declarations

extensions/qute/deployment/src/main/java/io/quarkus/qute/deployment/QuteProcessor.java
@@ -28,6 +28,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -45,6 +46,7 @@ import org.jboss.jandex.FieldInfo;
 import org.jboss.jandex.IndexView;
 import org.jboss.jandex.MethodInfo;
 import org.jboss.jandex.ParameterizedType;
+import org.jboss.jandex.PrimitiveType;
 import org.jboss.jandex.Type;
 import org.jboss.jandex.TypeVariable;
 import org.jboss.logging.Logger;
@@ -572,55 +574,100 @@ public class QuteProcessor {
         }
 
         while (iterator.hasNext()) {
-            // Now iterate over all parts of the expression and check each part against the current "match class"
+            // Now iterate over all parts of the expression and check each part against the current match type
             Info info = iterator.next();
             if (!match.isEmpty()) {
-                // By default, we only consider properties
-                Set<String> membersUsed = implicitClassToMembersUsed.get(match.clazz().name());
-                if (membersUsed == null) {
-                    membersUsed = new HashSet<>();
-                    implicitClassToMembersUsed.put(match.clazz().name(), membersUsed);
-                }
-                AnnotationTarget member = null;
-                // First try to find a java member
-                if (info.isVirtualMethod()) {
-                    member = findMethod(info.part.asVirtualMethod(), match.clazz(), expression, index, templateIdToPathFun,
-                            results);
-                    if (member != null) {
-                        membersUsed.add(member.asMethod().name());
+
+                // Arrays are handled specifically
+                // We use the built-in resolver at runtime because the extension methods cannot be used to cover all combinations of dimensions and component types 
+                if (match.isArray()) {
+                    if (info.isProperty()) {
+                        String name = info.asProperty().name;
+                        if (name.equals("length")) {
+                            // myArray.length
+                            match.setValues(null, PrimitiveType.INT);
+                            continue;
+                        } else {
+                            // myArray[0], myArray.1
+                            try {
+                                Integer.parseInt(name);
+                                match.setValues(null, match.type().asArrayType().component());
+                                continue;
+                            } catch (NumberFormatException e) {
+                                // not an integer index
+                            }
+                        }
+                    } else if (info.isVirtualMethod() && info.asVirtualMethod().name.equals("get")) {
+                        // array.get(84)
+                        List<Expression> params = info.asVirtualMethod().part.asVirtualMethod().getParameters();
+                        if (params.size() == 1) {
+                            Expression param = params.get(0);
+                            Object literalValue;
+                            try {
+                                literalValue = param.getLiteralValue().get();
+                            } catch (InterruptedException | ExecutionException e) {
+                                literalValue = null;
+                            }
+                            if (literalValue == null || literalValue instanceof Integer) {
+                                match.setValues(null, match.type().asArrayType().component());
+                                continue;
+                            }
+                        }
                     }
-                } else if (info.isProperty()) {
-                    member = findProperty(info.asProperty().name, match.clazz(), index);
-                    if (member != null) {
-                        membersUsed.add(member.kind() == Kind.FIELD ? member.asField().name() : member.asMethod().name());
-                    }
-                }
-                if (member == null) {
-                    // Then try to find an etension method
-                    member = findTemplateExtensionMethod(info, match.clazz(), templateExtensionMethods, expression,
-                            index,
-                            templateIdToPathFun, results);
                 }
 
-                if (member == null) {
-                    // Test whether the validation should be skipped
-                    TypeCheck check = new TypeCheck(info.isProperty() ? info.asProperty().name : info.asVirtualMethod().name,
-                            match.clazz(),
-                            info.part.isVirtualMethod() ? info.part.asVirtualMethod().getParameters().size() : -1);
-                    if (isExcluded(check, excludes)) {
-                        LOGGER.debugf(
-                                "Expression part [%s] excluded from validation of [%s] against class [%s]",
-                                info.value,
-                                expression.toOriginalString(), match.clazz());
-                        match.clearValues();
-                        break;
+                AnnotationTarget member = null;
+
+                if (!match.isPrimitive()) {
+                    Set<String> membersUsed = implicitClassToMembersUsed.get(match.type().name());
+                    if (membersUsed == null) {
+                        membersUsed = new HashSet<>();
+                        implicitClassToMembersUsed.put(match.type().name(), membersUsed);
+                    }
+                    // First try to find a java member
+                    if (match.clazz() != null) {
+                        if (info.isVirtualMethod()) {
+                            member = findMethod(info.part.asVirtualMethod(), match.clazz(), expression, index,
+                                    templateIdToPathFun,
+                                    results);
+                            if (member != null) {
+                                membersUsed.add(member.asMethod().name());
+                            }
+                        } else if (info.isProperty()) {
+                            member = findProperty(info.asProperty().name, match.clazz(), index);
+                            if (member != null) {
+                                membersUsed
+                                        .add(member.kind() == Kind.FIELD ? member.asField().name() : member.asMethod().name());
+                            }
+                        }
+                    }
+                    if (member == null) {
+                        // Then try to find an etension method
+                        member = findTemplateExtensionMethod(info, match.type(), templateExtensionMethods, expression,
+                                index,
+                                templateIdToPathFun, results);
+                    }
+                    if (member == null) {
+                        // Test whether the validation should be skipped
+                        TypeCheck check = new TypeCheck(
+                                info.isProperty() ? info.asProperty().name : info.asVirtualMethod().name,
+                                match.clazz(),
+                                info.part.isVirtualMethod() ? info.part.asVirtualMethod().getParameters().size() : -1);
+                        if (isExcluded(check, excludes)) {
+                            LOGGER.debugf(
+                                    "Expression part [%s] excluded from validation of [%s] against type [%s]",
+                                    info.value,
+                                    expression.toOriginalString(), match.type());
+                            match.clearValues();
+                            break;
+                        }
                     }
                 }
 
                 if (member == null) {
                     // No member found - incorrect expression
                     incorrectExpressions.produce(new IncorrectExpressionBuildItem(expression.toOriginalString(),
-                            info.value, match.clazz().toString(), expression.getOrigin()));
+                            info.value, match.type().toString(), expression.getOrigin()));
                     match.clearValues();
                     break;
                 } else {
@@ -637,9 +684,6 @@ public class QuteProcessor {
                             processHints(templateAnalysis, hint, match, index, expression, generatedIdsToMatches,
                                     incorrectExpressions);
                         }
-                    }
-                    if (match.isPrimitive()) {
-                        break;
                     }
                 }
             } else {
@@ -736,8 +780,7 @@ public class QuteProcessor {
             matchRegex = matchRegexValue.asString();
         }
         extensionMethods.produce(new TemplateExtensionMethodBuildItem(method, matchName, matchRegex,
-                namespace.isEmpty() ? index.getClassByName(method.parameters().get(0).name()) : null,
-                priority, namespace));
+                method.parameters().get(0), priority, namespace));
     }
 
     private void validateInjectExpression(TemplateAnalysis templateAnalysis, Expression expression, IndexView index,
@@ -1352,7 +1395,8 @@ public class QuteProcessor {
         }
 
         boolean isEmpty() {
-            return clazz == null;
+            // For arrays the class is null 
+            return type == null;
         }
 
         void autoExtractType() {
@@ -1371,7 +1415,7 @@ public class QuteProcessor {
         }
     }
 
-    private static AnnotationTarget findTemplateExtensionMethod(Info info, ClassInfo matchClass,
+    private static AnnotationTarget findTemplateExtensionMethod(Info info, Type matchType,
             List<TemplateExtensionMethodBuildItem> templateExtensionMethods, Expression expression, IndexView index,
             Function<String, String> templateIdToPathFun, Map<String, Match> results) {
         if (!info.isProperty() && !info.isVirtualMethod()) {
@@ -1383,7 +1427,7 @@ public class QuteProcessor {
                 // Skip namespace extensions
                 continue;
             }
-            if (!Types.isAssignableFrom(extensionMethod.getMatchClass().name(), matchClass.name(), index)) {
+            if (!Types.isAssignableFrom(extensionMethod.getMatchType(), matchType, index)) {
                 // If "Bar extends Foo" then Bar should be matched for the extension method "int get(Foo)"   
                 continue;
             }

extensions/qute/deployment/src/main/java/io/quarkus/qute/deployment/TemplateExtensionMethodBuildItem.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.MethodInfo;
+import org.jboss.jandex.Type;
 
 import io.quarkus.builder.item.MultiBuildItem;
 import io.quarkus.qute.TemplateExtension;
@@ -19,17 +20,16 @@ public final class TemplateExtensionMethodBuildItem extends MultiBuildItem {
     private final String matchName;
     private final String matchRegex;
     private final Pattern matchPattern;
-    private final ClassInfo matchClass;
+    private final Type matchType;
     private final int priority;
     private final String namespace;
 
-    public TemplateExtensionMethodBuildItem(MethodInfo method, String matchName, String matchRegex, ClassInfo matchClass,
-            int priority,
-            String namespace) {
+    public TemplateExtensionMethodBuildItem(MethodInfo method, String matchName, String matchRegex, Type matchType,
+            int priority, String namespace) {
         this.method = method;
         this.matchName = matchName;
         this.matchRegex = matchRegex;
-        this.matchClass = matchClass;
+        this.matchType = matchType;
         this.priority = priority;
         this.namespace = namespace;
         this.matchPattern = (matchRegex == null || matchRegex.isEmpty()) ? null : Pattern.compile(matchRegex);
@@ -47,8 +47,8 @@ public final class TemplateExtensionMethodBuildItem extends MultiBuildItem {
         return matchRegex;
     }
 
-    public ClassInfo getMatchClass() {
-        return matchClass;
+    public Type getMatchType() {
+        return matchType;
     }
 
     public int getPriority() {
@@ -60,7 +60,7 @@ public final class TemplateExtensionMethodBuildItem extends MultiBuildItem {
     }
 
     boolean matchesClass(ClassInfo clazz) {
-        return matchClass.name().equals(clazz.name());
+        return matchType.name().equals(clazz.name());
     }
 
     boolean matchesName(String name) {

commit c4835c2f12984c5581d19325731b55bf722bfe89

Qute - fix validation of primitives
- fix a regression caused by
https://github.com/quarkusio/quarkus/commit/2b01ca7b0b3287db87ee98dd62fff2f426bff3e9

extensions/qute/deployment/src/main/java/io/quarkus/qute/deployment/QuteProcessor.java
@@ -642,26 +642,28 @@ public class QuteProcessor {
                             }
                         }
                     }
-                    if (member == null) {
-                        // Then try to find an etension method
-                        member = findTemplateExtensionMethod(info, match.type(), templateExtensionMethods, expression,
-                                index,
-                                templateIdToPathFun, results);
-                    }
-                    if (member == null) {
-                        // Test whether the validation should be skipped
-                        TypeCheck check = new TypeCheck(
-                                info.isProperty() ? info.asProperty().name : info.asVirtualMethod().name,
-                                match.clazz(),
-                                info.part.isVirtualMethod() ? info.part.asVirtualMethod().getParameters().size() : -1);
-                        if (isExcluded(check, excludes)) {
-                            LOGGER.debugf(
-                                    "Expression part [%s] excluded from validation of [%s] against type [%s]",
-                                    info.value,
-                                    expression.toOriginalString(), match.type());
-                            match.clearValues();
-                            break;
-                        }
+                }
+
+                if (member == null) {
+                    // Then try to find an etension method
+                    member = findTemplateExtensionMethod(info, match.type(), templateExtensionMethods, expression,
+                            index,
+                            templateIdToPathFun, results);
+                }
+
+                if (member == null) {
+                    // Test whether the validation should be skipped
+                    TypeCheck check = new TypeCheck(
+                            info.isProperty() ? info.asProperty().name : info.asVirtualMethod().name,
+                            match.clazz(),
+                            info.part.isVirtualMethod() ? info.part.asVirtualMethod().getParameters().size() : -1);
+                    if (isExcluded(check, excludes)) {
+                        LOGGER.debugf(
+                                "Expression part [%s] excluded from validation of [%s] against type [%s]",
+                                info.value,
+                                expression.toOriginalString(), match.type());
+                        match.clearValues();
+                        break;
                     }
                 }
 

independent-projects/qute/generator/src/main/java/io/quarkus/qute/generator/DotNames.java
@@ -1,0 +1,20 @@
+package io.quarkus.qute.generator;
+
+import java.util.concurrent.CompletionStage;
+import org.jboss.jandex.DotName;
+
+final class DotNames {
+
+    static final DotName BOOLEAN = DotName.createSimple(Boolean.class.getName());
+    static final DotName BYTE = DotName.createSimple(Byte.class.getName());
+    static final DotName CHARACTER = DotName.createSimple(Character.class.getName());
+    static final DotName DOUBLE = DotName.createSimple(Double.class.getName());
+    static final DotName FLOAT = DotName.createSimple(Float.class.getName());
+    static final DotName INTEGER = DotName.createSimple(Integer.class.getName());
+    static final DotName LONG = DotName.createSimple(Long.class.getName());
+    static final DotName SHORT = DotName.createSimple(Short.class.getName());
+    static final DotName STRING = DotName.createSimple(String.class.getName());
+    static final DotName COMPLETION_STAGE = DotName.createSimple(CompletionStage.class.getName());
+    static final DotName OBJECT = DotName.createSimple(Object.class.getName());
+
+}

independent-projects/qute/generator/src/main/java/io/quarkus/qute/generator/ExtensionMethodGenerator.java
@@ -43,6 +43,7 @@ import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.IndexView;
 import org.jboss.jandex.MethodInfo;
+import org.jboss.jandex.PrimitiveType.Primitive;
 import org.jboss.jandex.Type;
 import org.jboss.jandex.Type.Kind;
 
@@ -55,8 +56,6 @@ import org.jboss.jandex.Type.Kind;
 public class ExtensionMethodGenerator {
 
     public static final DotName TEMPLATE_EXTENSION = DotName.createSimple(TemplateExtension.class.getName());
-    static final DotName STRING = DotName.createSimple(String.class.getName());
-
     public static final String SUFFIX = "_Extension" + ValueResolverGenerator.SUFFIX;
     public static final String NAMESPACE_SUFFIX = "_Namespace" + SUFFIX;
 
@@ -132,7 +131,7 @@ public class ExtensionMethodGenerator {
 
         if (matchRegex != null || matchName.equals(TemplateExtension.ANY)) {
             // The second parameter must be a string
-            if (parameters.size() < 2 || !parameters.get(1).name().equals(STRING)) {
+            if (parameters.size() < 2 || !parameters.get(1).name().equals(io.quarkus.qute.generator.DotNames.STRING)) {
                 throw new TemplateException(
                         "A template extension method matching multiple names or a regular expression must declare at least two parameters and the second parameter must be string: "
                                 + method);
@@ -352,7 +351,8 @@ public class ExtensionMethodGenerator {
 
         // Test base object class
         ResultHandle baseClass = appliesTo.invokeVirtualMethod(Descriptors.GET_CLASS, base);
-        ResultHandle testClass = appliesTo.loadClass(parameters.get(0).name().toString());
+        // Perform autoboxing for primitives
+        ResultHandle testClass = appliesTo.loadClass(box(parameters.get(0)).name().toString());
         ResultHandle baseClassTest = appliesTo.invokeVirtualMethod(Descriptors.IS_ASSIGNABLE_FROM, testClass,
                 baseClass);
         BytecodeCreator baseNotAssignable = appliesTo.ifTrue(baseClassTest).falseBranch();
@@ -630,6 +630,36 @@ public class ExtensionMethodGenerator {
             return sb.toString();
         } catch (NoSuchAlgorithmException e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    static Type box(Type type) {
+        if (type.kind() == Kind.PRIMITIVE) {
+            return box(type.asPrimitiveType().primitive());
+        }
+        return type;
+    }
+
+    static Type box(Primitive primitive) {
+        switch (primitive) {
+            case BOOLEAN:
+                return Type.create(DotNames.BOOLEAN, Kind.CLASS);
+            case DOUBLE:
+                return Type.create(DotNames.DOUBLE, Kind.CLASS);
+            case FLOAT:
+                return Type.create(DotNames.FLOAT, Kind.CLASS);
+            case LONG:
+                return Type.create(DotNames.LONG, Kind.CLASS);
+            case INT:
+                return Type.create(DotNames.INTEGER, Kind.CLASS);
+            case BYTE:
+                return Type.create(DotNames.BYTE, Kind.CLASS);
+            case CHAR:
+                return Type.create(DotNames.CHARACTER, Kind.CLASS);
+            case SHORT:
+                return Type.create(DotNames.SHORT, Kind.CLASS);
+            default:
+                throw new IllegalArgumentException("Unsupported primitive: " + primitive);
         }
     }
 

independent-projects/qute/generator/src/main/java/io/quarkus/qute/generator/ValueResolverGenerator.java
@@ -64,10 +64,6 @@ public class ValueResolverGenerator {
     public static final DotName TEMPLATE_DATA = DotName.createSimple(TemplateData.class.getName());
     public static final DotName TEMPLATE_DATA_CONTAINER = DotName.createSimple(TemplateData.Container.class.getName());
 
-    private static final DotName COMPLETION_STAGE = DotName.createSimple(CompletionStage.class.getName());
-    private static final DotName OBJECT = DotName.createSimple(Object.class.getName());
-    private static final DotName BOOLEAN = DotName.createSimple(Boolean.class.getName());
-
     public static final String SUFFIX = "_ValueResolver";
     public static final String NESTED_SEPARATOR = "$_";
 
@@ -115,7 +111,7 @@ public class ValueResolverGenerator {
         Map<DotName, Set<DotName>> superToSub = new HashMap<>();
         for (Entry<DotName, ClassInfo> entry : nameToClass.entrySet()) {
             DotName superName = entry.getValue().superName();
-            if (superName != null && !OBJECT.equals(superName)) {
+            if (superName != null && !DotNames.OBJECT.equals(superName)) {
                 superToSub.computeIfAbsent(superName, name -> new HashSet<>()).add(entry.getKey());
             }
         }
@@ -135,7 +131,7 @@ public class ValueResolverGenerator {
                     generate(entry.getKey(), priority);
                     // Queue a class removal
                     DotName superName = entry.getValue().superName();
-                    if (superName != null && !OBJECT.equals(superName)) {
+                    if (superName != null && !DotNames.OBJECT.equals(superName)) {
                         superToSubRemovals.computeIfAbsent(superName, name -> new HashSet<>()).add(entry.getKey());
                     }
                     // Remove the processed binding
@@ -229,7 +225,7 @@ public class ValueResolverGenerator {
                 .collect(Collectors.toList());
         if (!ignoreSuperclasses) {
             DotName superName = clazz.superName();
-            while (superName != null && !superName.equals(OBJECT)) {
+            while (superName != null && !superName.equals(DotNames.OBJECT)) {
                 ClassInfo superClass = index.getClassByName(superName);
                 if (superClass != null) {
                     methods.addAll(
@@ -250,7 +246,7 @@ public class ValueResolverGenerator {
                 if ((field.type().kind() == org.jboss.jandex.Type.Kind.PRIMITIVE
                         && field.type().asPrimitiveType().equals(PrimitiveType.BOOLEAN))
                         || (field.type().kind() == org.jboss.jandex.Type.Kind.CLASS
-                                && field.type().name().equals(BOOLEAN))) {
+                                && field.type().name().equals(DotNames.BOOLEAN))) {
                     getterName = IS_PREFIX + capitalize(field.name());
                 } else {
                     getterName = GET_PREFIX + capitalize(field.name());
@@ -837,7 +833,7 @@ public class ValueResolverGenerator {
             return true;
         }
         if (returnType == null
-                || (returnType.name().equals(PrimitiveType.BOOLEAN.name()) || returnType.name().equals(BOOLEAN))) {
+                || (returnType.name().equals(PrimitiveType.BOOLEAN.name()) || returnType.name().equals(DotNames.BOOLEAN))) {
             return name.startsWith(IS_PREFIX) || name.startsWith(HAS_PREFIX);
         }
         return false;
@@ -939,7 +935,7 @@ public class ValueResolverGenerator {
 
     public static boolean hasCompletionStageInTypeClosure(ClassInfo classInfo,
             IndexView index) {
-        return hasClassInTypeClosure(classInfo, COMPLETION_STAGE, index);
+        return hasClassInTypeClosure(classInfo, DotNames.COMPLETION_STAGE, index);
     }
 
     public static boolean hasClassInTypeClosure(ClassInfo classInfo, DotName className,

extensions/qute/deployment/src/test/java/io/quarkus/qute/deployment/typesafe/Movie.java
@@ -7,6 +7,8 @@ public class Movie {
 
     public final Boolean alwaysTrue;
 
+    public final boolean alwaysFalsePrimitive = false;
+
     public final List<String> mainCharacters;
 
     public Movie(String... mainCharacters) {

extensions/qute/deployment/src/test/java/io/quarkus/qute/deployment/typesafe/MovieExtensions.java
@@ -17,4 +17,8 @@ public class MovieExtensions {
         return ret;
     }
 
+    static boolean negate(boolean val) {
+        return !val;
+    }
+
 }

extensions/qute/deployment/src/test/java/io/quarkus/qute/deployment/typesafe/ValidationSuccessTest.java
@@ -30,6 +30,8 @@ public class ValidationSuccessTest {
                             // Built-in value resolvers
                             + "{movie.name ?: 'Mono'} "
                             + "{movie.alwaysTrue ? 'Mono' : 'Stereo'} "
+                            + "{movie.alwaysFalsePrimitive ? 'Mono' : 'Stereo'} "
+                            + "{movie.alwaysFalsePrimitive.negate} "
                             + "{movie.mainCharacters.size} "
                             // Name and number of params ok and param type ignored
                             + "{movie.findService('foo')} "
@@ -53,7 +55,7 @@ public class ValidationSuccessTest {
     @Test
     public void testResult() {
         // Validation succeeded! Yay!
-        assertEquals("Jason Jason Mono 1 10 11 ok 43 3 ohn bar",
+        assertEquals("Jason Jason Mono Stereo true 1 10 11 ok 43 3 ohn bar",
                 movie.data("movie", new Movie("John"), "name", "Vasik", "surname", "Hu", "age", 10l, "map",
                         Collections.singletonMap("foo", "bar")).render());
     }
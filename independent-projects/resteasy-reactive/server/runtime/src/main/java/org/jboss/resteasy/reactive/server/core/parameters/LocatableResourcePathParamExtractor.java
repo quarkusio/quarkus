b28c5958b6efa5e49ba74b3e0ed6b1fbacdf7d57
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-02-18 14:49:46 +1100

    Handle more than one level of sub resource params
    Fixes #15114
    
    (cherry picked from commit 7beab6dc93bdb9f5c322b23f5218d44b546a18b9)

--- independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/parameters/LocatableResourcePathParamExtractor.java
+++ independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/parameters/LocatableResourcePathParamExtractor.java
@@ -1,7 +1,6 @@
 package org.jboss.resteasy.reactive.server.core.parameters;
 
 import org.jboss.resteasy.reactive.server.core.ResteasyReactiveRequestContext;
-import org.jboss.resteasy.reactive.server.mapping.URITemplate;
 
 public class LocatableResourcePathParamExtractor implements ParameterExtractor {
 
@@ -13,48 +12,7 @@ public class LocatableResourcePathParamExtractor implements ParameterExtractor {
 
     @Override
     public Object extractParameter(ResteasyReactiveRequestContext context) {
-        int index = findPathParamIndex(context.getLocatorTarget().getClassPath(), context.getLocatorTarget().getPath());
-        if (index >= 0) {
-            return context.getLocatorPathParam(index);
-        }
-        return null;
-    }
-
-    private int findPathParamIndex(URITemplate classPathTemplate, URITemplate methodPathTemplate) {
-        int index = 0;
-        if (classPathTemplate != null) {
-            for (URITemplate.TemplateComponent component : classPathTemplate.components) {
-                if (component.name != null) {
-                    if (component.name.equals(this.name)) {
-                        return index;
-                    }
-                    index++;
-                } else if (component.names != null) {
-                    for (String nm : component.names) {
-                        if (nm.equals(this.name)) {
-                            return index;
-                        }
-                    }
-                    index++;
-                }
-            }
-        }
-        for (URITemplate.TemplateComponent component : methodPathTemplate.components) {
-            if (component.name != null) {
-                if (component.name.equals(this.name)) {
-                    return index;
-                }
-                index++;
-            } else if (component.names != null) {
-                for (String nm : component.names) {
-                    if (nm.equals(this.name)) {
-                        return index;
-                    }
-                }
-                index++;
-            }
-        }
-        return -1;
+        return context.getResourceLocatorPathParam(name);
     }
 
 }

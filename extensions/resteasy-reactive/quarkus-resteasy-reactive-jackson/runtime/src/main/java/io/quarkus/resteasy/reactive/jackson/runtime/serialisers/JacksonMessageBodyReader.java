581205d565091fa7dcf82da55bb20fcc1277bc9c
Georgios Andrianakis <geoand@gmail.com>
2021-03-03 22:54:53 +0200

    Make Jackson work with ReaderInterceptors
    Fixes: #15453
    (cherry picked from commit 199b91c2e0ef74d0b687736c1a5a4d8054fc11d2)

--- extensions/resteasy-reactive/quarkus-resteasy-reactive-jackson/runtime/src/main/java/io/quarkus/resteasy/reactive/jackson/runtime/serialisers/JacksonMessageBodyReader.java
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive-jackson/runtime/src/main/java/io/quarkus/resteasy/reactive/jackson/runtime/serialisers/JacksonMessageBodyReader.java
@@ -31,7 +31,11 @@ public class JacksonMessageBodyReader extends AbstractJsonMessageBodyReader {
     @Override
     public Object readFrom(Class<Object> type, Type genericType, Annotation[] annotations, MediaType mediaType,
             MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException, WebApplicationException {
-        throw new IllegalStateException("Should never be called");
+        try {
+            return doReadFrom(type, genericType, entityStream);
+        } catch (MismatchedInputException e) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
     }
 
     @Override

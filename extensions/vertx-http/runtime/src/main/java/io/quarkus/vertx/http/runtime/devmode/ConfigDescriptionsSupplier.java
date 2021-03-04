d7a2f2b40a56eb13c387ca9efd50497a707443ee
Phillip Kruger <phillip.kruger@gmail.com>
2021-03-04 20:32:36 +0200

    Update to the config Dev UI screen
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/devmode/ConfigDescriptionsSupplier.java
+++ extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/devmode/ConfigDescriptionsSupplier.java
@@ -3,21 +3,18 @@ package io.quarkus.vertx.http.runtime.devmode;
 import static io.smallrye.config.Expressions.withoutExpansion;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.TreeMap;
 import java.util.function.Supplier;
 
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
+import io.smallrye.config.ConfigValue;
 import io.smallrye.config.SmallRyeConfig;
 
-public class ConfigDescriptionsSupplier implements Supplier<List<ConfigDescription>> {
-
-    Map<String, List<ConfigDescription>> mappedBySource = new HashMap<>();
+public class ConfigDescriptionsSupplier implements Supplier<Map<ConfigSourceName, List<ConfigDescription>>> {
 
     private List<ConfigDescription> configDescriptions;
 
@@ -29,7 +26,9 @@ public class ConfigDescriptionsSupplier implements Supplier<List<ConfigDescripti
     }
 
     @Override
-    public List<ConfigDescription> get() {
+    public Map<ConfigSourceName, List<ConfigDescription>> get() {
+
+        Map<ConfigSourceName, List<ConfigDescription>> ordered = new TreeMap<>();
         List<String> properties = new ArrayList<>();
         SmallRyeConfig current = (SmallRyeConfig) ConfigProvider.getConfig();
 
@@ -38,9 +37,10 @@ public class ConfigDescriptionsSupplier implements Supplier<List<ConfigDescripti
             item.setConfigValue(current.getConfigValue(item.getName()));
 
             String configSourceName = item.getConfigValue().getConfigSourceName();
+            int configSourceOrdinal = item.getConfigValue().getConfigSourceOrdinal();
 
-            mappedBySource.putIfAbsent(configSourceName, new ArrayList<>());
-            mappedBySource.get(configSourceName).add(item);
+            ordered.putIfAbsent(new ConfigSourceName(configSourceName, configSourceOrdinal), new ArrayList<>());
+            ordered.get(new ConfigSourceName(configSourceName, configSourceOrdinal)).add(item);
         }
 
         for (ConfigSource configSource : current.getConfigSources()) {
@@ -56,53 +56,17 @@ public class ConfigDescriptionsSupplier implements Supplier<List<ConfigDescripti
                 }
 
                 ConfigDescription item = new ConfigDescription(propertyName, null, null, current.getConfigValue(propertyName));
-                String configSourceName = current.getConfigValue(propertyName).getConfigSourceName();
-                mappedBySource.putIfAbsent(configSourceName, new ArrayList<>());
-                mappedBySource.get(configSourceName).add(item);
+                ConfigValue configValue = current.getConfigValue(propertyName);
+                ConfigSourceName csn = new ConfigSourceName(configValue.getConfigSourceName(),
+                        configValue.getConfigSourceOrdinal());
+                ordered.putIfAbsent(csn, new ArrayList<>());
+                ordered.get(csn).add(item);
 
                 configDescriptions.add(item);
             }
         });
 
-        List<ConfigDescription> orderedBySource = new ArrayList<>();
-
-        // PropertiesConfigSource[source=application.properties]
-        if (mappedBySource.containsKey(APPLICATION_PROPERTIES_CONFIG_SOURCE)) {
-            List<ConfigDescription> applicationProperties = mappedBySource.remove(APPLICATION_PROPERTIES_CONFIG_SOURCE);
-            Collections.sort(applicationProperties);
-            orderedBySource.addAll(applicationProperties);
-        }
-
-        // All other PropertiesConfigSource
-        List<String> otherPropertySources = getOtherPropertiesConfigSources(mappedBySource.keySet());
-        for (String name : otherPropertySources) {
-            List<ConfigDescription> other = mappedBySource.remove(name);
-            Collections.sort(other);
-            orderedBySource.addAll(other);
-        }
-
-        // default value
-        if (mappedBySource.containsKey(DEFAULT_VALUES)) {
-            List<ConfigDescription> p = mappedBySource.remove(DEFAULT_VALUES);
-            Collections.sort(p);
-            orderedBySource.addAll(p);
-        }
-        // null 
-        if (mappedBySource.containsKey(null)) {
-            List<ConfigDescription> p = mappedBySource.remove(null);
-            Collections.sort(p);
-            orderedBySource.addAll(p);
-        }
-
-        // Now the rest
-        Set<Map.Entry<String, List<ConfigDescription>>> entrySet = mappedBySource.entrySet();
-        for (Map.Entry<String, List<ConfigDescription>> e : entrySet) {
-            List<ConfigDescription> p = e.getValue();
-            Collections.sort(p);
-            orderedBySource.addAll(p);
-        }
-
-        return orderedBySource;
+        return ordered;
     }
 
     public List<ConfigDescription> getConfigDescriptions() {
@@ -112,18 +76,4 @@ public class ConfigDescriptionsSupplier implements Supplier<List<ConfigDescripti
     public void setConfigDescriptions(final List<ConfigDescription> configDescriptions) {
         this.configDescriptions = configDescriptions;
     }
-
-    private List<String> getOtherPropertiesConfigSources(Set<String> allNames) {
-        List<String> l = new ArrayList<>();
-        for (String name : allNames) {
-            if (name != null && name.startsWith(PROPERTIES_CONFIG_SOURCE)) {
-                l.add(name);
-            }
-        }
-        return l;
-    }
-
-    private static final String APPLICATION_PROPERTIES_CONFIG_SOURCE = "PropertiesConfigSource[source=application.properties]";
-    private static final String PROPERTIES_CONFIG_SOURCE = "PropertiesConfigSource";
-    private static final String DEFAULT_VALUES = "default values";
 }

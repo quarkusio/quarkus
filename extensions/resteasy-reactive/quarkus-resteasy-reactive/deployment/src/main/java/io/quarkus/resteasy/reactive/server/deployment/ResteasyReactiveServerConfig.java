6b8844917cc29ddfe8fa1064d9746ccf4767b803
Erin Schnabel <ebullient@redhat.com>
2021-03-05 14:12:26 -0500

    Tests for quarkus.rest.path

--- extensions/resteasy-reactive/quarkus-resteasy-reactive/deployment/src/main/java/io/quarkus/resteasy/reactive/server/deployment/ResteasyReactiveServerConfig.java
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive/deployment/src/main/java/io/quarkus/resteasy/reactive/server/deployment/ResteasyReactiveServerConfig.java
@@ -1,5 +1,7 @@
 package io.quarkus.resteasy.reactive.server.deployment;
 
+import java.util.Optional;
+
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigRoot;
 
@@ -7,9 +9,12 @@ import io.quarkus.runtime.annotations.ConfigRoot;
 public class ResteasyReactiveServerConfig {
 
     /**
-     * Set this to override the default path for JAX-RS resources if there are no
-     * annotated application classes.
+     * Set this to define the application path that serves as the base URI for all
+     * JAX-RS resource URIs provided by {@code @Path} annotations when there are no
+     * {@code @ApplicationPath} annotations defined on {@code Application} classes.
+     * <p>
+     * This value is always resolved relative to {@code quarkus.http.root-path}.
      */
-    @ConfigItem(defaultValue = "/")
-    String path;
+    @ConfigItem
+    Optional<String> path;
 }

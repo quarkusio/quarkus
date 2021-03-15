a2ad55de263ba7a94fd936ae6422227a60563497
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2021-03-15 11:11:32 +0100

    Rename RestEasy Reactive MicroProfile Client to quarkus-rest-client-reactive

---
+++ extensions/resteasy-reactive/rest-client-reactive/deployment/src/test/java/io/quarkus/rest/client/reactive/TestBean.java
@@ -0,0 +1,30 @@
+package io.quarkus.rest.client.reactive;
+
+import java.net.URL;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+@ApplicationScoped
+public class TestBean {
+
+    @ConfigProperty(name = "test-url")
+    URL url;
+
+    @RestClient
+    HelloClient2 client2;
+
+    String helloViaInjectedClient(String name) {
+        return client2.echo(name);
+    }
+
+    String helloViaBuiltClient(String name) {
+        HelloClient helloClient = RestClientBuilder.newBuilder()
+                .baseUrl(url)
+                .build(HelloClient.class);
+        return helloClient.echo(name);
+    }
+}

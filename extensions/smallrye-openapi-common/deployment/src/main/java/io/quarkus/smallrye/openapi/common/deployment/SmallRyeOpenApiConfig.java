9ff9f5d34dfd7737912d44cd3eead48fd335612c
Phillip Kruger <phillip.kruger@gmail.com>
2021-03-16 13:29:54 +0200

    Added ability to define some OpenAPI Security in Config
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- extensions/smallrye-openapi-common/deployment/src/main/java/io/quarkus/smallrye/openapi/common/deployment/SmallRyeOpenApiConfig.java
+++ extensions/smallrye-openapi-common/deployment/src/main/java/io/quarkus/smallrye/openapi/common/deployment/SmallRyeOpenApiConfig.java
@@ -20,4 +20,70 @@ public final class SmallRyeOpenApiConfig {
      */
     @ConfigItem
     public Optional<Path> storeSchemaDirectory;
+
+    /**
+     * Add a certain SecurityScheme with config
+     */
+    public Optional<SecurityScheme> securityScheme;
+
+    /**
+     * Add a Security Scheme name to the generated OpenAPI document
+     */
+    @ConfigItem(defaultValue = "SecurityScheme")
+    public String securitySchemeName;
+
+    /**
+     * Add a description to the Security Scheme
+     */
+    @ConfigItem(defaultValue = "Authentication")
+    public String securitySchemeDescription;
+
+    /**
+     * Add a scheme value to the Basic HTTP Security Scheme
+     */
+    @ConfigItem(defaultValue = "basic")
+    public String basicSecuritySchemeValue;
+
+    /**
+     * Add a scheme value to the JWT Security Scheme
+     */
+    @ConfigItem(defaultValue = "bearer")
+    public String jwtSecuritySchemeValue;
+
+    /**
+     * Add a scheme value to the JWT Security Scheme
+     */
+    @ConfigItem(defaultValue = "JWT")
+    public String jwtBearerFormat;
+
+    /**
+     * Add a openIdConnectUrl value to the OIDC Security Scheme
+     */
+    @ConfigItem
+    public Optional<String> oidcOpenIdConnectUrl;
+
+    /**
+     * Add a implicit flow refreshUrl value to the OAuth2 Security Scheme
+     */
+    @ConfigItem
+    public Optional<String> oauth2ImplicitRefreshUrl;
+
+    /**
+     * Add an implicit flow authorizationUrl value to the OAuth2 Security Scheme
+     */
+    @ConfigItem
+    public Optional<String> oauth2ImplicitAuthorizationUrl;
+
+    /**
+     * Add an implicit flow tokenUrl value to the OAuth2 Security Scheme
+     */
+    @ConfigItem
+    public Optional<String> oauth2ImplicitTokenUrl;
+
+    public enum SecurityScheme {
+        basic,
+        jwt,
+        oidc,
+        oauth2Implicit
+    }
 }

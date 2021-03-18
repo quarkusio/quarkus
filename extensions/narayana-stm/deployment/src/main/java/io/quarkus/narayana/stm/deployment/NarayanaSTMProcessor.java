8585f5e6c164a0ff44bdb36a681b95dd1130c3d0
Mayank Kunwar <msingh722@gmail.com>
2021-03-18 22:04:39 +0530

    initializing SocketProcessId at run time

--- extensions/narayana-stm/deployment/src/main/java/io/quarkus/narayana/stm/deployment/NarayanaSTMProcessor.java
+++ extensions/narayana-stm/deployment/src/main/java/io/quarkus/narayana/stm/deployment/NarayanaSTMProcessor.java
@@ -18,6 +18,7 @@ import org.jboss.stm.annotations.Transactional;
 
 import com.arjuna.ats.internal.arjuna.coordinator.CheckedActionFactoryImple;
 import com.arjuna.ats.internal.arjuna.objectstore.ShadowNoFileLockStore;
+import com.arjuna.ats.internal.arjuna.utils.SocketProcessId;
 import com.arjuna.ats.txoj.Lock;
 
 import io.quarkus.deployment.Feature;
@@ -30,6 +31,7 @@ import io.quarkus.deployment.builditem.nativeimage.NativeImageProxyDefinitionBui
 import io.quarkus.deployment.builditem.nativeimage.NativeImageSystemPropertyBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveHierarchyBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
 import io.quarkus.narayana.stm.runtime.NarayanaSTMRecorder;
 
 class NarayanaSTMProcessor {
@@ -60,8 +62,11 @@ class NarayanaSTMProcessor {
     // so disable it at runtime
     @BuildStep()
     @Record(RUNTIME_INIT)
-    public void configureRuntimeProperties(NarayanaSTMRecorder recorder) {
+    public void configureRuntimeProperties(NarayanaSTMRecorder recorder,
+            BuildProducer<RuntimeInitializedClassBuildItem> runtimeInit) {
         recorder.disableTransactionStatusManager();
+        runtimeInit.produce(new RuntimeInitializedClassBuildItem(SocketProcessId.class.getName()));
+        runtimeInit.produce(new RuntimeInitializedClassBuildItem(Lock.class.getName()));
     }
 
     // register STM dynamic proxies

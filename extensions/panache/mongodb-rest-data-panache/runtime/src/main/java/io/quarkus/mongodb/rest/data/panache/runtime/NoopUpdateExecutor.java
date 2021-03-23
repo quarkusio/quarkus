20edd61ea9f0de7a50c812af7c15529ec8cd2e55
Loïc Mathieu <loikeseke@gmail.com>
2021-03-23 13:14:17 +0100

    Allow to use a different UpdateExecutor for MongoDB to avoid having a tx

---
+++ extensions/panache/mongodb-rest-data-panache/runtime/src/main/java/io/quarkus/mongodb/rest/data/panache/runtime/NoopUpdateExecutor.java
@@ -0,0 +1,16 @@
+package io.quarkus.mongodb.rest.data.panache.runtime;
+
+import java.util.function.Supplier;
+
+import javax.inject.Singleton;
+
+import io.quarkus.rest.data.panache.runtime.UpdateExecutor;
+
+@Singleton
+public class NoopUpdateExecutor implements UpdateExecutor {
+
+    @Override
+    public <T> T execute(Supplier<T> supplier) {
+        return supplier.get();
+    }
+}

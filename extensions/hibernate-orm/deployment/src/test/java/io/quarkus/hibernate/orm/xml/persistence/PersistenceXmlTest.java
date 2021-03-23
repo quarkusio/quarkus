3c19698e4c69a14b01a850a3121ec0a6c273aa68
Yoann Rodière <yoann@hibernate.org>
2021-03-23 12:19:27 +0100

    Test a simple setup with a persistence.xml that works in hibernate-orm/deployment
    We only had tests for when it doesn't work here; the other tests
    were all integration tests.
    
    Since I will have to test more complicated setups, I figured I may
    as well test the basics, too.

---
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/xml/persistence/PersistenceXmlTest.java
@@ -0,0 +1,53 @@
+package io.quarkus.hibernate.orm.xml.persistence;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.arc.Arc;
+import io.quarkus.hibernate.orm.SmokeTestUtils;
+import io.quarkus.test.QuarkusUnitTest;
+
+/**
+ * @author Emmanuel Bernard emmanuel@hibernate.org
+ */
+public class PersistenceXmlTest {
+
+    @RegisterExtension
+    static QuarkusUnitTest runner = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClass(SmokeTestUtils.class)
+                    .addClass(MyEntity.class)
+                    .addAsManifestResource("META-INF/some-persistence.xml", "persistence.xml")
+                    .addAsResource("application-datasource-only.properties", "application.properties"));
+
+    @Inject
+    EntityManager entityManager;
+
+    @Test
+    public void puIsFromPersistenceXml() {
+        Arc.container().requestContext().activate();
+        try {
+            // the PU is templatePU from the persistence.xml, not the default entity manager from application.properties
+            Assertions.assertEquals("templatePU",
+                    entityManager.getEntityManagerFactory().getProperties().get("hibernate.ejb.persistenceUnitName"));
+        } finally {
+            Arc.container().requestContext().deactivate();
+        }
+    }
+
+    @Test
+    @Transactional
+    public void smokeTest() {
+        SmokeTestUtils.testSimplePersistRetrieveUpdateDelete(entityManager,
+                MyEntity.class, MyEntity::new,
+                e -> e.id, (e, value) -> e.name = value, e -> e.name);
+    }
+
+}

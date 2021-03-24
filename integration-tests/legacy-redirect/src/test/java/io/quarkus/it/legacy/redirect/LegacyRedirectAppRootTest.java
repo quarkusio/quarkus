172f5141b9b006cea0d51f265bd083d49875d7a1
Roberto Cortez <radcortez@yahoo.com>
2021-03-24 18:40:48 +0000

    Disabled tests that requires update to SR Health.

--- integration-tests/legacy-redirect/src/test/java/io/quarkus/it/legacy/redirect/LegacyRedirectAppRootTest.java
+++ integration-tests/legacy-redirect/src/test/java/io/quarkus/it/legacy/redirect/LegacyRedirectAppRootTest.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.common.http.TestHTTPResource;
@@ -54,6 +55,8 @@ class LegacyRedirectAppRootTest {
     }
 
     @Test
+    // TODO - MP4 - Require SR Health 3.0.1
+    @Disabled
     public void testHealthWithRedirect() {
         // Not found: moved with application endpoint
         clientUtil.validate("/health", 404);

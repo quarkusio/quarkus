8c807acbe7c7fcd958ebe3c57c5dd1724a358bef
Sanne Grinovero <sanne@hibernate.org>
2021-03-30 20:42:10 +0100

    Automatically enable the custom Agroal exception sorter for Oracle DBs

--- extensions/jdbc/jdbc-oracle/deployment/src/main/java/io/quarkus/jdbc/oracle/deployment/OracleProcessor.java
+++ extensions/jdbc/jdbc-oracle/deployment/src/main/java/io/quarkus/jdbc/oracle/deployment/OracleProcessor.java
@@ -3,10 +3,15 @@ package io.quarkus.jdbc.oracle.deployment;
 import static io.quarkus.deployment.Feature.JDBC_ORACLE;
 
 import io.quarkus.agroal.spi.JdbcDriverBuildItem;
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.arc.processor.BuiltinScope;
 import io.quarkus.datasource.common.runtime.DatabaseKind;
+import io.quarkus.deployment.Capabilities;
+import io.quarkus.deployment.Capability;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.jdbc.oracle.runtime.OracleAgroalConnectionConfigurer;
 
 /**
  * N.B. this processor is relatively simple as we rely on the /META-INF/native-image/
@@ -25,6 +30,17 @@ public class OracleProcessor {
     void registerDriver(BuildProducer<JdbcDriverBuildItem> jdbcDriver) {
         jdbcDriver.produce(new JdbcDriverBuildItem(DatabaseKind.ORACLE, "oracle.jdbc.driver.OracleDriver",
                 "oracle.jdbc.xa.client.OracleXADataSource"));
+    }
+
+    @BuildStep
+    void configureAgroalConnection(BuildProducer<AdditionalBeanBuildItem> additionalBeans,
+            Capabilities capabilities) {
+        if (capabilities.isPresent(Capability.AGROAL)) {
+            additionalBeans.produce(new AdditionalBeanBuildItem.Builder().addBeanClass(OracleAgroalConnectionConfigurer.class)
+                    .setDefaultScope(BuiltinScope.APPLICATION.getName())
+                    .setUnremovable()
+                    .build());
+        }
     }
 
 }

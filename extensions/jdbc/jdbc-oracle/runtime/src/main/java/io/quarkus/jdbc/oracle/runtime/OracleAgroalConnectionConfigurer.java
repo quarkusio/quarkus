4af5917862e46e3a07b23c5ce67430506305b0f8
Sanne Grinovero <sanne@hibernate.org>
2021-03-31 18:11:16 +0100

    More user friendly message in case SSL is inconsistently configured

--- extensions/jdbc/jdbc-oracle/runtime/src/main/java/io/quarkus/jdbc/oracle/runtime/OracleAgroalConnectionConfigurer.java
+++ extensions/jdbc/jdbc-oracle/runtime/src/main/java/io/quarkus/jdbc/oracle/runtime/OracleAgroalConnectionConfigurer.java
@@ -1,5 +1,7 @@
 package io.quarkus.jdbc.oracle.runtime;
 
+import java.util.Properties;
+
 import io.agroal.api.configuration.supplier.AgroalDataSourceConfigurationSupplier;
 import io.agroal.api.exceptionsorter.OracleExceptionSorter;
 import io.quarkus.agroal.runtime.AgroalConnectionConfigurer;
@@ -8,6 +10,26 @@ import io.quarkus.datasource.common.runtime.DatabaseKind;
 
 @JdbcDriver(DatabaseKind.ORACLE)
 public class OracleAgroalConnectionConfigurer implements AgroalConnectionConfigurer {
+
+    /**
+     * Oracle connection properties are documented here:
+     * https://docs.oracle.com/en/database/oracle/oracle-database/21/jajdb/oracle/jdbc/OracleConnection.html#Transport_Layer_Security__TLS_SSL_
+     *
+     * SSL seems to be disabled by default, so rather than disabling it explicitly we check that the user didn't attempt to
+     * enable it.
+     */
+    @Override
+    public void disableSslSupport(String databaseKind, AgroalDataSourceConfigurationSupplier dataSourceConfiguration) {
+        final Properties jdbcProperties = dataSourceConfiguration.connectionPoolConfiguration()
+                .connectionFactoryConfiguration()
+                .get()
+                .jdbcProperties();
+        final Object setting = jdbcProperties.get("oracle.net.authentication_services");
+        if (setting != null && "SSL".equalsIgnoreCase(setting.toString())) {
+            log.warnv(
+                    "SSL support has been disabled, but one of the Oracle JDBC connections has been configured to use SSL. This will likely fail");
+        }
+    }
 
     @Override
     public void setExceptionSorter(String databaseKind, AgroalDataSourceConfigurationSupplier dataSourceConfiguration) {

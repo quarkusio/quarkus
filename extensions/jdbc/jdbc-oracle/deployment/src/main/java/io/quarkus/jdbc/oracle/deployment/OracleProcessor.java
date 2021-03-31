371acede7d24b4f7367a235ab5807ca96c1a1d6b
Sanne Grinovero <sanne@hibernate.org>
2021-03-31 17:23:42 +0100

    Add also automatic bindings for Kubernetes
    (cherry picked from commit 3bec0c59bec9afdca21828262b685149ceb43d99)

--- extensions/jdbc/jdbc-oracle/deployment/src/main/java/io/quarkus/jdbc/oracle/deployment/OracleProcessor.java
+++ extensions/jdbc/jdbc-oracle/deployment/src/main/java/io/quarkus/jdbc/oracle/deployment/OracleProcessor.java
@@ -6,12 +6,15 @@ import io.quarkus.agroal.spi.JdbcDriverBuildItem;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.arc.processor.BuiltinScope;
 import io.quarkus.datasource.common.runtime.DatabaseKind;
+import io.quarkus.datasource.deployment.spi.DefaultDataSourceDbKindBuildItem;
 import io.quarkus.deployment.Capabilities;
 import io.quarkus.deployment.Capability;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
 import io.quarkus.jdbc.oracle.runtime.OracleAgroalConnectionConfigurer;
+import io.quarkus.jdbc.oracle.runtime.OracleServiceBindingConverter;
 
 /**
  * N.B. this processor is relatively simple as we rely on the /META-INF/native-image/
@@ -41,6 +44,18 @@ public class OracleProcessor {
                     .setUnremovable()
                     .build());
         }
+    }
+
+    @BuildStep
+    void registerServiceBinding(Capabilities capabilities,
+            BuildProducer<ServiceProviderBuildItem> serviceProvider,
+            BuildProducer<DefaultDataSourceDbKindBuildItem> dbKind) {
+        if (capabilities.isPresent(Capability.KUBERNETES_SERVICE_BINDING)) {
+            serviceProvider.produce(
+                    new ServiceProviderBuildItem("io.quarkus.kubernetes.service.binding.runtime.ServiceBindingConverter",
+                            OracleServiceBindingConverter.class.getName()));
+        }
+        dbKind.produce(new DefaultDataSourceDbKindBuildItem(DatabaseKind.ORACLE));
     }
 
 }

8b1754f2f76c6f1d6c783a2622ed6237c1ebc44f
Roberto Cortez <radcortez@yahoo.com>
2021-04-01 18:15:32 +0100

    API to check if property exists.

---
+++ integration-tests/jpa-postgresql/src/test/java/io/quarkus/it/jpa/postgresql/OverrideJdbcUrlBuildTimeConfigSource.java
@@ -0,0 +1,40 @@
+package io.quarkus.it.jpa.postgresql;
+
+import java.util.HashMap;
+
+import javax.annotation.Priority;
+
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+import io.smallrye.config.Priorities;
+import io.smallrye.config.common.MapBackedConfigSource;
+
+// To test https://github.com/quarkusio/quarkus/issues/16123
+@Priority(Priorities.APPLICATION + 100)
+public class OverrideJdbcUrlBuildTimeConfigSource extends MapBackedConfigSource {
+    public OverrideJdbcUrlBuildTimeConfigSource() {
+        super(OverrideJdbcUrlBuildTimeConfigSource.class.getName(), new HashMap<>(), 1000);
+    }
+
+    @Override
+    public String getValue(final String propertyName) {
+        if (!propertyName.equals("quarkus.datasource.jdbc.url")) {
+            return super.getValue(propertyName);
+        }
+
+        boolean isBuildTime = false;
+        for (ConfigSource configSource : ConfigProvider.getConfig().getConfigSources()) {
+            if (configSource.getClass().getSimpleName().equals("BuildTimeEnvConfigSource")) {
+                isBuildTime = true;
+                break;
+            }
+        }
+
+        if (isBuildTime) {
+            return "${postgres.url}";
+        }
+
+        return super.getValue(propertyName);
+    }
+}

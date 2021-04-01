8977265125ad0babbb4212269e6b6c3000a569d0
Georgios Andrianakis <geoand@gmail.com>
2021-04-01 09:30:12 +0300

    Fix Uni response type handling in quarkus-rest-client-reactive
    Uni was not being treated as an async return type resulting in
    the client making blocking calls and trying to deserialize the
    result into a Uni
    
    Fixes: #16148

--- integration-tests/resteasy-reactive-rest-client/src/main/java/io/quarkus/it/rest/client/ClientCallingResource.java
+++ integration-tests/resteasy-reactive-rest-client/src/main/java/io/quarkus/it/rest/client/ClientCallingResource.java
@@ -1,7 +1,6 @@
 package io.quarkus.it.rest.client;
 
 import java.net.URI;
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -13,14 +12,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 
+import io.smallrye.mutiny.Uni;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 
 @ApplicationScoped
 public class ClientCallingResource {
     private static final ObjectMapper mapper = new JsonMapper();
 
-    private static final String[] RESPONSES = { "cortland", "cortland2" };
+    private static final String[] RESPONSES = { "cortland", "cortland2", "cortland3" };
     private final AtomicInteger count = new AtomicInteger(0);
 
     void init(@Observes Router router) {
@@ -36,20 +37,41 @@ public class ClientCallingResource {
             String url = rc.getBody().toString();
             SimpleClient client = RestClientBuilder.newBuilder().baseUri(URI.create(url))
                     .build(SimpleClient.class);
-            Apple swappedApple = client.swapApple(new Apple("lobo"));
-            client.completionApple(new Apple("lobo2")).whenComplete((apple2, t) -> {
-                if (t == null) {
-                    try {
-                        rc.response().putHeader("content-type", "application/json")
-                                .end(mapper.writeValueAsString(Arrays.asList(swappedApple, apple2)));
-                        return;
-                    } catch (JsonProcessingException e) {
-                        t = e;
-                    }
-                }
-                rc.response().putHeader("content-type", "text/plain").setStatusCode(500).end(t.getMessage());
-            });
+            Uni<Apple> apple1 = Uni.createFrom().item(client.swapApple(new Apple("lobo")));
+            Uni<Apple> apple2 = Uni.createFrom().completionStage(client.completionSwapApple(new Apple("lobo2")));
+            Uni<Apple> apple3 = client.uniSwapApple(new Apple("lobo3"));
+            Uni<Apple> apple4 = Uni.createFrom().item(client.someApple());
+            Uni<Apple> apple5 = Uni.createFrom().completionStage(client.completionSomeApple());
+            Uni<Apple> apple6 = client.uniSomeApple();
+            Uni<Apple> apple7 = Uni.createFrom().item(client.stringApple()).onItem().transform(this::toApple);
+            Uni<Apple> apple8 = Uni.createFrom().completionStage(client.completionStringApple()).onItem()
+                    .transform(this::toApple);
+            Uni<Apple> apple9 = client.uniStringApple().onItem().transform(this::toApple);
+            Uni.combine().all().unis(apple1, apple2, apple3, apple4, apple5, apple6, apple7, apple8, apple9).asTuple()
+                    .subscribe()
+                    .with(tuple -> {
+                        try {
+                            rc.response().putHeader("content-type", "application/json")
+                                    .end(mapper.writeValueAsString(tuple.asList()));
+                        } catch (JsonProcessingException e) {
+                            fail(rc, e.getMessage());
+                        }
+                    }, t -> {
+                        fail(rc, t.getMessage());
+                    });
         });
+    }
+
+    private void fail(RoutingContext rc, String message) {
+        rc.response().putHeader("content-type", "text/plain").setStatusCode(500).end(message);
+    }
+
+    private Apple toApple(String s) {
+        try {
+            return mapper.readValue(s, Apple.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }

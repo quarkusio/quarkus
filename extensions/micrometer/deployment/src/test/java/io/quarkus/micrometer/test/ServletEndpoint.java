a0510b5b677dea0fab8c64d29953414351041bc5
Erin Schnabel <ebullient@redhat.com>
2021-04-01 00:11:01 -0400

    micrometer: defer uri processing + servlet + tests

---
+++ extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/test/ServletEndpoint.java
@@ -0,0 +1,18 @@
+package io.quarkus.micrometer.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ServletEndpoint", urlPatterns = "/servlet/*")
+public class ServletEndpoint extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("OK");
+    }
+}
---
+++ extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/test/VertxWebEndpoint.java
@@ -0,0 +1,19 @@
+package io.quarkus.micrometer.test;
+
+import io.quarkus.vertx.web.Param;
+import io.quarkus.vertx.web.Route;
+import io.quarkus.vertx.web.RouteBase;
+import io.vertx.core.http.HttpMethod;
+
+@RouteBase(path = "/vertx")
+public class VertxWebEndpoint {
+    @Route(path = "item/:id", methods = HttpMethod.GET)
+    public String item(@Param("id") Integer id) {
+        return "message with id " + id;
+    }
+
+    @Route(path = "item/:id/:sub", methods = HttpMethod.GET)
+    public String item(@Param("id") Integer id, @Param("sub") Integer sub) {
+        return "message with id " + id + " and sub " + sub;
+    }
+}

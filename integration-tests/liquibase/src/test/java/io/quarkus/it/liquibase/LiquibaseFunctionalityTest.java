5aa7d0d28381beb1797fbc90d80f685d7d4de972
Falko Modler <famod@users.noreply.github.com>
2021-04-06 19:35:08 +0200

    Disable Liquibase includeAll test in native for now
    Relates to #16292

--- integration-tests/liquibase/src/test/java/io/quarkus/it/liquibase/LiquibaseFunctionalityTest.java
+++ integration-tests/liquibase/src/test/java/io/quarkus/it/liquibase/LiquibaseFunctionalityTest.java
@@ -20,7 +20,7 @@ public class LiquibaseFunctionalityTest {
                 .then()
                 .body(is(
                         "create-tables-1,test-1,create-view-inline,create-view-file-abs,create-view-file-rel,"
-                                + "includeAll-1,includeAll-2,"
+                                + (isIncludeAllExpectedToWork() ? "includeAll-1,includeAll-2," : "")
                                 + "json-create-tables-1,json-test-1,"
                                 + "sql-create-tables-1,sql-test-1,"
                                 + "yaml-create-tables-1,yaml-test-1,"
@@ -28,4 +28,7 @@ public class LiquibaseFunctionalityTest {
                                 + "1613578374533-1,1613578374533-2,1613578374533-3"));
     }
 
+    protected boolean isIncludeAllExpectedToWork() {
+        return true;
+    }
 }

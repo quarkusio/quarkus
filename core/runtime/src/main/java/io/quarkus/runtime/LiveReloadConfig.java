9c57b4ff168778737e94b4a64628b5beaa0911a2
Guillaume Smet <guillaume.smet@gmail.com>
2021-04-06 20:15:30 +0200

    Disable live reload instrumentation by default
    (cherry picked from commit 75340fa01dcd432f9bd3bd0ba5d6f5ed9ff2311e)

--- core/runtime/src/main/java/io/quarkus/runtime/LiveReloadConfig.java
+++ core/runtime/src/main/java/io/quarkus/runtime/LiveReloadConfig.java
@@ -11,12 +11,12 @@ import io.quarkus.runtime.annotations.ConfigRoot;
 public class LiveReloadConfig {
 
     /**
-     * Whether or not Quarkus should disable it's ability to not do a full restart
+     * Whether or not Quarkus should enable its ability to not do a full restart
      * when changes to classes are compatible with JVM instrumentation.
      *
-     * If this is set to false, Quarkus will always restart on changes and never perform class redefinition.
+     * If this is set to true, Quarkus will perform class redefinition when possible.
      */
-    @ConfigItem(defaultValue = "true")
+    @ConfigItem(defaultValue = "false")
     boolean instrumentation;
 
     /**

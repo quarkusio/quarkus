9310adc74840aade692db86da28e95dcbe78b9b1
Thomas Segismont <tsegismo@redhat.com>
2021-04-06 15:42:48 +0200

    Reactive DS idle-timeout for pooled connections
    Before Vert.x 3.9.6, the reactive SQL client pool implementation did not support idle-timeout.
    
    So we temporarily used the TCP connection idle-timeout config to achieve the same effect (with the caveat that the connection could be closed as idle after it was borrowed but not yet used for request execution).
    
    With https://github.com/quarkusio/quarkus/pull/16100, Quarkus uses Vert.x 3.9.6 so we can now apply the idle-timeout config at the pool level and get rid of the warnings in the doc.

--- extensions/reactive-datasource/runtime/src/main/java/io/quarkus/reactive/datasource/runtime/DataSourceReactiveRuntimeConfig.java
+++ extensions/reactive-datasource/runtime/src/main/java/io/quarkus/reactive/datasource/runtime/DataSourceReactiveRuntimeConfig.java
@@ -110,7 +110,7 @@ public class DataSourceReactiveRuntimeConfig {
     public Duration reconnectInterval = Duration.ofSeconds(1L);
 
     /**
-     * The maximum time without data written to or read from a connection before it is closed.
+     * The maximum time a connection remains unused in the pool before it is closed.
      */
     @ConfigItem(defaultValueDocumentation = "no timeout")
     public Optional<Duration> idleTimeout = Optional.empty();

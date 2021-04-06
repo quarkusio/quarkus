ede3f90750c7c96014cb2697fe9050cdaf923bd9
Guillaume Le Floch <glfloch@gmail.com>
2021-04-06 08:45:42 +0200

    Add avro IDL code generator

--- integration-tests/avro-reload/src/test/java/io/quarkus/avro/deployment/AvroReloadResource.java
+++ integration-tests/avro-reload/src/test/java/io/quarkus/avro/deployment/AvroReloadResource.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import test.Level;
 import test.PrivacyImport;
 import test.ProtocolPrivacy;
 
@@ -21,6 +22,12 @@ public class AvroReloadResource {
     @Path("/protocol")
     public String getAvailableProtocolPrivacies() {
         return Arrays.stream(ProtocolPrivacy.values()).map(String::valueOf).collect(joining(","));
+    }
+
+    @GET
+    @Path("/avdl")
+    public String getAvailableLevel() {
+        return Arrays.stream(Level.values()).map(String::valueOf).collect(joining(","));
     }
 
 }

de967998f3aec60855cdfc461faa65d300bc32a9
Georgios Andrianakis <geoand@gmail.com>
2021-04-08 21:38:57 +0300

    Fix build issue when client and server RESTEasy Reactive jackson deps are used together

--- extensions/resteasy-reactive/quarkus-resteasy-reactive-jackson-common/deployment/src/main/java/io/quarkus/resteasy/reactive/jackson/deployment/processor/ResteasyReactiveJacksonProviderDefinedBuildItem.java
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive-jackson-common/deployment/src/main/java/io/quarkus/resteasy/reactive/jackson/deployment/processor/ResteasyReactiveJacksonProviderDefinedBuildItem.java
@@ -6,6 +6,6 @@ import io.quarkus.builder.item.MultiBuildItem;
  * A BuildItem to mark that the server side jackson provider is defined.
  * If not "emitted" by any of the processors, the reactive rest client (if used) will add its own jackson provider
  */
-public class ResteasyReactiveJacksonProviderDefinedBuildItem extends MultiBuildItem {
+public final class ResteasyReactiveJacksonProviderDefinedBuildItem extends MultiBuildItem {
 
 }

d06627e5e6c9ae2e4105b829ac1dd70001f9106d
Erin Schnabel <ebullient@redhat.com>
2021-04-08 20:23:05 -0400

    micrometer: tidy uri tag tests

--- extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/deployment/binder/UriWithMaxTagMeterFilterTest.java
+++ extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/deployment/binder/UriWithMaxTagMeterFilterTest.java
@@ -11,7 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.quarkus.micrometer.runtime.config.runtime.HttpClientConfig;
 import io.quarkus.micrometer.runtime.config.runtime.HttpServerConfig;
+import io.quarkus.micrometer.test.HelloResource;
 import io.quarkus.micrometer.test.PingPongResource;
 import io.quarkus.test.QuarkusUnitTest;
 
@@ -20,30 +22,37 @@ public class UriWithMaxTagMeterFilterTest {
     static final QuarkusUnitTest config = new QuarkusUnitTest()
             .withConfigurationResource("test-logging.properties")
             .overrideConfigKey("quarkus.micrometer.binder-enabled-default", "false")
+            .overrideConfigKey("quarkus.micrometer.binder.http-client.enabled", "true")
+            .overrideConfigKey("quarkus.micrometer.binder.http-client.max-uri-tags", "1")
             .overrideConfigKey("quarkus.micrometer.binder.http-server.enabled", "true")
             .overrideConfigKey("quarkus.micrometer.binder.http-server.max-uri-tags", "1")
             .overrideConfigKey("quarkus.micrometer.export.prometheus.enabled", "true")
             .overrideConfigKey("quarkus.micrometer.binder.vertx.enabled", "true")
             .overrideConfigKey("pingpong/mp-rest/url", "${test.url}")
             .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
-                    .addClasses(PingPongResource.class, PingPongResource.PingPongRestClient.class));
+                    .addClasses(HelloResource.class, PingPongResource.class, PingPongResource.PingPongRestClient.class));
 
     @Inject
     HttpServerConfig httpServerConfig;
+    @Inject
+    HttpClientConfig httpClientConfig;
 
     @Inject
     MeterRegistry registry;
 
     @Test
-    public void testMetricFactoryCreatedMetrics() throws Exception {
+    public void test() throws Exception {
         Assertions.assertEquals(1, httpServerConfig.maxUriTags);
+        Assertions.assertEquals(1, httpClientConfig.maxUriTags);
 
-        // Server will have both /ping/{message} and /pong/{message}
-        // Due to limit, there should only be one.
+        // Server limit is constrained to 1
         when().get("/ping/one").then().statusCode(200);
         when().get("/ping/two").then().statusCode(200);
         when().get("/ping/three").then().statusCode(200);
+        when().get("/one").then().statusCode(200);
         Assertions.assertEquals(1, registry.find("http.server.requests").timers().size());
-    }
 
+        // Client limit is constrained to 1
+        Assertions.assertEquals(1, registry.find("http.client.requests").timers().size());
+    }
 }
--- extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/test/HelloResource.java
+++ extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/test/HelloResource.java
@@ -2,6 +2,7 @@ package io.quarkus.micrometer.test;
 
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -11,6 +12,12 @@ public class HelloResource {
     @GET
     @Path("{message}")
     public String hello(@PathParam("message") String message) {
+        return "hello " + message;
+    }
+
+    @OPTIONS
+    @Path("{message}")
+    public String helloOptions(@PathParam("message") String message) {
         return "hello " + message;
     }
 }

1163b58e05f18c87a0ca1944238fe288556fb25d
Clement Escoffier <clement.escoffier@gmail.com>
2021-04-12 19:17:06 +0200

    Rewrote Kafka SSL tests
    * the previous code was actually not configured SSL consistently
    * switch to test container (Strimzi containers)
    * configure the native test to actually use SSL

--- integration-tests/kafka-ssl/src/test/java/io/quarkus/it/kafka/KafkaSSLTestResource.java
+++ integration-tests/kafka-ssl/src/test/java/io/quarkus/it/kafka/KafkaSSLTestResource.java
@@ -1,125 +1,143 @@
 package io.quarkus.it.kafka;
 
-import static org.awaitility.Awaitility.await;
-
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
-import org.apache.kafka.common.config.SslConfigs;
+import org.jboss.logging.Logger;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.images.builder.Transferable;
+import org.testcontainers.utility.MountableFile;
 
-import io.debezium.kafka.KafkaCluster;
-import io.debezium.util.Testing;
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.model.ContainerNetwork;
+
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
-import kafka.server.KafkaServer;
-import kafka.server.RunningAsBroker;
 
 public class KafkaSSLTestResource implements QuarkusTestResourceLifecycleManager {
 
-    private KafkaCluster kafka;
+    private final SSLStrimziKafkaContainer kafka = new SSLStrimziKafkaContainer()
+            .withCopyFileToContainer(MountableFile.forClasspathResource("server.properties"),
+                    "/opt/kafka/config/server.properties")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("kafka-keystore.p12"),
+                    "/opt/kafka/config/kafka-keystore.p12")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("kafka-truststore.p12"),
+                    "/opt/kafka/config/kafka-truststore.p12");
 
     @Override
     public Map<String, String> start() {
-        try {
-            File directory = Testing.Files.createTestingDirectory("kafka-data-ssl", true);
-            File sslDir = sslDir(directory, true);
+        kafka.start();
+        // Used by the test
+        System.setProperty("bootstrap.servers", kafka.getBootstrapServers());
+        // Used by the application
+        Map<String, String> properties = new HashMap<>();
+        properties.put("kafka.bootstrap.servers", kafka.getBootstrapServers());
+        properties.put("ssl-dir", new File("src/test/resources").getAbsolutePath());
 
-            Path ksPath = new File(sslDir, "kafka-keystore.p12").toPath();
-            try (InputStream ksStream = getClass().getResourceAsStream("/kafka-keystore.p12")) {
-                Files.copy(
-                        ksStream,
-                        ksPath,
-                        StandardCopyOption.REPLACE_EXISTING);
-            }
-
-            Path tsPath = new File(sslDir, "kafka-truststore.p12").toPath();
-            try (InputStream tsStream = getClass().getResourceAsStream("/kafka-truststore.p12")) {
-                Files.copy(
-                        tsStream,
-                        tsPath,
-                        StandardCopyOption.REPLACE_EXISTING);
-            }
-            String password = "Z_pkTh9xgZovK4t34cGB2o6afT4zZg0L";
-            String type = "PKCS12";
-
-            Properties props = new Properties();
-            props.setProperty("zookeeper.connection.timeout.ms", "45000");
-            //See http://kafka.apache.org/documentation.html#security_ssl for detail
-            props.setProperty("listener.security.protocol.map", "CLIENT:SSL");
-            props.setProperty("listeners", "CLIENT://:19099");
-            props.setProperty("inter.broker.listener.name", "CLIENT");
-            props.setProperty(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, ksPath.toString());
-            props.setProperty(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, password);
-            props.setProperty(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, type);
-            props.setProperty(SslConfigs.SSL_KEY_PASSWORD_CONFIG, password);
-            props.setProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, tsPath.toString());
-            props.setProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, password);
-            props.setProperty(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, type);
-            props.setProperty(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "");
-
-            kafka = new KafkaCluster()
-                    .withPorts(2189, 19099)
-                    .addBrokers(1)
-                    .usingDirectory(directory)
-                    .deleteDataUponShutdown(true)
-                    .withKafkaConfiguration(props)
-                    .deleteDataPriorToStartup(true)
-                    .startup();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        KafkaServer server = extract(kafka);
-        await().until(() -> server.brokerState().currentState() == RunningAsBroker.state());
-        server.logger().underlying().info("Broker 'kafka-ssl' started");
-
-        return Collections.emptyMap();
+        return properties;
     }
 
     @Override
     public void stop() {
         if (kafka != null) {
-            kafka.shutdown();
+            kafka.close();
+        }
+        System.clearProperty("boostrap.servers");
+    }
+
+    static class SSLStrimziKafkaContainer extends GenericContainer<SSLStrimziKafkaContainer> {
+
+        private static final Logger LOGGER = Logger.getLogger(SSLStrimziKafkaContainer.class.getName());
+
+        private static final String STARTER_SCRIPT = "/testcontainers_start.sh";
+        private static final int KAFKA_PORT = 9092;
+        private static final int ZOOKEEPER_PORT = 2181;
+        private static final String LATEST_KAFKA_VERSION;
+
+        private int kafkaExposedPort;
+        private static final List<String> supportedKafkaVersions = new ArrayList<>(3);
+
+        static {
+            InputStream inputStream = io.strimzi.StrimziKafkaContainer.class.getResourceAsStream("/kafka-versions.txt");
+            InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+
+            try (BufferedReader bufferedReader = new BufferedReader(streamReader)) {
+                String kafkaVersion;
+                while ((kafkaVersion = bufferedReader.readLine()) != null) {
+                    supportedKafkaVersions.add(kafkaVersion);
+                }
+            } catch (IOException e) {
+                LOGGER.error("Unable to load the supported Kafka versions", e);
+            }
+
+            // sort kafka version from low to high
+            Collections.sort(supportedKafkaVersions);
+
+            LATEST_KAFKA_VERSION = supportedKafkaVersions.get(supportedKafkaVersions.size() - 1);
+        }
+
+        public SSLStrimziKafkaContainer(final String version) {
+            super("quay.io/strimzi/kafka:" + version);
+            super.withNetwork(Network.SHARED);
+
+            // exposing kafka port from the container
+            withExposedPorts(KAFKA_PORT);
+
+            withEnv("LOG_DIR", "/tmp");
+        }
+
+        public SSLStrimziKafkaContainer() {
+            this("latest-kafka-" + LATEST_KAFKA_VERSION);
+        }
+
+        @Override
+        protected void doStart() {
+            // we need it for the startZookeeper(); and startKafka(); to run container before...
+            withCommand("sh", "-c", "while [ ! -f " + STARTER_SCRIPT + " ]; do sleep 0.1; done; " + STARTER_SCRIPT);
+            super.doStart();
+        }
+
+        @Override
+        protected void containerIsStarting(InspectContainerResponse containerInfo, boolean reused) {
+            super.containerIsStarting(containerInfo, reused);
+
+            kafkaExposedPort = getMappedPort(KAFKA_PORT);
+
+            StringBuilder advertisedListeners = new StringBuilder(getBootstrapServers());
+
+            Collection<ContainerNetwork> cns = containerInfo.getNetworkSettings().getNetworks().values();
+
+            for (ContainerNetwork cn : cns) {
+                advertisedListeners.append("," + "BROKER://").append(cn.getIpAddress()).append(":9093");
+            }
+
+            String command = "#!/bin/bash \n";
+            command += "bin/zookeeper-server-start.sh config/zookeeper.properties &\n";
+            command += "bin/kafka-server-start.sh config/server.properties --override listeners=BROKER://0.0.0.0:9093,SSL://0.0.0.0:"
+                    + KAFKA_PORT +
+                    " --override advertised.listeners=" + advertisedListeners.toString() +
+                    " --override zookeeper.connect=localhost:" + ZOOKEEPER_PORT +
+                    " --override listener.security.protocol.map=BROKER:PLAINTEXT,SSL:SSL" +
+                    " --override inter.broker.listener.name=BROKER\n";
+
+            copyFileToContainer(
+                    Transferable.of(command.getBytes(StandardCharsets.UTF_8), 700),
+                    STARTER_SCRIPT);
+        }
+
+        public String getBootstrapServers() {
+            return String.format("SSL://%s:%s", getContainerIpAddress(), kafkaExposedPort);
         }
     }
 
-    public static File sslDir(File directory, boolean removeExistingContent) throws IOException {
-        if (directory == null) {
-            directory = Testing.Files.createTestingDirectory("kafka-data-ssl", removeExistingContent);
-        }
-
-        File targetDir = directory.getParentFile().getParentFile();
-        File sslDir = new File(targetDir, "ssl_test");
-        if (!sslDir.exists()) {
-            //noinspection ResultOfMethodCallIgnored
-            sslDir.mkdir();
-        }
-        return sslDir;
-    }
-
-    @SuppressWarnings("unchecked")
-    static kafka.server.KafkaServer extract(KafkaCluster cluster) {
-        Field kafkaServersField;
-        Field serverField;
-        try {
-            kafkaServersField = cluster.getClass().getDeclaredField("kafkaServers");
-            kafkaServersField.setAccessible(true);
-            Map<Integer, io.debezium.kafka.KafkaServer> map = (Map<Integer, io.debezium.kafka.KafkaServer>) kafkaServersField
-                    .get(cluster);
-            io.debezium.kafka.KafkaServer server = map.get(1);
-            serverField = io.debezium.kafka.KafkaServer.class.getDeclaredField("server");
-            serverField.setAccessible(true);
-            return (kafka.server.KafkaServer) serverField.get(server);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-    }
 }

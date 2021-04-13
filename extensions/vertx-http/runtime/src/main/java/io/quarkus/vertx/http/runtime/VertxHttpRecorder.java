999fc2c3f4969073649c845732f15d132ee84014
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-04-13 10:39:09 +1000

    HTTP/2 + BodyHandler fix
    Fixes #16170
    
    (cherry picked from commit a071f848c5df00789e78cbab0f8120a6f17890bb)

--- extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/VertxHttpRecorder.java
+++ extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/VertxHttpRecorder.java
@@ -1120,7 +1120,9 @@ public class VertxHttpRecorder {
                         }
                     });
                 } else {
-                    event.request().resume();
+                    if (!event.request().isEnded()) {
+                        event.request().resume();
+                    }
                     if (CAN_HAVE_BODY.contains(event.request().method())) {
                         bodyHandler.handle(event);
                     } else {

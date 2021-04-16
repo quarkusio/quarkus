ae40e34b85e36d8581d1e2625310bd3512e0130f
Martin Kouba <mkouba@redhat.com>
2021-04-16 16:30:54 +0200

    ArC - fail the build if a local/inner/anonymous class
    ...has a scope annotation, declares an observer or producer

---
+++ extensions/arc/deployment/src/test/java/io/quarkus/arc/test/wrongannotations/ObserverOnInnerClassTest.java
@@ -0,0 +1,43 @@
+package io.quarkus.arc.test.wrongannotations;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.enterprise.event.Observes;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.runtime.util.ExceptionUtil;
+import io.quarkus.test.QuarkusUnitTest;
+
+public class ObserverOnInnerClassTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(IgnoredClass.class))
+            .assertException(t -> {
+                Throwable rootCause = ExceptionUtil.getRootCause(t);
+                assertTrue(
+                        rootCause.getMessage().contains(
+                                "INNER class io.quarkus.arc.test.wrongannotations.ObserverOnInnerClassTest$IgnoredClass"),
+                        t.toString());
+            });
+
+    @Test
+    public void testValidationFailed() {
+        // This method should not be invoked
+        fail();
+    }
+
+    class IgnoredClass {
+
+        void observe(@Observes String event) {
+        }
+
+    }
+
+}

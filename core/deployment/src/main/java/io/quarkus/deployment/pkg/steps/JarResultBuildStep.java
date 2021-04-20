bb53b23849270d0af1e7ff774e3669f139bf473f
Guillaume Smet <guillaume.smet@gmail.com>
2021-04-20 23:09:52 +0200

    Add some more ignored entries for uberjars

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/JarResultBuildStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/JarResultBuildStep.java
@@ -107,6 +107,8 @@ public class JarResultBuildStep {
             "META-INF/LICENSE",
             "META-INF/LICENSE.txt",
             "META-INF/LICENSE.md",
+            "META-INF/LGPL-3.0.txt",
+            "META-INF/ASL-2.0.txt",
             "META-INF/NOTICE",
             "META-INF/NOTICE.txt",
             "META-INF/NOTICE.md",

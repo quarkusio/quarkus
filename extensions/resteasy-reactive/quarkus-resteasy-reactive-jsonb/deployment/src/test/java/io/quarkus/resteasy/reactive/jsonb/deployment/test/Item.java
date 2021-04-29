d621f8dab3d1ddecbb9d4726ba5fb9d9c445477c
Georgios Andrianakis <geoand@gmail.com>
2021-04-29 16:27:46 +0300

    Fix generic type handling of body parameter in RESTEasy Reactive
    Fixes: #16905

---
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive-jsonb/deployment/src/test/java/io/quarkus/resteasy/reactive/jsonb/deployment/test/Item.java
@@ -0,0 +1,23 @@
+package io.quarkus.resteasy.reactive.jsonb.deployment.test;
+
+public class Item {
+
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

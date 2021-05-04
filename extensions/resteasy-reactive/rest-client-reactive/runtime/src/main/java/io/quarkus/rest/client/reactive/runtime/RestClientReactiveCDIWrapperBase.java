6ef1df8dab7149b6e515259f60510d2ee7c74005
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2021-05-04 10:23:50 +0200

    Rest Client Reactive: close CDI-created rest client instances

---
+++ extensions/resteasy-reactive/rest-client-reactive/runtime/src/main/java/io/quarkus/rest/client/reactive/runtime/RestClientReactiveCDIWrapperBase.java
@@ -0,0 +1,38 @@
+package io.quarkus.rest.client.reactive.runtime;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import javax.annotation.PreDestroy;
+
+import org.jboss.logging.Logger;
+
+public abstract class RestClientReactiveCDIWrapperBase<T extends Closeable> implements Closeable {
+    private static final Logger log = Logger.getLogger(RestClientReactiveCDIWrapperBase.class);
+
+    private final T delegate;
+
+    public RestClientReactiveCDIWrapperBase(Class<T> jaxrsInterface, String baseUriFromAnnotation, String propertyPrefix) {
+        this.delegate = RestClientCDIDelegateBuilder.createDelegate(jaxrsInterface, baseUriFromAnnotation, propertyPrefix);
+    }
+
+    @Override
+    public void close() throws IOException {
+        delegate.close();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        try {
+            close();
+        } catch (IOException e) {
+            log.warn("Failed to close cdi-created rest client instance", e);
+        }
+    }
+
+    // used by generated code
+    @SuppressWarnings("unused")
+    public Object getDelegate() {
+        return delegate;
+    }
+}

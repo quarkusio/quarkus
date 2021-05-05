0f1e9f26a743dfe9e82160bc4f81b86fe949b11f
Georgios Andrianakis <geoand@gmail.com>
2021-05-05 09:32:58 +0300

    Make vert.x http register static resources in native
    Fixes: #16953

--- integration-tests/resteasy-jackson/src/test/java/io/quarkus/it/resteasy/jackson/StaticContentTest.java
+++ integration-tests/resteasy-jackson/src/test/java/io/quarkus/it/resteasy/jackson/StaticContentTest.java
@@ -1,15 +1,10 @@
 package io.quarkus.it.resteasy.jackson;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 
 /**
@@ -20,24 +15,8 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 public class StaticContentTest {
 
-    @TestHTTPResource("index.html")
-    URL url;
-
     @Test
-    public void testIndexHtml() throws Exception {
-        try (InputStream in = url.openStream()) {
-            String contents = readStream(in);
-            Assertions.assertTrue(contents.contains("<title>Testing Guide</title>"));
-        }
-    }
-
-    static String readStream(InputStream in) throws IOException {
-        byte[] data = new byte[1024];
-        int r;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        while ((r = in.read(data)) > 0) {
-            out.write(data, 0, r);
-        }
-        return new String(out.toByteArray(), StandardCharsets.UTF_8);
+    public void testIndexHtml() {
+        when().get("/index.html").then().statusCode(200).body(containsString("<title>Testing Guide</title>"));
     }
 }
--- integration-tests/resteasy-jackson/src/test/java/io/quarkus/it/resteasy/jackson/StaticContentWithTestProfileTest.java
+++ integration-tests/resteasy-jackson/src/test/java/io/quarkus/it/resteasy/jackson/StaticContentWithTestProfileTest.java
@@ -1,12 +1,10 @@
 package io.quarkus.it.resteasy.jackson;
 
-import java.io.InputStream;
-import java.net.URL;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.QuarkusTestProfile;
 import io.quarkus.test.junit.TestProfile;
@@ -15,15 +13,9 @@ import io.quarkus.test.junit.TestProfile;
 @TestProfile(StaticContentWithTestProfileTest.DummyProfile.class)
 public class StaticContentWithTestProfileTest {
 
-    @TestHTTPResource("index.html")
-    URL url;
-
     @Test
     public void testIndexHtml() throws Exception {
-        try (InputStream in = url.openStream()) {
-            String contents = StaticContentTest.readStream(in);
-            Assertions.assertTrue(contents.contains("<title>Testing Guide</title>"));
-        }
+        when().get("/index.html").then().statusCode(200).body(containsString("<title>Testing Guide</title>"));
     }
 
     public static class DummyProfile implements QuarkusTestProfile {

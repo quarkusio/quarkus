d7229fe0692efab2dca617461b2b75a897a577f2
Foivos Zakkak <fzakkak@redhat.com>
2021-05-06 16:46:25 +0300

    Disable Bouncycastle JSSE native test
    Issue tracked in https://github.com/quarkusio/quarkus/issues/17046

--- integration-tests/bouncycastle-jsse/src/test/java/io/quarkus/it/bouncycastle/BouncyCastleJsseITCase.java
+++ integration-tests/bouncycastle-jsse/src/test/java/io/quarkus/it/bouncycastle/BouncyCastleJsseITCase.java
@@ -2,11 +2,13 @@ package io.quarkus.it.bouncycastle;
 
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.test.junit.DisabledOnNativeImage;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
 public class BouncyCastleJsseITCase extends BouncyCastleJsseTestCase {
     @Test
+    @DisabledOnNativeImage
     @Override
     public void testListProviders() {
         doTestListProviders();

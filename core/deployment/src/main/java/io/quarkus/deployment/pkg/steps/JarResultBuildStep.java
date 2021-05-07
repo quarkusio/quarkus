ea6023fee767719ebe1f45eb38ebda5621e3ec4e
George Gastaldi <gegastaldi@gmail.com>
2021-05-07 12:42:21 -0300

    Introduce UberJarMergedResourceBuildItem and UberJarIgnoredResourceBuildItem
    Fixes #5677
    
    (cherry picked from commit b3d3788ae92542d5fb39d89488890e16d64cec90)

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/JarResultBuildStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/JarResultBuildStep.java
@@ -83,6 +83,8 @@ import io.quarkus.deployment.pkg.builditem.JarBuildItem;
 import io.quarkus.deployment.pkg.builditem.LegacyJarRequiredBuildItem;
 import io.quarkus.deployment.pkg.builditem.NativeImageSourceJarBuildItem;
 import io.quarkus.deployment.pkg.builditem.OutputTargetBuildItem;
+import io.quarkus.deployment.pkg.builditem.UberJarIgnoredResourceBuildItem;
+import io.quarkus.deployment.pkg.builditem.UberJarMergedResourceBuildItem;
 import io.quarkus.deployment.pkg.builditem.UberJarRequiredBuildItem;
 import io.quarkus.deployment.util.FileUtil;
 
@@ -141,24 +143,41 @@ public class JarResultBuildStep {
     };
 
     private static final Logger log = Logger.getLogger(JarResultBuildStep.class);
+
     // we shouldn't have to specify these flags when opening a ZipFS (since they are the default ones), but failure to do so
     // makes a subsequent uberJar creation fail in java 8 (but works fine in Java 11)
     private static final StandardOpenOption[] DEFAULT_OPEN_OPTIONS = { TRUNCATE_EXISTING, WRITE, CREATE };
+
     private static final BiPredicate<Path, BasicFileAttributes> IS_JSON_FILE_PREDICATE = new IsJsonFilePredicate();
+
     public static final String DEPLOYMENT_CLASS_PATH_DAT = "deployment-class-path.dat";
+
     public static final String BUILD_SYSTEM_PROPERTIES = "build-system.properties";
+
     public static final String DEPLOYMENT_LIB = "deployment";
+
     public static final String APPMODEL_DAT = "appmodel.dat";
+
     public static final String QUARKUS_RUN_JAR = "quarkus-run.jar";
+
     public static final String QUARKUS_APP_DEPS = "quarkus-app-dependencies.txt";
+
     public static final String BOOT_LIB = "boot";
+
     public static final String LIB = "lib";
+
     public static final String MAIN = "main";
+
     public static final String GENERATED_BYTECODE_JAR = "generated-bytecode.jar";
+
     public static final String TRANSFORMED_BYTECODE_JAR = "transformed-bytecode.jar";
+
     public static final String APP = "app";
+
     public static final String QUARKUS = "quarkus";
+
     public static final String DEFAULT_FAST_JAR_DIRECTORY_NAME = "quarkus-app";
+
     public static final String MP_CONFIG_FILE = "META-INF/microprofile-config.properties";
 
     @BuildStep
@@ -189,6 +208,8 @@ public class JarResultBuildStep {
             List<GeneratedClassBuildItem> generatedClasses,
             List<GeneratedResourceBuildItem> generatedResources,
             List<UberJarRequiredBuildItem> uberJarRequired,
+            List<UberJarMergedResourceBuildItem> uberJarMergedResourceBuildItems,
+            List<UberJarIgnoredResourceBuildItem> uberJarIgnoredResourceBuildItems,
             List<LegacyJarRequiredBuildItem> legacyJarRequired,
             QuarkusBuildCloseablesBuildItem closeablesBuildItem,
             List<AdditionalApplicationArchiveBuildItem> additionalApplicationArchiveBuildItems,
@@ -207,7 +228,7 @@ public class JarResultBuildStep {
                 || packageConfig.type.equalsIgnoreCase(PackageConfig.UBER_JAR))) {
             return buildUberJar(curateOutcomeBuildItem, outputTargetBuildItem, transformedClasses, applicationArchivesBuildItem,
                     packageConfig, applicationInfo, generatedClasses, generatedResources, closeablesBuildItem,
-                    mainClassBuildItem);
+                    uberJarMergedResourceBuildItems, uberJarIgnoredResourceBuildItems, mainClassBuildItem);
         } else if (!legacyJarRequired.isEmpty() || packageConfig.isLegacyJar()
                 || packageConfig.type.equalsIgnoreCase(PackageConfig.LEGACY)) {
             return buildLegacyThinJar(curateOutcomeBuildItem, outputTargetBuildItem, transformedClasses,
@@ -255,6 +276,8 @@ public class JarResultBuildStep {
             List<GeneratedClassBuildItem> generatedClasses,
             List<GeneratedResourceBuildItem> generatedResources,
             QuarkusBuildCloseablesBuildItem closeablesBuildItem,
+            List<UberJarMergedResourceBuildItem> mergeResources,
+            List<UberJarIgnoredResourceBuildItem> ignoredResources,
             MainClassBuildItem mainClassBuildItem) throws Exception {
 
         //we use the -runner jar name, unless we are building both types
@@ -269,6 +292,8 @@ public class JarResultBuildStep {
                 applicationInfo,
                 generatedClasses,
                 generatedResources,
+                mergeResources,
+                ignoredResources,
                 mainClassBuildItem,
                 runnerJar);
 
@@ -293,6 +318,8 @@ public class JarResultBuildStep {
             ApplicationInfoBuildItem applicationInfo,
             List<GeneratedClassBuildItem> generatedClasses,
             List<GeneratedResourceBuildItem> generatedResources,
+            List<UberJarMergedResourceBuildItem> mergedResources,
+            List<UberJarIgnoredResourceBuildItem> ignoredResources,
             MainClassBuildItem mainClassBuildItem,
             Path runnerJar) throws Exception {
         try (FileSystem runnerZipFs = ZipUtils.newZip(runnerJar)) {
@@ -302,8 +329,14 @@ public class JarResultBuildStep {
             final Map<String, String> seen = new HashMap<>();
             final Map<String, Set<AppDependency>> duplicateCatcher = new HashMap<>();
             final Map<String, List<byte[]>> concatenatedEntries = new HashMap<>();
+            final Set<String> mergeResourcePaths = mergedResources.stream()
+                    .map(UberJarMergedResourceBuildItem::getPath)
+                    .collect(Collectors.toSet());
             Set<String> finalIgnoredEntries = new HashSet<>(IGNORED_ENTRIES);
             packageConfig.userConfiguredIgnoredEntries.ifPresent(finalIgnoredEntries::addAll);
+            ignoredResources.stream()
+                    .map(UberJarIgnoredResourceBuildItem::getPath)
+                    .forEach(finalIgnoredEntries::add);
 
             final List<AppDependency> appDeps = curateOutcomeBuildItem.getEffectiveModel().getUserDependencies();
 
@@ -328,12 +361,13 @@ public class JarResultBuildStep {
                         try (FileSystem artifactFs = ZipUtils.newFileSystem(resolvedDep)) {
                             for (final Path root : artifactFs.getRootDirectories()) {
                                 walkFileDependencyForDependency(root, runnerZipFs, seen, duplicateCatcher, concatenatedEntries,
-                                        finalIgnoredEntries, appDep, transformedFromThisArchive);
+                                        finalIgnoredEntries, appDep, transformedFromThisArchive, mergeResourcePaths);
                             }
                         }
                     } else {
                         walkFileDependencyForDependency(resolvedDep, runnerZipFs, seen, duplicateCatcher,
-                                concatenatedEntries, finalIgnoredEntries, appDep, transformedFromThisArchive);
+                                concatenatedEntries, finalIgnoredEntries, appDep, transformedFromThisArchive,
+                                mergeResourcePaths);
                     }
                 }
             }
@@ -360,7 +394,8 @@ public class JarResultBuildStep {
 
     private void walkFileDependencyForDependency(Path root, FileSystem runnerZipFs, Map<String, String> seen,
             Map<String, Set<AppDependency>> duplicateCatcher, Map<String, List<byte[]>> concatenatedEntries,
-            Set<String> finalIgnoredEntries, AppDependency appDep, Set<String> transformedFromThisArchive) throws IOException {
+            Set<String> finalIgnoredEntries, AppDependency appDep, Set<String> transformedFromThisArchive,
+            Set<String> mergeResourcePaths) throws IOException {
         final Path metaInfDir = root.resolve("META-INF");
         Files.walkFileTree(root, EnumSet.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE,
                 new SimpleFileVisitor<Path>() {
@@ -392,7 +427,8 @@ public class JarResultBuildStep {
                         boolean transformed = transformedFromThisArchive != null
                                 && transformedFromThisArchive.contains(relativePath);
                         if (!transformed) {
-                            if (CONCATENATED_ENTRIES_PREDICATE.test(relativePath)) {
+                            if (CONCATENATED_ENTRIES_PREDICATE.test(relativePath)
+                                    || mergeResourcePaths.contains(relativePath)) {
                                 concatenatedEntries.computeIfAbsent(relativePath, (u) -> new ArrayList<>())
                                         .add(Files.readAllBytes(file));
                                 return FileVisitResult.CONTINUE;
@@ -818,7 +854,6 @@ public class JarResultBuildStep {
 
     /**
      * Native images are built from a specially created jar file. This allows for changes in how the jar file is generated.
-     *
      */
     @BuildStep
     public NativeImageSourceJarBuildItem buildNativeImageJar(CurateOutcomeBuildItem curateOutcomeBuildItem,
@@ -831,7 +866,9 @@ public class JarResultBuildStep {
             List<GeneratedNativeImageClassBuildItem> nativeImageResources,
             List<GeneratedResourceBuildItem> generatedResources,
             MainClassBuildItem mainClassBuildItem,
-            List<UberJarRequiredBuildItem> uberJarRequired) throws Exception {
+            List<UberJarRequiredBuildItem> uberJarRequired,
+            List<UberJarMergedResourceBuildItem> mergeResources,
+            List<UberJarIgnoredResourceBuildItem> ignoreResources) throws Exception {
         Path targetDirectory = outputTargetBuildItem.getOutputDirectory()
                 .resolve(outputTargetBuildItem.getBaseName() + "-native-image-source-jar");
         IoUtils.createOrEmptyDir(targetDirectory);
@@ -850,7 +887,9 @@ public class JarResultBuildStep {
             final NativeImageSourceJarBuildItem nativeImageSourceJarBuildItem = buildNativeImageUberJar(curateOutcomeBuildItem,
                     outputTargetBuildItem, transformedClasses,
                     applicationArchivesBuildItem,
-                    packageConfig, applicationInfo, allClasses, generatedResources, mainClassBuildItem, targetDirectory);
+                    packageConfig, applicationInfo, allClasses, generatedResources, mergeResources,
+                    ignoreResources, mainClassBuildItem,
+                    targetDirectory);
             // additionally copy any json config files to a location accessible by native-image tool during
             // native-image generation
             copyJsonConfigFiles(applicationArchivesBuildItem, targetDirectory);
@@ -898,6 +937,8 @@ public class JarResultBuildStep {
             ApplicationInfoBuildItem applicationInfo,
             List<GeneratedClassBuildItem> generatedClasses,
             List<GeneratedResourceBuildItem> generatedResources,
+            List<UberJarMergedResourceBuildItem> mergeResources,
+            List<UberJarIgnoredResourceBuildItem> ignoreResources,
             MainClassBuildItem mainClassBuildItem,
             Path targetDirectory) throws Exception {
         //we use the -runner jar name, unless we are building both types
@@ -911,6 +952,8 @@ public class JarResultBuildStep {
                 applicationInfo,
                 generatedClasses,
                 generatedResources,
+                mergeResources,
+                ignoreResources,
                 mainClassBuildItem,
                 runnerJar);
 
@@ -1093,6 +1136,7 @@ public class JarResultBuildStep {
         for (Map.Entry<String, List<byte[]>> entry : concatenatedEntries.entrySet()) {
             try (final OutputStream os = wrapForJDK8232879(
                     Files.newOutputStream(runnerZipFs.getPath(entry.getKey()), DEFAULT_OPEN_OPTIONS))) {
+                // TODO: Handle merging of XMLs
                 for (byte[] i : entry.getValue()) {
                     os.write(i);
                     os.write('\n');
---
+++ core/test-extension/deployment/src/test/java/io/quarkus/deployment/pkg/builditem/UberJarIgnoredResourceBuildItemTest.java
@@ -0,0 +1,52 @@
+package io.quarkus.deployment.pkg.builditem;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.bootstrap.model.AppArtifact;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import io.quarkus.test.QuarkusProdModeTest;
+
+class UberJarIgnoredResourceBuildItemTest {
+
+    @RegisterExtension
+    static final QuarkusProdModeTest runner = new QuarkusProdModeTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addAsManifestResource("application.properties", "microprofile-config.properties")
+                    .addClass(UberJarMain.class))
+            .setApplicationName("uber-jar-ignored")
+            .setApplicationVersion("0.1-SNAPSHOT")
+            .setRun(true)
+            .setExpectExit(true)
+            .overrideConfigKey("quarkus.package.type", "uber-jar")
+            .setForcedDependencies(
+                    Collections.singletonList(
+                            // META-INF/cxf/cxf.fixml should be present in the cxf-rt-transports-http and cxf-core JARs
+                            new AppArtifact("org.apache.cxf", "cxf-rt-transports-http", "3.4.3")));
+
+    @Test
+    public void testResourceWasIgnored() throws IOException {
+        assertThat(runner.getStartupConsoleOutput()).contains("RESOURCES: 0");
+        assertThat(runner.getExitCode()).isZero();
+    }
+
+    @QuarkusMain
+    public static class UberJarMain {
+
+        public static void main(String[] args) throws IOException {
+            List<URL> resources = Collections
+                    .list(UberJarMain.class.getClassLoader().getResources("META-INF/cxf/cxf.fixml"));
+            System.out.println("RESOURCES: " + resources.size());
+        }
+
+    }
+}

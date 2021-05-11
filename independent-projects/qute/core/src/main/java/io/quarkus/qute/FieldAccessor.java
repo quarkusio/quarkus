b72c5a52925fda88a0bc928f61afdd0dffb827ac
Martin Kouba <mkouba@redhat.com>
2021-05-11 15:35:48 +0200

    Qute reflection resolver - support methods with params

---
+++ independent-projects/qute/core/src/main/java/io/quarkus/qute/FieldAccessor.java
@@ -0,0 +1,39 @@
+package io.quarkus.qute;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+/**
+ *
+ * @see ReflectionValueResolver
+ */
+class FieldAccessor implements ValueAccessor, AccessorCandidate {
+
+    private final Field field;
+
+    FieldAccessor(Field field) {
+        this.field = field;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public CompletionStage<Object> getValue(Object instance) {
+        try {
+            Object ret = field.get(instance);
+            if (ret instanceof CompletionStage) {
+                return (CompletionStage<Object>) ret;
+            } else {
+                return CompletableFuture.completedFuture(ret);
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException("Reflection invocation error", e);
+        }
+    }
+
+    @Override
+    public ValueAccessor getAccessor(EvalContext context) {
+        return this;
+    }
+
+}

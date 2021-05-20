96b42d2ec86ae680a7c1a161d1480d541ebf522c
Georgios Andrianakis <geoand@gmail.com>
2021-05-20 06:42:26 +0300

    Add extension for supporting coroutine in Emitter

--- integration-tests/resteasy-reactive-kotlin/standard/src/main/kotlin/io/quarkus/it/resteasy/reactive/kotlin/CountriesEndpoint.kt
+++ integration-tests/resteasy-reactive-kotlin/standard/src/main/kotlin/io/quarkus/it/resteasy/reactive/kotlin/CountriesEndpoint.kt
@@ -1,13 +1,18 @@
 package io.quarkus.it.resteasy.reactive.kotlin
 
+import io.quarkus.smallrye.reactivemessaging.sendSuspending
 import kotlinx.coroutines.delay
+import org.eclipse.microprofile.reactive.messaging.Channel
+import org.eclipse.microprofile.reactive.messaging.Emitter
 import org.eclipse.microprofile.rest.client.inject.RestClient
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 
 @Path("country")
 class CountriesEndpoint(@RestClient private val countriesGateway: CountriesGateway,
-                        private val countryNameConsumer: CountryNameConsumer) {
+                        private val countryNameConsumer: CountryNameConsumer,
+                        @Channel("countries-emitter") private val countryEmitter: Emitter<String>) {
 
     @GET
     @Path("/name/{name}")
@@ -15,6 +20,14 @@ class CountriesEndpoint(@RestClient private val countriesGateway: CountriesGatew
         val result = countriesGateway.byName(name)
         delay(50)
         return result
+    }
+
+    @POST
+    @Path("/kafka/{name}")
+    suspend fun sendCountryNameToKafka(name: String): String {
+        delay(50)
+        countryEmitter.sendSuspending(name)
+        return name
     }
 
     @GET
--- integration-tests/resteasy-reactive-kotlin/standard/src/test/kotlin/io/quarkus/it/resteasy/reactive/kotlin/ReactiveMessagingTest.kt
+++ integration-tests/resteasy-reactive-kotlin/standard/src/test/kotlin/io/quarkus/it/resteasy/reactive/kotlin/ReactiveMessagingTest.kt
@@ -3,6 +3,7 @@ package io.quarkus.it.resteasy.reactive.kotlin
 import io.quarkus.test.common.QuarkusTestResource
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.get
+import io.restassured.RestAssured.given
 import io.restassured.common.mapper.TypeRef
 import org.awaitility.Awaitility.await
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -17,8 +18,19 @@ class ReactiveMessagingTest {
 
     @Test
     fun test() {
+        assertCountries(6)
+
+        given()
+                .`when`().post("/country/kafka/dummy")
+                .then()
+                .statusCode(200)
+
+        assertCountries(8)
+    }
+
+    private fun assertCountries(num: Int) {
         await().atMost(Duration.ofMinutes(1)).pollInterval(Duration.ofSeconds(5)).untilAsserted {
-            assertEquals(get("/country/resolved").`as`(TYPE_REF).size, 6)
+            assertEquals(get("/country/resolved").`as`(TYPE_REF).size, num)
         }
     }
 }

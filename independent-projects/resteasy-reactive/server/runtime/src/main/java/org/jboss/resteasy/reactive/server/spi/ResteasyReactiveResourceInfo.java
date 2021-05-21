6aef436f21eff83d14c76bd26b3757ac0dbecc02
Sanne Grinovero <sanne@hibernate.org>
2021-05-21 14:02:18 +0100

    Optimisations in ResteasyReactiveResourceInfo

--- independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/spi/ResteasyReactiveResourceInfo.java
+++ independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/spi/ResteasyReactiveResourceInfo.java
@@ -14,11 +14,17 @@ import javax.ws.rs.container.ResourceInfo;
  */
 public class ResteasyReactiveResourceInfo implements ResourceInfo {
 
+    private static final String JSON_VIEW_NAME = "com.fasterxml.jackson.annotation.JsonView";
+    private static final String CUSTOM_SERIALIZATION = "io.quarkus.resteasy.reactive.jackson.CustomSerialization";
+
     private final String name;
     private final Class<?> declaringClass;
     private final Class[] parameterTypes;
     private final Set<String> classAnnotationNames;
     private final Set<String> methodAnnotationNames;
+    private final boolean requiresCustomSerialization;
+    private final boolean requiresJsonViewName;
+
     private volatile Annotation[] classAnnotations;
     private volatile Method method;
     private volatile Annotation[] annotations;
@@ -31,6 +37,8 @@ public class ResteasyReactiveResourceInfo implements ResourceInfo {
         this.parameterTypes = parameterTypes;
         this.classAnnotationNames = classAnnotationNames;
         this.methodAnnotationNames = methodAnnotationNames;
+        this.requiresCustomSerialization = methodAnnotationNames.contains(CUSTOM_SERIALIZATION);
+        this.requiresJsonViewName = methodAnnotationNames.contains(JSON_VIEW_NAME);
     }
 
     public String getName() {
@@ -101,5 +109,19 @@ public class ResteasyReactiveResourceInfo implements ResourceInfo {
     public Annotation[] getParameterAnnotations(int index) {
         // Should we cache this?
         return getMethod().getParameterAnnotations()[index];
+    }
+
+    /**
+     * Short cut to check if the method is annotated with io.quarkus.resteasy.reactive.jackson.CustomSerialization
+     */
+    public boolean requiresCustomSerialization() {
+        return this.requiresCustomSerialization;
+    }
+
+    /**
+     * Short cut to check if the method is annotated with com.fasterxml.jackson.annotation.JsonView
+     */
+    public boolean requiresJsonViewName() {
+        return this.requiresJsonViewName;
     }
 }

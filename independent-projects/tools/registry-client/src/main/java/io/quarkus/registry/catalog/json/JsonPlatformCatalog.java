8324e61a53e45da8b98cd8aa4c79885d030bedd9
Alexey Loubyansky <olubyans@redhat.com>
2021-05-24 14:28:03 +0200

    Support for platform streams in the registry client

--- independent-projects/tools/registry-client/src/main/java/io/quarkus/registry/catalog/json/JsonPlatformCatalog.java
+++ independent-projects/tools/registry-client/src/main/java/io/quarkus/registry/catalog/json/JsonPlatformCatalog.java
@@ -2,18 +2,16 @@ package io.quarkus.registry.catalog.json;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import io.quarkus.maven.ArtifactCoords;
 import io.quarkus.registry.catalog.Platform;
 import io.quarkus.registry.catalog.PlatformCatalog;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class JsonPlatformCatalog implements PlatformCatalog {
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class JsonPlatformCatalog extends JsonEntityWithAnySupport implements PlatformCatalog {
 
     private List<Platform> platforms;
-    private ArtifactCoords defaultPlatform;
 
     @Override
     @JsonDeserialize(contentAs = JsonPlatform.class)
@@ -30,14 +28,5 @@ public class JsonPlatformCatalog implements PlatformCatalog {
             platforms = new ArrayList<>();
         }
         platforms.add(platform);
-    }
-
-    @Override
-    public ArtifactCoords getDefaultPlatform() {
-        return defaultPlatform;
-    }
-
-    public void setDefaultPlatform(ArtifactCoords defaultPlatform) {
-        this.defaultPlatform = defaultPlatform;
     }
 }

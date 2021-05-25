e9b71f2f495a5d5d175651da170ad4f786d2431d
Phillip Kruger <phillip.kruger@gmail.com>
2021-05-25 22:01:18 +0200

    Upgrade to SmallRye GraphQL 1.2.2 (Implement the "GraphQL over HTTP" spec properly) Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- extensions/smallrye-graphql/deployment/src/test/java/io/quarkus/smallrye/graphql/deployment/AbstractGraphQLTest.java
+++ extensions/smallrye-graphql/deployment/src/test/java/io/quarkus/smallrye/graphql/deployment/AbstractGraphQLTest.java
@@ -1,12 +1,15 @@
 package io.quarkus.smallrye.graphql.deployment;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Map;
 import java.util.Properties;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
 
 import org.hamcrest.CoreMatchers;
 
@@ -45,20 +48,29 @@ public abstract class AbstractGraphQLTest {
     }
 
     protected String getPayload(String query) {
-        JsonObject jsonObject = createRequestBody(query);
+        return getPayload(query, null);
+    }
+
+    protected String getPayload(String query, String variables) {
+        JsonObject jsonObject = createRequestBody(query, variables);
         return jsonObject.toString();
     }
 
-    protected JsonObject createRequestBody(String graphQL) {
-        return createRequestBody(graphQL, null);
-    }
-
-    protected JsonObject createRequestBody(String graphQL, JsonObject variables) {
+    protected JsonObject createRequestBody(String graphQL, String variables) {
         // Create the request
-        if (variables == null || variables.isEmpty()) {
-            variables = Json.createObjectBuilder().build();
+        JsonObject vjo = Json.createObjectBuilder().build();
+        if (variables != null && !variables.isEmpty()) {
+            try (JsonReader jsonReader = Json.createReader(new StringReader(variables))) {
+                vjo = jsonReader.readObject();
+            }
         }
-        return Json.createObjectBuilder().add(QUERY, graphQL).add(VARIABLES, variables).build();
+
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        if (graphQL != null && !graphQL.isEmpty()) {
+            job.add(QUERY, graphQL);
+        }
+
+        return job.add(VARIABLES, vjo).build();
     }
 
     protected static String getPropertyAsString() {

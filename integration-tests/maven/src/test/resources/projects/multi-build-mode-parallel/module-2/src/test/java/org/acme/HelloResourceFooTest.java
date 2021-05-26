525f575939aca2c16b7f1ffb4492a208338fec24
essobedo <nicolas_filotto@ultimatesoftware.com>
2021-05-26 19:43:54 +0200

    Make the maven build task compatible with parallel builds
    (cherry picked from commit c44cdc8d8980ada8b5e7044fa9411c8549cddaff)

---
+++ integration-tests/maven/src/test/resources/projects/multi-build-mode-parallel/module-2/src/test/java/org/acme/HelloResourceFooTest.java
@@ -0,0 +1,26 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@Tag("foo")
+@QuarkusTest
+public class HelloResourceFooTest {
+    @Test
+    void testHelloEndpoint() throws Exception {
+        for (int i = 0; i < 5; i++) {
+            if (i > 0) {
+                Thread.sleep(1_000L); // Make it wait to cause build conflicts
+            }
+            given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello foo 2"));
+        }
+    }
+}

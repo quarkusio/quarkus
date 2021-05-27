ab8f4707515798bf1f497788c15d3f2dbcb6f9ca
Stephane Epardaud <stef@epardaud.fr>
2021-05-27 16:39:48 +0200

    Revert "RESTEasy Reactive: allow, test and document HttpServerResponse injection"
    This reverts commit 5e4e822f27f2b4aca24faac7cf91d02a793309d9.
    
    (cherry picked from commit a35c0c55761282b98b9752a5c9cf096878fe6e54)

--- extensions/resteasy-reactive/quarkus-resteasy-reactive/runtime/src/main/java/io/quarkus/resteasy/reactive/server/runtime/ServerRequestFilterOverride.java
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive/runtime/src/main/java/io/quarkus/resteasy/reactive/server/runtime/ServerRequestFilterOverride.java
@@ -18,14 +18,12 @@ import io.smallrye.safer.annotations.DefinitionOverride;
 import io.smallrye.safer.annotations.OverrideTarget;
 import io.smallrye.safer.annotations.TargetMethod;
 import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.RoutingContext;
 
 /**
  * Adds the following allowed parameters to the Safer-Annotations type checking
  * - RoutingContext
  * - HttpServerRequest
- * - HttpServerResponse
  */
 @OverrideTarget(ServerRequestFilter.class)
 @TargetMethod(returnTypes = { void.class, Response.class, UniResponse.class, UniVoid.class,
@@ -33,8 +31,7 @@ import io.vertx.ext.web.RoutingContext;
                 ContainerRequestContext.class, UriInfo.class, HttpHeaders.class, Request.class,
                 ResourceInfo.class, SimpleResourceInfo.class, ResteasyReactiveContainerRequestContext.class,
                 RoutingContext.class,
-                HttpServerRequest.class,
-                HttpServerResponse.class })
+                HttpServerRequest.class })
 public class ServerRequestFilterOverride implements DefinitionOverride {
 
 }

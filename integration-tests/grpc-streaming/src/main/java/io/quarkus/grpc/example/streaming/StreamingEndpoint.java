0bc19a103d128d20702339a0a41088b8ec8aca8d
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2021-05-27 15:26:50 +0200

    testcommit 3

--- integration-tests/grpc-streaming/src/main/java/io/quarkus/grpc/example/streaming/StreamingEndpoint.java
+++ integration-tests/grpc-streaming/src/main/java/io/quarkus/grpc/example/streaming/StreamingEndpoint.java
@@ -1,15 +1,10 @@
 package io.quarkus.grpc.example.streaming;
 
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+
+import org.jboss.logging.Logger;
 
 import io.grpc.examples.streaming.Empty;
 import io.grpc.examples.streaming.Item;
@@ -17,7 +12,6 @@ import io.grpc.examples.streaming.MutinyStreamingGrpc;
 import io.quarkus.grpc.GrpcClient;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
-import org.jboss.logging.Logger;
 
 @Path("/streaming")
 public class StreamingEndpoint {
@@ -45,24 +39,28 @@ public class StreamingEndpoint {
     @GET
     @Path("/{max}")
     public Multi<String> invokePipe(@PathParam("max") int max) {
-//    public List<String> invokePipe(@PathParam("max") int max) {
+        //    public List<String> invokePipe(@PathParam("max") int max) {
         Multi<Item> inputs = Multi.createFrom().range(0, max)
                 .map(i -> Integer.toString(i))
                 .map(i -> Item.newBuilder().setValue(i).build());
-//        List<String> collector = new CopyOnWriteArrayList<>();
-//        CompletableFuture<Void> finish = new CompletableFuture<>();
+        //        List<String> collector = new CopyOnWriteArrayList<>();
+        //        CompletableFuture<Void> finish = new CompletableFuture<>();
         return streaming.pipe(inputs)
                 .onFailure().invoke(error -> log.error("gRPC failure", error))
-                .onItem().transform(Item::getValue);
-//                .subscribe().with(
-//                collector::add, () -> finish.complete(null));
-        /*try {
-            finish.get(10, TimeUnit.SECONDS);
-        } catch (InterruptedException | ExecutionException e) {
-            throw new IllegalStateException("Failed waiting for responses", e);
-        } catch (TimeoutException e) {
-            throw new IllegalStateException("Timed out waiting for responses", e);
-        }
-        return collector;*/
+                .onItem().transform(Item::getValue)
+                .onItem().invoke(value -> log.infof("received %s", value))
+                .onCompletion().invoke(() -> log.info("completed"));
+        //                .subscribe().with(
+        //                collector::add, () -> finish.complete(null));
+        /*
+         * try {
+         * finish.get(10, TimeUnit.SECONDS);
+         * } catch (InterruptedException | ExecutionException e) {
+         * throw new IllegalStateException("Failed waiting for responses", e);
+         * } catch (TimeoutException e) {
+         * throw new IllegalStateException("Timed out waiting for responses", e);
+         * }
+         * return collector;
+         */
     }
 }

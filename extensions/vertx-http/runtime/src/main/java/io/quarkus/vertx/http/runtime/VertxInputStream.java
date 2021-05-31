5593159ccccbcb4f4ee43e9f0b1374dc0425bc55
Georgios Andrianakis <geoand@gmail.com>
2021-05-31 12:49:10 +0300

    Ensure that VertxInputStream doesn't result in infinite attempts to read data
    This change is needed because Netty now calls available to determine
    whether or not more bytes are available, before calling read.
    The negative value that we used to use does not play well with
    PushbackInputStream which is used by Netty to wrap the stream.
    For a full description of how I can came to this, see
    https://github.com/quarkusio/quarkus/issues/17506#issuecomment-851365258
    
    Fixes: #17506
    (cherry picked from commit fc1bbd4ac032c582e04bb956807bdc40d66c0c3e)

--- extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/VertxInputStream.java
+++ extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/VertxInputStream.java
@@ -131,7 +131,7 @@ public class VertxInputStream extends InputStream {
             throw new IOException("Stream is closed");
         }
         if (finished) {
-            return -1;
+            return 0;
         }
 
         return exchange.readBytesAvailable();

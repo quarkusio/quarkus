1dd5a03070c5a8ac4e350593529147f794db7aca
Ken Finnigan <ken@kenfinnigan.me>
2021-06-01 11:44:55 -0400

    Resolves #17385
    - Moves REST Client filter into a separate hidden extension
    - Uses Quarkus conditional dependency handling to only include the OpenTelemetry Rest Client extension if `quarkus-rest-client` is present in user application

--- extensions/opentelemetry/rest-client/runtime/src/main/java/io/quarkus/opentelemetry/restclient/QuarkusRestClientListener.java
+++ extensions/opentelemetry/rest-client/runtime/src/main/java/io/quarkus/opentelemetry/restclient/QuarkusRestClientListener.java
@@ -1,4 +1,4 @@
-package io.quarkus.opentelemetry.runtime.tracing.client;
+package io.quarkus.opentelemetry.restclient;
 
 import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.spi.CDI;

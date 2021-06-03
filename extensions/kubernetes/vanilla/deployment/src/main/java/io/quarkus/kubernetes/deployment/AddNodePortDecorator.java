72043eb2f6f1ef1da9525aa5f95377281b388254
Georgios Andrianakis <geoand@gmail.com>
2021-06-03 11:49:31 +0300

    Allow specifying per-port nodePort
    Resolves: #17582
    (cherry picked from commit 6db9384d82eea56979eed070fa068e4ab01e5af0)

--- extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/AddNodePortDecorator.java
+++ extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/AddNodePortDecorator.java
@@ -2,11 +2,15 @@ package io.quarkus.kubernetes.deployment;
 
 import static io.quarkus.kubernetes.deployment.Constants.*;
 
+import java.util.Optional;
+import java.util.function.Predicate;
+
 import org.jboss.logging.Logger;
 
 import io.dekorate.kubernetes.decorator.Decorator;
 import io.dekorate.kubernetes.decorator.NamedResourceDecorator;
 import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.ServicePortBuilder;
 import io.fabric8.kubernetes.api.model.ServiceSpecFluent;
 
 public class AddNodePortDecorator extends NamedResourceDecorator<ServiceSpecFluent> {
@@ -14,21 +18,39 @@ public class AddNodePortDecorator extends NamedResourceDecorator<ServiceSpecFlue
     private static final Logger log = Logger.getLogger(AddNodePortDecorator.class);
 
     private final int nodePort;
+    private final Optional<String> matchingPortName;
 
     public AddNodePortDecorator(String name, int nodePort) {
+        this(name, nodePort, Optional.empty());
+    }
+
+    public AddNodePortDecorator(String name, int nodePort, Optional<String> matchingPortName) {
         super(name);
         if (nodePort < MIN_NODE_PORT_VALUE || nodePort > MAX_NODE_PORT_VALUE) {
             log.info("Using a port outside of the " + MIN_NODE_PORT_VALUE + "-" + MAX_NODE_PORT_VALUE
                     + " range might not work, see https://kubernetes.io/docs/concepts/services-networking/service/#nodeport");
         }
         this.nodePort = nodePort;
+        this.matchingPortName = matchingPortName;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void andThenVisit(ServiceSpecFluent service, ObjectMeta resourceMeta) {
-        ServiceSpecFluent.PortsNested<?> editFirstPort = service.editFirstPort();
-        editFirstPort.withNodePort(nodePort);
-        editFirstPort.endPort();
+        ServiceSpecFluent.PortsNested<?> editPort;
+        if (matchingPortName.isPresent()) {
+            editPort = service.editMatchingPort(new Predicate<ServicePortBuilder>() {
+                @Override
+                public boolean test(ServicePortBuilder servicePortBuilder) {
+                    return (servicePortBuilder.hasName())
+                            && (servicePortBuilder.getName().equals(matchingPortName.get()));
+                }
+            });
+        } else {
+            editPort = service.editFirstPort();
+        }
+        editPort.withNodePort(nodePort);
+        editPort.endPort();
     }
 
     @Override

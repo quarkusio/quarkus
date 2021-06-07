a4ee7421a4aff1fb119b6537937bc4a47e3029a9
Nithanim <git@nithanim.me>
2021-06-07 23:14:09 +0200

    Refactored path extraction

--- extensions/amazon-lambda-rest/runtime/src/main/java/io/quarkus/amazon/lambda/http/LambdaHttpHandler.java
+++ extensions/amazon-lambda-rest/runtime/src/main/java/io/quarkus/amazon/lambda/http/LambdaHttpHandler.java
@@ -4,13 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.handler.codec.http.DefaultHttpRequest;
-import io.netty.handler.codec.http.DefaultLastHttpContent;
-import io.netty.handler.codec.http.HttpContent;
-import io.netty.handler.codec.http.HttpHeaderNames;
-import io.netty.handler.codec.http.HttpMethod;
-import io.netty.handler.codec.http.HttpVersion;
-import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.handler.codec.http.*;
 import io.quarkus.amazon.lambda.http.model.AwsProxyRequest;
 import io.quarkus.amazon.lambda.http.model.AwsProxyRequestContext;
 import io.quarkus.amazon.lambda.http.model.AwsProxyResponse;
@@ -18,19 +12,22 @@ import io.quarkus.amazon.lambda.http.model.Headers;
 import io.quarkus.netty.runtime.virtual.VirtualClientConnection;
 import io.quarkus.vertx.http.runtime.QuarkusHttpHeaders;
 import io.quarkus.vertx.http.runtime.VertxHttpRecorder;
+import org.jboss.logging.Logger;
+
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map;
-import org.jboss.logging.Logger;
 
 @SuppressWarnings("unused")
 public class LambdaHttpHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
     private static final Logger log = Logger.getLogger("quarkus.amazon.lambda.http");
 
     private static final Headers errorHeaders = new Headers();
+
     static {
         errorHeaders.putSingle("Content-Type", "application/json");
     }
@@ -57,32 +54,7 @@ public class LambdaHttpHandler implements RequestHandler<AwsProxyRequest, AwsPro
 
     private AwsProxyResponse nettyDispatch(InetSocketAddress clientAddress, AwsProxyRequest request, Context context)
             throws Exception {
-        String path = request.getPath();
-        //log.info("---- Got lambda request: " + path);
-        if (request.getMultiValueQueryStringParameters() != null && !request.getMultiValueQueryStringParameters().isEmpty()) {
-            StringBuilder sb = new StringBuilder(path);
-            sb.append("?");
-            boolean first = true;
-            for (Map.Entry<String, List<String>> e : request.getMultiValueQueryStringParameters().entrySet()) {
-                for (String v : e.getValue()) {
-                    if (first) {
-                        first = false;
-                    } else {
-                        sb.append("&");
-                    }
-                    if (request.getRequestSource() == AwsProxyRequest.RequestSource.ALB) {
-                        sb.append(e.getKey());
-                        sb.append("=");
-                        sb.append(v);
-                    } else {
-                        sb.append(URLEncoder.encode(e.getKey(), StandardCharsets.UTF_8.name()));
-                        sb.append("=");
-                        sb.append(URLEncoder.encode(v, StandardCharsets.UTF_8.name()));
-                    }
-                }
-            }
-            path = sb.toString();
-        }
+        String path = getPathFromRequest(request);
         QuarkusHttpHeaders quarkusHeaders = new QuarkusHttpHeaders();
         quarkusHeaders.setContextObject(Context.class, context);
         quarkusHeaders.setContextObject(AwsProxyRequestContext.class, request.getRequestContext());
@@ -104,13 +76,16 @@ public class LambdaHttpHandler implements RequestHandler<AwsProxyRequest, AwsPro
                 ByteBuf body = Unpooled.wrappedBuffer(Base64.getMimeDecoder().decode(request.getBody()));
                 requestContent = new DefaultLastHttpContent(body);
             } else {
-                ByteBuf body = Unpooled.copiedBuffer(request.getBody(), StandardCharsets.UTF_8); //TODO: do we need to look at the request encoding?
+                ByteBuf body = Unpooled.copiedBuffer(
+                        request.getBody(),
+                        StandardCharsets.UTF_8
+                ); //TODO: do we need to look at the request encoding?
                 requestContent = new DefaultLastHttpContent(body);
             }
         }
         NettyResponseHandler handler = new NettyResponseHandler(request);
-        VirtualClientConnection connection = VirtualClientConnection.connect(handler, VertxHttpRecorder.VIRTUAL_HTTP,
-                clientAddress);
+        VirtualClientConnection connection = VirtualClientConnection
+                .connect(handler, VertxHttpRecorder.VIRTUAL_HTTP, clientAddress);
 
         connection.sendMessage(nettyRequest);
         connection.sendMessage(requestContent);
@@ -119,5 +94,46 @@ public class LambdaHttpHandler implements RequestHandler<AwsProxyRequest, AwsPro
         } finally {
             connection.close();
         }
+    }
+
+    private String getPathFromRequest(AwsProxyRequest request) throws UnsupportedEncodingException {
+        String path;
+        if (hasQueryParameters(request)) {
+            path = getPathWithQueryParameters(request.getPath(), request);
+        } else {
+            path = request.getPath();
+        }
+        return path;
+    }
+
+    private boolean hasQueryParameters(AwsProxyRequest request) {
+        return request.getMultiValueQueryStringParameters() != null && !request.getMultiValueQueryStringParameters()
+                .isEmpty();
+    }
+
+    private String getPathWithQueryParameters(String path,
+            AwsProxyRequest request) throws UnsupportedEncodingException {
+        StringBuilder sb = new StringBuilder(path);
+        sb.append("?");
+        boolean first = true;
+        for (Map.Entry<String, List<String>> e : request.getMultiValueQueryStringParameters().entrySet()) {
+            for (String v : e.getValue()) {
+                if (first) {
+                    first = false;
+                } else {
+                    sb.append("&");
+                }
+                if (request.getRequestSource() == AwsProxyRequest.RequestSource.ALB) {
+                    sb.append(e.getKey());
+                    sb.append("=");
+                    sb.append(v);
+                } else {
+                    sb.append(URLEncoder.encode(e.getKey(), StandardCharsets.UTF_8.name()));
+                    sb.append("=");
+                    sb.append(URLEncoder.encode(v, StandardCharsets.UTF_8.name()));
+                }
+            }
+        }
+        return sb.toString();
     }
 }

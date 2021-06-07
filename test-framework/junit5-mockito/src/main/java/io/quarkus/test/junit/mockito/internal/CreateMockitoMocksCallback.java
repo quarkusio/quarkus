03d709f8004225c42689de8ed07dfd3c22352354
Stéphane Épardaud <stef@epardaud.fr>
2021-06-07 15:03:16 +0200

    Quarkus Mockito: make sure we unwrap mocked interfaces too

--- test-framework/junit5-mockito/src/main/java/io/quarkus/test/junit/mockito/internal/CreateMockitoMocksCallback.java
+++ test-framework/junit5-mockito/src/main/java/io/quarkus/test/junit/mockito/internal/CreateMockitoMocksCallback.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -18,6 +19,7 @@ import io.quarkus.arc.Arc;
 import io.quarkus.arc.ArcContainer;
 import io.quarkus.arc.ClientProxy;
 import io.quarkus.arc.InjectableBean;
+import io.quarkus.arc.impl.Mockable;
 import io.quarkus.test.junit.callback.QuarkusTestAfterConstructCallback;
 import io.quarkus.test.junit.mockito.InjectMock;
 
@@ -48,6 +50,19 @@ public class CreateMockitoMocksCallback implements QuarkusTestAfterConstructCall
             // and yet some of them appear to have Object as supertype, avoid them
             if (beanClass.getSuperclass() != Object.class)
                 beanClass = beanClass.getSuperclass();
+            else {
+                // try to find the mocked interface
+                Set<Class<?>> foundInterf = new HashSet<>();
+                for (Class<?> interf : beanClass.getInterfaces()) {
+                    if (interf == Mockable.class || interf == ClientProxy.class)
+                        continue;
+                    foundInterf.add(interf);
+                }
+                // only act if we found a single interface
+                if (foundInterf.size() == 1) {
+                    beanClass = foundInterf.iterator().next();
+                }
+            }
         }
         Object mock;
         boolean isNew;

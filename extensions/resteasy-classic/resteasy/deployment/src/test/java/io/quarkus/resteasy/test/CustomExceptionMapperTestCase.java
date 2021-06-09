ab3bfe67fecc2dc59419c4f0c7216af7ba944d47
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-06-09 16:34:32 +1000

    Support constructor injection into providers
    (cherry picked from commit e5b19df7bc0e1c4b3ed39d923136ffacef8831ab)

--- extensions/resteasy-classic/resteasy/deployment/src/test/java/io/quarkus/resteasy/test/CustomExceptionMapperTestCase.java
+++ extensions/resteasy-classic/resteasy/deployment/src/test/java/io/quarkus/resteasy/test/CustomExceptionMapperTestCase.java
@@ -1,5 +1,6 @@
 package io.quarkus.resteasy.test;
 
+import javax.enterprise.context.Dependent;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -18,7 +19,7 @@ public class CustomExceptionMapperTestCase {
     @RegisterExtension
     static QuarkusUnitTest runner = new QuarkusUnitTest()
             .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
-                    .addClasses(RootResource.class, CustomExceptionMapper.class));
+                    .addClasses(RootResource.class, CustomExceptionMapper.class, MessageService.class));
 
     @Test
     public void testResourceNotFound() {
@@ -30,9 +31,23 @@ public class CustomExceptionMapperTestCase {
 
     @Provider
     public static class CustomExceptionMapper implements ExceptionMapper<NotFoundException> {
+
+        final MessageService service;
+
+        public CustomExceptionMapper(MessageService service) {
+            this.service = service;
+        }
+
         @Override
         public Response toResponse(NotFoundException exception) {
             return Response.status(200).entity("not found but OK").build();
+        }
+    }
+
+    @Dependent
+    public static class MessageService {
+        public String msg() {
+            return "not found but OK";
         }
     }
 }

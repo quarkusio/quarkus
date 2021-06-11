0eeb61024f31fb81c829ec99c4e822c5d5ece35a
Georgios Andrianakis <geoand@gmail.com>
2021-06-11 09:41:22 +0300

    Prevent unlikely NoSuchElementException in continuous testing
    Fixes: #17824

--- core/deployment/src/main/java/io/quarkus/deployment/dev/testing/JunitTestRunner.java
+++ core/deployment/src/main/java/io/quarkus/deployment/dev/testing/JunitTestRunner.java
@@ -268,6 +268,7 @@ public class JunitTestRunner {
                                 }
                             }
                         }
+                        touchedClasses.push(Collections.synchronizedSet(new HashSet<>()));
                     }
 
                     @Override

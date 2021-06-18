565cbe6e8bccce8a953b522ee8053e9729a157c1
Jose <josecarvajalhilario@gmail.com>
2021-06-18 17:34:22 +0200

    OpenShift: Fix `quarkus.openshift.arguments` property
    At the moment, when using the `quarkus.openshift.arguments` property and an unknown base image, it's replacing some arguments that are required for the Java command and hence making the application fail. For example, with no arguments are supplied, the OpenShift.yaml looks like:
    
    ```
    command: java
    args:
        - '-Dquarkus.http.host=0.0.0.0'
        - '-Djava.util.logging.manager=org.jboss.logmanager.LogManager'
        - '-jar'
        - /deployments/quarkus-run.jar
    ```
    
    If we now set `quarkus.openshift.arguments=A,B`, then it changes to:
    
    ```
    command: java
    args:
        - 'A'
        - 'B'
    ```
    
    This PR addresses the above situation by moving the required JAVA arguments to be part of the command and leaving the args field empty:
    
    ```
    command: 
        - java
        - '-Dquarkus.http.host=0.0.0.0'
        - '-Djava.util.logging.manager=org.jboss.logmanager.LogManager'
        - '-jar'
        - /deployments/quarkus-run.jar
    ```
    
    And now if we supply the arguments: `quarkus.openshift.arguments=A,B`, then:
    
    ```
    command: 
        - java -Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager -jar /deployments/quarkus-run.jar
    args:
        - 'A'
        - 'B'
    ```
    
    Users would still be able to replace the whole command by using the command property: `quarkus.openshift.command` which I think it's the expected behaviour. Now the command and argument properties work the same in Kubernetes and OpenShift, regardless the base image we use for s2i. 
    
    I've covered these changes using integration tests with the following scenarios:
    - Kubernetes when using arguments
    - Kubernetes when using command
    - OpenShift when using arguments
    - OpenShift when using commands
    
    Also, I've verified these changes manually by deploying a sample application (Rest ++ Command Line application) and now it works fine.

--- extensions/kubernetes/spi/src/main/java/io/quarkus/kubernetes/spi/KubernetesCommandBuildItem.java
+++ extensions/kubernetes/spi/src/main/java/io/quarkus/kubernetes/spi/KubernetesCommandBuildItem.java
@@ -1,22 +1,38 @@
 package io.quarkus.kubernetes.spi;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import io.quarkus.builder.item.SimpleBuildItem;
 
 public final class KubernetesCommandBuildItem extends SimpleBuildItem {
 
-    private final String command;
-    private final String[] args;
+    private final List<String> command;
+    private final List<String> args;
 
-    public KubernetesCommandBuildItem(String command, String... args) {
+    public KubernetesCommandBuildItem(List<String> command, List<String> args) {
         this.command = command;
         this.args = args;
     }
 
-    public String getCommand() {
+    public List<String> getCommand() {
         return command;
     }
 
-    public String[] getArgs() {
+    public List<String> getArgs() {
         return args;
+    }
+
+    public static KubernetesCommandBuildItem command(String... cmd) {
+        return command(Arrays.asList(cmd));
+    }
+
+    public static KubernetesCommandBuildItem command(List<String> cmd) {
+        return new KubernetesCommandBuildItem(cmd, Collections.emptyList());
+    }
+
+    public static KubernetesCommandBuildItem commandWithArgs(String cmd, List<String> args) {
+        return new KubernetesCommandBuildItem(Collections.singletonList(cmd), args);
     }
 }

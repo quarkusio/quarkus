4f66d140385b801b36b1b70b7d03a5b1e8701e19
Yoann Rodière <yoann@hibernate.org>
2021-06-23 12:04:08 +0200

    Remove extra config doc sections in hibernate-search-orm-elasticsearch-aws
    They just look terrible because there are multiple levels of nesting...

--- extensions/hibernate-search-orm-elasticsearch-aws/runtime/src/main/java/io/quarkus/hibernate/search/orm/elasticsearch/aws/runtime/HibernateSearchOrmElasticsearchAwsRuntimeConfig.java
+++ extensions/hibernate-search-orm-elasticsearch-aws/runtime/src/main/java/io/quarkus/hibernate/search/orm/elasticsearch/aws/runtime/HibernateSearchOrmElasticsearchAwsRuntimeConfig.java
@@ -3,7 +3,6 @@ package io.quarkus.hibernate.search.orm.elasticsearch.aws.runtime;
 import java.util.Map;
 
 import io.quarkus.runtime.annotations.ConfigDocMapKey;
-import io.quarkus.runtime.annotations.ConfigDocSection;
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
@@ -20,7 +19,6 @@ public class HibernateSearchOrmElasticsearchAwsRuntimeConfig {
     /**
      * Configuration for additional named persistence units.
      */
-    @ConfigDocSection
     @ConfigDocMapKey("persistence-unit-name")
     @ConfigItem(name = ConfigItem.PARENT)
     public Map<String, HibernateSearchOrmElasticsearchAwsRuntimeConfigPersistenceUnit> persistenceUnits;
--- extensions/hibernate-search-orm-elasticsearch-aws/runtime/src/main/java/io/quarkus/hibernate/search/orm/elasticsearch/aws/runtime/HibernateSearchOrmElasticsearchAwsRuntimeConfigPersistenceUnit.java
+++ extensions/hibernate-search-orm-elasticsearch-aws/runtime/src/main/java/io/quarkus/hibernate/search/orm/elasticsearch/aws/runtime/HibernateSearchOrmElasticsearchAwsRuntimeConfigPersistenceUnit.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import io.quarkus.amazon.common.runtime.AwsCredentialsProviderConfig;
 import io.quarkus.runtime.annotations.ConfigDocMapKey;
-import io.quarkus.runtime.annotations.ConfigDocSection;
 import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigItem;
 import software.amazon.awssdk.regions.Region;
@@ -17,14 +16,12 @@ public class HibernateSearchOrmElasticsearchAwsRuntimeConfigPersistenceUnit {
      * Default backend
      */
     @ConfigItem(name = "elasticsearch")
-    @ConfigDocSection
     ElasticsearchBackendRuntimeConfig defaultBackend;
 
     /**
      * Named backends
      */
     @ConfigItem(name = "elasticsearch")
-    @ConfigDocSection
     public ElasticsearchNamedBackendsRuntimeConfig namedBackends;
 
     @ConfigGroup

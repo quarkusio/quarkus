d09ba4aa07a45de94f148cfd8cf5225d44eba1b0
Jan Martiška <jmartisk@redhat.com>
2021-06-28 10:45:34 +0200

    Test for GraphQL client with NonNull types in native mode
    (cherry picked from commit 0bbf08585d749c53f21078b1078cc0a5bce5f6bb)

--- integration-tests/smallrye-graphql-client/src/test/java/io/quarkus/it/smallrye/graphql/client/TypesafeClientTest.java
+++ integration-tests/smallrye-graphql-client/src/test/java/io/quarkus/it/smallrye/graphql/client/TypesafeClientTest.java
@@ -3,6 +3,7 @@ package io.quarkus.it.smallrye.graphql.client;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URL;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +25,18 @@ public class TypesafeClientTest {
                 .build(LuckyNumbersClientApi.class);
         client.setLuckyNumber(21);
         assertEquals(21, client.luckyNumber());
+    }
+
+    /**
+     * Test a method that contains a `@NonNull` parameter.
+     */
+    @Test
+    public void testNonNull() throws Exception {
+        LuckyNumbersClientApi client = TypesafeGraphQLClientBuilder.newBuilder()
+                .endpoint(url.toString() + "/graphql")
+                .build(LuckyNumbersClientApi.class);
+        List<Integer> someNumbers = List.of(12, 33);
+        List<Integer> returned = client.echoList(someNumbers);
+        assertEquals(someNumbers, returned);
     }
 }

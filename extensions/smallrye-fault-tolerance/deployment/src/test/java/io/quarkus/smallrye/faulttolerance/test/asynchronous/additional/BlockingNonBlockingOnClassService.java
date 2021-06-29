ebbcc968f09ee4608c173f5f0aff46ae4bbbcf5f
Ladislav Thon <lthon@redhat.com>
2021-06-29 18:02:27 +0200

    Update to SmallRye Fault Tolerance 5.2.0
    This SmallRye Fault Tolerance release includes a significant
    refactoring of the configuration system, which has been done
    with Quarkus in mind.
    
    This commit moves a lot of fault tolerance method discovery
    to build time. It also reads fault tolerance annotations from
    ArC's `AnnotationStore`, so that extensions are finally able
    to transform fault tolerance annotations.
    
    Also, more build-time validations are added, corresponding to
    deployment-time validations added in SmallRye Fault Tolerance.
    
    (cherry picked from commit 47476df7066fb58f1eb1853845cc4982184e267d)

---
+++ extensions/smallrye-fault-tolerance/deployment/src/test/java/io/quarkus/smallrye/faulttolerance/test/asynchronous/additional/BlockingNonBlockingOnClassService.java
@@ -0,0 +1,20 @@
+package io.quarkus.smallrye.faulttolerance.test.asynchronous.additional;
+
+import java.util.concurrent.CompletionStage;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.faulttolerance.Retry;
+
+import io.smallrye.common.annotation.Blocking;
+import io.smallrye.common.annotation.NonBlocking;
+
+@ApplicationScoped
+@Blocking
+@NonBlocking
+public class BlockingNonBlockingOnClassService {
+    @Retry
+    public CompletionStage<String> hello() {
+        throw new IllegalArgumentException();
+    }
+}
---
+++ extensions/smallrye-fault-tolerance/deployment/src/test/java/io/quarkus/smallrye/faulttolerance/test/retry/backoff/TwoBackoffsOnMethodTest.java
@@ -0,0 +1,30 @@
+package io.quarkus.smallrye.faulttolerance.test.retry.backoff;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.enterprise.inject.spi.DeploymentException;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class TwoBackoffsOnMethodTest {
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(TwoBackoffsOnMethodService.class))
+            .assertException(e -> {
+                assertEquals(DeploymentException.class, e.getClass());
+                assertTrue(e.getMessage().contains("More than one backoff defined"));
+            });
+
+    @Test
+    public void test() {
+        fail();
+    }
+}

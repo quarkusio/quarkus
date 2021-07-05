248dfed5074a3c7cd413e79cce319978236c818c
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-07-05 14:07:31 +0200

    Sort imports.

--- extensions/logging-sentry/deployment/src/main/java/io/quarkus/logging/sentry/deployment/SentryProcessor.java
+++ extensions/logging-sentry/deployment/src/main/java/io/quarkus/logging/sentry/deployment/SentryProcessor.java
@@ -12,8 +12,6 @@ import io.quarkus.logging.sentry.SentryHandlerValueFactory;
 import io.sentry.Breadcrumb;
 import io.sentry.SentryBaseEvent;
 import io.sentry.SentryEvent;
-import io.sentry.protocol.SentryTransaction;
-import io.sentry.protocol.SentrySpan;
 import io.sentry.SpanContext;
 import io.sentry.SpanId;
 import io.sentry.SpanStatus;
@@ -34,9 +32,11 @@ import io.sentry.protocol.SentryException;
 import io.sentry.protocol.SentryId;
 import io.sentry.protocol.SentryPackage;
 import io.sentry.protocol.SentryRuntime;
+import io.sentry.protocol.SentrySpan;
 import io.sentry.protocol.SentryStackFrame;
 import io.sentry.protocol.SentryStackTrace;
 import io.sentry.protocol.SentryThread;
+import io.sentry.protocol.SentryTransaction;
 import io.sentry.protocol.User;
 
 class SentryProcessor {

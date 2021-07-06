3e1e0a1ee1eafc5d6f3d1465de1641497be72438
Sanne Grinovero <sanne@hibernate.org>
2021-07-06 14:27:30 +0100

    When disposing of a Mutiny.Session on a vert.x thread don't block it

--- extensions/hibernate-reactive/runtime/src/main/java/io/quarkus/hibernate/reactive/runtime/ReactiveSessionProducer.java
+++ extensions/hibernate-reactive/runtime/src/main/java/io/quarkus/hibernate/reactive/runtime/ReactiveSessionProducer.java
@@ -1,5 +1,7 @@
 package io.quarkus.hibernate.reactive.runtime;
 
+import java.util.concurrent.CompletableFuture;
+
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
@@ -23,13 +25,23 @@ public class ReactiveSessionProducer {
 
     public void disposeMutinySession(@Disposes Mutiny.Session reactiveSession) {
         if (reactiveSession != null) {
-            //We're ignoring the returned CompletionStage!
-            //This could certainly done better but it should be effective enough to
-            //eventually close the session: the connection pool will order the operations
-            //as it won't be able to return connections to other consumers when it's saturated,
-            //ensuring close operations are handled.
-            //N.B. for the Mutiny API we need to make sure to subscribe
-            reactiveSession.close().subscribe().asCompletionStage().join();
+            //N.B. make sure to subscribe as this is a Mutiny based Session:
+            // operations don't happen at all if there is no subscription.
+            final CompletableFuture<Void> closeOperation = reactiveSession.close()
+                    .subscribe()
+                    .asCompletionStage();
+            if (!io.vertx.core.Context.isOnVertxThread()) {
+                //When invoked from blocking code, behave as expected and block on the operation
+                //so to not starve resources with a deferred close.
+                closeOperation.join();
+            }
+            // [else] no need to block. Worst that can happen is that the opening
+            // of a new Mutiny.Session needs to wait for an available connection,
+            // which implicitly orders it as "downstream" from the previous close
+            // to have actually happened as the connection pool is reactive.
+            // Also, if connections are available there is no real need to wait for
+            // it, so this should be good.
         }
     }
+
 }

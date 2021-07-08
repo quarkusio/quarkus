bf42f71019e54265f4f81574a348aa4f91243b8f
Georgios Andrianakis <geoand@gmail.com>
2021-07-08 17:58:12 +0300

    Allow injection of entire interface hierarchy when using @ConfigMapping
    Fixes: #18524
    (cherry picked from commit 0dc3171400a96cb1b90c6c13de6605df68e09d81)

--- extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/ConfigBuildStep.java
+++ extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/ConfigBuildStep.java
@@ -35,6 +35,7 @@ import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.ClassType;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.FieldInfo;
+import org.jboss.jandex.IndexView;
 import org.jboss.jandex.MethodInfo;
 import org.jboss.jandex.Type;
 import org.jboss.jandex.Type.Kind;
@@ -77,6 +78,7 @@ public class ConfigBuildStep {
     private static final DotName LIST_NAME = DotName.createSimple(List.class.getName());
     private static final DotName SUPPLIER_NAME = DotName.createSimple(Supplier.class.getName());
     private static final DotName CONFIG_VALUE_NAME = DotName.createSimple(io.smallrye.config.ConfigValue.class.getName());
+    public static final AnnotationInstance[] EMPTY_ANNOTATION_INSTANCES = {};
 
     @BuildStep
     void additionalBeans(BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
@@ -314,6 +316,7 @@ public class ConfigBuildStep {
     void beanConfigClasses(
             List<ConfigClassBuildItem> configClasses,
             BeanRegistrationPhaseBuildItem beanRegistrationPhase,
+            CombinedIndexBuildItem combinedIndex,
             BuildProducer<BeanConfiguratorBuildItem> beanConfigurationRegistry) {
 
         for (ConfigClassBuildItem configClass : configClasses) {
@@ -328,14 +331,50 @@ public class ConfigBuildStep {
                                 new AnnotationValue[] { createStringValue("prefix", configClass.getPrefix()) }));
             }
 
+            collectTypes(combinedIndex, configClass);
+
             beanConfigurationRegistry.produce(new BeanConfiguratorBuildItem(
                     beanRegistrationPhase.getContext()
                             .configure(configClass.getConfigClass())
-                            .types(configClass.getConfigClass())
-                            .qualifiers(qualifiers.toArray(new AnnotationInstance[] {}))
+                            .types(collectTypes(combinedIndex, configClass))
+                            .qualifiers(qualifiers.toArray(EMPTY_ANNOTATION_INSTANCES))
                             .creator(ConfigMappingCreator.class)
                             .param("type", configClass.getConfigClass())
                             .param("prefix", configClass.getPrefix())));
+        }
+    }
+
+    private Type[] collectTypes(CombinedIndexBuildItem combinedIndex, ConfigClassBuildItem configClass) {
+        IndexView index = combinedIndex.getIndex();
+        DotName configIfaceName = DotName.createSimple(configClass.getConfigClass().getName());
+        ClassInfo configIfaceInfo = index.getClassByName(configIfaceName);
+        if ((configIfaceInfo == null) || configIfaceInfo.interfaceNames().isEmpty()) {
+            return new Type[] { Type.create(configIfaceName, Kind.CLASS) };
+        }
+
+        Set<DotName> allIfaces = new HashSet<>();
+        allIfaces.add(configIfaceName);
+        collectInterfacesRec(configIfaceInfo, index, allIfaces);
+        Type[] result = new Type[allIfaces.size()];
+        int i = 0;
+        for (DotName iface : allIfaces) {
+            result[i++] = Type.create(iface, Kind.CLASS);
+        }
+        return result;
+    }
+
+    private static void collectInterfacesRec(ClassInfo current, IndexView index, Set<DotName> result) {
+        List<DotName> interfaces = current.interfaceNames();
+        if (interfaces.isEmpty()) {
+            return;
+        }
+        for (DotName iface : interfaces) {
+            ClassInfo classByName = index.getClassByName(iface);
+            if (classByName == null) {
+                continue; // just ignore this type
+            }
+            result.add(iface);
+            collectInterfacesRec(classByName, index, result);
         }
     }
 

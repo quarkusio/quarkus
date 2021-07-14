28c917abd1303192878e5468ff021eb422e93f0c
StringTek <stringtek@foxmail.com>
2021-07-15 02:31:09 +0800

    refactor: format code

--- extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/naming/QuarkusPhysicalNamingStrategy.java
+++ extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/naming/QuarkusPhysicalNamingStrategy.java
@@ -30,75 +30,75 @@ import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
  */
 public class QuarkusPhysicalNamingStrategy implements PhysicalNamingStrategy {
 
-    @Override
-    public Identifier toPhysicalCatalogName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return apply(name, jdbcEnvironment);
-    }
+	@Override
+	public Identifier toPhysicalCatalogName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+		return apply(name, jdbcEnvironment);
+	}
 
-    @Override
-    public Identifier toPhysicalSchemaName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return apply(name, jdbcEnvironment);
-    }
+	@Override
+	public Identifier toPhysicalSchemaName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+		return apply(name, jdbcEnvironment);
+	}
 
-    @Override
-    public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return apply(name, jdbcEnvironment);
-    }
+	@Override
+	public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+		return apply(name, jdbcEnvironment);
+	}
 
-    @Override
-    public Identifier toPhysicalSequenceName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return apply(name, jdbcEnvironment);
-    }
+	@Override
+	public Identifier toPhysicalSequenceName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+		return apply(name, jdbcEnvironment);
+	}
 
-    @Override
-    public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return apply(name, jdbcEnvironment);
-    }
+	@Override
+	public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+		return apply(name, jdbcEnvironment);
+	}
 
-    private Identifier apply(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        if (name == null) {
-            return null;
-        }
-        StringBuilder builder = new StringBuilder(name.getText().replace('.', '_'));
-        for (int i = 1; i < builder.length() - 1; i++) {
-            if (isUnderscoreRequired(builder.charAt(i - 1), builder.charAt(i),
-                builder.charAt(i + 1))) {
-                builder.insert(i++, '_');
-            }
-        }
-        return getIdentifier(builder.toString(), name.isQuoted(), jdbcEnvironment);
-    }
+	private Identifier apply(Identifier name, JdbcEnvironment jdbcEnvironment) {
+		if (name == null) {
+			return null;
+		}
+		StringBuilder builder = new StringBuilder(name.getText().replace('.', '_'));
+		for (int i = 1; i < builder.length() - 1; i++) {
+			if (isUnderscoreRequired(builder.charAt(i - 1), builder.charAt(i),
+				builder.charAt(i + 1))) {
+				builder.insert(i++, '_');
+			}
+		}
+		return getIdentifier(builder.toString(), name.isQuoted(), jdbcEnvironment);
+	}
 
-    /**
-     * Get an identifier for the specified details. By default this method will return an identifier
-     * with the name adapted based on the result of {@link #isCaseInsensitive(JdbcEnvironment)}
-     *
-     * @param name            the name of the identifier
-     * @param quoted          if the identifier is quoted
-     * @param jdbcEnvironment the JDBC environment
-     * @return an identifier instance
-     */
-    protected Identifier getIdentifier(String name, boolean quoted,
-        JdbcEnvironment jdbcEnvironment) {
-        if (isCaseInsensitive(jdbcEnvironment)) {
-            name = name.toLowerCase(Locale.ROOT);
-        }
-        return new Identifier(name, quoted);
-    }
+	/**
+	 * Get an identifier for the specified details. By default this method will return an identifier
+	 * with the name adapted based on the result of {@link #isCaseInsensitive(JdbcEnvironment)}
+	 *
+	 * @param name            the name of the identifier
+	 * @param quoted          if the identifier is quoted
+	 * @param jdbcEnvironment the JDBC environment
+	 * @return an identifier instance
+	 */
+	protected Identifier getIdentifier(String name, boolean quoted,
+		JdbcEnvironment jdbcEnvironment) {
+		if (isCaseInsensitive(jdbcEnvironment)) {
+			name = name.toLowerCase(Locale.ROOT);
+		}
+		return new Identifier(name, quoted);
+	}
 
-    /**
-     * Specify whether the database is case sensitive.
-     *
-     * @param jdbcEnvironment the JDBC environment which can be used to determine case
-     * @return true if the database is case insensitive sensitivity
-     */
-    protected boolean isCaseInsensitive(JdbcEnvironment jdbcEnvironment) {
-        return true;
-    }
+	/**
+	 * Specify whether the database is case sensitive.
+	 *
+	 * @param jdbcEnvironment the JDBC environment which can be used to determine case
+	 * @return true if the database is case insensitive sensitivity
+	 */
+	protected boolean isCaseInsensitive(JdbcEnvironment jdbcEnvironment) {
+		return true;
+	}
 
-    private boolean isUnderscoreRequired(char before, char current, char after) {
-        return Character.isLowerCase(before) && Character.isUpperCase(current) && Character
-            .isLowerCase(after);
-    }
+	private boolean isUnderscoreRequired(char before, char current, char after) {
+		return Character.isLowerCase(before) && Character.isUpperCase(current) && Character
+			.isLowerCase(after);
+	}
 
 }

e5d6356f41dfecd76597ecb7fac2b9c68b50e08d
Georgios Andrianakis <geoand@gmail.com>
2021-07-15 15:25:51 +0300

    Refactor blocking error handling in RESTEasy Reactive
    (cherry picked from commit cdc1c385ed4851c92b463d0b4978af70060cdf7e)

--- independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/ExceptionMapping.java
+++ independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/ExceptionMapping.java
@@ -2,20 +2,22 @@ package org.jboss.resteasy.reactive.server.core;
 
 import io.smallrye.common.annotation.Blocking;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import org.jboss.logging.Logger;
-import org.jboss.resteasy.reactive.common.core.BlockingNotAllowedException;
 import org.jboss.resteasy.reactive.common.model.ResourceExceptionMapper;
 import org.jboss.resteasy.reactive.server.spi.ResteasyReactiveAsyncExceptionMapper;
 import org.jboss.resteasy.reactive.server.spi.ResteasyReactiveExceptionMapper;
@@ -32,8 +34,7 @@ public class ExceptionMapping {
      * <p>
      * We have a special log message for this.
      */
-    private final Set<Class<? extends Throwable>> blockingProblemExceptionClasses = new HashSet<>(
-            Arrays.asList(BlockingNotAllowedException.class));
+    private final List<Predicate<Throwable>> blockingProblemPredicates = new ArrayList<>();
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public void mapException(Throwable throwable, ResteasyReactiveRequestContext context) {
@@ -105,8 +106,10 @@ public class ExceptionMapping {
                 return false;
             }
             seen.add(e);
-            if (blockingProblemExceptionClasses.contains(e.getClass())) {
-                return true;
+            for (Predicate<Throwable> predicate : blockingProblemPredicates) {
+                if (predicate.test(e)) {
+                    return true;
+                }
             }
             e = e.getCause();
         }
@@ -154,8 +157,12 @@ public class ExceptionMapping {
         return null;
     }
 
-    public void addBlockingProblem(Class<? extends Throwable> clazz) {
-        blockingProblemExceptionClasses.add(clazz);
+    public void addBlockingProblem(Class<? extends Throwable> throwable) {
+        blockingProblemPredicates.add(new ExceptionTypePredicate(throwable));
+    }
+
+    public void addBlockingProblem(Predicate<Throwable> predicate) {
+        blockingProblemPredicates.add(predicate);
     }
 
     public <T extends Throwable> void addExceptionMapper(Class<T> exceptionClass, ResourceExceptionMapper<T> mapper) {
@@ -173,8 +180,8 @@ public class ExceptionMapping {
         return mappers;
     }
 
-    public Set<Class<? extends Throwable>> getBlockingProblemExceptionClasses() {
-        return blockingProblemExceptionClasses;
+    public List<Predicate<Throwable>> getBlockingProblemPredicates() {
+        return blockingProblemPredicates;
     }
 
     public void initializeDefaultFactories(Function<String, BeanFactory<?>> factoryCreator) {
@@ -182,6 +189,32 @@ public class ExceptionMapping {
             if (entry.getValue().getFactory() == null) {
                 entry.getValue().setFactory((BeanFactory) factoryCreator.apply(entry.getValue().getClassName()));
             }
+        }
+    }
+
+    public static class ExceptionTypePredicate implements Predicate<Throwable> {
+
+        private Class<? extends Throwable> throwable;
+
+        // needed for bytecode recording
+        public ExceptionTypePredicate() {
+        }
+
+        public ExceptionTypePredicate(Class<? extends Throwable> throwable) {
+            this.throwable = throwable;
+        }
+
+        public Class<? extends Throwable> getThrowable() {
+            return throwable;
+        }
+
+        public void setThrowable(Class<? extends Throwable> throwable) {
+            this.throwable = throwable;
+        }
+
+        @Override
+        public boolean test(Throwable t) {
+            return t.getClass().equals(throwable);
         }
     }
 }

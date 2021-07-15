7f78e71c647e0f0655fddd8ed4531dfca323f9b7
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-07-15 12:47:47 +1000

    Mark generate-code-tests as thread safe

--- devtools/maven/src/main/java/io/quarkus/maven/GenerateCodeTestsMojo.java
+++ devtools/maven/src/main/java/io/quarkus/maven/GenerateCodeTestsMojo.java
@@ -9,7 +9,7 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 
-@Mojo(name = "generate-code-tests", defaultPhase = LifecyclePhase.GENERATE_TEST_SOURCES, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
+@Mojo(name = "generate-code-tests", defaultPhase = LifecyclePhase.GENERATE_TEST_SOURCES, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME, threadSafe = true)
 public class GenerateCodeTestsMojo extends GenerateCodeMojo {
     @Override
     protected void doExecute() throws MojoExecutionException, MojoFailureException {

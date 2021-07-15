2be876f35879f14d29aa70f04e1e482a412942be
Georgios Andrianakis <geoand@gmail.com>
2021-07-15 09:26:53 +0300

    Provide easy way for QuarkusTestResourceLifecycleManager impls to inject into test
    Resolves: #18698

--- test-framework/common/src/main/java/io/quarkus/test/common/QuarkusTestResourceLifecycleManager.java
+++ test-framework/common/src/main/java/io/quarkus/test/common/QuarkusTestResourceLifecycleManager.java
@@ -1,6 +1,9 @@
 package io.quarkus.test.common;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.function.Predicate;
 
 /**
  * Manage the lifecycle of a test resource, for instance a H2 test server.
@@ -47,9 +50,31 @@ public interface QuarkusTestResourceLifecycleManager {
     }
 
     /**
-     * Allow each resource to provide custom injection of fields of the test class
+     * Allow each resource to provide custom injection of fields of the test class.
+     *
+     * Most implementations will likely use {@link QuarkusTestResourceLifecycleManager#inject(TestInjector)}
+     * as it provides a simpler way to inject into fields of tests.
+     *
+     * It is worth mentioning that this injection into the test class is not under the control of CDI and happens after CDI has
+     * performed
+     * any necessary injections into the test class.
      */
     default void inject(Object testInstance) {
+
+    }
+
+    /**
+     * Simplifies the injection of fields of the test class by providing methods to handle the common injection cases.
+     *
+     * In situations not covered by {@link TestInjector}, user can resort to implementing
+     * {@link QuarkusTestResourceLifecycleManager#inject(Object)}
+     *
+     * It is worth mentioning that this injection into the test class is not under the control of CDI and happens after CDI has
+     * performed
+     * any necessary injections into the test class.
+     */
+    default void inject(TestInjector testInjector) {
+
     }
 
     /**
@@ -60,5 +85,59 @@ public interface QuarkusTestResourceLifecycleManager {
      */
     default int order() {
         return 0;
+    }
+
+    /**
+     * Provides methods to handle the common injection cases. See
+     * {@link QuarkusTestResourceLifecycleManager#inject(TestInjector)}
+     */
+    interface TestInjector {
+
+        /**
+         * @param fieldValue The actual value to inject into a test field
+         * @param predicate User supplied predicate which can be used to determine whether or not the field should be
+         *        set with with {@code fieldValue}
+         */
+        void injectIntoFields(Object fieldValue, Predicate<Field> predicate);
+
+        /**
+         * Returns {@code true} if the field is annotated with the supplied annotation.
+         */
+        class Annotated implements Predicate<Field> {
+
+            private final Class<? extends Annotation> annotationClass;
+
+            public Annotated(Class<? extends Annotation> annotationClass) {
+                this.annotationClass = annotationClass;
+            }
+
+            @Override
+            public boolean test(Field field) {
+                return field.getAnnotation(annotationClass) != null;
+            }
+        }
+
+        /**
+         * Returns {@code true} if the field is annotated with the supplied annotation and can also be assigned
+         * to the supplied type.
+         */
+        class AnnotatedAndMatchesType implements Predicate<Field> {
+
+            private final Class<? extends Annotation> annotationClass;
+            private final Class<?> expectedFieldType;
+
+            public AnnotatedAndMatchesType(Class<? extends Annotation> annotationClass, Class<?> expectedFieldType) {
+                this.annotationClass = annotationClass;
+                this.expectedFieldType = expectedFieldType;
+            }
+
+            @Override
+            public boolean test(Field field) {
+                if (field.getAnnotation(annotationClass) == null) {
+                    return false;
+                }
+                return field.getType().isAssignableFrom(expectedFieldType);
+            }
+        }
     }
 }

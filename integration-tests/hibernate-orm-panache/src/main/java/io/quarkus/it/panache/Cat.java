4284f510abce7de3e856ca57fe7c8bcfcbe56458
Guillaume Smet <guillaume.smet@gmail.com>
2021-07-16 13:38:55 +0200

    Move ProjectedFieldName to the common subpackage to avoid split package
    For now, only deprecate the old annotation, it will be removed in 2.2.

--- integration-tests/hibernate-orm-panache/src/main/java/io/quarkus/it/panache/Cat.java
+++ integration-tests/hibernate-orm-panache/src/main/java/io/quarkus/it/panache/Cat.java
@@ -8,10 +8,13 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 @Entity
 public class Cat extends PanacheEntity {
 
+    String name;
+
     @ManyToOne
     CatOwner owner;
 
-    public Cat(CatOwner owner) {
+    public Cat(String name, CatOwner owner) {
+        this.name = name;
         this.owner = owner;
     }
 

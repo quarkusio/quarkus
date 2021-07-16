4ecde58587ef496a63cc651dcef08f6f8fc029ac
Loïc Mathieu <loikeseke@gmail.com>
2021-07-16 12:16:08 +0200

    Allow creating a Kafka ObjectMapperDeserializer with a TypeReference
    This is needed to handle more complex deserialization features, like deserializing to a collection type.

--- integration-tests/kafka/src/test/java/io/quarkus/it/kafka/KafkaCodecTest.java
+++ integration-tests/kafka/src/test/java/io/quarkus/it/kafka/KafkaCodecTest.java
@@ -66,4 +66,47 @@ public class KafkaCodecTest {
 
     }
 
+    @Test
+    public void testJsonbCodecWithList() {
+        RestAssured
+                .given()
+                .header("Content-Type", "application/json")
+                .body("[{\"name\":\"kate\", \"id\":\"1234\"},{\"name\":\"john\", \"id\":\"2345\"}]")
+                .post("/codecs/person-list");
+
+        RestAssured
+                .given()
+                .header("Accept", "application/json")
+                .get("/codecs/person-list")
+                .then()
+                .body("size()", is(2))
+                .body("[0].name", is("kate"))
+                .body("[0].id", is(1234))
+                .body("[1].name", is("john"))
+                .body("[1].id", is(2345));
+        ;
+
+    }
+
+    @Test
+    public void testJacksonCodecWithList() {
+        RestAssured
+                .given()
+                .header("Content-Type", "application/json")
+                .body("[{\"title\":\"Inception\", \"year\":\"2010\"},{\"title\":\"Terminator\", \"year\":\"1984\"}]")
+                .post("/codecs/movie-list");
+
+        RestAssured
+                .given()
+                .header("Accept", "application/json")
+                .get("/codecs/movie-list")
+                .then()
+                .body("size()", is(2))
+                .body("[0].title", is("Inception"))
+                .body("[0].year", is(2010))
+                .body("[1].title", is("Terminator"))
+                .body("[1].year", is(1984));
+
+    }
+
 }

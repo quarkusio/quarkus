d63d9f13e5d19f02c8d4240866eb88b8eb4b4816
Georgios Andrianakis <geoand@gmail.com>
2021-07-30 12:00:06 +0300

    Suppress split package warning in Fabric8 Openshift Client
    Fixes: #19092
    (cherry picked from commit 5dcf1d9c7c4b5ae06a1f72654e92375d21e8028e)

--- extensions/openshift-client/deployment/src/main/java/io/quarkus/openshift/client/deployment/OpenShiftClientProcessor.java
+++ extensions/openshift-client/deployment/src/main/java/io/quarkus/openshift/client/deployment/OpenShiftClientProcessor.java
@@ -1,6 +1,9 @@
 package io.quarkus.openshift.client.deployment;
 
+import java.util.Collections;
+
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.arc.deployment.IgnoreSplitPackageBuildItem;
 import io.quarkus.deployment.Feature;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
@@ -18,5 +21,10 @@ public class OpenShiftClientProcessor {
     @BuildStep
     public FeatureBuildItem feature() {
         return new FeatureBuildItem(Feature.OPENSHIFT_CLIENT);
+    }
+
+    @BuildStep
+    public IgnoreSplitPackageBuildItem splitPackages() {
+        return new IgnoreSplitPackageBuildItem(Collections.singleton("io.fabric8.openshift.api.model"));
     }
 }

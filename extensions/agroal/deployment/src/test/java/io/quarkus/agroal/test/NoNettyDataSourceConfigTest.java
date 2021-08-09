6c73e1364700ae0e01bb0002f5d6f8b6f6a27f27
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-08-09 12:15:21 +1000

    Fall back to ThreadLocal if Netty is not present
    Fixes #19247
    
    (cherry picked from commit 523c184cb09b0949c600fcfd995f24bb0a17ed86)

---
+++ extensions/agroal/deployment/src/test/java/io/quarkus/agroal/test/NoNettyDataSourceConfigTest.java
@@ -0,0 +1,43 @@
+package io.quarkus.agroal.test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.agroal.api.AgroalDataSource;
+import io.agroal.api.configuration.AgroalConnectionPoolConfiguration;
+import io.quarkus.test.QuarkusUnitTest;
+
+public class NoNettyDataSourceConfigTest {
+
+    @Inject
+    AgroalDataSource defaultDataSource;
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withConfigurationResource("base.properties")
+            //this is a bit yuck, but lots of deps used by other tests bring in Netty as transitive dependencies
+            //we need to exclude them all
+            //this removes the need to have a whole new module just for this test
+            .overrideConfigKey("quarkus.class-loading.removed-artifacts",
+                    "io.netty:netty-common,io.quarkus:quarkus-vertx-core,io.quarkus:quarkus-vertx-core-deployment," +
+                            "io.quarkus:quarkus-resteasy-deployment,io.quarkus:quarkus-resteasy," +
+                            "io.quarkus:quarkus-resteasy-server-common-deployment, io.quarkus:quarkus-resteasy-server-common," +
+                            "io.quarkus:quarkus-vertx-http,io.quarkus:quarkus-vertx-http-deployment," +
+                            "io.quarkus:quarkus-netty,io.quarkus:quarkus-netty-deployment," +
+                            "io.quarkus:quarkus-smallrye-metrics,io.quarkus:quarkus-smallrye-metrics-deployment," +
+                            "io.quarkus:quarkus-smallrye-open-api,io.quarkus:quarkus-smallrye-open-api-deployment," +
+                            "io.quarkus:quarkus-smallrye-health-deployment,io.quarkus:quarkus-smallrye-health,");
+
+    @Test
+    public void testNoNetty() throws SQLException {
+        AgroalConnectionPoolConfiguration configuration = defaultDataSource.getConfiguration().connectionPoolConfiguration();
+
+        try (Connection connection = defaultDataSource.getConnection()) {
+        }
+    }
+}
--- extensions/agroal/runtime/src/main/java/io/quarkus/agroal/runtime/QuarkusSimpleConnectionCache.java
+++ extensions/agroal/runtime/src/main/java/io/quarkus/agroal/runtime/QuarkusSimpleConnectionCache.java
@@ -4,17 +4,15 @@ import org.jboss.threads.JBossThread;
 
 import io.agroal.api.cache.Acquirable;
 import io.agroal.api.cache.ConnectionCache;
-import io.netty.util.concurrent.FastThreadLocal;
-import io.netty.util.concurrent.FastThreadLocalThread;
 
-class QuarkusConnectionCache implements ConnectionCache {
+class QuarkusSimpleConnectionCache implements ConnectionCache {
 
-    volatile FastThreadLocal<Acquirable> connectionCache = new FastThreadLocal<>();
+    volatile ThreadLocal<Acquirable> connectionCache = new ThreadLocal<>();
 
     @Override
     public Acquirable get() {
         Thread thread = Thread.currentThread();
-        if (thread instanceof FastThreadLocalThread || thread instanceof JBossThread) {
+        if (thread instanceof JBossThread) {
             //we only want to cache on threads that we control the lifecycle
             //which are the vert.x and potentially jboss threads
             //JBossThread still works with FastThreadLocal, it is just slower, and for most apps
@@ -29,13 +27,13 @@ class QuarkusConnectionCache implements ConnectionCache {
     @Override
     public void put(Acquirable acquirable) {
         Thread thread = Thread.currentThread();
-        if (thread instanceof FastThreadLocalThread || thread instanceof JBossThread) {
+        if (thread instanceof JBossThread) {
             connectionCache.set(acquirable);
         }
     }
 
     @Override
     public void reset() {
-        connectionCache = new FastThreadLocal<>();
+        connectionCache = new ThreadLocal<>();
     }
 }

76939af430b624240cdbfa634da74413bf54a7df
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-08-12 10:54:10 +1000

    Add tests for CT + Kafka + DevServices
    Fixes #19180

---
+++ extensions/smallrye-reactive-messaging-kafka/deployment/src/test/java/io/quarkus/smallrye/reactivemessaging/kafka/deployment/testing/PriceGenerator.java
@@ -0,0 +1,20 @@
+package io.quarkus.smallrye.reactivemessaging.kafka.deployment.testing;
+
+//import io.smallrye.mutiny.Multi;
+//import org.eclipse.microprofile.reactive.messaging.Outgoing;
+//
+//import javax.enterprise.context.ApplicationScoped;
+//import java.time.Duration;
+//import java.util.Random;
+//
+//@ApplicationScoped
+public class PriceGenerator {
+    //        private final Random random = new Random();
+    //
+    //        @Outgoing("generated-price")
+    //        public Multi<Integer> generate() {
+    //            return Multi.createFrom().ticks().every(Duration.ofMillis(10))
+    //                    .onOverflow().drop()
+    //                    .map(tick -> this.random.nextInt(100));
+    //        }
+}
---
+++ extensions/smallrye-reactive-messaging-kafka/deployment/src/test/java/io/quarkus/smallrye/reactivemessaging/kafka/deployment/testing/PriceResourceET.java
@@ -0,0 +1,47 @@
+package io.quarkus.smallrye.reactivemessaging.kafka.deployment.testing;
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.sse.SseEventSource;
+
+import org.assertj.core.api.Assertions;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class PriceResourceET {
+    @TestHTTPEndpoint(PriceResource.class)
+    @TestHTTPResource("/stream")
+    URI uri;
+
+    @Test
+    public void sseStream() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(this.uri);
+
+        List<Double> received = new CopyOnWriteArrayList<>();
+
+        try (SseEventSource source = SseEventSource.target(target).build()) {
+            source.register(inboundSseEvent -> received.add(Double.valueOf(inboundSseEvent.readData())));
+            source.open();
+
+            Awaitility.await()
+                    .atMost(Duration.ofSeconds(1))
+                    .until(() -> received.size() >= 2);
+        }
+
+        Assertions.assertThat(received)
+                .hasSizeGreaterThan(2)
+                .allMatch(value -> (value >= 0) && (value < 100));
+    }
+}

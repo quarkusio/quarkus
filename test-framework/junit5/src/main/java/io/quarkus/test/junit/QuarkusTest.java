8e2b27e840e12e8ab62c1c68d56091b3845e3f09
Guillaume Smet <guillaume.smet@gmail.com>
2021-08-18 12:50:24 +0200

    Add a JUnit tag for @QuarkusTest so that we can exclude them easily
    If you mix tests in your project, you will need to exclude the
    @QuarkusTests so we'd better include a tag for it.
    
    (cherry picked from commit 77621f9ac6f28a607d80375f3f645cf1dbebdf94)

--- test-framework/junit5/src/main/java/io/quarkus/test/junit/QuarkusTest.java
+++ test-framework/junit5/src/main/java/io/quarkus/test/junit/QuarkusTest.java
@@ -5,10 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(QuarkusTestExtension.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Tag("io.quarkus.test.junit.QuarkusTest")
 public @interface QuarkusTest {
 }

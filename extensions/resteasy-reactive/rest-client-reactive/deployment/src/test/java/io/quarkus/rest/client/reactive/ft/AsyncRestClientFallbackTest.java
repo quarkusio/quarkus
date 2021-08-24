e5a688f5cfaaaa3550b439afc279cc56c90d1a85
Ladislav Thon <lthon@redhat.com>
2021-08-24 09:47:27 +0200

    Fix SmallRye Fault Tolerance build-time class loading
    The Fault Tolerance extension performs discovery at build time.
    It finds bean classes that use fault tolerance annotations and
    passes that information to runtime (where SmallRye Fault Tolerance
    applies runtime configuration). To represent types, SmallRye
    Fault Tolerance uses `Class` objects, and so the discovery process
    used to load bean classes from the deployment classloader.
    
    This works fine, unless one of the bean classes is generated
    by another Quarkus extension. These generated classes are stored
    in memory and the deployment classloader doesn't know about them.
    Naturally, attempting to load the class fails and aborts the build.
    
    This commit fixes the problem by not loading the classes at all.
    Instead, a class proxy is generated for each class. This still lets
    us use `Class` objects to represent types, but they are never loaded
    during build, only at runtime.
    
    The class proxy construct is marked as deprecated, because loading
    classes at build time is possible (it wasn't in the past). In this
    situation, though, where the class potentially isn't available,
    it's a perfect fit.

---
+++ extensions/resteasy-reactive/rest-client-reactive/deployment/src/test/java/io/quarkus/rest/client/reactive/ft/AsyncRestClientFallbackTest.java
@@ -0,0 +1,68 @@
+package io.quarkus.rest.client.reactive.ft;
+
+import static io.quarkus.rest.client.reactive.RestClientTestUtil.setUrlForClass;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
+
+import org.eclipse.microprofile.faulttolerance.Asynchronous;
+import org.eclipse.microprofile.faulttolerance.ExecutionContext;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.FallbackHandler;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class AsyncRestClientFallbackTest {
+    @RegisterExtension
+    static final QuarkusUnitTest TEST = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(TestEndpoint.class, Client.class, MyFallback.class)
+                    .addAsResource(new StringAsset(setUrlForClass(Client.class)), "application.properties"));
+
+    @Inject
+    @RestClient
+    Client client;
+
+    @Test
+    public void testFallbackWasUsed() throws ExecutionException, InterruptedException {
+        assertEquals("pong", client.ping().toCompletableFuture().get());
+    }
+
+    @Path("/test")
+    public static class TestEndpoint {
+        @GET
+        public String get() {
+            throw new WebApplicationException(404);
+        }
+    }
+
+    @RegisterRestClient
+    public interface Client {
+        @GET
+        @Path("/test")
+        @Asynchronous
+        @Fallback(MyFallback.class)
+        CompletionStage<String> ping();
+    }
+
+    public static class MyFallback implements FallbackHandler<CompletionStage<String>> {
+        @Override
+        public CompletionStage<String> handle(ExecutionContext context) {
+            return completedFuture("pong");
+        }
+    }
+}

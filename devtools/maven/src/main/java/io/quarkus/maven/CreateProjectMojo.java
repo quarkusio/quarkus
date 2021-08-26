222e292e846b4a535a72e4f6dcfb99d5dfa5b84d
Alexey Loubyansky <olubyans@redhat.com>
2021-08-26 17:32:12 +0200

    Check whether the platform catalog couldn't be resolved from a Maven mirror and indicate that it might not be up-to-date in the error message
    (cherry picked from commit d04bbbca5d1b8f40e4575741a9efa03c4ecaf763)

--- devtools/maven/src/main/java/io/quarkus/maven/CreateProjectMojo.java
+++ devtools/maven/src/main/java/io/quarkus/maven/CreateProjectMojo.java
@@ -368,7 +368,7 @@ public class CreateProjectMojo extends AbstractMojo {
                             new ArtifactCoords(getPlatformGroupId(mojo, groupId), getPlatformArtifactId(artifactId), "pom",
                                     getPlatformVersion(mojo, version))));
         } catch (RegistryResolutionException e) {
-            throw new MojoExecutionException("Failed to resolve the extensions catalog", e);
+            throw new MojoExecutionException("Failed to resolve the extension catalog", e);
         }
 
         if (catalog == null) {

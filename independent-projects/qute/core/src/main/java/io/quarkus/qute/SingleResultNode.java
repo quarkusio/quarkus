a4872504d993d5a54a7c8bd4a0365aa1a8324f3a
Martin Kouba <mkouba@redhat.com>
2021-08-27 09:27:58 +0200

    Qute - unwrap java.util.Optional automatically
    - also handle empty Optional specially in the "or" resolver
    - resolves #19554

--- independent-projects/qute/core/src/main/java/io/quarkus/qute/SingleResultNode.java
+++ independent-projects/qute/core/src/main/java/io/quarkus/qute/SingleResultNode.java
@@ -1,26 +1,27 @@
 package io.quarkus.qute;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
  * A result node backed by a single object value.
  */
-public class SingleResultNode implements ResultNode {
+public final class SingleResultNode implements ResultNode {
 
     private final Object value;
-    private final ExpressionNode expressionNode;
+    private final ExpressionNode node;
 
     public SingleResultNode(Object value, ExpressionNode expressionNode) {
-        this.value = value;
-        this.expressionNode = expressionNode.getEngine().getResultMappers().isEmpty() ? null : expressionNode;
+        this.value = value instanceof Optional ? ((Optional<?>) value).orElse(null) : value;
+        this.node = expressionNode.hasEngineResultMappers() ? expressionNode : null;
     }
 
     @Override
     public void process(Consumer<String> consumer) {
         if (value != null) {
-            String result = null;
-            if (expressionNode != null) {
-                result = expressionNode.getEngine().mapResult(value, expressionNode.expression);
+            String result;
+            if (node != null) {
+                result = node.mapResult(value);
             } else {
                 result = value.toString();
             }

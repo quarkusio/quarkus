a248b0851f2f41c59d3d30413a392aaef1109165
Georgios Andrianakis <geoand@gmail.com>
2021-09-01 18:47:08 +0300

    Take @TestSecurity(authorizationEnabled = false) into account for RESTEasy Reactive
    Relates to: #19834
    
    (cherry picked from commit 07f9e8741d35016df1727ffd834412b8d2bb1b15)

--- extensions/security/runtime/src/main/java/io/quarkus/security/runtime/interceptor/check/AuthenticatedCheck.java
+++ extensions/security/runtime/src/main/java/io/quarkus/security/runtime/interceptor/check/AuthenticatedCheck.java
@@ -2,21 +2,47 @@ package io.quarkus.security.runtime.interceptor.check;
 
 import java.lang.reflect.Method;
 
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.ArcContainer;
+import io.quarkus.arc.InstanceHandle;
 import io.quarkus.security.UnauthorizedException;
 import io.quarkus.security.identity.SecurityIdentity;
+import io.quarkus.security.spi.runtime.AuthorizationController;
 import io.quarkus.security.spi.runtime.SecurityCheck;
 
 public class AuthenticatedCheck implements SecurityCheck {
 
     public static final AuthenticatedCheck INSTANCE = new AuthenticatedCheck();
 
+    private volatile AuthorizationController authorizationController;
+
     private AuthenticatedCheck() {
     }
 
     @Override
     public void apply(SecurityIdentity identity, Method method, Object[] parameters) {
+        if (isAuthorizationDisabled()) {
+            return;
+        }
         if (identity.isAnonymous()) {
             throw new UnauthorizedException();
         }
+    }
+
+    private boolean isAuthorizationDisabled() {
+        if (authorizationController != null) {
+            return !authorizationController.isAuthorizationEnabled();
+        }
+
+        ArcContainer container = Arc.container();
+        if ((container == null) || !container.isRunning()) {
+            return false;
+        }
+        InstanceHandle<AuthorizationController> instance = container.instance(AuthorizationController.class);
+        if (instance.isAvailable()) {
+            authorizationController = instance.get();
+            return !instance.get().isAuthorizationEnabled();
+        }
+        return false;
     }
 }

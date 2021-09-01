85ed84e37373ba0993f3f908e7a6e008d2514210
Jan Martiška <jmartisk@redhat.com>
2021-09-01 13:20:42 +0200

    SmallRye GraphQL 1.3.2
    (cherry picked from commit 40ced5eed45d9ee94e42df8e99b46fa9043410fa)

--- extensions/smallrye-graphql-client/runtime/src/main/java/io/quarkus/smallrye/graphql/client/runtime/GraphQLClientConfigurationMergerBean.java
+++ extensions/smallrye-graphql-client/runtime/src/main/java/io/quarkus/smallrye/graphql/client/runtime/GraphQLClientConfigurationMergerBean.java
@@ -13,14 +13,13 @@ import io.smallrye.graphql.client.GraphQLClientsConfiguration;
  * On startup, this beans takes Quarkus-specific configuration of GraphQL clients (quarkus.* properties)
  * and merges this configuration with the configuration parsed by SmallRye GraphQL itself (CLIENT/mp-graphql/* properties)
  *
- * The resulting merged configuration resides in the application-scoped `io.smallrye.graphql.client.GraphQLClientConfiguration`
+ * The resulting merged configuration resides in `io.smallrye.graphql.client.GraphQLClientsConfiguration`
  *
  * Quarkus configuration overrides SmallRye configuration where applicable.
  */
 @Singleton
 public class GraphQLClientConfigurationMergerBean {
 
-    @Inject
     GraphQLClientsConfiguration upstreamConfiguration;
 
     @Inject
@@ -31,6 +30,7 @@ public class GraphQLClientConfigurationMergerBean {
 
     @PostConstruct
     void enhanceGraphQLConfiguration() {
+        upstreamConfiguration = GraphQLClientsConfiguration.getInstance();
         for (Map.Entry<String, GraphQLClientConfig> client : quarkusConfiguration.clients.entrySet()) {
             // the raw config key provided in the config, this might be a short class name,
             // so translate that into the fully qualified name if applicable
@@ -43,14 +43,14 @@ public class GraphQLClientConfigurationMergerBean {
 
             GraphQLClientConfig quarkusConfig = client.getValue();
             // if SmallRye configuration does not contain this client, simply use it
-            if (!upstreamConfiguration.getClients().containsKey(configKey)) {
+            if (upstreamConfiguration.getClient(configKey) == null) {
                 GraphQLClientConfiguration transformed = new GraphQLClientConfiguration();
                 transformed.setHeaders(quarkusConfig.headers);
                 quarkusConfig.url.ifPresent(transformed::setUrl);
-                upstreamConfiguration.getClients().put(configKey, transformed);
+                upstreamConfiguration.addClient(configKey, transformed);
             } else {
                 // if SmallRye configuration already contains this client, override it with the Quarkus configuration
-                GraphQLClientConfiguration upstreamConfig = upstreamConfiguration.getClients().get(configKey);
+                GraphQLClientConfiguration upstreamConfig = upstreamConfiguration.getClient(configKey);
                 quarkusConfig.url.ifPresent(upstreamConfig::setUrl);
                 // merge the headers
                 if (quarkusConfig.headers != null) {

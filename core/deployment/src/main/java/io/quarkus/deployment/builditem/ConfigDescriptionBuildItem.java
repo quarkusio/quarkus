377ae75a947997ae08e927402c336c878ad8b065
Phillip Kruger <phillip.kruger@gmail.com>
2021-09-01 08:51:49 +0200

    Dev UI Config screen render config editor based on type (Fix #19636) Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- core/deployment/src/main/java/io/quarkus/deployment/builditem/ConfigDescriptionBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/builditem/ConfigDescriptionBuildItem.java
@@ -1,5 +1,7 @@
 package io.quarkus.deployment.builditem;
 
+import java.util.List;
+
 import io.quarkus.builder.item.MultiBuildItem;
 
 /**
@@ -12,12 +14,17 @@ public final class ConfigDescriptionBuildItem extends MultiBuildItem implements 
     private final Class<?> type;
     private final String defaultValue;
     private final String docs;
+    private final String valueTypeName;
+    private final List<String> allowedValues;
 
-    public ConfigDescriptionBuildItem(String propertyName, Class<?> type, String defaultValue, String docs) {
+    public ConfigDescriptionBuildItem(String propertyName, Class<?> type, String defaultValue, String docs,
+            String valueTypeName, List<String> allowedValues) {
         this.propertyName = propertyName;
         this.type = type;
         this.defaultValue = defaultValue;
         this.docs = docs;
+        this.valueTypeName = valueTypeName;
+        this.allowedValues = allowedValues;
     }
 
     public String getPropertyName() {
@@ -34,6 +41,18 @@ public final class ConfigDescriptionBuildItem extends MultiBuildItem implements 
 
     public String getDocs() {
         return docs;
+    }
+
+    public String getValueTypeName() {
+        return valueTypeName;
+    }
+
+    public List<String> getAllowedValues() {
+        return allowedValues;
+    }
+
+    public boolean hasAllowedValues() {
+        return allowedValues != null && !allowedValues.isEmpty();
     }
 
     @Override
--- core/deployment/src/main/java/io/quarkus/deployment/steps/ConfigDescriptionBuildStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/steps/ConfigDescriptionBuildStep.java
@@ -3,10 +3,17 @@ package io.quarkus.deployment.steps;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
 import java.util.Properties;
+import java.util.Set;
 import java.util.function.Consumer;
+import java.util.logging.Level;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
@@ -50,6 +57,9 @@ public class ConfigDescriptionBuildStep {
                 final ConfigProperty configProperty = field.getAnnotation(ConfigProperty.class);
                 String defaultDefault;
                 final Class<?> valueClass = field.getType();
+
+                EffectiveConfigTypeAndValues effectiveConfigTypeAndValues = getTypeName(field);
+
                 if (valueClass == boolean.class) {
                     defaultDefault = "false";
                 } else if (valueClass.isPrimitive() && valueClass != char.class) {
@@ -72,9 +82,138 @@ public class ConfigDescriptionBuildStep {
                 String javadocKey = field.getDeclaringClass().getName().replace('$', '.') + '.' + field.getName();
                 ret.add(new ConfigDescriptionBuildItem("quarkus." + node.getPropertyName(),
                         node.findEnclosingClass().getConfigurationClass(),
-                        defVal, javadoc.getProperty(javadocKey)));
+                        defVal,
+                        javadoc.getProperty(javadocKey),
+                        effectiveConfigTypeAndValues.getTypeName(),
+                        effectiveConfigTypeAndValues.getAllowedValues()));
             }
         });
+    }
+
+    private EffectiveConfigTypeAndValues getTypeName(Field field) {
+        final Class<?> valueClass = field.getType();
+        return getTypeName(field, valueClass);
+    }
+
+    private EffectiveConfigTypeAndValues getTypeName(Field field, Class<?> valueClass) {
+        EffectiveConfigTypeAndValues typeAndValues = new EffectiveConfigTypeAndValues();
+        String name = valueClass.getName();
+
+        // Extract Optionals, Lists and Sets
+        if ((valueClass.equals(Optional.class) || valueClass.equals(List.class) || valueClass.equals(Set.class))) {
+
+            if (field != null) {
+                Type genericType = field.getGenericType();
+                name = genericType.getTypeName();
+            }
+
+            if (name.contains("<") && name.contains(">")) {
+                name = name.substring(name.lastIndexOf("<") + 1, name.indexOf(">"));
+            }
+
+            try {
+                Class<?> c = Class.forName(name);
+                return getTypeName(null, c);
+            } catch (ClassNotFoundException ex) {
+                // Then we use the name as is.
+            }
+        }
+
+        // Check other optionals
+        if (valueClass.equals(OptionalInt.class)) {
+            name = Integer.class.getName();
+        } else if (valueClass.equals(OptionalDouble.class)) {
+            name = Double.class.getName();
+        } else if (valueClass.equals(OptionalLong.class)) {
+            name = Long.class.getName();
+        }
+
+        // Check if this is an enum
+        if (Enum.class.isAssignableFrom(valueClass)) {
+            name = Enum.class.getName();
+
+            Object[] values = valueClass.getEnumConstants();
+            for (Object v : values) {
+                Enum casted = (Enum) valueClass.cast(v);
+                typeAndValues.addAllowedValue(casted.name());
+            }
+        }
+
+        // Special case for Log level
+        if (valueClass.isAssignableFrom(Level.class)) {
+            typeAndValues.addAllowedValue(Level.ALL.getName());
+            typeAndValues.addAllowedValue(Level.CONFIG.getName());
+            typeAndValues.addAllowedValue(Level.FINE.getName());
+            typeAndValues.addAllowedValue(Level.FINER.getName());
+            typeAndValues.addAllowedValue(Level.FINEST.getName());
+            typeAndValues.addAllowedValue(Level.INFO.getName());
+            typeAndValues.addAllowedValue(Level.OFF.getName());
+            typeAndValues.addAllowedValue(Level.SEVERE.getName());
+            typeAndValues.addAllowedValue(Level.WARNING.getName());
+        }
+
+        // Map all primatives 
+        if (name.equals("int")) {
+            name = Integer.class.getName();
+        } else if (name.equals("boolean")) {
+            name = Boolean.class.getName();
+        } else if (name.equals("float")) {
+            name = Float.class.getName();
+        } else if (name.equals("double")) {
+            name = Double.class.getName();
+        } else if (name.equals("long")) {
+            name = Long.class.getName();
+        } else if (name.equals("byte")) {
+            name = Byte.class.getName();
+        } else if (name.equals("short")) {
+            name = Short.class.getName();
+        } else if (name.equals("char")) {
+            name = Character.class.getName();
+        }
+
+        typeAndValues.setTypeName(name);
+        return typeAndValues;
+    }
+
+    class EffectiveConfigTypeAndValues {
+        private String typeName;
+        private List<String> allowedValues;
+
+        public EffectiveConfigTypeAndValues() {
+
+        }
+
+        public EffectiveConfigTypeAndValues(String typeName) {
+            this.typeName = typeName;
+        }
+
+        public EffectiveConfigTypeAndValues(String typeName, List<String> allowedValues) {
+            this.typeName = typeName;
+            this.allowedValues = allowedValues;
+        }
+
+        public String getTypeName() {
+            return typeName;
+        }
+
+        public void setTypeName(String typeName) {
+            this.typeName = typeName;
+        }
+
+        public List<String> getAllowedValues() {
+            return allowedValues;
+        }
+
+        public void setAllowedValues(List<String> allowedValues) {
+            this.allowedValues = allowedValues;
+        }
+
+        public void addAllowedValue(String v) {
+            if (allowedValues == null) {
+                allowedValues = new ArrayList<>();
+            }
+            allowedValues.add(v);
+        }
     }
 
 }

daa7878e317919f5cb4519a9bbc95bac586f282f
George Gastaldi <gegastaldi@gmail.com>
2021-09-14 02:08:42 -0300

    Improved assertions in BasicJavaNativeBuildIT
    To get better failure messages

--- integration-tests/gradle/src/test/java/io/quarkus/gradle/nativeimage/BasicJavaNativeBuildIT.java
+++ integration-tests/gradle/src/test/java/io/quarkus/gradle/nativeimage/BasicJavaNativeBuildIT.java
@@ -1,13 +1,11 @@
 package io.quarkus.gradle.nativeimage;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.gradle.BuildResult;
@@ -24,15 +22,17 @@ public class BasicJavaNativeBuildIT extends QuarkusNativeGradleITBase {
         assertThat(build.getTasks().get(":quarkusBuild")).isEqualTo(BuildResult.SUCCESS_OUTCOME);
         final String buildOutput = build.getOutput();
         // make sure the output log during the build contains some expected logs from the native-image process
-        assertTrue(buildOutput.contains("(clinit):") && buildOutput.contains("(typeflow):") && buildOutput.contains("[total]:"),
-                "native-image build log is missing certain expected log messages");
+        assertThat(buildOutput)
+                .withFailMessage("native-image build log is missing certain expected log messages: \n\n %s", buildOutput)
+                .contains("(clinit):", "(typeflow):", "[total]:");
         Path nativeImagePath = projectDir.toPath().resolve("build").resolve("foo-1.0.0-SNAPSHOT-runner");
         assertThat(nativeImagePath).exists();
         Process nativeImageProcess = runNativeImage(nativeImagePath.toAbsolutePath().toString());
         try {
             final String response = DevModeTestUtils.getHttpResponse("/hello");
-            Assertions.assertTrue(response.contains("hello"),
-                    "Response " + response + " for /hello was expected to contain the hello, but didn't");
+            assertThat(response)
+                    .withFailMessage("Response %s for /hello was expected to contain the hello, but didn't", response)
+                    .contains("hello");
         } finally {
             nativeImageProcess.destroy();
         }

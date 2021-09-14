1794c3a1ccf25875f8d670315427dd52df3fcdfc
Josef Smrcka <jsmrcka@redhat.com>
2021-09-14 16:14:42 +0200

    Reintroduce lost tests in mongodb-client
    Fixes https://github.com/quarkusio/quarkus/issues/20139.

---
+++ integration-tests/mongodb-client/src/test/java/io/quarkus/it/mongodb/NativePojoResourceIT.java
@@ -0,0 +1,8 @@
+package io.quarkus.it.mongodb;
+
+import io.quarkus.it.mongodb.pojo.PojoResource;
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativePojoResourceIT extends PojoResource {
+}

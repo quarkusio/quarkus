d2b98bdc138264d6bc5b7552d921b001d816358f
Yelzhas Suleimenov <elzhas.suleimenov@gmail.com>
2021-09-17 21:40:01 +0600

    make @Header applicable to annotation only

--- extensions/resteasy-reactive/quarkus-resteasy-reactive/common-runtime/src/main/java/io/quarkus/resteasy/reactive/server/common/Header.java
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive/common-runtime/src/main/java/io/quarkus/resteasy/reactive/server/common/Header.java
@@ -1,5 +1,9 @@
 package io.quarkus.resteasy.reactive.server.common;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.ANNOTATION_TYPE)
 public @interface Header {
     public String name();
 

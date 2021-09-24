3724f871cf612cc5ef316957e24b8b01d8dd0733
Georgios Andrianakis <geoand@gmail.com>
2021-09-24 09:08:51 +0300

    Permit the use of suspend on a method returning Flow
    Fixes: #20345
    (cherry picked from commit 1486dffe92264ad40796bbfdf9511132db335ddf)

--- integration-tests/resteasy-reactive-kotlin/standard/src/test/kotlin/io/quarkus/it/resteasy/reactive/kotlin/FlowResourceTest.kt
+++ integration-tests/resteasy-reactive-kotlin/standard/src/test/kotlin/io/quarkus/it/resteasy/reactive/kotlin/FlowResourceTest.kt
@@ -18,8 +18,15 @@ class FlowResourceTest {
     var flowPath: String? = null
 
     @Test
-    fun testSeeStrings() {
+    fun testSseStrings() {
         testSse("str", 5) {
+            assertThat(it).containsExactly("HELLO", "FROM", "KOTLIN", "FLOW")
+        }
+    }
+
+    @Test
+    fun testSuspendSseStrings() {
+        testSse("suspendStr", 5) {
             assertThat(it).containsExactly("HELLO", "FROM", "KOTLIN", "FLOW")
         }
     }

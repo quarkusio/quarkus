9d7c1a867cbe45d7bba3408873fa6b3e1759b1b1
Yoann Rodière <yoann@hibernate.org>
2021-09-24 09:22:53 +0200

    Disabled test for nested container types in Hibernate Validator
    See https://github.com/quarkusio/quarkus/issues/20377
    
    (cherry picked from commit 80f7b0e48586c960d3aa488e556b16abfe527bc4)

--- extensions/hibernate-validator/deployment/src/test/java/io/quarkus/hibernate/validator/test/valueextractor/Container.java
+++ extensions/hibernate-validator/deployment/src/test/java/io/quarkus/hibernate/validator/test/valueextractor/Container.java
@@ -2,6 +2,7 @@ package io.quarkus.hibernate.validator.test.valueextractor;
 
 // TODO for some reason, this must be a top-level type, not a nested/inner type,
 //  in order for annotations on type parameters to be detected.
+//  See NestedContainerTypeCustomValueExtractorTest
 public class Container<T> {
 
     public T value;

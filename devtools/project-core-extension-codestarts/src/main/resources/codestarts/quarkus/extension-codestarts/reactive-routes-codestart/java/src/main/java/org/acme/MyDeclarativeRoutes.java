f877098627a392e22993be9a126702d06645846d
lducamp <laurent.ducamp@zenika.com>
2021-10-20 19:23:32 +0200

    issue #20871 : Reactive Routes Codestart (in java and kotlin)
    https://github.com/quarkusio/quarkus/issues/20871

---
+++ devtools/project-core-extension-codestarts/src/main/resources/codestarts/quarkus/extension-codestarts/reactive-routes-codestart/java/src/main/java/org/acme/MyDeclarativeRoutes.java
@@ -0,0 +1,16 @@
+package org.acme;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import io.quarkus.vertx.web.Route;
+import io.quarkus.vertx.web.RoutingExchange;
+import io.vertx.ext.web.RoutingContext;
+
+@ApplicationScoped 
+public class MyDeclarativeRoutes {
+    // neither path nor regex is set - match a path derived from the method name (ie helloRoute => /hello-route )
+    @Route(methods = Route.HttpMethod.GET)
+    void helloRoute(RoutingExchange ex) { 
+        ex.ok("Hello " + ex.getParam("name").orElse("Reactive Route") +" !!");
+    }
+}

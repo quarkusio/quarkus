6eed09a0d22699acf5f24eb286927f1a7d4378f3
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-10-26 10:02:13 +1100

    Allow List<PathSegment> injection
    Fixes #17034
    
    (cherry picked from commit d83e522f55548a09b91fef8b8e5eff0ec77ecf98)

--- independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/startup/RuntimeResourceDeployment.java
+++ independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/startup/RuntimeResourceDeployment.java
@@ -490,7 +490,7 @@ public class RuntimeResourceDeployment {
                         extractor = new NullParamExtractor();
                     }
                 } else {
-                    extractor = new PathParamExtractor(index, encoded);
+                    extractor = new PathParamExtractor(index, encoded, single);
                 }
                 return extractor;
             case CONTEXT:

2e38c967158010e0f00ecd00afb5d12187c92bd4
Roberto Cortez <radcortez@yahoo.com>
2021-10-28 13:44:14 +0100

    Do not use ConfigConfig directly (it is only required for documentation)

--- core/deployment/src/main/java/io/quarkus/deployment/steps/ProfileBuildStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/steps/ProfileBuildStep.java
@@ -1,15 +1,19 @@
 package io.quarkus.deployment.steps;
 
+import static io.smallrye.config.SmallRyeConfig.SMALLRYE_CONFIG_LOCATIONS;
+
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
 
 import org.apache.commons.io.FilenameUtils;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.HotDeploymentWatchedFileBuildItem;
 import io.quarkus.deployment.builditem.LaunchModeBuildItem;
 import io.quarkus.deployment.builditem.RunTimeConfigurationDefaultBuildItem;
-import io.quarkus.runtime.ConfigConfig;
 import io.quarkus.runtime.LaunchMode;
 
 public class ProfileBuildStep {
@@ -22,7 +26,6 @@ public class ProfileBuildStep {
 
     @BuildStep
     void watchProfileSpecificFile(LaunchModeBuildItem launchModeBuildItem,
-            ConfigConfig config,
             BuildProducer<HotDeploymentWatchedFileBuildItem> items) {
         String activeProfile = getProfileValue(launchModeBuildItem.getLaunchMode());
         String suffixedApplicationProperties = String.format("application-%s.properties", activeProfile);
@@ -31,8 +34,10 @@ public class ProfileBuildStep {
                 Paths.get(String.format(".env-%s", activeProfile)).toAbsolutePath().toString()));
         items.produce(new HotDeploymentWatchedFileBuildItem(Paths
                 .get(System.getProperty("user.dir"), "config", suffixedApplicationProperties).toAbsolutePath().toString()));
-        if (config.locations.isPresent()) {
-            for (String location : config.locations.get()) {
+        Optional<List<String>> locations = ConfigProvider.getConfig().getOptionalValues(SMALLRYE_CONFIG_LOCATIONS,
+                String.class);
+        if (locations.isPresent()) {
+            for (String location : locations.get()) {
                 items.produce(new HotDeploymentWatchedFileBuildItem(appendProfileToFilename(location, activeProfile)));
             }
         }

08129011353d04d0ada0a7b6b811dddbc344cc63
Stuart Douglas <stuart.w.douglas@gmail.com>
2021-10-28 15:06:34 +1100

    Experimental test bandaid

--- integration-tests/reactive-messaging-rabbitmq/src/main/java/io/quarkus/it/rabbitmq/PeopleProducer.java
+++ integration-tests/reactive-messaging-rabbitmq/src/main/java/io/quarkus/it/rabbitmq/PeopleProducer.java
@@ -1,21 +1,37 @@
 package io.quarkus.it.rabbitmq;
 
+import java.util.function.Consumer;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.subscription.MultiEmitter;
 
 @ApplicationScoped
 public class PeopleProducer {
     @Outgoing("people-out")
     public Multi<Person> generatePeople() {
-        return Multi.createFrom().items(
-                new Person("bob"),
-                new Person("alice"),
-                new Person("tom"),
-                new Person("jerry"),
-                new Person("anna"),
-                new Person("ken"));
+        System.out.println("---- Calling generatePeople");
+        return Multi.createFrom().emitter(new Consumer<MultiEmitter<? super Person>>() {
+            @Override
+            public void accept(MultiEmitter<? super Person> multiEmitter) {
+                System.out.println("---- Calling accept called, multi has been subscribed");
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                    }
+                    multiEmitter.emit(new Person("bob"));
+                    multiEmitter.emit(new Person("alice"));
+                    multiEmitter.emit(new Person("tom"));
+                    multiEmitter.emit(new Person("jerry"));
+                    multiEmitter.emit(new Person("anna"));
+                    multiEmitter.emit(new Person("ken"));
+
+                }).start();
+            }
+        });
     }
 }

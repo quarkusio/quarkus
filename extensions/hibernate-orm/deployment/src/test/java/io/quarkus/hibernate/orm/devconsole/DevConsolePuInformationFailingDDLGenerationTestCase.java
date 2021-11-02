aa62a23fd1bb5836239da04fc44a34786f560da3
Yoann Rodière <yoann@hibernate.org>
2021-11-02 15:00:39 +0100

    Test Hibernate ORM Dev UI's persistence info gathering
    (cherry picked from commit fecfa60bc6465f27dd65e758cce156bb4ae449bb)

---
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/devconsole/DevConsolePuInformationFailingDDLGenerationTestCase.java
@@ -0,0 +1,32 @@
+package io.quarkus.hibernate.orm.devconsole;
+
+import static org.hamcrest.Matchers.is;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusDevModeTest;
+import io.restassured.RestAssured;
+
+public class DevConsolePuInformationFailingDDLGenerationTestCase {
+    @RegisterExtension
+    final static QuarkusDevModeTest TEST = new QuarkusDevModeTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClasses(MyEntityWithFailingDDLGeneration.class,
+                            TypeWithUnsupportedSqlCode.class,
+                            DevConsoleInfoSupplierTestResource.class)
+                    .addAsResource("application-generation-none.properties", "application.properties")
+                    .addAsResource("import-custom-table-name.sql", "import.sql"));
+
+    @Test
+    public void infoAvailableButWithException() {
+        RestAssured.given()
+                .param("expectedCreateDDLContent", "EXCEPTION")
+                .param("expectedDropDDLContent", "EXCEPTION")
+                .when().get("/dev-console-info-supplier/check-pu-info-with-failing-ddl-generation")
+                .then().body(is("OK"));
+    }
+
+}

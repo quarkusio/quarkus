d9026fcb0fdecbbcf5bc98ec06ba395b8ab506b8
Martin Kouba <mkouba@redhat.com>
2021-11-02 11:09:09 +0100

    Vert.x extension - remove quarkus-jackson, add optional jackson-databind
    - resolves #21139

--- extensions/vertx/runtime/src/main/java/io/quarkus/vertx/runtime/jackson/QuarkusJacksonFactory.java
+++ extensions/vertx/runtime/src/main/java/io/quarkus/vertx/runtime/jackson/QuarkusJacksonFactory.java
@@ -1,5 +1,7 @@
 package io.quarkus.vertx.runtime.jackson;
 
+import io.vertx.core.json.jackson.DatabindCodec;
+import io.vertx.core.json.jackson.JacksonCodec;
 import io.vertx.core.spi.JsonFactory;
 import io.vertx.core.spi.json.JsonCodec;
 
@@ -10,7 +12,20 @@ public class QuarkusJacksonFactory implements JsonFactory {
 
     @Override
     public JsonCodec codec() {
-        return new QuarkusJacksonJsonCodec();
+        JsonCodec codec;
+        try {
+            // First try the Quarkus databind codec
+            codec = new QuarkusJacksonJsonCodec();
+        } catch (Throwable t1) {
+            // Then try the Vert.x databind codec
+            try {
+                codec = new DatabindCodec();
+            } catch (Throwable t2) {
+                // Finally, use the jackson.core codec
+                codec = new JacksonCodec();
+            }
+        }
+        return codec;
     }
 
 }

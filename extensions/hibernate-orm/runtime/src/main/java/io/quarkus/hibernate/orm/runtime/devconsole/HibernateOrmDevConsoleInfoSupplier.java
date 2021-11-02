5d5a6b00880dd314c51aee0b1db8a4a0c454280a
Yoann Rodière <yoann@hibernate.org>
2021-11-02 15:14:01 +0100

    Allow the application to start in dev mode even if we can't generate DDL scripts for the Dev UI
    (cherry picked from commit d07ecb23bfba38ee48868635e155c4b513ce6af9)

--- extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/devconsole/HibernateOrmDevConsoleInfoSupplier.java
+++ extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/devconsole/HibernateOrmDevConsoleInfoSupplier.java
@@ -1,5 +1,6 @@
 package io.quarkus.hibernate.orm.runtime.devconsole;
 
+import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -64,23 +65,29 @@ public class HibernateOrmDevConsoleInfoSupplier implements Supplier<HibernateOrm
         schemaExport.setDelimiter(";");
         schemaExport.setImportFiles(importFiles);
         StringWriter writer = new StringWriter();
-        schemaExport.doExecution(action, false, metadata, serviceRegistry,
-                new TargetDescriptor() {
-                    @Override
-                    public EnumSet<TargetType> getTargetTypes() {
-                        return EnumSet.of(TargetType.SCRIPT);
-                    }
+        try {
+            schemaExport.doExecution(action, false, metadata, serviceRegistry,
+                    new TargetDescriptor() {
+                        @Override
+                        public EnumSet<TargetType> getTargetTypes() {
+                            return EnumSet.of(TargetType.SCRIPT);
+                        }
 
-                    @Override
-                    public ScriptTargetOutput getScriptTargetOutput() {
-                        return new ScriptTargetOutputToWriter(writer) {
-                            @Override
-                            public void accept(String command) {
-                                super.accept(command);
-                            }
-                        };
-                    }
-                });
+                        @Override
+                        public ScriptTargetOutput getScriptTargetOutput() {
+                            return new ScriptTargetOutputToWriter(writer) {
+                                @Override
+                                public void accept(String command) {
+                                    super.accept(command);
+                                }
+                            };
+                        }
+                    });
+        } catch (RuntimeException e) {
+            StringWriter stackTraceWriter = new StringWriter();
+            e.printStackTrace(new PrintWriter(stackTraceWriter));
+            return "Could not generate DDL: \n" + stackTraceWriter.toString();
+        }
         return writer.toString();
     }
 

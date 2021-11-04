b9b37c3c84e6d7760c04d9e702f6a3eaa03cfbf3
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2021-11-04 15:13:12 +0100

    Fix CORS Filter to allow non-CORS OPTIONS requests
    The previous implementation assumed that all OPTIONS requests were cors requests, and terminated them early, this fixes that, by ensuring that we're actually receiving a CORS request (presence of Origin and Access-Control-Request-* headers).
    
    Included is a full set of test cases for the existing and changed behaviour. If you revert the change to CORSFilter.java, then you can see the bug.
    
    (cherry picked from commit 07d90a72b422780aa902a6b7efc1567bf8f73e57)

--- integration-tests/vertx-http/src/main/java/io/quarkus/it/vertx/SimpleResource.java
+++ integration-tests/vertx-http/src/main/java/io/quarkus/it/vertx/SimpleResource.java
@@ -1,7 +1,9 @@
 package io.quarkus.it.vertx;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -12,5 +14,11 @@ public class SimpleResource {
     @Path("/access-log-test-endpoint")
     public String accessLogTest() {
         return "passed";
+    }
+
+    @OPTIONS
+    @Path("/options")
+    public Response optionsHandler() {
+        return Response.ok("options").header("X-Custom-Header", "abc").build();
     }
 }

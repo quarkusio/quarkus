bdad03452e30fd60e7c9782e7fb1d126a77ae009
Clement Escoffier <clement@apache.org>
2021-11-15 19:19:26 +0100

    Fix OpenTelemetry native compilation issue
    We need to use the TlsUtils class to get the TrustManager.
    This fixes https://github.com/quarkusio/quarkus-quickstarts/issues/992
    
    (cherry picked from commit e1c76811b9620d00296984aa3d00cfbbc670c8ff)

--- extensions/opentelemetry/opentelemetry-exporter-otlp/runtime/src/main/java/io/quarkus/opentelemetry/exporter/otlp/runtime/graal/OtlpExporterSubstitutions.java
+++ extensions/opentelemetry/opentelemetry-exporter-otlp/runtime/src/main/java/io/quarkus/opentelemetry/exporter/otlp/runtime/graal/OtlpExporterSubstitutions.java
@@ -3,9 +3,8 @@ package io.quarkus.opentelemetry.exporter.otlp.runtime.graal;
 import static java.util.Objects.requireNonNull;
 
 import javax.net.ssl.SSLException;
-import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
-import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 
@@ -27,25 +26,18 @@ final class Target_io_opentelemetry_exporter_otlp_internal_grpc_ManagedChannelUt
         requireNonNull(managedChannelBuilder, "managedChannelBuilder");
         requireNonNull(trustedCertificatesPem, "trustedCertificatesPem");
 
-        TrustManagerFactory tmf = trustManagerFactory(trustedCertificatesPem);
+        X509TrustManager tm = io.opentelemetry.exporter.otlp.internal.TlsUtil.trustManager(trustedCertificatesPem);
 
         // gRPC does not abstract TLS configuration so we need to check the implementation and act
         // accordingly.
         if (managedChannelBuilder.getClass().getName().equals("io.grpc.netty.NettyChannelBuilder")) {
             NettyChannelBuilder nettyBuilder = (NettyChannelBuilder) managedChannelBuilder;
-            nettyBuilder.sslContext(GrpcSslContexts.forClient().trustManager(tmf).build());
+            nettyBuilder.sslContext(GrpcSslContexts.forClient().trustManager(tm).build());
         } else {
             throw new SSLException(
                     "TLS certificate configuration not supported for unrecognized ManagedChannelBuilder "
                             + managedChannelBuilder.getClass().getName());
         }
-    }
-
-    // Just provide access to "trustManagerFactory"
-    @Alias
-    private static TrustManagerFactory trustManagerFactory(byte[] trustedCertificatesPem)
-            throws SSLException {
-        return null;
     }
 }
 

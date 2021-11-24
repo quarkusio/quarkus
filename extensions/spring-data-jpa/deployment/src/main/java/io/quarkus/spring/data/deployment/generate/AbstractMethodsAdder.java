2a18362b6722dbff5a6a8e4821f2792585c50d22
Jose <josecarvajalhilario@gmail.com>
2021-11-24 13:56:06 +0100

    Spring Data: Fix interface projection using Page and Slice
    The interface project worked fine using Lists, but not using Page or Slice. Example:
    
    ```java
    @Repository
    public interface BasicTypeDataRepository extends CrudRepository<BasicTypeData, Integer> {
    
        // works
        @Query(value = "select id, doubleValue from BasicTypeData")
        List<WithDoubleValue> findAllDoubleValuesToList();
    
       // it does not work (1)
        @Query(value = "select id, doubleValue from BasicTypeData")
        Page<WithDoubleValue> findAllDoubleValuesToPage();
    }
    ```
    
    This PR fixes the interface projection when returning a Page or a Slice object (1).
    
    Fixes https://github.com/quarkusio/quarkus/issues/21616
    
    (cherry picked from commit a04b79793fb10da6b28b9126dd201078c70bb8b6)

--- extensions/spring-data-jpa/deployment/src/main/java/io/quarkus/spring/data/deployment/generate/AbstractMethodsAdder.java
+++ extensions/spring-data-jpa/deployment/src/main/java/io/quarkus/spring/data/deployment/generate/AbstractMethodsAdder.java
@@ -143,7 +143,8 @@ public abstract class AbstractMethodsAdder {
                     MethodDescriptor.ofMethod(Optional.class, "empty", Optional.class));
             catchBlock.returnValue(emptyOptional);
         } else if (DotNames.LIST.equals(returnType) || DotNames.COLLECTION.equals(returnType)
-                || DotNames.SET.equals(returnType) || DotNames.ITERATOR.equals(returnType)) {
+                || DotNames.SET.equals(returnType) || DotNames.ITERATOR.equals(returnType)
+                || DotNames.SPRING_DATA_PAGE.equals(returnType) || DotNames.SPRING_DATA_SLICE.equals(returnType)) {
             ResultHandle list;
 
             if (customResultType == null) {
@@ -188,6 +189,34 @@ public abstract class AbstractMethodsAdder {
                 ResultHandle set = methodCreator.newInstance(
                         MethodDescriptor.ofConstructor(LinkedHashSet.class, Collection.class), list);
                 methodCreator.returnValue(set);
+            } else if (DotNames.SPRING_DATA_PAGE.equals(returnType)) {
+                ResultHandle pageResult;
+                if (pageableParameterIndex != null) {
+                    ResultHandle count = methodCreator.invokeInterfaceMethod(
+                            MethodDescriptor.ofMethod(PanacheQuery.class, "count", long.class),
+                            panacheQuery);
+                    pageResult = methodCreator.newInstance(
+                            MethodDescriptor.ofConstructor(PageImpl.class, List.class, Pageable.class, long.class),
+                            list, methodCreator.getMethodParam(pageableParameterIndex), count);
+                } else {
+                    pageResult = methodCreator.newInstance(MethodDescriptor.ofConstructor(PageImpl.class, List.class), list);
+                }
+
+                methodCreator.returnValue(pageResult);
+            } else if (DotNames.SPRING_DATA_SLICE.equals(returnType)) {
+                ResultHandle sliceResult;
+                if (pageableParameterIndex != null) {
+                    ResultHandle hasNextPage = methodCreator.invokeInterfaceMethod(
+                            MethodDescriptor.ofMethod(PanacheQuery.class, "hasNextPage", boolean.class),
+                            panacheQuery);
+                    sliceResult = methodCreator.newInstance(
+                            MethodDescriptor.ofConstructor(SliceImpl.class, List.class, Pageable.class, boolean.class),
+                            list, methodCreator.getMethodParam(pageableParameterIndex), hasNextPage);
+                } else {
+                    sliceResult = methodCreator.newInstance(MethodDescriptor.ofConstructor(SliceImpl.class, List.class), list);
+                }
+
+                methodCreator.returnValue(sliceResult);
             }
             methodCreator.returnValue(list);
 
@@ -197,41 +226,6 @@ public abstract class AbstractMethodsAdder {
                     panacheQuery);
             methodCreator.returnValue(stream);
 
-        } else if (DotNames.SPRING_DATA_PAGE.equals(returnType)) {
-            ResultHandle list = methodCreator.invokeInterfaceMethod(
-                    MethodDescriptor.ofMethod(PanacheQuery.class, "list", List.class),
-                    panacheQuery);
-            ResultHandle pageResult;
-            if (pageableParameterIndex != null) {
-                ResultHandle count = methodCreator.invokeInterfaceMethod(
-                        MethodDescriptor.ofMethod(PanacheQuery.class, "count", long.class),
-                        panacheQuery);
-                pageResult = methodCreator.newInstance(
-                        MethodDescriptor.ofConstructor(PageImpl.class, List.class, Pageable.class, long.class),
-                        list, methodCreator.getMethodParam(pageableParameterIndex), count);
-            } else {
-                pageResult = methodCreator.newInstance(MethodDescriptor.ofConstructor(PageImpl.class, List.class), list);
-            }
-
-            methodCreator.returnValue(pageResult);
-
-        } else if (DotNames.SPRING_DATA_SLICE.equals(returnType)) {
-            ResultHandle list = methodCreator.invokeInterfaceMethod(
-                    MethodDescriptor.ofMethod(PanacheQuery.class, "list", List.class),
-                    panacheQuery);
-            ResultHandle sliceResult;
-            if (pageableParameterIndex != null) {
-                ResultHandle hasNextPage = methodCreator.invokeInterfaceMethod(
-                        MethodDescriptor.ofMethod(PanacheQuery.class, "hasNextPage", boolean.class),
-                        panacheQuery);
-                sliceResult = methodCreator.newInstance(
-                        MethodDescriptor.ofConstructor(SliceImpl.class, List.class, Pageable.class, boolean.class),
-                        list, methodCreator.getMethodParam(pageableParameterIndex), hasNextPage);
-            } else {
-                sliceResult = methodCreator.newInstance(MethodDescriptor.ofConstructor(SliceImpl.class, List.class), list);
-            }
-
-            methodCreator.returnValue(sliceResult);
         } else if (isHibernateSupportedReturnType(returnType)) {
             ResultHandle singleResult = methodCreator.invokeInterfaceMethod(
                     MethodDescriptor.ofMethod(PanacheQuery.class, "singleResult", Object.class),

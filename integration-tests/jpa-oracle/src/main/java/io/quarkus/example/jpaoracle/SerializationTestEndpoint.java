cd6688e66e7838eb33eed832dfdb0fa98c06fb06
Sanne Grinovero <sanne@hibernate.org>
2021-12-03 23:44:34 +0000

    Test serialization functionality in native-images with Oracle JDBC

---
+++ integration-tests/jpa-oracle/src/main/java/io/quarkus/example/jpaoracle/SerializationTestEndpoint.java
@@ -0,0 +1,41 @@
+package io.quarkus.example.jpaoracle;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "JPATestOracleSerialization", urlPatterns = "/jpa-oracle/testserialization")
+public class SerializationTestEndpoint extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            final String output = serializedstring();
+            resp.getWriter().write(output);
+
+        } catch (Exception e) {
+            resp.getWriter().write("An error occurred while attempting serialization operations");
+        }
+    }
+
+    private String serializedstring() throws IOException, ClassNotFoundException {
+        byte[] bytes = null;
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+            oos.writeObject("Hello from Serialization Test");
+            oos.flush();
+            bytes = baos.toByteArray();
+        }
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+                ObjectInputStream ois = new ObjectInputStream(bais)) {
+            return (String) ois.readObject();
+        }
+    }
+}

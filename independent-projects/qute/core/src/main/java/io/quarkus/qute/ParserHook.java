2146757dd1434ce23dfa2f39850037359493f748
Martin Kouba <mkouba@redhat.com>
2021-12-06 17:37:30 +0100

    Qute - minor doc updates
    - "with" section should not be used in type-safe templates
    - add some more javadoc

--- independent-projects/qute/core/src/main/java/io/quarkus/qute/ParserHook.java
+++ independent-projects/qute/core/src/main/java/io/quarkus/qute/ParserHook.java
@@ -1,8 +1,12 @@
 package io.quarkus.qute;
 
+/**
+ * This component can be used to hook into the parser logic.
+ */
 public interface ParserHook {
 
     /**
+     * This method is invoked before a template contents is parsed
      * 
      * @param parserHelper
      */

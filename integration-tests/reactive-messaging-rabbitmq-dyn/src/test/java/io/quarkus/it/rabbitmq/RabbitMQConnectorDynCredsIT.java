5fd82bdf1290c29e30b16d7961af368c5f738952
Kevin Wooten <kevin@wooten.com>
2021-12-08 12:08:19 -0700

    Adds support for RabbitMQ dynamic credentials from Vault

---
+++ integration-tests/reactive-messaging-rabbitmq-dyn/src/test/java/io/quarkus/it/rabbitmq/RabbitMQConnectorDynCredsIT.java
@@ -0,0 +1,8 @@
+package io.quarkus.it.rabbitmq;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class RabbitMQConnectorDynCredsIT extends RabbitMQConnectorDynCredsTest {
+
+}

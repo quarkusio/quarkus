24609e2685d2715115ac83ef76ffb836d95d5172
Kevin Wooten <kevin@wooten.com>
2021-12-08 12:08:19 -0700

    Adds support for RabbitMQ dynamic credentials from Vault
    (cherry picked from commit 5fd82bdf1290c29e30b16d7961af368c5f738952)

--- extensions/smallrye-reactive-messaging-rabbitmq/deployment/src/main/java/io/quarkus/smallrye/reactivemessaging/rabbitmq/deployment/RabbitMQBuildTimeConfig.java
+++ extensions/smallrye-reactive-messaging-rabbitmq/deployment/src/main/java/io/quarkus/smallrye/reactivemessaging/rabbitmq/deployment/RabbitMQBuildTimeConfig.java
@@ -1,5 +1,7 @@
 package io.quarkus.smallrye.reactivemessaging.rabbitmq.deployment;
 
+import java.util.Optional;
+
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
@@ -12,4 +14,21 @@ public class RabbitMQBuildTimeConfig {
      */
     @ConfigItem
     public RabbitMQDevServicesBuildTimeConfig devservices;
+
+    /**
+     * The credentials provider name.
+     */
+    @ConfigItem
+    public Optional<String> credentialsProvider = Optional.empty();
+
+    /**
+     * The credentials provider bean name.
+     * <p>
+     * It is the {@code &#64;Named} value of the credentials provider bean. It is used to discriminate if multiple
+     * CredentialsProvider beans are available.
+     * <p>
+     * For Vault it is: vault-credentials-provider. Not necessary if there is only one credentials provider available.
+     */
+    @ConfigItem
+    public Optional<String> credentialsProviderName = Optional.empty();
 }

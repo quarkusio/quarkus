27dde975f6e669f416eb6c08410f075202092d95
Michael Simons <michael@simons.ac>
2021-12-13 22:35:59 +0100

    Don't require Docker when devservices are locally or globally disable via the Neo4j extension
    Move `isDockerWorking` check into start method: Having it in the `@BuildStep` annotation causes unnecessary initialization of the Testcontainers environment (Ryuk and other). Fixes #22167.

--- extensions/neo4j/deployment/src/main/java/io/quarkus/neo4j/deployment/Neo4jDevServicesProcessor.java
+++ extensions/neo4j/deployment/src/main/java/io/quarkus/neo4j/deployment/Neo4jDevServicesProcessor.java
@@ -6,12 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.BooleanSupplier;
 
 import org.jboss.logging.Logger;
 import org.testcontainers.containers.Neo4jContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import io.quarkus.deployment.IsDockerWorking;
 import io.quarkus.deployment.IsNormal;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
@@ -36,18 +36,9 @@ class Neo4jDevServicesProcessor {
     static volatile Neo4jDevServiceConfig runningConfiguration;
     static volatile boolean first = true;
 
-    static final class IsDockerWorking implements BooleanSupplier {
+    private final IsDockerWorking isDockerWorking = new IsDockerWorking(true);
 
-        private final io.quarkus.deployment.IsDockerWorking delegate = new io.quarkus.deployment.IsDockerWorking(true);
-
-        @Override
-        public boolean getAsBoolean() {
-
-            return delegate.getAsBoolean();
-        }
-    }
-
-    @BuildStep(onlyIfNot = IsNormal.class, onlyIf = { IsDockerWorking.class, GlobalDevServicesConfig.Enabled.class })
+    @BuildStep(onlyIfNot = IsNormal.class, onlyIf = GlobalDevServicesConfig.Enabled.class)
     public Neo4jDevServiceBuildItem startNeo4jDevService(
             LaunchModeBuildItem launchMode,
             Neo4jBuildTimeConfig neo4jBuildTimeConfig,
@@ -108,6 +99,11 @@ class Neo4jDevServicesProcessor {
     }
 
     private Neo4jContainer<?> startNeo4j(Neo4jDevServiceConfig configuration, Optional<Duration> timeout) {
+
+        if (!isDockerWorking.getAsBoolean()) {
+            log.debug("Not starting Dev Services for Neo4j, as Docker is not working.");
+            return null;
+        }
 
         if (!configuration.devServicesEnabled) {
             log.debug("Not starting Dev Services for Neo4j, as it has been disabled in the config.");

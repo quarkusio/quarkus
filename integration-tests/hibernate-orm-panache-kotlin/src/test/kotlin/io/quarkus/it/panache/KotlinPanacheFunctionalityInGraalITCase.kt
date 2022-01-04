cb319dd2c97457a7724421cd5779dadaf4dc6fa5
Rostislav Svoboda <rsvoboda@redhat.com>
2022-01-04 19:52:36 +0100

    Enable native tests for hibernate-orm-panache-kotlin

--- integration-tests/hibernate-orm-panache-kotlin/src/test/kotlin/io/quarkus/it/panache/KotlinPanacheFunctionalityInGraalITCase.kt
+++ integration-tests/hibernate-orm-panache-kotlin/src/test/kotlin/io/quarkus/it/panache/KotlinPanacheFunctionalityInGraalITCase.kt
@@ -7,5 +7,4 @@ import org.junit.jupiter.api.Disabled
  * Test various Panache operations running in native mode
  */
 @QuarkusIntegrationTest
-@Disabled("Fails on GraalVM 20.1")
 class KotlinPanacheFunctionalityInGraalITCase : KotlinPanacheFunctionalityTest()

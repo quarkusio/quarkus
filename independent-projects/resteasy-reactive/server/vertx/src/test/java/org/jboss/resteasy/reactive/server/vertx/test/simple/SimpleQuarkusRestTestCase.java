91ead07ae3810b1dde7d8f29dd0f32eebd933abc
Georgios Andrianakis <geoand@gmail.com>
2022-01-07 12:24:17 +0200

    Enable returning a Mutiny Buffer from RESTEasy Reactive Resource Method
    Resolves: #22694

--- independent-projects/resteasy-reactive/server/vertx/src/test/java/org/jboss/resteasy/reactive/server/vertx/test/simple/SimpleQuarkusRestTestCase.java
+++ independent-projects/resteasy-reactive/server/vertx/src/test/java/org/jboss/resteasy/reactive/server/vertx/test/simple/SimpleQuarkusRestTestCase.java
@@ -182,6 +182,9 @@ public class SimpleQuarkusRestTestCase {
 
         RestAssured.get("/simple/writer/vertx-buffer")
                 .then().body(Matchers.equalTo("VERTX-BUFFER"));
+
+        RestAssured.get("/simple/writer/mutiny-buffer")
+                .then().body(Matchers.equalTo("MUTINY-BUFFER"));
     }
 
     @DisabledOnOs(OS.WINDOWS)

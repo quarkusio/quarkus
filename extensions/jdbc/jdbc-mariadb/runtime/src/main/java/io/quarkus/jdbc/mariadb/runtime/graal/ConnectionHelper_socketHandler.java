8a174ef46220bdcf9bfe026b52b3af20eb122fd7
Falko Modler <famod@users.noreply.github.com>
2022-01-27 23:28:30 +0100

    Upgrade mariadb-java-client from 2.7.5 to 3.0.3

--- extensions/jdbc/jdbc-mariadb/runtime/src/main/java/io/quarkus/jdbc/mariadb/runtime/graal/ConnectionHelper_socketHandler.java
+++ extensions/jdbc/jdbc-mariadb/runtime/src/main/java/io/quarkus/jdbc/mariadb/runtime/graal/ConnectionHelper_socketHandler.java
@@ -1,13 +1,13 @@
 package io.quarkus.jdbc.mariadb.runtime.graal;
 
-import org.mariadb.jdbc.internal.io.socket.SocketHandlerFunction;
+import org.mariadb.jdbc.client.socket.impl.SocketHandlerFunction;
 
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.TargetClass;
 
-@TargetClass(org.mariadb.jdbc.internal.util.Utils.class)
-public final class Utils_socketHandler {
+@TargetClass(className = "org.mariadb.jdbc.client.impl.ConnectionHelper")
+public final class ConnectionHelper_socketHandler {
 
     @Alias
     @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.FromAlias)

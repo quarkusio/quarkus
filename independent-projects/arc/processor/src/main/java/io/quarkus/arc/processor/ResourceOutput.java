5db6271bd81e48d4f2c36b4c09e29ad09369f2bf
Martin Kouba <mkouba@redhat.com>
2022-02-04 15:11:26 +0100

    ArC - register client proxies and subclasses for reflection if needed
    - introduce ReflectiveBeanClassBuildItem
    - produce ReflectiveBeanClassBuildItem automatically for bean class
    annotated with RegisterForReflection
    - resolves #7507

--- independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/ResourceOutput.java
+++ independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/ResourceOutput.java
@@ -68,14 +68,16 @@ public interface ResourceOutput {
         enum Type {
             JAVA_CLASS,
             JAVA_SOURCE,
-            SERVICE_PROVIDER,
+            SERVICE_PROVIDER
         }
 
         enum SpecialType {
             BEAN,
             INTERCEPTOR_BEAN,
             DECORATOR_BEAN,
-            OBSERVER;
+            OBSERVER,
+            CLIENT_PROXY,
+            SUBCLASS
         }
 
     }

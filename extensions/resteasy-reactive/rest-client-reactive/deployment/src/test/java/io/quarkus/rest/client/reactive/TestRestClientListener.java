15458945486d6439dee08cffc602fd49fdbe2c42
pravussum <pravussum@users.noreply.github.com>
2022-02-15 13:29:44 +0100

    [rest-client-reactive] Fix RestClientListener discovery

---
+++ extensions/resteasy-reactive/rest-client-reactive/deployment/src/test/java/io/quarkus/rest/client/reactive/TestRestClientListener.java
@@ -0,0 +1,26 @@
+package io.quarkus.rest.client.reactive;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.eclipse.microprofile.rest.client.spi.RestClientListener;
+
+public class TestRestClientListener implements RestClientListener {
+
+    public static final String HEADER_PARAM_NAME = "filterheader";
+    public static final String HEADER_PARAM_VALUE = "present";
+
+    @Override
+    public void onNewClient(Class<?> aClass, RestClientBuilder restClientBuilder) {
+        restClientBuilder.register(new TestRestClientFilter());
+    }
+
+    static class TestRestClientFilter implements ClientRequestFilter {
+
+        @Override
+        public void filter(ClientRequestContext clientRequestContext) {
+            clientRequestContext.getHeaders().putSingle(HEADER_PARAM_NAME, HEADER_PARAM_VALUE);
+        }
+    }
+}

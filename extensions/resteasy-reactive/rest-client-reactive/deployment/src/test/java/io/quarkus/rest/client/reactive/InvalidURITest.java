195e7f081e4980c2b52364f3031f194d950b6db2
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2022-03-02 14:48:57 +0100

    REST Client Reactive uri validation test + Stork URI validation
    (cherry picked from commit 268b4dee2cfaf7af11e005bc8bcd41653bfef6e4)

---
+++ extensions/resteasy-reactive/rest-client-reactive/deployment/src/test/java/io/quarkus/rest/client/reactive/InvalidURITest.java
@@ -0,0 +1,77 @@
+package io.quarkus.rest.client.reactive;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.assertj.core.api.Assertions;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class InvalidURITest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest TEST = new QuarkusUnitTest();
+
+    @Test
+    void shouldFailOnMissingSchema() {
+        Client client = clientWithUri("localhost:8080");
+
+        Assertions.assertThatThrownBy(client::get).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldFailOnMissingColon() {
+        Client client = clientWithUri("http//localhost:8080");
+
+        Assertions.assertThatThrownBy(client::get).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldFailOnStorkUrlWithoutColonAfterScheme() {
+        Client client = clientWithUri("stork//localhost:8080");
+
+        Assertions.assertThatThrownBy(client::get).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldFailOnStorkUrlWithoutSlashes() {
+        Client client = clientWithUri("stork:somethingwrong");
+
+        Assertions.assertThatThrownBy(client::get).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldWork() {
+        Client client = clientWithUri(
+                "http://localhost:" + ConfigProvider.getConfig().getValue("quarkus.http.test-port", Integer.class));
+
+        assertThat(client.get()).isEqualTo("bar-of-chocolate");
+    }
+
+    private Client clientWithUri(String uri) {
+        return RestClientBuilder.newBuilder().baseUri(URI.create(uri)).build(Client.class);
+    }
+
+    @Path("/foo")
+    public interface Client {
+        @GET
+        String get();
+    }
+
+    @Path("/foo")
+    public static class Resource {
+        @GET
+        public String get() {
+            return "bar-of-chocolate";
+        }
+    }
+
+}
--- independent-projects/resteasy-reactive/client/runtime/src/main/java/org/jboss/resteasy/reactive/client/handlers/ClientSendRequestHandler.java
+++ independent-projects/resteasy-reactive/client/runtime/src/main/java/org/jboss/resteasy/reactive/client/handlers/ClientSendRequestHandler.java
@@ -268,11 +268,15 @@ public class ClientSendRequestHandler implements ClientRestHandler {
         state.setMultipartResponsesData(multipartResponseDataMap);
         if (uri.getScheme() == null) { // invalid URI
             return Uni.createFrom()
-                    .failure(new IllegalArgumentException("Invalid REST Client URL used: '" + uri.toString() + "'"));
+                    .failure(new IllegalArgumentException("Invalid REST Client URL used: '" + uri + "'"));
         }
         if (uri.getScheme().startsWith(Stork.STORK)) {
             boolean isHttps = "storks".equals(uri.getScheme());
             String serviceName = uri.getHost();
+            if (serviceName == null) { // invalid URI
+                return Uni.createFrom()
+                        .failure(new IllegalArgumentException("Invalid REST Client URL used: '" + uri + "'"));
+            }
             Uni<ServiceInstance> serviceInstance;
             try {
                 serviceInstance = Stork.getInstance()
@@ -300,7 +304,7 @@ public class ClientSendRequestHandler implements ClientRestHandler {
             } catch (MalformedURLException mue) {
                 log.error("Invalid REST Client URL used: '" + uri + "'");
                 return Uni.createFrom()
-                        .failure(new IllegalArgumentException("Invalid REST Client URL used"));
+                        .failure(new IllegalArgumentException("Invalid REST Client URL used: '" + uri + "'"));
             }
 
             boolean isHttps = "https".equals(uri.getScheme());

488cb208166b442f82962a01d6deaf30cf96357d
Georgios Andrianakis <geoand@gmail.com>
2022-03-09 11:06:54 +0200

    Allow injection of Lambda types into JAX-RS methods via @Context in RESTEasy Reactive
    Fixes: #24181

--- extensions/amazon-lambda-http/deployment/src/main/java/io/quarkus/amazon/lambda/http/deployment/AmazonLambdaHttpProcessor.java
+++ extensions/amazon-lambda-http/deployment/src/main/java/io/quarkus/amazon/lambda/http/deployment/AmazonLambdaHttpProcessor.java
@@ -2,8 +2,10 @@ package io.quarkus.amazon.lambda.http.deployment;
 
 import static io.vertx.core.file.impl.FileResolverImpl.CACHE_DIR_BASE_PROP_NAME;
 
+import org.jboss.jandex.DotName;
 import org.jboss.logging.Logger;
 
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 
@@ -16,16 +18,22 @@ import io.quarkus.amazon.lambda.http.LambdaHttpHandler;
 import io.quarkus.amazon.lambda.http.model.Headers;
 import io.quarkus.amazon.lambda.http.model.MultiValuedTreeMap;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.SystemPropertyBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.pkg.builditem.ArtifactResultBuildItem;
 import io.quarkus.deployment.pkg.builditem.OutputTargetBuildItem;
+import io.quarkus.resteasy.reactive.server.spi.ContextTypeBuildItem;
 import io.quarkus.vertx.http.deployment.RequireVirtualHttpBuildItem;
 
 public class AmazonLambdaHttpProcessor {
     private static final Logger log = Logger.getLogger(AmazonLambdaHttpProcessor.class);
+    public static final DotName CONTEXT = DotName.createSimple(Context.class.getName());
+    public static final DotName API_GATEWAY_HTTP_EVENT = DotName.createSimple(APIGatewayV2HTTPEvent.class.getName());
+    public static final DotName REQUEST_CONTEXT = DotName.createSimple(
+            APIGatewayV2HTTPEvent.RequestContext.class.getName());
 
     @BuildStep
     public void setupCDI(BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
@@ -94,6 +102,16 @@ public class AmazonLambdaHttpProcessor {
         output = LambdaUtil.copyResource("http/sam.native.yaml")
                 .replace("${lambdaName}", lambdaName);
         LambdaUtil.writeFile(target, "sam.native.yaml", output);
+    }
+
+    @BuildStep
+    public void resteasyReactiveIntegration(BuildProducer<ContextTypeBuildItem> contextTypeProducer,
+            BuildProducer<UnremovableBeanBuildItem> unremovableBeanProducer) {
+        contextTypeProducer.produce(new ContextTypeBuildItem(CONTEXT));
+        contextTypeProducer.produce(new ContextTypeBuildItem(API_GATEWAY_HTTP_EVENT));
+        contextTypeProducer.produce(new ContextTypeBuildItem(REQUEST_CONTEXT));
+
+        unremovableBeanProducer.produce(UnremovableBeanBuildItem.beanTypes(CONTEXT, API_GATEWAY_HTTP_EVENT, REQUEST_CONTEXT));
     }
 
 }

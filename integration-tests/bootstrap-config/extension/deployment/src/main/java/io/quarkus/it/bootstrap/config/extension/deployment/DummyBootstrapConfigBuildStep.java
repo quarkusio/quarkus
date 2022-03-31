21e3ace08761cd465e9c8fc042b163b6523e00f1
Roberto Cortez <radcortez@yahoo.com>
2022-03-31 15:57:05 +0100

    Fix Bootstrap Config to use default Config Sources (System and Env)

--- integration-tests/bootstrap-config/extension/deployment/src/main/java/io/quarkus/it/bootstrap/config/extension/deployment/DummyBootstrapConfigBuildStep.java
+++ integration-tests/bootstrap-config/extension/deployment/src/main/java/io/quarkus/it/bootstrap/config/extension/deployment/DummyBootstrapConfigBuildStep.java
@@ -8,15 +8,12 @@ import io.quarkus.deployment.builditem.RunTimeConfigurationSourceValueBuildItem;
 import io.quarkus.it.bootstrap.config.extension.DummyBootstrapRecorder;
 import io.quarkus.it.bootstrap.config.extension.DummyBootstrapRecorder2;
 import io.quarkus.it.bootstrap.config.extension.DummyConfig;
-import io.quarkus.runtime.ApplicationConfig;
 
 public class DummyBootstrapConfigBuildStep {
-
     @BuildStep
     @Record(ExecutionTime.RUNTIME_INIT)
-    public RunTimeConfigurationSourceValueBuildItem dummyRecorder(DummyBootstrapRecorder recorder, DummyConfig dummyConfig,
-            ApplicationConfig applicationConfig) {
-        return new RunTimeConfigurationSourceValueBuildItem(recorder.create(dummyConfig, applicationConfig));
+    public RunTimeConfigurationSourceValueBuildItem dummyRecorder(DummyBootstrapRecorder recorder, DummyConfig dummyConfig) {
+        return new RunTimeConfigurationSourceValueBuildItem(recorder.create(dummyConfig));
     }
 
     @BuildStep
--- integration-tests/bootstrap-config/extension/runtime/src/main/java/io/quarkus/it/bootstrap/config/extension/DummyConfig.java
+++ integration-tests/bootstrap-config/extension/runtime/src/main/java/io/quarkus/it/bootstrap/config/extension/DummyConfig.java
@@ -1,5 +1,8 @@
 package io.quarkus.it.bootstrap.config.extension;
 
+import java.util.Map;
+
+import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
@@ -17,4 +20,19 @@ public class DummyConfig {
      */
     @ConfigItem(defaultValue = "2")
     public Integer times;
+
+    /**
+     * dummy map
+     */
+    @ConfigItem(name = "map")
+    public Map<String, MapConfig> map;
+
+    @ConfigGroup
+    public static class MapConfig {
+        /**
+         * dummy value
+         */
+        @ConfigItem(name = ConfigItem.PARENT)
+        public String value;
+    }
 }

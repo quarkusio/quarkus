1497bc9003e444faaf2c9a12cd95e03e64047bf2
Foivos Zakkak <fzakkak@redhat.com>
2022-04-04 23:40:04 +0300

    Fix IT main for GraalVM >= 22.1

--- integration-tests/main/src/main/java/io/quarkus/it/corestuff/serialization/SerializationConfig.java
+++ integration-tests/main/src/main/java/io/quarkus/it/corestuff/serialization/SerializationConfig.java
@@ -1,11 +1,10 @@
 package io.quarkus.it.corestuff.serialization;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
-@RegisterForReflection(targets = { List.class, ArrayList.class, String.class }, serialization = true)
+@RegisterForReflection(targets = { ArrayList.class, String.class }, serialization = true)
 public class SerializationConfig {
 
 }

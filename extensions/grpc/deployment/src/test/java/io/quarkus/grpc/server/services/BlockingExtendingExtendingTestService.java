da51a8d3e29ae28fe88a831e9432e5a8ed475e05
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2022-04-07 23:19:47 +0200

    Blocking inheritance for gRPC: simplified tests

--- extensions/grpc/deployment/src/test/java/io/quarkus/grpc/server/services/BlockingExtendingExtendingTestService.java
+++
@@ -1,7 +0,0 @@
-package io.quarkus.grpc.server.services;
-
-import io.quarkus.grpc.GrpcService;
-
-@GrpcService
-public class BlockingExtendingExtendingTestService extends BlockingBaseExtendingTestService {
-}
--- extensions/grpc/deployment/src/test/java/io/quarkus/grpc/server/services/BlockingMutinyExtendingExtendingTestService.java
+++
@@ -1,7 +0,0 @@
-package io.quarkus.grpc.server.services;
-
-import io.quarkus.grpc.GrpcService;
-
-@GrpcService
-public class BlockingMutinyExtendingExtendingTestService extends BlockingMutinyBaseExtendingTestService {
-}

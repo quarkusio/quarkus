a1758d212a25d5b5202ccbe3f647c1d698bb87e8
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2022-04-21 13:07:12 +0200

    Fix REST Client Reactive path handling for sub-resources
    fixes #25028
    
    (cherry picked from commit a535cc0df19d52d77eaf28e03c14897d1de7744c)

--- extensions/resteasy-reactive/rest-client-reactive/deployment/src/test/java/io/quarkus/rest/client/reactive/subresource/Resource.java
+++ extensions/resteasy-reactive/rest-client-reactive/deployment/src/test/java/io/quarkus/rest/client/reactive/subresource/Resource.java
@@ -17,16 +17,22 @@ import org.jboss.resteasy.reactive.RestQuery;
 public class Resource {
 
     @GET
+    @Path("{part1}/{part2}/{part3}")
+    public String getUriParts(@RestPath String part1, @RestPath String part2, @RestPath String part3) {
+        return String.format("%s/%s/%s", part1, part2, part3);
+    }
+
+    @GET
+    @Path("{part1}/{part2}/{part3}/{part4}")
+    public String getUriParts(@RestPath String part1, @RestPath String part2, @RestPath String part3, @RestPath String part4) {
+        return String.format("%s/%s/%s/%s", part1, part2, part3, part4);
+    }
+
+    @GET
     @Path("{part1}/{part2}/{part3}/{part4}/{part5}")
     public String getUriParts(@RestPath String part1, @RestPath String part2, @RestPath String part3, @RestPath String part4,
             @RestPath String part5) {
         return String.format("%s/%s/%s/%s/%s", part1, part2, part3, part4, part5);
-    }
-
-    @GET
-    @Path("{part1}/{part2}/{part3}")
-    public String getUriParts(@RestPath String part1, @RestPath String part2, @RestPath String part3) {
-        return String.format("%s/%s/%s", part1, part2, part3);
     }
 
     @POST
@@ -46,11 +52,11 @@ public class Resource {
     }
 
     @POST
-    @Path("{part1}/{part2}/{part3}/{part4}")
+    @Path("{part1}/{part2}/{part3}")
     public Response getUriEntityAndQueryParamFromSubResource(@RestPath String part1, @RestPath String part2,
-            @RestPath String part3, @RestPath String part4,
-            @RestQuery String queryParam, String entity, @Context HttpHeaders headers) {
-        Response.ResponseBuilder responseBuilder = Response.ok(String.format("%s/%s:%s:%s", part1, part2, entity, queryParam));
+            @RestPath String part3, @RestQuery String queryParam, String entity, @Context HttpHeaders headers) {
+        Response.ResponseBuilder responseBuilder = Response
+                .ok(String.format("%s/%s/%s:%s:%s", part1, part2, part3, entity, queryParam));
 
         for (Map.Entry<String, List<String>> headerEntry : headers.getRequestHeaders().entrySet()) {
             String headerName = headerEntry.getKey();

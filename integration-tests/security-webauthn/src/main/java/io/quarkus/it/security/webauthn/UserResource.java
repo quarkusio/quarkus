098bc7af618a7c5ad9a816db3905d4ed364b316c
Stephane Epardaud <stef@epardaud.fr>
2022-04-26 15:48:08 +0200

    Added WebAuthn IT

---
+++ integration-tests/security-webauthn/src/main/java/io/quarkus/it/security/webauthn/UserResource.java
@@ -0,0 +1,18 @@
+package io.quarkus.it.security.webauthn;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+
+@Path("/api/users")
+public class UserResource {
+
+    @GET
+    @RolesAllowed("user")
+    @Path("/me")
+    public String me(@Context SecurityContext securityContext) {
+        return securityContext.getUserPrincipal().getName();
+    }
+}

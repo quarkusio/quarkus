57cbeeb28e4ee3200204f4775332d29c2433187e
Georgios Andrianakis <geoand@gmail.com>
2022-04-28 17:04:03 +0300

    Remove Mutiny service loading
    N.B. This requires a new Mutiny version

---
+++ extensions/mutiny/deployment/src/main/java/io/quarkus/mutiny/deployment/ServiceLoadingRemovalProcessor.java
@@ -0,0 +1,87 @@
+package io.quarkus.mutiny.deployment;
+
+import static org.objectweb.asm.Opcodes.ACONST_NULL;
+
+import java.util.List;
+import java.util.function.BiFunction;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import io.quarkus.deployment.IsNormal;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.BytecodeTransformerBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
+import io.quarkus.gizmo.Gizmo;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+
+public class ServiceLoadingRemovalProcessor {
+
+    //TODO: this simply returns empty lists because we don't currently register the implementations with ServiceProviderBuildItem
+
+    @BuildStep(onlyIf = IsNormal.class)
+    public void transformSmallRyeConfigBuilder(List<ServiceProviderBuildItem> servicesItems,
+            BuildProducer<BytecodeTransformerBuildItem> producer) {
+        producer.produce(new BytecodeTransformerBuildItem(Infrastructure.class.getName(),
+                new BiFunction<>() {
+                    @Override
+                    public ClassVisitor apply(String s, ClassVisitor classVisitor) {
+                        return new ClassVisitor(Gizmo.ASM_API_VERSION, classVisitor) {
+
+                            @Override
+                            public MethodVisitor visitMethod(int access, String name, String descriptor,
+                                    String signature, String[] exceptions) {
+                                MethodVisitor original = super.visitMethod(access, name, descriptor, signature, exceptions);
+                                if (name.equals("loadExecutorConfiguration")) {
+                                    return new MethodVisitor(Gizmo.ASM_API_VERSION, original) {
+                                        @Override
+                                        public void visitCode() {
+                                            super.visitCode();
+                                            visitInsn(ACONST_NULL);
+                                            visitInsn(Opcodes.ARETURN);
+                                        }
+                                    };
+                                } else if (name.equals("loadUniInterceptors")) {
+                                    return new MethodVisitor(Gizmo.ASM_API_VERSION, original) {
+                                        @Override
+                                        public void visitCode() {
+                                            super.visitCode();
+                                            visitMethodInsn(Opcodes.INVOKESTATIC, "java/util/Collections", "emptyList",
+                                                    "()Ljava/util/List;",
+                                                    false);
+                                            visitInsn(Opcodes.ARETURN);
+                                        }
+                                    };
+                                } else if (name.equals("loadMultiInterceptors")) {
+                                    return new MethodVisitor(Gizmo.ASM_API_VERSION, original) {
+                                        @Override
+                                        public void visitCode() {
+                                            super.visitCode();
+                                            visitMethodInsn(Opcodes.INVOKESTATIC, "java/util/Collections", "emptyList",
+                                                    "()Ljava/util/List;",
+                                                    false);
+                                            visitInsn(Opcodes.ARETURN);
+                                        }
+                                    };
+                                } else if (name.equals("loadCallbackDecorators")) {
+                                    return new MethodVisitor(Gizmo.ASM_API_VERSION, original) {
+                                        @Override
+                                        public void visitCode() {
+                                            super.visitCode();
+                                            visitMethodInsn(Opcodes.INVOKESTATIC, "java/util/Collections", "emptyList",
+                                                    "()Ljava/util/List;",
+                                                    false);
+                                            visitInsn(Opcodes.ARETURN);
+                                        }
+                                    };
+                                }
+
+                                return original;
+                            }
+                        };
+                    }
+                }));
+    }
+}

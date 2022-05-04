f4480941c7c13fe00c97fedf0fe63c4e84c58099
Roberto Cortez <radcortez@yahoo.com>
2022-05-04 01:07:01 +0100

    Fix Bootstrap Config to use default Config Sources (System and Env)

--- integration-tests/bootstrap-config/extension/deployment/src/main/java/io/quarkus/it/bootstrap/config/extension/deployment/DummyBootstrapConfigBuildStep.java
+++ integration-tests/bootstrap-config/extension/deployment/src/main/java/io/quarkus/it/bootstrap/config/extension/deployment/DummyBootstrapConfigBuildStep.java
@@ -4,12 +4,22 @@ import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.RunTimeConfigBuilderBuildItem;
 import io.quarkus.deployment.builditem.RunTimeConfigurationSourceValueBuildItem;
+import io.quarkus.deployment.builditem.StaticInitConfigBuilderBuildItem;
 import io.quarkus.it.bootstrap.config.extension.DummyBootstrapRecorder;
 import io.quarkus.it.bootstrap.config.extension.DummyBootstrapRecorder2;
 import io.quarkus.it.bootstrap.config.extension.DummyConfig;
+import io.quarkus.it.bootstrap.config.extension.DummyConfigBuilder;
 
 public class DummyBootstrapConfigBuildStep {
+    @BuildStep
+    public void configBuilders(BuildProducer<StaticInitConfigBuilderBuildItem> staticConfigBuilders,
+            BuildProducer<RunTimeConfigBuilderBuildItem> runtimeConfigBuilders) {
+        staticConfigBuilders.produce(new StaticInitConfigBuilderBuildItem(DummyConfigBuilder.class.getName()));
+        runtimeConfigBuilders.produce(new RunTimeConfigBuilderBuildItem(DummyConfigBuilder.class.getName()));
+    }
+
     @BuildStep
     @Record(ExecutionTime.RUNTIME_INIT)
     public RunTimeConfigurationSourceValueBuildItem dummyRecorder(DummyBootstrapRecorder recorder, DummyConfig dummyConfig) {

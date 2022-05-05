769327d91c7c7fa706bf702552beea020315a516
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2022-05-05 23:59:15 +0200

    fix(quartz): make sure that disabled jobs are paused
    Fixes https://github.com/quarkusio/quarkus/issues/25214
    
    Disabled methods whose jobs details and triggers were already in the
    database were still getting triggered because Quartz invokes all the
    jobs in the database that are not in the paused State.
    This is to make sure that the disabled methods are effectively not
    triggered by Quartz in any way.
    
    Furthermore, I do not believe the full scheduled method implementing the Application custom logic
    was called because of the checks in the extensions which meant the
    App business logic trigger was not full invoked but pausing the triggers
    via Quartz we are sure that the trigger won't be invoked when it is
    disabled.
    
    Previously this was done via checks in the extension which mean that
    only the Application logic of the scheduled method was not called.
    
    - https://github.com/quarkusio/quarkus/blob/4df39b60a0b3d132713e6d60087a1054588d4a5f/extensions/quartz/runtime/src/main/java/io/quarkus/quartz/runtime/QuartzScheduler.java#L215-L218
    - https://github.com/quarkusio/quarkus/blob/4df39b60a0b3d132713e6d60087a1054588d4a5f/extensions/quartz/runtime/src/main/java/io/quarkus/quartz/runtime/QuartzScheduler.java#L168-L170
    
    which meant that the trigger was not stored in known business triggers
    at https://github.com/quarkusio/quarkus/blob/4df39b60a0b3d132713e6d60087a1054588d4a5f/extensions/quartz/runtime/src/main/java/io/quarkus/quartz/runtime/QuartzScheduler.java#L302-L303
    
    meaning that upon the execution invocation of the method in
    https://github.com/quarkusio/quarkus/blob/4df39b60a0b3d132713e6d60087a1054588d4a5f/extensions/quartz/runtime/src/main/java/io/quarkus/quartz/runtime/QuartzScheduler.java#L560
    the business logic wasn't triggered but a NPE (same as https://github.com/quarkusio/quarkus/issues/24931) could have been raised
    with the code in main branch
    
    (cherry picked from commit 52e83f107c353cc6931d4896d5429b00cdca28b8)

--- integration-tests/quartz/src/test/java/io/quarkus/it/quartz/QuartzTestCase.java
+++ integration-tests/quartz/src/test/java/io/quarkus/it/quartz/QuartzTestCase.java
@@ -1,6 +1,7 @@
 package io.quarkus.it.quartz;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -19,11 +20,28 @@ public class QuartzTestCase {
         assertCounter("/scheduler/count/fix-8555");
     }
 
+    @Test
+    public void testDisabledMethodsShouldNeverBeExecuted() throws InterruptedException {
+        // Wait at least 1 second
+        Thread.sleep(1000);
+        assertEmptyValueForDisabledMethod("/scheduler/disabled/cron");
+        assertEmptyValueForDisabledMethod("/scheduler/disabled/every");
+    }
+
     private void assertCounter(String counterPath) {
         Response response = given().when().get(counterPath);
         String body = response.asString();
         int count = Integer.valueOf(body);
         assertTrue(count > 0);
+        response
+                .then()
+                .statusCode(200);
+    }
+
+    private void assertEmptyValueForDisabledMethod(String path) {
+        Response response = given().when().get(path);
+        String body = response.asString();
+        assertEquals("", body);
         response
                 .then()
                 .statusCode(200);

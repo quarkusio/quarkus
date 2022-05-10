c4a4640107f36407c0e7c8d53685c88cbbd7bb75
Thomas Segismont <tsegismo@redhat.com>
2022-05-10 18:09:04 +0200

    Dev services: MSSQL reactive client does not support JDBC url format
    Fixes #25233
    
    The "encrypt" property is not needed anyway, it is the default of both the JDBC and reactive clients.
    
    https://docs.microsoft.com/en-us/sql/connect/jdbc/understanding-ssl-support?view=sql-server-ver15#remarks

--- extensions/datasource/deployment-spi/src/main/java/io/quarkus/datasource/deployment/spi/DevServicesDatasourceConfigurationHandlerBuildItem.java
+++ extensions/datasource/deployment-spi/src/main/java/io/quarkus/datasource/deployment/spi/DevServicesDatasourceConfigurationHandlerBuildItem.java
@@ -3,6 +3,7 @@ package io.quarkus.datasource.deployment.spi;
 import java.util.Collections;
 import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import io.quarkus.builder.item.MultiBuildItem;
@@ -85,17 +86,31 @@ public final class DevServicesDatasourceConfigurationHandlerBuildItem extends Mu
     }
 
     public static DevServicesDatasourceConfigurationHandlerBuildItem reactive(String dbKind) {
+        return reactive(dbKind, new Function<String, String>() {
+            @Override
+            public String apply(String url) {
+                return url.replaceFirst("jdbc:", "vertx-reactive:");
+            }
+        });
+    }
+
+    public static DevServicesDatasourceConfigurationHandlerBuildItem reactive(String dbKind,
+            Function<String, String> jdbcUrlTransformer) {
         return new DevServicesDatasourceConfigurationHandlerBuildItem(dbKind,
                 new BiFunction<String, DevServicesDatasourceProvider.RunningDevServicesDatasource, Map<String, String>>() {
                     @Override
                     public Map<String, String> apply(String dsName,
                             DevServicesDatasourceProvider.RunningDevServicesDatasource runningDevDb) {
+                        String url = jdbcUrlTransformer.apply(runningDevDb.getUrl());
                         if (dsName == null) {
-                            return Collections.singletonMap("quarkus.datasource.reactive.url",
-                                    runningDevDb.getUrl().replaceFirst("jdbc:", "vertx-reactive:"));
+                            return Collections.singletonMap("quarkus.datasource.reactive.url", url);
                         } else {
-                            return Collections.singletonMap("quarkus.datasource.\"" + dsName + "\".reactive.url",
-                                    runningDevDb.getUrl().replaceFirst("jdbc:", "vertx-reactive:"));
+                            // we use quoted and unquoted versions because depending on whether a user configured other JDBC properties
+                            // one of the URLs may be ignored
+                            // see https://github.com/quarkusio/quarkus/issues/21387
+                            return Map.of(
+                                    datasourceReactiveURLPropName(dsName, false), url,
+                                    datasourceReactiveURLPropName(dsName, true), url);
                         }
                     }
                 }, new Predicate<String>() {
@@ -104,10 +119,24 @@ public final class DevServicesDatasourceConfigurationHandlerBuildItem extends Mu
                         if (dsName == null) {
                             return ConfigUtils.isPropertyPresent("quarkus.datasource.reactive.url");
                         } else {
-                            return ConfigUtils.isPropertyPresent("quarkus.datasource.\"" + dsName + "\".reactive.url") ||
-                                    ConfigUtils.isPropertyPresent("quarkus.datasource." + dsName + ".reactive.url");
+                            return ConfigUtils.isPropertyPresent(datasourceReactiveURLPropName(dsName, false)) ||
+                                    ConfigUtils.isPropertyPresent(datasourceReactiveURLPropName(dsName, true));
                         }
                     }
                 });
+    }
+
+    private static String datasourceReactiveURLPropName(String dsName, boolean quotedName) {
+        StringBuilder key = new StringBuilder("quarkus.datasource");
+        key.append('.');
+        if (quotedName) {
+            key.append('"');
+        }
+        key.append(dsName);
+        if (quotedName) {
+            key.append('"');
+        }
+        key.append(".reactive.url");
+        return key.toString();
     }
 }

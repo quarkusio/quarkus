10cecc87b8780777e21ab236b6245db1f30977d1
Emanuel Alves <emanuel.j.b.alves@gmail.com>
2022-05-11 15:57:45 +0100

    Add Optional converter
    (cherry picked from commit 3c27dbb998c8fea7b9cf12c4a30363c64f8b6d84)

--- test-framework/junit5/src/main/java/io/quarkus/test/junit/internal/XStreamDeepClone.java
+++ test-framework/junit5/src/main/java/io/quarkus/test/junit/internal/XStreamDeepClone.java
@@ -1,5 +1,6 @@
 package io.quarkus.test.junit.internal;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import com.thoughtworks.xstream.XStream;
@@ -17,10 +18,13 @@ class XStreamDeepClone implements DeepClone {
             XStream result = new XStream();
             result.allowTypesByRegExp(new String[] { ".*" });
             result.setClassLoader(classLoader);
+            result.allowTypes(new Class[] { Optional.class });
             result.registerConverter(new CustomListConverter(result.getMapper()));
             result.registerConverter(new CustomSetConverter(result.getMapper()));
             result.registerConverter(new CustomMapConverter(result.getMapper()));
             result.registerConverter(new CustomMapEntryConverter(result.getMapper()));
+            result.registerConverter(new OptionalConverter(result.getMapper()));
+
             return result;
         };
     }

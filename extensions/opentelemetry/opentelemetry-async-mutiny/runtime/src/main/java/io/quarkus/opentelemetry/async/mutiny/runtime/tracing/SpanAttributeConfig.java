43b48018b9da2129d1188d4baebd3ac4fccfdca2
Christopher <holomekc.github@gmail.com>
2022-05-14 01:22:46 +0200

    add missing javadoc for config

--- extensions/opentelemetry/opentelemetry-async-mutiny/runtime/src/main/java/io/quarkus/opentelemetry/async/mutiny/runtime/tracing/SpanAttributeConfig.java
+++ extensions/opentelemetry/opentelemetry-async-mutiny/runtime/src/main/java/io/quarkus/opentelemetry/async/mutiny/runtime/tracing/SpanAttributeConfig.java
@@ -6,6 +6,10 @@ import io.quarkus.runtime.annotations.ConfigItem;
 @ConfigGroup
 public class SpanAttributeConfig {
 
+    /**
+     * Add a span attribute in case a mutiny stream is cancelled. It is a boolean attribute with key:
+     * <code>mutiny.canceled</code>
+     */
     @ConfigItem(defaultValue = "true")
     public Boolean onCancellation;
 }

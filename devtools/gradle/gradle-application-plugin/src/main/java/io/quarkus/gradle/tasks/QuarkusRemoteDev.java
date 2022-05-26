6424dbe216e3e0f5bdc8aa7b1453a1ece338d30e
Steve Ebersole <steve@hibernate.org>
2022-05-26 14:16:48 -0500

    Work combining previous PRs for better leveraging Gradle deferred initialization

--- devtools/gradle/gradle-application-plugin/src/main/java/io/quarkus/gradle/tasks/QuarkusRemoteDev.java
+++ devtools/gradle/gradle-application-plugin/src/main/java/io/quarkus/gradle/tasks/QuarkusRemoteDev.java
@@ -1,9 +1,19 @@
 package io.quarkus.gradle.tasks;
 
+import javax.inject.Inject;
+
+import org.gradle.api.artifacts.Configuration;
+
+import io.quarkus.gradle.extension.QuarkusPluginExtension;
+
 public class QuarkusRemoteDev extends QuarkusDev {
 
-    public QuarkusRemoteDev() {
-        super("Remote development mode: enables hot deployment on remote JVM with background compilation");
+    @Inject
+    public QuarkusRemoteDev(Configuration quarkusDevConfiguration, QuarkusPluginExtension extension) {
+        super(
+                "Remote development mode: enables hot deployment on remote JVM with background compilation",
+                quarkusDevConfiguration,
+                extension);
     }
 
     protected void modifyDevModeContext(GradleDevModeLauncher.Builder builder) {

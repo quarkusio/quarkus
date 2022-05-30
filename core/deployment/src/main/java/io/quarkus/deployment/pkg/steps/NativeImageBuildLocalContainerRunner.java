a0900068386424a7f83c71111b48ed936cec8aaa
Michal Karm Babacek <karm@redhat.com>
2022-05-30 21:27:06 +0200

    Podman on Windows needs one /, not two // prefix
    fixes #25865
    
    (cherry picked from commit db7ab714570bb49ed55710b8219f14ce8b956346)

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/NativeImageBuildLocalContainerRunner.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/NativeImageBuildLocalContainerRunner.java
@@ -37,7 +37,8 @@ public class NativeImageBuildLocalContainerRunner extends NativeImageBuildContai
         List<String> containerRuntimeArgs = super.getContainerRuntimeBuildArgs();
         String volumeOutputPath = outputPath;
         if (SystemUtils.IS_OS_WINDOWS) {
-            volumeOutputPath = FileUtil.translateToVolumePath(volumeOutputPath);
+            volumeOutputPath = FileUtil.translateToVolumePath(volumeOutputPath,
+                    containerRuntime == NativeConfig.ContainerRuntime.PODMAN);
         }
 
         Collections.addAll(containerRuntimeArgs, "-v",

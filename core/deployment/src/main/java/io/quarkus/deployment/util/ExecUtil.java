quarkusio/quarkus

commit 418d986f7ea21f93aef3490921f436612bcfe141

DockerStatusBuildItem for caching isDockerWorking status.
* Reduce socket timeout for checking docker listening on DOCKER_HOST.
* Timeout for checking docker commands

core/deployment/src/main/java/io/quarkus/deployment/util/ExecUtil.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 import org.jboss.logging.Logger;
@@ -16,6 +18,8 @@ public class ExecUtil {
 
     private static final Function<InputStream, Runnable> PRINT_OUTPUT = i -> new HandleOutput(i);
     private static final Function<InputStream, Runnable> SILENT = i -> new HandleOutput(i, Logger.Level.DEBUG);
+
+    private static final int PROCESS_CHECK_INTERVAL = 500;
 
     private static class HandleOutput implements Runnable {
 
@@ -61,6 +65,18 @@ public class ExecUtil {
     }
 
     /**
+     * Execute the specified command until the given timeout from within the current directory.
+     *
+     * @param timeout The timeout
+     * @param command The command
+     * @param args The command arguments
+     * @return true if commands where executed successfully
+     */
+    public static boolean execWithTimeout(Duration timeout, String command, String... args) {
+        return execWithTimeout(new File("."), timeout, command, args);
+    }
+
+    /**
      * Execute the specified command from within the current directory and hide the output.
      *
      * @param command The command
@@ -69,6 +85,18 @@ public class ExecUtil {
      */
     public static boolean execSilent(String command, String... args) {
         return execSilent(new File("."), command, args);
+    }
+
+    /**
+     * Execute the specified command until the given timeout from within the current directory and hide the output.
+     *
+     * @param timeout The timeout
+     * @param command The command
+     * @param args The command arguments
+     * @return true if commands where executed successfully
+     */
+    public static boolean execSilentWithTimeout(Duration timeout, String command, String... args) {
+        return execSilentWithTimeout(new File("."), timeout, command, args);
     }
 
     /**
@@ -84,6 +112,19 @@ public class ExecUtil {
     }
 
     /**
+     * Execute the specified command until the given timeout from within the specified directory.
+     *
+     * @param directory The directory
+     * @param timeout The timeout
+     * @param command The command
+     * @param args The command arguments
+     * @return true if commands where executed successfully
+     */
+    public static boolean execWithTimeout(File directory, Duration timeout, String command, String... args) {
+        return execWithTimeout(directory, PRINT_OUTPUT, timeout, command, args);
+    }
+
+    /**
      * Execute the specified command from within the specified directory and hide the output.
      *
      * @param directory The directory
@@ -93,6 +134,19 @@ public class ExecUtil {
      */
     public static boolean execSilent(File directory, String command, String... args) {
         return exec(directory, SILENT, command, args);
+    }
+
+    /**
+     * Execute the specified command until the given timeout from within the specified directory and hide the output.
+     *
+     * @param directory The directory
+     * @param timeout The timeout
+     * @param command The command
+     * @param args The command arguments
+     * @return true if commands where executed successfully
+     */
+    public static boolean execSilentWithTimeout(File directory, Duration timeout, String command, String... args) {
+        return execWithTimeout(directory, SILENT, timeout, command, args);
     }
 
     /**
@@ -107,27 +161,87 @@ public class ExecUtil {
      */
     public static boolean exec(File directory, Function<InputStream, Runnable> outputFilterFunction, String command,
             String... args) {
-        Process process = null;
+        try {
+            Process process = startProcess(directory, command, args);
+            outputFilterFunction.apply(process.getInputStream());
+            process.waitFor();
+            return process.exitValue() == 0;
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Execute the specified command until the given timeout from within the specified directory.
+     * The method allows specifying an output filter that processes the command output.
+     *
+     * @param directory The directory
+     * @param outputFilterFunction A {@link Function} that gets an {@link InputStream} and returns an outputFilter.
+     * @param timeout The timeout
+     * @param command The command
+     * @param args The command arguments
+     * @return true if commands where executed successfully
+     */
+    public static boolean execWithTimeout(File directory, Function<InputStream, Runnable> outputFilterFunction,
+            Duration timeout, String command, String... args) {
+        try {
+            Process process = startProcess(directory, command, args);
+            Thread t = new Thread(outputFilterFunction.apply(process.getInputStream()));
+            t.setName("Process stdout");
+            t.setDaemon(true);
+            t.start();
+            process.waitFor(timeout.toMillis(), TimeUnit.MILLISECONDS);
+            destroyProcess(process);
+            return process.exitValue() == 0;
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Start a process executing given command with arguments within the specified directory.
+     *
+     * @param directory The directory
+     * @param command The command
+     * @param args The command arguments
+     * @return the process
+     */
+    public static Process startProcess(File directory, String command, String... args) {
         try {
             String[] cmd = new String[args.length + 1];
             cmd[0] = command;
             if (args.length > 0) {
                 System.arraycopy(args, 0, cmd, 1, args.length);
             }
-            process = new ProcessBuilder()
+            return new ProcessBuilder()
                     .directory(directory)
                     .command(cmd)
                     .redirectErrorStream(true)
                     .start();
-
-            outputFilterFunction.apply(process.getInputStream()).run();
-            process.waitFor();
         } catch (IOException e) {
             throw new RuntimeException("Input/Output error while executing command.", e);
-        } catch (InterruptedException e) {
-            return false;
         }
-        return process != null && process.exitValue() == 0;
+    }
+
+    /**
+     * Kill the process, if still alive, kill it forcibly
+     *
+     * @param process the process to kill
+     */
+    public static void destroyProcess(Process process) {
+        process.destroy();
+        int i = 0;
+        while (process.isAlive() && i++ < 10) {
+            try {
+                process.waitFor(PROCESS_CHECK_INTERVAL, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        if (process.isAlive()) {
+            process.destroyForcibly();
+        }
     }
 
 }

core/deployment/src/main/java/io/quarkus/deployment/DockerStatusProcessor.java
@@ -1,0 +1,13 @@
+package io.quarkus.deployment;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.DockerStatusBuildItem;
+import io.quarkus.deployment.builditem.LaunchModeBuildItem;
+
+public class DockerStatusProcessor {
+
+    @BuildStep
+    DockerStatusBuildItem IsDockerWorking(LaunchModeBuildItem launchMode) {
+        return new DockerStatusBuildItem(new IsDockerWorking(launchMode.getLaunchMode().isDevOrTest()));
+    }
+}

core/deployment/src/main/java/io/quarkus/deployment/IsDockerWorking.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.BooleanSupplier;
@@ -25,6 +27,8 @@ import io.quarkus.deployment.util.ExecUtil;
 public class IsDockerWorking implements BooleanSupplier {
 
     private static final Logger LOGGER = Logger.getLogger(IsDockerWorking.class.getName());
+    public static final int DOCKER_HOST_CHECK_TIMEOUT = 1000;
+    public static final int DOCKER_CMD_CHECK_TIMEOUT = 3000;
 
     private final List<Strategy> strategies;
 
@@ -40,6 +44,7 @@ public class IsDockerWorking implements BooleanSupplier {
     @Override
     public boolean getAsBoolean() {
         for (Strategy strategy : strategies) {
+            LOGGER.debugf("Checking Docker Environment using strategy %s", strategy.getClass().getName());
             Result result = strategy.get();
             if (result == Result.AVAILABLE) {
                 return true;
@@ -113,7 +118,8 @@ public class IsDockerWorking implements BooleanSupplier {
             if (dockerHost != null && !dockerHost.startsWith("unix:")) {
                 try {
                     URI url = new URI(dockerHost);
-                    try (Socket s = new Socket(url.getHost(), url.getPort())) {
+                    try (Socket s = new Socket()) {
+                        s.connect(new InetSocketAddress(url.getHost(), url.getPort()), DOCKER_HOST_CHECK_TIMEOUT);
                         return Result.AVAILABLE;
                     } catch (IOException e) {
                         LOGGER.warnf(
@@ -143,7 +149,7 @@ public class IsDockerWorking implements BooleanSupplier {
         @Override
         public Result get() {
             try {
-                if (!ExecUtil.execSilent(binary, "-v")) {
+                if (!ExecUtil.execSilentWithTimeout(Duration.ofMillis(DOCKER_CMD_CHECK_TIMEOUT), binary, "-v")) {
                     LOGGER.warnf("'%s -v' returned an error code. Make sure your Docker binary is correct", binary);
                     return Result.UNKNOWN;
                 }
@@ -154,7 +160,8 @@ public class IsDockerWorking implements BooleanSupplier {
 
             try {
                 OutputFilter filter = new OutputFilter();
-                if (ExecUtil.exec(new File("."), filter, "docker", "version", "--format", "'{{.Server.Version}}'")) {
+                if (ExecUtil.execWithTimeout(new File("."), filter, Duration.ofMillis(DOCKER_CMD_CHECK_TIMEOUT),
+                        "docker", "version", "--format", "'{{.Server.Version}}'")) {
                     LOGGER.debugf("Docker daemon found. Version: %s", filter.getOutput());
                     return Result.AVAILABLE;
                 } else {

core/deployment/src/main/java/io/quarkus/deployment/builditem/DockerStatusBuildItem.java
@@ -1,0 +1,21 @@
+package io.quarkus.deployment.builditem;
+
+import io.quarkus.builder.item.SimpleBuildItem;
+import io.quarkus.deployment.IsDockerWorking;
+
+public final class DockerStatusBuildItem extends SimpleBuildItem {
+
+    private final IsDockerWorking isDockerWorking;
+    private Boolean cachedStatus;
+
+    public DockerStatusBuildItem(IsDockerWorking isDockerWorking) {
+        this.isDockerWorking = isDockerWorking;
+    }
+
+    public synchronized boolean isDockerAvailable() {
+        if (cachedStatus == null) {
+            cachedStatus = isDockerWorking.getAsBoolean();
+        }
+        return cachedStatus;
+    }
+}

extensions/container-image/container-image-docker/deployment/src/main/java/io/quarkus/container/image/docker/deployment/DockerProcessor.java
@@ -30,10 +30,10 @@ import io.quarkus.container.spi.ContainerImageBuildRequestBuildItem;
 import io.quarkus.container.spi.ContainerImageBuilderBuildItem;
 import io.quarkus.container.spi.ContainerImageInfoBuildItem;
 import io.quarkus.container.spi.ContainerImagePushRequestBuildItem;
-import io.quarkus.deployment.IsDockerWorking;
 import io.quarkus.deployment.IsNormalNotRemoteDev;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.DockerStatusBuildItem;
 import io.quarkus.deployment.pkg.PackageConfig;
 import io.quarkus.deployment.pkg.builditem.AppCDSResultBuildItem;
 import io.quarkus.deployment.pkg.builditem.ArtifactResultBuildItem;
@@ -55,8 +55,6 @@ public class DockerProcessor {
     private static final String DOCKER_DIRECTORY_NAME = "docker";
     static final String DOCKER_CONTAINER_IMAGE_NAME = "docker";
 
-    private final IsDockerWorking isDockerWorking = new IsDockerWorking();
-
     @BuildStep
     public AvailableContainerImageExtensionBuildItem availability() {
         return new AvailableContainerImageExtensionBuildItem(DOCKER);
@@ -64,6 +62,7 @@ public class DockerProcessor {
 
     @BuildStep(onlyIf = { IsNormalNotRemoteDev.class, DockerBuild.class }, onlyIfNot = NativeBuild.class)
     public void dockerBuildFromJar(DockerConfig dockerConfig,
+            DockerStatusBuildItem dockerStatusBuildItem,
             ContainerImageConfig containerImageConfig,
             OutputTargetBuildItem out,
             ContainerImageInfoBuildItem containerImageInfo,
@@ -83,7 +82,7 @@ public class DockerProcessor {
             return;
         }
 
-        if (!isDockerWorking.getAsBoolean()) {
+        if (!dockerStatusBuildItem.isDockerAvailable()) {
             throw new RuntimeException("Unable to build docker image. Please check your docker installation");
         }
 
@@ -116,6 +115,7 @@ public class DockerProcessor {
 
     @BuildStep(onlyIf = { IsNormalNotRemoteDev.class, NativeBuild.class, DockerBuild.class })
     public void dockerBuildFromNativeImage(DockerConfig dockerConfig,
+            DockerStatusBuildItem dockerStatusBuildItem,
             ContainerImageConfig containerImageConfig,
             ContainerImageInfoBuildItem containerImage,
             Optional<ContainerImageBuildRequestBuildItem> buildRequest,
@@ -134,7 +134,7 @@ public class DockerProcessor {
             return;
         }
 
-        if (!isDockerWorking.getAsBoolean()) {
+        if (!dockerStatusBuildItem.isDockerAvailable()) {
             throw new RuntimeException("Unable to build docker image. Please check your docker installation");
         }
 

extensions/datasource/deployment/src/main/java/io/quarkus/datasource/deployment/devservices/DevServicesDatasourceProcessor.java
@@ -20,13 +20,13 @@ import io.quarkus.datasource.deployment.spi.DevServicesDatasourceProviderBuildIt
 import io.quarkus.datasource.deployment.spi.DevServicesDatasourceResultBuildItem;
 import io.quarkus.datasource.runtime.DataSourceBuildTimeConfig;
 import io.quarkus.datasource.runtime.DataSourcesBuildTimeConfig;
-import io.quarkus.deployment.IsDockerWorking;
 import io.quarkus.deployment.IsNormal;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.CuratedApplicationShutdownBuildItem;
 import io.quarkus.deployment.builditem.DevServicesResultBuildItem;
 import io.quarkus.deployment.builditem.DevServicesResultBuildItem.RunningDevService;
+import io.quarkus.deployment.builditem.DockerStatusBuildItem;
 import io.quarkus.deployment.builditem.LaunchModeBuildItem;
 import io.quarkus.deployment.console.ConsoleInstalledBuildItem;
 import io.quarkus.deployment.console.StartupLogCompressor;
@@ -45,10 +45,9 @@ public class DevServicesDatasourceProcessor {
 
     static volatile boolean first = true;
 
-    private final IsDockerWorking isDockerWorking = new IsDockerWorking(true);
-
     @BuildStep(onlyIfNot = IsNormal.class, onlyIf = GlobalDevServicesConfig.Enabled.class)
     DevServicesDatasourceResultBuildItem launchDatabases(CurateOutcomeBuildItem curateOutcomeBuildItem,
+            DockerStatusBuildItem dockerStatusBuildItem,
             List<DefaultDataSourceDbKindBuildItem> installedDrivers,
             List<DevServicesDatasourceProviderBuildItem> devDBProviders,
             DataSourcesBuildTimeConfig dataSourceBuildTimeConfig,
@@ -127,7 +126,8 @@ public class DevServicesDatasourceProcessor {
                 !dataSourceBuildTimeConfig.namedDataSources.isEmpty(),
                 devDBProviderMap,
                 dataSourceBuildTimeConfig.defaultDataSource,
-                configHandlersByDbType, propertiesMap, launchMode.getLaunchMode(), consoleInstalledBuildItem,
+                configHandlersByDbType, propertiesMap,
+                dockerStatusBuildItem, launchMode.getLaunchMode(), consoleInstalledBuildItem,
                 loggingSetupBuildItem, globalDevServicesConfig);
         if (defaultDevService != null) {
             runningDevServices.add(defaultDevService);
@@ -137,6 +137,7 @@ public class DevServicesDatasourceProcessor {
             RunningDevService namedDevService = startDevDb(entry.getKey(), curateOutcomeBuildItem,
                     installedDrivers, true,
                     devDBProviderMap, entry.getValue(), configHandlersByDbType, propertiesMap,
+                    dockerStatusBuildItem,
                     launchMode.getLaunchMode(), consoleInstalledBuildItem, loggingSetupBuildItem, globalDevServicesConfig);
             if (namedDevService != null) {
                 runningDevServices.add(namedDevService);
@@ -187,6 +188,7 @@ public class DevServicesDatasourceProcessor {
             Map<String, DevServicesDatasourceProvider> devDBProviders, DataSourceBuildTimeConfig dataSourceBuildTimeConfig,
             Map<String, List<DevServicesDatasourceConfigurationHandlerBuildItem>> configurationHandlerBuildItems,
             Map<String, String> propertiesMap,
+            DockerStatusBuildItem dockerStatusBuildItem,
             LaunchMode launchMode, Optional<ConsoleInstalledBuildItem> consoleInstalledBuildItem,
             LoggingSetupBuildItem loggingSetupBuildItem, GlobalDevServicesConfig globalDevServicesConfig) {
         boolean explicitlyDisabled = !(dataSourceBuildTimeConfig.devservices.enabled.orElse(true));
@@ -232,7 +234,7 @@ public class DevServicesDatasourceProcessor {
         }
 
         String prettyName = dbName == null ? "the default datasource" : " datasource '" + dbName + "'";
-        if (devDbProvider.isDockerRequired() && !isDockerWorking.getAsBoolean()) {
+        if (devDbProvider.isDockerRequired() && !dockerStatusBuildItem.isDockerAvailable()) {
             String message = "Please configure the datasource URL for "
                     + prettyName
                     + " or ensure the Docker daemon is up and running.";

extensions/devservices/deployment/src/main/java/io/quarkus/devservices/deployment/DevServicesProcessor.java
@@ -19,12 +19,12 @@ import com.github.dockerjava.api.model.ContainerNetwork;
 import com.github.dockerjava.api.model.ContainerNetworkSettings;
 
 import io.quarkus.deployment.IsDevelopment;
-import io.quarkus.deployment.IsDockerWorking;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.ConsoleCommandBuildItem;
 import io.quarkus.deployment.builditem.DevServicesLauncherConfigResultBuildItem;
 import io.quarkus.deployment.builditem.DevServicesResultBuildItem;
+import io.quarkus.deployment.builditem.DockerStatusBuildItem;
 import io.quarkus.deployment.builditem.LaunchModeBuildItem;
 import io.quarkus.deployment.console.ConsoleCommand;
 import io.quarkus.deployment.console.ConsoleStateManager;
@@ -36,20 +36,19 @@ public class DevServicesProcessor {
 
     private static final String EXEC_FORMAT = "docker exec -it %s /bin/bash";
 
-    private final IsDockerWorking isDockerWorking = new IsDockerWorking(true);
-
     static volatile ConsoleStateManager.ConsoleContext context;
     static volatile boolean logForwardEnabled = false;
     static Map<String, ContainerLogForwarder> containerLogForwarders = new HashMap<>();
 
     @BuildStep(onlyIf = { IsDevelopment.class })
     public List<DevServiceDescriptionBuildItem> config(
+            DockerStatusBuildItem dockerStatusBuildItem,
             BuildProducer<ConsoleCommandBuildItem> commandBuildItemBuildProducer,
             LaunchModeBuildItem launchModeBuildItem,
             Optional<DevServicesLauncherConfigResultBuildItem> devServicesLauncherConfig,
             List<DevServicesResultBuildItem> devServicesResults) {
-        List<DevServiceDescriptionBuildItem> serviceDescriptions = buildServiceDescriptions(devServicesResults,
-                devServicesLauncherConfig);
+        List<DevServiceDescriptionBuildItem> serviceDescriptions = buildServiceDescriptions(
+                dockerStatusBuildItem, devServicesResults, devServicesLauncherConfig);
 
         for (DevServiceDescriptionBuildItem devService : serviceDescriptions) {
             if (devService.hasContainerInfo()) {
@@ -72,8 +71,8 @@ public class DevServicesProcessor {
         }
         context.reset(
                 new ConsoleCommand('c', "Show dev services containers", null, () -> {
-                    List<DevServiceDescriptionBuildItem> descriptions = buildServiceDescriptions(devServicesResults,
-                            devServicesLauncherConfig);
+                    List<DevServiceDescriptionBuildItem> descriptions = buildServiceDescriptions(
+                            dockerStatusBuildItem, devServicesResults, devServicesLauncherConfig);
                     StringBuilder builder = new StringBuilder();
                     builder.append("\n\n")
                             .append(RED + "==" + RESET + " " + UNDERLINE + "Dev Services" + NO_UNDERLINE)
@@ -91,14 +90,16 @@ public class DevServicesProcessor {
         return serviceDescriptions;
     }
 
-    private List<DevServiceDescriptionBuildItem> buildServiceDescriptions(List<DevServicesResultBuildItem> devServicesResults,
+    private List<DevServiceDescriptionBuildItem> buildServiceDescriptions(
+            DockerStatusBuildItem dockerStatusBuildItem,
+            List<DevServicesResultBuildItem> devServicesResults,
             Optional<DevServicesLauncherConfigResultBuildItem> devServicesLauncherConfig) {
         // Fetch container infos
         Set<String> containerIds = devServicesResults.stream()
                 .map(DevServicesResultBuildItem::getContainerId)
                 .filter(Objects::nonNull)
                 .collect(Collectors.toSet());
-        Map<String, Container> containerInfos = fetchContainerInfos(containerIds);
+        Map<String, Container> containerInfos = fetchContainerInfos(dockerStatusBuildItem, containerIds);
         // Build descriptions
         Set<String> configKeysFromDevServices = new HashSet<>();
         List<DevServiceDescriptionBuildItem> descriptions = new ArrayList<>();
@@ -121,8 +122,9 @@ public class DevServicesProcessor {
         return descriptions;
     }
 
-    private Map<String, Container> fetchContainerInfos(Set<String> containerIds) {
-        if (containerIds.isEmpty() || !isDockerWorking.getAsBoolean()) {
+    private Map<String, Container> fetchContainerInfos(DockerStatusBuildItem dockerStatusBuildItem,
+            Set<String> containerIds) {
+        if (containerIds.isEmpty() || !dockerStatusBuildItem.isDockerAvailable()) {
             return Collections.emptyMap();
         }
         return DockerClientFactory.lazyClient().listContainersCmd()

extensions/elasticsearch-rest-client-common/deployment/src/main/java/io/quarkus/elasticsearch/restclient/common/deployment/DevServicesElasticsearchProcessor.java
@@ -16,12 +16,12 @@ import org.testcontainers.utility.DockerImageName;
 
 import io.quarkus.builder.BuildException;
 import io.quarkus.deployment.Feature;
-import io.quarkus.deployment.IsDockerWorking;
 import io.quarkus.deployment.IsNormal;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.CuratedApplicationShutdownBuildItem;
 import io.quarkus.deployment.builditem.DevServicesResultBuildItem;
 import io.quarkus.deployment.builditem.DevServicesSharedNetworkBuildItem;
+import io.quarkus.deployment.builditem.DockerStatusBuildItem;
 import io.quarkus.deployment.builditem.LaunchModeBuildItem;
 import io.quarkus.deployment.console.ConsoleInstalledBuildItem;
 import io.quarkus.deployment.console.StartupLogCompressor;
@@ -52,10 +52,9 @@ public class DevServicesElasticsearchProcessor {
     static volatile ElasticsearchDevServicesBuildTimeConfig cfg;
     static volatile boolean first = true;
 
-    private final IsDockerWorking isDockerWorking = new IsDockerWorking(true);
-
     @BuildStep(onlyIfNot = IsNormal.class, onlyIf = GlobalDevServicesConfig.Enabled.class)
     public DevServicesResultBuildItem startElasticsearchDevService(
+            DockerStatusBuildItem dockerStatusBuildItem,
             LaunchModeBuildItem launchMode,
             ElasticsearchDevServicesBuildTimeConfig configuration,
             List<DevServicesSharedNetworkBuildItem> devServicesSharedNetworkBuildItem,
@@ -86,7 +85,7 @@ public class DevServicesElasticsearchProcessor {
                 (launchMode.isTest() ? "(test) " : "") + "Elasticsearch Dev Services Starting:",
                 consoleInstalledBuildItem, loggingSetupBuildItem);
         try {
-            devService = startElasticsearch(configuration, buildItemsConfig, launchMode,
+            devService = startElasticsearch(dockerStatusBuildItem, configuration, buildItemsConfig, launchMode,
                     !devServicesSharedNetworkBuildItem.isEmpty(),
                     devServicesConfig.timeout);
             if (devService == null) {
@@ -145,7 +144,9 @@ public class DevServicesElasticsearchProcessor {
         }
     }
 
-    private DevServicesResultBuildItem.RunningDevService startElasticsearch(ElasticsearchDevServicesBuildTimeConfig config,
+    private DevServicesResultBuildItem.RunningDevService startElasticsearch(
+            DockerStatusBuildItem dockerStatusBuildItem,
+            ElasticsearchDevServicesBuildTimeConfig config,
             DevservicesElasticsearchBuildItemsConfiguration buildItemConfig,
             LaunchModeBuildItem launchMode, boolean useSharedNetwork, Optional<Duration> timeout) throws BuildException {
         if (!config.enabled.orElse(true)) {
@@ -162,7 +163,7 @@ public class DevServicesElasticsearchProcessor {
             }
         }
 
-        if (!isDockerWorking.getAsBoolean()) {
+        if (!dockerStatusBuildItem.isDockerAvailable()) {
             log.warnf("Docker isn't working, please configure the Elasticsearch hosts property (%s).",
                     displayProperties(buildItemConfig.hostsConfigProperties));
             return null;

commit 0ca82fdb03ce68e1371e5af05b97c9696f83b51a

Fix container build logging
In Quarkus 2.10.CR1, the container build (e.g. Docker) performed by Quarkus does not log any output anymore. This appears to be caused by the refactoring in commit 418d986f7ea21f93aef3490921f436612bcfe141, as the `Runnable#run()` method is never called in `ExecUtil#exec()` on the `Runnable` returned by `outputFilterFunction`.

core/deployment/src/main/java/io/quarkus/deployment/util/ExecUtil.java
@@ -163,7 +163,7 @@ public class ExecUtil {
             String... args) {
         try {
             Process process = startProcess(directory, command, args);
-            outputFilterFunction.apply(process.getInputStream());
+            outputFilterFunction.apply(process.getInputStream()).run();
             process.waitFor();
             return process.exitValue() == 0;
         } catch (InterruptedException e) {
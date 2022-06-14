a4c3bf2da403ee2eadde54ab3239781630dc4d25
Marc Wrobel <marc.wrobel@gmail.com>
2022-06-14 14:16:46 +0000

    Fix typos in extensions/hibernate-orm

--- extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/FastBootHibernatePersistenceProvider.java
+++ extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/FastBootHibernatePersistenceProvider.java
@@ -154,13 +154,13 @@ public final class FastBootHibernatePersistenceProvider implements PersistencePr
             final boolean matches = persistenceUnitName == null
                     || persistenceUnit.getName().equals(persistenceUnitName);
             if (!matches) {
-                log.debugf("Excluding from consideration '%s' due to name mis-match", persistenceUnit.getName());
+                log.debugf("Excluding from consideration '%s' due to name mismatch", persistenceUnit.getName());
                 continue;
             }
 
             // See if we (Hibernate) are the persistence provider
             if (!isProvider(persistenceUnit)) {
-                log.debug("Excluding from consideration due to provider mis-match");
+                log.debug("Excluding from consideration due to provider mismatch");
                 continue;
             }
 

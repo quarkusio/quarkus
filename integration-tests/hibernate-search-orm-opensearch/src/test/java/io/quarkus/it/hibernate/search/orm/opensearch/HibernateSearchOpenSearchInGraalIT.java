67a7ca55d646a7d7bd6c4492c8ae8639c80d05e4
Yoann Rodière <yoann@hibernate.org>
2022-06-15 11:56:52 +0200

    Use a more consistent package structure for Hibernate Search integration tests

---
+++ integration-tests/hibernate-search-orm-opensearch/src/test/java/io/quarkus/it/hibernate/search/orm/opensearch/HibernateSearchOpenSearchInGraalIT.java
@@ -0,0 +1,8 @@
+package io.quarkus.it.hibernate.search.orm.opensearch;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HibernateSearchOpenSearchInGraalIT extends HibernateSearchOpenSearchTest {
+
+}

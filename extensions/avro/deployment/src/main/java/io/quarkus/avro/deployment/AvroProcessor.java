cebda9ed8292b4e603adcdc2459f62b6add5deed
Clement Escoffier <clement@apache.org>
2022-06-22 08:23:32 +0200

    Allows Avro Specific Record classes (generated from Avro schema) to be serialized using Jackson
    They cannot be serialized by default. This commit registers serializers to make them serializable.
    It particularly convenient when you receive an Avro record (from Kafka) and want to returns it from a HTTP endpoint.

--- extensions/avro/deployment/src/main/java/io/quarkus/avro/deployment/AvroProcessor.java
+++ extensions/avro/deployment/src/main/java/io/quarkus/avro/deployment/AvroProcessor.java
@@ -3,11 +3,14 @@ package io.quarkus.avro.deployment;
 import java.util.Collection;
 
 import org.apache.avro.specific.AvroGenerated;
+import org.apache.avro.specific.SpecificRecordBase;
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.AnnotationTarget;
+import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
 
 import io.quarkus.avro.runtime.AvroRecorder;
+import io.quarkus.avro.runtime.jackson.SpecificRecordBaseSerializer;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
@@ -17,6 +20,7 @@ import io.quarkus.deployment.builditem.LaunchModeBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageConfigBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageSystemPropertyBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.quarkus.jackson.spi.JacksonModuleBuildItem;
 
 public class AvroProcessor {
 
@@ -48,5 +52,22 @@ public class AvroProcessor {
         builder.addRuntimeInitializedClass("org.apache.avro.reflect.ReflectData");
         conf.produce(builder.build());
         sys.produce(new NativeImageSystemPropertyBuildItem("avro.disable.unsafe", "true"));
+    }
+
+    @BuildStep
+    void registerJacksonSerializer(CombinedIndexBuildItem indexBuildItem, BuildProducer<JacksonModuleBuildItem> producer) {
+        Collection<AnnotationInstance> annotations = indexBuildItem.getIndex()
+                .getAnnotations(DotName.createSimple(AvroGenerated.class.getName()));
+        JacksonModuleBuildItem.Builder builder = new JacksonModuleBuildItem.Builder("AvroSpecificRecordModule");
+        for (AnnotationInstance annotation : annotations) {
+            if (annotation.target().kind() == AnnotationTarget.Kind.CLASS) {
+                ClassInfo classInfo = annotation.target().asClass();
+                String className = classInfo.name().toString();
+                if (classInfo.superName().equals(DotName.createSimple(SpecificRecordBase.class.getName()))) {
+                    builder.addSerializer(SpecificRecordBaseSerializer.class.getName(), className);
+                }
+            }
+        }
+        producer.produce(builder.build());
     }
 }

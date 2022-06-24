207504610c33f17b34a543576e78b1dc356223f4
Roberto Cortez <radcortez@yahoo.com>
2022-06-24 20:57:21 +0100

    Propagate OpenTelemetry context to gRPC services

--- extensions/opentelemetry/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/grpc/GrpcTracingClientInterceptor.java
+++ extensions/opentelemetry/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/grpc/GrpcTracingClientInterceptor.java
@@ -49,22 +49,16 @@ public class GrpcTracingClientInterceptor implements ClientInterceptor {
 
         GrpcRequest grpcRequest = GrpcRequest.client(method);
         Context parentContext = Context.current();
-        Context spanContext = null;
-        Scope scope = null;
         boolean shouldStart = instrumenter.shouldStart(parentContext, grpcRequest);
         if (shouldStart) {
-            spanContext = instrumenter.start(parentContext, grpcRequest);
-            scope = spanContext.makeCurrent();
-        }
-
-        try {
-            ClientCall<ReqT, RespT> clientCall = next.newCall(method, callOptions);
-            return new TracingClientCall<>(clientCall, spanContext, grpcRequest);
-        } finally {
-            if (scope != null) {
-                scope.close();
+            Context spanContext = instrumenter.start(parentContext, grpcRequest);
+            try (Scope ignored = spanContext.makeCurrent()) {
+                ClientCall<ReqT, RespT> clientCall = next.newCall(method, callOptions);
+                return new TracingClientCall<>(clientCall, spanContext, grpcRequest);
             }
         }
+
+        return next.newCall(method, callOptions);
     }
 
     private enum GrpcTextMapSetter implements TextMapSetter<GrpcRequest> {

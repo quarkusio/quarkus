8f2f6335b124861bc069a89e77f46f679af0d74f
Sergey Beryozkin <sberyozkin@gmail.com>
2022-06-24 18:59:53 +0100

    Integrate Vertx HTTP with CredentialsProvider

---
+++ integration-tests/bouncycastle-jsse/src/main/java/io/quarkus/it/bouncycastle/SecretProvider.java
@@ -0,0 +1,23 @@
+package io.quarkus.it.bouncycastle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import io.quarkus.arc.Unremovable;
+import io.quarkus.credentials.CredentialsProvider;
+
+@ApplicationScoped
+@Unremovable
+public class SecretProvider implements CredentialsProvider {
+
+    @Override
+    public Map<String, String> getCredentials(String credentialsProviderName) {
+        Map<String, String> creds = new HashMap<>();
+        creds.put("keystore-password", "password");
+        creds.put("truststore-password", "password");
+        return creds;
+    }
+
+}

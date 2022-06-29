f61c64a40b52cef6063994465e4f21f998fe6240
Roberto Cortez <radcortez@yahoo.com>
2022-06-29 18:21:42 +0100

    OpenTelemetry DevServices Datasource JDBC url for integration tests

--- extensions/opentelemetry/opentelemetry/deployment/src/main/java/io/quarkus/opentelemetry/deployment/dev/DevServicesOpenTelemetryProcessor.java
+++ extensions/opentelemetry/opentelemetry/deployment/src/main/java/io/quarkus/opentelemetry/deployment/dev/DevServicesOpenTelemetryProcessor.java
@@ -1,11 +1,8 @@
 package io.quarkus.opentelemetry.deployment.dev;
 
-import static io.quarkus.opentelemetry.runtime.dev.OpenTelemetryDevServicesConfigBuilder.OPENTELEMETRY_DEVSERVICES_CONFIG;
-
-import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Properties;
 
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.ConfigValue;
@@ -14,22 +11,19 @@ import io.quarkus.datasource.deployment.spi.DevServicesDatasourceResultBuildItem
 import io.quarkus.deployment.IsNormal;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.GeneratedResourceBuildItem;
-import io.quarkus.deployment.builditem.RunTimeConfigBuilderBuildItem;
+import io.quarkus.deployment.builditem.DevServicesAdditionalConfigBuildItem;
 import io.quarkus.deployment.dev.devservices.GlobalDevServicesConfig;
 import io.quarkus.opentelemetry.deployment.OpenTelemetryEnabled;
-import io.quarkus.opentelemetry.runtime.dev.OpenTelemetryDevServicesConfigBuilder;
 
 public class DevServicesOpenTelemetryProcessor {
 
     @BuildStep(onlyIfNot = IsNormal.class, onlyIf = { OpenTelemetryEnabled.class, GlobalDevServicesConfig.Enabled.class })
     void devServicesDatasources(
             Optional<DevServicesDatasourceResultBuildItem> devServicesDatasources,
-            BuildProducer<GeneratedResourceBuildItem> generatedResource,
-            BuildProducer<RunTimeConfigBuilderBuildItem> runtimeConfigBuilder) throws Exception {
+            BuildProducer<DevServicesAdditionalConfigBuildItem> devServicesAdditionalConfig) {
 
         if (devServicesDatasources.isPresent()) {
-            Properties properties = new Properties();
+            Map<String, String> properties = new HashMap<>();
             DevServicesDatasourceResultBuildItem.DbResult defaultDatasource = devServicesDatasources.get()
                     .getDefaultDatasource();
             if (defaultDatasource != null) {
@@ -47,28 +41,20 @@ public class DevServicesOpenTelemetryProcessor {
             }
 
             // if we find a dev service datasource and the OTel driver, we rewrite the jdbc url to add the otel prefix
-            for (Map.Entry<Object, Object> property : properties.entrySet()) {
-                String key = (String) property.getKey();
-                String value = (String) property.getValue();
+            for (Map.Entry<String, String> property : properties.entrySet()) {
+                String key = property.getKey();
+                String value = property.getValue();
                 if (key.endsWith(".url") && value.startsWith("jdbc:")) {
                     String driverKey = key.substring(0, key.length() - 4) + ".driver";
                     ConfigValue driverValue = ConfigProvider.getConfig().getConfigValue(driverKey);
                     if (driverValue.getValue() != null
                             && driverValue.getValue().equals("io.opentelemetry.instrumentation.jdbc.OpenTelemetryDriver")) {
-                        properties.put(key, value.replaceFirst("jdbc:", "jdbc:otel:"));
+                        devServicesAdditionalConfig.produce(new DevServicesAdditionalConfigBuildItem(key, key,
+                                value.replaceFirst("jdbc:", "jdbc:otel:"), () -> {
+                                }));
                     }
                 }
             }
-
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            properties.store(out, null);
-
-            // write the config with a slightly higher priority then runtime defaults
-            // see io.quarkus.deployment.steps.ConfigGenerationBuildStep#runtimeDefaultsConfig
-            generatedResource
-                    .produce(new GeneratedResourceBuildItem(OPENTELEMETRY_DEVSERVICES_CONFIG, out.toByteArray()));
-            runtimeConfigBuilder
-                    .produce(new RunTimeConfigBuilderBuildItem(OpenTelemetryDevServicesConfigBuilder.class.getName()));
         }
     }
 }

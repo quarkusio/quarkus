21306dbabe2e77bd99c5f71b440d9699e24d9c65
Bill Burke <bburke@redhat.com>
2022-06-29 16:38:06 -0400

    Set remote address correctly

--- extensions/amazon-lambda-http/runtime/src/main/java/io/quarkus/amazon/lambda/http/LambdaHttpHandler.java
+++ extensions/amazon-lambda-http/runtime/src/main/java/io/quarkus/amazon/lambda/http/LambdaHttpHandler.java
@@ -4,7 +4,6 @@ import static java.util.Optional.ofNullable;
 
 import java.io.ByteArrayOutputStream;
 import java.net.InetSocketAddress;
-import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.WritableByteChannel;
 import java.nio.charset.StandardCharsets;
@@ -36,7 +35,6 @@ import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.LastHttpContent;
 import io.netty.util.ReferenceCountUtil;
-import io.quarkus.amazon.lambda.runtime.AmazonLambdaContext;
 import io.quarkus.netty.runtime.virtual.VirtualClientConnection;
 import io.quarkus.netty.runtime.virtual.VirtualResponseHandler;
 import io.quarkus.vertx.http.runtime.QuarkusHttpHeaders;
@@ -67,7 +65,7 @@ public class LambdaHttpHandler implements RequestHandler<APIGatewayV2HTTPEvent, 
         }
 
         try {
-            return nettyDispatch(clientAddress, request, (AmazonLambdaContext) context);
+            return nettyDispatch(clientAddress, request, context);
         } catch (Exception e) {
             log.error("Request Failure", e);
             APIGatewayV2HTTPResponse res = new APIGatewayV2HTTPResponse();
@@ -168,7 +166,7 @@ public class LambdaHttpHandler implements RequestHandler<APIGatewayV2HTTPEvent, 
     }
 
     private APIGatewayV2HTTPResponse nettyDispatch(InetSocketAddress clientAddress, APIGatewayV2HTTPEvent request,
-            AmazonLambdaContext context)
+            Context context)
             throws Exception {
         QuarkusHttpHeaders quarkusHeaders = new QuarkusHttpHeaders();
         quarkusHeaders.setContextObject(Context.class, context);
@@ -223,12 +221,19 @@ public class LambdaHttpHandler implements RequestHandler<APIGatewayV2HTTPEvent, 
         NettyResponseHandler handler = new NettyResponseHandler(request);
         VirtualClientConnection connection = VirtualClientConnection.connect(handler, VertxHttpRecorder.VIRTUAL_HTTP,
                 clientAddress);
-        if (connection.peer().remoteAddress().equals(VertxHttpRecorder.VIRTUAL_HTTP)) {
-            URL requestURL = context.getRequestURL();
+        if (request.getRequestContext() != null
+                && request.getRequestContext().getHttp() != null
+                && request.getRequestContext().getHttp().getSourceIp() != null
+                && request.getRequestContext().getHttp().getSourceIp().length() > 0) {
+            int port = 443; // todo, may be bad to assume 443?
+            if (request.getHeaders() != null &&
+                    request.getHeaders().get("X-Forwarded-Port") != null) {
+                port = Integer.parseInt(request.getHeaders().get("X-Forwarded-Port"));
+            }
             connection.peer().attr(ConnectionBase.REMOTE_ADDRESS_OVERRIDE).set(
-                    SocketAddress.inetSocketAddress(requestURL.getPort(), requestURL.getHost()));
+                    SocketAddress.inetSocketAddress(port,
+                            request.getRequestContext().getHttp().getSourceIp()));
         }
-
         connection.sendMessage(nettyRequest);
         connection.sendMessage(requestContent);
         try {

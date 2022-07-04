13abf974f02a636b762ca95d658870822c561cb5
Davide D'Alto <davide@hibernate.org>
2022-07-04 15:30:47 +0100

    [#23739] Support @ActivateRequestContext with Uni

--- independent-projects/arc/runtime/src/main/java/io/quarkus/arc/impl/ActivateRequestContextInterceptor.java
+++ independent-projects/arc/runtime/src/main/java/io/quarkus/arc/impl/ActivateRequestContextInterceptor.java
@@ -2,6 +2,7 @@ package io.quarkus.arc.impl;
 
 import io.quarkus.arc.Arc;
 import io.quarkus.arc.ManagedContext;
+import io.smallrye.mutiny.Uni;
 import javax.annotation.Priority;
 import javax.enterprise.context.control.ActivateRequestContext;
 import javax.interceptor.AroundInvoke;
@@ -15,17 +16,46 @@ public class ActivateRequestContextInterceptor {
 
     @AroundInvoke
     Object aroundInvoke(InvocationContext ctx) throws Exception {
-        ManagedContext requestContext = Arc.container().requestContext();
-        if (requestContext.isActive()) {
-            return ctx.proceed();
-        } else {
-            try {
-                requestContext.activate();
-                return ctx.proceed();
-            } finally {
-                requestContext.terminate();
+        if (ctx.getMethod().getReturnType().equals(Uni.class)) {
+            return invokeUni(ctx);
+        }
+
+        return invoke(ctx);
+    }
+
+    private Uni<?> invokeUni(InvocationContext ctx) {
+        return Uni.createFrom().deferred(() -> {
+            ManagedContext requestContext = Arc.container().requestContext();
+            if (requestContext.isActive()) {
+                return proceedWithUni(ctx);
             }
+
+            return Uni.createFrom().deferred(() -> {
+                requestContext.activate();
+                return proceedWithUni(ctx);
+            }).eventually(requestContext::terminate);
+        });
+    }
+
+    private Uni<?> proceedWithUni(InvocationContext ctx) {
+        try {
+            return (Uni<?>) ctx.proceed();
+        } catch (Throwable t) {
+            return Uni.createFrom().failure(t);
         }
     }
 
+    private Object invoke(InvocationContext ctx) throws Exception {
+        ManagedContext requestContext = Arc.container().requestContext();
+        if (requestContext.isActive()) {
+            return ctx.proceed();
+        }
+
+        try {
+            requestContext.activate();
+            return ctx.proceed();
+        } finally {
+            requestContext.terminate();
+        }
+    }
 }

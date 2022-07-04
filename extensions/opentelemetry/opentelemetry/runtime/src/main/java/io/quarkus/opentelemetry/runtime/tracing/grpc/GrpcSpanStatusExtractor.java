19cec86243ce2e30f8f9b79570dee86acc5a9785
Guillaume Smet <guillaume.smet@gmail.com>
2022-07-04 11:12:15 +0200

    Remove JSR 305 annotations
    They are causing issues for the Jakarta migration.

--- extensions/opentelemetry/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/grpc/GrpcSpanStatusExtractor.java
+++ extensions/opentelemetry/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/grpc/GrpcSpanStatusExtractor.java
@@ -1,7 +1,5 @@
 package io.quarkus.opentelemetry.runtime.tracing.grpc;
 
-import javax.annotation.Nullable;
-
 import io.grpc.Status;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.instrumentation.api.instrumenter.SpanStatusBuilder;
@@ -12,8 +10,8 @@ class GrpcSpanStatusExtractor implements SpanStatusExtractor<GrpcRequest, Status
     public void extract(
             final SpanStatusBuilder spanStatusBuilder,
             final GrpcRequest grpcRequest,
-            @Nullable final Status status,
-            @Nullable final Throwable error) {
+            final Status status,
+            final Throwable error) {
         if (status != null && status.isOk()) {
             spanStatusBuilder.setStatus(StatusCode.UNSET);
         } else if (error != null) {

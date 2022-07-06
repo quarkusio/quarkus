248d9f6412472a2c569b5ce892f970bea90191f5
Davide D'Alto <davide@hibernate.org>
2022-07-06 14:36:58 +0100

    [#23739] Add support for @ActivateRequestContext with CompletionStage

--- independent-projects/arc/runtime/src/main/java/io/quarkus/arc/impl/ActivateRequestContextInterceptor.java
+++ independent-projects/arc/runtime/src/main/java/io/quarkus/arc/impl/ActivateRequestContextInterceptor.java
@@ -3,6 +3,8 @@ package io.quarkus.arc.impl;
 import io.quarkus.arc.Arc;
 import io.quarkus.arc.ManagedContext;
 import io.smallrye.mutiny.Uni;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import javax.annotation.Priority;
 import javax.enterprise.context.control.ActivateRequestContext;
 import javax.interceptor.AroundInvoke;
@@ -20,7 +22,39 @@ public class ActivateRequestContextInterceptor {
             return invokeUni(ctx);
         }
 
+        if (ctx.getMethod().getReturnType().equals(CompletionStage.class)) {
+            return invokeStage(ctx);
+        }
+
         return invoke(ctx);
+    }
+
+    private CompletionStage<?> invokeStage(InvocationContext ctx) {
+        ManagedContext requestContext = Arc.container().requestContext();
+        if (requestContext.isActive()) {
+            return proceedWithStage(ctx);
+        }
+
+        return activate(requestContext)
+                .thenCompose(v -> proceedWithStage(ctx))
+                .whenComplete((r, t) -> requestContext.terminate());
+    }
+
+    private static CompletionStage<ManagedContext> activate(ManagedContext requestContext) {
+        try {
+            requestContext.activate();
+            return CompletableFuture.completedStage(requestContext);
+        } catch (Throwable t) {
+            return CompletableFuture.failedStage(t);
+        }
+    }
+
+    private CompletionStage<?> proceedWithStage(InvocationContext ctx) {
+        try {
+            return (CompletionStage<?>) ctx.proceed();
+        } catch (Throwable t) {
+            return CompletableFuture.failedStage(t);
+        }
     }
 
     private Uni<?> invokeUni(InvocationContext ctx) {

cada7db12e95d1dd4c495faaf6de7b227d1a499b
Ozan Gunalp <ozangunalp@gmail.com>
2022-07-11 18:48:21 +0300

    Bump Smallrye Reactive Messaging to 3.19.1
    Update Kafka documentation
    
    Update HR RM test with activate request context
    
    Added HR ORM RM IT
    
    (cherry picked from commit c086c038229ea161933d8626665199c3df7923c5)

---
+++ integration-tests/reactive-messaging-hibernate-orm/src/test/java/io/quarkus/it/kafka/KafkaConnectorTest.java
@@ -0,0 +1,60 @@
+package io.quarkus.it.kafka;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.is;
+
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.kafka.InjectKafkaCompanion;
+import io.quarkus.test.kafka.KafkaCompanionResource;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
+import io.smallrye.reactive.messaging.kafka.companion.KafkaCompanion;
+
+@QuarkusTest
+@QuarkusTestResource(KafkaCompanionResource.class)
+public class KafkaConnectorTest {
+
+    protected static final TypeRef<List<Fruit>> TYPE_REF = new TypeRef<List<Fruit>>() {
+    };
+
+    @InjectKafkaCompanion
+    KafkaCompanion companion;
+
+    @Test
+    public void testFruits() {
+        given().body(new Fruit("apple")).contentType(ContentType.JSON).when().post("/kafka/fruits").then()
+                .assertThat().statusCode(is(Response.Status.NO_CONTENT.getStatusCode()));
+        given().body(new Fruit("banana")).contentType(ContentType.JSON).when().post("/kafka/fruits").then()
+                .assertThat().statusCode(is(Response.Status.NO_CONTENT.getStatusCode()));
+        given().body(new Fruit("peach")).contentType(ContentType.JSON).when().post("/kafka/fruits").then()
+                .assertThat().statusCode(is(Response.Status.NO_CONTENT.getStatusCode()));
+        given().body(new Fruit("orange")).contentType(ContentType.JSON).when().post("/kafka/fruits").then()
+                .assertThat().statusCode(is(Response.Status.NO_CONTENT.getStatusCode()));
+        given().body(new Fruit("cherry")).contentType(ContentType.JSON).when().post("/kafka/fruits").then()
+                .assertThat().statusCode(is(Response.Status.NO_CONTENT.getStatusCode()));
+        given().body(new Fruit("pear")).contentType(ContentType.JSON).when().post("/kafka/fruits").then()
+                .assertThat().statusCode(is(Response.Status.NO_CONTENT.getStatusCode()));
+
+        await().untilAsserted(() -> Assertions.assertEquals(6, get("/kafka/fruits").as(TYPE_REF).size()));
+
+        for (ConsumerRecord<String, String> record : companion
+                .consumeWithDeserializers(new StringDeserializer(), new StringDeserializer())
+                .fromTopics("fruits-persisted", 6)
+                .awaitCompletion()) {
+            System.out.println(record);
+        }
+    }
+
+}

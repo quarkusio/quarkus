da4818126682e07b4b26fb655149b4499009369c
Georgios Andrianakis <geoand@gmail.com>
2022-07-14 08:25:48 +0300

    Add Javadoc to command-mode testing API
    (cherry picked from commit aa70206da5cf889b5d5ac0b1f4c9afa2f18834c7)

--- test-framework/junit5/src/main/java/io/quarkus/test/junit/main/QuarkusMainLauncher.java
+++ test-framework/junit5/src/main/java/io/quarkus/test/junit/main/QuarkusMainLauncher.java
@@ -1,5 +1,11 @@
 package io.quarkus.test.junit.main;
 
+/**
+ * The annotation is meant to be used on test methods only and it allows the launching a command line application with
+ * arbitrary parameters.
+ *
+ * See also {@link LaunchResult} and {@link Launch}
+ */
 public interface QuarkusMainLauncher {
 
     LaunchResult launch(String... args);

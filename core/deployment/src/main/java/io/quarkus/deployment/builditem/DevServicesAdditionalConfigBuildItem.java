edec33cc5924cf2c35a0d027ebfcd812c88e9254
Yoann Rodière <yoann@hibernate.org>
2022-07-19 12:13:44 +0200

    Consistently consider quoted AND non-quoted versions of quarkus.datasource configuration properties
    We were already taking both versions into account in
    DevServicesDatasourceConfigurationHandlerBuildItem, but not in every
    other place.
    
    (cherry picked from commit 52925f72e93d67cd0fa5f78ca5119ce1bd98f0a0)

--- core/deployment/src/main/java/io/quarkus/deployment/builditem/DevServicesAdditionalConfigBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/builditem/DevServicesAdditionalConfigBuildItem.java
@@ -1,5 +1,8 @@
 package io.quarkus.deployment.builditem;
 
+import java.util.Collection;
+import java.util.List;
+
 import io.quarkus.builder.item.MultiBuildItem;
 
 /**
@@ -12,21 +15,40 @@ import io.quarkus.builder.item.MultiBuildItem;
  */
 public final class DevServicesAdditionalConfigBuildItem extends MultiBuildItem {
 
-    private final String triggeringKey;
+    private final Collection<String> triggeringKeys;
     private final String key;
     private final String value;
     private final Runnable callbackWhenEnabled;
 
+    /**
+     * @deprecated Call
+     *             {@link DevServicesAdditionalConfigBuildItem#DevServicesAdditionalConfigBuildItem(Collection, String, String, Runnable)}
+     *             instead.
+     */
+    @Deprecated
     public DevServicesAdditionalConfigBuildItem(String triggeringKey,
             String key, String value, Runnable callbackWhenEnabled) {
-        this.triggeringKey = triggeringKey;
+        this(List.of(triggeringKey), key, value, callbackWhenEnabled);
+    }
+
+    public DevServicesAdditionalConfigBuildItem(Collection<String> triggeringKeys,
+            String key, String value, Runnable callbackWhenEnabled) {
+        this.triggeringKeys = triggeringKeys;
         this.key = key;
         this.value = value;
         this.callbackWhenEnabled = callbackWhenEnabled;
     }
 
+    /**
+     * @deprecated Call {@link #getTriggeringKeys()} instead.
+     */
+    @Deprecated
     public String getTriggeringKey() {
-        return triggeringKey;
+        return getTriggeringKeys().iterator().next();
+    }
+
+    public Collection<String> getTriggeringKeys() {
+        return triggeringKeys;
     }
 
     public String getKey() {

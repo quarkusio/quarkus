3d053612140e171615e42c60fa110e7b64f6098e
Georgios Andrianakis <geoand@gmail.com>
2022-08-02 12:44:25 +0300

    Actually use dev-services in mongodb-devservices module

--- integration-tests/mongodb-devservices/src/test/java/io/quarkus/it/mongodb/BookResourceTest.java
+++ integration-tests/mongodb-devservices/src/test/java/io/quarkus/it/mongodb/BookResourceTest.java
@@ -10,18 +10,12 @@ import javax.json.bind.Jsonb;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.OS;
 
 import io.quarkus.mongodb.health.MongoHealthCheck;
-import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.mongodb.MongoTestResource;
 import io.restassured.RestAssured;
 
 @QuarkusTest
-@QuarkusTestResource(MongoTestResource.class)
-@DisabledOnOs(OS.WINDOWS)
 public class BookResourceTest {
     private static Jsonb jsonb;
 

b1a91f2abfcae0534ff7ac8f4dba7bd5f341f6e8
Martin Kouba <mkouba@redhat.com>
2022-08-03 13:53:32 +0200

    Fix termination of request context for non-blocking scheduled methods
    - fix the problem mentioned in
    https://github.com/quarkusio/quarkus/issues/23739#issuecomment-1203720309
    
    (cherry picked from commit 5ee7fe67424d22bb57dd9378540c18be77f72fcd)

--- extensions/scheduler/common/src/main/java/io/quarkus/scheduler/common/runtime/DefaultInvoker.java
+++ extensions/scheduler/common/src/main/java/io/quarkus/scheduler/common/runtime/DefaultInvoker.java
@@ -3,6 +3,7 @@ package io.quarkus.scheduler.common.runtime;
 import java.util.concurrent.CompletionStage;
 
 import io.quarkus.arc.Arc;
+import io.quarkus.arc.InjectableContext.ContextState;
 import io.quarkus.arc.ManagedContext;
 import io.quarkus.scheduler.ScheduledExecution;
 
@@ -14,15 +15,26 @@ public abstract class DefaultInvoker implements ScheduledInvoker {
         if (requestContext.isActive()) {
             return invokeBean(execution);
         } else {
+            // 1. Activate the context
+            // 2. Capture the state (which is basically a shared Map instance)
+            // 3. Destroy the context correctly when the returned stage completes
+            requestContext.activate();
+            final ContextState state = requestContext.getState();
             try {
-                requestContext.activate();
-                return invokeBean(execution);
-            } finally {
+                return invokeBean(execution).whenComplete((v, t) -> {
+                    requestContext.destroy(state);
+                });
+            } catch (RuntimeException e) {
+                // Just terminate the context and rethrow the exception if something goes really wrong
                 requestContext.terminate();
+                throw e;
+            } finally {
+                // Always deactivate the context
+                requestContext.deactivate();
             }
         }
     }
 
-    protected abstract CompletionStage<Void> invokeBean(ScheduledExecution execution) throws Exception;
+    protected abstract CompletionStage<Void> invokeBean(ScheduledExecution execution);
 
 }

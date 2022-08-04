a51d5be1b01f9b288f213331c4f588bd13248ae4
Michael Musgrove <mmusgrov@redhat.com>
2022-08-04 09:50:10 +0100

    [27128] narayana upgrade - remove two unnecessary classes

--- extensions/narayana-lra/runtime/src/main/java/io/quarkus/narayana/lra/runtime/NarayanaLRAProducers.java
+++ extensions/narayana-lra/runtime/src/main/java/io/quarkus/narayana/lra/runtime/NarayanaLRAProducers.java
@@ -3,7 +3,6 @@ package io.quarkus.narayana.lra.runtime;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 
-import io.narayana.lra.client.internal.proxy.ParticipantProxyResource;
 import io.narayana.lra.client.internal.proxy.nonjaxrs.LRAParticipantRegistry;
 
 @Dependent
@@ -11,10 +10,5 @@ public class NarayanaLRAProducers {
     @Produces
     public LRAParticipantRegistry lraParticipantRegistry() {
         return NarayanaLRARecorder.registry;
-    }
-
-    @Produces
-    public ParticipantProxyResource participantProxyResource() {
-        return new ParticipantProxyResource();
     }
 }

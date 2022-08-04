549433633e1de8896b7ab0f8ad4387f01c8035f4
Georgios Andrianakis <geoand@gmail.com>
2022-08-04 08:50:52 +0300

    Rename OracleReflections to OracleNativeImage
    (cherry picked from commit f20d41c9055afdfa62cb380ccdb3a5b9e6698ad9)

--- extensions/jdbc/jdbc-oracle/deployment/src/main/java/io/quarkus/jdbc/oracle/deployment/OracleNativeImage.java
+++ extensions/jdbc/jdbc-oracle/deployment/src/main/java/io/quarkus/jdbc/oracle/deployment/OracleNativeImage.java
@@ -5,15 +5,16 @@ import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 /**
- * Registers the {@code oracle.jdbc.driver.OracleDriver} so that it can be loaded
- * by reflection, as commonly expected.
- *
  * @author Sanne Grinovero <sanne@hibernate.org>
  */
-public final class OracleReflections {
+public final class OracleNativeImage {
 
+    /**
+     * Registers the {@code oracle.jdbc.driver.OracleDriver} so that it can be loaded
+     * by reflection, as commonly expected.
+     */
     @BuildStep
-    void build(BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
+    void reflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
         //Not strictly necessary when using Agroal, as it also registers
         //any JDBC driver being configured explicitly through its configuration.
         //We register it for the sake of people not using Agroal.

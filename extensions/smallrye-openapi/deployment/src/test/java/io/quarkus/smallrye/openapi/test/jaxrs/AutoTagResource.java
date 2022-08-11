e1ad34c98b4f6cc316ca36a2670e7f5e2eefabb6
Michael Edgar <michael@xlate.io>
2022-08-11 05:41:28 -0400

    Update OpenAPI auto-tag processing to account for SR 2.2.0 changes
    Save method hashes based on resource implementation classes and both the
    concrete and (potentially) abstract methods.
    
    (cherry picked from commit 1e0bb5b3397975ebb22147be7d3cf17577d0edc0)

--- extensions/smallrye-openapi/deployment/src/test/java/io/quarkus/smallrye/openapi/test/jaxrs/AutoTagResource.java
+++ extensions/smallrye-openapi/deployment/src/test/java/io/quarkus/smallrye/openapi/test/jaxrs/AutoTagResource.java
@@ -1,11 +1,20 @@
 package io.quarkus.smallrye.openapi.test.jaxrs;
 
+import java.util.List;
+
 import javax.ws.rs.Path;
 
-@Path("/address")
-public class AutoTagResource implements AbstractAutoTagResource<String> {
+@Path("/tagged")
+public class AutoTagResource extends AutoTagFetchableResource<String> {
+
     @Override
     public String getById(long id) {
         return "Disney Land, Gate " + id;
     }
+
+    @Override
+    public List<String> getAll() {
+        return null;
+    }
+
 }

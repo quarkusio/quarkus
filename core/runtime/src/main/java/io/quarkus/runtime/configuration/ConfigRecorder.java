c344879a52af997712de54c90168871d10fc3bd5
Roberto Cortez <radcortez@yahoo.com>
2022-08-29 16:51:59 +0100

    Support config expansion when validating config changes

--- core/runtime/src/main/java/io/quarkus/runtime/configuration/ConfigRecorder.java
+++ core/runtime/src/main/java/io/quarkus/runtime/configuration/ConfigRecorder.java
@@ -1,9 +1,11 @@
 package io.quarkus.runtime.configuration;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.ConfigValue;
 import org.eclipse.microprofile.config.spi.ConfigSource;
@@ -11,6 +13,8 @@ import org.jboss.logging.Logger;
 
 import io.quarkus.runtime.annotations.Recorder;
 import io.quarkus.runtime.configuration.ConfigurationRuntimeConfig.BuildTimeMismatchAtRuntime;
+import io.smallrye.config.ConfigSourceInterceptorContext;
+import io.smallrye.config.ExpressionConfigSourceInterceptor;
 import io.smallrye.config.SmallRyeConfig;
 import io.smallrye.config.SmallRyeConfigBuilder;
 
@@ -26,18 +30,51 @@ public class ConfigRecorder {
     }
 
     public void handleConfigChange(Map<String, ConfigValue> buildTimeRuntimeValues) {
+        // Create a new Config without the "BuildTime RunTime Fixed" sources to check for different values
         SmallRyeConfigBuilder configBuilder = ConfigUtils.emptyConfigBuilder();
+        // We need to disable the expression resolution, because we may be missing expressions from the "BuildTime RunTime Fixed" source
+        configBuilder.withDefaultValue(Config.PROPERTY_EXPRESSIONS_ENABLED, "false");
         for (ConfigSource configSource : ConfigProvider.getConfig().getConfigSources()) {
             if ("BuildTime RunTime Fixed".equals(configSource.getName())) {
                 continue;
             }
             configBuilder.withSources(configSource);
         }
+        // Add a new expression resolution to fall back to the current Config if we cannot expand the expression
+        configBuilder.withInterceptors(new ExpressionConfigSourceInterceptor() {
+            @Override
+            public io.smallrye.config.ConfigValue getValue(final ConfigSourceInterceptorContext context, final String name) {
+                return super.getValue(new ConfigSourceInterceptorContext() {
+                    @Override
+                    public io.smallrye.config.ConfigValue proceed(final String name) {
+                        io.smallrye.config.ConfigValue configValue = context.proceed(name);
+                        if (configValue == null) {
+                            configValue = (io.smallrye.config.ConfigValue) ConfigProvider.getConfig().getConfigValue(name);
+                            if (configValue.getValue() == null) {
+                                return null;
+                            }
+                        }
+                        return configValue;
+                    }
+
+                    @Override
+                    public Iterator<String> iterateNames() {
+                        return context.iterateNames();
+                    }
+
+                    @Override
+                    public Iterator<io.smallrye.config.ConfigValue> iterateValues() {
+                        return context.iterateValues();
+                    }
+                }, name);
+            }
+        });
         SmallRyeConfig config = configBuilder.build();
 
         List<String> mismatches = new ArrayList<>();
         for (Map.Entry<String, ConfigValue> entry : buildTimeRuntimeValues.entrySet()) {
             ConfigValue currentValue = config.getConfigValue(entry.getKey());
+            // Check for changes. Also, we only have a change if the source ordinal is higher
             if (currentValue.getValue() != null && !entry.getValue().getValue().equals(currentValue.getValue())
                     && entry.getValue().getSourceOrdinal() < currentValue.getSourceOrdinal()) {
                 mismatches.add(

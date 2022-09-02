04d70df3879ba7807a2fd3720a59dc64a21bedce
Roberto Cortez <radcortez@yahoo.com>
2022-09-02 08:52:04 +0100

    Update SmallRye Config to 2.12.0
    (cherry picked from commit 44dca6f09f9f49a857ffde28baaf95b47e56aa14)

--- extensions/arc/runtime/src/main/java/io/quarkus/arc/runtime/ConfigMappingCreator.java
+++ extensions/arc/runtime/src/main/java/io/quarkus/arc/runtime/ConfigMappingCreator.java
@@ -1,16 +1,19 @@
 package io.quarkus.arc.runtime;
 
-import static io.smallrye.config.inject.ConfigMappingInjectionBean.getPrefixFromInjectionPoint;
-
+import java.lang.annotation.Annotation;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Annotated;
 import javax.enterprise.inject.spi.InjectionPoint;
 
 import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.inject.ConfigProperties;
 
 import io.quarkus.arc.BeanCreator;
 import io.quarkus.arc.impl.InjectionPointProvider;
+import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.SmallRyeConfig;
 
 public class ConfigMappingCreator implements BeanCreator<Object> {
@@ -26,5 +29,35 @@ public class ConfigMappingCreator implements BeanCreator<Object> {
 
         SmallRyeConfig config = (SmallRyeConfig) ConfigProvider.getConfig();
         return config.getConfigMapping(interfaceType, getPrefixFromInjectionPoint(injectionPoint).orElse(prefix));
+    }
+
+    private static Optional<String> getPrefixFromInjectionPoint(final InjectionPoint injectionPoint) {
+        Annotated annotated = injectionPoint.getAnnotated();
+        if (annotated != null) {
+            ConfigMapping configMapping = annotated.getAnnotation(ConfigMapping.class);
+            if (configMapping != null) {
+                if (!configMapping.prefix().isEmpty()) {
+                    return Optional.of(configMapping.prefix());
+                }
+            }
+
+            ConfigProperties configProperties = annotated.getAnnotation(ConfigProperties.class);
+            if (configProperties != null) {
+                if (!ConfigProperties.UNCONFIGURED_PREFIX.equals(configProperties.prefix())) {
+                    return Optional.of(configProperties.prefix());
+                }
+            }
+        }
+
+        for (Annotation qualifier : injectionPoint.getQualifiers()) {
+            if (qualifier instanceof ConfigProperties) {
+                ConfigProperties configPropertiesQualifier = (ConfigProperties) qualifier;
+                if (!ConfigProperties.UNCONFIGURED_PREFIX.equals(configPropertiesQualifier.prefix())) {
+                    return Optional.of(configPropertiesQualifier.prefix());
+                }
+            }
+        }
+
+        return Optional.empty();
     }
 }

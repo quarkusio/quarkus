3afcb4007a8b219e6462fa735c3b41ff8dba5470
Georgios Andrianakis <geoand@gmail.com>
2022-10-07 18:56:42 +0300

    Update documentation to display the use of @OidcClientFilter
    (cherry picked from commit c46a9b4b5aff217551f63e550d06c4875ba01d07)

--- integration-tests/resteasy-reactive-kotlin/standard/src/main/kotlin/io/quarkus/it/resteasy/reactive/kotlin/GreetingResource.kt
+++ integration-tests/resteasy-reactive-kotlin/standard/src/main/kotlin/io/quarkus/it/resteasy/reactive/kotlin/GreetingResource.kt
@@ -2,8 +2,13 @@ package io.quarkus.it.resteasy.reactive.kotlin
 
 import org.jboss.resteasy.reactive.RestHeader
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.core.Context
 import javax.ws.rs.core.HttpHeaders
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriInfo
 
 @Path("/greeting")
 class GreetingResource(val headers: HttpHeaders) {
@@ -18,6 +23,10 @@ class GreetingResource(val headers: HttpHeaders) {
     @Path("noop")
     suspend fun noop() {
     }
+
+    @POST
+    @Path("body/{name}")
+    suspend fun body(@PathParam(value = "name") name: String, greeting: Greeting, @Context uriInfo: UriInfo) = Response.ok(greeting).build()
 }
 
 data class Greeting(val message: String)

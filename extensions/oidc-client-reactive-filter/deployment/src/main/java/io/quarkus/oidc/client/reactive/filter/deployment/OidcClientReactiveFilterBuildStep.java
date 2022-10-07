527f84ab052b8d7fc4bc23ff5a5d1ff6cd163027
Georgios Andrianakis <geoand@gmail.com>
2022-10-07 17:42:03 +0300

    Introduce a version of @OidcClientFilter for the reactive client
    Closes: #16522

--- extensions/oidc-client-reactive-filter/deployment/src/main/java/io/quarkus/oidc/client/reactive/filter/deployment/OidcClientReactiveFilterBuildStep.java
+++ extensions/oidc-client-reactive-filter/deployment/src/main/java/io/quarkus/oidc/client/reactive/filter/deployment/OidcClientReactiveFilterBuildStep.java
@@ -1,16 +1,45 @@
 package io.quarkus.oidc.client.reactive.filter.deployment;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.AnnotationValue;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.Type;
+
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.BuildSteps;
 import io.quarkus.deployment.builditem.AdditionalIndexedClassesBuildItem;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.oidc.client.deployment.OidcClientBuildStep.IsEnabled;
+import io.quarkus.oidc.client.reactive.filter.OidcClientFilter;
 import io.quarkus.oidc.client.reactive.filter.OidcClientRequestReactiveFilter;
+import io.quarkus.rest.client.reactive.deployment.DotNames;
+import io.quarkus.rest.client.reactive.deployment.RegisterProviderAnnotationInstanceBuildItem;
 
 @BuildSteps(onlyIf = IsEnabled.class)
 public class OidcClientReactiveFilterBuildStep {
+
+    private static final DotName OIDC_CLIENT_FILTER = DotName.createSimple(OidcClientFilter.class.getName());
+    private static final DotName OIDC_CLIENT_REQUEST_REACTIVE_FILTER = DotName
+            .createSimple(OidcClientRequestReactiveFilter.class.getName());
+
+    // we simply pretend that @OidcClientFilter means @RegisterProvider(OidcClientRequestReactiveFilter.class)
+    @BuildStep
+    void oidcClientFilterSupport(CombinedIndexBuildItem indexBuildItem,
+            BuildProducer<RegisterProviderAnnotationInstanceBuildItem> producer) {
+        Collection<AnnotationInstance> instances = indexBuildItem.getIndex().getAnnotations(OIDC_CLIENT_FILTER);
+        for (AnnotationInstance instance : instances) {
+            String targetClass = instance.target().asClass().name().toString();
+            producer.produce(new RegisterProviderAnnotationInstanceBuildItem(targetClass, AnnotationInstance.create(
+                    DotNames.REGISTER_PROVIDER, instance.target(), List.of(AnnotationValue.createClassValue("value",
+                            Type.create(OIDC_CLIENT_REQUEST_REACTIVE_FILTER, org.jboss.jandex.Type.Kind.CLASS))))));
+        }
+    }
 
     @BuildStep
     void registerProvider(BuildProducer<AdditionalBeanBuildItem> additionalBeans,

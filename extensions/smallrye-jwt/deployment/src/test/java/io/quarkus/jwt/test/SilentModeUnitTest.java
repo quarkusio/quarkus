5b1b49c511b39d70536b4135c80429d50e7d35bf
Sergey Beryozkin <sberyozkin@gmail.com>
2022-10-10 19:07:00 +0100

    Support a silent mode for JWTAuthMechanism
    (cherry picked from commit a12573d3d16613d463204422ae296b60d33dae58)

---
+++ extensions/smallrye-jwt/deployment/src/test/java/io/quarkus/jwt/test/SilentModeUnitTest.java
@@ -0,0 +1,32 @@
+package io.quarkus.jwt.test;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+import io.restassured.RestAssured;
+
+public class SilentModeUnitTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withApplicationRoot((jar) -> jar
+                    .addClass(DefaultGroupsEndpoint.class)
+                    .addAsResource("publicKey.pem")
+                    .addAsResource("applicationJwtFormAuth.properties", "application.properties"));
+
+    @Test
+    public void testFormChallengeWithoutAuthorizationHeader() {
+        RestAssured.given().redirects().follow(false)
+                .get("/endp/echo")
+                .then().assertThat().statusCode(302);
+    }
+
+    @Test
+    public void testJwtChallengeWithAuthorizationHeader() {
+        RestAssured.given().auth()
+                .oauth2("123")
+                .get("/endp/routingContext")
+                .then().assertThat().statusCode(401);
+    }
+}

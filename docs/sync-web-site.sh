f89a463cdbf3e445678df03d21aae0d8c847a589
Guillaume Smet <guillaume.smet@gmail.com>
2022-10-18 17:30:31 +0200

    Fix the path of documentation examples
    And make things cleaner globally.
    The assorted changes have already been merged into the website.

--- docs/sync-web-site.sh
+++ docs/sync-web-site.sh
@@ -37,10 +37,10 @@ fi
 
 if [ $BRANCH == "main" ] && [ "$QUARKUS_RELEASE" == "true" ]; then
   TARGET_GUIDES=${TARGET_DIR}/_guides
-  TARGET_CONFIG=${TARGET_DIR}/_generated-config/latest
+  TARGET_CONFIG=${TARGET_DIR}/_generated-doc/latest
 else
   TARGET_GUIDES=${TARGET_DIR}/_versions/${BRANCH}/guides
-  TARGET_CONFIG=${TARGET_DIR}/_generated-config/${BRANCH}
+  TARGET_CONFIG=${TARGET_DIR}/_generated-doc/${BRANCH}
 fi
 
 echo "Copying from target/asciidoc/sources/* to $TARGET_GUIDES"

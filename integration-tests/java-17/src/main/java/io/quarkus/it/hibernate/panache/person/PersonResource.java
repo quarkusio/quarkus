096f587c94c920fb7f1cf6ab216c4a07cc731938
Loïc Mathieu <loikeseke@gmail.com>
2022-10-18 09:25:26 +0200

    Java 17 integration test

---
+++ integration-tests/java-17/src/main/java/io/quarkus/it/hibernate/panache/person/PersonResource.java
@@ -0,0 +1,27 @@
+package io.quarkus.it.hibernate.panache.person;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/hibernate/persons")
+@Transactional
+public class PersonResource {
+    @GET
+    public List<PersonName> getPersons() {
+        return Person.findAll().project(PersonName.class).list();
+    }
+
+    @POST
+    public Response addPerson(Person person) {
+        person.persist();
+        String id = person.id.toString();
+        return Response.created(URI.create("/persons/entity/" + id)).build();
+    }
+
+}

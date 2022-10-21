1bb39f18874df9e789dc93077a34a9d684979317
Michal Vavřík <mvavrik@redhat.com>
2022-10-21 19:19:54 +0200

    Reactive Routes- go to the next handler when policy is set in properties

--- extensions/reactive-routes/runtime/src/main/java/io/quarkus/vertx/web/runtime/VertxWebRecorder.java
+++ extensions/reactive-routes/runtime/src/main/java/io/quarkus/vertx/web/runtime/VertxWebRecorder.java
@@ -121,6 +121,8 @@ public class VertxWebRecorder {
                                             }
                                         }
                                     });
+                        } else {
+                            routingContext.next();
                         }
                     });
                 }

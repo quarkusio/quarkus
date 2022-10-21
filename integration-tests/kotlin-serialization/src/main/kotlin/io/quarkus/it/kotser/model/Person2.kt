ec7ac16f69118360af9b69f38ef8904a9d56194e
Georgios Andrianakis <geoand@gmail.com>
2022-10-21 10:05:32 +0300

    Improve Kotlin Serialization for native mode
    This PR makes a few changes:
    
    * It moves the discovery of Kotlin @Serializable classes
    from the server module to the common module
    * It enhances changes the companion class discovery mechanism
    to match what Kotlin does at runtime
    * It enhances the reflection registration process to
    ensure that the metadata Kotlin needs at runtime
    is actually included in the native binary
    
    (cherry picked from commit e1f4104b2702c20dc8e3c43f7abefda3d7ff9a5e)

--- integration-tests/kotlin-serialization/src/main/kotlin/io/quarkus/it/kotser/model/Person2.kt
+++ integration-tests/kotlin-serialization/src/main/kotlin/io/quarkus/it/kotser/model/Person2.kt
@@ -3,7 +3,7 @@ package io.quarkus.it.kotser.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Person(var name: String, var defaulted: String = "hi there!") {
+data class Person2(var name: String, var defaulted: String = "hey") {
     override fun toString(): String {
         TODO("this shouldn't get called. a proper serialization should be invoked.")
     }

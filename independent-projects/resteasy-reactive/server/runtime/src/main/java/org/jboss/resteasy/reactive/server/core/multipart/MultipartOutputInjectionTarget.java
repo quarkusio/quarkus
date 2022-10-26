6967fbc1c7d46ef1342c3d332ac85ecd6f87f73c
Jose <josecarvajalhilario@gmail.com>
2022-10-26 08:28:25 +0200

    Support programmatic `multipart/form-data` responses
    With these changes, users can also manually append the parts of the form using the class `MultipartFormDataOutput` as:
    
    ```java
    import javax.ws.rs.GET;
    import javax.ws.rs.Path;
    import javax.ws.rs.Produces;
    import javax.ws.rs.core.MediaType;
    
    import org.jboss.resteasy.reactive.server.core.multipart.MultipartFormDataOutput;
    
    @Path("multipart")
    public class Endpoint {
    
        @GET
        @Produces(MediaType.MULTIPART_FORM_DATA)
        @Path("file")
        public MultipartFormDataOutput getFile() {
            MultipartFormDataOutput form = new MultipartFormDataOutput();
            form.addFormData("person", new Person("John"), MediaType.APPLICATION_JSON_TYPE);
            form.addFormData("status", "a status", MediaType.TEXT_PLAIN_TYPE)
                    .getHeaders().putSingle("extra-header", "extra-value");
            return form;
        }
    }
    ```
    
    Fix https://github.com/quarkusio/quarkus/issues/28631
    
    This last approach allows you adding extra headers to the output part.

--- independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/multipart/MultipartOutputInjectionTarget.java
+++ independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/multipart/MultipartOutputInjectionTarget.java
@@ -1,7 +1,5 @@
 package org.jboss.resteasy.reactive.server.core.multipart;
 
-import java.util.List;
-
 public interface MultipartOutputInjectionTarget {
-    List<PartItem> mapFrom(Object pojo);
+    MultipartFormDataOutput mapFrom(Object pojo);
 }

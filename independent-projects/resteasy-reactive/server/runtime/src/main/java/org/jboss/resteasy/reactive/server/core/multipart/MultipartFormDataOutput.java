f2ac8520f7a2dc0cca3c1d57eca7ad9deddd300e
Jose <josecarvajalhilario@gmail.com>
2022-10-26 08:28:25 +0200

    Support programmatic `multipart/form-data` responses
    With these changes, users can also manually append the parts of the form using the class `MultipartFormDataOutput` as:
    
    ```java
    import javax.ws.rs.GET;
    import javax.ws.rs.Path;
    import javax.ws.rs.Produces;
    import javax.ws.rs.core.MediaType;
    
    import org.jboss.resteasy.reactive.server.core.multipart.MultipartFormDataOutput;
    
    @Path("multipart")
    public class Endpoint {
    
        @GET
        @Produces(MediaType.MULTIPART_FORM_DATA)
        @Path("file")
        public MultipartFormDataOutput getFile() {
            MultipartFormDataOutput form = new MultipartFormDataOutput();
            form.addFormData("person", new Person("John"), MediaType.APPLICATION_JSON_TYPE);
            form.addFormData("status", "a status", MediaType.TEXT_PLAIN_TYPE)
                    .getHeaders().putSingle("extra-header", "extra-value");
            return form;
        }
    }
    ```
    
    Fix https://github.com/quarkusio/quarkus/issues/28631
    
    This last approach allows you adding extra headers to the output part.
    
    (cherry picked from commit 6967fbc1c7d46ef1342c3d332ac85ecd6f87f73c)

---
+++ independent-projects/resteasy-reactive/server/runtime/src/main/java/org/jboss/resteasy/reactive/server/core/multipart/MultipartFormDataOutput.java
@@ -0,0 +1,25 @@
+package org.jboss.resteasy.reactive.server.core.multipart;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.core.MediaType;
+
+public class MultipartFormDataOutput {
+    private final Map<String, PartItem> parts = new HashMap<>();
+
+    public Map<String, PartItem> getFormData() {
+        return Collections.unmodifiableMap(parts);
+    }
+
+    public PartItem addFormData(String key, Object entity, MediaType mediaType) {
+        return addFormData(key, entity, null, mediaType);
+    }
+
+    public PartItem addFormData(String key, Object entity, String genericType, MediaType mediaType) {
+        PartItem part = new PartItem(entity, genericType, mediaType);
+        parts.put(key, part);
+        return part;
+    }
+}

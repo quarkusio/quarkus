5191900480dd18e8e63a4fec4b6992e3d0833708
Georgios Andrianakis <geoand@gmail.com>
2022-11-01 11:14:10 +0200

    Introduce API allowing users to customize Kotlin Serialization Json bean
    This is done by introducing beans of type JsonBuilderCustomizer which
    are called with the JsonBuilder that Quarkus has built up
    based on the Quarkus configuration

--- extensions/resteasy-reactive/quarkus-resteasy-reactive-kotlin-serialization-common/runtime/src/main/kotlin/io/quarkus/resteasy/reactive/kotlin/serialization/common/runtime/JsonProducer.kt
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive-kotlin-serialization-common/runtime/src/main/kotlin/io/quarkus/resteasy/reactive/kotlin/serialization/common/runtime/JsonProducer.kt
@@ -1,6 +1,8 @@
 package io.quarkus.resteasy.reactive.kotlin.serialization.common.runtime
 
+import io.quarkus.arc.All
 import io.quarkus.arc.DefaultBean
+import io.quarkus.resteasy.reactive.kotlin.serialization.common.JsonBuilderCustomizer
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import javax.enterprise.inject.Produces
@@ -13,7 +15,7 @@ class JsonProducer {
     @Singleton
     @Produces
     @DefaultBean
-    fun json(configuration: KotlinSerializationConfig) = Json {
+    fun defaultJson(configuration: KotlinSerializationConfig, @All customizers: java.util.List<JsonBuilderCustomizer>) = Json {
         allowSpecialFloatingPointValues = configuration.json.allowSpecialFloatingPointValues
         allowStructuredMapKeys = configuration.json.allowStructuredMapKeys
         classDiscriminator = configuration.json.classDiscriminator
@@ -26,5 +28,21 @@ class JsonProducer {
         prettyPrintIndent = configuration.json.prettyPrintIndent
         useAlternativeNames = configuration.json.useAlternativeNames
         useArrayPolymorphism = configuration.json.useArrayPolymorphism
+
+        val sortedCustomizers = sortCustomizersInDescendingPriorityOrder(customizers)
+        for (customizer in sortedCustomizers) {
+            customizer.customize(this)
+        }
+    }
+
+    private fun sortCustomizersInDescendingPriorityOrder(
+        customizers: Iterable<JsonBuilderCustomizer>
+    ): List<JsonBuilderCustomizer> {
+        val sortedCustomizers: MutableList<JsonBuilderCustomizer> = ArrayList()
+        for (customizer in customizers) {
+            sortedCustomizers.add(customizer)
+        }
+        sortedCustomizers.sort()
+        return sortedCustomizers
     }
 }

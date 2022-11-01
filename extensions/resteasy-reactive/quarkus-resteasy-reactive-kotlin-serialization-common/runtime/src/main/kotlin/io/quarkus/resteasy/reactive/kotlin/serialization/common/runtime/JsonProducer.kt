209cc193378be9bc4de0ac5288b5432eb9d52a4a
Georgios Andrianakis <geoand@gmail.com>
2022-11-01 09:40:18 +0200

    Simplify Kotlin Serialization CDI integration
    There is no reason to use a Synthetic bean and a Recorder
    in this case.
    Using standard CDI annotations and typical Arc BuildItems
    makes things much simpler

---
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive-kotlin-serialization-common/runtime/src/main/kotlin/io/quarkus/resteasy/reactive/kotlin/serialization/common/runtime/JsonProducer.kt
@@ -0,0 +1,28 @@
+package io.quarkus.resteasy.reactive.kotlin.serialization.common.runtime
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import javax.enterprise.inject.Produces
+import javax.inject.Singleton
+
+@Singleton
+class JsonProducer {
+
+    @ExperimentalSerializationApi
+    @Singleton
+    @Produces
+    fun json(configuration: KotlinSerializationConfig) = Json {
+        allowSpecialFloatingPointValues = configuration.json.allowSpecialFloatingPointValues
+        allowStructuredMapKeys = configuration.json.allowStructuredMapKeys
+        classDiscriminator = configuration.json.classDiscriminator
+        coerceInputValues = configuration.json.coerceInputValues
+        encodeDefaults = configuration.json.encodeDefaults
+        explicitNulls = configuration.json.explicitNulls
+        ignoreUnknownKeys = configuration.json.ignoreUnknownKeys
+        isLenient = configuration.json.isLenient
+        prettyPrint = configuration.json.prettyPrint
+        prettyPrintIndent = configuration.json.prettyPrintIndent
+        useAlternativeNames = configuration.json.useAlternativeNames
+        useArrayPolymorphism = configuration.json.useArrayPolymorphism
+    }
+}

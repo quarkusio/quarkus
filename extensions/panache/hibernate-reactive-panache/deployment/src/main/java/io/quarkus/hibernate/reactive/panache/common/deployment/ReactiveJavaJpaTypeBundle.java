26bed8b24c4c092d004d5b05724a300bb47196af
Jose <josecarvajalhilario@gmail.com>
2022-11-04 11:29:59 +0100

    Fix warning in panache rest data reactive
    These changes fix the following warning message when building an application using rest-data reactive with panache:
    
    ```
    [INFO] --- quarkus-maven-plugin:999-SNAPSHOT:build (default) @ rest-data-panache-reactive ---
    [WARNING] [io.quarkus.arc.deployment.SplitPackageProcessor] Detected a split package usage which is considered a bad practice and should be avoided. Following packages were detected in multiple archives: 
    - "io.quarkus.hibernate.reactive.panache" found in [io.quarkus:quarkus-hibernate-reactive-panache-common::jar, io.quarkus:quarkus-hibernate-reactive-panache::jar]
    
    ```

--- extensions/panache/hibernate-reactive-panache/deployment/src/main/java/io/quarkus/hibernate/reactive/panache/common/deployment/ReactiveJavaJpaTypeBundle.java
+++ extensions/panache/hibernate-reactive-panache/deployment/src/main/java/io/quarkus/hibernate/reactive/panache/common/deployment/ReactiveJavaJpaTypeBundle.java
@@ -1,4 +1,4 @@
-package io.quarkus.hibernate.reactive.panache.deployment;
+package io.quarkus.hibernate.reactive.panache.common.deployment;
 
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;

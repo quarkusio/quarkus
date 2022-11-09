4c0e5dcdaa5317a2cca6867eff509a1e99febf8a
Sergey Beryozkin <sberyozkin@gmail.com>
2022-11-09 12:11:46 +0000

    Update OIDC propagation reactive to support AccessToken annotation

--- extensions/oidc-token-propagation-reactive/deployment/src/test/java/io/quarkus/oidc/token/propagation/reactive/AccessTokenPropagationService.java
+++ extensions/oidc-token-propagation-reactive/deployment/src/test/java/io/quarkus/oidc/token/propagation/reactive/AccessTokenPropagationService.java
@@ -3,11 +3,12 @@ package io.quarkus.oidc.token.propagation.reactive;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import io.quarkus.oidc.token.propagation.AccessToken;
+
 @RegisterRestClient
-@RegisterProvider(AccessTokenRequestReactiveFilter.class)
+@AccessToken
 @Path("/")
 public interface AccessTokenPropagationService {
 

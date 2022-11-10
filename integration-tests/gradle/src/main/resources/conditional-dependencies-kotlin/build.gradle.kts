5924121bc5ff7f3f10a136ee96a24cc881e769d7
Georgios Andrianakis <geoand@gmail.com>
2022-11-10 10:07:41 +0200

    Bump to Kotlin 1.7.21
    Supersedes: #29137
    (cherry picked from commit f02ea935e0a71531223c8e18999c420ae5cf35f3)

--- integration-tests/gradle/src/main/resources/conditional-dependencies-kotlin/build.gradle.kts
+++ integration-tests/gradle/src/main/resources/conditional-dependencies-kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.allopen") version "1.7.20"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.allopen") version "1.7.21"
     id("io.quarkus")
 }
 

77d6dd18f42abe4b48b7f7d391e993f59627adef
theacousticguy <42766600+abhishekS14@users.noreply.github.com>
2022-11-20 15:04:50 +0530

    Update QuarkusPluginExtension.java
    javadoc update

--- devtools/gradle/gradle-application-plugin/src/main/java/io/quarkus/gradle/extension/QuarkusPluginExtension.java
+++ devtools/gradle/gradle-application-plugin/src/main/java/io/quarkus/gradle/extension/QuarkusPluginExtension.java
@@ -137,6 +137,9 @@ public class QuarkusPluginExtension {
 
     /**
      * Returns the last file from the specified {@link FileCollection}.
+     *
+     * @param fileCollection the collection of files present in the directory
+     * @return result returns the last file
      */
     public static File getLastFile(FileCollection fileCollection) {
         File result = null;

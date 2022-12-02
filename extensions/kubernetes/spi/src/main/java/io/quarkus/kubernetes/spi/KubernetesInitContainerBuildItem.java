aa0a68d851ea20398e2780b86a5c657c703fa1cc
Ioannis Canellos <iocanel@gmail.com>
2022-12-02 14:01:29 +0200

    doc: update flyway and liquibase/mongo docs

--- extensions/kubernetes/spi/src/main/java/io/quarkus/kubernetes/spi/KubernetesInitContainerBuildItem.java
+++ extensions/kubernetes/spi/src/main/java/io/quarkus/kubernetes/spi/KubernetesInitContainerBuildItem.java
@@ -100,6 +100,7 @@ public final class KubernetesInitContainerBuildItem extends MultiBuildItem {
      * Flag for tasks that require access to the environment variables of the application.
      * Often tasks need to access resources, configured via environment variables. This
      * flag expresses that the task should be executed using the same envrironment variables as the application.
+     *
      * @return true when the task is meant to share environment variables with the application.
      */
     public boolean isSharedEnvironment() {
@@ -110,11 +111,14 @@ public final class KubernetesInitContainerBuildItem extends MultiBuildItem {
         return new KubernetesInitContainerBuildItem(name, target, image, command, arguments, envVars, sharedEnvironment,
                 sharedFilesystem);
     }
+
     /**
      * Flag for tasks that need to share filesystem with the application.
-     * Often tasks need to access resources, configured via filesystem (e.g. local config files, kubernetes service binding etc).
+     * Often tasks need to access resources, configured via filesystem (e.g. local config files, kubernetes service binding
+     * etc).
      * In other cases, tasks may need to produce files needed by the application.
      * This flag expresses that the task should share filesystem with the application.
+     *
      * @return true when the task is meant to share filesystem.
      */
     public boolean isSharedFilesystem() {

643c2db104a159d233cb7f95025ce37644ed4f1e
Sergey Beryozkin <sberyozkin@gmail.com>
2022-12-12 17:32:04 +0000

    Fix OIDC tests which depend on Resteasy Classic Client

--- integration-tests/oidc-token-propagation/src/main/java/io/quarkus/it/keycloak/AccessTokenPropagationService.java
+++ integration-tests/oidc-token-propagation/src/main/java/io/quarkus/it/keycloak/AccessTokenPropagationService.java
@@ -7,7 +7,7 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import io.quarkus.oidc.token.propagation.AccessToken;
 
-@RegisterRestClient
+@RegisterRestClient(configKey = "access-token-propagation")
 @AccessToken
 @Path("/")
 public interface AccessTokenPropagationService {

9e9565d3168682243033ed4474fe20a919b7cfa8
arik-dig <asher@digma.ai>
2022-12-18 13:33:10 +0200

    refer to all JAX-RS annotations

--- extensions/opentelemetry/deployment/src/main/java/io/quarkus/opentelemetry/deployment/JaxRsAnnotationsTransformer.java
+++ extensions/opentelemetry/deployment/src/main/java/io/quarkus/opentelemetry/deployment/JaxRsAnnotationsTransformer.java
@@ -1,5 +1,7 @@
 package io.quarkus.opentelemetry.deployment;
 
+import java.util.List;
+
 import org.jboss.jandex.AnnotationTarget;
 import org.jboss.jandex.AnnotationTarget.Kind;
 import org.jboss.jandex.DotName;
@@ -10,7 +12,15 @@ import io.quarkus.opentelemetry.runtime.tracing.intrumentation.jaxrs.JaxRsBindin
 
 public class JaxRsAnnotationsTransformer implements AnnotationsTransformer {
 
+    static final DotName ANNOT_DELETE = DotName.createSimple(javax.ws.rs.DELETE.class);
     static final DotName ANNOT_GET = DotName.createSimple(javax.ws.rs.GET.class);
+    static final DotName ANNOT_HEAD = DotName.createSimple(javax.ws.rs.HEAD.class);
+    static final DotName ANNOT_OPTIONS = DotName.createSimple(javax.ws.rs.OPTIONS.class);
+    static final DotName ANNOT_PATCH = DotName.createSimple(javax.ws.rs.PATCH.class);
+    static final DotName ANNOT_POST = DotName.createSimple(javax.ws.rs.POST.class);
+    static final DotName ANNOT_PUT = DotName.createSimple(javax.ws.rs.PUT.class);
+    static final List<DotName> JAX_RS_ANNOTATIONS_LIST = List.of(
+            ANNOT_DELETE, ANNOT_GET, ANNOT_HEAD, ANNOT_OPTIONS, ANNOT_PATCH, ANNOT_POST, ANNOT_PUT);
     private static final DotName JAX_RS_INTERCEPTOR_BINDING = DotName.createSimple(JaxRsBinding.class);
 
     @Override
@@ -25,10 +35,13 @@ public class JaxRsAnnotationsTransformer implements AnnotationsTransformer {
             return;
         }
         MethodInfo methodInfo = target.asMethod();
-        if (methodInfo.hasAnnotation(ANNOT_GET)) {
-            transformationContext.transform()
-                    .add(JAX_RS_INTERCEPTOR_BINDING)
-                    .done();
+        for (DotName currExpectedAnnotation : JAX_RS_ANNOTATIONS_LIST) {
+            if (methodInfo.hasAnnotation(currExpectedAnnotation)) {
+                transformationContext.transform()
+                        .add(JAX_RS_INTERCEPTOR_BINDING)
+                        .done();
+                break; // out of loop
+            }
         }
     }
 }

09c7f9afa5498d759d9443165a27598765691d0c
arik-dig <asher@digma.ai>
2022-12-18 13:41:27 +0200

    get rid of System.out

--- extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/intrumentation/jaxrs/JaxRsInterceptor.java
+++ extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/intrumentation/jaxrs/JaxRsInterceptor.java
@@ -16,33 +16,15 @@ import io.quarkus.arc.ArcInvocationContext;
 @Priority(Interceptor.Priority.LIBRARY_AFTER + 50)
 public class JaxRsInterceptor {
 
-    private boolean logToConsole = true;
-
-    public JaxRsInterceptor() {
-        logToConsole = evalIsLogToConsole();
-        System.out.println("JaxRsInterceptor. logToConsole=" + logToConsole);
-    }
-
     @AroundInvoke
     public Object span(final ArcInvocationContext invocationContext) throws Exception {
         Method method = invocationContext.getMethod();
         Class<?> classOfTarget = method.getDeclaringClass();
         Span localRootSpan = LocalRootSpan.current();
-        if (logToConsole) {
-            System.out.println(
-                    "JaxRsInterceptor. intercepted method=" + method.getName() + ", on class=" + classOfTarget.getName()
-                            + ", localRootSpan=" + localRootSpan);
-        }
 
         localRootSpan.setAttribute(SemanticAttributes.CODE_NAMESPACE, classOfTarget.getName());
         localRootSpan.setAttribute(SemanticAttributes.CODE_FUNCTION, method.getName());
 
         return invocationContext.proceed();
-    }
-
-    private static boolean evalIsLogToConsole() {
-        String envVal = System.getenv("OTEL_LOG_TO_CONSOLE");
-        boolean retVal = !("false".equalsIgnoreCase(envVal));
-        return retVal;
     }
 }

163a6932c4305678644e2ae830ad2860c29da012
arik-dig <asher@digma.ai>
2022-12-18 10:28:25 +0200

    adding the code object attributes to localRootSpan

--- extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/intrumentation/jaxrs/JaxRsInterceptor.java
+++ extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/intrumentation/jaxrs/JaxRsInterceptor.java
@@ -1,11 +1,15 @@
 package io.quarkus.opentelemetry.runtime.tracing.intrumentation.jaxrs;
 
-import io.quarkus.arc.ArcInvocationContext;
+import java.lang.reflect.Method;
 
 import javax.annotation.Priority;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
-import java.lang.reflect.Method;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.instrumentation.api.instrumenter.LocalRootSpan;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import io.quarkus.arc.ArcInvocationContext;
 
 @JaxRsBinding
 @Interceptor
@@ -22,11 +26,17 @@ public class JaxRsInterceptor {
     @AroundInvoke
     public Object span(final ArcInvocationContext invocationContext) throws Exception {
         Method method = invocationContext.getMethod();
-        Class<?> targetClass = method.getDeclaringClass();
+        Class<?> classOfTarget = method.getDeclaringClass();
+        Span localRootSpan = LocalRootSpan.current();
         if (logToConsole) {
             System.out.println(
-                "JaxRsInterceptor. intercepted method=" + method.getName() + ", on class=" + targetClass.getName());
+                    "JaxRsInterceptor. intercepted method=" + method.getName() + ", on class=" + classOfTarget.getName()
+                            + ", localRootSpan=" + localRootSpan);
         }
+
+        localRootSpan.setAttribute(SemanticAttributes.CODE_NAMESPACE, classOfTarget.getName());
+        localRootSpan.setAttribute(SemanticAttributes.CODE_FUNCTION, method.getName());
+
         return invocationContext.proceed();
     }
 

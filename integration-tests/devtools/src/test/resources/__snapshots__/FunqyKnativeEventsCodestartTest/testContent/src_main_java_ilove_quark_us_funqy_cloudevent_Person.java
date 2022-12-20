60470679bb5daf8a27c6139faf341945ebbfe5ab
Loïc Mathieu <loikeseke@gmail.com>
2022-12-20 12:19:27 +0100

    Add missing IT for codestart examples

---
+++ integration-tests/devtools/src/test/resources/__snapshots__/FunqyKnativeEventsCodestartTest/testContent/src_main_java_ilove_quark_us_funqy_cloudevent_Person.java
@@ -0,0 +1,19 @@
+package ilove.quark.us.funqy.cloudevent;
+
+public class Person {
+    private String name;
+
+    public Person() {}
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

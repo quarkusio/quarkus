00a1aa9ee258fa587ee1db977ddcd59a5279d033
Diogo Carleto <diogocarleto@gmail.com>
2022-12-30 12:02:49 -0300

    format

--- extensions/redis-client/deployment/src/main/java/io/quarkus/redis/client/deployment/RedisMetricsBuildItem.java
+++ extensions/redis-client/deployment/src/main/java/io/quarkus/redis/client/deployment/RedisMetricsBuildItem.java
@@ -6,13 +6,13 @@ import io.quarkus.runtime.RuntimeValue;
 
 public final class RedisMetricsBuildItem extends SimpleBuildItem {
 
-	private final RuntimeValue<ObservableRedisMetrics> metrics;
+    private final RuntimeValue<ObservableRedisMetrics> metrics;
 
-	public RedisMetricsBuildItem(RuntimeValue<ObservableRedisMetrics> metrics) {
-		this.metrics = metrics;
-	}
+    public RedisMetricsBuildItem(RuntimeValue<ObservableRedisMetrics> metrics) {
+        this.metrics = metrics;
+    }
 
-	public RuntimeValue<ObservableRedisMetrics> get() {
-		return metrics;
-	}
+    public RuntimeValue<ObservableRedisMetrics> get() {
+        return metrics;
+    }
 }

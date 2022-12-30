300e338757774bee92ff1226a48ae5f483a45ca5
Diogo Carleto <diogocarleto@gmail.com>
2022-12-30 00:28:28 -0300

    reset to main branch

--- extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/deployment/MetricFiltersTest.java
+++ extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/deployment/MetricFiltersTest.java
@@ -1,65 +1,81 @@
 package io.quarkus.micrometer.deployment;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.concurrent.TimeUnit;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import javax.inject.Inject;
 
-import javax.inject.Singleton;
-import javax.ws.rs.Produces;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import io.micrometer.core.instrument.FunctionCounter;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.Meter;
-import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import io.micrometer.core.instrument.search.Search;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import io.quarkus.micrometer.runtime.MicrometerMetricsFactory;
+import io.quarkus.micrometer.runtime.config.MicrometerConfig;
+import io.quarkus.micrometer.test.MeasureThis;
 import io.quarkus.test.QuarkusUnitTest;
 
-public class MetricFiltersTest {
+public class MetricsFromMetricsFactoryTestCase {
 
     @RegisterExtension
-    static final QuarkusUnitTest config = new QuarkusUnitTest()
+    static QuarkusUnitTest runner = new QuarkusUnitTest()
+            .withConfigurationResource("test-logging.properties")
+            .overrideConfigKey("quarkus.micrometer.registry-enabled-default", "false")
+            .overrideConfigKey("quarkus.micrometer.binder-enabled-default", "false")
+            .overrideConfigKey("quarkus.redis.devservices.enabled", "false")
             .withApplicationRoot((jar) -> jar
-                    .addClasses(AnnotatedFilter.class, NonAnnotatedFilter.class,
-                            MeterFilterProducer.class));
+                    .addClass(MeasureThis.class));
+
+    @Inject
+    MeterRegistry meterRegistry;
+
+    @Inject
+    MicrometerConfig config;
 
     @Test
-    public void testCDIFilters() {
-        assertTrue(AnnotatedFilter.COUNT.get() > 0);
-        assertTrue(NonAnnotatedFilter.COUNT.get() > 0);
-        assertEquals(AnnotatedFilter.COUNT.get(), NonAnnotatedFilter.COUNT.get());
-    }
+    public void testMetricFactoryCreatedMetrics() throws Exception {
+        ((CompositeMeterRegistry) meterRegistry).add(new SimpleMeterRegistry());
+        MicrometerMetricsFactory factory = new MicrometerMetricsFactory(config, meterRegistry);
+        MeasureThis.registerMetrics().accept(factory);
 
-    @Singleton
-    public static class AnnotatedFilter implements MeterFilter {
+        Meter count_me = Search.in(meterRegistry).name("count.me").meter();
+        Assertions.assertTrue(FunctionCounter.class.isInstance(count_me));
+        MeasureThis.counter.increment();
+        Assertions.assertEquals(MeasureThis.counter.doubleValue(), ((FunctionCounter) count_me).count());
 
-        public static final AtomicInteger COUNT = new AtomicInteger(0);
+        Meter gauge_me = Search.in(meterRegistry).name("gauge.supplier").meter();
+        Assertions.assertTrue(Gauge.class.isInstance(gauge_me));
+        MeasureThis.gauge.increment();
+        Assertions.assertEquals(MeasureThis.gauge.doubleValue(), ((Gauge) gauge_me).value());
 
-        @Override
-        public Meter.Id map(Meter.Id id) {
-            COUNT.incrementAndGet();
-            return id;
-        }
-    }
+        Meter runnable = Search.in(meterRegistry).name("time.runnable").meter();
+        Assertions.assertTrue(Timer.class.isInstance(runnable));
+        MeasureThis.wrappedRunnable.run();
+        Assertions.assertEquals(MeasureThis.runnableCount.longValue(), ((Timer) runnable).count(),
+                "Runnable invocation count should match");
 
-    public static class NonAnnotatedFilter implements MeterFilter {
+        Meter callable = Search.in(meterRegistry).name("time.callable").meter();
+        Assertions.assertTrue(Timer.class.isInstance(callable));
+        MeasureThis.wrappedCallable.call();
+        Assertions.assertEquals(MeasureThis.callableCount.longValue(), ((Timer) callable).count(),
+                "Callable invocation count should match");
 
-        public static final AtomicInteger COUNT = new AtomicInteger(0);
+        Meter supplier = Search.in(meterRegistry).name("time.supplier").meter();
+        Assertions.assertTrue(Timer.class.isInstance(supplier));
+        MeasureThis.wrappedSupplier.get();
+        Assertions.assertEquals(MeasureThis.supplierCount.longValue(), ((Timer) supplier).count(),
+                "Supplier invocation count should match");
 
-        @Override
-        public Meter.Id map(Meter.Id id) {
-            COUNT.incrementAndGet();
-            return id;
-        }
-    }
-
-    @Singleton
-    public static class MeterFilterProducer {
-
-        @Produces
-        @Singleton
-        public MeterFilter producedMeterFilter() {
-            return new NonAnnotatedFilter();
-        }
+        Meter recorder = Search.in(meterRegistry).name("time.recorder").meter();
+        Assertions.assertTrue(Timer.class.isInstance(recorder));
+        MeasureThis.timeRecorder.update(10, TimeUnit.MINUTES);
+        Assertions.assertEquals(1L, ((Timer) recorder).count(),
+                "Recorder invocation count should match");
     }
 }

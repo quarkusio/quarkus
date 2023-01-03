3d90f835883f9d0e5e82ab94aae430d74c8a7520
Georgios Andrianakis <geoand@gmail.com>
2023-01-03 15:59:47 +0200

    Properly segregate Json MessageBodyReader/Writer classes for server and client
    Fixes: #30089

--- extensions/resteasy-reactive/quarkus-resteasy-reactive-jsonb-common/deployment/src/main/java/io/quarkus/resteasy/reactive/jsonb/common/deployment/ResteasyReactiveJsonbCommonProcessor.java
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive-jsonb-common/deployment/src/main/java/io/quarkus/resteasy/reactive/jsonb/common/deployment/ResteasyReactiveJsonbCommonProcessor.java
@@ -3,6 +3,7 @@ package io.quarkus.resteasy.reactive.jsonb.common.deployment;
 import java.util.Collections;
 import java.util.List;
 
+import javax.ws.rs.RuntimeType;
 import javax.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.reactive.common.util.RestMediaType;
@@ -29,33 +30,37 @@ public class ResteasyReactiveJsonbCommonProcessor {
             VertxJson.JsonArrayDeserializer.class.getName());
 
     @BuildStep
-    void additionalProviders(BuildProducer<AdditionalBeanBuildItem> additionalBean,
-            BuildProducer<MessageBodyReaderBuildItem> additionalReaders,
-            BuildProducer<MessageBodyWriterBuildItem> additionalWriters) {
+    public void registerVertxJsonSupport(
+            BuildProducer<JsonbSerializerBuildItem> serializers,
+            BuildProducer<JsonbDeserializerBuildItem> deserializers) {
+        serializers.produce(new JsonbSerializerBuildItem(VERTX_SERIALIZERS));
+        deserializers.produce(new JsonbDeserializerBuildItem(VERTX_DESERIALIZERS));
+    }
+
+    @BuildStep
+    public void beans(BuildProducer<AdditionalBeanBuildItem> additionalBean) {
         // make these beans to they can get instantiated with the Quarkus CDI configured Jsonb object
         additionalBean.produce(AdditionalBeanBuildItem.builder()
                 .addBeanClass(JsonbMessageBodyReader.class.getName())
                 .addBeanClass(JsonbMessageBodyWriter.class.getName())
                 .setUnremovable().build());
+    }
+
+    public static void additionalProviders(BuildProducer<MessageBodyReaderBuildItem> additionalReaders,
+            BuildProducer<MessageBodyWriterBuildItem> additionalWriters, RuntimeType runtimeType) {
 
         additionalReaders.produce(
                 new MessageBodyReaderBuildItem.Builder(JsonbMessageBodyReader.class.getName(), Object.class.getName())
                         .setMediaTypeStrings(Collections.singletonList(MediaType.APPLICATION_JSON))
                         .setBuiltin(true)
+                        .setRuntimeType(runtimeType)
                         .build());
         additionalWriters.produce(
                 new MessageBodyWriterBuildItem.Builder(JsonbMessageBodyWriter.class.getName(), Object.class.getName())
                         .setMediaTypeStrings(List.of(MediaType.APPLICATION_JSON, RestMediaType.APPLICATION_NDJSON,
                                 RestMediaType.APPLICATION_STREAM_JSON))
                         .setBuiltin(true)
+                        .setRuntimeType(runtimeType)
                         .build());
-    }
-
-    @BuildStep
-    public void registerVertxJsonSupport(
-            BuildProducer<JsonbSerializerBuildItem> serializers,
-            BuildProducer<JsonbDeserializerBuildItem> deserializers) {
-        serializers.produce(new JsonbSerializerBuildItem(VERTX_SERIALIZERS));
-        deserializers.produce(new JsonbDeserializerBuildItem(VERTX_DESERIALIZERS));
     }
 }

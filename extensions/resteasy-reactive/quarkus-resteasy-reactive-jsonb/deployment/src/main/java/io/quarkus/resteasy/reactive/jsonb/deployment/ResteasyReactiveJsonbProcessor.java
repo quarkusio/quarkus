335e2670735cc35b2d350835fa8c99e91ba491e9
Georgios Andrianakis <geoand@gmail.com>
2023-01-03 15:59:47 +0200

    Properly segregate Json MessageBodyReader/Writer classes for server and client
    Fixes: #30089
    (cherry picked from commit a95a5f7ff8a1ecfb0e2954dea94d0d44626bb82a)

--- extensions/resteasy-reactive/quarkus-resteasy-reactive-jsonb/deployment/src/main/java/io/quarkus/resteasy/reactive/jsonb/deployment/ResteasyReactiveJsonbProcessor.java
+++ extensions/resteasy-reactive/quarkus-resteasy-reactive-jsonb/deployment/src/main/java/io/quarkus/resteasy/reactive/jsonb/deployment/ResteasyReactiveJsonbProcessor.java
@@ -1,10 +1,15 @@
 package io.quarkus.resteasy.reactive.jsonb.deployment;
 
+import javax.ws.rs.RuntimeType;
+
 import io.quarkus.deployment.Feature;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.resteasy.reactive.common.deployment.ServerDefaultProducesHandlerBuildItem;
+import io.quarkus.resteasy.reactive.jsonb.common.deployment.ResteasyReactiveJsonbCommonProcessor;
+import io.quarkus.resteasy.reactive.spi.MessageBodyReaderBuildItem;
+import io.quarkus.resteasy.reactive.spi.MessageBodyWriterBuildItem;
 
 public class ResteasyReactiveJsonbProcessor {
 
@@ -16,5 +21,12 @@ public class ResteasyReactiveJsonbProcessor {
     @BuildStep
     ServerDefaultProducesHandlerBuildItem jsonDefault() {
         return ServerDefaultProducesHandlerBuildItem.json();
+    }
+
+    @BuildStep
+    void additionalProviders(BuildProducer<MessageBodyReaderBuildItem> additionalReaders,
+            BuildProducer<MessageBodyWriterBuildItem> additionalWriters) {
+        ResteasyReactiveJsonbCommonProcessor.additionalProviders(additionalReaders, additionalWriters,
+                RuntimeType.SERVER);
     }
 }

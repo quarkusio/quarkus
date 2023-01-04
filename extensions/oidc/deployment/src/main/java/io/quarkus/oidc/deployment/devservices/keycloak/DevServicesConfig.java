74fa2e0703457d5b7bd3e53da703d8088b0f10b9
Sergey Beryozkin <sberyozkin@gmail.com>
2023-01-04 18:15:13 +0000

    Bump Keycloak version to 20.0.2

--- extensions/oidc/deployment/src/main/java/io/quarkus/oidc/deployment/devservices/keycloak/DevServicesConfig.java
+++ extensions/oidc/deployment/src/main/java/io/quarkus/oidc/deployment/devservices/keycloak/DevServicesConfig.java
@@ -35,7 +35,7 @@ public class DevServicesConfig {
      * string.
      * Set 'quarkus.keycloak.devservices.keycloak-x-image' to override this check.
      */
-    @ConfigItem(defaultValue = "quay.io/keycloak/keycloak:20.0.1")
+    @ConfigItem(defaultValue = "quay.io/keycloak/keycloak:20.0.2")
     public String imageName;
 
     /**

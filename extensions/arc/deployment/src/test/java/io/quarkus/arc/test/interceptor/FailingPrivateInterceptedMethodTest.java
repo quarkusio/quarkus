9ddabe1178f993d25c76badaef4df1a8aa649830
Yoann Rodière <yoann@hibernate.org>
2023-01-18 12:37:35 +0100

    Improve error message for quarkus.arc.fail-on-intercepted-private-method

--- extensions/arc/deployment/src/test/java/io/quarkus/arc/test/interceptor/FailingPrivateInterceptedMethodTest.java
+++ extensions/arc/deployment/src/test/java/io/quarkus/arc/test/interceptor/FailingPrivateInterceptedMethodTest.java
@@ -3,6 +3,7 @@ package io.quarkus.arc.test.interceptor;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -27,7 +28,10 @@ public class FailingPrivateInterceptedMethodTest {
     static final QuarkusUnitTest config = new QuarkusUnitTest()
             .withApplicationRoot((jar) -> jar
                     .addClasses(SimpleBean.class, SimpleInterceptor.class, Simple.class))
-            .setExpectedException(DeploymentException.class);
+            .assertException(e -> assertThat(e).isInstanceOf(DeploymentException.class)
+                    .hasMessageContainingAll("@Simple will have no effect on method " + SimpleBean.class.getName() + ".foo",
+                            "Either remove the annotation from the method",
+                            "or turn this exception into a simple warning by setting configuration property 'quarkus.arc.fail-on-intercepted-private-method' to 'false'"));
 
     @Test
     public void testDeploymentFailed() {

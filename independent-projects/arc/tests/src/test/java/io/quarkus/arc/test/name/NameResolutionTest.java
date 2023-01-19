b5d825acaa27acc11198bf07a7152d966ee632fe
Ladislav Thon <lthon@redhat.com>
2023-01-19 17:36:04 +0100

    ArC: fix treatment of injection points with @Named without value
    The specification is clear that field injection points with `@Named`
    without value should use `@Named(fieldName)` as the qualifier. Other
    such injection points lead to a definition error.

--- independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/name/NameResolutionTest.java
+++ independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/name/NameResolutionTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.literal.NamedLiteral;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
@@ -18,7 +19,7 @@ import io.quarkus.arc.test.ArcTestContainer;
 public class NameResolutionTest {
 
     @RegisterExtension
-    public ArcTestContainer container = new ArcTestContainer(Bravo.class, Alpha.class);
+    public ArcTestContainer container = new ArcTestContainer(Bravo.class, Alpha.class, Consumer.class);
 
     @Test
     public void testBeanNames() {
@@ -29,6 +30,10 @@ public class NameResolutionTest {
         assertEquals(1, Arc.container().beanManager().getBeans("bongo").size());
         // Test that for defaulted name the @Named qualifier is replaced the defaulted value
         assertEquals("bing", Arc.container().instance(String.class, NamedLiteral.of("producedBing")).get());
+
+        Consumer consumer = Arc.container().instance(Consumer.class).get();
+        assertEquals("bing", consumer.producedBing);
+        assertEquals(12345, consumer.bongo);
     }
 
     @Named("A")
@@ -53,4 +58,14 @@ public class NameResolutionTest {
 
     }
 
+    @Dependent
+    static class Consumer {
+        @Inject
+        @Named
+        String producedBing;
+
+        @Inject
+        @Named
+        Integer bongo;
+    }
 }

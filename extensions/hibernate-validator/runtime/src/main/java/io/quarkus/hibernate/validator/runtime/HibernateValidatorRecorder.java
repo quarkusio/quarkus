f5cf52932da21b8c78d2e73281e9d135cf9666c4
Yoann Rodière <yoann@hibernate.org>
2023-01-19 13:49:59 +0100

    Work around the fact that ValueExtractor<*> is no longer a bean type in most cases

--- extensions/hibernate-validator/runtime/src/main/java/io/quarkus/hibernate/validator/runtime/HibernateValidatorRecorder.java
+++ extensions/hibernate-validator/runtime/src/main/java/io/quarkus/hibernate/validator/runtime/HibernateValidatorRecorder.java
@@ -1,10 +1,11 @@
 package io.quarkus.hibernate.validator.runtime;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Supplier;
 
-import javax.enterprise.util.TypeLiteral;
 import javax.validation.ClockProvider;
 import javax.validation.ConstraintValidatorFactory;
 import javax.validation.MessageInterpolator;
@@ -36,11 +37,8 @@ import io.quarkus.runtime.annotations.Recorder;
 @Recorder
 public class HibernateValidatorRecorder {
 
-    private static final TypeLiteral<ValueExtractor<?>> TYPE_LITERAL_VALUE_EXTRACTOR_WITH_WILDCARD = new TypeLiteral<ValueExtractor<?>>() {
-    };
-
     public BeanContainerListener initializeValidatorFactory(Set<Class<?>> classesToBeValidated,
-            Set<String> detectedBuiltinConstraints,
+            Set<String> detectedBuiltinConstraints, Set<Class<?>> valueExtractorClasses,
             boolean hasXmlConfiguration, boolean jpaInClasspath,
             ShutdownContext shutdownContext, LocalesBuildTimeConfig localesBuildTimeConfig,
             HibernateValidatorBuildTimeConfig hibernateValidatorBuildTimeConfig) {
@@ -143,11 +141,15 @@ public class HibernateValidatorRecorder {
                 }
 
                 // Automatically add all the values extractors declared as beans
-                for (ValueExtractor<?> valueExtractor : Arc.container().beanManager().createInstance()
-                        // Apparently passing ValueExtractor.class
-                        // won't match beans implementing ValueExtractor<NotAWildcard>,
-                        // so we need a type literal here.
-                        .select(TYPE_LITERAL_VALUE_EXTRACTOR_WITH_WILDCARD)) {
+                for (ValueExtractor<?> valueExtractor : HibernateValidatorRecorder
+                        // We cannot do something like `instance(...).select(ValueExtractor.class)`,
+                        // because `ValueExtractor` is usually implemented
+                        // as a parameterized type with wildcards,
+                        // and the CDI spec does not consider such types as bean types.
+                        // We work around that by listing all classes implementing `ValueExtractor` at build time,
+                        // then retrieving all bean instances implementing those types here.
+                        // See https://github.com/quarkusio/quarkus/pull/30447
+                        .<ValueExtractor<?>> uniqueBeanInstances(valueExtractorClasses)) {
                     configuration.addValueExtractor(valueExtractor);
                 }
 
@@ -174,6 +176,35 @@ public class HibernateValidatorRecorder {
         };
 
         return beanContainerListener;
+    }
+
+    // Ideally we'd retrieve all instances of a set of bean types
+    // simply by calling something like ArcContainer#select(Set<Type>)
+    // but that method does not exist.
+    // This method acts as a replacement.
+    private static <T> Iterable<T> uniqueBeanInstances(Set<Class<?>> classes) {
+        Set<String> beanIds = new HashSet<>();
+        for (Class<?> clazz : classes) {
+            for (InstanceHandle<?> handle : Arc.container().select(clazz).handles()) {
+                if (!handle.isAvailable()) {
+                    continue;
+                }
+                // A single bean can have multiple types.
+                // To avoid returning duplicate instances of the same bean,
+                // we first retrieve all bean IDs, deduplicate those,
+                // then retrieve the instance for each bean.
+                // Note that just retrieving all instances and putting them in a identity-based Set
+                // would not work, because beans can have the dependent pseudo-scope,
+                // in which case we'd have two instances of the same bean.
+                beanIds.add(handle.getBean().getIdentifier());
+            }
+        }
+        List<T> instances = new ArrayList<>();
+        for (String beanId : beanIds) {
+            var arcContainer = Arc.container();
+            instances.add(arcContainer.instance(arcContainer.<T> bean(beanId)).get());
+        }
+        return instances;
     }
 
     public Supplier<ResteasyConfigSupport> resteasyConfigSupportSupplier(boolean jsonDefault) {

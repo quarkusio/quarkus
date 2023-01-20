860401e1d5947fe8032cf3ad3bf11d4e0775b60a
Thomas Segismont <tsegismo@redhat.com>
2023-01-20 18:03:28 +0100

    Add OraclePoolCreator

--- extensions/reactive-oracle-client/deployment/src/main/java/io/quarkus/reactive/oracle/client/deployment/ReactiveOracleClientProcessor.java
+++ extensions/reactive-oracle-client/deployment/src/main/java/io/quarkus/reactive/oracle/client/deployment/ReactiveOracleClientProcessor.java
@@ -1,12 +1,26 @@
 package io.quarkus.reactive.oracle.client.deployment;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.Type;
+
 import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
 import io.quarkus.arc.deployment.SyntheticBeanBuildItem.ExtendedBeanConfigurator;
+import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
+import io.quarkus.arc.deployment.ValidationPhaseBuildItem;
+import io.quarkus.arc.deployment.devconsole.Name;
+import io.quarkus.arc.processor.BeanInfo;
 import io.quarkus.arc.processor.DotNames;
 import io.quarkus.datasource.common.runtime.DataSourceUtil;
 import io.quarkus.datasource.common.runtime.DatabaseKind;
@@ -34,6 +48,7 @@ import io.quarkus.reactive.datasource.runtime.DataSourceReactiveBuildTimeConfig;
 import io.quarkus.reactive.datasource.runtime.DataSourcesReactiveBuildTimeConfig;
 import io.quarkus.reactive.datasource.runtime.DataSourcesReactiveRuntimeConfig;
 import io.quarkus.reactive.oracle.client.runtime.DataSourcesReactiveOracleConfig;
+import io.quarkus.reactive.oracle.client.runtime.OraclePoolCreator;
 import io.quarkus.reactive.oracle.client.runtime.OraclePoolRecorder;
 import io.quarkus.reactive.oracle.client.runtime.OracleServiceBindingConverter;
 import io.quarkus.runtime.RuntimeValue;
@@ -87,6 +102,34 @@ class ReactiveOracleClientProcessor {
     @BuildStep
     DevServicesDatasourceConfigurationHandlerBuildItem devDbHandler() {
         return DevServicesDatasourceConfigurationHandlerBuildItem.reactive(DatabaseKind.ORACLE);
+    }
+
+    @BuildStep
+    void unremoveableBeans(BuildProducer<UnremovableBeanBuildItem> producer) {
+        producer.produce(UnremovableBeanBuildItem.beanTypes(OraclePoolCreator.class));
+    }
+
+    @BuildStep
+    void validateBeans(ValidationPhaseBuildItem validationPhase,
+            BuildProducer<ValidationPhaseBuildItem.ValidationErrorBuildItem> errors) {
+        // no two OraclePoolCreator beans can be associated with the same datasource
+        Map<String, Boolean> seen = new HashMap<>();
+        for (BeanInfo beanInfo : validationPhase.getContext().beans()
+                .matchBeanTypes(new OraclePoolCreatorBeanClassPredicate())) {
+            Set<Name> qualifiers = new TreeSet<>(); // use a TreeSet in order to get a predictable iteration order
+            for (AnnotationInstance qualifier : beanInfo.getQualifiers()) {
+                qualifiers.add(Name.from(qualifier));
+            }
+            String qualifiersStr = qualifiers.stream().map(Name::toString).collect(Collectors.joining("_"));
+            if (seen.getOrDefault(qualifiersStr, false)) {
+                errors.produce(new ValidationPhaseBuildItem.ValidationErrorBuildItem(
+                        new IllegalStateException(
+                                "There can be at most one bean of type '" + OraclePoolCreator.class.getName()
+                                        + "' for each datasource.")));
+            } else {
+                seen.put(qualifiersStr, true);
+            }
+        }
     }
 
     @BuildStep
@@ -234,6 +277,16 @@ class ReactiveOracleClientProcessor {
             configurator.addQualifier().annotation(DotNames.NAMED).addValue("value", dataSourceName).done();
             configurator.addQualifier().annotation(ReactiveDataSource.class).addValue("value", dataSourceName)
                     .done();
+        }
+    }
+
+    private static class OraclePoolCreatorBeanClassPredicate implements Predicate<Set<Type>> {
+        private static final Type ORACLE_POOL_CREATOR = Type.create(DotName.createSimple(OraclePoolCreator.class.getName()),
+                Type.Kind.CLASS);
+
+        @Override
+        public boolean test(Set<Type> types) {
+            return types.contains(ORACLE_POOL_CREATOR);
         }
     }
 }

manovotn/quarkus-jakarta-arc

ArC: fix treatment of primitive wrapper type producers that produce null
In such case, the corresponding zero value must be injected instead.

independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/BeanGenerator.java
@@ -868,16 +868,19 @@ public class BeanGenerator extends AbstractGenerator {
                 if (i == disposedParamPosition) {
                     referenceHandles[i] = destroy.getMethodParam(0);
                 } else {
+                    InjectionPointInfo injectionPoint = injectionPointsIterator.next();
                     ResultHandle childCtxHandle = destroy.invokeStaticMethod(MethodDescriptors.CREATIONAL_CTX_CHILD_CONTEXTUAL,
                             declaringProviderHandle, ctxHandle);
                     ResultHandle providerSupplierHandle = destroy
                             .readInstanceField(FieldDescriptor.of(beanCreator.getClassName(),
-                                    injectionPointToProviderField.get(injectionPointsIterator.next()),
+                                    injectionPointToProviderField.get(injectionPoint),
                                     Supplier.class.getName()), destroy.getThis());
                     ResultHandle providerHandle = destroy.invokeInterfaceMethod(MethodDescriptors.SUPPLIER_GET,
                             providerSupplierHandle);
-                    ResultHandle referenceHandle = destroy.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
-                            providerHandle, childCtxHandle);
+                    AssignableResultHandle referenceHandle = destroy.createVariable(Object.class);
+                    destroy.assign(referenceHandle, destroy.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
+                            providerHandle, childCtxHandle));
+                    checkPrimitiveInjection(destroy, injectionPoint, referenceHandle);
                     referenceHandles[i] = referenceHandle;
                 }
             }
@@ -998,8 +1001,10 @@ public class BeanGenerator extends AbstractGenerator {
                         providerSupplierHandle);
                 ResultHandle childCtx = createMethod.invokeStaticMethod(MethodDescriptors.CREATIONAL_CTX_CHILD_CONTEXTUAL,
                         providerHandle, createMethod.getMethodParam(0));
-                ResultHandle referenceHandle = createMethod.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
-                        providerHandle, childCtx);
+                AssignableResultHandle referenceHandle = createMethod.createVariable(Object.class);
+                createMethod.assign(referenceHandle, createMethod
+                        .invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET, providerHandle, childCtx));
+                checkPrimitiveInjection(createMethod, injectionPoint, referenceHandle);
                 injectableParamHandles.add(referenceHandle);
                 if (injectionPoint.isDependentTransientReference()) {
                     transientReferences.add(new TransientReference(providerHandle, referenceHandle, childCtx));
@@ -1029,6 +1034,24 @@ public class BeanGenerator extends AbstractGenerator {
                 ResultHandle decoratorProviderHandle = createMethod.invokeInterfaceMethod(
                         MethodDescriptors.SUPPLIER_GET, decoratorProviderSupplierHandle);
                 allOtherParamHandles.add(decoratorProviderHandle);
+            }
+        }
+    }
+
+    static void checkPrimitiveInjection(BytecodeCreator bytecode, InjectionPointInfo injectionPoint,
+            AssignableResultHandle referenceHandle) {
+        if (injectionPoint.getType().kind() == Type.Kind.PRIMITIVE) {
+            Type type = null;
+            if (injectionPoint.getResolvedBean().isProducerField()) {
+                type = injectionPoint.getResolvedBean().getTarget().get().asField().type();
+            } else if (injectionPoint.getResolvedBean().isProducerMethod()) {
+                type = injectionPoint.getResolvedBean().getTarget().get().asMethod().returnType();
+            }
+
+            if (type != null && Types.isPrimitiveWrapperType(type)) {
+                BytecodeCreator isNull = bytecode.ifNull(referenceHandle).trueBranch();
+                isNull.assign(referenceHandle,
+                        Types.loadZero(injectionPoint.getType().asPrimitiveType().primitive(), isNull));
             }
         }
     }
@@ -1234,8 +1257,10 @@ public class BeanGenerator extends AbstractGenerator {
                     providerSupplierHandle);
             ResultHandle childCtxHandle = create.invokeStaticMethod(MethodDescriptors.CREATIONAL_CTX_CHILD_CONTEXTUAL,
                     providerHandle, create.getMethodParam(0));
-            ResultHandle referenceHandle = create.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
-                    providerHandle, childCtxHandle);
+            AssignableResultHandle referenceHandle = create.createVariable(Object.class);
+            create.assign(referenceHandle, create.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
+                    providerHandle, childCtxHandle));
+            checkPrimitiveInjection(create, injectionPoint, referenceHandle);
             referenceHandles[paramIdx++] = referenceHandle;
             // We need to destroy dependent beans for @TransientReference injection points
             if (injectionPoint.isDependentTransientReference()) {
@@ -1501,8 +1526,10 @@ public class BeanGenerator extends AbstractGenerator {
                     MethodDescriptors.SUPPLIER_GET, providerSupplierHandle);
             ResultHandle childCtxHandle = tryBlock.invokeStaticMethod(MethodDescriptors.CREATIONAL_CTX_CHILD_CONTEXTUAL,
                     providerHandle, tryBlock.getMethodParam(0));
-            ResultHandle referenceHandle = tryBlock.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
-                    providerHandle, childCtxHandle);
+            AssignableResultHandle referenceHandle = tryBlock.createVariable(Object.class);
+            tryBlock.assign(referenceHandle, tryBlock.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
+                    providerHandle, childCtxHandle));
+            checkPrimitiveInjection(tryBlock, injectionPoint, referenceHandle);
 
             FieldInfo injectedField = fieldInjection.target.asField();
             // only use reflection fallback if we are not performing transformation
@@ -1541,8 +1568,10 @@ public class BeanGenerator extends AbstractGenerator {
                         providerSupplierHandle);
                 ResultHandle childCtxHandle = create.invokeStaticMethod(MethodDescriptors.CREATIONAL_CTX_CHILD_CONTEXTUAL,
                         providerHandle, create.getMethodParam(0));
-                ResultHandle referenceHandle = create.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
-                        providerHandle, childCtxHandle);
+                AssignableResultHandle referenceHandle = create.createVariable(Object.class);
+                create.assign(referenceHandle, create.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
+                        providerHandle, childCtxHandle));
+                checkPrimitiveInjection(create, injectionPoint, referenceHandle);
                 referenceHandles[paramIdx++] = referenceHandle;
                 // We need to destroy dependent beans for @TransientReference injection points
                 if (injectionPoint.isDependentTransientReference()) {

independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/Types.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiConsumer;
 
@@ -57,18 +58,25 @@ public final class Types {
 
     private static final Type OBJECT_TYPE = Type.create(DotNames.OBJECT, Kind.CLASS);
 
-    private static final Set<String> PRIMITIVE_CLASS_NAMES = new HashSet<>();
+    private static final Set<String> PRIMITIVE_CLASS_NAMES = Set.of(
+            "boolean",
+            "byte",
+            "short",
+            "int",
+            "long",
+            "float",
+            "double",
+            "char");
 
-    static {
-        PRIMITIVE_CLASS_NAMES.add("byte");
-        PRIMITIVE_CLASS_NAMES.add("char");
-        PRIMITIVE_CLASS_NAMES.add("double");
-        PRIMITIVE_CLASS_NAMES.add("float");
-        PRIMITIVE_CLASS_NAMES.add("int");
-        PRIMITIVE_CLASS_NAMES.add("long");
-        PRIMITIVE_CLASS_NAMES.add("short");
-        PRIMITIVE_CLASS_NAMES.add("boolean");
-    }
+    private static final Set<DotName> PRIMITIVE_WRAPPERS = Set.of(
+            DotNames.BOOLEAN,
+            DotNames.BYTE,
+            DotNames.SHORT,
+            DotNames.INTEGER,
+            DotNames.LONG,
+            DotNames.FLOAT,
+            DotNames.DOUBLE,
+            DotNames.CHARACTER);
 
     // we ban these interfaces because they are new to Java 12 and are used by java.lang.String which
     // means that they cannot be included in bytecode if we want to have application built with Java 12+ but targeting Java 8 - 11
@@ -702,6 +710,46 @@ public final class Types {
 
     static boolean isPrimitiveClassName(String className) {
         return PRIMITIVE_CLASS_NAMES.contains(className);
+    }
+
+    static boolean isPrimitiveWrapperType(Type type) {
+        if (type.kind() == Kind.CLASS) {
+            return PRIMITIVE_WRAPPERS.contains(type.name());
+        }
+        return false;
+    }
+
+    /**
+     * Emits a bytecode instruction to load the zero value of given {@code primitive} type
+     * into given {@code bytecode} creator and returns the {@link ResultHandle} of the loaded
+     * zero value. Can also be used to load a zero value of the corresponding wrapper type,
+     * because Gizmo will box automatically.
+     *
+     * @param primitive primitive type, must not be {@code null}
+     * @param bytecode bytecode creator that will receive the load instruction, must not be {@code null}
+     * @return a result handle of the loaded zero value
+     */
+    static ResultHandle loadZero(Primitive primitive, BytecodeCreator bytecode) {
+        switch (Objects.requireNonNull(primitive)) {
+            case BOOLEAN:
+                return bytecode.load(false);
+            case BYTE:
+                return bytecode.load((byte) 0);
+            case SHORT:
+                return bytecode.load((short) 0);
+            case INT:
+                return bytecode.load(0);
+            case LONG:
+                return bytecode.load(0L);
+            case FLOAT:
+                return bytecode.load(0.0F);
+            case DOUBLE:
+                return bytecode.load(0.0);
+            case CHAR:
+                return bytecode.load((char) 0);
+            default:
+                throw new IllegalArgumentException("Unknown primitive type: " + primitive);
+        }
     }
 
     static boolean containsTypeVariable(Type type) {

quarkusio/quarkus

independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/BeanGenerator.java
@@ -868,16 +868,19 @@ public class BeanGenerator extends AbstractGenerator {
                 if (i == disposedParamPosition) {
                     referenceHandles[i] = destroy.getMethodParam(0);
                 } else {
+                    InjectionPointInfo injectionPoint = injectionPointsIterator.next();
                     ResultHandle childCtxHandle = destroy.invokeStaticMethod(MethodDescriptors.CREATIONAL_CTX_CHILD_CONTEXTUAL,
                             declaringProviderHandle, ctxHandle);
                     ResultHandle providerSupplierHandle = destroy
                             .readInstanceField(FieldDescriptor.of(beanCreator.getClassName(),
-                                    injectionPointToProviderField.get(injectionPointsIterator.next()),
+                                    injectionPointToProviderField.get(injectionPoint),
                                     Supplier.class.getName()), destroy.getThis());
                     ResultHandle providerHandle = destroy.invokeInterfaceMethod(MethodDescriptors.SUPPLIER_GET,
                             providerSupplierHandle);
-                    ResultHandle referenceHandle = destroy.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
-                            providerHandle, childCtxHandle);
+                    AssignableResultHandle referenceHandle = destroy.createVariable(Object.class);
+                    destroy.assign(referenceHandle, destroy.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
+                            providerHandle, childCtxHandle));
+                    checkPrimitiveInjection(destroy, injectionPoint, referenceHandle);
                     referenceHandles[i] = referenceHandle;
                 }
             }
@@ -998,8 +1001,10 @@ public class BeanGenerator extends AbstractGenerator {
                         providerSupplierHandle);
                 ResultHandle childCtx = createMethod.invokeStaticMethod(MethodDescriptors.CREATIONAL_CTX_CHILD_CONTEXTUAL,
                         providerHandle, createMethod.getMethodParam(0));
-                ResultHandle referenceHandle = createMethod.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
-                        providerHandle, childCtx);
+                AssignableResultHandle referenceHandle = createMethod.createVariable(Object.class);
+                createMethod.assign(referenceHandle, createMethod
+                        .invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET, providerHandle, childCtx));
+                checkPrimitiveInjection(createMethod, injectionPoint, referenceHandle);
                 injectableParamHandles.add(referenceHandle);
                 if (injectionPoint.isDependentTransientReference()) {
                     transientReferences.add(new TransientReference(providerHandle, referenceHandle, childCtx));
@@ -1029,6 +1034,24 @@ public class BeanGenerator extends AbstractGenerator {
                 ResultHandle decoratorProviderHandle = createMethod.invokeInterfaceMethod(
                         MethodDescriptors.SUPPLIER_GET, decoratorProviderSupplierHandle);
                 allOtherParamHandles.add(decoratorProviderHandle);
+            }
+        }
+    }
+
+    static void checkPrimitiveInjection(BytecodeCreator bytecode, InjectionPointInfo injectionPoint,
+            AssignableResultHandle referenceHandle) {
+        if (injectionPoint.getType().kind() == Type.Kind.PRIMITIVE) {
+            Type type = null;
+            if (injectionPoint.getResolvedBean().isProducerField()) {
+                type = injectionPoint.getResolvedBean().getTarget().get().asField().type();
+            } else if (injectionPoint.getResolvedBean().isProducerMethod()) {
+                type = injectionPoint.getResolvedBean().getTarget().get().asMethod().returnType();
+            }
+
+            if (type != null && Types.isPrimitiveWrapperType(type)) {
+                BytecodeCreator isNull = bytecode.ifNull(referenceHandle).trueBranch();
+                isNull.assign(referenceHandle,
+                        Types.loadPrimitiveDefault(injectionPoint.getType().asPrimitiveType().primitive(), isNull));
             }
         }
     }
@@ -1234,8 +1257,10 @@ public class BeanGenerator extends AbstractGenerator {
                     providerSupplierHandle);
             ResultHandle childCtxHandle = create.invokeStaticMethod(MethodDescriptors.CREATIONAL_CTX_CHILD_CONTEXTUAL,
                     providerHandle, create.getMethodParam(0));
-            ResultHandle referenceHandle = create.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
-                    providerHandle, childCtxHandle);
+            AssignableResultHandle referenceHandle = create.createVariable(Object.class);
+            create.assign(referenceHandle, create.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
+                    providerHandle, childCtxHandle));
+            checkPrimitiveInjection(create, injectionPoint, referenceHandle);
             referenceHandles[paramIdx++] = referenceHandle;
             // We need to destroy dependent beans for @TransientReference injection points
             if (injectionPoint.isDependentTransientReference()) {
@@ -1501,8 +1526,10 @@ public class BeanGenerator extends AbstractGenerator {
                     MethodDescriptors.SUPPLIER_GET, providerSupplierHandle);
             ResultHandle childCtxHandle = tryBlock.invokeStaticMethod(MethodDescriptors.CREATIONAL_CTX_CHILD_CONTEXTUAL,
                     providerHandle, tryBlock.getMethodParam(0));
-            ResultHandle referenceHandle = tryBlock.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
-                    providerHandle, childCtxHandle);
+            AssignableResultHandle referenceHandle = tryBlock.createVariable(Object.class);
+            tryBlock.assign(referenceHandle, tryBlock.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
+                    providerHandle, childCtxHandle));
+            checkPrimitiveInjection(tryBlock, injectionPoint, referenceHandle);
 
             FieldInfo injectedField = fieldInjection.target.asField();
             // only use reflection fallback if we are not performing transformation
@@ -1541,8 +1568,10 @@ public class BeanGenerator extends AbstractGenerator {
                         providerSupplierHandle);
                 ResultHandle childCtxHandle = create.invokeStaticMethod(MethodDescriptors.CREATIONAL_CTX_CHILD_CONTEXTUAL,
                         providerHandle, create.getMethodParam(0));
-                ResultHandle referenceHandle = create.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
-                        providerHandle, childCtxHandle);
+                AssignableResultHandle referenceHandle = create.createVariable(Object.class);
+                create.assign(referenceHandle, create.invokeInterfaceMethod(MethodDescriptors.INJECTABLE_REF_PROVIDER_GET,
+                        providerHandle, childCtxHandle));
+                checkPrimitiveInjection(create, injectionPoint, referenceHandle);
                 referenceHandles[paramIdx++] = referenceHandle;
                 // We need to destroy dependent beans for @TransientReference injection points
                 if (injectionPoint.isDependentTransientReference()) {

independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/Types.java
@@ -12,6 +12,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiConsumer;
 
@@ -55,18 +56,25 @@ public final class Types {
 
     private static final Type OBJECT_TYPE = Type.create(DotNames.OBJECT, Kind.CLASS);
 
-    private static final Set<String> PRIMITIVE_CLASS_NAMES = new HashSet<>();
+    private static final Set<String> PRIMITIVE_CLASS_NAMES = Set.of(
+            "boolean",
+            "byte",
+            "short",
+            "int",
+            "long",
+            "float",
+            "double",
+            "char");
 
-    static {
-        PRIMITIVE_CLASS_NAMES.add("byte");
-        PRIMITIVE_CLASS_NAMES.add("char");
-        PRIMITIVE_CLASS_NAMES.add("double");
-        PRIMITIVE_CLASS_NAMES.add("float");
-        PRIMITIVE_CLASS_NAMES.add("int");
-        PRIMITIVE_CLASS_NAMES.add("long");
-        PRIMITIVE_CLASS_NAMES.add("short");
-        PRIMITIVE_CLASS_NAMES.add("boolean");
-    }
+    private static final Set<DotName> PRIMITIVE_WRAPPERS = Set.of(
+            DotNames.BOOLEAN,
+            DotNames.BYTE,
+            DotNames.SHORT,
+            DotNames.INTEGER,
+            DotNames.LONG,
+            DotNames.FLOAT,
+            DotNames.DOUBLE,
+            DotNames.CHARACTER);
 
     // we ban these interfaces because they are new to Java 12 and are used by java.lang.String which
     // means that they cannot be included in bytecode if we want to have application built with Java 12+ but targeting Java 8 - 11
@@ -742,6 +750,50 @@ public final class Types {
 
     static boolean isPrimitiveClassName(String className) {
         return PRIMITIVE_CLASS_NAMES.contains(className);
+    }
+
+    static boolean isPrimitiveWrapperType(Type type) {
+        if (type.kind() == Kind.CLASS) {
+            return PRIMITIVE_WRAPPERS.contains(type.name());
+        }
+        return false;
+    }
+
+    /**
+     * Emits a bytecode instruction to load the default value of given {@code primitive} type
+     * into given {@code bytecode} creator and returns the {@link ResultHandle} of the loaded
+     * default value. The default primitive value is {@code 0} for integral types, {@code 0.0}
+     * for floating point types, {@code false} for {@code boolean} and the null character for
+     * {@code char}.
+     * <p>
+     * Can also be used to load a default primitive value of the corresponding wrapper type,
+     * because Gizmo will box automatically.
+     *
+     * @param primitive primitive type, must not be {@code null}
+     * @param bytecode bytecode creator that will receive the load instruction, must not be {@code null}
+     * @return a result handle of the loaded default primitive value
+     */
+    static ResultHandle loadPrimitiveDefault(Primitive primitive, BytecodeCreator bytecode) {
+        switch (Objects.requireNonNull(primitive)) {
+            case BOOLEAN:
+                return bytecode.load(false);
+            case BYTE:
+                return bytecode.load((byte) 0);
+            case SHORT:
+                return bytecode.load((short) 0);
+            case INT:
+                return bytecode.load(0);
+            case LONG:
+                return bytecode.load(0L);
+            case FLOAT:
+                return bytecode.load(0.0F);
+            case DOUBLE:
+                return bytecode.load(0.0);
+            case CHAR:
+                return bytecode.load((char) 0);
+            default:
+                throw new IllegalArgumentException("Unknown primitive type: " + primitive);
+        }
     }
 
     static boolean containsTypeVariable(Type type) {
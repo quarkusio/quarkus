c4c40bee92ed6301e4841e832c55ceb896b0a39b
Sergey Beryozkin <sberyozkin@gmail.com>
2023-01-29 17:10:14 +0000

    Set SameSite Strict only on OIDC session cookie

--- extensions/oidc/runtime/src/main/java/io/quarkus/oidc/OidcTenantConfig.java
+++ extensions/oidc/runtime/src/main/java/io/quarkus/oidc/OidcTenantConfig.java
@@ -603,7 +603,7 @@ public class OidcTenantConfig extends OidcCommonConfig {
     public static class Authentication {
 
         /**
-         * SameSite attribute values for the session, state and post logout cookies.
+         * SameSite attribute values for the session cookie.
          */
         public enum CookieSameSite {
             STRICT,
@@ -767,7 +767,7 @@ public class OidcTenantConfig extends OidcCommonConfig {
         public Optional<String> cookieDomain = Optional.empty();
 
         /**
-         * SameSite attribute for the session, state and post logout cookies.
+         * SameSite attribute for the session cookie.
          */
         @ConfigItem(defaultValue = "strict")
         public CookieSameSite cookieSameSite = CookieSameSite.STRICT;

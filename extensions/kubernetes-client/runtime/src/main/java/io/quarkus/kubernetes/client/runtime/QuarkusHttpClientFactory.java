86a60403ac281ccf84967a54e853a82e0ffc479d
Marc Nuri <marc@marcnuri.com>
2023-01-31 14:48:32 +0100

    feat: reuse Vert.x object in KubernetesClient
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

---
+++ extensions/kubernetes-client/runtime/src/main/java/io/quarkus/kubernetes/client/runtime/QuarkusHttpClientFactory.java
@@ -0,0 +1,38 @@
+package io.quarkus.kubernetes.client.runtime;
+
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.http.HttpClient;
+import io.fabric8.kubernetes.client.vertx.VertxHttpClientBuilder;
+import io.vertx.core.Vertx;
+
+import javax.enterprise.inject.spi.CDI;
+
+public class QuarkusHttpClientFactory implements io.fabric8.kubernetes.client.http.HttpClient.Factory {
+
+    private Vertx vertx;
+
+    public QuarkusHttpClientFactory() {
+        // The client might get initialized outside a Quarkus context that can provide the Vert.x instance
+        // This is the case for the MockServer / @KubernetesTestServer where the server provides the KubernetesClient instance
+        try {
+            this.vertx = CDI.current().select(Vertx.class).get();
+        } catch (Exception e) {
+            this.vertx = Vertx.vertx();
+        }
+    }
+
+    @Override
+    public HttpClient.Builder newBuilder(Config config) {
+        return HttpClient.Factory.super.newBuilder(config);
+    }
+
+    @Override
+    public VertxHttpClientBuilder<QuarkusHttpClientFactory> newBuilder() {
+        return new VertxHttpClientBuilder<>(this, vertx);
+    }
+
+    @Override
+    public int priority() {
+        return 1;
+    }
+}

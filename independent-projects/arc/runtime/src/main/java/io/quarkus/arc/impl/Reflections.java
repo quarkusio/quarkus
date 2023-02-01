Ladicek/quarkus-jakarta-arc

ArC: fix generating Bean.create() to wrap checked exceptions in CreationException

independent-projects/arc/runtime/src/main/java/io/quarkus/arc/impl/Reflections.java
@@ -21,6 +21,8 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 
+import jakarta.enterprise.inject.CreationException;
+
 /**
  * Neither the class nor its methods are considered a public API and should only be used internally.
  */
@@ -129,7 +131,17 @@ public final class Reflections {
             }
             try {
                 return constructor.newInstance(args);
-            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            } catch (InvocationTargetException e) {
+                Throwable cause = e.getCause();
+                if (cause instanceof RuntimeException) {
+                    throw (RuntimeException) cause;
+                }
+                if (cause instanceof Error) {
+                    throw (Error) cause;
+                }
+                // this method is only used to instantiate beans, so throwing `CreationException` is fine
+                throw new CreationException(cause);
+            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException e) {
                 throw new RuntimeException("Cannot invoke constructor: " + clazz.getName(), e);
             }
         }

independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/bean/create/BeanCreateErrorTest.java
@@ -1,0 +1,86 @@
+package io.quarkus.arc.test.bean.create;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import jakarta.enterprise.context.Dependent;
+import jakarta.enterprise.inject.CreationException;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.test.ArcTestContainer;
+
+public class BeanCreateErrorTest {
+    @RegisterExtension
+    ArcTestContainer container = new ArcTestContainer(CheckedExceptionBean.class, CheckedExceptionProducerBean.class,
+            UncheckedExceptionBean.class, UncheckedExceptionProducerBean.class);
+
+    @Test
+    public void checkedExceptionWrapped() {
+        // Test that the checked exception is wrapped
+
+        CreationException exception = assertThrows(CreationException.class, () -> {
+            Arc.container().instance(CheckedExceptionBean.class);
+        });
+        assertEquals("foo", exception.getCause().getMessage());
+
+        exception = assertThrows(CreationException.class, () -> {
+            Arc.container().instance(BigInteger.class);
+        });
+        assertEquals("bar", exception.getCause().getMessage());
+    }
+
+    @Test
+    public void uncheckedExceptionNotWrapped() {
+        // Test that the unchecked exception is _not_ wrapped
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Arc.container().instance(UncheckedExceptionBean.class);
+        });
+
+        assertThrows(IllegalStateException.class, () -> {
+            Arc.container().instance(BigDecimal.class);
+        });
+    }
+
+    @Dependent
+    static class CheckedExceptionBean {
+        @Inject
+        public CheckedExceptionBean() throws Exception {
+            throw new Exception("foo");
+        }
+    }
+
+    @Dependent
+    static class CheckedExceptionProducerBean {
+        @Produces
+        @Dependent
+        BigInteger produce() throws Exception {
+            throw new Exception("bar");
+        }
+    }
+
+    @Dependent
+    static class UncheckedExceptionBean {
+        @Inject
+        public UncheckedExceptionBean() {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Dependent
+    static class UncheckedExceptionProducerBean {
+        @Produces
+        @Dependent
+        BigDecimal produce() {
+            throw new IllegalStateException();
+        }
+    }
+}

quarkusio/quarkus

independent-projects/arc/runtime/src/main/java/io/quarkus/arc/impl/Reflections.java
@@ -21,6 +21,8 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 
+import javax.enterprise.inject.CreationException;
+
 /**
  * Neither the class nor its methods are considered a public API and should only be used internally.
  */
@@ -129,7 +131,17 @@ public final class Reflections {
             }
             try {
                 return constructor.newInstance(args);
-            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            } catch (InvocationTargetException e) {
+                Throwable cause = e.getCause();
+                if (cause instanceof RuntimeException) {
+                    throw (RuntimeException) cause;
+                }
+                if (cause instanceof Error) {
+                    throw (Error) cause;
+                }
+                // this method is only used to instantiate beans, so throwing `CreationException` is fine
+                throw new CreationException(cause);
+            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException e) {
                 throw new RuntimeException("Cannot invoke constructor: " + clazz.getName(), e);
             }
         }

independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/bean/create/BeanCreateErrorTest.java
@@ -1,0 +1,86 @@
+package io.quarkus.arc.test.bean.create;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.CreationException;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.test.ArcTestContainer;
+
+public class BeanCreateErrorTest {
+    @RegisterExtension
+    ArcTestContainer container = new ArcTestContainer(CheckedExceptionBean.class, CheckedExceptionProducerBean.class,
+            UncheckedExceptionBean.class, UncheckedExceptionProducerBean.class);
+
+    @Test
+    public void checkedExceptionWrapped() {
+        // Test that the checked exception is wrapped
+
+        CreationException exception = assertThrows(CreationException.class, () -> {
+            Arc.container().instance(CheckedExceptionBean.class);
+        });
+        assertEquals("foo", exception.getCause().getMessage());
+
+        exception = assertThrows(CreationException.class, () -> {
+            Arc.container().instance(BigInteger.class);
+        });
+        assertEquals("bar", exception.getCause().getMessage());
+    }
+
+    @Test
+    public void uncheckedExceptionNotWrapped() {
+        // Test that the unchecked exception is _not_ wrapped
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Arc.container().instance(UncheckedExceptionBean.class);
+        });
+
+        assertThrows(IllegalStateException.class, () -> {
+            Arc.container().instance(BigDecimal.class);
+        });
+    }
+
+    @Dependent
+    static class CheckedExceptionBean {
+        @Inject
+        public CheckedExceptionBean() throws Exception {
+            throw new Exception("foo");
+        }
+    }
+
+    @Dependent
+    static class CheckedExceptionProducerBean {
+        @Produces
+        @Dependent
+        BigInteger produce() throws Exception {
+            throw new Exception("bar");
+        }
+    }
+
+    @Dependent
+    static class UncheckedExceptionBean {
+        @Inject
+        public UncheckedExceptionBean() {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Dependent
+    static class UncheckedExceptionProducerBean {
+        @Produces
+        @Dependent
+        BigDecimal produce() {
+            throw new IllegalStateException();
+        }
+    }
+}
01396ac2e0288c5ba60c2dfa79717f1a33a20fdb
Martin Kouba <mkouba@redhat.com>
2023-02-08 16:24:03 +0100

    ArC: detect unsupported Inject annotations
    - including javax.inject.Inject, com.google.inject.Inject,
    com.oracle.svm.core.annotate.Inject and
    org.gradle.internal.impldep.javax.inject.Inject

---
+++ extensions/arc/deployment/src/test/java/io/quarkus/arc/test/wrongannotations/BeanWithIncorrectInject.java
@@ -0,0 +1,21 @@
+package io.quarkus.arc.test.wrongannotations;
+
+import jakarta.enterprise.inject.spi.BeanManager;
+
+import com.google.inject.Inject;
+
+public class BeanWithIncorrectInject {
+
+    @Inject
+    BeanManager bm1;
+
+    @javax.inject.Inject
+    BeanManager bm2;
+
+    @com.oracle.svm.core.annotate.Inject
+    BeanManager bm3;
+
+    @org.gradle.internal.impldep.javax.inject.Inject
+    BeanManager bm4;
+
+}

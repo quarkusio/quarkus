8177fed25022ba2b352b3f613f9234f92f3e8ab2
Sanne Grinovero <sanne@hibernate.org>
2023-02-14 12:17:08 +0000

    No longer need environment properties to control the BytecodeProvider selection

--- extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/HibernateOrmProcessor.java
+++ extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/HibernateOrmProcessor.java
@@ -301,16 +301,6 @@ public final class HibernateOrmProcessor {
         return new AdditionalIndexedClassesBuildItem(ClassNames.QUARKUS_PERSISTENCE_UNIT.toString());
     }
 
-    // We do our own enhancement during the compilation phase, so disable any
-    // automatic entity enhancement by Hibernate ORM
-    // This has to happen before Hibernate ORM classes are initialized: see
-    // org.hibernate.cfg.Environment#BYTECODE_PROVIDER_INSTANCE
-    @BuildStep
-    public SystemPropertyBuildItem enforceDisableRuntimeEnhancer() {
-        return new SystemPropertyBuildItem(AvailableSettings.BYTECODE_PROVIDER,
-                org.hibernate.cfg.Environment.BYTECODE_PROVIDER_NAME_NONE);
-    }
-
     @BuildStep
     public void enrollBeanValidationTypeSafeActivatorForReflection(Capabilities capabilities,
             BuildProducer<ReflectiveClassBuildItem> reflectiveClasses) {

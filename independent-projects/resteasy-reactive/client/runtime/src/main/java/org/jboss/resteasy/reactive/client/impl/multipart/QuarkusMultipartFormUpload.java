a9a6c97dc068d3007902817984a025c0dceacca6
Jose <josecarvajalhilario@gmail.com>
2023-02-23 11:22:50 +0100

    Avoid to send an extra request with empty buffer

--- independent-projects/resteasy-reactive/client/runtime/src/main/java/org/jboss/resteasy/reactive/client/impl/multipart/QuarkusMultipartFormUpload.java
+++ independent-projects/resteasy-reactive/client/runtime/src/main/java/org/jboss/resteasy/reactive/client/impl/multipart/QuarkusMultipartFormUpload.java
@@ -2,7 +2,6 @@ package org.jboss.resteasy.reactive.client.impl.multipart;
 
 import java.io.File;
 import java.nio.charset.Charset;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.UnpooledByteBufAllocator;
@@ -11,6 +10,7 @@ import io.netty.handler.codec.http.HttpConstants;
 import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.LastHttpContent;
 import io.netty.handler.codec.http.multipart.DefaultHttpDataFactory;
 import io.netty.handler.codec.http.multipart.FileUpload;
 import io.netty.handler.codec.http.multipart.MemoryFileUpload;
@@ -148,25 +148,24 @@ public class QuarkusMultipartFormUpload implements ReadStream<Buffer>, Runnable 
             throw new IllegalArgumentException("Wrong Vert.x context used for multipart upload. Expected: " + context +
                     ", actual: " + Vertx.currentContext());
         }
-        AtomicInteger counter = new AtomicInteger();
         while (!ended) {
             if (encoder.isChunked()) {
                 try {
                     HttpContent chunk = encoder.readChunk(ALLOC);
                     if (chunk == PausableHttpPostRequestEncoder.WAIT_MARKER) {
                         return; // resumption will be scheduled by encoder
-                    }
-                    ByteBuf content = chunk.content();
-                    Buffer buff = Buffer.buffer(content);
-                    counter.incrementAndGet();
-                    boolean writable = pending.write(buff);
-                    if (encoder.isEndOfInput()) {
+                    } else if (chunk == LastHttpContent.EMPTY_LAST_CONTENT || encoder.isEndOfInput()) {
                         ended = true;
                         request = null;
                         encoder = null;
                         pending.write(InboundBuffer.END_SENTINEL);
-                    } else if (!writable) {
-                        break;
+                    } else {
+                        ByteBuf content = chunk.content();
+                        Buffer buff = Buffer.buffer(content);
+                        boolean writable = pending.write(buff);
+                        if (!writable) {
+                            break;
+                        }
                     }
                 } catch (Exception e) {
                     handleError(e);

437711b04544797881823356b5f97b1b0498e154
Guillaume Smet <guillaume.smet@gmail.com>
2023-03-02 10:24:28 +0100

    Upgrade to maven-compiler-plugin 3.11.0

--- independent-projects/tools/devtools-common/src/main/java/io/quarkus/devtools/commands/CreateExtension.java
+++ independent-projects/tools/devtools-common/src/main/java/io/quarkus/devtools/commands/CreateExtension.java
@@ -60,7 +60,7 @@ public class CreateExtension {
     public static final String DEFAULT_QUARKIVERSE_GUIDE_URL = "https://quarkiverse.github.io/quarkiverse-docs/%s/dev/";
 
     private static final String DEFAULT_SUREFIRE_PLUGIN_VERSION = "3.0.0-M9";
-    private static final String DEFAULT_COMPILER_PLUGIN_VERSION = "3.10.1";
+    private static final String DEFAULT_COMPILER_PLUGIN_VERSION = "3.11.0";
 
     private final QuarkusExtensionCodestartProjectInputBuilder builder = QuarkusExtensionCodestartProjectInput.builder();
     private final Path baseDir;

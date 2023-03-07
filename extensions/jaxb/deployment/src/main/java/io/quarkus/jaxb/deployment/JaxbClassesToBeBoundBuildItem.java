5e3926404bb46857f9f062ab11f502cdbfb3bfd2
Peter Palaga <ppalaga@redhat.com>
2023-03-07 22:36:45 +0100

    Validate the default JAXBContext at build time only if it is really used in the application; do not validate if user provides his own JAXBContext bean or if there is no JAXBContext injection point #31646

--- extensions/jaxb/deployment/src/main/java/io/quarkus/jaxb/deployment/JaxbClassesToBeBoundBuildItem.java
+++ extensions/jaxb/deployment/src/main/java/io/quarkus/jaxb/deployment/JaxbClassesToBeBoundBuildItem.java
@@ -1,19 +1,24 @@
 package io.quarkus.jaxb.deployment;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 import io.quarkus.builder.item.MultiBuildItem;
 
 /**
- * List of classes to be bound in the JAXB context.
+ * List of class names to be bound in the JAXB context. Note that some of the class names can be removed via
+ * {@code quarkus.jaxb.exclude-classes}.
+ *
+ * @see FilteredJaxbClassesToBeBoundBuildItem
  */
 public final class JaxbClassesToBeBoundBuildItem extends MultiBuildItem {
 
     private final List<String> classes;
 
     public JaxbClassesToBeBoundBuildItem(List<String> classes) {
-        this.classes = Objects.requireNonNull(classes);
+        this.classes = Objects.requireNonNull(Collections.unmodifiableList(new ArrayList<>(classes)));
     }
 
     public List<String> getClasses() {

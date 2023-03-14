2239362f518c992cfbbe6ff566d63de923875594
Martin Kouba <mkouba@redhat.com>
2023-03-14 21:08:12 +0100

    Hibernate reactive panache - validation of interceptor bindings
    - the build only fails if any of [WithSession, WithTransaction,
    WithSessionOnDemand] is declared on a method that does not return Uni
    - if declared on a class then the methods that do not return Uni are
    effectively ignored

---
+++ extensions/panache/hibernate-reactive-panache/deployment/src/test/java/io/quarkus/hibernate/reactive/panache/test/WithTransactionClassLevelValidationTest.java
@@ -0,0 +1,41 @@
+package io.quarkus.hibernate.reactive.panache.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.arc.Unremovable;
+import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.quarkus.test.QuarkusUnitTest;
+
+public class WithTransactionClassLevelValidationTest {
+
+    @RegisterExtension
+    static QuarkusUnitTest runner = new QuarkusUnitTest()
+            .withApplicationRoot(root -> root
+                    .addClasses(Bean.class));
+
+    @Inject
+    Bean bean;
+
+    @Test
+    public void testBindingIgnored() {
+        assertEquals("ok", bean.ping());
+    }
+
+    @WithTransaction
+    @Unremovable
+    @ApplicationScoped
+    static class Bean {
+
+        // this method is just ignored by the interceptor
+        String ping() {
+            return "ok";
+        }
+
+    }
+}

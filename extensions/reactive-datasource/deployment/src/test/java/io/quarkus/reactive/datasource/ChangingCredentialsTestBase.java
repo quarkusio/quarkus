3e5afbf156eace41df56264f9cab1b0852dec81e
Thomas Segismont <tsegismo@redhat.com>
2023-03-15 19:26:31 +0100

    Reactive Datasource: support CredentialsProvider changing values
    Instead of:
    
    - creating pgConnectOptions after calling the CredentialsProvider on startup
    - using static pool configuration
    
    We:
    
    - use dynamic pool configuration
    - call the Credentials provider every time a new connection must be created

---
+++ extensions/reactive-datasource/deployment/src/test/java/io/quarkus/reactive/datasource/ChangingCredentialsTestBase.java
@@ -0,0 +1,36 @@
+package io.quarkus.reactive.datasource;
+
+import static io.restassured.RestAssured.given;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+
+public abstract class ChangingCredentialsTestBase {
+
+    private String user1;
+    private String user2;
+
+    protected ChangingCredentialsTestBase(String user1, String user2) {
+        this.user1 = user1;
+        this.user2 = user2;
+    }
+
+    @Test
+    public void testConnect() throws Exception {
+        given()
+                .when().get("/test")
+                .then()
+                .statusCode(200)
+                .body(CoreMatchers.equalTo(user1));
+
+        SECONDS.sleep(2); // sleep longer than pool idle connection timeout
+
+        given()
+                .when().get("/test")
+                .then()
+                .statusCode(200)
+                .body(CoreMatchers.equalTo(user2));
+    }
+
+}

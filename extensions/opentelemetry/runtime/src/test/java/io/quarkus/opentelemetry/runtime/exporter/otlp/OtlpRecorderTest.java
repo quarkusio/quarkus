d86aaa426bc41e23f1d6d7e0623a9764fd7a727f
brunobat <brunobat@gmail.com>
2023-03-16 16:18:54 +0000

    Fix endpoint resolution and improve documentation

--- extensions/opentelemetry/runtime/src/test/java/io/quarkus/opentelemetry/runtime/exporter/otlp/OtlpRecorderTest.java
+++ extensions/opentelemetry/runtime/src/test/java/io/quarkus/opentelemetry/runtime/exporter/otlp/OtlpRecorderTest.java
@@ -1,5 +1,6 @@
 package io.quarkus.opentelemetry.runtime.exporter.otlp;
 
+import static io.quarkus.opentelemetry.runtime.config.runtime.exporter.OtlpExporterRuntimeConfig.Constants.DEFAULT_GRPC_BASE_URI;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Optional;
@@ -8,50 +9,58 @@ import org.junit.jupiter.api.Test;
 
 import io.quarkus.opentelemetry.runtime.config.runtime.exporter.OtlpExporterRuntimeConfig;
 import io.quarkus.opentelemetry.runtime.config.runtime.exporter.OtlpExporterTracesConfig;
-import io.quarkus.runtime.LaunchMode;
 
 class OtlpRecorderTest {
 
     @Test
     public void resolveEndpoint_legacyWins() {
         assertEquals("http://localhost:1111/",
-                OtlpRecorder.resolveEndpoint(LaunchMode.NORMAL, createOtlpExporterRuntimeConfig(
-                        "http://localhost:4317/",
+                OtlpRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
+                        DEFAULT_GRPC_BASE_URI,
                         "http://localhost:1111/",
                         "http://localhost:2222/")));
     }
 
     @Test
+    public void resolveEndpoint_newWins() {
+        assertEquals("http://localhost:2222/",
+                OtlpRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
+                        "http://localhost:1111/",
+                        DEFAULT_GRPC_BASE_URI,
+                        "http://localhost:2222/")));
+    }
+
+    @Test
+    public void resolveEndpoint_globalWins() {
+        assertEquals("http://localhost:1111/",
+                OtlpRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
+                        "http://localhost:1111/",
+                        DEFAULT_GRPC_BASE_URI,
+                        DEFAULT_GRPC_BASE_URI)));
+    }
+
+    @Test
     public void resolveEndpoint_legacyTraceWins() {
         assertEquals("http://localhost:2222/",
-                OtlpRecorder.resolveEndpoint(LaunchMode.NORMAL, createOtlpExporterRuntimeConfig(
-                        "http://localhost:4317/",
+                OtlpRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
+                        DEFAULT_GRPC_BASE_URI,
                         null,
                         "http://localhost:2222/")));
     }
 
     @Test
     public void resolveEndpoint_legacyGlobalWins() {
-        assertEquals("http://localhost:4317/",
-                OtlpRecorder.resolveEndpoint(LaunchMode.NORMAL, createOtlpExporterRuntimeConfig(
-                        "http://localhost:4317/",
+        assertEquals(DEFAULT_GRPC_BASE_URI,
+                OtlpRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
+                        DEFAULT_GRPC_BASE_URI,
                         null,
                         null)));
     }
 
     @Test
     public void resolveEndpoint_testIsSet() {
-        assertEquals("http://localhost:4317/",
-                OtlpRecorder.resolveEndpoint(LaunchMode.DEVELOPMENT, createOtlpExporterRuntimeConfig(
-                        null,
-                        null,
-                        null)));
-    }
-
-    @Test
-    public void resolveEndpoint_NothingSet() {
-        assertEquals("",
-                OtlpRecorder.resolveEndpoint(LaunchMode.NORMAL, createOtlpExporterRuntimeConfig(
+        assertEquals(DEFAULT_GRPC_BASE_URI,
+                OtlpRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
                         null,
                         null,
                         null)));

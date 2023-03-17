2ab2cf5c87c5e3e27ceecdcc2f09db97b9164e8f
Guillaume Nieser <16840894+gnieser@users.noreply.github.com>
2023-03-17 11:43:41 +0100

    Enhance exclude-classes property to exclude packages from the default JAXBContext

---
+++ extensions/jaxb/deployment/src/test/java/io/quarkus/jaxb/deployment/ClassExcludeTest.java
@@ -0,0 +1,17 @@
+package io.quarkus.jaxb.deployment;
+
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class ClassExcludeTest extends AbstractJaxbContextTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withApplicationRoot((jar) -> jar
+                    .addClasses(
+                            io.quarkus.jaxb.deployment.one.Model.class,
+                            io.quarkus.jaxb.deployment.two.Model.class)
+                    .addPackage("io.quarkus.jaxb.deployment.info"))
+            .overrideConfigKey("quarkus.jaxb.exclude-classes", "io.quarkus.jaxb.deployment.two.Model");
+}

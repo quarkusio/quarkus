fe0d5b848828344ee1f57f246730d72c610a7262
Alessandro Moscatelli <alessandro.moscatelli@live.com>
2023-03-21 20:22:08 +0100

    automatic websocket codecs reflection registration

--- extensions/websockets/server/deployment/src/main/java/io/quarkus/websockets/deployment/ServerWebSocketProcessor.java
+++ extensions/websockets/server/deployment/src/main/java/io/quarkus/websockets/deployment/ServerWebSocketProcessor.java
@@ -1,9 +1,15 @@
 package io.quarkus.websockets.deployment;
 
+import static org.jboss.jandex.AnnotationTarget.Kind.CLASS;
+
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import jakarta.websocket.Decoder;
+import jakarta.websocket.Encoder;
 import jakarta.websocket.server.ServerEndpoint;
 
 import org.jboss.jandex.AnnotationInstance;
@@ -20,6 +26,8 @@ import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveHierarchyBuildItem;
+import io.quarkus.deployment.util.JandexUtil;
 import io.quarkus.vertx.http.deployment.FilterBuildItem;
 import io.quarkus.websockets.client.deployment.AnnotatedWebsocketEndpointBuildItem;
 import io.quarkus.websockets.client.deployment.ServerWebSocketContainerBuildItem;
@@ -31,6 +39,17 @@ import io.quarkus.websockets.runtime.WebsocketServerRecorder;
 public class ServerWebSocketProcessor {
 
     private static final DotName SERVER_ENDPOINT = DotName.createSimple(ServerEndpoint.class.getName());
+
+    public static final Collection<DotName> CODECS = Arrays.asList(
+            Decoder.TextStream.class,
+            Decoder.Text.class,
+            Decoder.BinaryStream.class,
+            Decoder.Binary.class,
+            Encoder.TextStream.class,
+            Encoder.Text.class,
+            Encoder.BinaryStream.class,
+            Encoder.Binary.class).stream().map(Class::getName).map(DotName::createSimple)
+            .collect(Collectors.toList());
 
     @BuildStep
     void holdConfig(BuildProducer<FeatureBuildItem> feature) {
@@ -52,6 +71,19 @@ public class ServerWebSocketProcessor {
                 }
             }
         }
+    }
+
+    @BuildStep
+    void scanForCodecs(CombinedIndexBuildItem index,
+            BuildProducer<ReflectiveHierarchyBuildItem> reflectiveHierarchyBuildItemProducer) {
+        CODECS.stream().forEach(
+                codec -> index.getIndex().getAllKnownImplementors(codec).stream()
+                        .filter(implementor -> CLASS.equals(implementor.kind()) && !Modifier.isAbstract(implementor.flags()))
+                        .forEach(implementor -> JandexUtil.resolveTypeParameters(
+                                implementor.name(),
+                                codec, index.getComputingIndex()).forEach(
+                                        typeParameter -> reflectiveHierarchyBuildItemProducer.produce(
+                                                new ReflectiveHierarchyBuildItem.Builder().type(typeParameter).build()))));
     }
 
     @BuildStep

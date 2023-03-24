b3b5c039a26b3c094a6d00517f40943a44bf70c8
Jose <josecarvajalhilario@gmail.com>
2023-03-24 07:19:52 +0100

    Allow using the annotation `@PartFilename` on method parameters
    Fix https://github.com/quarkusio/quarkus/issues/32033

--- independent-projects/resteasy-reactive/common/runtime/src/main/java/org/jboss/resteasy/reactive/PartFilename.java
+++ independent-projects/resteasy-reactive/common/runtime/src/main/java/org/jboss/resteasy/reactive/PartFilename.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * Used on fields of {@link MultipartForm} POJOs to designate the filename of a part.
  * This is only applicable in the client, not the server.
  */
-@Target({ ElementType.FIELD })
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PartFilename {
     String value();

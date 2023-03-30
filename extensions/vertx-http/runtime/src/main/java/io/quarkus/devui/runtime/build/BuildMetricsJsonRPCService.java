dbe8b8aa31f93726db5ac851720a27356f2e726b
Guillaume Smet <guillaume.smet@gmail.com>
2023-03-30 14:29:07 +0200

    Dev UI - Make BuildMetricsJsonRPCService reactive

--- extensions/vertx-http/runtime/src/main/java/io/quarkus/devui/runtime/build/BuildMetricsJsonRPCService.java
+++ extensions/vertx-http/runtime/src/main/java/io/quarkus/devui/runtime/build/BuildMetricsJsonRPCService.java
@@ -4,10 +4,14 @@ import java.util.Map;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+
 @ApplicationScoped
 public class BuildMetricsJsonRPCService {
 
-    public Map<String, Object> getBuildStepsMetrics() {
-        return BuildMetricsDevUIController.get().getBuildStepsMetrics();
+    public Uni<Map<String, Object>> getBuildStepsMetrics() {
+        return Uni.createFrom().item(() -> BuildMetricsDevUIController.get().getBuildStepsMetrics())
+                .runSubscriptionOn(Infrastructure.getDefaultWorkerPool());
     }
 }

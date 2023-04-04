9cc073612e1d0a7af4e4f31d5e0de9adf01b96a4
Ioannis Canellos <iocanel@gmail.com>
2023-04-04 08:36:03 +0300

    fix: kind command name

--- devtools/cli/src/main/java/io/quarkus/cli/deploy/Kind.java
+++ devtools/cli/src/main/java/io/quarkus/cli/deploy/Kind.java
@@ -3,7 +3,7 @@ package io.quarkus.cli.deploy;
 import io.quarkus.cli.BuildToolContext;
 import picocli.CommandLine;
 
-@CommandLine.Command(name = "kind", sortOptions = false, showDefaultValues = true, mixinStandardHelpOptions = false, header = "Perform the deploy action on minikube.", description = "%n"
+@CommandLine.Command(name = "kind", sortOptions = false, showDefaultValues = true, mixinStandardHelpOptions = false, header = "Perform the deploy action on kind.", description = "%n"
         + "The command will deploy the application on kind.", footer = "%n"
                 + "For example (using default values), it will create a Deployment named '<project.artifactId>' using the image with REPOSITORY='${user.name}/<project.artifactId>' and TAG='<project.version>' and will deploy it to the target cluster.", headerHeading = "%n", commandListHeading = "%nCommands:%n", synopsisHeading = "%nUsage: ", parameterListHeading = "%n", optionListHeading = "Options:%n")
 public class Kind extends BaseKubernetesDeployCommand {

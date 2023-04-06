fbecf74e719e07406a81cd604b6e6b06d982e03e
Georgios Andrianakis <geoand@gmail.com>
2023-04-06 17:28:48 +0300

    Add a warning about usage of NativeImageRunnerBuildItem

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/builditem/NativeImageRunnerBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/builditem/NativeImageRunnerBuildItem.java
@@ -5,6 +5,11 @@ import io.quarkus.deployment.pkg.steps.NativeImageBuildRunner;
 
 /**
  * The resolved factory for the native image runner.
+ * <p>
+ * Warning: This build item should not be consumed without the use of
+ * {@link io.quarkus.deployment.pkg.steps.NativeOrNativeSourcesBuild} in the {@code onlyIf} method of
+ * {@link io.quarkus.builder.BuildStep} because that leads to Quarkus having to resolve the container image runtime
+ * unnecessarily.
  */
 public final class NativeImageRunnerBuildItem extends SimpleBuildItem {
     private final NativeImageBuildRunner buildRunner;

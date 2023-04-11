ade20108a8269846bb8750da2608bcd6b99d804b
Ioannis Canellos <iocanel@gmail.com>
2023-04-11 14:16:20 +0300

    chore: typos and jdoc imp for DeploymentDecorator

--- extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/DeploymentDecorator.java
+++ extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/DeploymentDecorator.java
@@ -7,14 +7,13 @@ import io.fabric8.kubernetes.client.Config;
 public interface DeploymentDecorator<D extends DeploymentDecorator<D>> {
 
     /**
-     * Check if the decoratora has been applied.
-     *
-     * @return true if it has.
+     * Check if the decorator has been applied.
+     * @return true if the decorator has been applied.
      */
     public boolean isApplied();
 
     /**
-     * An optional message to display when applying the Decrator.
+     * An optional message to display when applying the decorator.
      * As this decorator is not affecting files, its usually a good idea
      * to log a message so that the user know what changed during deployment.
      * This method is meant to be used for that purpose.

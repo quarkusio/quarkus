574948eb15dbd0e097933709f2bc545924a10283
Robert Stupp <snazy@snazy.de>
2023-04-14 13:09:16 +0200

    Correct JDK-19 -> 20 exclusions (Gradle 8.1)
    (cherry picked from commit ccf168a0e5cb6d3fe6cec59fcfd50b919a888c8a)

--- integration-tests/gradle/src/test/java/io/quarkus/gradle/devmode/MultiModuleKotlinProjectDevModeTest.java
+++ integration-tests/gradle/src/test/java/io/quarkus/gradle/devmode/MultiModuleKotlinProjectDevModeTest.java
@@ -2,12 +2,9 @@ package io.quarkus.gradle.devmode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Disabled;
-
 import com.google.common.collect.ImmutableMap;
 
-@Disabled
-@org.junit.jupiter.api.Tag("failsOnJDK19")
+@org.junit.jupiter.api.Tag("failsOnJDK20")
 public class MultiModuleKotlinProjectDevModeTest extends QuarkusDevGradleTestBase {
 
     @Override

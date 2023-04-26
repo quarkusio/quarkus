a6f898b6e15b3595254804357d412428074e8100
Alex Martel <13215031+manofthepeace@users.noreply.github.com>
2023-04-26 15:22:20 -0400

    Add possibility to parse days and milliseconds with config DurationConverter

--- core/runtime/src/main/java/io/quarkus/runtime/configuration/DurationConverter.java
+++ core/runtime/src/main/java/io/quarkus/runtime/configuration/DurationConverter.java
@@ -17,15 +17,18 @@ import org.eclipse.microprofile.config.spi.Converter;
 @Priority(DEFAULT_QUARKUS_CONVERTER_PRIORITY)
 public class DurationConverter implements Converter<Duration>, Serializable {
     private static final long serialVersionUID = 7499347081928776532L;
+    private static final String PERIOD = "P";
     private static final String PERIOD_OF_TIME = "PT";
     private static final Pattern DIGITS = Pattern.compile("^[-+]?\\d+$");
-    private static final Pattern START_WITH_DIGITS = Pattern.compile("^[-+]?\\d+.*");
+    private static final Pattern DIGITS_AND_UNIT = Pattern.compile("^[-+]?\\d+(?:\\.\\d+)?(?i)[hms]$");
+    private static final Pattern DAYS = Pattern.compile("^[-+]?\\d+(?i)d$");
+    private static final Pattern MILLIS = Pattern.compile("^[-+]?\\d+(?i)ms$");
 
     public DurationConverter() {
     }
 
     /**
-     * The converter accepts a value which start with a number by implicitly appending `PT` to it.
+     * The converter accepts a value which start with a number by implicitly appending `PT` to it or `P` for days.
      * If the value consists only of a number, it implicitly treats the value as seconds.
      * Otherwise, tries to convert the value assuming that it is in the accepted ISO-8601 duration format.
      *
@@ -38,7 +41,7 @@ public class DurationConverter implements Converter<Duration>, Serializable {
     }
 
     /**
-     * Converts a value which start with a number by implicitly appending `PT` to it.
+     * Converts a value which start with a number by implicitly appending `PT` to it or `P` for days.
      * If the value consists only of a number, it implicitly treats the value as seconds.
      * Otherwise, tries to convert the value assuming that it is in the accepted ISO-8601 duration format.
      *
@@ -52,11 +55,15 @@ public class DurationConverter implements Converter<Duration>, Serializable {
         }
         if (DIGITS.asPredicate().test(value)) {
             return Duration.ofSeconds(Long.parseLong(value));
+        } else if (MILLIS.asPredicate().test(value)) {
+            return Duration.ofMillis(Long.parseLong(value.substring(0, value.length() - 2)));
         }
 
         try {
-            if (START_WITH_DIGITS.asPredicate().test(value)) {
+            if (DIGITS_AND_UNIT.asPredicate().test(value)) {
                 return Duration.parse(PERIOD_OF_TIME + value);
+            } else if (DAYS.asPredicate().test(value)) {
+                return Duration.parse(PERIOD + value);
             }
 
             return Duration.parse(value);

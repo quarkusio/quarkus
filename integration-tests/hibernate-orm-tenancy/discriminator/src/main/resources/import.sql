7f1d4517e6ad459ed1a8394170019cf93beb8f77
humberto <humberto.pereira@codequanthum.com>
2023-04-30 17:14:26 +0200

    Fix issue from automatic review commit (flyway)

--- integration-tests/hibernate-orm-tenancy/discriminator/src/main/resources/import.sql
+++ integration-tests/hibernate-orm-tenancy/discriminator/src/main/resources/import.sql
@@ -1,10 +1,3 @@
-CREATE TABLE known_fruits
-(
-  tenantId  VARCHAR(40),
-  id        INT,
-  name      VARCHAR(40)
-);
-CREATE SEQUENCE known_fruits_id_seq START WITH 7;
 INSERT INTO known_fruits(tenantId, id, name) VALUES ('base', 1, 'Cherry');
 INSERT INTO known_fruits(tenantId, id, name) VALUES ('base', 2, 'Apple');
 INSERT INTO known_fruits(tenantId, id, name) VALUES ('base', 3, 'Banana');
@@ -12,4 +5,3 @@ INSERT INTO known_fruits(tenantId, id, name) VALUES ('base', 3, 'Banana');
 INSERT INTO known_fruits(tenantId, id, name) VALUES ('mycompany', 4, 'Avocado');
 INSERT INTO known_fruits(tenantId, id, name) VALUES ('mycompany', 5, 'Apricots');
 INSERT INTO known_fruits(tenantId, id, name) VALUES ('mycompany', 6, 'Blackberries');
-

fcd28088b154c6bc5c1348e7c018409f4bdbf60d
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-05-05 17:00:45 +1000

    Fix Hibernate Reactive dev mode
    The session is being cached in a static so after dev mode restart users get a closed session.

--- extensions/panache/hibernate-reactive-panache-common/runtime/src/main/java/io/quarkus/hibernate/reactive/panache/common/runtime/PanacheHibernateRecorder.java
+++ extensions/panache/hibernate-reactive-panache-common/runtime/src/main/java/io/quarkus/hibernate/reactive/panache/common/runtime/PanacheHibernateRecorder.java
@@ -3,11 +3,21 @@ package io.quarkus.hibernate.reactive.panache.common.runtime;
 import java.util.Map;
 import java.util.Set;
 
+import io.quarkus.runtime.ShutdownContext;
 import io.quarkus.runtime.annotations.Recorder;
 
 @Recorder
 public class PanacheHibernateRecorder {
     public void setNamedQueryMap(Map<String, Set<String>> namedQueryMap) {
         NamedQueryUtil.setNamedQueryMap(namedQueryMap);
+    }
+
+    public void clear(ShutdownContext shutdownContext) {
+        shutdownContext.addShutdownTask(new Runnable() {
+            @Override
+            public void run() {
+                SessionOperations.clear();
+            }
+        });
     }
 }

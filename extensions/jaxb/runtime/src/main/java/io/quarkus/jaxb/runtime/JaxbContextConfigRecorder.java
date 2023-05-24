d4fe4694e807651e619bab5c071b07971f8643c6
Avinash Gupta <avinash2fly@gmail.com>
2023-05-24 22:28:09 +1000

    fix class check to return existing marshaller
    It used to create new marshaller everytime due to a typo.
    
    (cherry picked from commit 8acf45f7e0c93d4e71d96e6181c202fbe6febbc9)

--- extensions/jaxb/runtime/src/main/java/io/quarkus/jaxb/runtime/JaxbContextConfigRecorder.java
+++ extensions/jaxb/runtime/src/main/java/io/quarkus/jaxb/runtime/JaxbContextConfigRecorder.java
@@ -24,6 +24,6 @@ public class JaxbContextConfigRecorder {
     }
 
     public static boolean isClassBound(Class<?> clazz) {
-        return classesToBeBound.contains(clazz.getName());
+        return classesToBeBound.contains(clazz);
     }
 }

fea1aeeaa69e67be4ee8e6b0cb0d9fd7048d1055
Marc Nuri <marc@marcnuri.com>
2023-06-09 15:29:22 +0200

    feat: static KubernetesResource class registration in synthetic bean
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- extensions/kubernetes-client/runtime/src/main/java/io/quarkus/kubernetes/client/runtime/KubernetesSerializationProducer.java
+++ extensions/kubernetes-client/runtime/src/main/java/io/quarkus/kubernetes/client/runtime/KubernetesSerializationProducer.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.fabric8.kubernetes.client.utils.KubernetesSerialization;
 import io.quarkus.arc.DefaultBean;
 import io.quarkus.kubernetes.client.KubernetesClientObjectMapper;
+import io.quarkus.kubernetes.client.KubernetesResources;
 
 @Singleton
 public class KubernetesSerializationProducer {
@@ -15,9 +16,13 @@ public class KubernetesSerializationProducer {
     @DefaultBean
     @Singleton
     @Produces
-    public KubernetesSerialization kubernetesSerialization(@KubernetesClientObjectMapper ObjectMapper objectMapper) {
+    public KubernetesSerialization kubernetesSerialization(
+            @KubernetesClientObjectMapper ObjectMapper objectMapper,
+            @KubernetesResources Class[] kubernetesResources) {
         final var kubernetesSerialization = new KubernetesSerialization(objectMapper, false);
-        KubernetesClientUtils.scanKubernetesResources().forEach(kubernetesSerialization::registerKubernetesResource);
+        for (var kubernetesResource : kubernetesResources) {
+            kubernetesSerialization.registerKubernetesResource(kubernetesResource);
+        }
         return kubernetesSerialization;
     }
 }

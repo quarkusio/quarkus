a62dbadf37535c83295455e3d7612e8d5cad5281
Sergey Beryozkin <sberyozkin@gmail.com>
2023-06-20 20:26:31 +0100

    Support custom claim types in quarkus-test-security-oidc
    (cherry picked from commit e31bb9a4d1a6bf874313a011ae03b6273f0d6530)

--- test-framework/security-oidc/src/main/java/io/quarkus/test/security/oidc/Claim.java
+++ test-framework/security-oidc/src/main/java/io/quarkus/test/security/oidc/Claim.java
@@ -7,7 +7,19 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({})
 public @interface Claim {
+    /**
+     * Claim name
+     */
     String key();
 
+    /**
+     * Claim value
+     */
     String value();
+
+    /**
+     * Claim value type, the value will be converted to String if this type is set to Object.class.
+     * Supported types: String, Integer, int, Long, long, Boolean, boolean, jakarta.json.JsonArray, jakarta.json.JsonObject.
+     */
+    Class<?> type() default Object.class;
 }

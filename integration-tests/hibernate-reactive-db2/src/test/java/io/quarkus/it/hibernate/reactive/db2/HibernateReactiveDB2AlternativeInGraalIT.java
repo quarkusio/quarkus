51fa1a1cb54ca8c87ce1d988c52a46c292467f77
Davide D'Alto <davide@hibernate.org>
2023-06-23 09:54:40 +0200

    [#34032] Remove alternative end-points from integration-tests
    Their purpose was to test the CompletionStage API for Hibernate Reactive
    in Quarkus. But, we decided some time ago
    to only allow the Mutiny API.
    
    Insted of removing the classes, we changed them to use Mutiny, ending
    with the same tests we have in the non alternative end-point.

--- integration-tests/hibernate-reactive-db2/src/test/java/io/quarkus/it/hibernate/reactive/db2/HibernateReactiveDB2AlternativeInGraalIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkus.it.hibernate.reactive.db2;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class HibernateReactiveDB2AlternativeInGraalIT extends HibernateReactiveDB2AlternativeTest {
-}

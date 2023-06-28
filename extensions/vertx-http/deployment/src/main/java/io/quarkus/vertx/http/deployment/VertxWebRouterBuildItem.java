b19bc9b1d8dc557d5307f660588ab2bed702c960
Siva_M7 <SivaM07@users.noreply.github.com>
2023-06-28 17:23:39 +0530

    34261: grpc route to use mainrouter when http.root-path is not '/'
    (cherry picked from commit 79d779aafc95858ac03e6ed3199401094d0fa45a)

--- extensions/vertx-http/deployment/src/main/java/io/quarkus/vertx/http/deployment/VertxWebRouterBuildItem.java
+++ extensions/vertx-http/deployment/src/main/java/io/quarkus/vertx/http/deployment/VertxWebRouterBuildItem.java
@@ -32,7 +32,7 @@ public final class VertxWebRouterBuildItem extends SimpleBuildItem {
      *
      * @return RuntimeValue<Router>
      */
-    RuntimeValue<Router> getMainRouter() {
+    public RuntimeValue<Router> getMainRouter() {
         return mainRouter;
     }
 

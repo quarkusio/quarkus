23ed661d7029be178b929a27c12c63d411b79700
Georgios Andrianakis <geoand@gmail.com>
2023-07-03 13:22:25 +0300

    Fix span export issue when user provided SpanProcessor beans exist
    SpanProcessor implementations are hooked into OTel in Quarkus
    via the AutoConfiguredOpenTelemetrySdkBuilder#addTracerProviderCustomizer
    mechanism, which in turn obtains those processor from CDI.
    However, because LateBoundBatchSpanProcessor (which is the one
    that ends up being responsible for the span export
    via its BatchSpanProcessor delegate) was marked as a
    @DefaultBean, when a user provided SpanProcessor became
    part of the application, the LateBoundBatchSpanProcessor
    was completely ignored.
    The fix is simply to remove @DefaultBean and always have
    LateBoundBatchSpanProcessor be considered as a SpanProcessor.
    
    Fixes: #33407
    (cherry picked from commit bfecf760ca4309444213748a3b9c62f9a5badcb0)

--- extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/exporter/otlp/OtlpExporterProvider.java
+++ extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/exporter/otlp/OtlpExporterProvider.java
@@ -3,14 +3,11 @@ package io.quarkus.opentelemetry.runtime.exporter.otlp;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Singleton;
 
-import io.quarkus.arc.DefaultBean;
-
 @Deprecated
 @Singleton
 public class OtlpExporterProvider {
     @Produces
     @Singleton
-    @DefaultBean
     public LateBoundBatchSpanProcessor batchSpanProcessorForOtlp() {
         return new LateBoundBatchSpanProcessor();
     }

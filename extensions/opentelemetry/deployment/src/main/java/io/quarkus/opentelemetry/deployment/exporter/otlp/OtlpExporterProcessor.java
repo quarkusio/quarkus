859ac575f5ad764965b32c4e34ffdc5c0bfc68a0
A.Moscatelli <A.Moscatelli@AC-AMOSCATELLI1.itmaster.local>
2023-07-07 09:18:15 +0200

    EndUserSpanProcessor integration

--- extensions/opentelemetry/deployment/src/main/java/io/quarkus/opentelemetry/deployment/exporter/otlp/OtlpExporterProcessor.java
+++ extensions/opentelemetry/deployment/src/main/java/io/quarkus/opentelemetry/deployment/exporter/otlp/OtlpExporterProcessor.java
@@ -14,7 +14,9 @@ import org.jboss.jandex.Type;
 
 import io.opentelemetry.sdk.trace.SpanProcessor;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.BuildSteps;
 import io.quarkus.deployment.annotations.ExecutionTime;
@@ -23,6 +25,7 @@ import io.quarkus.opentelemetry.runtime.config.build.OTelBuildConfig;
 import io.quarkus.opentelemetry.runtime.config.build.exporter.OtlpExporterBuildConfig;
 import io.quarkus.opentelemetry.runtime.config.runtime.OTelRuntimeConfig;
 import io.quarkus.opentelemetry.runtime.config.runtime.exporter.OtlpExporterRuntimeConfig;
+import io.quarkus.opentelemetry.runtime.exporter.otlp.EndUserSpanProcessor;
 import io.quarkus.opentelemetry.runtime.exporter.otlp.LateBoundBatchSpanProcessor;
 import io.quarkus.opentelemetry.runtime.exporter.otlp.OtlpRecorder;
 import io.quarkus.vertx.core.deployment.CoreVertxBuildItem;
@@ -39,6 +42,17 @@ public class OtlpExporterProcessor {
                     otelBuildConfig.traces().enabled().orElse(Boolean.TRUE) &&
                     otelBuildConfig.traces().exporter().contains(CDI_VALUE) &&
                     exportBuildConfig.enabled();
+        }
+    }
+
+    @BuildStep
+    void createEndUserSpanProcessor(
+            BuildProducer<AdditionalBeanBuildItem> buildProducer,
+            OTelBuildConfig otelBuildConfig) {
+        if (otelBuildConfig.traces().eusp().enabled().orElse(Boolean.FALSE)) {
+            buildProducer.produce(
+                    AdditionalBeanBuildItem.unremovableOf(
+                            EndUserSpanProcessor.class));
         }
     }
 

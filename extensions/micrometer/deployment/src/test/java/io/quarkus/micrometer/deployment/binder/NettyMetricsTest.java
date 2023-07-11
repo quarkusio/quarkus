d64c02709d39ed3931563d4a94367e7b56da37e9
Ales Justin <ales.justin@gmail.com>
2023-07-11 16:55:45 +0200

    Add memory value check, pending tasks check. Check if exists / found Do await in finally Fill event loop until there are some pending tasks. Get the value directly from the gauge. Use more stable test.

--- extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/deployment/binder/NettyMetricsTest.java
+++ extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/deployment/binder/NettyMetricsTest.java
@@ -1,7 +1,12 @@
 package io.quarkus.micrometer.deployment.binder;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 import jakarta.enterprise.inject.Any;
@@ -11,8 +16,11 @@ import jakarta.inject.Inject;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import io.micrometer.common.docs.KeyName;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
@@ -75,12 +83,69 @@ public class NettyMetricsTest {
             .stream()
             .collect(Collectors.toSet());
 
+    private static final Tag HEAP_MEMORY = Tag.of(AllocatorMemoryKeyNames.MEMORY_TYPE.asString(), "heap");
+    private static final Tag DIRECT_MEMORY = Tag.of(AllocatorMemoryKeyNames.MEMORY_TYPE.asString(), "direct");
+
+    enum AllocatorKeyNames implements KeyName {
+        ID {
+            public String asString() {
+                return "id";
+            }
+        },
+        ALLOCATOR_TYPE {
+            public String asString() {
+                return "allocator.type";
+            }
+        };
+    }
+
+    enum AllocatorMemoryKeyNames implements KeyName {
+        MEMORY_TYPE {
+            public String asString() {
+                return "memory.type";
+            }
+        };
+    }
+
     private void testNettyMetrics(long expected, Class<? extends MeterBinder> mbClass) {
         Assertions.assertFalse(binders.isUnsatisfied());
         long count = binders.stream()
                 .filter(mbClass::isInstance)
                 .count();
         Assertions.assertEquals(expected, count);
+    }
+
+    private static Double getValue(List<Meter> meters, Set<Tag> expected) {
+        for (Meter meter : meters) {
+            List<Tag> tags = meter.getId().getTags();
+            if (tags.containsAll(expected)) {
+                return meter.match(Gauge::value, null, null, null, null, null, null, null, null);
+            }
+        }
+        return null;
+    }
+
+    private static Set<Tag> tags(Set<Tag> tags, Tag tag) {
+        Set<Tag> newTags = new HashSet<>(tags);
+        newTags.add(tag);
+        return newTags;
+    }
+
+    private void testAllocatorMetricsValues(Set<Tag> tags) {
+        List<Meter> meters = registry.getMeters();
+
+        Double heap0 = getValue(meters, tags(tags, HEAP_MEMORY));
+        Assertions.assertNotNull(heap0);
+        Double direct0 = getValue(meters, tags(tags, DIRECT_MEMORY));
+        Assertions.assertNotNull(direct0);
+
+        RestAssured.get("/hello/Netty").then().body(Matchers.equalTo("hello Netty"));
+
+        Double heap1 = getValue(meters, tags(tags, HEAP_MEMORY));
+        Double direct1 = getValue(meters, tags(tags, DIRECT_MEMORY));
+
+        Assertions.assertTrue(heap0 <= heap1);
+        Assertions.assertTrue(direct0 <= direct1);
     }
 
     @Test
@@ -95,56 +160,72 @@ public class NettyMetricsTest {
 
     @Test
     public void testAllocatorMetricsValues() {
-        RestAssured.get("/hello/Netty").then().body(Matchers.equalTo("hello Netty"));
-
-        boolean pbba_found = false;
-        boolean unpbba_found = false;
-        boolean vx_pbba_found = false;
-        boolean vx_unpbba_found = false;
-        List<Meter> meters = registry.getMeters();
-        for (Meter meter : meters) {
-            List<Tag> tags = meter.getId().getTags();
-            if (tags.containsAll(NAM_PBBA_TAGS)) {
-                pbba_found = true;
-            }
-            if (tags.containsAll(NAM_UNPBBA_TAGS)) {
-                unpbba_found = true;
-            }
-            if (tags.containsAll(VX_NAM_PBBA_TAGS)) {
-                vx_pbba_found = true;
-            }
-            if (tags.containsAll(VX_NAM_UNPBBA_TAGS)) {
-                vx_unpbba_found = true;
-            }
-        }
-        Assertions.assertTrue(pbba_found);
-        Assertions.assertTrue(unpbba_found);
-        Assertions.assertTrue(vx_pbba_found);
-        Assertions.assertTrue(vx_unpbba_found);
+        testAllocatorMetricsValues(NAM_PBBA_TAGS);
+        testAllocatorMetricsValues(NAM_UNPBBA_TAGS);
+        testAllocatorMetricsValues(VX_NAM_PBBA_TAGS);
+        testAllocatorMetricsValues(VX_NAM_UNPBBA_TAGS);
     }
 
     @Test
-    public void testEventExecutorMetricsValues() {
+    @Timeout(60L)
+    public void testEventExecutorMetricsValues() throws Exception {
         VertxInternal vi = (VertxInternal) vertx;
         assertEventGroup(vi.getEventLoopGroup());
         assertEventGroup(vi.getAcceptorEventLoopGroup());
     }
 
-    private void assertEventGroup(EventLoopGroup eventLoopGroup) {
-        for (EventExecutor eventExecutor : eventLoopGroup) {
-            if (eventExecutor instanceof SingleThreadEventExecutor) {
-                SingleThreadEventExecutor singleThreadEventExecutor = (SingleThreadEventExecutor) eventExecutor;
-                Tag tag = Tag.of("name", singleThreadEventExecutor.threadProperties().name());
-                boolean found = false;
-                List<Meter> meters = registry.getMeters();
-                for (Meter meter : meters) {
-                    List<Tag> tags = meter.getId().getTags();
-                    if (tags.contains(tag)) {
-                        found = true;
-                    }
+    private void assertEventGroup(EventLoopGroup group) throws Exception {
+        int tasks = 0;
+        for (EventExecutor ee : group) {
+            tasks++;
+        }
+        final CyclicBarrier allPendingTasksAreIn = new CyclicBarrier(tasks + 1);
+        CountDownLatch waitCollectingMeasures = new CountDownLatch(1);
+        List<Future<Future<?>>> pendingTasksCompleted = new ArrayList<>(tasks);
+        for (EventExecutor eventLoop : group) {
+            pendingTasksCompleted.add(eventLoop.submit(() -> {
+                try {
+                    Future<?> pendingTask = eventLoop.submit(() -> {
+                    });
+                    // this executor will have 1 pending task because is still running this current one
+                    allPendingTasksAreIn.await();
+                    waitCollectingMeasures.await();
+                    return pendingTask;
+                } catch (Throwable ignore) {
+                    return null;
                 }
-                Assertions.assertTrue(found);
-            }
+            }));
+        }
+        allPendingTasksAreIn.await();
+        List<Meter> meters = registry.getMeters();
+        // this would return 1 for everyone
+        for (EventExecutor eventLoop : group) {
+            checkMetrics(meters, eventLoop, 1);
+        }
+        waitCollectingMeasures.countDown();
+        for (Future<Future<?>> pendingTaskCompleted : pendingTasksCompleted) {
+            pendingTaskCompleted.get().get();
+        }
+        // this would return 0 for everyone
+        for (EventExecutor eventLoop : group) {
+            checkMetrics(meters, eventLoop, 0);
+        }
+    }
+
+    private void checkMetrics(List<Meter> meters, EventExecutor executor, int expected) {
+        if (executor instanceof SingleThreadEventExecutor) {
+            SingleThreadEventExecutor stee = (SingleThreadEventExecutor) executor;
+
+            int pendingTasks = stee.pendingTasks();
+            Assertions.assertEquals(expected, pendingTasks);
+
+            Tag tag = Tag.of("name", stee.threadProperties().name());
+            Set<Tag> tags = Set.of(tag);
+
+            Double metricsValue = getValue(meters, tags);
+            Assertions.assertNotNull(metricsValue);
+            int mvInt = metricsValue.intValue();
+            Assertions.assertEquals(expected, mvInt);
         }
     }
 }

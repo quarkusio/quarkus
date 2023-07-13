23db3bd9e65b98b5b5f23ee18103a4d2901b0ec3
Guillaume Smet <guillaume.smet@gmail.com>
2023-07-13 12:15:06 +0200

    Update Kotlin to 1.9.0

--- integration-tests/gradle/src/main/resources/conditional-dependencies-kotlin/build.gradle.kts
+++ integration-tests/gradle/src/main/resources/conditional-dependencies-kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.allopen") version "1.8.22"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.allopen") version "1.9.0"
     id("io.quarkus")
 }
 

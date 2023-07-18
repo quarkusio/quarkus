3f9de0c8af37a6bf9a869af117f95b5ae206dd28
Georgios Andrianakis <geoand@gmail.com>
2023-07-18 17:23:12 +0300

    Ensure that DataSource beans dependencies are initialized before the datasources

--- extensions/agroal/runtime/src/main/java/io/quarkus/agroal/runtime/AgroalRecorder.java
+++ extensions/agroal/runtime/src/main/java/io/quarkus/agroal/runtime/AgroalRecorder.java
@@ -1,8 +1,10 @@
 package io.quarkus.agroal.runtime;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import io.agroal.api.AgroalDataSource;
+import io.quarkus.arc.SyntheticCreationalContext;
 import io.quarkus.datasource.runtime.DataSourcesRuntimeConfig;
 import io.quarkus.runtime.annotations.Recorder;
 
@@ -18,13 +20,14 @@ public class AgroalRecorder {
         };
     }
 
-    public Supplier<AgroalDataSource> agroalDataSourceSupplier(String dataSourceName,
+    public Function<SyntheticCreationalContext<AgroalDataSource>, AgroalDataSource> agroalDataSourceSupplier(
+            String dataSourceName,
             @SuppressWarnings("unused") DataSourcesRuntimeConfig dataSourcesRuntimeConfig) {
-        final AgroalDataSource agroalDataSource = DataSources.fromName(dataSourceName);
-        return new Supplier<AgroalDataSource>() {
+        return new Function<>() {
             @Override
-            public AgroalDataSource get() {
-                return agroalDataSource;
+            public AgroalDataSource apply(SyntheticCreationalContext<AgroalDataSource> context) {
+                DataSources dataSources = context.getInjectedReference(DataSources.class);
+                return dataSources.getDataSource(dataSourceName);
             }
         };
     }

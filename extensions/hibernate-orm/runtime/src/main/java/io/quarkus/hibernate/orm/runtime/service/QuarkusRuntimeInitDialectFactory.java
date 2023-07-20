1f77223d4aad101a2e10a5517e3ffa5f0c21c50a
marko-bekhta <marko.prykladna@gmail.com>
2023-07-20 15:21:43 +0200

    Change how actual DB version is checked

--- extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/service/QuarkusRuntimeInitDialectFactory.java
+++ extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/service/QuarkusRuntimeInitDialectFactory.java
@@ -29,13 +29,13 @@ public class QuarkusRuntimeInitDialectFactory implements DialectFactory {
     private final String persistenceUnitName;
     private final Dialect dialect;
     private final Optional<String> datasourceName;
-    private final Optional<DatabaseVersion> buildTimeDbVersion;
+    private final DatabaseVersion buildTimeDbVersion;
 
     private boolean triedToRetrieveDbVersion = false;
     private Optional<DatabaseVersion> actualDbVersion = Optional.empty();
 
     public QuarkusRuntimeInitDialectFactory(String persistenceUnitName, Dialect dialect,
-            Optional<String> datasourceName, Optional<DatabaseVersion> buildTimeDbVersion) {
+            Optional<String> datasourceName, DatabaseVersion buildTimeDbVersion) {
         this.persistenceUnitName = persistenceUnitName;
         this.dialect = dialect;
         this.datasourceName = datasourceName;
@@ -45,29 +45,25 @@ public class QuarkusRuntimeInitDialectFactory implements DialectFactory {
     @Override
     public Dialect buildDialect(Map<String, Object> configValues, DialectResolutionInfoSource resolutionInfoSource)
             throws HibernateException {
-        if (buildTimeDbVersion.isPresent() && actualDbVersion.isEmpty()) {
+        if (actualDbVersion.isEmpty()) {
             this.actualDbVersion = retrieveDbVersion(resolutionInfoSource);
         }
         return dialect;
     }
 
     public void checkActualDbVersion() {
-        if (buildTimeDbVersion.isEmpty()) {
-            // Nothing to check
-            return;
-        }
         if (!triedToRetrieveDbVersion) {
             LOG.warnf("Persistence unit %1$s: Could not retrieve the database version to check it is at least %2$s",
-                    persistenceUnitName, DialectVersions.toString(buildTimeDbVersion.get()));
+                    persistenceUnitName, DialectVersions.toString(buildTimeDbVersion));
             return;
         }
-        if (actualDbVersion.isPresent() && buildTimeDbVersion.get().isAfter(actualDbVersion.get())) {
+        if (actualDbVersion.isPresent() && buildTimeDbVersion.isAfter(actualDbVersion.get())) {
             throw new ConfigurationException(String.format(Locale.ROOT,
                     "Persistence unit '%1$s' was configured to run with a database version"
                             + " of at least '%2$s', but the actual version is '%3$s'."
                             + " Consider upgrading your database.",
                     persistenceUnitName,
-                    DialectVersions.toString(buildTimeDbVersion.get()), DialectVersions.toString(actualDbVersion.get()))
+                    DialectVersions.toString(buildTimeDbVersion), DialectVersions.toString(actualDbVersion.get()))
                     // It shouldn't be possible to reach this code if datasourceName is empty,
                     // but just let's be safe...
                     + (datasourceName.isEmpty() ? ""
@@ -93,7 +89,7 @@ public class QuarkusRuntimeInitDialectFactory implements DialectFactory {
                     databaseMetadata.getDatabaseMinorVersion()));
         } catch (RuntimeException | SQLException e) {
             LOG.warnf(e, "Persistence unit %1$s: Could not retrieve the database version to check it is at least %2$s",
-                    persistenceUnitName, buildTimeDbVersion.get());
+                    persistenceUnitName, buildTimeDbVersion);
             return Optional.empty();
         }
     }

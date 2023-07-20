6fa2c5298a1ac3843da7686e41815c3f69206a0c
Clement Escoffier <clement@apache.org>
2023-07-20 11:56:11 +0200

    Revert to openjdk-17-runtime:1.16 images
    See
    https://catalog.redhat.com/software/containers/ubi8/openjdk-17-runtime/618bdc5f843af1624c4e4ba8?architecture=amd64&container-tabs=overview
    
    (cherry picked from commit 7ea28c5b91aeee856b7eee69c4ad5f4dae0039a5)

--- extensions/container-image/container-image-jib/deployment/src/main/java/io/quarkus/container/image/jib/deployment/JibConfig.java
+++ extensions/container-image/container-image-jib/deployment/src/main/java/io/quarkus/container/image/jib/deployment/JibConfig.java
@@ -16,7 +16,7 @@ public class JibConfig {
     /**
      * The base image to be used when a container image is being produced for the jar build.
      *
-     * When the application is built against Java 17 or higher, {@code registry.access.redhat.com/ubi8/openjdk-17-runtime:1.17}
+     * When the application is built against Java 17 or higher, {@code registry.access.redhat.com/ubi8/openjdk-17-runtime:1.16}
      * is used as the default.
      * Otherwise {@code registry.access.redhat.com/ubi8/openjdk-11-runtime:1.16} is used as the default.
      */

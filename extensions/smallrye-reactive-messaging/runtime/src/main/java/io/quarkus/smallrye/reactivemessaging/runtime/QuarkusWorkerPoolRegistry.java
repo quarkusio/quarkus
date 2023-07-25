3be4c1a6cf6ee78fef9f6839b25b09c187ee46d3
Ozan Gunalp <ozangunalp@gmail.com>
2023-07-25 14:31:03 +0200

    Virtual threads extension providing executor supplier for running virtual threads
    - Handles executor close on shutdown hook
    - Creates named virtual threads

--- extensions/smallrye-reactive-messaging/runtime/src/main/java/io/quarkus/smallrye/reactivemessaging/runtime/QuarkusWorkerPoolRegistry.java
+++ extensions/smallrye-reactive-messaging/runtime/src/main/java/io/quarkus/smallrye/reactivemessaging/runtime/QuarkusWorkerPoolRegistry.java
@@ -1,15 +1,11 @@
 package io.quarkus.smallrye.reactivemessaging.runtime;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.function.Supplier;
 
 import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -21,17 +17,14 @@ import jakarta.inject.Inject;
 
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.logging.Logger;
-import org.slf4j.LoggerFactory;
 
-import io.quarkus.runtime.ExecutorRecorder;
+import io.quarkus.virtual.threads.VirtualThreadsRecorder;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.reactive.messaging.annotations.Blocking;
 import io.smallrye.reactive.messaging.providers.connectors.ExecutionHolder;
 import io.smallrye.reactive.messaging.providers.connectors.WorkerPoolRegistry;
 import io.smallrye.reactive.messaging.providers.helpers.Validation;
-import io.vertx.core.Vertx;
 import io.vertx.core.impl.ConcurrentHashSet;
-import io.vertx.core.impl.ContextInternal;
 import io.vertx.mutiny.core.Context;
 import io.vertx.mutiny.core.WorkerExecutor;
 
@@ -41,7 +34,8 @@ import io.vertx.mutiny.core.WorkerExecutor;
 // TODO: create a different entry for WorkerPoolRegistry than `analyzeWorker` and drop this class
 public class QuarkusWorkerPoolRegistry extends WorkerPoolRegistry {
 
-    private static final Logger logger = Logger.getLogger(QuarkusWorkerPoolRegistry.class);
+    private static final Logger log = Logger.getLogger(WorkerPoolRegistry.class);
+
     private static final String WORKER_CONFIG_PREFIX = "smallrye.messaging.worker";
     private static final String WORKER_CONCURRENCY = "max-concurrency";
     public static final String DEFAULT_VIRTUAL_THREAD_WORKER = "<virtual-thread>";
@@ -57,61 +51,6 @@ public class QuarkusWorkerPoolRegistry extends WorkerPoolRegistry {
         Set<String> set = new ConcurrentHashSet<>();
         set.add(DEFAULT_VIRTUAL_THREAD_WORKER);
         return set;
-    }
-
-    private enum VirtualExecutorSupplier implements Supplier<Executor> {
-        Instance;
-
-        private final Executor executor;
-
-        /**
-         * This method uses reflection in order to allow developers to quickly test quarkus-loom without needing to
-         * change --release, --source, --target flags and to enable previews.
-         * Since we try to load the "Loom-preview" classes/methods at runtime, the application can even be compiled
-         * using java 11 and executed with a loom-compliant JDK.
-         */
-        VirtualExecutorSupplier() {
-            Executor actual;
-            try {
-                var virtual = (Executor) Executors.class.getMethod("newVirtualThreadPerTaskExecutor")
-                        .invoke(this);
-                actual = new Executor() {
-                    @Override
-                    public void execute(Runnable command) {
-                        var context = Vertx.currentContext();
-                        if (!(context instanceof ContextInternal)) {
-                            virtual.execute(command);
-                        } else {
-                            ContextInternal contextInternal = (ContextInternal) context;
-                            virtual.execute(new Runnable() {
-                                @Override
-                                public void run() {
-                                    final var previousContext = contextInternal.beginDispatch();
-                                    try {
-                                        command.run();
-                                    } finally {
-                                        contextInternal.endDispatch(previousContext);
-                                    }
-                                }
-                            });
-                        }
-                    }
-                };
-            } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
-                //quite ugly but works
-                logger.warnf(e, "You weren't able to create an executor that spawns virtual threads, the default" +
-                        " blocking executor will be used, please check that your JDK is compatible with " +
-                        "virtual threads");
-                //if for some reason a class/method can't be loaded or invoked we return the traditional EXECUTOR
-                actual = ExecutorRecorder.getCurrent();
-            }
-            this.executor = actual;
-        }
-
-        @Override
-        public Executor get() {
-            return this.executor;
-        }
     }
 
     public void terminate(
@@ -151,7 +90,7 @@ public class QuarkusWorkerPoolRegistry extends WorkerPoolRegistry {
     }
 
     private <T> Uni<T> runOnVirtualThread(Context currentContext, Uni<T> uni) {
-        return uni.runSubscriptionOn(VirtualExecutorSupplier.Instance.get())
+        return uni.runSubscriptionOn(VirtualThreadsRecorder.getCurrent())
                 .onItemOrFailure().transformToUni((item, failure) -> {
                     return Uni.createFrom().emitter(emitter -> {
                         if (currentContext != null) {
@@ -186,9 +125,8 @@ public class QuarkusWorkerPoolRegistry extends WorkerPoolRegistry {
                     if (executor == null) {
                         executor = executionHolder.vertx().createSharedWorkerExecutor(workerName,
                                 workerConcurrency.get(workerName));
-                        LoggerFactory.getLogger(WorkerPoolRegistry.class)
-                                .info("Created worker pool named " + workerName + " with concurrency of "
-                                        + workerConcurrency.get(workerName));
+                        log.info("Created worker pool named " + workerName + " with concurrency of "
+                                + workerConcurrency.get(workerName));
                         workerExecutors.put(workerName, executor);
                     }
                 }

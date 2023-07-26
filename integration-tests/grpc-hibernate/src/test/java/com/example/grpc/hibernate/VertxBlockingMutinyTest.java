6934915425c440f916a3c11b91c894c95357f792
Guillaume Smet <guillaume.smet@gmail.com>
2023-07-26 17:24:46 +0200

    Disable VertxBlockingMutinyTest as it's very unstable

--- integration-tests/grpc-hibernate/src/test/java/com/example/grpc/hibernate/VertxBlockingMutinyTest.java
+++ integration-tests/grpc-hibernate/src/test/java/com/example/grpc/hibernate/VertxBlockingMutinyTest.java
@@ -1,10 +1,13 @@
 package com.example.grpc.hibernate;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.grpc.test.utils.VertxGRPCTestProfile;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 
 @QuarkusTest
 @TestProfile(VertxGRPCTestProfile.class)
+@Disabled
 public class VertxBlockingMutinyTest extends BlockingMutinyTestBase {
 }

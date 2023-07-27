5aeef321be873d5e2dd2a2dbf734c826896ffb45
Guillaume Smet <guillaume.smet@gmail.com>
2023-07-27 11:33:36 +0200

    Use grep and not ggrep in filter-virtual-threads-tests-json.sh
    See https://github.com/quarkusio/quarkus/actions/runs/5672786922/job/15373773822

--- .github/filter-virtual-threads-tests-json.sh
+++ .github/filter-virtual-threads-tests-json.sh
@@ -26,7 +26,7 @@ EXPR='((?:(?<=^)|(?<=,)|(?<=, ))('
 while read -r impacted
 do
   EXPR+="${impacted}|"
-done < <(echo -n "$1" | ggrep -Po '(?<=integration-tests/virtual-threads/).+')
+done < <(echo -n "$1" | grep -Po '(?<=integration-tests/virtual-threads/).+')
 EXPR+=')(,|$))+'
 
 # Step 2: apply the filter expression via grep to each "test-modules" list and replace each original list with the filtered one
@@ -35,7 +35,7 @@ do
   # Notes:
   # - trailing "|" (after EXPR) avoids grep return code > 0 if nothing matches (which is a valid case)
   # - "paste" joins all matches to get a single line
-  FILTERED=$(echo -n "${modules}" | ggrep -Po "${EXPR}|" | paste -sd " " -)
+  FILTERED=$(echo -n "${modules}" | grep -Po "${EXPR}|" | paste -sd " " -)
   JSON=$(echo -n "${JSON}" | sed "s|${modules}|${FILTERED}|")
 done < <(echo -n "${JSON}" | jq -r '.include[] | ."test-modules"')
 

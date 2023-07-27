202addd77bc472eb37a474c31063dc7d783c7b5a
Michal Vavřík <mvavrik@redhat.com>
2023-07-27 16:19:13 +0200

    Support token propagation during identity augmentation

--- extensions/oidc-token-propagation-reactive/deployment/src/main/java/io/quarkus/oidc/token/propagation/reactive/OidcTokenPropagationReactiveBuildStep.java
+++ extensions/oidc-token-propagation-reactive/deployment/src/main/java/io/quarkus/oidc/token/propagation/reactive/OidcTokenPropagationReactiveBuildStep.java
@@ -1,5 +1,8 @@
 package io.quarkus.oidc.token.propagation.reactive;
 
+import static io.quarkus.oidc.token.propagation.TokenPropagationConstants.JWT_PROPAGATE_TOKEN_CREDENTIAL;
+import static io.quarkus.oidc.token.propagation.TokenPropagationConstants.OIDC_PROPAGATE_TOKEN_CREDENTIAL;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.function.BooleanSupplier;
@@ -10,15 +13,19 @@ import org.jboss.jandex.DotName;
 import org.jboss.jandex.Type;
 
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.deployment.Capabilities;
+import io.quarkus.deployment.Capability;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.BuildSteps;
 import io.quarkus.deployment.builditem.AdditionalIndexedClassesBuildItem;
 import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import io.quarkus.deployment.builditem.SystemPropertyBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.oidc.token.propagation.AccessToken;
 import io.quarkus.rest.client.reactive.deployment.DotNames;
 import io.quarkus.rest.client.reactive.deployment.RegisterProviderAnnotationInstanceBuildItem;
+import io.quarkus.runtime.configuration.ConfigurationException;
 
 @BuildSteps(onlyIf = OidcTokenPropagationReactiveBuildStep.IsEnabled.class)
 public class OidcTokenPropagationReactiveBuildStep {
@@ -51,11 +58,35 @@ public class OidcTokenPropagationReactiveBuildStep {
 
     }
 
+    @BuildStep(onlyIf = IsEnabledDuringAuth.class)
+    SystemPropertyBuildItem activateTokenCredentialPropagationViaDuplicatedContext(Capabilities capabilities) {
+        if (capabilities.isPresent(Capability.OIDC)) {
+            return new SystemPropertyBuildItem(OIDC_PROPAGATE_TOKEN_CREDENTIAL, "true");
+        }
+
+        if (capabilities.isPresent(Capability.JWT)) {
+            return new SystemPropertyBuildItem(JWT_PROPAGATE_TOKEN_CREDENTIAL, "true");
+        }
+
+        throw new ConfigurationException(
+                "Configuration property 'quarkus.oidc-token-propagation-reactive.enabled-during-authentication' is set to " +
+                        "'true', however this configuration property is only supported when either 'quarkus-oidc' or " +
+                        "'quarkus-smallrye-jwt' extensions are present.");
+    }
+
     public static class IsEnabled implements BooleanSupplier {
         OidcTokenPropagationReactiveBuildTimeConfig config;
 
         public boolean getAsBoolean() {
             return config.enabled;
+        }
+    }
+
+    public static class IsEnabledDuringAuth implements BooleanSupplier {
+        OidcTokenPropagationReactiveBuildTimeConfig config;
+
+        public boolean getAsBoolean() {
+            return config.enabledDuringAuthentication;
         }
     }
 }

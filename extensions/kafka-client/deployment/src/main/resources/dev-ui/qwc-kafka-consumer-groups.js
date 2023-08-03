43c5f09f1e28777c33baf13420cd2d5d914279a6
Phillip Kruger <phillip.kruger@gmail.com>
2023-08-03 11:03:55 +1000

    Dev UI Kafka screenshot update
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- extensions/kafka-client/deployment/src/main/resources/dev-ui/qwc-kafka-consumer-groups.js
+++ extensions/kafka-client/deployment/src/main/resources/dev-ui/qwc-kafka-consumer-groups.js
@@ -18,7 +18,9 @@ export class QwcKafkaConsumerGroups extends QwcHotReloadElement {
             flex-direction: column;
             overflow: hidden;
         }
-
+        .backButton{
+            margin-left: 10px;
+        }
         .table {
             height: 100%;
         }
@@ -121,7 +123,7 @@ export class QwcKafkaConsumerGroups extends QwcHotReloadElement {
             let name = this._selectedConsumerGroups[0].name;
             let members = this._selectedConsumerGroups[0].members;
             return html`<div class="top-bar">
-                            <vaadin-button @click="${() => {this._selectedConsumerGroups = []}}">
+                            <vaadin-button @click="${() => {this._selectedConsumerGroups = []}}" class="backButton">
                                 <vaadin-icon icon="font-awesome-solid:caret-left" slot="prefix"></vaadin-icon>
                                 Back
                             </vaadin-button>

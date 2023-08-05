3b88d1bc114c005ed7ced18375f8017946d4b2cd
Guillaume Smet <guillaume.smet@gmail.com>
2023-08-05 10:36:43 +0200

    Improve summary a bit

--- .mvn/gradle-enterprise-custom-user-data.groovy
+++ .mvn/gradle-enterprise-custom-user-data.groovy
@@ -53,7 +53,7 @@ if (System.env.GITHUB_ACTIONS) {
                 }
             }
             new File(System.env.GITHUB_STEP_SUMMARY).withWriterAppend { out ->
-                out.println("\n[Build scan for '${mvnCommand}' in ${jobName}](${publishedBuildScan.buildScanUri})\n")
+                out.println("\n[Build scan](${publishedBuildScan.buildScanUri})\n<sup>`${mvnCommand}`</sup>\n\n")
             }
         }
     }

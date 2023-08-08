7c1cfd238735e0be0cfe1c38b06042e7351b8485
Rolfe Dlugy-Hegwer <rolfedh@users.noreply.github.com>
2023-08-08 11:36:29 -0400

    second iteration: ignore headings

--- replace-quarkus.py
+++ replace-quarkus.py
@@ -4,21 +4,34 @@ import re
 def replace_instances(directory):
     for filename in os.listdir(directory):
         if filename.endswith(".adoc"):
-            with open(os.path.join(directory, filename), 'r') as file:
-                content = file.read()
-                occurrences = [match.start() for match in re.finditer(r'\bQuarkus\b', content)]
-                if occurrences:
-                    # Replace the rest of the occurrences with ":otherinstance:" starting from the last occurrence
-                    for occurrence in reversed(occurrences[1:]):
-                        content = content[:occurrence] + ':otherinstance:' + content[occurrence + len('Quarkus'):]
+            new_content = []
+            found_first_instance = False
 
-                    # Replace the first occurrence with ":firstinstance:"
-                    occurrence = occurrences[0]
-                    content = content[:occurrence] + ':firstinstance:' + content[occurrence + len('Quarkus'):]
+            with open(os.path.join(directory, filename), 'r') as file:
+                for line in file:
+                    # Check if the line is a heading
+                    if line.startswith('= '):
+                        new_content.append(line)
+                        continue
+
+                    # Perform replacements on lines that are not headings
+                    occurrences = [match.start() for match in re.finditer(r'\bQuarkus\b', line)]
+                    if occurrences:
+                        if not found_first_instance:
+                            # Replace the first occurrence with ":firstinstance:"
+                            line = line[:occurrences[0]] + ':firstinstance:' + line[occurrences[0] + len('Quarkus'):]
+                            found_first_instance = True
+                            occurrences.pop(0) # Remove first occurrence from the list
+
+                        # Replace the rest of the occurrences with ":otherinstance:" starting from the last occurrence
+                        for occurrence in reversed(occurrences):
+                            line = line[:occurrence] + ':otherinstance:' + line[occurrence + len('Quarkus'):]
+
+                    new_content.append(line)
 
             # Write the modified content back to the file
             with open(os.path.join(directory, filename), 'w') as file:
-                file.write(content)
+                file.writelines(new_content)
 
 # Provide the path to the directory containing the AsciiDoc files
 directory_path = './docs/src/main/asciidoc'

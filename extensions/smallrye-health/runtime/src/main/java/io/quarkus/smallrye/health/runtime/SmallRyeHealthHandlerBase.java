1cacf97afa1ea99f832eea6e440caaa777655e35
Alexander Schwartz <aschwart@redhat.com>
2023-08-08 17:52:48 +0200

    Instead of buffering the response, send it to the response in chunks as they are available

--- extensions/smallrye-health/runtime/src/main/java/io/quarkus/smallrye/health/runtime/SmallRyeHealthHandlerBase.java
+++ extensions/smallrye-health/runtime/src/main/java/io/quarkus/smallrye/health/runtime/SmallRyeHealthHandlerBase.java
@@ -1,15 +1,17 @@
 package io.quarkus.smallrye.health.runtime;
 
+import java.io.OutputStream;
+
 import org.jboss.logging.Logger;
 
 import io.quarkus.arc.Arc;
 import io.quarkus.arc.ManagedContext;
 import io.quarkus.security.identity.CurrentIdentityAssociation;
-import io.quarkus.vertx.core.runtime.BufferOutputStream;
 import io.quarkus.vertx.http.runtime.security.QuarkusHttpUser;
 import io.smallrye.health.SmallRyeHealth;
 import io.smallrye.health.SmallRyeHealthReporter;
 import io.smallrye.mutiny.Uni;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpHeaders;
@@ -49,22 +51,37 @@ abstract class SmallRyeHealthHandlerBase implements Handler<RoutingContext> {
                 resp.setStatusCode(503);
             }
             resp.headers().set(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
-            Buffer buffer = Buffer.buffer(256); // this size seems to cover the basic health checks
-            try (BufferOutputStream outputStream = new BufferOutputStream(buffer)) {
+            try (ChunkedResponseOutputStream outputStream = new ChunkedResponseOutputStream(resp)) {
                 reporter.reportHealth(outputStream, health);
             } catch (Throwable e) {
                 log.error("Unable to prepare health response message", e);
-                resp.setStatusCode(500);
-                resp.end();
-                return;
             }
-            resp.end(buffer);
+            resp.end((Handler<AsyncResult<Void>>) null);
         }, throwable -> {
             log.error("Unable to prepare health response message", throwable);
             HttpServerResponse resp = ctx.response();
             resp.setStatusCode(500);
-            resp.end();
+            resp.end((Handler<AsyncResult<Void>>) null);
         });
     }
 
+    private static class ChunkedResponseOutputStream extends OutputStream {
+
+        private final HttpServerResponse response;
+
+        private ChunkedResponseOutputStream(HttpServerResponse response) {
+            this.response = response;
+            response.setChunked(true);
+        }
+
+        @Override
+        public void write(byte[] b, int off, int len) {
+            response.write(Buffer.buffer().appendBytes(b, off, len), null);
+        }
+
+        @Override
+        public void write(int b) {
+            response.write(Buffer.buffer().appendByte((byte) b), null);
+        }
+    }
 }

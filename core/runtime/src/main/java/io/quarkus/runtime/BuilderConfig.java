b7f641cd74f9d7b42bcff44ab9a96676c4b037e7
Guillaume Smet <guillaume.smet@gmail.com>
2023-08-11 14:05:34 +0200

    Convert newly added config to @ConfigMapping

--- core/runtime/src/main/java/io/quarkus/runtime/BuilderConfig.java
+++ core/runtime/src/main/java/io/quarkus/runtime/BuilderConfig.java
@@ -2,28 +2,27 @@ package io.quarkus.runtime;
 
 import java.util.Optional;
 
-import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
 
 /**
  * This configuration class is here to avoid warnings when using {@code -Dquarkus.builder.=...}.
  *
  * @see io.quarkus.builder.BuildChainBuilder
  */
-@ConfigRoot(name = "builder", phase = ConfigPhase.RUN_TIME)
-public class BuilderConfig {
+@ConfigMapping(prefix = "quarkus.builder")
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
+public interface BuilderConfig {
 
     /**
      * Dump the graph output to a file. This is useful for debugging.
      */
-    @ConfigItem
-    public Optional<String> graphOutput;
+    Optional<String> graphOutput();
 
     /**
      * Whether to log the cause of a conflict.
      */
-    @ConfigItem
-    public Optional<Boolean> logConflictCause;
+    Optional<Boolean> logConflictCause();
 
 }

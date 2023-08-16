654e185a3ca6dbd766234cb869541df22a1cdb33
Sergey Beryozkin <sberyozkin@gmail.com>
2023-08-16 12:44:28 +0100

    Re-enable BearerTokenAuthorizationInGraalITCase

--- integration-tests/oidc/src/test/java/io/quarkus/it/keycloak/BearerTokenAuthorizationInGraalITCase.java
+++ integration-tests/oidc/src/test/java/io/quarkus/it/keycloak/BearerTokenAuthorizationInGraalITCase.java
@@ -2,14 +2,12 @@ package io.quarkus.it.keycloak;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.common.DevServicesContext;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-@Disabled
 public class BearerTokenAuthorizationInGraalITCase extends BearerTokenAuthorizationTest {
 
     DevServicesContext context;

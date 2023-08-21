0a75463a7e44d918c67ab98ff3892443fa21acb9
Yoann Rodière <yoann@hibernate.org>
2023-08-21 18:38:21 +0200

    Add configuration property quarkus.hibernate-search-orm.elasticsearch.query.shard-failure.ignore
    See https://docs.jboss.org/hibernate/search/6.2/reference/en-US/html_single/#backend-elasticsearch-search-ignore-partial-shard-failure

---
+++ extensions/hibernate-search-orm-elasticsearch/deployment/src/test/java/io/quarkus/hibernate/search/orm/elasticsearch/test/search/shard_failure/MyEntity1.java
@@ -0,0 +1,44 @@
+package io.quarkus.hibernate.search.orm.elasticsearch.test.search.shard_failure;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
+
+@Entity
+@Indexed
+public class MyEntity1 {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @KeywordField
+    private String text;
+
+    public MyEntity1() {
+    }
+
+    public MyEntity1(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}

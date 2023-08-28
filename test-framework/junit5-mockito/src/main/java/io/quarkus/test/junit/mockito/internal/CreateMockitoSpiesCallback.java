1046d339abf400bc69999902a469cf37ffc6a8bc
Matej Novotny <manovotn@redhat.com>
2023-08-28 17:51:40 +0200

    Mockito testing - manually activate req. context when creating Mockito spies

--- test-framework/junit5-mockito/src/main/java/io/quarkus/test/junit/mockito/internal/CreateMockitoSpiesCallback.java
+++ test-framework/junit5-mockito/src/main/java/io/quarkus/test/junit/mockito/internal/CreateMockitoSpiesCallback.java
@@ -1,20 +1,34 @@
 package io.quarkus.test.junit.mockito.internal;
 
 import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.mockito.AdditionalAnswers;
 import org.mockito.Mockito;
 
+import io.quarkus.arc.Arc;
 import io.quarkus.arc.ClientProxy;
+import io.quarkus.arc.InjectableContext;
 import io.quarkus.arc.InstanceHandle;
+import io.quarkus.test.junit.callback.QuarkusTestAfterAllCallback;
 import io.quarkus.test.junit.callback.QuarkusTestAfterConstructCallback;
+import io.quarkus.test.junit.callback.QuarkusTestContext;
 import io.quarkus.test.junit.mockito.InjectSpy;
 
-public class CreateMockitoSpiesCallback implements QuarkusTestAfterConstructCallback {
+public class CreateMockitoSpiesCallback implements QuarkusTestAfterConstructCallback, QuarkusTestAfterAllCallback {
+
+    // in nested tests, there are multiple states created before destruction is triggered
+    private static Set<InjectableContext.ContextState> statesToDestroy = new HashSet<>();
 
     @Override
     public void afterConstruct(Object testInstance) {
         Class<?> current = testInstance.getClass();
+        // QuarkusTestAfterConstructCallback can be used in @QuarkusIntegrationTest where there is no Arc
+        boolean contextPreviouslyActive = Arc.container() != null && Arc.container().requestContext().isActive();
+        if (!contextPreviouslyActive) {
+            Arc.container().requestContext().activate();
+        }
         while (current.getSuperclass() != null) {
             for (Field field : current.getDeclaredFields()) {
                 InjectSpy injectSpyAnnotation = field.getAnnotation(InjectSpy.class);
@@ -28,16 +42,21 @@ public class CreateMockitoSpiesCallback implements QuarkusTestAfterConstructCall
             }
             current = current.getSuperclass();
         }
+        if (!contextPreviouslyActive) {
+            // only deactivate; we will destroy them in QuarkusTestAfterAllCallback
+            statesToDestroy.add(Arc.container().requestContext().getState());
+            Arc.container().requestContext().deactivate();
+        }
     }
 
     private Object createSpyAndSetTestField(Object testInstance, Field field, InstanceHandle<?> beanHandle, boolean delegate) {
         Object spy;
+        // Unwrap the client proxy if needed
         Object contextualInstance = ClientProxy.unwrap(beanHandle.get());
         if (delegate) {
             spy = Mockito.mock(beanHandle.getBean().getImplementationClass(),
                     AdditionalAnswers.delegatesTo(contextualInstance));
         } else {
-            // Unwrap the client proxy if needed
             spy = Mockito.spy(contextualInstance);
         }
         field.setAccessible(true);
@@ -49,4 +68,13 @@ public class CreateMockitoSpiesCallback implements QuarkusTestAfterConstructCall
         return spy;
     }
 
+    @Override
+    public void afterAll(QuarkusTestContext context) {
+        if (!statesToDestroy.isEmpty()) {
+            for (InjectableContext.ContextState state : statesToDestroy) {
+                Arc.container().requestContext().destroy(state);
+            }
+            statesToDestroy.clear();
+        }
+    }
 }

31e77553179cefc9befee717abe92df260a6cea3
Roberto Cortez <radcortez@yahoo.com>
2023-09-07 00:22:26 +0100

    Do not include in the list of property names Kubernetes config fallbacks

--- extensions/kubernetes/vanilla/runtime/src/main/java/io/quarkus/kubernetes/runtime/config/KubernetesConfigFallback.java
+++ extensions/kubernetes/vanilla/runtime/src/main/java/io/quarkus/kubernetes/runtime/config/KubernetesConfigFallback.java
@@ -1,27 +1,30 @@
 package io.quarkus.kubernetes.runtime.config;
 
+import java.util.Iterator;
 import java.util.function.Function;
 
+import io.smallrye.config.ConfigSourceInterceptorContext;
 import io.smallrye.config.FallbackConfigSourceInterceptor;
 
 public class KubernetesConfigFallback extends FallbackConfigSourceInterceptor {
-    private static final String QUARKUS_KUBERNETES_CONFIG_PREFIX = "quarkus.kubernetes.";
-    private static final String QUARKUS_OPENSHIFT_CONFIG_PREFIX = "quarkus.openshift.";
-    private static final int OPENSHIFT_CONFIG_NAME_BEGIN = QUARKUS_OPENSHIFT_CONFIG_PREFIX.length();
-    private static final String QUARKUS_KNATIVE_CONFIG_PREFIX = "quarkus.knative.";
-    private static final int KNATIVE_CONFIG_NAME_BEGIN = QUARKUS_KNATIVE_CONFIG_PREFIX.length();
-
     public KubernetesConfigFallback() {
-        super(new Function<String, String>() {
-            @Override
-            public String apply(final String name) {
-                if (name.startsWith(QUARKUS_OPENSHIFT_CONFIG_PREFIX)) {
-                    return QUARKUS_KUBERNETES_CONFIG_PREFIX + name.substring(OPENSHIFT_CONFIG_NAME_BEGIN);
-                } else if (name.startsWith(QUARKUS_KNATIVE_CONFIG_PREFIX)) {
-                    return QUARKUS_KUBERNETES_CONFIG_PREFIX + name.substring(KNATIVE_CONFIG_NAME_BEGIN);
-                }
-                return name;
+        super(new FallbackToKubernetesConfig());
+    }
+
+    @Override
+    public Iterator<String> iterateNames(final ConfigSourceInterceptorContext context) {
+        return context.iterateNames();
+    }
+
+    private static class FallbackToKubernetesConfig implements Function<String, String> {
+        @Override
+        public String apply(final String name) {
+            if (name.startsWith("quarkus.openshift.")) {
+                return "quarkus.kubernetes." + name.substring(18);
+            } else if (name.startsWith("quarkus.knative.")) {
+                return "quarkus.kubernetes." + name.substring(16);
             }
-        });
+            return name;
+        }
     }
 }

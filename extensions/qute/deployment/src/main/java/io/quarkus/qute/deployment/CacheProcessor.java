d6ec94d901c5869a5d58e58e6b70d61dfb60c634
Georgios Andrianakis <geoand@gmail.com>
2023-09-08 13:22:28 +0300

    Don't use cache for Qute when the cache is remote
    Closes: #35680
    (cherry picked from commit 8ffe29deaa81b756bbcd4f0ca8bb5f5357cc25dc)

--- extensions/qute/deployment/src/main/java/io/quarkus/qute/deployment/CacheProcessor.java
+++ extensions/qute/deployment/src/main/java/io/quarkus/qute/deployment/CacheProcessor.java
@@ -1,9 +1,10 @@
 package io.quarkus.qute.deployment;
 
+import java.util.Optional;
+
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.cache.deployment.spi.AdditionalCacheNameBuildItem;
-import io.quarkus.deployment.Capabilities;
-import io.quarkus.deployment.Capability;
+import io.quarkus.cache.deployment.spi.CacheTypeBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.qute.cache.QuteCache;
@@ -11,13 +12,19 @@ import io.quarkus.qute.cache.QuteCache;
 public class CacheProcessor {
 
     @BuildStep
-    void initialize(Capabilities capabilities, BuildProducer<AdditionalBeanBuildItem> beans,
+    void initialize(Optional<CacheTypeBuildItem> cacheTypeBuildItem,
+            BuildProducer<AdditionalBeanBuildItem> beans,
             BuildProducer<AdditionalCacheNameBuildItem> cacheNames) {
-        if (capabilities.isPresent(Capability.CACHE)) {
-            beans.produce(new AdditionalBeanBuildItem("io.quarkus.qute.runtime.cache.CacheConfigurator"));
-            // We need to produce additional cache name because quarkus-cache only considers the CombinedIndexBuildItem and not the bean archive index
-            cacheNames.produce(new AdditionalCacheNameBuildItem(QuteCache.NAME));
+        if (cacheTypeBuildItem.isEmpty()) { // no caching enabled
+            return;
         }
+        CacheTypeBuildItem.Type type = cacheTypeBuildItem.get().getType();
+        if (type != CacheTypeBuildItem.Type.LOCAL) { // it does not make sense to use a remote cache for Qute
+            return;
+        }
+        beans.produce(new AdditionalBeanBuildItem("io.quarkus.qute.runtime.cache.CacheConfigurator"));
+        // We need to produce additional cache name because quarkus-cache only considers the CombinedIndexBuildItem and not the bean archive index
+        cacheNames.produce(new AdditionalCacheNameBuildItem(QuteCache.NAME));
     }
 
 }

6e3b906242d8853f81a51978d7e965d50a7b1f34
George Gastaldi <gegastaldi@gmail.com>
2023-09-20 10:53:12 -0300

    Bubble up WebApplicationExceptions in RestDataResourceMethodListener implementations
    - Fixes #35169

---
+++ extensions/panache/hibernate-orm-rest-data-panache/deployment/src/test/java/io/quarkus/hibernate/orm/rest/data/panache/deployment/entity/PanacheEntityResourceMethodListenerExceptionTest.java
@@ -0,0 +1,43 @@
+package io.quarkus.hibernate.orm.rest.data.panache.deployment.entity;
+
+import static io.restassured.RestAssured.given;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.ForbiddenException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.hibernate.orm.rest.data.panache.RestDataResourceMethodListener;
+import io.quarkus.test.QuarkusUnitTest;
+
+class PanacheEntityResourceMethodListenerExceptionTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest TEST = new QuarkusUnitTest()
+            .withApplicationRoot((jar) -> jar
+                    .addClasses(Collection.class, CollectionsResource.class, AbstractEntity.class, AbstractItem.class,
+                            Item.class, ItemsResource.class, WebApplicationExceptionResourceMethodListener.class)
+                    .addAsResource("application.properties")
+                    .addAsResource("import.sql"));
+
+    @Test
+    void shouldReceiveWebApplicationException() {
+        given().accept("application/json")
+                .and().contentType("application/json")
+                .and().body("{\"name\": \"test-simple\", \"collection\": {\"id\": \"full\"}}")
+                .when().post("/items")
+                .then()
+                .statusCode(403);
+    }
+
+    @ApplicationScoped
+    public static class WebApplicationExceptionResourceMethodListener implements RestDataResourceMethodListener<Item> {
+
+        @Override
+        public void onBeforeAdd(Item item) {
+            throw new ForbiddenException("You shall not pass");
+        }
+    }
+
+}

7e23cf507d30cbd32e5aa65102d0fc519ab6264f
Georgios Andrianakis <geoand@gmail.com>
2023-10-10 10:00:58 +0300

    Fix issue in Java migration in dev-mode
    Fixes: #36299

---
+++ extensions/flyway/deployment/src/test/java/io/quarkus/flyway/test/FlywayExtensionWithJavaMigrationDevModeTestEndpoint.java
@@ -0,0 +1,37 @@
+package io.quarkus.flyway.test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import org.flywaydb.core.Flyway;
+
+import io.agroal.api.AgroalDataSource;
+
+@Path("/fly")
+public class FlywayExtensionWithJavaMigrationDevModeTestEndpoint {
+
+    @Inject
+    AgroalDataSource defaultDataSource;
+
+    @Inject
+    Flyway flyway;
+
+    @GET
+    public String result() throws Exception {
+        int count = 0;
+        try (Connection connection = defaultDataSource.getConnection(); Statement stat = connection.createStatement()) {
+            try (ResultSet countQuery = stat.executeQuery("select count(1) from quarked_flyway")) {
+                countQuery.first();
+                count = countQuery.getInt(1);
+            }
+        }
+        String currentVersion = flyway.info().current().getVersion().toString();
+
+        return count + "/" + currentVersion;
+    }
+}

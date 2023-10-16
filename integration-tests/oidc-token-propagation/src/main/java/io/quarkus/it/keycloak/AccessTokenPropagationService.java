3cc528b80122d69b394c1cba1872ee0f65cb2915
Sergey Beryozkin <sberyozkin@gmail.com>
2023-10-16 15:34:23 +0100

    Let custom OIDC token propagation filters customize the exchange status

--- integration-tests/oidc-token-propagation/src/main/java/io/quarkus/it/keycloak/AccessTokenPropagationService.java
+++ integration-tests/oidc-token-propagation/src/main/java/io/quarkus/it/keycloak/AccessTokenPropagationService.java
@@ -3,12 +3,11 @@ package io.quarkus.it.keycloak;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import io.quarkus.oidc.token.propagation.AccessToken;
-
 @RegisterRestClient(configKey = "access-token-propagation")
-@AccessToken
+@RegisterProvider(CustomAccessTokenRequestFilter.class)
 @Path("/")
 public interface AccessTokenPropagationService {
 

195df296ca1ad9ae70f24e19961ed6078b4ea87b
Foivos Zakkak <fzakkak@redhat.com>
2023-10-20 15:46:57 +0300

    Fix version reporting and parsing
    Quarkus currently only parses the feature and update version of JDK and
    incorrectly prints message like:
    
    ```
    [INFO] [io.quarkus.deployment.pkg.steps.NativeImageBuildStep] Running Quarkus native-image plugin on MANDREL 22.3.3.1 JDK 17.8
    ```
    
    while it should be
    
    ```
    [INFO] [io.quarkus.deployment.pkg.steps.NativeImageBuildStep] Running Quarkus native-image plugin on MANDREL 22.3.3.1 JDK 17.0.8
    ```
    
    i.e. 17.0.8 vs 17.8
    
    This change fixes that issue and relies on java.lang.Runtime.Version for
    parsing and comparing the JDK version.
    
    Follow up to https://github.com/quarkusio/quarkus/pull/36267
    
    (cherry picked from commit 89ac005a6e8f1266e0b365452158beb635f5cf16)

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/NativeImageBuildStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/NativeImageBuildStep.java
@@ -299,7 +299,7 @@ public class NativeImageBuildStep {
             return new NativeImageBuildItem(finalExecutablePath,
                     new NativeImageBuildItem.GraalVMVersion(graalVMVersion.fullVersion,
                             graalVMVersion.getVersionAsString(),
-                            graalVMVersion.javaFeatureVersion,
+                            graalVMVersion.javaVersion.feature(),
                             graalVMVersion.distribution.name()));
         } catch (ImageGenerationFailureException e) {
             throw e;
@@ -477,7 +477,7 @@ public class NativeImageBuildStep {
 
     private void checkGraalVMVersion(GraalVM.Version version) {
         log.info("Running Quarkus native-image plugin on " + version.distribution.name() + " " + version.getVersionAsString()
-                + " JDK " + version.javaFeatureVersion + "." + version.javaUpdateVersion);
+                + " JDK " + version.javaVersion);
         if (version.isObsolete()) {
             throw new IllegalStateException("Out of date version of GraalVM detected: " + version.getVersionAsString() + "."
                     + " Quarkus currently supports " + GraalVM.Version.CURRENT.getVersionAsString()
@@ -968,17 +968,10 @@ public class NativeImageBuildStep {
                 }
 
                 if (nativeMinimalJavaVersions != null && !nativeMinimalJavaVersions.isEmpty()) {
-                    if (graalVMVersion.javaUpdateVersion == GraalVM.Version.UNDEFINED) {
-                        log.warnf(
-                                "Unable to parse used Java version from native-image version string `%s'. Java version checks will be skipped.",
-                                graalVMVersion.fullVersion);
-                    } else {
-                        nativeMinimalJavaVersions.stream()
-                                .filter(a -> !graalVMVersion.jdkVersionGreaterOrEqualTo(a.minFeature, a.minUpdate))
-                                .forEach(a -> log.warnf("Expected: Java %d, update %d, Actual: Java %d, update %d. %s",
-                                        a.minFeature, a.minUpdate, graalVMVersion.javaFeatureVersion,
-                                        graalVMVersion.javaUpdateVersion, a.warning));
-                    }
+                    nativeMinimalJavaVersions.stream()
+                            .filter(a -> !graalVMVersion.jdkVersionGreaterOrEqualTo(a))
+                            .forEach(a -> log.warnf("Expected: Java %s, Actual: Java %s. %s",
+                                    a.minVersion, graalVMVersion.javaVersion, a.warning));
                 }
 
                 if (unsupportedOSes != null && !unsupportedOSes.isEmpty()) {

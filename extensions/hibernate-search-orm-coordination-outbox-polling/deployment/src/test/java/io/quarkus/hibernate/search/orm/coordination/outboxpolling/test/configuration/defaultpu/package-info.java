06d8729f3687fd6510bfd9b781bbffa1a6b9c98b
Yoann Rodière <yoann@hibernate.org>
2023-11-06 11:42:41 +0100

    Rename extension quarkus-hibernate-search-orm-coordination-outbox-polling to quarkus-hibernate-search-orm-outbox-polling
    To align on Hibernate Search renaming the module
    hibernate-search-orm-coordination-outbox-polling to hibernate-search-orm-outbox-polling

--- extensions/hibernate-search-orm-coordination-outbox-polling/deployment/src/test/java/io/quarkus/hibernate/search/orm/coordination/outboxpolling/test/configuration/defaultpu/package-info.java
+++
@@ -1,4 +0,0 @@
-@PersistenceUnit(PersistenceUnit.DEFAULT)
-package io.quarkus.hibernate.search.orm.coordination.outboxpolling.test.configuration.defaultpu;
-
-import io.quarkus.hibernate.orm.PersistenceUnit;

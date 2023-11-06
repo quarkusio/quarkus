7ec0c0a302943e4de50e1ea41ee2072b2b38af86
Andreas Eberle <andreas.eberle@arconsis.com>
2023-11-06 15:38:30 +0100

    Update Kotlin to version 1.9.21, Mockito to 5.7.0
    (cherry picked from commit b206484ff53f565671067172671439783bd2ad5e)

--- integration-tests/gradle/src/main/resources/conditional-dependencies-kotlin/build.gradle.kts
+++ integration-tests/gradle/src/main/resources/conditional-dependencies-kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.allopen") version "1.9.10"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.allopen") version "1.9.21"
     id("io.quarkus")
 }
 

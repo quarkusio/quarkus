28eac006fa12e25e7da67b10d7a00cfc0fc0de00
Foivos Zakkak <fzakkak@redhat.com>
2023-11-13 11:35:05 +0200

    Refactor: Simplify class names array creation
    (cherry picked from commit 94cf51115110b4f7d4449c8c8105d9e16d88db5c)

--- extensions/resteasy-reactive/jaxrs-client-reactive/deployment/src/main/java/io/quarkus/jaxrs/client/reactive/deployment/JaxrsClientReactiveProcessor.java
+++ extensions/resteasy-reactive/jaxrs-client-reactive/deployment/src/main/java/io/quarkus/jaxrs/client/reactive/deployment/JaxrsClientReactiveProcessor.java
@@ -44,7 +44,6 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import jakarta.ws.rs.ProcessingException;
 import jakarta.ws.rs.RuntimeType;
@@ -284,8 +283,7 @@ public class JaxrsClientReactiveProcessor {
             scannedParameterContainers.addAll(parameterContainersBuildItem.getClassNames());
         }
         reflectiveClassBuildItemBuildProducer.produce(ReflectiveClassBuildItem
-                .builder(scannedParameterContainers.stream().map(name -> name.toString()).collect(Collectors.toSet())
-                        .toArray(new String[0]))
+                .builder(scannedParameterContainers.stream().map(DotName::toString).distinct().toArray(String[]::new))
                 .methods().fields().build());
 
         if (resourceScanningResultBuildItem.isEmpty()

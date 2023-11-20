42ffdefbe8770a68102b03e8b32b3c3e1f604e69
Martin Kouba <mkouba@redhat.com>
2023-11-20 13:24:00 +0100

    OpenTelemetry: fine-grained instrumentation enablement
    - resolves #35912

--- extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/intrumentation/InstrumentationRecorder.java
+++ extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/intrumentation/InstrumentationRecorder.java
@@ -1,16 +1,20 @@
 package io.quarkus.opentelemetry.runtime.tracing.intrumentation;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.quarkus.arc.runtime.BeanContainer;
+import io.quarkus.opentelemetry.runtime.config.runtime.OTelRuntimeConfig;
 import io.quarkus.opentelemetry.runtime.tracing.intrumentation.vertx.EventBusInstrumenterVertxTracer;
 import io.quarkus.opentelemetry.runtime.tracing.intrumentation.vertx.HttpInstrumenterVertxTracer;
+import io.quarkus.opentelemetry.runtime.tracing.intrumentation.vertx.InstrumenterVertxTracer;
 import io.quarkus.opentelemetry.runtime.tracing.intrumentation.vertx.OpenTelemetryVertxMetricsFactory;
 import io.quarkus.opentelemetry.runtime.tracing.intrumentation.vertx.OpenTelemetryVertxTracer;
 import io.quarkus.opentelemetry.runtime.tracing.intrumentation.vertx.OpenTelemetryVertxTracingFactory;
 import io.quarkus.opentelemetry.runtime.tracing.intrumentation.vertx.SqlClientInstrumenterVertxTracer;
+import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.annotations.Recorder;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.metrics.MetricsOptions;
@@ -21,6 +25,12 @@ public class InstrumentationRecorder {
 
     public static final OpenTelemetryVertxTracingFactory FACTORY = new OpenTelemetryVertxTracingFactory();
 
+    private final RuntimeValue<OTelRuntimeConfig> config;
+
+    public InstrumentationRecorder(RuntimeValue<OTelRuntimeConfig> config) {
+        this.config = config;
+    }
+
     /* RUNTIME INIT */
     public Consumer<VertxOptions> getVertxTracingOptions() {
         TracingOptions tracingOptions = new TracingOptions()
@@ -29,13 +39,19 @@ public class InstrumentationRecorder {
     }
 
     /* RUNTIME INIT */
-    public void setupVertxTracer(BeanContainer beanContainer) {
+    public void setupVertxTracer(BeanContainer beanContainer, boolean sqlClientAvailable) {
         OpenTelemetry openTelemetry = beanContainer.beanInstance(OpenTelemetry.class);
-        OpenTelemetryVertxTracer openTelemetryVertxTracer = new OpenTelemetryVertxTracer(List.of(
-                new HttpInstrumenterVertxTracer(openTelemetry),
-                new EventBusInstrumenterVertxTracer(openTelemetry),
-                // TODO - Selectively register this in the recorder if the SQL Client is available.
-                new SqlClientInstrumenterVertxTracer(openTelemetry)));
+        List<InstrumenterVertxTracer<?, ?>> tracers = new ArrayList<>(3);
+        if (config.getValue().instrument().vertxHttp()) {
+            tracers.add(new HttpInstrumenterVertxTracer(openTelemetry));
+        }
+        if (config.getValue().instrument().vertxEventBus()) {
+            tracers.add(new EventBusInstrumenterVertxTracer(openTelemetry));
+        }
+        if (sqlClientAvailable && config.getValue().instrument().vertxSqlClient()) {
+            tracers.add(new SqlClientInstrumenterVertxTracer(openTelemetry));
+        }
+        OpenTelemetryVertxTracer openTelemetryVertxTracer = new OpenTelemetryVertxTracer(tracers);
         FACTORY.getVertxTracerDelegator().setDelegate(openTelemetryVertxTracer);
     }
 

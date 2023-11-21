c8f603943f0addf81148c33d8908021e3cee66bb
Georgios Andrianakis <geoand@gmail.com>
2023-11-21 18:16:17 +0200

    Bump to Kotlin 1.9.20
    Replaces #37234

--- integration-tests/gradle/src/main/resources/conditional-dependencies-kotlin/build.gradle.kts
+++ integration-tests/gradle/src/main/resources/conditional-dependencies-kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.allopen") version "1.9.10"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.allopen") version "1.9.20"
     id("io.quarkus")
 }
 

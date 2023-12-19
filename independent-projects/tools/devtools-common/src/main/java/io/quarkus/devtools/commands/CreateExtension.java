fdb03f5010d5d861df151005b46927caf2af2657
George Gastaldi <gegastaldi@gmail.com>
2023-12-19 17:06:56 -0300

    Bump maven-compiler-plugin to 3.12.0
    Also bump the extensions to avoid compile errors

--- independent-projects/tools/devtools-common/src/main/java/io/quarkus/devtools/commands/CreateExtension.java
+++ independent-projects/tools/devtools-common/src/main/java/io/quarkus/devtools/commands/CreateExtension.java
@@ -91,7 +91,7 @@ public class CreateExtension {
     public static final String DEFAULT_QUARKIVERSE_GUIDE_URL = "https://quarkiverse.github.io/quarkiverse-docs/%s/dev/";
 
     private static final String DEFAULT_SUREFIRE_PLUGIN_VERSION = "3.0.0";
-    private static final String DEFAULT_COMPILER_PLUGIN_VERSION = "3.11.0";
+    private static final String DEFAULT_COMPILER_PLUGIN_VERSION = "3.12.0";
 
     private final QuarkusExtensionCodestartProjectInputBuilder builder = QuarkusExtensionCodestartProjectInput.builder();
     private final Path baseDir;

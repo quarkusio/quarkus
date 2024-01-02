356bf86cc5614ace24bd336fe65cee777bb17f9f
Clement Escoffier <clement.escoffier@gmail.com>
2024-01-02 10:25:48 +0100

    Remove usage of the EventLoopContext
    This class has been removed.
    The worker context class has also been removed (used in test).
    
    We now use the ContextInternal class.

--- extensions/grpc/deployment/src/test/java/io/quarkus/grpc/client/MutinyClientInjectionTest.java
+++ extensions/grpc/deployment/src/test/java/io/quarkus/grpc/client/MutinyClientInjectionTest.java
@@ -22,8 +22,6 @@ import io.quarkus.test.QuarkusUnitTest;
 import io.smallrye.common.vertx.VertxContext;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.impl.ContextInternal;
-import io.vertx.core.impl.EventLoopContext;
-import io.vertx.core.impl.WorkerContext;
 import io.vertx.mutiny.core.Context;
 import io.vertx.mutiny.core.Vertx;
 
@@ -73,7 +71,7 @@ public class MutinyClientInjectionTest {
                     service.sayHello(HelloRequest.newBuilder().setName(s).build())
                             .map(HelloReply::getMessage)
                             .invoke(() -> assertThat(Vertx.currentContext()).isNotNull().isEqualTo(context))
-                            .invoke(() -> assertThat(Vertx.currentContext().getDelegate()).isInstanceOf(EventLoopContext.class))
+                            .invoke(() -> assertThat(Vertx.currentContext().getDelegate()).isInstanceOf(ContextInternal.class))
                             .subscribe().with(e::complete, e::fail);
                 });
             }).await().atMost(Duration.ofSeconds(5));
@@ -87,7 +85,6 @@ public class MutinyClientInjectionTest {
                     service.sayHello(HelloRequest.newBuilder().setName(s).build())
                             .map(HelloReply::getMessage)
                             .invoke(() -> assertThat(Vertx.currentContext().getDelegate())
-                                    .isNotInstanceOf(EventLoopContext.class).isNotInstanceOf(WorkerContext.class)
                                     .isEqualTo(duplicate))
                             .subscribe().with(e::complete, e::fail);
                 });

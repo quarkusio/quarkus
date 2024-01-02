fd290ffd7655f78c4e832a618c97dff1745b6ba0
Clement Escoffier <clement.escoffier@gmail.com>
2024-01-02 15:39:07 +0100

    OIDC - Avoid reading the body directly, and use the body handler instead
    It avoids a race condition when reading the body

--- integration-tests/oidc/src/main/java/io/quarkus/it/keycloak/VertxResource.java
+++ integration-tests/oidc/src/main/java/io/quarkus/it/keycloak/VertxResource.java
@@ -5,25 +5,22 @@ import jakarta.enterprise.event.Observes;
 
 import io.quarkus.vertx.http.runtime.security.QuarkusHttpUser;
 import io.vertx.core.Handler;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 
 @ApplicationScoped
 public class VertxResource {
 
     void setup(@Observes Router router) {
-        router.route("/vertx").handler(new Handler<RoutingContext>() {
-            @Override
-            public void handle(RoutingContext event) {
-                event.request().bodyHandler(new Handler<Buffer>() {
+        router.route("/vertx")
+                .handler(BodyHandler.create())
+                .handler(new Handler<RoutingContext>() {
                     @Override
-                    public void handle(Buffer data) {
-                        event.response().end(data);
+                    public void handle(RoutingContext event) {
+                        event.end(event.body().buffer());
                     }
                 });
-            }
-        });
         router.route("/basic-only").handler(new Handler<RoutingContext>() {
             @Override
             public void handle(RoutingContext event) {

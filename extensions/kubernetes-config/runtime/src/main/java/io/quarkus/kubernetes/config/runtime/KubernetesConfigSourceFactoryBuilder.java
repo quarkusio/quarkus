f2236e3dc222d6e4efc633eb458e2df778448b46
Georgios Andrianakis <geoand@gmail.com>
2024-01-04 12:56:08 +0200

    Disable Kubernetes Config when in AppCDs generation
    Closes: #38020

--- extensions/kubernetes-config/runtime/src/main/java/io/quarkus/kubernetes/config/runtime/KubernetesConfigSourceFactoryBuilder.java
+++ extensions/kubernetes-config/runtime/src/main/java/io/quarkus/kubernetes/config/runtime/KubernetesConfigSourceFactoryBuilder.java
@@ -2,9 +2,12 @@ package io.quarkus.kubernetes.config.runtime;
 
 import static io.smallrye.config.Converters.getImplicitConverter;
 
+import java.util.Collections;
+
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.quarkus.arc.runtime.appcds.AppCDSRecorder;
 import io.quarkus.kubernetes.client.runtime.KubernetesClientBuildConfig;
 import io.quarkus.kubernetes.client.runtime.KubernetesClientUtils;
 import io.quarkus.runtime.TlsConfig;
@@ -23,6 +26,12 @@ public class KubernetesConfigSourceFactoryBuilder implements ConfigBuilder {
         @Override
         public Iterable<ConfigSource> getConfigSources(final ConfigSourceContext context,
                 final KubernetesClientBuildConfig config) {
+            boolean inAppCDsGeneration = Boolean
+                    .parseBoolean(System.getProperty(AppCDSRecorder.QUARKUS_APPCDS_GENERATE_PROP, "false"));
+            if (inAppCDsGeneration) {
+                return Collections.emptyList();
+            }
+
             // TODO - TlsConfig is used in a lot of place. This is to avoid having it to migrate to ConfigMapping.
             boolean trustAll = getImplicitConverter(Boolean.class)
                     .convert(context.getValue("quarkus.tls.trust-all").getValue());

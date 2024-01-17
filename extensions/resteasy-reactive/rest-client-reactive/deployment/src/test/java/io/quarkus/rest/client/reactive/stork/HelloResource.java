23b62131874cd22456aae267afd1b9ef3dd84279
Auri Munoz <amunozhe@redhat.com>
2024-01-17 11:54:14 +0100

    fix: use raw path and avoid double encoding, adapt tests accordingly
    Related to #37713

--- extensions/resteasy-reactive/rest-client-reactive/deployment/src/test/java/io/quarkus/rest/client/reactive/stork/HelloResource.java
+++ extensions/resteasy-reactive/rest-client-reactive/deployment/src/test/java/io/quarkus/rest/client/reactive/stork/HelloResource.java
@@ -1,7 +1,13 @@
 package io.quarkus.rest.client.reactive.stork;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Request;
 
 @Path("/hello")
 public class HelloResource {
@@ -11,5 +17,17 @@ public class HelloResource {
     @GET
     public String hello() {
         return HELLO_WORLD;
+    }
+
+    @GET
+    @Path("/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String invoke(@PathParam("name") String name) {
+        return "Hello, " + name;
+    }
+
+    @POST
+    public String echo(String name, @Context Request request) {
+        return "hello, " + name;
     }
 }

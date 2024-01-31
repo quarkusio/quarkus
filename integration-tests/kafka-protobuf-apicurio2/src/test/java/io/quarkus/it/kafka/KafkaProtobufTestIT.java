ede45bd6969a02d0e5ffd068fb657b47046ebb35
Carles Arnal <carlesarnal92@gmail.com>
2024-01-31 11:49:11 +0100

    Add protobuf native integration tests for apicurio serdes support

---
+++ integration-tests/kafka-protobuf-apicurio2/src/test/java/io/quarkus/it/kafka/KafkaProtobufTestIT.java
@@ -0,0 +1,28 @@
+package io.quarkus.it.kafka;
+
+import io.apicurio.registry.rest.client.RegistryClientFactory;
+import io.apicurio.rest.client.VertxHttpClientProvider;
+import io.quarkus.it.kafka.protobuf.ProtobufKafkaCreator;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.vertx.core.Vertx;
+import org.junit.jupiter.api.BeforeAll;
+
+@QuarkusIntegrationTest
+@QuarkusTestResource(value = KafkaResource.class, restrictToAnnotatedClass = true)
+public class KafkaProtobufTestIT extends KafkaProtobufTestBase {
+
+    ProtobufKafkaCreator creator;
+
+    @Override
+    ProtobufKafkaCreator creator() {
+        return creator;
+    }
+
+    @BeforeAll
+    public static void setUp() {
+        // this is for the test JVM, which also uses Kafka client, which in turn also interacts with the registry
+        RegistryClientFactory.setProvider(new VertxHttpClientProvider(Vertx.vertx()));
+    }
+
+}

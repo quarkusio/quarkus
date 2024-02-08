024016e932ebe17f6ea21b94fe945d00be61dda4
Michael Edgar <michael@xlate.io>
2024-02-08 18:26:21 -0500

    OpenAPI: remove check that avoids running auto-security at build
    Signed-off-by: Michael Edgar <michael@xlate.io>
    (cherry picked from commit 03286ee1d61be49ac0c49b10693cd7f043ea78f7)

--- extensions/smallrye-openapi/deployment/src/test/java/io/quarkus/smallrye/openapi/test/jaxrs/AutoSecurityRolesAllowedWithScopesTestCase.java
+++ extensions/smallrye-openapi/deployment/src/test/java/io/quarkus/smallrye/openapi/test/jaxrs/AutoSecurityRolesAllowedWithScopesTestCase.java
@@ -1,24 +1,12 @@
 package io.quarkus.smallrye.openapi.test.jaxrs;
 
-import static org.hamcrest.Matchers.aMapWithSize;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.emptyIterable;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.iterableWithSize;
-import static org.hamcrest.Matchers.nullValue;
-
-import org.hamcrest.Matcher;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.test.QuarkusUnitTest;
-import io.restassured.RestAssured;
 
-class AutoSecurityRolesAllowedWithScopesTestCase {
+class AutoSecurityRolesAllowedWithScopesTestCase extends AutoSecurityRolesAllowedWithScopesTestBase {
 
     @RegisterExtension
     static QuarkusUnitTest runner = new QuarkusUnitTest()
@@ -27,52 +15,13 @@ class AutoSecurityRolesAllowedWithScopesTestCase {
                             OpenApiResourceSecuredAtMethodLevel.class, OpenApiResourceSecuredAtMethodLevel2.class)
                     .addAsResource(
                             new StringAsset("quarkus.smallrye-openapi.security-scheme=oauth2-implicit\n"
-                                    + "quarkus.smallrye-openapi.security-scheme-name=OAuth2\n"
-                                    + "quarkus.smallrye-openapi.security-scheme-description=OAuth2 Authentication"),
-
+                                    + "quarkus.smallrye-openapi.security-scheme-name=MyScheme\n"
+                                    + "quarkus.smallrye-openapi.security-scheme-description=Authentication using MyScheme"),
                             "application.properties"));
-
-    static Matcher<Iterable<Object>> schemeArray(String schemeName, Matcher<Iterable<?>> scopesMatcher) {
-        return allOf(
-                iterableWithSize(1),
-                hasItem(allOf(
-                        aMapWithSize(1),
-                        hasEntry(equalTo(schemeName), scopesMatcher))));
-    }
 
     @Test
     void testAutoSecurityRequirement() {
-        RestAssured.given()
-                .header("Accept", "application/json")
-                .when()
-                .get("/q/openapi")
-                .then()
-                .log().body()
-                .and()
-                .body("components.securitySchemes.OAuth2", allOf(
-                        hasEntry("type", "oauth2"),
-                        hasEntry("description", "OAuth2 Authentication")))
-                .and()
-                // OpenApiResourceSecuredAtMethodLevel
-                .body("paths.'/resource2/test-security/naked'.get.security", schemeArray("OAuth2", contains("admin")))
-                .body("paths.'/resource2/test-security/annotated'.get.security",
-                        schemeArray("JWTCompanyAuthentication", emptyIterable()))
-                .body("paths.'/resource2/test-security/methodLevel/1'.get.security", schemeArray("OAuth2", contains("user1")))
-                .body("paths.'/resource2/test-security/methodLevel/2'.get.security", schemeArray("OAuth2", contains("user2")))
-                .body("paths.'/resource2/test-security/methodLevel/public'.get.security", nullValue())
-                .body("paths.'/resource2/test-security/annotated/documented'.get.security",
-                        schemeArray("JWTCompanyAuthentication", emptyIterable()))
-                .body("paths.'/resource2/test-security/methodLevel/3'.get.security", schemeArray("OAuth2", contains("admin")))
-                .and()
-                // OpenApiResourceSecuredAtClassLevel
-                .body("paths.'/resource2/test-security/classLevel/1'.get.security", schemeArray("OAuth2", contains("user1")))
-                .body("paths.'/resource2/test-security/classLevel/2'.get.security", schemeArray("OAuth2", contains("user2")))
-                .body("paths.'/resource2/test-security/classLevel/3'.get.security", schemeArray("MyOwnName", emptyIterable()))
-                .body("paths.'/resource2/test-security/classLevel/4'.get.security", schemeArray("OAuth2", contains("admin")))
-                .and()
-                // OpenApiResourceSecuredAtMethodLevel2
-                .body("paths.'/resource3/test-security/annotated'.get.security",
-                        schemeArray("AtClassLevel", emptyIterable()));
+        testAutoSecurityRequirement("oauth2");
     }
 
 }

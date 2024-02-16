b8bdad1004716ab473f377f06fb01a58349fccc9
Sergey Beryozkin <sberyozkin@gmail.com>
2024-02-16 19:08:37 +0000

    Check the code flow access token after ID token

--- integration-tests/oidc-wiremock/src/main/java/io/quarkus/it/keycloak/CodeFlowTokenIntrospectionResource.java
+++ integration-tests/oidc-wiremock/src/main/java/io/quarkus/it/keycloak/CodeFlowTokenIntrospectionResource.java
@@ -4,6 +4,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
+import io.quarkus.oidc.TokenIntrospection;
 import io.quarkus.security.Authenticated;
 import io.quarkus.security.identity.SecurityIdentity;
 
@@ -14,8 +15,11 @@ public class CodeFlowTokenIntrospectionResource {
     @Inject
     SecurityIdentity identity;
 
+    @Inject
+    TokenIntrospection tokenIntrospection;
+
     @GET
     public String access() {
-        return identity.getPrincipal().getName();
+        return identity.getPrincipal().getName() + ":" + tokenIntrospection.getUsername();
     }
 }

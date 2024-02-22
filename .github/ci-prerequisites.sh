f3edb77c488c1044c3d65c25a17d7bf4c479a1e1
Guillaume Smet <guillaume.smet@gmail.com>
2024-02-22 10:08:23 +0100

    Try to get more disk space
    (cherry picked from commit 05be84e10cb7a631134942e246508cb8d30e0757)

--- .github/ci-prerequisites.sh
+++ .github/ci-prerequisites.sh
@@ -41,8 +41,19 @@ time sudo rm -rf /usr/share/swift || true
 time sudo rm -rf /usr/local/lib/android || true
 # Remove Haskell
 time sudo rm -rf /opt/ghc || true
+time sudo rm -rf /usr/local/.ghcup || true
 # Remove pipx
 time sudo rm -rf /opt/pipx || true
+# Remove Rust
+time sudo rm -rf /usr/share/rust || true
+# Remove Go
+time sudo rm -rf /usr/local/go || true
+# Remove miniconda
+time sudo rm -rf /usr/share/miniconda || true
+# Remove powershell
+time sudo rm -rf /usr/local/share/powershell || true
+# Remove Google Cloud SDK
+time sudo rm -rf /usr/lib/google-cloud-sdk || true
 
 # Remove infrastructure things that are unused and take a lot of space
 time sudo rm -rf /opt/hostedtoolcache/CodeQL || true

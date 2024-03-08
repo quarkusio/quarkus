35e56054a456786c80b87a5854d233101d5daa76
Martin Kouba <mkouba@redhat.com>
2024-03-08 10:12:25 +0100

    WebSockets Next: add test for automatic pong from the server
    - add sendPing() and sendPong() methods; currently there's no way to
    test it

--- extensions/websockets-next/server/runtime/src/main/java/io/quarkus/websockets/next/BlockingSender.java
+++ extensions/websockets-next/server/runtime/src/main/java/io/quarkus/websockets/next/BlockingSender.java
@@ -4,7 +4,7 @@ import io.smallrye.common.annotation.Experimental;
 import io.vertx.core.buffer.Buffer;
 
 /**
- * Sends a message to the connected WebSocket client and waits for the completion.
+ * Sends messages to the connected WebSocket client and waits for the completion.
  * <p>
  * Note that blocking sender methods should never be called on an event loop thread.
  */
@@ -46,5 +46,27 @@ public interface BlockingSender extends Sender {
      */
     default void sendBinaryAndAwait(byte[] message) {
         sendBinary(message).await().indefinitely();
+    }
+
+    /**
+     * Send a ping message and waits for the completion.
+     * 
+     * @param data May be at most 125 bytes
+     */
+    default void sendPingAndAwait(Buffer data) {
+        sendPing(data).await().indefinitely();
+    }
+
+    /**
+     * Send an unsolicited pong message.
+     * <p>
+     * Note that the server automatically responds to a ping message sent from the client. However, the RFC 6455
+     * <a href="https://tools.ietf.org/html/rfc6455#section-5.5.3">section 5.5.3</a> states that unsolicited pong may serve as a
+     * unidirectional heartbeat.
+     * 
+     * @param data May be at most 125 bytes
+     */
+    default void sendPongAndAwait(Buffer data) {
+        sendPong(data).await().indefinitely();
     }
 }

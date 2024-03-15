f17813ffc746b6dde8631bd751b72cd4d34e8f4b
Martin Kouba <mkouba@redhat.com>
2024-03-15 08:21:57 +0100

    WebSockets Next: detect incorrect path parameter usage
    - resolves #39182

--- extensions/websockets-next/server/deployment/src/test/java/io/quarkus/websockets/next/deployment/WebSocketServerProcessorTest.java
+++ extensions/websockets-next/server/deployment/src/test/java/io/quarkus/websockets/next/deployment/WebSocketServerProcessorTest.java
@@ -1,18 +1,35 @@
 package io.quarkus.websockets.next.deployment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import io.quarkus.websockets.next.WebSocketServerException;
 
 public class WebSocketServerProcessorTest {
 
     @Test
     public void testGetPath() {
         assertEquals("/foo/:id", WebSocketServerProcessor.getPath("/foo/{id}"));
+        assertEquals("/foo/:id/bar/:id2", WebSocketServerProcessor.getPath("/foo/{id}/bar/{id2}"));
         assertEquals("/foo/:bar-:baz", WebSocketServerProcessor.getPath("/foo/{bar}-{baz}"));
         assertEquals("/ws/v:version", WebSocketServerProcessor.getPath("/ws/v{version}"));
-        assertEquals("/foo/v:bar/:bazand:alpha_1-:name",
-                WebSocketServerProcessor.getPath("/foo/v{bar}/{baz}and{alpha_1}-{name}"));
+        WebSocketServerException e = assertThrows(WebSocketServerException.class,
+                () -> WebSocketServerProcessor.getPath("/foo/v{bar}/{baz}and{alpha_1}-{name}"));
+        assertEquals(
+                "Path parameter {baz} may not be followed by an alphanumeric character or underscore: /foo/v{bar}/{baz}and{alpha_1}-{name}",
+                e.getMessage());
+        e = assertThrows(WebSocketServerException.class,
+                () -> WebSocketServerProcessor.getPath("/foo/v{bar}/{baz}_{alpha_1}-{name}"));
+        assertEquals(
+                "Path parameter {baz} may not be followed by an alphanumeric character or underscore: /foo/v{bar}/{baz}_{alpha_1}-{name}",
+                e.getMessage());
+        e = assertThrows(WebSocketServerException.class,
+                () -> WebSocketServerProcessor.getPath("/foo/v{bar}/{baz}1-{name}"));
+        assertEquals(
+                "Path parameter {baz} may not be followed by an alphanumeric character or underscore: /foo/v{bar}/{baz}1-{name}",
+                e.getMessage());
     }
 
     @Test

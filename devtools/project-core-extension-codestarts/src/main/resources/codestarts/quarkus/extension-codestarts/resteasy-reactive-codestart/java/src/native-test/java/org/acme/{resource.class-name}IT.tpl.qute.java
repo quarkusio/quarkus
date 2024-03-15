34ef25ed89dfddff84de05646cccf5e4298b5c7f
Andy Damevin <ia3andy@gmail.com>
2024-03-15 10:33:03 +0100

    Fix codestarts compatibility with older CLI
    (cherry picked from commit 5bc19c0392be8718d33285dead59ee256fe33b29)

---
+++ devtools/project-core-extension-codestarts/src/main/resources/codestarts/quarkus/extension-codestarts/resteasy-reactive-codestart/java/src/native-test/java/org/acme/{resource.class-name}IT.tpl.qute.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class {resource.class-name}IT extends {resource.class-name}Test {
+    // Execute the same tests but in packaged mode.
+}

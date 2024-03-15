12b5054ed1b08616e4208177c42bd94c2b77f3ff
ennishol <dennisholu@gmail.com>
2024-03-15 10:15:10 +0100

    Allow occasional pin events in ShouldNotPin
    ShouldNotPin annotation doesn't currently allow
    any pin events to happen. However, in some
    situations one still want to allow occasional
    pin to happen.
    
    Bye adding a parameter to ShouldNotPin makes
    it possible.

--- independent-projects/junit5-virtual-threads/src/main/java/io/quarkus/test/junit5/virtual/ShouldNotPin.java
+++ independent-projects/junit5-virtual-threads/src/main/java/io/quarkus/test/junit5/virtual/ShouldNotPin.java
@@ -8,9 +8,16 @@ import java.lang.annotation.Target;
 /**
  * Marker indicating that the test method or class should not pin the carrier thread.
  * If, during the execution of the test, a virtual thread pins the carrier thread, the test fails.
+ * However, occasional pin can still be allowed by setting {@code atMost} value
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.TYPE })
 public @interface ShouldNotPin {
 
+    /**
+     * Set value to allow occasional pin events
+     *
+     * @return
+     */
+    int atMost() default 0;
 }

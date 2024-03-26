919679226dda6d31a2bb36baec50db464efb3171
brunobat <brunobat@gmail.com>
2024-03-26 16:02:41 +0000

    Backport hide EndUserSpanProcessor integration
    (cherry picked from commit a1f4fab40f6e359d77335e5613275b43abdcec9d)

--- extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/exporter/otlp/EndUserSpanProcessor.java
+++ extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/exporter/otlp/EndUserSpanProcessor.java
@@ -5,6 +5,7 @@ import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
 
 import org.eclipse.microprofile.context.ManagedExecutor;
+import org.jboss.logging.Logger;
 
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.context.Context;
@@ -12,10 +13,14 @@ import io.opentelemetry.sdk.trace.ReadWriteSpan;
 import io.opentelemetry.sdk.trace.ReadableSpan;
 import io.opentelemetry.sdk.trace.SpanProcessor;
 import io.opentelemetry.semconv.SemanticAttributes;
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.ManagedContext;
 import io.quarkus.security.identity.SecurityIdentity;
 
 @ApplicationScoped
 public class EndUserSpanProcessor implements SpanProcessor {
+
+    private static final Logger LOG = Logger.getLogger(EndUserSpanProcessor.class);
 
     @Inject
     protected SecurityIdentity securityIdentity;
@@ -27,14 +32,31 @@ public class EndUserSpanProcessor implements SpanProcessor {
     @ActivateRequestContext
     public void onStart(Context parentContext, ReadWriteSpan span) {
         managedExecutor.execute(
-                () -> span.setAllAttributes(
-                        securityIdentity.isAnonymous()
-                                ? Attributes.empty()
-                                : Attributes.of(
-                                        SemanticAttributes.ENDUSER_ID,
-                                        securityIdentity.getPrincipal().getName(),
-                                        SemanticAttributes.ENDUSER_ROLE,
-                                        securityIdentity.getRoles().toString())));
+                () -> {
+                    try {
+                        if (!span.isRecording()) {
+                            LOG.debug("Failed to set end user attributes on spanId:" +
+                                    span.getSpanContext().getSpanId() + " Span not recording");
+                            return;
+                        }
+                        ManagedContext managedContext = Arc.container().requestContext();
+                        if (!managedContext.isActive()) {
+                            LOG.debug("Failed to set end user attributes on span.spanId:" +
+                                    span.getSpanContext().getSpanId() + " Request Context not active");
+                            return;
+                        }
+                        span.setAllAttributes(
+                                securityIdentity.isAnonymous()
+                                        ? Attributes.empty()
+                                        : Attributes.of(
+                                                SemanticAttributes.ENDUSER_ID,
+                                                securityIdentity.getPrincipal().getName(),
+                                                SemanticAttributes.ENDUSER_ROLE,
+                                                securityIdentity.getRoles().toString()));
+                    } catch (Exception e) {
+                        LOG.debug("Failed to set end user attributes on span", e);
+                    }
+                });
     }
 
     @Override

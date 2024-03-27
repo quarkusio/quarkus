dcd84db68ed300279c926a6e66fb532c8dd1bee1
Damiano Renfer <x9w2n7xnu@relay.firefox.com>
2024-03-27 10:46:16 +0100

    Fix StorkClientRequestFilter exception handling
    Exceptions were not caught properly because a Uni subscription was done
    outside a try-catch block.
    
    (cherry picked from commit 04cffc5df44d7d97557b55869fd61d4f450fe2d9)

--- independent-projects/resteasy-reactive/client/runtime/src/main/java/org/jboss/resteasy/reactive/client/impl/StorkClientRequestFilter.java
+++ independent-projects/resteasy-reactive/client/runtime/src/main/java/org/jboss/resteasy/reactive/client/impl/StorkClientRequestFilter.java
@@ -15,9 +15,7 @@ import org.jboss.resteasy.reactive.client.spi.ResteasyReactiveClientRequestConte
 import org.jboss.resteasy.reactive.client.spi.ResteasyReactiveClientRequestFilter;
 
 import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
 import io.smallrye.stork.Stork;
-import io.smallrye.stork.api.ServiceInstance;
 
 @Priority(Priorities.AUTHENTICATION)
 @Provider
@@ -34,67 +32,64 @@ public class StorkClientRequestFilter implements ResteasyReactiveClientRequestFi
             }
 
             requestContext.suspend();
-            Uni<ServiceInstance> serviceInstance;
             boolean measureTime = shouldMeasureTime(requestContext.getResponseType());
             try {
-                serviceInstance = Stork.getInstance()
+                Stork.getInstance()
                         .getService(serviceName)
-                        .selectInstanceAndRecordStart(measureTime);
+                        .selectInstanceAndRecordStart(measureTime)
+                        .subscribe()
+                        .with(instance -> {
+                            boolean isHttps = instance.isSecure() || "storks".equals(uri.getScheme());
+                            String scheme = isHttps ? "https" : "http";
+                            try {
+                                // In the case the service instance does not set the host and/or port
+                                String host = instance.getHost() == null ? "localhost" : instance.getHost();
+                                int port = instance.getPort();
+                                if (instance.getPort() == 0) {
+                                    if (isHttps) {
+                                        port = 433;
+                                    } else {
+                                        port = 80;
+                                    }
+                                }
+                                // Service instance can also contain an optional path.
+                                Optional<String> path = instance.getPath();
+                                String actualPath = uri.getRawPath();
+                                if (path.isPresent()) {
+                                    var p = path.get();
+                                    if (!p.startsWith("/")) {
+                                        p = "/" + p;
+                                    }
+                                    if (actualPath == null) {
+                                        actualPath = p;
+                                    } else {
+                                        // Append both.
+                                        if (actualPath.startsWith("/") || p.endsWith("/")) {
+                                            actualPath = p + actualPath;
+                                        } else {
+                                            actualPath = p + "/" + actualPath;
+                                        }
+                                    }
+                                }
+                                //To avoid the path double encoding we create uri with path=null and set the path after
+                                URI newUri = new URI(scheme,
+                                        uri.getUserInfo(), host, port,
+                                        null, uri.getQuery(), uri.getFragment());
+                                URI build = UriBuilder.fromUri(newUri).path(actualPath).build();
+                                requestContext.setUri(build);
+                                if (measureTime && instance.gatherStatistics()) {
+                                    requestContext.setCallStatsCollector(instance);
+                                }
+                                requestContext.resume();
+                            } catch (URISyntaxException e) {
+                                requestContext.resume(new IllegalArgumentException("Invalid URI", e));
+                            }
+                        },
+                                requestContext::resume);
             } catch (Throwable e) {
                 log.error("Error selecting service instance for serviceName: " + serviceName, e);
                 requestContext.resume(e);
-                return;
             }
-
-            serviceInstance.subscribe()
-                    .with(instance -> {
-                        boolean isHttps = instance.isSecure() || "storks".equals(uri.getScheme());
-                        String scheme = isHttps ? "https" : "http";
-                        try {
-                            // In the case the service instance does not set the host and/or port
-                            String host = instance.getHost() == null ? "localhost" : instance.getHost();
-                            int port = instance.getPort();
-                            if (instance.getPort() == 0) {
-                                if (isHttps) {
-                                    port = 433;
-                                } else {
-                                    port = 80;
-                                }
-                            }
-                            // Service instance can also contain an optional path.
-                            Optional<String> path = instance.getPath();
-                            String actualPath = uri.getRawPath();
-                            if (path.isPresent()) {
-                                var p = path.get();
-                                if (!p.startsWith("/")) {
-                                    p = "/" + p;
-                                }
-                                if (actualPath == null) {
-                                    actualPath = p;
-                                } else {
-                                    // Append both.
-                                    if (actualPath.startsWith("/") || p.endsWith("/")) {
-                                        actualPath = p + actualPath;
-                                    } else {
-                                        actualPath = p + "/" + actualPath;
-                                    }
-                                }
-                            }
-                            //To avoid the path double encoding we create uri with path=null and set the path after
-                            URI newUri = new URI(scheme,
-                                    uri.getUserInfo(), host, port,
-                                    null, uri.getQuery(), uri.getFragment());
-                            URI build = UriBuilder.fromUri(newUri).path(actualPath).build();
-                            requestContext.setUri(build);
-                            if (measureTime && instance.gatherStatistics()) {
-                                requestContext.setCallStatsCollector(instance);
-                            }
-                            requestContext.resume();
-                        } catch (URISyntaxException e) {
-                            requestContext.resume(new IllegalArgumentException("Invalid URI", e));
-                        }
-                    },
-                            requestContext::resume);
         }
 
     }

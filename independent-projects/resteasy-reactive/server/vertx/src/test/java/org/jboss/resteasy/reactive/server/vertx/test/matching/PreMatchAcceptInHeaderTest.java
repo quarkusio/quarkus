4b109711f6441f7ba1390e77a977c239e5fdb9ed
Georgios Andrianakis <geoand@gmail.com>
2024-04-11 18:50:49 +0300

    Take MediaType set in pre-match filter into account during serialization
    Fixes: #40019
    (cherry picked from commit 4f86368e4f00984558956fdd5ddac9a240c8c78c)

--- independent-projects/resteasy-reactive/server/vertx/src/test/java/org/jboss/resteasy/reactive/server/vertx/test/matching/PreMatchAcceptInHeaderTest.java
+++ independent-projects/resteasy-reactive/server/vertx/src/test/java/org/jboss/resteasy/reactive/server/vertx/test/matching/PreMatchAcceptInHeaderTest.java
@@ -4,11 +4,16 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 import java.util.function.Supplier;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.PreMatching;
@@ -17,6 +22,9 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.ext.Provider;
 
+import org.jboss.resteasy.reactive.server.spi.ResteasyReactiveResourceInfo;
+import org.jboss.resteasy.reactive.server.spi.ServerMessageBodyWriter;
+import org.jboss.resteasy.reactive.server.spi.ServerRequestContext;
 import org.jboss.resteasy.reactive.server.vertx.test.framework.ResteasyReactiveUnitTest;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
@@ -84,6 +92,37 @@ public class PreMatchAcceptInHeaderTest {
                 .body(equalTo("text"));
     }
 
+    @Test
+    void entityJsonWithoutAcceptToTextInFilter() {
+        given().accept("application/json")
+                .when()
+                .get("test/entity")
+                .then()
+                .statusCode(200)
+                .body(containsString("\"text\""));
+    }
+
+    @Test
+    void entityTextWithoutAcceptToTextInFilter() {
+        given().accept("text/plain")
+                .when()
+                .get("test/entity")
+                .then()
+                .statusCode(200)
+                .body(equalTo("text"));
+    }
+
+    @Test
+    void entityTextWithAcceptToTextInFilter() {
+        given().accept("application/json")
+                .header("x-set-accept-to-text", "true")
+                .when()
+                .get("test/entity")
+                .then()
+                .statusCode(200)
+                .body(equalTo("text"));
+    }
+
     @Path("/test")
     public static class Resource {
 
@@ -106,6 +145,16 @@ public class PreMatchAcceptInHeaderTest {
                     </html>
                     """;
         }
+
+        @GET
+        @Path("entity")
+        @Produces({ MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON })
+        public Entity entity() {
+            return new Entity("text");
+        }
+    }
+
+    public record Entity(String value) {
     }
 
     @PreMatching
@@ -118,6 +167,64 @@ public class PreMatchAcceptInHeaderTest {
             if ("true".equals(headers.getFirst("x-set-accept-to-text"))) {
                 headers.putSingle(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN);
             }
+        }
+    }
+
+    @Provider
+    @Produces(MediaType.TEXT_PLAIN)
+    public static class DummyTextMessageBodyWriter implements ServerMessageBodyWriter<Object> {
+
+        @Override
+        public boolean isWriteable(Class<?> type, Type genericType, ResteasyReactiveResourceInfo target,
+                MediaType mediaType) {
+            return Entity.class.equals(type);
+        }
+
+        @Override
+        public void writeResponse(Object o, Type genericType, ServerRequestContext context)
+                throws WebApplicationException, IOException {
+            context.serverResponse().end(((Entity) o).value());
+        }
+
+        @Override
+        public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+            return Entity.class.equals(type);
+        }
+
+        @Override
+        public void writeTo(Object o, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+                MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream)
+                throws IOException, WebApplicationException {
+            throw new IllegalStateException("should not be called");
+        }
+    }
+
+    @Provider
+    @Produces(MediaType.APPLICATION_JSON)
+    public static class DummyJsonMessageBodyWriter implements ServerMessageBodyWriter<Object> {
+
+        @Override
+        public boolean isWriteable(Class<?> type, Type genericType, ResteasyReactiveResourceInfo target,
+                MediaType mediaType) {
+            return Entity.class.equals(type);
+        }
+
+        @Override
+        public void writeResponse(Object o, Type genericType, ServerRequestContext context)
+                throws WebApplicationException, IOException {
+            context.serverResponse().end("{\"value\":\"" + ((Entity) o).value() + "\"}");
+        }
+
+        @Override
+        public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+            return Entity.class.equals(type);
+        }
+
+        @Override
+        public void writeTo(Object o, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+                MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream)
+                throws IOException, WebApplicationException {
+            throw new IllegalStateException("should not be called");
         }
     }
 }

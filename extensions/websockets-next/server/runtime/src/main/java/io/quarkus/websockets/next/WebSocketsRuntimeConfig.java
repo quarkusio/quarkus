c70c45ee37914ab1c2610437b9c0ce89de0f1908
Martin Kouba <mkouba@redhat.com>
2024-04-18 21:11:56 +0200

    WebSockets Next: configuration updates
    - remove unimplemented timeout
    - add compression support/level
    - add max message size
    - also invoke error handlers for connection exceptions
    - resolves #39590

--- extensions/websockets-next/server/runtime/src/main/java/io/quarkus/websockets/next/WebSocketsRuntimeConfig.java
+++ extensions/websockets-next/server/runtime/src/main/java/io/quarkus/websockets/next/WebSocketsRuntimeConfig.java
@@ -1,12 +1,14 @@
 package io.quarkus.websockets.next;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalInt;
 
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+import io.vertx.core.http.HttpServerOptions;
 
 @ConfigMapping(prefix = "quarkus.websockets-next")
 @ConfigRoot(phase = ConfigPhase.RUN_TIME)
@@ -14,16 +16,27 @@ public interface WebSocketsRuntimeConfig {
 
     /**
      * See <a href="https://datatracker.ietf.org/doc/html/rfc6455#page-12">The WebSocket Protocol</a>
-     *
-     * @return the supported subprotocols
      */
     Optional<List<String>> supportedSubprotocols();
 
     /**
-     * TODO Not implemented yet.
-     *
-     * The default timeout to complete processing of a message.
+     * Compression Extensions for WebSocket are supported by default.
+     * <p>
+     * See also <a href="https://datatracker.ietf.org/doc/html/rfc7692">RFC 7692</a>
      */
-    Optional<Duration> timeout();
+    @WithDefault("true")
+    boolean perMessageCompressionSupported();
+
+    /**
+     * The compression level must be a value between 0 and 9. The default value is
+     * {@value HttpServerOptions#DEFAULT_WEBSOCKET_COMPRESSION_LEVEL}.
+     */
+    OptionalInt compressionLevel();
+
+    /**
+     * The maximum size of a message in bytes. The default values is
+     * {@value HttpServerOptions#DEFAULT_MAX_WEBSOCKET_MESSAGE_SIZE}.
+     */
+    OptionalInt maxMessageSize();
 
 }

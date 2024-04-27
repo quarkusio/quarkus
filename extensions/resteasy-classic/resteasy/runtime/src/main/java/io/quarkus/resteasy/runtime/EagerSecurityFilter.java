6c50a18697b795b53a9af745c882d5e08925ff78
Michal Vavřík <mvavrik@redhat.com>
2024-04-27 17:30:03 +0200

    Initialize SecurityEventHelper in RESTEasy Classic when config ready

--- extensions/resteasy-classic/resteasy/runtime/src/main/java/io/quarkus/resteasy/runtime/EagerSecurityFilter.java
+++ extensions/resteasy-classic/resteasy/runtime/src/main/java/io/quarkus/resteasy/runtime/EagerSecurityFilter.java
@@ -1,13 +1,9 @@
 package io.quarkus.resteasy.runtime;
 
-import static io.quarkus.security.spi.runtime.SecurityEventHelper.AUTHORIZATION_FAILURE;
-import static io.quarkus.security.spi.runtime.SecurityEventHelper.AUTHORIZATION_SUCCESS;
-
 import java.io.IOException;
 import java.util.Map;
 
 import jakarta.annotation.Priority;
-import jakarta.enterprise.event.Event;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
@@ -15,8 +11,6 @@ import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ResourceInfo;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.ext.Provider;
-
-import org.eclipse.microprofile.config.ConfigProvider;
 
 import io.quarkus.arc.Arc;
 import io.quarkus.security.UnauthorizedException;
@@ -28,7 +22,6 @@ import io.quarkus.security.spi.runtime.AuthorizationSuccessEvent;
 import io.quarkus.security.spi.runtime.MethodDescription;
 import io.quarkus.security.spi.runtime.SecurityCheck;
 import io.quarkus.security.spi.runtime.SecurityCheckStorage;
-import io.quarkus.security.spi.runtime.SecurityEventHelper;
 import io.quarkus.vertx.http.runtime.security.EagerSecurityInterceptorStorage;
 import io.vertx.ext.web.RoutingContext;
 
@@ -37,7 +30,6 @@ import io.vertx.ext.web.RoutingContext;
 public class EagerSecurityFilter implements ContainerRequestFilter {
     static final String SKIP_DEFAULT_CHECK = "io.quarkus.resteasy.runtime.EagerSecurityFilter#SKIP_DEFAULT_CHECK";
     private final EagerSecurityInterceptorStorage interceptorStorage;
-    private final SecurityEventHelper<AuthorizationSuccessEvent, AuthorizationFailureEvent> eventHelper;
 
     @Context
     ResourceInfo resourceInfo;
@@ -60,12 +52,6 @@ public class EagerSecurityFilter implements ContainerRequestFilter {
     public EagerSecurityFilter() {
         var interceptorStorageHandle = Arc.container().instance(EagerSecurityInterceptorStorage.class);
         this.interceptorStorage = interceptorStorageHandle.isAvailable() ? interceptorStorageHandle.get() : null;
-        Event<Object> event = Arc.container().beanManager().getEvent();
-        // TODO - quarkus.security.events.enabled was marked as runtime, but providers are initialized during static init
-        this.eventHelper = new SecurityEventHelper<>(event.select(AuthorizationSuccessEvent.class),
-                event.select(AuthorizationFailureEvent.class), AUTHORIZATION_SUCCESS, AUTHORIZATION_FAILURE,
-                Arc.container().beanManager(),
-                ConfigProvider.getConfig().getOptionalValue("quarkus.security.events.enabled", Boolean.class).orElse(false));
     }
 
     @Override
@@ -76,7 +62,7 @@ public class EagerSecurityFilter implements ContainerRequestFilter {
                 applyEagerSecurityInterceptors(description);
             }
             if (jaxRsPermissionChecker.shouldRunPermissionChecks()) {
-                jaxRsPermissionChecker.applyPermissionChecks(eventHelper);
+                jaxRsPermissionChecker.applyPermissionChecks();
             }
             applySecurityChecks(description);
         }
@@ -96,7 +82,7 @@ public class EagerSecurityFilter implements ContainerRequestFilter {
                 if (check.requiresMethodArguments()) {
                     if (identityAssociation.getIdentity().isAnonymous()) {
                         var exception = new UnauthorizedException();
-                        if (eventHelper.fireEventOnFailure()) {
+                        if (jaxRsPermissionChecker.getEventHelper().fireEventOnFailure()) {
                             fireEventOnAuthZFailure(exception, check);
                         }
                         throw exception;
@@ -104,7 +90,7 @@ public class EagerSecurityFilter implements ContainerRequestFilter {
                     // security check will be performed by CDI interceptor
                     return;
                 }
-                if (eventHelper.fireEventOnFailure()) {
+                if (jaxRsPermissionChecker.getEventHelper().fireEventOnFailure()) {
                     try {
                         check.apply(identityAssociation.getIdentity(), description, null);
                     } catch (Exception e) {
@@ -122,14 +108,14 @@ public class EagerSecurityFilter implements ContainerRequestFilter {
     }
 
     private void fireEventOnAuthZFailure(Exception exception, SecurityCheck check) {
-        eventHelper.fireFailureEvent(new AuthorizationFailureEvent(
+        jaxRsPermissionChecker.getEventHelper().fireFailureEvent(new AuthorizationFailureEvent(
                 identityAssociation.getIdentity(), exception, check.getClass().getName(),
                 Map.of(RoutingContext.class.getName(), routingContext)));
     }
 
     private void fireEventOnAuthZSuccess(SecurityCheck check, SecurityIdentity securityIdentity) {
-        if (eventHelper.fireEventOnSuccess()) {
-            eventHelper.fireSuccessEvent(new AuthorizationSuccessEvent(securityIdentity,
+        if (jaxRsPermissionChecker.getEventHelper().fireEventOnSuccess()) {
+            jaxRsPermissionChecker.getEventHelper().fireSuccessEvent(new AuthorizationSuccessEvent(securityIdentity,
                     check.getClass().getName(), Map.of(RoutingContext.class.getName(), routingContext)));
         }
     }

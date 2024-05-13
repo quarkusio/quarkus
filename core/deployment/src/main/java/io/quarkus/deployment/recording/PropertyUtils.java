c82ed8989ee5cf2a6facbf8deaee3cdc290bf458
Sanne Grinovero <sanne@hibernate.org>
2024-05-13 20:35:57 +0100

    Potential problem in recognizing boolean getters

--- core/deployment/src/main/java/io/quarkus/deployment/recording/PropertyUtils.java
+++ core/deployment/src/main/java/io/quarkus/deployment/recording/PropertyUtils.java
@@ -43,7 +43,7 @@ final class PropertyUtils {
                     if (existingGetter == null || existingGetter.getReturnType().isAssignableFrom(i.getReturnType())) {
                         getters.put(name, i);
                     }
-                } else if (i.getName().startsWith("is") && i.getName().length() > 3 && i.getParameterCount() == 0
+                } else if (i.getName().startsWith("is") && i.getName().length() > 2 && i.getParameterCount() == 0
                         && (i.getReturnType() == boolean.class || i.getReturnType() == Boolean.class)) {
                     String name = Character.toLowerCase(i.getName().charAt(2)) + i.getName().substring(3);
                     isGetters.put(name, i);

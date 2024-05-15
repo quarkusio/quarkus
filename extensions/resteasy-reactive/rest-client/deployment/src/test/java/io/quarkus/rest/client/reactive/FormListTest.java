1c2c30f61ac966cd95b8f5b39dd3e0cd9efebf62
Georgios Andrianakis <geoand@gmail.com>
2024-05-15 12:03:54 +0300

    Fix List form handling in REST Client bean params
    Fixes: #40324
    (cherry picked from commit 9feb173c5aa3640c5e5db9b470a393d0413f2b28)

--- extensions/resteasy-reactive/rest-client/deployment/src/test/java/io/quarkus/rest/client/reactive/FormListTest.java
+++ extensions/resteasy-reactive/rest-client/deployment/src/test/java/io/quarkus/rest/client/reactive/FormListTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.net.URI;
 import java.util.List;
 
+import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
@@ -26,19 +27,21 @@ public class FormListTest {
     URI baseUri;
 
     @Test
-    void testHeadersWithSubresource() {
+    void test() {
         Client client = RestClientBuilder.newBuilder().baseUri(baseUri).build(Client.class);
 
-        assertThat(client.call(List.of("first", "second", "third"))).isEqualTo("first-second-third");
-        assertThat(client.call(List.of("first"))).isEqualTo("first");
+        Holder holder = new Holder();
+        holder.input2 = List.of("1", "2");
+        assertThat(client.call(List.of("first", "second", "third"), holder)).isEqualTo("first-second-third/1-2");
+        assertThat(client.call(List.of("first"), holder)).isEqualTo("first/1-2");
     }
 
     @Path("/test")
     public static class Resource {
 
         @POST
-        public String response(@RestForm List<String> input) {
-            return String.join("-", input);
+        public String response(@RestForm List<String> input, @RestForm List<String> input2) {
+            return String.join("-", input) + "/" + String.join("-", input2);
         }
     }
 
@@ -46,6 +49,11 @@ public class FormListTest {
     public interface Client {
 
         @POST
-        String call(@RestForm List<String> input);
+        String call(@RestForm List<String> input, @BeanParam Holder holder);
+    }
+
+    public static class Holder {
+        @RestForm
+        public List<String> input2;
     }
 }

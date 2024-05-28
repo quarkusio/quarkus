514c42663b4701a1525007ca588983bc446d5364
Michal Vavřík <mvavrik@redhat.com>
2024-05-28 11:56:28 +0200

    WebSocket NEXT: automatically close connection when token expires

--- extensions/websockets-next/runtime/src/main/java/io/quarkus/websockets/next/runtime/SecuritySupport.java
+++ extensions/websockets-next/runtime/src/main/java/io/quarkus/websockets/next/runtime/SecuritySupport.java
@@ -1,22 +1,36 @@
 package io.quarkus.websockets.next.runtime;
 
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 import jakarta.enterprise.inject.Instance;
 
+import org.jboss.logging.Logger;
+
 import io.quarkus.security.identity.CurrentIdentityAssociation;
 import io.quarkus.security.identity.SecurityIdentity;
+import io.quarkus.websockets.next.CloseReason;
+import io.vertx.core.Vertx;
 
 public class SecuritySupport {
 
-    static final SecuritySupport NOOP = new SecuritySupport(null, null);
+    private static final Logger LOG = Logger.getLogger(SecuritySupport.class);
+    static final SecuritySupport NOOP = new SecuritySupport(null, null, null, null);
 
     private final Instance<CurrentIdentityAssociation> currentIdentity;
     private final SecurityIdentity identity;
+    private final Runnable onClose;
 
-    SecuritySupport(Instance<CurrentIdentityAssociation> currentIdentity, SecurityIdentity identity) {
+    SecuritySupport(Instance<CurrentIdentityAssociation> currentIdentity, SecurityIdentity identity, Vertx vertx,
+            WebSocketConnectionImpl connection) {
         this.currentIdentity = currentIdentity;
-        this.identity = currentIdentity != null ? Objects.requireNonNull(identity) : identity;
+        if (this.currentIdentity != null) {
+            this.identity = Objects.requireNonNull(identity);
+            this.onClose = closeConnectionWhenIdentityExpired(vertx, connection, this.identity);
+        } else {
+            this.identity = null;
+            this.onClose = null;
+        }
     }
 
     /**
@@ -29,4 +43,25 @@ public class SecuritySupport {
         }
     }
 
+    void onClose() {
+        if (onClose != null) {
+            onClose.run();
+        }
+    }
+
+    private static Runnable closeConnectionWhenIdentityExpired(Vertx vertx, WebSocketConnectionImpl connection,
+            SecurityIdentity identity) {
+        if (identity.getAttribute("quarkus.identity.expire-time") instanceof Long expireAt) {
+            long timerId = vertx.setTimer(TimeUnit.SECONDS.toMillis(expireAt) - System.currentTimeMillis(),
+                    ignored -> connection
+                            .close(new CloseReason(1008, "Authentication expired"))
+                            .subscribe()
+                            .with(
+                                    v -> LOG.tracef("Closed connection due to expired authentication: %s", connection),
+                                    e -> LOG.errorf("Unable to close connection [%s] after authentication "
+                                            + "expired due to unhandled failure: %s", connection, e)));
+            return () -> vertx.cancelTimer(timerId);
+        }
+        return null;
+    }
 }

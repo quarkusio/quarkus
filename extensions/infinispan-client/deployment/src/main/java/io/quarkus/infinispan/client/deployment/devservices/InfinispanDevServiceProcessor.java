9bee30da9e77ff1ded69a7f0bb885eeec50522e3
Georgios Andrianakis <geoand@gmail.com>
2024-06-03 08:28:31 +0300

    Move Infinispan DevService logging
    Prior to this, InfinispanDevServiceProcessor logged a
    misleading message about starting a DevService.
    
    (cherry picked from commit c995ded7e0ec03d6a2e11fea5a992e48de10af36)

--- extensions/infinispan-client/deployment/src/main/java/io/quarkus/infinispan/client/deployment/devservices/InfinispanDevServiceProcessor.java
+++ extensions/infinispan-client/deployment/src/main/java/io/quarkus/infinispan/client/deployment/devservices/InfinispanDevServiceProcessor.java
@@ -160,9 +160,8 @@ public class InfinispanDevServiceProcessor {
             Map<String, RunningDevService> newDevServices,
             Map<String, String> properties) {
         try {
-            log.infof("Starting Dev Service for connection %s", clientName);
+
             InfinispanDevServicesConfig namedDevServiceConfig = config.devService.devservices;
-            log.infof("Apply Dev Services config %s", namedDevServiceConfig);
             RunningDevService devService = startContainer(clientName, dockerStatusBuildItem, namedDevServiceConfig,
                     launchMode.getLaunchMode(),
                     !devServicesSharedNetworkBuildItem.isEmpty(), globalDevServicesConfig.timeout, properties);
@@ -205,6 +204,8 @@ public class InfinispanDevServiceProcessor {
                     "Please configure 'quarkus.infinispan-client.hosts' or 'quarkus.infinispan-client.uri' or get a working docker instance");
             return null;
         }
+        log.infof("Starting Dev Service for connection %s", clientName);
+        log.infof("Apply Dev Services config %s", devServicesConfig);
 
         Supplier<RunningDevService> infinispanServerSupplier = () -> {
             QuarkusInfinispanContainer infinispanContainer = new QuarkusInfinispanContainer(clientName, devServicesConfig,

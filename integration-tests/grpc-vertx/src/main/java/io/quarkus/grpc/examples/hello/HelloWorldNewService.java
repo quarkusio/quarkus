3ce355360ad45505d857212ab99b1b8819f37467
Ales Justin <ales.justin@gmail.com>
2024-06-04 15:33:00 +0200

    Initial gRPC CLI

--- integration-tests/grpc-vertx/src/main/java/io/quarkus/grpc/examples/hello/HelloWorldNewService.java
+++ integration-tests/grpc-vertx/src/main/java/io/quarkus/grpc/examples/hello/HelloWorldNewService.java
@@ -1,5 +1,7 @@
 package io.quarkus.grpc.examples.hello;
 
+import com.google.protobuf.Empty;
+
 import examples.HelloReply;
 import examples.HelloRequest;
 import examples.MutinyGreeterGrpc;
@@ -13,6 +15,12 @@ public class HelloWorldNewService extends MutinyGreeterGrpc.GreeterImplBase {
     public Uni<HelloReply> sayHello(HelloRequest request) {
         String name = request.getName();
         return Uni.createFrom().item("Hello " + name)
+                .map(res -> HelloReply.newBuilder().setMessage(res).build());
+    }
+
+    @Override
+    public Uni<HelloReply> sayJo(Empty request) {
+        return Uni.createFrom().item("Jo!")
                 .map(res -> HelloReply.newBuilder().setMessage(res).build());
     }
 

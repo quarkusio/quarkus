1c8dbd8e38aa3d438d18d94e742089e804767c73
Guillaume Smet <guillaume.smet@gmail.com>
2024-06-06 12:27:22 +0200

    Revert "Improve loading of classes in RunnerClassLoader"
    This reverts commit 27102380b3caa867e195a2ec84c70aeac860d782.

--- independent-projects/bootstrap/runner/src/main/java/io/quarkus/bootstrap/runner/RunnerClassLoader.java
+++ independent-projects/bootstrap/runner/src/main/java/io/quarkus/bootstrap/runner/RunnerClassLoader.java
@@ -28,10 +28,6 @@ import org.crac.Resource;
  */
 public final class RunnerClassLoader extends ClassLoader {
 
-    static {
-        registerAsParallelCapable();
-    }
-
     /**
      * A map of resources by dir name. Root dir/default package is represented by the empty string
      */
@@ -107,55 +103,18 @@ public final class RunnerClassLoader extends ClassLoader {
                     continue;
                 }
                 definePackage(packageName, resources);
-                return defineClass(name, data, resource);
-            }
-        }
-        return getParent().loadClass(name);
-    }
-
-    private void definePackage(String pkgName, ClassLoadingResource[] resources) {
-        if ((pkgName != null) && getDefinedPackage(pkgName) == null) {
-            for (ClassLoadingResource classPathElement : resources) {
-                ManifestInfo mf = classPathElement.getManifestInfo();
-                if (mf != null) {
-                    try {
-                        definePackage(pkgName, mf.getSpecTitle(),
-                                mf.getSpecVersion(),
-                                mf.getSpecVendor(),
-                                mf.getImplTitle(),
-                                mf.getImplVersion(),
-                                mf.getImplVendor(), null);
-                    } catch (IllegalArgumentException e) {
-                        var loaded = getDefinedPackage(pkgName);
-                        if (loaded == null) {
-                            throw e;
-                        }
+                try {
+                    return defineClass(name, data, 0, data.length, resource.getProtectionDomain());
+                } catch (LinkageError e) {
+                    loaded = findLoadedClass(name);
+                    if (loaded != null) {
+                        return loaded;
                     }
-                    return;
-                }
-            }
-            try {
-                definePackage(pkgName, null, null, null, null, null, null, null);
-            } catch (IllegalArgumentException e) {
-                var loaded = getDefinedPackage(pkgName);
-                if (loaded == null) {
                     throw e;
                 }
             }
         }
-    }
-
-    private Class<?> defineClass(String name, byte[] data, ClassLoadingResource resource) {
-        Class<?> loaded;
-        try {
-            return defineClass(name, data, 0, data.length, resource.getProtectionDomain());
-        } catch (LinkageError e) {
-            loaded = findLoadedClass(name);
-            if (loaded != null) {
-                return loaded;
-            }
-            throw e;
-        }
+        return getParent().loadClass(name);
     }
 
     private void accessingResource(final ClassLoadingResource resource) {
@@ -260,6 +219,28 @@ public final class RunnerClassLoader extends ClassLoader {
             }
         }
         return Collections.enumeration(urls);
+    }
+
+    private void definePackage(String pkgName, ClassLoadingResource[] resources) {
+        if ((pkgName != null) && getPackage(pkgName) == null) {
+            synchronized (getClassLoadingLock(pkgName)) {
+                if (getPackage(pkgName) == null) {
+                    for (ClassLoadingResource classPathElement : resources) {
+                        ManifestInfo mf = classPathElement.getManifestInfo();
+                        if (mf != null) {
+                            definePackage(pkgName, mf.getSpecTitle(),
+                                    mf.getSpecVersion(),
+                                    mf.getSpecVendor(),
+                                    mf.getImplTitle(),
+                                    mf.getImplVersion(),
+                                    mf.getImplVendor(), null);
+                            return;
+                        }
+                    }
+                    definePackage(pkgName, null, null, null, null, null, null, null);
+                }
+            }
+        }
     }
 
     private String getPackageNameFromClassName(String className) {

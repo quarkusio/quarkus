6b3dc9c8ffad1b31450151134ccbb1ee186d2cb4
Yoann Rodière <yoann@hibernate.org>
2024-06-21 16:13:10 +0200

    Remove QuarkusMutableIdentifierGeneratorFactory
    It no longer has a purpose:
    
    1. We removed the type caching in a previous commit, because it wasn't
       used anymore: ORM hasn't needed access to generator types at runtime
       for a while now.
    2. StandardIdentifierGeneratorFactory has a constructor parameter to
       disable reliance on CDI, so we don't need to rewrite that code.
    3. StandardIdentifierGeneratorFactory is registering custom generators
       itself, so we don't need to make `register` public to register them
       externally.

--- extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/service/QuarkusIdentifierGeneratorFactoryInitiator.java
+++ extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/service/QuarkusIdentifierGeneratorFactoryInitiator.java
@@ -4,22 +4,21 @@ import java.util.Map;
 
 import org.hibernate.boot.registry.StandardServiceInitiator;
 import org.hibernate.id.factory.IdentifierGeneratorFactory;
+import org.hibernate.id.factory.internal.StandardIdentifierGeneratorFactory;
 import org.hibernate.service.spi.ServiceRegistryImplementor;
 
 /**
- * We need to mimic the standard IdentifierGeneratorFactory but allowing
- * to capture which Identifier strategies are being used, so that we can keep a reference to the classed
- * needed at runtime.
+ * Uses a StandardIdentifierGeneratorFactory, but one that doesn't retrieve generators from CDI.
  *
  * @see IdentifierGeneratorFactory
  */
-public final class QuarkusMutableIdentifierGeneratorFactoryInitiator
+public final class QuarkusIdentifierGeneratorFactoryInitiator
         implements StandardServiceInitiator<IdentifierGeneratorFactory> {
 
     @Override
     public IdentifierGeneratorFactory initiateService(final Map configurationValues,
             final ServiceRegistryImplementor registry) {
-        return new QuarkusMutableIdentifierGeneratorFactory(registry);
+        return new StandardIdentifierGeneratorFactory(registry, true /* ignore bean container */);
     }
 
     @Override

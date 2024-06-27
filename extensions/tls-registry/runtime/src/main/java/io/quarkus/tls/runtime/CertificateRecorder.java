10654d6a220a5cc9905340de4306bc4cc35e2146
Clement Escoffier <clement.escoffier@gmail.com>
2024-06-27 08:31:39 +0200

    Periodic TLS certificate reload
    Provide a way to periodically reload certificates from the file system and document how to implement your own reloader.

--- extensions/tls-registry/runtime/src/main/java/io/quarkus/tls/runtime/CertificateRecorder.java
+++ extensions/tls-registry/runtime/src/main/java/io/quarkus/tls/runtime/CertificateRecorder.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 import io.quarkus.runtime.RuntimeValue;
+import io.quarkus.runtime.ShutdownContext;
 import io.quarkus.runtime.annotations.Recorder;
 import io.quarkus.tls.TlsConfiguration;
 import io.quarkus.tls.TlsConfigurationRegistry;
@@ -25,6 +26,7 @@ import io.vertx.core.Vertx;
 public class CertificateRecorder implements TlsConfigurationRegistry {
 
     private final Map<String, TlsConfiguration> certificates = new ConcurrentHashMap<>();
+    private volatile TlsCertificateUpdater reloader;
 
     /**
      * Validate the certificate configuration.
@@ -35,7 +37,7 @@ public class CertificateRecorder implements TlsConfigurationRegistry {
      * @param config the configuration
      * @param vertx the Vert.x instance
      */
-    public void validateCertificates(TlsConfig config, RuntimeValue<Vertx> vertx) {
+    public void validateCertificates(TlsConfig config, RuntimeValue<Vertx> vertx, ShutdownContext shutdownContext) {
         // Verify the default config
         if (config.defaultCertificateConfig().isPresent()) {
             verifyCertificateConfig(config.defaultCertificateConfig().get(), vertx.getValue(), TlsConfig.DEFAULT_NAME);
@@ -45,6 +47,15 @@ public class CertificateRecorder implements TlsConfigurationRegistry {
         for (String name : config.namedCertificateConfig().keySet()) {
             verifyCertificateConfig(config.namedCertificateConfig().get(name), vertx.getValue(), name);
         }
+
+        shutdownContext.addShutdownTask(new Runnable() {
+            @Override
+            public void run() {
+                if (reloader != null) {
+                    reloader.close();
+                }
+            }
+        });
     }
 
     public void verifyCertificateConfig(TlsBucketConfig config, Vertx vertx, String name) {
@@ -55,7 +66,7 @@ public class CertificateRecorder implements TlsConfigurationRegistry {
             KeyStoreConfig keyStoreConfig = config.keyStore().get();
             ks = verifyKeyStore(keyStoreConfig, vertx, name);
             sni = keyStoreConfig.sni();
-            if (sni) {
+            if (sni && ks != null) {
                 try {
                     if (Collections.list(ks.keyStore.aliases()).size() <= 1) {
                         throw new IllegalStateException(
@@ -81,6 +92,14 @@ public class CertificateRecorder implements TlsConfigurationRegistry {
         }
 
         certificates.put(name, new VertxCertificateHolder(vertx, name, config, ks, ts));
+
+        // Handle reloading if needed
+        if (config.reloadPeriod().isPresent()) {
+            if (reloader == null) {
+                reloader = new TlsCertificateUpdater(vertx);
+            }
+            reloader.add(name, certificates.get(name), config.reloadPeriod().get());
+        }
     }
 
     public static KeyStoreAndKeyCertOptions verifyKeyStore(KeyStoreConfig config, Vertx vertx, String name) {
--- extensions/vertx-http/deployment/src/test/java/io/quarkus/vertx/http/certReload/MainHttpServerTlsCertificateReloadWithTlsRegistryAndUpdateEventTest.java
+++ extensions/vertx-http/deployment/src/test/java/io/quarkus/vertx/http/certReload/MainHttpServerTlsCertificateReloadWithTlsRegistryAndUpdateEventTest.java
@@ -5,34 +5,29 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.security.cert.X509Certificate;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
-import java.util.function.Consumer;
 
 import javax.net.ssl.SSLHandshakeException;
 
 import jakarta.enterprise.event.Event;
+import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import io.quarkus.builder.BuildChainBuilder;
-import io.quarkus.builder.BuildContext;
-import io.quarkus.builder.BuildStep;
 import io.quarkus.test.QuarkusUnitTest;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.tls.CertificateUpdatedEvent;
 import io.quarkus.tls.TlsConfigurationRegistry;
-import io.quarkus.vertx.http.deployment.NonApplicationRootPathBuildItem;
-import io.quarkus.vertx.http.deployment.RouteBuildItem;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpClientOptions;
@@ -40,46 +35,44 @@ import io.vertx.core.http.HttpClientRequest;
 import io.vertx.core.http.HttpClientResponse;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.net.PemTrustOptions;
-import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.Router;
 import me.escoffier.certs.Format;
 import me.escoffier.certs.junit5.Certificate;
 import me.escoffier.certs.junit5.Certificates;
 
 @Certificates(baseDir = "target/certificates", certificates = {
-        @Certificate(name = "reload-C", formats = Format.PEM),
-        @Certificate(name = "reload-D", formats = Format.PEM, duration = 365),
+        @Certificate(name = "reload-A", formats = Format.PEM),
+        @Certificate(name = "reload-B", formats = Format.PEM, duration = 365),
 })
 @DisabledOnOs(OS.WINDOWS)
-public class ManagementHttpServerTlsCertificateReloadWithTlsRegistryTest {
+public class MainHttpServerTlsCertificateReloadWithTlsRegistryAndUpdateEventTest {
+
+    @TestHTTPResource(value = "/hello", tls = true)
+    URL url;
 
     public static final File temp = new File("target/test-certificates-" + UUID.randomUUID());
 
-    private static final String APP_PROPS = """
-            quarkus.management.enabled=true
-            quarkus.tls.key-store.pem.0.cert=%s
-            quarkus.tls.key-store.pem.0.key=%s
-            loc=%s
-            """.formatted(temp.getAbsolutePath() + "/tls.crt", temp.getAbsolutePath() + "/tls.key", temp.getAbsolutePath());
-
     @RegisterExtension
     static final QuarkusUnitTest config = new QuarkusUnitTest()
-            .withApplicationRoot((jar) -> jar
-                    .addAsResource(new StringAsset(APP_PROPS), "application.properties"))
+            .withApplicationRoot((jar) -> jar.addClasses(MyBean.class))
+            .overrideConfigKey("quarkus.http.ssl.insecure-requests", "redirect")
+            .overrideConfigKey("quarkus.tls.key-store.pem.0.cert", temp.getAbsolutePath() + "/tls.crt")
+            .overrideConfigKey("quarkus.tls.key-store.pem.0.key", temp.getAbsolutePath() + "/tls.key")
+            .overrideConfigKey("loc", temp.getAbsolutePath())
             .setBeforeAllCustomizer(() -> {
                 try {
                     // Prepare a random directory to store the certificates.
                     temp.mkdirs();
-                    Files.copy(new File("target/certificates/reload-C.crt").toPath(),
+                    Files.copy(new File("target/certificates/reload-A.crt").toPath(),
                             new File(temp, "/tls.crt").toPath());
-                    Files.copy(new File("target/certificates/reload-C.key").toPath(),
+                    Files.copy(new File("target/certificates/reload-A.key").toPath(),
                             new File(temp, "/tls.key").toPath());
-                    Files.copy(new File("target/certificates/reload-C-ca.crt").toPath(),
+                    Files.copy(new File("target/certificates/reload-A-ca.crt").toPath(),
                             new File(temp, "/ca.crt").toPath());
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
             })
-            .addBuildChainCustomizer(buildCustomizer())
             .setAfterAllCustomizer(() -> {
                 try {
                     Files.deleteIfExists(new File(temp, "/tls.crt").toPath());
@@ -91,27 +84,6 @@ public class ManagementHttpServerTlsCertificateReloadWithTlsRegistryTest {
                 }
             });
 
-    static Consumer<BuildChainBuilder> buildCustomizer() {
-        return new Consumer<BuildChainBuilder>() {
-            @Override
-            public void accept(BuildChainBuilder builder) {
-                builder.addBuildStep(new BuildStep() {
-                    @Override
-                    public void execute(BuildContext context) {
-                        NonApplicationRootPathBuildItem buildItem = context.consume(NonApplicationRootPathBuildItem.class);
-                        context.produce(buildItem.routeBuilder()
-                                .management()
-                                .route("/hello")
-                                .handler(new MyHandler())
-                                .build());
-                    }
-                }).produces(RouteBuildItem.class)
-                        .consumes(NonApplicationRootPathBuildItem.class)
-                        .build();
-            }
-        };
-    }
-
     @Inject
     Vertx vertx;
 
@@ -119,30 +91,30 @@ public class ManagementHttpServerTlsCertificateReloadWithTlsRegistryTest {
     File certs;
 
     @Inject
-    TlsConfigurationRegistry registry;
+    Event<CertificateUpdatedEvent> event;
 
     @Inject
-    Event<CertificateUpdatedEvent> event;
+    TlsConfigurationRegistry registry;
 
     @Test
     void test() throws IOException, ExecutionException, InterruptedException, TimeoutException {
         var options = new HttpClientOptions()
                 .setSsl(true)
-                .setDefaultPort(9001) // Management interface test port
-                .setDefaultHost("localhost")
-                .setTrustOptions(new PemTrustOptions().addCertPath(new File(certs, "/ca.crt").getAbsolutePath()));
+                .setDefaultPort(url.getPort())
+                .setDefaultHost(url.getHost())
+                .setTrustOptions(new PemTrustOptions().addCertPath("target/certificates/reload-A-ca.crt"));
 
         String response1 = vertx.createHttpClient(options)
-                .request(HttpMethod.GET, "/q/hello")
+                .request(HttpMethod.GET, "/hello")
                 .flatMap(HttpClientRequest::send)
                 .flatMap(HttpClientResponse::body)
                 .map(Buffer::toString)
                 .toCompletionStage().toCompletableFuture().join();
 
         // Update certs
-        Files.copy(new File("target/certificates/reload-D.crt").toPath(),
+        Files.copy(new File("target/certificates/reload-B.crt").toPath(),
                 new File(certs, "/tls.crt").toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING);
-        Files.copy(new File("target/certificates/reload-D.key").toPath(),
+        Files.copy(new File("target/certificates/reload-B.key").toPath(),
                 new File(certs, "/tls.key").toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING);
 
         // Trigger the reload
@@ -158,7 +130,7 @@ public class ManagementHttpServerTlsCertificateReloadWithTlsRegistryTest {
                 .toCompletionStage().toCompletableFuture().join()).hasCauseInstanceOf(SSLHandshakeException.class);
 
         var options2 = new HttpClientOptions(options)
-                .setTrustOptions(new PemTrustOptions().addCertPath("target/certificates/reload-D-ca.crt"));
+                .setTrustOptions(new PemTrustOptions().addCertPath("target/certificates/reload-B-ca.crt"));
 
         var response2 = vertx.createHttpClient(options2)
                 .request(HttpMethod.GET, "/hello")
@@ -168,15 +140,31 @@ public class ManagementHttpServerTlsCertificateReloadWithTlsRegistryTest {
                 .toCompletionStage().toCompletableFuture().join();
 
         assertThat(response1).isNotEqualTo(response2); // Because cert duration are different.
+
+        // Trigger another reload
+        registry.getDefault().orElseThrow().reload();
+        event.fire(new CertificateUpdatedEvent("<default>", registry.getDefault().orElseThrow()));
+
+        var response3 = vertx.createHttpClient(options2)
+                .request(HttpMethod.GET, "/hello")
+                .flatMap(HttpClientRequest::send)
+                .flatMap(HttpClientResponse::body)
+                .map(Buffer::toString)
+                .toCompletionStage().toCompletableFuture().join();
+
+        assertThat(response2).isEqualTo(response3);
     }
 
-    public static class MyHandler implements Handler<RoutingContext> {
-        @Override
-        public void handle(RoutingContext rc) {
-            var exp = ((X509Certificate) rc.request().connection().sslSession().getLocalCertificates()[0]).getNotAfter()
-                    .toInstant().toEpochMilli();
-            rc.response().end("Hello Management " + exp);
+    public static class MyBean {
+
+        public void onStart(@Observes Router router) {
+            router.get("/hello").handler(rc -> {
+                var exp = ((X509Certificate) rc.request().connection().sslSession().getLocalCertificates()[0]).getNotAfter()
+                        .toInstant().toEpochMilli();
+                rc.response().end("Hello " + exp);
+            });
         }
+
     }
 
 }

e68ddbeed39486e6f388b884b047f3907f1f0b12
Guillaume Smet <guillaume.smet@gmail.com>
2024-07-01 19:18:24 +0200

    Clean up StartupContext when closing it
    Given StartupContext is a static field of ApplicationImpl, if too many
    class loaders are kept around (which is unfortunately still the case in
    our tests), we end up leaking the whole values of StartupContext.
    
    When closing the context, we can clean up the values.

--- core/runtime/src/main/java/io/quarkus/runtime/StartupContext.java
+++ core/runtime/src/main/java/io/quarkus/runtime/StartupContext.java
@@ -78,6 +78,8 @@ public class StartupContext implements Closeable {
     public void close() {
         runAllAndClear(shutdownTasks);
         runAllAndClear(lastShutdownTasks);
+        values.clear();
+        lastValue = null;
     }
 
     private void runAllAndClear(Deque<Runnable> tasks) {

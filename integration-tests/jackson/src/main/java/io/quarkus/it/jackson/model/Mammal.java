ea6a78046ce56b253eb657962cc1f3cf83af8312
Georgios Andrianakis <geoand@gmail.com>
2024-07-03 10:03:36 +0300

    Register @JsonNaming value for reflection
    (cherry picked from commit 05e244a593b0bbb53268f5f9fe139b6838e3012c)

--- integration-tests/jackson/src/main/java/io/quarkus/it/jackson/model/Mammal.java
+++ integration-tests/jackson/src/main/java/io/quarkus/it/jackson/model/Mammal.java
@@ -5,11 +5,14 @@ import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonTypeInfo(use = NAME, include = PROPERTY)
 @JsonSubTypes({
         @JsonSubTypes.Type(value = Elephant.class, name = "Elephant"),
         @JsonSubTypes.Type(value = Whale.class, name = "Whale"),
 })
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public interface Mammal {
 }

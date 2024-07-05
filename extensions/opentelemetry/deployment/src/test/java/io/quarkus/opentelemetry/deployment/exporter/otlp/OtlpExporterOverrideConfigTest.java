8ef35a8931d849c9099b9fe2d7e15d3363b77ee1
brunobat <brunobat@gmail.com>
2024-07-05 10:10:43 +0100

    OTel Metrics documentation. Base OTel connection config and overrides.

--- extensions/opentelemetry/deployment/src/test/java/io/quarkus/opentelemetry/deployment/exporter/otlp/OtlpExporterOverrideConfigTest.java
+++ extensions/opentelemetry/deployment/src/test/java/io/quarkus/opentelemetry/deployment/exporter/otlp/OtlpExporterOverrideConfigTest.java
@@ -11,15 +11,13 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import io.quarkus.opentelemetry.runtime.config.runtime.exporter.OtlpExporterRuntimeConfig;
 import io.quarkus.test.QuarkusUnitTest;
 
-public class OtlpExporterConfigTest {
+public class OtlpExporterOverrideConfigTest {
     @RegisterExtension
     static final QuarkusUnitTest TEST = new QuarkusUnitTest()
             .withEmptyApplication()
             .overrideConfigKey("quarkus.otel.traces.exporter", "cdi")
-            .overrideConfigKey("quarkus.otel.exporter.otlp.protocol", "wrong")
-            .overrideConfigKey("quarkus.otel.exporter.otlp.traces.protocol", "http/protobuf")
-            .overrideConfigKey("quarkus.otel.exporter.otlp.traces.endpoint", "http://localhost ")
-            .overrideConfigKey("quarkus.otel.metrics.exporter", "none")
+            .overrideConfigKey("quarkus.otel.exporter.otlp.protocol", "http/protobuf")
+            .overrideConfigKey("quarkus.otel.exporter.otlp.endpoint", "http://localhost ")
             .overrideConfigKey("quarkus.otel.bsp.schedule.delay", "50")
             .overrideConfigKey("quarkus.otel.bsp.export.timeout", "PT1S");
 
--- extensions/opentelemetry/runtime/src/test/java/io/quarkus/opentelemetry/runtime/exporter/otlp/OtlpExporterProviderTest.java
+++ extensions/opentelemetry/runtime/src/test/java/io/quarkus/opentelemetry/runtime/exporter/otlp/OtlpExporterProviderTest.java
@@ -18,65 +18,130 @@ import io.quarkus.opentelemetry.runtime.config.runtime.exporter.OtlpExporterTrac
 class OtlpExporterProviderTest {
 
     @Test
-    public void resolveEndpoint_legacyWins() {
-        assertEquals("http://localhost:1111/",
-                OTelExporterRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
-                        DEFAULT_GRPC_BASE_URI,
-                        "http://localhost:1111/",
-                        "http://localhost:2222/")));
-    }
-
-    @Test
-    public void resolveEndpoint_newWins() {
+    public void resolveTraceEndpoint_newWins() {
         assertEquals("http://localhost:2222/",
-                OTelExporterRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
+                OTelExporterRecorder.resolveTraceEndpoint(createOtlpExporterRuntimeConfig(
                         "http://localhost:1111/",
-                        DEFAULT_GRPC_BASE_URI,
                         "http://localhost:2222/")));
     }
 
     @Test
-    public void resolveEndpoint_globalWins() {
+    public void resolveTraceEndpoint_globalWins() {
         assertEquals("http://localhost:1111/",
-                OTelExporterRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
+                OTelExporterRecorder.resolveTraceEndpoint(createOtlpExporterRuntimeConfig(
                         "http://localhost:1111/",
-                        DEFAULT_GRPC_BASE_URI,
                         DEFAULT_GRPC_BASE_URI)));
     }
 
     @Test
-    public void resolveEndpoint_legacyTraceWins() {
+    public void resolveTraceEndpoint_legacyTraceWins() {
         assertEquals("http://localhost:2222/",
-                OTelExporterRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
+                OTelExporterRecorder.resolveTraceEndpoint(createOtlpExporterRuntimeConfig(
                         DEFAULT_GRPC_BASE_URI,
-                        null,
                         "http://localhost:2222/")));
     }
 
     @Test
-    public void resolveEndpoint_legacyGlobalWins() {
+    public void resolveTraceEndpoint_legacyGlobalWins() {
         assertEquals(DEFAULT_GRPC_BASE_URI,
-                OTelExporterRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
+                OTelExporterRecorder.resolveTraceEndpoint(createOtlpExporterRuntimeConfig(
                         DEFAULT_GRPC_BASE_URI,
+                        null)));
+    }
+
+    @Test
+    public void resolveTraceEndpoint_testIsSet() {
+        assertEquals(DEFAULT_GRPC_BASE_URI,
+                OTelExporterRecorder.resolveTraceEndpoint(createOtlpExporterRuntimeConfig(
                         null,
                         null)));
     }
 
     @Test
-    public void resolveEndpoint_testIsSet() {
+    public void resolveMetricEndpoint_newWins() {
+        assertEquals("http://localhost:2222/",
+                OTelExporterRecorder.resolveMetricEndpoint(createOtlpExporterRuntimeConfig(
+                        "http://localhost:1111/",
+                        "http://localhost:2222/")));
+    }
+
+    @Test
+    public void resolveMetricEndpoint_globalWins() {
+        assertEquals("http://localhost:1111/",
+                OTelExporterRecorder.resolveMetricEndpoint(createOtlpExporterRuntimeConfig(
+                        "http://localhost:1111/",
+                        DEFAULT_GRPC_BASE_URI)));
+    }
+
+    @Test
+    public void resolveMetricEndpoint_legacyTraceWins() {
+        assertEquals("http://localhost:2222/",
+                OTelExporterRecorder.resolveMetricEndpoint(createOtlpExporterRuntimeConfig(
+                        DEFAULT_GRPC_BASE_URI,
+                        "http://localhost:2222/")));
+    }
+
+    @Test
+    public void resolveMetricEndpoint_legacyGlobalWins() {
         assertEquals(DEFAULT_GRPC_BASE_URI,
-                OTelExporterRecorder.resolveEndpoint(createOtlpExporterRuntimeConfig(
-                        null,
+                OTelExporterRecorder.resolveMetricEndpoint(createOtlpExporterRuntimeConfig(
+                        DEFAULT_GRPC_BASE_URI,
+                        null)));
+    }
+
+    @Test
+    public void resolveMetricEndpoint_testIsSet() {
+        assertEquals(DEFAULT_GRPC_BASE_URI,
+                OTelExporterRecorder.resolveMetricEndpoint(createOtlpExporterRuntimeConfig(
                         null,
                         null)));
     }
 
-    private OtlpExporterRuntimeConfig createOtlpExporterRuntimeConfig(String exporterGlobal, String legacyTrace,
-            String newTrace) {
+    private OtlpExporterRuntimeConfig createOtlpExporterRuntimeConfig(String exporterGlobal, String newTrace) {
         return new OtlpExporterRuntimeConfig() {
             @Override
             public Optional<String> endpoint() {
                 return Optional.ofNullable(exporterGlobal);
+            }
+
+            @Override
+            public Optional<List<String>> headers() {
+                return Optional.empty();
+            }
+
+            @Override
+            public Optional<CompressionType> compression() {
+                return Optional.empty();
+            }
+
+            @Override
+            public Duration timeout() {
+                return null;
+            }
+
+            @Override
+            public Optional<String> protocol() {
+                return Optional.empty();
+            }
+
+            @Override
+            public KeyCert keyCert() {
+                return null;
+            }
+
+            @Override
+            public TrustCert trustCert() {
+                return null;
+            }
+
+            @Override
+            public Optional<String> tlsConfigurationName() {
+                return Optional.empty();
+            }
+
+            @Override
+            public ProxyConfig proxyOptions() {
+                return null;
             }
 
             @Override
@@ -85,11 +150,6 @@ class OtlpExporterProviderTest {
                     @Override
                     public Optional<String> endpoint() {
                         return Optional.ofNullable(newTrace);
-                    }
-
-                    @Override
-                    public Optional<String> legacyEndpoint() {
-                        return Optional.ofNullable(legacyTrace);
                     }
 
                     @Override

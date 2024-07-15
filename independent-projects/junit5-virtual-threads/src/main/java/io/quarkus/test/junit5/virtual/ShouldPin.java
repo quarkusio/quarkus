1cb8da9de627cd23954776bc490530b977a68862
bdeneuter <bdeneuter@gmail.com>
2024-07-15 16:29:00 +0200

    Add support for @ShouldNotPin/@ShouldPin on superclass
    Fix for https://github.com/quarkusio/quarkus/issues/41782
    Allow the usage of @ShouldPin and @ShouldNotPin on super classes

--- independent-projects/junit5-virtual-threads/src/main/java/io/quarkus/test/junit5/virtual/ShouldPin.java
+++ independent-projects/junit5-virtual-threads/src/main/java/io/quarkus/test/junit5/virtual/ShouldPin.java
@@ -1,6 +1,7 @@
 package io.quarkus.test.junit5.virtual;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -12,6 +13,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.TYPE })
+@Inherited
 public @interface ShouldPin {
     int atMost() default Integer.MAX_VALUE;
 

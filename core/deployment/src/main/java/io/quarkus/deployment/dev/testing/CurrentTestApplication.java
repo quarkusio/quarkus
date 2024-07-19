9b9758118af7d6c537b16d50c26301835f0ba56f
Holly Cummins <hcummins@redhat.com>
2024-07-19 16:41:14 +0100

    Load tests with runtime classloader, including profile support

--- core/deployment/src/main/java/io/quarkus/deployment/dev/testing/CurrentTestApplication.java
+++ core/deployment/src/main/java/io/quarkus/deployment/dev/testing/CurrentTestApplication.java
@@ -6,6 +6,8 @@ import io.quarkus.bootstrap.app.CuratedApplication;
 
 /**
  * This class is a bit of a hack, it provides a way to pass in the current curratedApplication into the TestExtension
+ * TODO It is only needed for QuarkusMainTest, so we may be able to find a better way.
+ * For example, what about JUnit state?
  */
 public class CurrentTestApplication implements Consumer<CuratedApplication> {
     public static volatile CuratedApplication curatedApplication;

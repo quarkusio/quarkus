8b15c3aadff795b7f55354857f2269d451d830d6
Clement Escoffier <clement.escoffier@gmail.com>
2024-07-22 16:24:22 +0200

    Rewrite TLS test for the rest-client and reactive-rest-client
    - Remove the trust store maven plugin which has not be super reliable recently on CI
    - Use the cert generator instead
    - Make sure the tests are not tainted with a quarkus.tls.trust-all=true
    - did not use the certificate annotation as the certificates are required to start the test resources, and it would require annotating all the test cases (as the order is undefined)

--- integration-tests/rest-client/src/main/java/io/quarkus/it/rest/client/selfsigned/ExternalSelfSignedResource.java
+++ integration-tests/rest-client/src/main/java/io/quarkus/it/rest/client/selfsigned/ExternalSelfSignedResource.java
@@ -1,9 +1,6 @@
 package io.quarkus.it.rest.client.selfsigned;
 
 import java.io.IOException;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -28,31 +25,6 @@ public class ExternalSelfSignedResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String perform() throws IOException {
         return String.valueOf(client.invoke().getStatus());
-    }
-
-    @GET
-    @Path("/java")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String invokeJavaURLWithDefaultTruststore() throws IOException {
-        try {
-            return doGetCipher();
-        } catch (IOException e) {
-            // if it fails it might be because the remote service is down, so sleep and try again
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ignored) {
-            }
-            return doGetCipher();
-        }
-    }
-
-    private String doGetCipher() throws IOException {
-        // this URL provides an always on example of an HTTPS URL utilizing self-signed certificate
-        URL url = new URL("https://self-signed.badssl.com/");
-        HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.getResponseCode();
-        return con.getCipherSuite();
     }
 
 }

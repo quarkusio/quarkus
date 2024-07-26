bf6232befeae0dbf8be8063b48635693db4867bb
Gunther C. Wenda <github@wenda-it.de>
2024-07-26 12:10:06 +0200

    fix include_all in native image

--- integration-tests/liquibase/src/test/java/io/quarkus/it/liquibase/LiquibaseFunctionalityTest.java
+++ integration-tests/liquibase/src/test/java/io/quarkus/it/liquibase/LiquibaseFunctionalityTest.java
@@ -15,7 +15,7 @@ public class LiquibaseFunctionalityTest {
     @Test
     @DisplayName("Migrates a schema correctly using integrated instance")
     public void testLiquibaseQuarkusFunctionality() {
-        doTestLiquibaseQuarkusFunctionality(isIncludeAllExpectedToWork());
+        doTestLiquibaseQuarkusFunctionality();
     }
 
     @Test
@@ -25,21 +25,17 @@ public class LiquibaseFunctionalityTest {
                 "ADMIN"));
     }
 
-    static void doTestLiquibaseQuarkusFunctionality(boolean isIncludeAllExpectedToWork) {
+    static void doTestLiquibaseQuarkusFunctionality() {
         when()
                 .get("/liquibase/update")
                 .then()
                 .body(is(
                         "create-tables-1,test-1,create-view-inline,create-view-file-abs,create-view-file-rel,"
-                                + (isIncludeAllExpectedToWork ? "includeAll-1,includeAll-2," : "")
+                                + ("includeAll-1,includeAll-2,")
                                 + "json-create-tables-1,json-test-1,"
                                 + "sql-create-tables-1,sql-test-1,"
                                 + "yaml-create-tables-1,yaml-test-1,"
                                 + "00000000000000,00000000000001,00000000000002,"
                                 + "1613578374533-1,1613578374533-2,1613578374533-3"));
-    }
-
-    protected boolean isIncludeAllExpectedToWork() {
-        return true;
     }
 }

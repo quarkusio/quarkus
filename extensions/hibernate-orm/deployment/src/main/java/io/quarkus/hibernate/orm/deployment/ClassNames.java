699708a1f16dba9f08db5f69e1c50a565cdd2849
Yoann Rodière <yoann@hibernate.org>
2024-08-01 09:42:14 +0200

    Revert "Work around https://hibernate.atlassian.net/browse/HHH-18284"
    This reverts commit 20940a56279b324da14d17a6a498a936ece7fd0c.

--- extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/ClassNames.java
+++ extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/ClassNames.java
@@ -83,10 +83,6 @@ public final class ClassNames {
             createConstant("org.hibernate.tuple.UpdateTimestampGeneration"),
             createConstant("org.hibernate.tuple.VmValueGeneration"));
 
-    public static final List<DotName> EXTRA_EVENT_LISTENERS = List.of(
-            createConstant("org.hibernate.event.spi.PreUpsertEventListener"),
-            createConstant("org.hibernate.event.spi.PostUpsertEventListener"));
-
     public static final List<DotName> PACKAGE_ANNOTATIONS = List.of(
             createConstant("org.hibernate.annotations.CollectionTypeRegistration"),
             createConstant("org.hibernate.annotations.CompositeTypeRegistration"),

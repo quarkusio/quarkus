4677fb7311d78d365824868ca160a7284db25683
peubouzon <peubouzon@gmail.com>
2024-08-28 06:55:51 -0300

    add a javadoc to JsonProviderHolder

--- extensions/jsonp/runtime/src/main/java/io/quarkus/jsonp/JsonProviderHolder.java
+++ extensions/jsonp/runtime/src/main/java/io/quarkus/jsonp/JsonProviderHolder.java
@@ -2,6 +2,12 @@ package io.quarkus.jsonp;
 
 import jakarta.json.spi.JsonProvider;
 
+/**
+ * A utility class that holds an instance of {@link JsonProvider}.
+ * Should be used instead of {@link jakarta.json.Json} for performance reasons.
+ *
+ * @see <a href="https://github.com/jakartaee/jsonp-api/issues/154">Json factory methods are very inefficient</a>
+ */
 public final class JsonProviderHolder {
 
     private JsonProviderHolder() {

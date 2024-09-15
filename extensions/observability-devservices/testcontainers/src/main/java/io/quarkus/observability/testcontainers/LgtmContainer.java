fcd314f5bba941efd580025629a23667a3e2bd5a
melloware <mellowaredev@gmail.com>
2024-09-15 21:05:27 +0200

    Fix #40933: LGTM Quarkus Dashboard

--- extensions/observability-devservices/testcontainers/src/main/java/io/quarkus/observability/testcontainers/LgtmContainer.java
+++ extensions/observability-devservices/testcontainers/src/main/java/io/quarkus/observability/testcontainers/LgtmContainer.java
@@ -3,7 +3,10 @@ package io.quarkus.observability.testcontainers;
 import java.util.Optional;
 import java.util.Set;
 
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.logging.Logger;
+import org.testcontainers.utility.MountableFile;
 
 import io.quarkus.observability.common.ContainerConstants;
 import io.quarkus.observability.common.config.AbstractGrafanaConfig;
@@ -17,6 +20,21 @@ public class LgtmContainer extends GrafanaContainer<LgtmContainer, LgtmConfig> {
 
     protected static final String LGTM_NETWORK_ALIAS = "ltgm.testcontainer.docker";
 
+    protected static final String PROMETHEUS_CONFIG = """
+                global:
+                  scrape_interval: 10s
+                  evaluation_interval: 10s
+                storage:
+                  tsdb:
+                    out_of_order_time_window: 10m
+                scrape_configs:
+                  - job_name: '%s'
+                    metrics_path: '%s%s'
+                    scrape_interval: 10s
+                    static_configs:
+                      - targets: ['%s:%d']
+            """;
+
     public LgtmContainer() {
         this(new LgtmConfigImpl());
     }
@@ -25,10 +43,27 @@ public class LgtmContainer extends GrafanaContainer<LgtmContainer, LgtmConfig> {
         super(config);
         addExposedPorts(config.otlpPort());
         withLogConsumer(new LgtmContainerLogConsumer(log).withPrefix("LGTM"));
+        withCopyFileToContainer(
+                MountableFile.forClasspathResource("/grafana-dashboard-quarkus-metrics.json"),
+                "/otel-lgtm/grafana-dashboard-jvm-metrics.json");
+        withCopyFileToContainer(
+                MountableFile.forClasspathResource("/grafana-dashboards.yaml"),
+                "/otel-lgtm/grafana-dashboards.yaml");
+        addFileToContainer(getPrometheusConfig().getBytes(), "/otel-lgtm/prometheus.yaml");
+
     }
 
     public int getOtlpPort() {
         return getMappedPort(config.otlpPort());
+    }
+
+    private String getPrometheusConfig() {
+        Config runtimeConfig = ConfigProvider.getConfig();
+        String rootPath = runtimeConfig.getOptionalValue("quarkus.management.root-path", String.class).orElse("/q");
+        String metricsPath = runtimeConfig.getOptionalValue("quarkus.management.metrics.path", String.class).orElse("/metrics");
+        int httpPort = runtimeConfig.getOptionalValue("quarkus.http.port", Integer.class).orElse(0);
+
+        return String.format(PROMETHEUS_CONFIG, config.serviceName(), rootPath, metricsPath, "host.docker.internal", httpPort);
     }
 
     protected static class LgtmConfigImpl extends AbstractGrafanaConfig implements LgtmConfig {

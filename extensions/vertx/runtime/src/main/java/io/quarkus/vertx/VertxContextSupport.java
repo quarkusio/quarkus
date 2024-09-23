d211835606fea19e6ec510a4191604e1d3569dcd
Martin Kouba <mkouba@redhat.com>
2024-09-23 15:31:42 +0200

    Add VertxContextSupport#executeBlocking()
    - to improve WS next UX; i.e to run blocking code in
    HttpUpgradeCheck#perform() easily

--- extensions/vertx/runtime/src/main/java/io/quarkus/vertx/VertxContextSupport.java
+++ extensions/vertx/runtime/src/main/java/io/quarkus/vertx/VertxContextSupport.java
@@ -1,5 +1,6 @@
 package io.quarkus.vertx;
 
+import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -34,7 +35,7 @@ public final class VertxContextSupport {
      * @throws IllegalStateException If called on an event loop thread.
      */
     public static <T> T subscribeAndAwait(Supplier<Uni<T>> uniSupplier) throws Throwable {
-        Context context = getContext();
+        Context context = getContext(false);
         VertxContextSafetyToggle.setContextSafe(context, true);
         return Uni.createFrom().<T> emitter(e -> {
             context.runOnContext(new Handler<Void>() {
@@ -69,7 +70,7 @@ public final class VertxContextSupport {
      * @param subscribeConsumer
      */
     public static <T> void subscribe(Supplier<Multi<T>> multiSupplier, Consumer<MultiSubscribe<T>> subscribeConsumer) {
-        Context context = getContext();
+        Context context = getContext(false);
         VertxContextSafetyToggle.setContextSafe(context, true);
         context.runOnContext(new Handler<Void>() {
 
@@ -96,14 +97,44 @@ public final class VertxContextSupport {
         });
     }
 
-    private static Context getContext() {
+    /**
+     * Executes the supplied blocking {@link Callable} on a Vertx duplicated context; does not block the current thread.
+     * <p>
+     * If necessary, the CDI request context is activated during execution of the blocking code.
+     *
+     * @param <T>
+     * @param callable
+     * @return the produced {@link Uni}
+     * @see VertxContext#getOrCreateDuplicatedContext(Vertx)
+     */
+    public static <T> Uni<T> executeBlocking(Callable<T> callable) {
+        Context context = getContext(true);
+        return Uni.createFrom().completionStage(() -> {
+            return context.executeBlocking(() -> {
+                ManagedContext requestContext = Arc.container().requestContext();
+                boolean terminate = requestContext.isActive() ? false : true;
+                if (terminate) {
+                    requestContext.activate();
+                }
+                try {
+                    return callable.call();
+                } finally {
+                    if (terminate) {
+                        requestContext.terminate();
+                    }
+                }
+            }, false).toCompletionStage();
+        });
+    }
+
+    private static Context getContext(boolean blocking) {
         Context context = Vertx.currentContext();
         if (context == null) {
             Vertx vertx = VertxCoreRecorder.getVertx().get();
             context = VertxContext.getOrCreateDuplicatedContext(vertx);
         } else {
             // Executed on a vertx thread...
-            if (Context.isOnEventLoopThread()) {
+            if (!blocking && Context.isOnEventLoopThread()) {
                 throw new IllegalStateException("VertxContextSupport#subscribeAndAwait() must not be called on an event loop!");
             }
             context = VertxContext.getOrCreateDuplicatedContext(context);

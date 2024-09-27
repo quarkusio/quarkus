dd7c80e6e00f898ddff09d6fb5c14a7ba23ea1e1
Michael Edgar <michael@xlate.io>
2024-09-27 05:04:24 -0400

    Bump smallrye-open-api 3.13.0, MP OpenAPI to 3.1.2
    Signed-off-by: Michael Edgar <michael@xlate.io>

--- extensions/smallrye-openapi/runtime/src/main/java/io/quarkus/smallrye/openapi/runtime/OpenApiDocumentService.java
+++ extensions/smallrye-openapi/runtime/src/main/java/io/quarkus/smallrye/openapi/runtime/OpenApiDocumentService.java
@@ -4,45 +4,63 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import org.eclipse.microprofile.config.Config;
-import org.eclipse.microprofile.openapi.OASConfig;
 import org.eclipse.microprofile.openapi.OASFilter;
 import org.eclipse.microprofile.openapi.models.OpenAPI;
+import org.jboss.jandex.IndexView;
 
 import io.quarkus.smallrye.openapi.runtime.filter.DisabledRestEndpointsFilter;
 import io.smallrye.openapi.api.SmallRyeOpenAPI;
+import io.smallrye.openapi.runtime.io.Format;
 
 /**
  * Loads the document and make it available
  */
 @ApplicationScoped
-public class OpenApiDocumentService implements OpenApiDocumentHolder {
+public class OpenApiDocumentService {
 
     private final OpenApiDocumentHolder documentHolder;
 
     @Inject
     public OpenApiDocumentService(OASFilter autoSecurityFilter,
-            OpenApiRecorder.UserDefinedRuntimeFilters userDefinedRuntimeFilters, Config config) {
+            OpenApiRecorder.UserDefinedRuntimeFilters runtimeFilters, Config config) {
 
         ClassLoader loader = Optional.ofNullable(OpenApiConstants.classLoader)
                 .orElseGet(Thread.currentThread()::getContextClassLoader);
 
         try (InputStream source = loader.getResourceAsStream(OpenApiConstants.BASE_NAME + "JSON")) {
             if (source != null) {
-                var userFilters = userDefinedRuntimeFilters.filters();
+                Set<String> userFilters = new LinkedHashSet<>(runtimeFilters.filters());
                 boolean dynamic = config.getOptionalValue("quarkus.smallrye-openapi.always-run-filter", Boolean.class)
                         .orElse(Boolean.FALSE);
+                SmallRyeOpenAPI.Builder builder = new OpenAPIRuntimeBuilder()
+                        .withConfig(config)
+                        .withApplicationClassLoader(loader)
+                        .enableModelReader(false)
+                        .enableStandardStaticFiles(false)
+                        .enableAnnotationScan(false)
+                        .enableStandardFilter(false)
+                        .withCustomStaticFile(() -> source);
 
-                if (dynamic) {
-                    this.documentHolder = new DynamicDocument(source, config, autoSecurityFilter, userFilters);
+                // Auth-security and disabled endpoint filters will only run once
+                Optional.ofNullable(autoSecurityFilter)
+                        .ifPresent(builder::addFilter);
+                DisabledRestEndpointsFilter.maybeGetInstance()
+                        .ifPresent(builder::addFilter);
+
+                if (dynamic && !userFilters.isEmpty()) {
+                    // Only regenerate the OpenAPI document when configured and there are filters to run
+                    this.documentHolder = new DynamicDocument(builder, loader, userFilters);
                 } else {
-                    this.documentHolder = new StaticDocument(source, config, autoSecurityFilter, userFilters);
+                    userFilters.forEach(name -> builder.addFilter(name, loader, (IndexView) null));
+                    this.documentHolder = new StaticDocument(builder.build());
                 }
             } else {
                 this.documentHolder = new EmptyDocument();
@@ -52,12 +70,11 @@ public class OpenApiDocumentService implements OpenApiDocumentHolder {
         }
     }
 
-    public byte[] getJsonDocument() {
-        return this.documentHolder.getJsonDocument();
-    }
-
-    public byte[] getYamlDocument() {
-        return this.documentHolder.getYamlDocument();
+    byte[] getDocument(Format format) {
+        if (format.equals(Format.JSON)) {
+            return documentHolder.getJsonDocument();
+        }
+        return documentHolder.getYamlDocument();
     }
 
     static class EmptyDocument implements OpenApiDocumentHolder {
@@ -80,20 +97,7 @@ public class OpenApiDocumentService implements OpenApiDocumentHolder {
         private byte[] jsonDocument;
         private byte[] yamlDocument;
 
-        StaticDocument(InputStream source, Config config, OASFilter autoFilter, List<String> userFilters) {
-            SmallRyeOpenAPI.Builder builder = SmallRyeOpenAPI.builder()
-                    .withConfig(config)
-                    .enableModelReader(false)
-                    .enableStandardStaticFiles(false)
-                    .enableAnnotationScan(false)
-                    .enableStandardFilter(false)
-                    .withCustomStaticFile(() -> source);
-
-            Optional.ofNullable(autoFilter).ifPresent(builder::addFilter);
-            builder.addFilter(new DisabledRestEndpointsFilter());
-            userFilters.forEach(builder::addFilterName);
-
-            SmallRyeOpenAPI openAPI = builder.build();
+        StaticDocument(SmallRyeOpenAPI openAPI) {
             jsonDocument = openAPI.toJSON().getBytes(StandardCharsets.UTF_8);
             yamlDocument = openAPI.toYAML().getBytes(StandardCharsets.UTF_8);
         }
@@ -108,31 +112,18 @@ public class OpenApiDocumentService implements OpenApiDocumentHolder {
     }
 
     /**
-     * Generate the document on every request.
+     * Generate the document on every request by re-running user-provided OASFilters.
      */
     static class DynamicDocument implements OpenApiDocumentHolder {
 
         private SmallRyeOpenAPI.Builder builder;
-        private OpenAPI generatedOnBuild;
 
-        DynamicDocument(InputStream source, Config config, OASFilter autoFilter, List<String> annotatedUserFilters) {
-            builder = SmallRyeOpenAPI.builder()
-                    .withConfig(config)
-                    .enableModelReader(false)
-                    .enableStandardStaticFiles(false)
-                    .enableAnnotationScan(false)
-                    .enableStandardFilter(false)
-                    .withCustomStaticFile(() -> source);
-
-            generatedOnBuild = builder.build().model();
-
+        DynamicDocument(SmallRyeOpenAPI.Builder builder, ClassLoader loader, Set<String> userFilters) {
+            OpenAPI generatedOnBuild = builder.build().model();
             builder.withCustomStaticFile(() -> null);
             builder.withInitialModel(generatedOnBuild);
-
-            Optional.ofNullable(autoFilter).ifPresent(builder::addFilter);
-            builder.addFilter(new DisabledRestEndpointsFilter());
-            config.getOptionalValue(OASConfig.FILTER, String.class).ifPresent(builder::addFilterName);
-            annotatedUserFilters.forEach(builder::addFilterName);
+            userFilters.forEach(name -> builder.addFilter(name, loader, (IndexView) null));
+            this.builder = builder;
         }
 
         public byte[] getJsonDocument() {

80c2e38336cb889b91c40ed79d9c183b08d48a4c
Martin Kouba <mkouba@redhat.com>
2024-09-30 17:36:52 +0200

    Qute: fix generated ValueResolvers
    - fixes #43489
    - consider default methods in class hierarchy; previously, interface default methods were only considered if a
    ValueResolver was generated for an interface
    - also consider inherited fields in a class hierarchy
    
    (cherry picked from commit bc206a186f5897cfcd259ac667136740422a6b4b)

---
+++ independent-projects/qute/generator/src/test/java/io/quarkus/qute/generator/hierarchy/Level4.java
@@ -0,0 +1,10 @@
+package io.quarkus.qute.generator.hierarchy;
+
+public class Level4 extends Level3 {
+
+    public int overridenLevel = 4;
+
+    public int getLevel4() {
+        return 4;
+    }
+}

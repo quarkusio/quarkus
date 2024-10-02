41728eae5dd162145899b97420fe89ced84959f1
Foivos Zakkak <fzakkak@redhat.com>
2024-10-02 16:12:58 +0300

    Refactor HttpContentCompressorSubstitutions for consistency
    Adopt `Target_*` naming convention for substitution classes

--- extensions/netty/runtime/src/main/java/io/quarkus/netty/runtime/graal/HttpContentCompressorSubstitutions.java
+++ extensions/netty/runtime/src/main/java/io/quarkus/netty/runtime/graal/HttpContentCompressorSubstitutions.java
@@ -9,61 +9,61 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 
 public class HttpContentCompressorSubstitutions {
+}
 
-    @TargetClass(className = "io.netty.handler.codec.compression.ZstdEncoder", onlyWith = IsZstdAbsent.class)
-    public static final class ZstdEncoderFactorySubstitution {
+@TargetClass(className = "io.netty.handler.codec.compression.ZstdEncoder", onlyWith = IsZstdAbsent.class)
+final class Target_io_netty_handler_codec_compression_ZstdEncoder {
 
-        @Substitute
-        protected ByteBuf allocateBuffer(ChannelHandlerContext ctx, ByteBuf msg, boolean preferDirect) throws Exception {
-            throw new UnsupportedOperationException();
-        }
-
-        @Substitute
-        protected void encode(ChannelHandlerContext ctx, ByteBuf in, ByteBuf out) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Substitute
-        public void flush(final ChannelHandlerContext ctx) {
-            throw new UnsupportedOperationException();
-        }
+    @Substitute
+    protected ByteBuf allocateBuffer(ChannelHandlerContext ctx, ByteBuf msg, boolean preferDirect) throws Exception {
+        throw new UnsupportedOperationException();
     }
 
     @Substitute
-    @TargetClass(className = "io.netty.handler.codec.compression.ZstdConstants", onlyWith = IsZstdAbsent.class)
-    public static final class ZstdConstants {
-
-        // The constants make <clinit> calls to com.github.luben.zstd.Zstd so we cut links with that substitution.
-
-        static final int DEFAULT_COMPRESSION_LEVEL = 0;
-
-        static final int MIN_COMPRESSION_LEVEL = 0;
-
-        static final int MAX_COMPRESSION_LEVEL = 0;
-
-        static final int MAX_BLOCK_SIZE = 0;
-
-        static final int DEFAULT_BLOCK_SIZE = 0;
+    protected void encode(ChannelHandlerContext ctx, ByteBuf in, ByteBuf out) {
+        throw new UnsupportedOperationException();
     }
 
-    public static class IsZstdAbsent implements BooleanSupplier {
+    @Substitute
+    public void flush(final ChannelHandlerContext ctx) {
+        throw new UnsupportedOperationException();
+    }
+}
 
-        private boolean zstdAbsent;
+@Substitute
+@TargetClass(className = "io.netty.handler.codec.compression.ZstdConstants", onlyWith = IsZstdAbsent.class)
+final class Target_io_netty_handler_codec_compression_ZstdConstants {
 
-        public IsZstdAbsent() {
-            try {
-                Class.forName("com.github.luben.zstd.Zstd");
-                zstdAbsent = false;
-            } catch (Exception e) {
-                // It can be a classloading issue (the library is not available), or a native issue
-                // (the library for the current OS/arch is not available)
-                zstdAbsent = true;
-            }
+    // The constants make <clinit> calls to com.github.luben.zstd.Zstd so we cut links with that substitution.
+
+    static final int DEFAULT_COMPRESSION_LEVEL = 0;
+
+    static final int MIN_COMPRESSION_LEVEL = 0;
+
+    static final int MAX_COMPRESSION_LEVEL = 0;
+
+    static final int MAX_BLOCK_SIZE = 0;
+
+    static final int DEFAULT_BLOCK_SIZE = 0;
+}
+
+class IsZstdAbsent implements BooleanSupplier {
+
+    private boolean zstdAbsent;
+
+    public IsZstdAbsent() {
+        try {
+            Class.forName("com.github.luben.zstd.Zstd");
+            zstdAbsent = false;
+        } catch (Exception e) {
+            // It can be a classloading issue (the library is not available), or a native issue
+            // (the library for the current OS/arch is not available)
+            zstdAbsent = true;
         }
+    }
 
-        @Override
-        public boolean getAsBoolean() {
-            return zstdAbsent;
-        }
+    @Override
+    public boolean getAsBoolean() {
+        return zstdAbsent;
     }
 }

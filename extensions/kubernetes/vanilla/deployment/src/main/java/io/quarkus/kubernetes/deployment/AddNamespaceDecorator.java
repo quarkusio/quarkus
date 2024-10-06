5677ed1904684ded9fb077f5c201d901b48139e8
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-10-05 22:33:05 -0300

    Use old strategy

--- extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/AddNamespaceDecorator.java
+++ extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/AddNamespaceDecorator.java
@@ -1,17 +1,16 @@
 package io.quarkus.kubernetes.deployment;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import io.dekorate.kubernetes.decorator.Decorator;
 import io.dekorate.kubernetes.decorator.ResourceProvidingDecorator;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.KubernetesListBuilder;
-import io.fabric8.kubernetes.api.model.Namespaced;
 
 public class AddNamespaceDecorator extends Decorator<KubernetesListBuilder> {
 
+    private static final Set<String> CLUSTERED_RESOURCES = Set.of("ClusterRoleBinding", "ClusterRole");
     private final String namespace;
 
     public AddNamespaceDecorator(String namespace) {
@@ -22,10 +21,17 @@ public class AddNamespaceDecorator extends Decorator<KubernetesListBuilder> {
     public void visit(KubernetesListBuilder list) {
         List<HasMetadata> buildItems = list.buildItems()
                 .stream()
-                .filter(Namespaced.class::isInstance)
-                .peek(o -> o.setMetadata(o.getMetadata().edit().withNamespace(namespace).build())).collect(Collectors.toList());
-
+                .filter(obj -> obj instanceof HasMetadata)
+                .peek(obj -> {
+                    if (isEligibleForChangingNamespace(obj)) {
+                        obj.setMetadata(obj.getMetadata().edit().withNamespace(namespace).build());
+                    }
+                }).collect(Collectors.toList());
         list.withItems(buildItems);
+    }
+
+    private boolean isEligibleForChangingNamespace(HasMetadata obj) {
+        return obj.getMetadata().getNamespace() == null && !CLUSTERED_RESOURCES.contains(obj.getKind());
     }
 
     @Override

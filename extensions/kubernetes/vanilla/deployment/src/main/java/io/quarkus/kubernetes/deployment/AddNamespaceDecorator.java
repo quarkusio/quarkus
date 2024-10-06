dd850c42ded3064549a199a4d3c2f3ab89de2593
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-10-05 22:34:43 -0300

    Apply formatter and impsort

--- extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/AddNamespaceDecorator.java
+++ extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/AddNamespaceDecorator.java
@@ -1,6 +1,8 @@
 package io.quarkus.kubernetes.deployment;
 
-import java.util.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import io.dekorate.kubernetes.decorator.Decorator;

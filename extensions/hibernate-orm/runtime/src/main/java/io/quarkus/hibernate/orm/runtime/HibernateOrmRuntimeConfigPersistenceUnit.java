cca4c34b434a92e7b8d0eff2caf21260741d48c6
Yoann Rodière <yoann@hibernate.org>
2024-10-08 10:41:04 +0200

    Re-enable detection of the database version by default when using explicit Hibernate ORM dialects
    Because that detection is now done by the Hibernate ORM dialect itself,
    and should work properly for all dialects.

--- extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/HibernateOrmRuntimeConfigPersistenceUnit.java
+++ extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/HibernateOrmRuntimeConfigPersistenceUnit.java
@@ -112,13 +112,10 @@ public interface HibernateOrmRuntimeConfigPersistenceUnit {
          *
          * @asciidoclet
          */
-        // TODO change the default to "always enabled" when we solve version detection problems
-        //   See https://github.com/quarkusio/quarkus/issues/43703
-        //   See https://github.com/quarkusio/quarkus/issues/42255
         // TODO disable the check by default when offline startup is opted in
         //   See https://github.com/quarkusio/quarkus/issues/13522
         @WithName("version-check.enabled")
-        @ConfigDocDefault("`true` if the dialect was set automatically by Quarkus, `false` if it was set explicitly")
+        @ConfigDocDefault("`true`")
         Optional<Boolean> versionCheckEnabled();
     }
 

93ec6bb8eebc7c531b037db062ce31cf3a44b750
Georgios Andrianakis <geoand@gmail.com>
2024-10-14 16:51:22 +0300

    Ensure that AppCDS works properly with command line applications
    Fixes: #43768

--- extensions/arc/runtime/src/main/java/io/quarkus/arc/runtime/appcds/AppCDSRecorder.java
+++ extensions/arc/runtime/src/main/java/io/quarkus/arc/runtime/appcds/AppCDSRecorder.java
@@ -1,5 +1,6 @@
 package io.quarkus.arc.runtime.appcds;
 
+import io.quarkus.runtime.ApplicationLifecycleManager;
 import io.quarkus.runtime.PreventFurtherStepsException;
 import io.quarkus.runtime.annotations.Recorder;
 import io.quarkus.runtime.init.InitializationTaskRecorder;
@@ -7,10 +8,8 @@ import io.quarkus.runtime.init.InitializationTaskRecorder;
 @Recorder
 public class AppCDSRecorder {
 
-    public static final String QUARKUS_APPCDS_GENERATE_PROP = "quarkus.appcds.generate";
-
     public void controlGenerationAndExit() {
-        if (Boolean.parseBoolean(System.getProperty(QUARKUS_APPCDS_GENERATE_PROP, "false"))) {
+        if (ApplicationLifecycleManager.isAppCDSGeneration()) {
             InitializationTaskRecorder.preventFurtherRecorderSteps(5,
                     "Unable to properly shutdown Quarkus application when creating AppCDS",
                     PreventFurtherStepsException::new);

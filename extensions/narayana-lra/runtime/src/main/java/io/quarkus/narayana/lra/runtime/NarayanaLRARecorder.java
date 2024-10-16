008c2a646cb11558a9dfd581ef8e2956a216183d
Marco Sappé Griot <msappegr@redhat.com>
2024-10-16 17:21:08 +0200

    Upgrade Narayana to 7.1.0.Final

--- extensions/narayana-lra/runtime/src/main/java/io/quarkus/narayana/lra/runtime/NarayanaLRARecorder.java
+++ extensions/narayana-lra/runtime/src/main/java/io/quarkus/narayana/lra/runtime/NarayanaLRARecorder.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 import org.jboss.logging.Logger;
 
-import io.narayana.lra.client.NarayanaLRAClient;
+import io.narayana.lra.client.internal.NarayanaLRAClient;
 import io.narayana.lra.client.internal.proxy.nonjaxrs.LRAParticipant;
 import io.narayana.lra.client.internal.proxy.nonjaxrs.LRAParticipantRegistry;
 import io.quarkus.runtime.annotations.Recorder;

3653c47e0e0a61463a8344a31f34e0789de6f15f
Jan Martiska <jmartisk@redhat.com>
2024-10-29 15:35:33 +0100

    For the third column (property default value), skip the description
    (cherry picked from commit 3912bb159a342beaed854819accd7a8a8dc4cfa1)

--- docs/src/main/java/io/quarkus/docs/generation/AssembleDownstreamDocumentation.java
+++ docs/src/main/java/io/quarkus/docs/generation/AssembleDownstreamDocumentation.java
@@ -62,7 +62,14 @@ public class AssembleDownstreamDocumentation {
     private static final String SOURCE_BLOCK_PREFIX = "[source";
     private static final String SOURCE_BLOCK_DELIMITER = "--";
     private static final Pattern FOOTNOTE_PATTERN = Pattern.compile("footnote:([a-z0-9_-]+)\\[(\\])?");
+
     private static final Pattern TOOLTIP_PATTERN = Pattern.compile("tooltip:([a-z0-9_-]+)\\[(.*?)\\](, ?)?");
+    // A tooltip that is detected to be in the third column of a configuration reference table
+    // (a default value for a configuration property) - in this case, let's strip out the description from it.
+    // We assume it's in the third column if it follows right after a | at the beginning of a line (if it's in
+    // the second column, it would be preceded by a|, not just |).
+    private static final Pattern TOOLTIP_PATTERN_DEFAULT_COLUMN = Pattern.compile("^\\|tooltip:([a-z0-9_-]+)\\[(.*?)\\](, ?)?",
+            Pattern.MULTILINE);
 
     private static final String PROJECT_NAME_ATTRIBUTE = "{project-name}";
     private static final String RED_HAT_BUILD_OF_QUARKUS = "Red Hat build of Quarkus";
@@ -482,6 +489,12 @@ public class AssembleDownstreamDocumentation {
             }
 
             return "footnoteref:[" + mr.group(1) + ", ";
+        });
+
+        content = TOOLTIP_PATTERN_DEFAULT_COLUMN.matcher(content).replaceAll(mr -> {
+            // for tooltips in the third column of the configuration reference table (the default values),
+            // don't include the description, just the plain value
+            return "|*" + mr.group(1) + "*";
         });
 
         content = TOOLTIP_PATTERN.matcher(content).replaceAll(mr -> {

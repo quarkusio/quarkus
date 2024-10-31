ff49e412cf884e04f7e070b17b6d7fba8c63b813
jcarvaja <jcarvaja@redhat.com>
2024-10-31 10:28:06 +0100

    Configure HAL links for user defined REST resources in REST Data Panache

--- extensions/panache/rest-data-panache/deployment/src/main/java/io/quarkus/rest/data/panache/deployment/methods/UserMethodsWithAnnotationsImplementor.java
+++ extensions/panache/rest-data-panache/deployment/src/main/java/io/quarkus/rest/data/panache/deployment/methods/UserMethodsWithAnnotationsImplementor.java
@@ -1,49 +1,33 @@
 package io.quarkus.rest.data.panache.deployment.methods;
 
-import org.jboss.jandex.AnnotationTarget;
-
+import io.quarkus.deployment.Capabilities;
 import io.quarkus.gizmo.ClassCreator;
 import io.quarkus.gizmo.FieldDescriptor;
-import io.quarkus.gizmo.MethodCreator;
-import io.quarkus.gizmo.MethodDescriptor;
-import io.quarkus.gizmo.ResultHandle;
 import io.quarkus.rest.data.panache.deployment.ResourceMetadata;
 import io.quarkus.rest.data.panache.deployment.properties.ResourceProperties;
 
 /**
- * Propagate all the user methods that have annotations.
- *
- * This is necessary when users use annotations with an interceptor binding like `@Transactional`.
+ * Loop over all the user-defined methods in the interface. These methods will be overridden in the generated resource
+ * if and only if it uses annotations.
  */
 public final class UserMethodsWithAnnotationsImplementor implements MethodImplementor {
+
+    private final Capabilities capabilities;
+
+    public UserMethodsWithAnnotationsImplementor(Capabilities capabilities) {
+        this.capabilities = capabilities;
+    }
 
     @Override
     public void implement(ClassCreator classCreator, ResourceMetadata resourceMetadata,
             ResourceProperties resourceProperties, FieldDescriptor resourceField) {
         if (resourceMetadata.getResourceInterface() != null) {
             for (var methodInfo : resourceMetadata.getResourceInterface().methods()) {
+                // propagate only the default methods with annotations (needed for `@Transactional` annotations).
                 if (methodInfo.isDefault() && !methodInfo.annotations().isEmpty()) {
-                    MethodCreator methodCreator = classCreator.getMethodCreator(MethodDescriptor.of(methodInfo));
-                    methodCreator.setSignature(methodInfo.genericSignatureIfRequired());
-                    for (var annotation : methodInfo.annotations()) {
-                        if (annotation.target().kind() == AnnotationTarget.Kind.METHOD_PARAMETER) {
-                            short position = annotation.target().asMethodParameter().position();
-                            methodCreator.getParameterAnnotations(position).addAnnotation(annotation);
-                        }
-
-                        if (annotation.target().kind() == AnnotationTarget.Kind.METHOD) {
-                            methodCreator.addAnnotation(annotation);
-                        }
-                    }
-
-                    ResultHandle[] params = new ResultHandle[methodInfo.parametersCount()];
-                    for (int paramIdx = 0; paramIdx < methodInfo.parametersCount(); paramIdx++) {
-                        params[paramIdx] = methodCreator.getMethodParam(paramIdx);
-                    }
-
-                    methodCreator.returnValue(
-                            methodCreator.invokeSpecialInterfaceMethod(methodInfo, methodCreator.getThis(), params));
-                    methodCreator.close();
+                    OverrideUserMethodImplementor implementor = new OverrideUserMethodImplementor(methodInfo,
+                            capabilities);
+                    implementor.implement(classCreator, resourceMetadata, resourceProperties, resourceField);
                 }
             }
         }

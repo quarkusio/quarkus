ac87489590d40f51a23b6c016bb50418a5c88187
Robert Stupp <snazy@snazy.de>
2024-11-04 15:17:59 +0100

    Consider resource arguments when ordering tests

--- test-framework/common/src/test/java/io/quarkus/test/common/TestResourceManagerReloadTest.java
+++ test-framework/common/src/test/java/io/quarkus/test/common/TestResourceManagerReloadTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
@@ -22,66 +23,87 @@ public class TestResourceManagerReloadTest {
     @Test
     public void differentCount() {
         assertTrue(testResourcesRequireReload(Collections.emptySet(),
-                Set.of(new TestResourceComparisonInfo("test", RESTRICTED_TO_CLASS))));
+                Set.of(new TestResourceComparisonInfo("test", RESTRICTED_TO_CLASS, Map.of()))));
 
-        assertTrue(testResourcesRequireReload(Set.of(new TestResourceComparisonInfo("test", RESTRICTED_TO_CLASS)),
+        assertTrue(testResourcesRequireReload(Set.of(new TestResourceComparisonInfo("test", RESTRICTED_TO_CLASS, Map.of())),
                 Collections.emptySet()));
     }
 
     @Test
     public void sameSingleRestrictedToClassResource() {
         assertTrue(testResourcesRequireReload(
-                Set.of(new TestResourceComparisonInfo("test", RESTRICTED_TO_CLASS)),
-                Set.of(new TestResourceComparisonInfo("test", RESTRICTED_TO_CLASS))));
+                Set.of(new TestResourceComparisonInfo("test", RESTRICTED_TO_CLASS, Map.of())),
+                Set.of(new TestResourceComparisonInfo("test", RESTRICTED_TO_CLASS, Map.of()))));
     }
 
     @Test
     public void sameSingleMatchingResource() {
         assertFalse(testResourcesRequireReload(
-                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES)),
-                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES))));
+                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of())),
+                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of()))));
+    }
+
+    @Test
+    public void sameSingleMatchingResourceWithArgs() {
+        assertFalse(testResourcesRequireReload(
+                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of("a", "b"))),
+                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of("a", "b")))));
+    }
+
+    @Test
+    public void sameSingleResourceDifferentArgs() {
+        assertTrue(testResourcesRequireReload(
+                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of())),
+                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of("a", "b")))));
+    }
+
+    @Test
+    public void sameSingleResourceDifferentArgValues() {
+        assertTrue(testResourcesRequireReload(
+                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of("a", "b"))),
+                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of("a", "c")))));
     }
 
     @Test
     public void differentSingleMatchingResource() {
         assertTrue(testResourcesRequireReload(
-                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES)),
-                Set.of(new TestResourceComparisonInfo("test2", MATCHING_RESOURCES))));
+                Set.of(new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of())),
+                Set.of(new TestResourceComparisonInfo("test2", MATCHING_RESOURCES, Map.of()))));
     }
 
     @Test
     public void sameMultipleMatchingResource() {
         assertFalse(testResourcesRequireReload(
                 Set.of(
-                        new TestResourceComparisonInfo("test", MATCHING_RESOURCES),
-                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES),
-                        new TestResourceComparisonInfo("test3", GLOBAL)),
-                Set.of(new TestResourceComparisonInfo("test3", GLOBAL),
-                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES),
-                        new TestResourceComparisonInfo("test", MATCHING_RESOURCES))));
+                        new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of()),
+                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES, Map.of()),
+                        new TestResourceComparisonInfo("test3", GLOBAL, Map.of())),
+                Set.of(new TestResourceComparisonInfo("test3", GLOBAL, Map.of()),
+                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES, Map.of()),
+                        new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of()))));
     }
 
     @Test
     public void differentMultipleMatchingResource() {
         assertTrue(testResourcesRequireReload(
                 Set.of(
-                        new TestResourceComparisonInfo("test", MATCHING_RESOURCES),
-                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES),
-                        new TestResourceComparisonInfo("test3", GLOBAL)),
-                Set.of(new TestResourceComparisonInfo("test3", GLOBAL),
-                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES),
-                        new TestResourceComparisonInfo("TEST", MATCHING_RESOURCES))));
+                        new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of()),
+                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES, Map.of()),
+                        new TestResourceComparisonInfo("test3", GLOBAL, Map.of())),
+                Set.of(new TestResourceComparisonInfo("test3", GLOBAL, Map.of()),
+                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES, Map.of()),
+                        new TestResourceComparisonInfo("TEST", MATCHING_RESOURCES, Map.of()))));
     }
 
     @Test
     public void differentGlobalMultipleMatchingResource() {
         assertTrue(testResourcesRequireReload(
                 Set.of(
-                        new TestResourceComparisonInfo("test", MATCHING_RESOURCES),
-                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES),
-                        new TestResourceComparisonInfo("test4", GLOBAL)),
-                Set.of(new TestResourceComparisonInfo("test3", GLOBAL),
-                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES),
-                        new TestResourceComparisonInfo("TEST", MATCHING_RESOURCES))));
+                        new TestResourceComparisonInfo("test", MATCHING_RESOURCES, Map.of()),
+                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES, Map.of()),
+                        new TestResourceComparisonInfo("test4", GLOBAL, Map.of())),
+                Set.of(new TestResourceComparisonInfo("test3", GLOBAL, Map.of()),
+                        new TestResourceComparisonInfo("test2", MATCHING_RESOURCES, Map.of()),
+                        new TestResourceComparisonInfo("TEST", MATCHING_RESOURCES, Map.of()))));
     }
 }

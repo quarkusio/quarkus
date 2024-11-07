77747da5a86d18ecb45827a43b3493bfe96285ec
Georgios Andrianakis <geoand@gmail.com>
2024-11-07 20:22:31 +0200

    Ensure that custom Jackson modules work in dev-mode
    Fixes: #44231

--- extensions/vertx/runtime/src/main/java/io/quarkus/vertx/runtime/jackson/QuarkusJacksonFactory.java
+++ extensions/vertx/runtime/src/main/java/io/quarkus/vertx/runtime/jackson/QuarkusJacksonFactory.java
@@ -1,5 +1,7 @@
 package io.quarkus.vertx.runtime.jackson;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import io.vertx.core.json.jackson.DatabindCodec;
 import io.vertx.core.json.jackson.JacksonCodec;
 import io.vertx.core.spi.JsonFactory;
@@ -9,6 +11,8 @@ import io.vertx.core.spi.json.JsonCodec;
  * Tie into Vert.x's {@code JsonFactory} SPI in order to ensure that the user customized {@code ObjectMapper} is used
  */
 public class QuarkusJacksonFactory implements JsonFactory {
+
+    private static final AtomicInteger COUNTER = new AtomicInteger();
 
     @Override
     public JsonCodec codec() {
@@ -25,7 +29,15 @@ public class QuarkusJacksonFactory implements JsonFactory {
                 codec = new JacksonCodec();
             }
         }
+        COUNTER.incrementAndGet();
         return codec;
+    }
+
+    public static void reset() {
+        // if we blindly reset, we could get NCDFE because Jackson classes would not have been loaded
+        if (COUNTER.get() > 0) {
+            QuarkusJacksonJsonCodec.reset();
+        }
     }
 
 }

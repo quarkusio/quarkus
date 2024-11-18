c3440a4c95fc111e1ea17af261a1664511823c14
Michal Vavřík <mvavrik@redhat.com>
2024-11-18 12:00:00 +0100

    feat(oidc): Add OIDC Redis Token State Manager extension

---
+++ extensions/oidc-redis-token-state-manager/deployment/src/test/java/io/quarkus/oidc/redis/token/state/manager/deployment/ProtectedResource.java
@@ -0,0 +1,31 @@
+package io.quarkus.oidc.redis.token.state.manager.deployment;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import io.quarkus.oidc.IdToken;
+import io.quarkus.security.Authenticated;
+
+@Path("/protected")
+@Authenticated
+public class ProtectedResource {
+
+    @Inject
+    @IdToken
+    JsonWebToken idToken;
+
+    @GET
+    public String getName() {
+        return idToken.getName();
+    }
+
+    @GET
+    @Path("logout")
+    public void logout() {
+        throw new RuntimeException("Logout must be handled by CodeAuthenticationMechanism");
+    }
+
+}

9199d387d5f13594989932dd3574d3ad1a78b659
PhilKes <phil.kessler@gmx.de>
2024-11-19 18:39:54 +0100

    Support protoc Kotlin code generation

--- integration-tests/gradle/src/main/resources/grpc-multi-module-no-java/application/build.gradle.kts
+++ integration-tests/gradle/src/main/resources/grpc-multi-module-no-java/application/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-grpc")
+    compileOnly("com.google.protobuf:protobuf-kotlin")
 
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")

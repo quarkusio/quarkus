ebe9ac2d61bc65fdb44f527c5dfb0016c9e56eae
ogomezdi <ogomezdi@gmail.com>
2024-11-22 13:30:59 +0100

    Ajustes en formato por error en proceso de validación

--- independent-projects/qute/core/src/main/java/io/quarkus/qute/JsonEscaper.java
+++ independent-projects/qute/core/src/main/java/io/quarkus/qute/JsonEscaper.java
@@ -25,29 +25,29 @@ public class JsonEscaper implements ResultMapper {
 
     String escapeJson(String value) {
         if (value == null)
-          return "";
-    
+            return "";
+
         StringBuilder b = new StringBuilder();
         for (char c : value.toCharArray()) {
-          if (c == '\r')
-            b.append("\\r");
-          else if (c == '\n')
-            b.append("\\n");
-          else if (c == '\t')
-            b.append("\\t");
-          else if (c == '"')
-            b.append("\\\"");
-          else if (c == '\\')
-            b.append("\\\\");
-          else if (c == ' ')
-            b.append(" ");
-          else if (Character.isWhitespace(c)) {
-            b.append("\\u"+String.format("%4s", Integer.toHexString(c)).replace(' ','0'));
-          } else if (((int) c) < 32)
-            b.append("\\u"+String.format("%4s", Integer.toHexString(c)).replace(' ','0'));
-          else
-            b.append(c);
+            if (c == '\r')
+                b.append("\\r");
+            else if (c == '\n')
+                b.append("\\n");
+            else if (c == '\t')
+                b.append("\\t");
+            else if (c == '"')
+                b.append("\\\"");
+            else if (c == '\\')
+                b.append("\\\\");
+            else if (c == ' ')
+                b.append(" ");
+            else if (Character.isWhitespace(c)) {
+                b.append("\\u" + String.format("%4s", Integer.toHexString(c)).replace(' ', '0'));
+            } else if (((int) c) < 32)
+                b.append("\\u" + String.format("%4s", Integer.toHexString(c)).replace(' ', '0'));
+            else
+                b.append(c);
         }
         return b.toString();
-      }
+    }
 }

c364abd5adaf4054b543c8216bce5a1d8cabe0de
Stéphane Épardaud <stef@epardaud.fr>
2024-11-25 17:18:26 +0100

    WebAuthn: document the security constraints of WebAuthnUserProvider.store

--- extensions/security-webauthn/runtime/src/main/java/io/quarkus/security/webauthn/WebAuthnUserProvider.java
+++ extensions/security-webauthn/runtime/src/main/java/io/quarkus/security/webauthn/WebAuthnUserProvider.java
@@ -52,11 +52,23 @@ public interface WebAuthnUserProvider {
      * You don't have to implement this method if you handle registration manually via
      * {@link WebAuthnSecurity#register(WebAuthnRegisterResponse, io.vertx.ext.web.RoutingContext)}
      *
+     * Make sure that you never allow creating
+     * new credentials for a `userName` that already exists. Otherwise you risk allowing third-parties to impersonate existing
+     * users by letting them add their own credentials to existing accounts. If you want to allow existing users to register
+     * more than one WebAuthn credential, you must make sure that the user is currently logged
+     * in under the same <code>userName</code> to which you want to add new credentials. In every other case, make sure to
+     * return a failed
+     * {@link Uni} from this method.
+     *
      * The default behaviour is to not do anything.
      *
      * @param userName the userName's credentials
      * @param credentialRecord the new credentials to store
      * @return a uni completion object
+     * @throws Exception a failed {@link Uni} if the <code>credentialId</code> already exists, or the <code>userName</code>
+     *         already
+     *         has a credential and you disallow having more, or if trying to add credentials to other users than the current
+     *         user.
      */
     public default Uni<Void> store(WebAuthnCredentialRecord credentialRecord) {
         return Uni.createFrom().voidItem();

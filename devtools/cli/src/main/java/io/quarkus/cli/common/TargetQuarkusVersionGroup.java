139a74ddf6e7b9d44c3b67ade6d50044420abb41
Guillaume Smet <guillaume.smet@gmail.com>
2024-11-29 17:17:27 +0100

    Update version examples in CLI documentation
    These were way outdated and not even using our new version convention.

--- devtools/cli/src/main/java/io/quarkus/cli/common/TargetQuarkusVersionGroup.java
+++ devtools/cli/src/main/java/io/quarkus/cli/common/TargetQuarkusVersionGroup.java
@@ -5,12 +5,12 @@ import picocli.CommandLine;
 public class TargetQuarkusVersionGroup {
 
     @CommandLine.Option(paramLabel = "targetStream", names = { "-S",
-            "--stream" }, description = "A target stream, for example:%n  2.0")
+            "--stream" }, description = "A target stream, for example:%n  3.15")
     public String streamId;
 
     @CommandLine.Option(paramLabel = "targetPlatformVersion", names = { "-P",
             "--platform-version" }, description = "A specific target Quarkus platform version, for example:%n"
-                    + "  2.2.0.Final%n")
+                    + "  3.15.2%n")
     public String platformVersion;
 
     //@CommandLine.Option(names = { "-L",

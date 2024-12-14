d61589b07d24245ca19f892d9ffbdda74778252b
Antonio Musarra <antonio.musarra@gmail.com>
2024-12-14 15:22:00 +0100

    Extended the DevUI JavaInfo card with the new info.

--- extensions/info/runtime/src/main/java/io/quarkus/info/JavaInfo.java
+++ extensions/info/runtime/src/main/java/io/quarkus/info/JavaInfo.java
@@ -1,10 +1,31 @@
 package io.quarkus.info;
 
+/**
+ * This interface provides information about the Java runtime.
+ *
+ * @see io.quarkus.info.runtime.InfoRecorder
+ * @see io.quarkus.info.runtime.JavaInfoContributor
+ */
 public interface JavaInfo {
 
+    /**
+     * Return the Java version with which application compiled.
+     *
+     * @return string that represent the Java version
+     */
     String version();
 
+    /**
+     * Return the Java vendor with which application compiled.
+     *
+     * @return string that represent the Java vendor
+     */
     String vendor();
 
+    /**
+     * Return the Java vendor version with which application compiled.
+     *
+     * @return string that represent the Java vendor version
+     */
     String vendorVersion();
 }

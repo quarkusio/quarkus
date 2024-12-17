83e6e85786b3c0d8aad4007749587fd61f11cbd1
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-12-17 08:00:47 -0300

    Convert jaxb to use @ConfigMapping

--- extensions/jaxb/runtime/src/main/java/io/quarkus/jaxb/runtime/JaxbConfig.java
+++ extensions/jaxb/runtime/src/main/java/io/quarkus/jaxb/runtime/JaxbConfig.java
@@ -3,24 +3,25 @@ package io.quarkus.jaxb.runtime;
 import java.util.List;
 import java.util.Optional;
 
-import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
-@ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED, name = "jaxb")
-public class JaxbConfig {
+@ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
+@ConfigMapping(prefix = "quarkus.jaxb")
+public interface JaxbConfig {
 
     /**
      * If enabled, it will validate the default JAXB context at build time.
      */
-    @ConfigItem(defaultValue = "false")
-    public boolean validateJaxbContext;
+    @WithDefault("false")
+    boolean validateJaxbContext();
 
     /**
      * Exclude classes to automatically be bound to the default JAXB context.
      * Values with suffix {@code .*}, i.e. {@code org.acme.*}, are considered packages and exclude all classes that are members
      * of these packages
      */
-    @ConfigItem
-    public Optional<List<String>> excludeClasses;
+    Optional<List<String>> excludeClasses();
 }

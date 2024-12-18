63432b1b8901bb206b353a5d39857a88b662b878
Holly Cummins <hcummins@redhat.com>
2024-12-18 21:49:16 +0000

    Revert formatting churn

--- core/deployment/src/main/java/io/quarkus/runner/bootstrap/RunningQuarkusApplicationImpl.java
+++ core/deployment/src/main/java/io/quarkus/runner/bootstrap/RunningQuarkusApplicationImpl.java
@@ -42,39 +42,30 @@ public class RunningQuarkusApplicationImpl implements RunningQuarkusApplication 
     @Override
     public <T> Optional<T> getConfigValue(String key, Class<T> type) {
 
-        if (false && new Exception().getStackTrace().length > 100) {
-            // TODO expensive, awkward
-            System.out.println("HOLLY averting infinite loop " + key);
-            new Exception().printStackTrace();
-            return Optional.empty();
-        } else {
-            ClassLoader old = Thread.currentThread()
-                    .getContextClassLoader();
-            try {
-                // TODO this infinite loops, check that assumption
-                // we are assuming here that the the classloader has been initialised with some kind of different provider that does not infinite loop.
-                Thread.currentThread()
-                        .setContextClassLoader(classLoader);
-                if (classLoader == ConfigProvider.class.getClassLoader()) {
-                    return ConfigProvider.getConfig(classLoader)
-                            .getOptionalValue(key, type);
-                } else {
-                    //the config is in an isolated CL
-                    //we need to extract it via reflection
-                    //this is pretty yuck, but I don't really see a solution
-                    Class<?> configProviderClass = classLoader.loadClass(ConfigProvider.class.getName());
-                    Method getConfig = configProviderClass.getMethod("getConfig", ClassLoader.class);
-                    Object config = getConfig.invoke(null, classLoader);
-                    return (Optional<T>) getConfig.getReturnType()
-                            .getMethod("getOptionalValue", String.class, Class.class)
-                            .invoke(config, key, type);
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            } finally {
-                Thread.currentThread()
-                        .setContextClassLoader(old);
+        ClassLoader old = Thread.currentThread()
+                .getContextClassLoader();
+        try {
+            // we are assuming here that the the classloader has been initialised with some kind of different provider that does not infinite loop.
+            Thread.currentThread()
+                    .setContextClassLoader(classLoader);
+            if (classLoader == ConfigProvider.class.getClassLoader()) {
+                return ConfigProvider.getConfig(classLoader)
+                        .getOptionalValue(key, type);
+            } else {
+                //the config is in an isolated CL
+                //we need to extract it via reflection
+                //this is pretty yuck, but I don't really see a solution
+                Class<?> configProviderClass = classLoader.loadClass(ConfigProvider.class.getName());
+                Method getConfig = configProviderClass.getMethod("getConfig", ClassLoader.class);
+                Object config = getConfig.invoke(null, classLoader);
+                return (Optional<T>) getConfig.getReturnType()
+                        .getMethod("getOptionalValue", String.class, Class.class)
+                        .invoke(config, key, type);
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            Thread.currentThread().setContextClassLoader(old);
         }
     }
 
@@ -113,14 +104,10 @@ public class RunningQuarkusApplicationImpl implements RunningQuarkusApplication 
             }
 
             Class<?> cdi = classLoader.loadClass("jakarta.enterprise.inject.spi.CDI");
-            Object instance = cdi.getMethod("current")
-                    .invoke(null);
+            Object instance = cdi.getMethod("current").invoke(null);
             Method selectMethod = cdi.getMethod("select", Class.class, Annotation[].class);
             Object cdiInstance = selectMethod.invoke(instance, actualClass, qualifiers);
-            return selectMethod.getReturnType()
-                    .getMethod("get")
-                    .invoke(cdiInstance);
-
+            return selectMethod.getReturnType().getMethod("get").invoke(cdiInstance);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }

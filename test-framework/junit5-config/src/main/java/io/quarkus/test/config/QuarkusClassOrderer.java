1486883e369412f0667cc3f6a22abec8d53dd510
Holly Cummins <hcummins@redhat.com>
2024-12-19 10:43:11 +0000

    Sort out GRPC regression by having orderer

--- test-framework/junit5-config/src/main/java/io/quarkus/test/config/QuarkusClassOrderer.java
+++ test-framework/junit5-config/src/main/java/io/quarkus/test/config/QuarkusClassOrderer.java
@@ -1,9 +1,13 @@
 package io.quarkus.test.config;
 
+import java.util.NoSuchElementException;
+
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.ClassOrdererContext;
+import org.junit.platform.commons.util.ReflectionUtils;
 
+import io.quarkus.deployment.dev.testing.TestConfig;
 import io.smallrye.config.SmallRyeConfig;
 
 /**
@@ -11,25 +15,42 @@ import io.smallrye.config.SmallRyeConfig;
  * config.
  */
 public class QuarkusClassOrderer implements ClassOrderer {
-    private final ClassOrderer delegate;
+    // TODO reinstate final
+    private ClassOrderer delegate;
 
     public QuarkusClassOrderer() {
         System.out.println("HOLLY class orderer TCCL is " + Thread.currentThread().getContextClassLoader());
         System.out.println("HOLLY I AM " + this.getClass().getClassLoader());
-        ClassLoader original = Thread.currentThread().getContextClassLoader();
-        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+        //        ClassLoader original = Thread.currentThread().getContextClassLoader();
+        //  Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
         SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
+
         // TODO obviously a bad idea, diagnostic
-        delegate = EMPTY;
-        //        TestConfig testConfig = config.getConfigMapping(TestConfig.class);
-        //
-        //        delegate = testConfig.classOrderer()
-        //                .map(klass -> ReflectionUtils.tryToLoadClass(klass)
-        //                        .andThenTry(ReflectionUtils::newInstance)
-        //                        .andThenTry(instance -> (ClassOrderer) instance)
-        //                        .toOptional().orElse(EMPTY))
-        //                .orElse(EMPTY);
-        Thread.currentThread().setContextClassLoader(original);
+        //        delegate = EMPTY;
+
+        try {
+            TestConfig testConfig = config.getConfigMapping(TestConfig.class);
+
+            delegate = testConfig.classOrderer()
+                    .map(klass -> ReflectionUtils.tryToLoadClass(klass)
+                            .andThenTry(ReflectionUtils::newInstance)
+                            .andThenTry(instance -> (ClassOrderer) instance)
+                            .toOptional()
+                            .orElse(EMPTY))
+                    .orElse(EMPTY);
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            // TODO this is a huge hack, fix this
+            // FIXME On the gradle tests, and maybe some others, the config is not there
+            System.out.println("HOLLY BAD BAD could not get config mapping for orderer, hardcoding a default");
+            String klass = "io.quarkus.test.junit.util.QuarkusTestProfileAwareClassOrderer";
+            delegate = ReflectionUtils.tryToLoadClass(klass)
+                    .andThenTry(ReflectionUtils::newInstance)
+                    .andThenTry(instance -> (ClassOrderer) instance)
+                    .toOptional()
+                    .orElse(EMPTY);
+        }
+        //   Thread.currentThread().setContextClassLoader(original);
     }
 
     @Override

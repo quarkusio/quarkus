7a72f332266508d5f4af6fd9ca5209629a379d00
Yoann Rodière <yoann@hibernate.org>
2024-12-19 14:47:09 +0100

    Always use quarkus-specific options for bean container lifecycle
    Otherwise some direct uses of BeanContainer in ORM will just fail,
    due to Arc not implementing all features.
    
    Technically I think it doesn't change much right now, but will have an
    impact if we enable more "CDI extensions" (see last commit).

--- extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/cdi/QuarkusManagedBeanRegistry.java
+++ extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/cdi/QuarkusManagedBeanRegistry.java
@@ -79,26 +79,4 @@ public class QuarkusManagedBeanRegistry implements ManagedBeanRegistry {
         }
     }
 
-    private static final class QuarkusBeanContainerLifecycleOptions implements BeanContainer.LifecycleOptions {
-        private static final QuarkusBeanContainerLifecycleOptions INSTANCE = new QuarkusBeanContainerLifecycleOptions();
-
-        private QuarkusBeanContainerLifecycleOptions() {
-        }
-
-        @Override
-        public boolean useJpaCompliantCreation() {
-            // Arc doesn't support all the BeanManager methods required to implement JPA-compliant bean creation.
-            // Anyway, JPA-compliant bean creation means we completely disregard the scope of beans
-            // (e.g. @Dependent, @ApplicationScoped), which doesn't seem wise.
-            // So we're probably better off this way.
-            return false;
-        }
-
-        @Override
-        public boolean canUseCachedReferences() {
-            // Let Arc do the caching based on scopes
-            return false;
-        }
-    }
-
 }

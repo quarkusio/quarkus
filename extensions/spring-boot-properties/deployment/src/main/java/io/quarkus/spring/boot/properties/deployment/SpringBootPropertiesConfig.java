6fe3e21c36ce844e26d69867481593bdb5886cf2
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-01-06 20:41:40 -0300

    Convert spring-boot-properties to use @ConfigMapping

--- extensions/spring-boot-properties/deployment/src/main/java/io/quarkus/spring/boot/properties/deployment/SpringBootPropertiesConfig.java
+++ extensions/spring-boot-properties/deployment/src/main/java/io/quarkus/spring/boot/properties/deployment/SpringBootPropertiesConfig.java
@@ -1,17 +1,18 @@
 package io.quarkus.spring.boot.properties.deployment;
 
-import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
 @ConfigRoot(phase = ConfigPhase.BUILD_TIME)
-public class SpringBootPropertiesConfig {
+@ConfigMapping(prefix = "quarkus.spring-boot-properties")
+public interface SpringBootPropertiesConfig {
 
     /**
      * The naming strategy used for {@code org.springframework.boot.context.properties.ConfigurationProperties}.
      */
-    @ConfigItem(defaultValue = "kebab-case")
-    public ConfigMapping.NamingStrategy configurationPropertiesNamingStrategy;
+    @WithDefault("kebab-case")
+    ConfigMapping.NamingStrategy configurationPropertiesNamingStrategy();
 
 }

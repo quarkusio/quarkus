6a7eb2c86c66e21c4d0390eedf432c8d589db114
punkratz312 <punkratz312@users.noreply.github.com>
2025-01-14 21:39:51 +0100

    restore package-private visibility

--- core/builder/src/main/java/io/quarkus/builder/Consume.java
+++ core/builder/src/main/java/io/quarkus/builder/Consume.java
@@ -4,7 +4,7 @@ import static io.quarkus.builder.Constraint.ORDER_ONLY;
 import static io.quarkus.builder.Constraint.REAL;
 import static io.quarkus.builder.ConsumeFlag.OPTIONAL;
 
-public record Consume(BuildStepBuilder buildStepBuilder, ItemId itemId, Constraint constraint, ConsumeFlags flags) {
+record Consume(BuildStepBuilder buildStepBuilder, ItemId itemId, Constraint constraint, ConsumeFlags flags) {
 
     public Consume combine(final Constraint constraint, final ConsumeFlags flags) {
         return new Consume(

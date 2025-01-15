0c731d48de19007a48d28f426addc73b478fc054
Foivos Zakkak <fzakkak@redhat.com>
2025-01-15 15:30:55 +0200

    Deprecate `RuntimeReinitializedClassBuildItem`
    Starting with Mandrel/GraalVM for JDK 21
    `RuntimeReinitializedClassBuildItem` is functionally the same with
    `RuntimeInitializedClassBuildItem`.

--- core/deployment/src/main/java/io/quarkus/deployment/builditem/nativeimage/RuntimeReinitializedClassBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/builditem/nativeimage/RuntimeReinitializedClassBuildItem.java
@@ -5,7 +5,11 @@ import io.quarkus.builder.item.MultiBuildItem;
 /**
  * A class that will be reinitialized at runtime in native mode. This will result in the static
  * initializer running twice.
+ *
+ * @deprecated Starting with Mandrel/GraalVM 23.1 for JDK 21 this is functionally the same with
+ *             {@link RuntimeInitializedClassBuildItem}.
  */
+@Deprecated(since = "3.18")
 public final class RuntimeReinitializedClassBuildItem extends MultiBuildItem {
 
     private final String className;

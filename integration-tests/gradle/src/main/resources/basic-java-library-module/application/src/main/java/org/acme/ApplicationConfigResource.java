e9319b9d9457137aedc74cad2a9b0e5cfce691d1
Vincent Potucek <vincent.potucek@sap.com>
2025-01-16 10:07:39 +0100

    Fix import format gaps

--- integration-tests/gradle/src/main/resources/basic-java-library-module/application/src/main/java/org/acme/ApplicationConfigResource.java
+++ integration-tests/gradle/src/main/resources/basic-java-library-module/application/src/main/java/org/acme/ApplicationConfigResource.java
@@ -1,12 +1,11 @@
 package org.acme;
 
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/app-config")
 public class ApplicationConfigResource {

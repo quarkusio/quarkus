8faa0428f3ff44db30321b521bad1c9ff3101750
Vincent Potucek <vincent.potucek@sap.com>
2025-01-16 10:07:39 +0100

    Fix import format gaps

--- integration-tests/maven/src/test/resources-filtered/projects/project-with-extension/runner/src/main/java/org/acme/HelloResource.java
+++ integration-tests/maven/src/test/resources-filtered/projects/project-with-extension/runner/src/main/java/org/acme/HelloResource.java
@@ -1,13 +1,12 @@
 package org.acme;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class HelloResource {

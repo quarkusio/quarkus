7615750541c405523f7de9de89f9e703124da10c
Guillaume Smet <guillaume.smet@gmail.com>
2025-01-16 14:12:52 +0100

    Fix wildcard dropping in DropTargetsSampler
    While /q/dev-ui/* would exclude:
    - /q/dev-ui/
    - /q/dev-ui/whatever
    it wouldn't exclude /q/dev-ui/whatever/whenever/, which was problematic.
    
    See additional tests.
    
    Note that I think this should be improved with a proper matcher that
    handles all our matching rules efficiently but for now, it will have to
    do.

--- extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/DropTargetsSampler.java
+++ extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/tracing/DropTargetsSampler.java
@@ -3,8 +3,10 @@ package io.quarkus.opentelemetry.runtime.tracing;
 import static io.opentelemetry.semconv.UrlAttributes.URL_PATH;
 import static io.opentelemetry.semconv.UrlAttributes.URL_QUERY;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.SpanKind;
@@ -15,12 +17,18 @@ import io.opentelemetry.sdk.trace.samplers.SamplingResult;
 
 public class DropTargetsSampler implements Sampler {
     private final Sampler sampler;
-    private final Set<String> dropTargets;
+    private final Set<String> dropTargetsExact;
+    private final Set<String> dropTargetsWildcard;
 
     public DropTargetsSampler(final Sampler sampler,
             final Set<String> dropTargets) {
         this.sampler = sampler;
-        this.dropTargets = dropTargets;
+        this.dropTargetsExact = dropTargets.stream().filter(s -> !s.endsWith("*"))
+                .collect(Collectors.toCollection(HashSet::new));
+        this.dropTargetsWildcard = dropTargets.stream()
+                .filter(s -> s.endsWith("*"))
+                .map(s -> s.substring(0, s.length() - 1))
+                .collect(Collectors.toCollection(HashSet::new));
     }
 
     @Override
@@ -49,26 +57,24 @@ public class DropTargetsSampler implements Sampler {
         if ((target == null) || target.isEmpty()) {
             return false;
         }
-        if (safeContains(target)) { // check exact match
+        if (containsExactly(target)) { // check exact match
             return true;
         }
         if (target.charAt(target.length() - 1) == '/') { // check if the path without the ending slash matched
-            if (safeContains(target.substring(0, target.length() - 1))) {
+            if (containsExactly(target.substring(0, target.length() - 1))) {
                 return true;
             }
         }
-        int lastSlashIndex = target.lastIndexOf('/');
-        if (lastSlashIndex != -1) {
-            if (safeContains(target.substring(0, lastSlashIndex) + "*")
-                    || safeContains(target.substring(0, lastSlashIndex) + "/*")) { // check if a wildcard matches
+        for (String dropTargetsWildcard : dropTargetsWildcard) {
+            if (target.startsWith(dropTargetsWildcard)) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean safeContains(String target) {
-        return dropTargets.contains(target);
+    private boolean containsExactly(String target) {
+        return dropTargetsExact.contains(target);
     }
 
     @Override

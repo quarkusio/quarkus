d5dc1254cf5a34c3a1c3b18a2b184f8ccb3e5753
brunobat <brunobat@gmail.com>
2025-01-16 16:10:26 +0000

    Fix flaky otel quickstart test

--- integration-tests/opentelemetry-quickstart/src/test/java/io/quarkus/it/opentelemetry/OpenTelemetryDisabledTest.java
+++ integration-tests/opentelemetry-quickstart/src/test/java/io/quarkus/it/opentelemetry/OpenTelemetryDisabledTest.java
@@ -2,12 +2,16 @@ package io.quarkus.it.opentelemetry;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static java.net.HttpURLConnection.HTTP_OK;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.CoreMatchers.is;
 
+import java.util.List;
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -18,6 +22,19 @@ import io.quarkus.test.junit.TestProfile;
 @TestProfile(OpenTelemetryDisabledTest.MyProfile.class)
 public class OpenTelemetryDisabledTest extends BaseTest {
 
+    @BeforeEach
+    void reset() {
+        await().atMost(5, SECONDS).until(() -> {
+            List<Map<String, Object>> spans = getSpans();
+            if (spans.size() == 0) {
+                return true;
+            } else {
+                given().get("/reset").then().statusCode(HTTP_OK);
+                return false;
+            }
+        });
+    }
+
     @Test
     void buildTimeDisabled() {
         given()
@@ -26,7 +43,7 @@ public class OpenTelemetryDisabledTest extends BaseTest {
                 .statusCode(200)
                 .body(is("Hello from Quarkus REST"));
         // Service will start nevertheless.
-        await().atMost(200, MILLISECONDS).until(() -> getSpans().size() == 0);
+        await().atMost(300, MILLISECONDS).until(() -> getSpans().size() == 0);
     }
 
     public static class MyProfile implements QuarkusTestProfile {

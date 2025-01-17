84d57f6e44b7eb714e20a424a39fe8c9b5e5c9f8
Ladislav Thon <lthon@redhat.com>
2025-01-17 17:35:04 +0100

    Redis Client: fix NPE when constructing XPendingSummary
    Certain fields that were deemed to be mandatory can in fact be `null`,
    as demonstrated in newly added tests.
    
    Also, this commit fixes translating `XTrimArgs` to the actual argument
    array in case `MAXLEN` is set to `0`. That is, in fact, a valid value,
    as demonstrated by the newly added tests and also by inspecting Redis
    source code [1].
    
    [1] https://github.com/redis/redis/blob/7.2.4/src/t_stream.c#L941

--- extensions/redis-client/runtime/src/test/java/io/quarkus/redis/datasource/TransactionalStreamCommandsTest.java
+++ extensions/redis-client/runtime/src/test/java/io/quarkus/redis/datasource/TransactionalStreamCommandsTest.java
@@ -18,6 +18,7 @@ import io.quarkus.redis.datasource.stream.TransactionalStreamCommands;
 import io.quarkus.redis.datasource.stream.XAddArgs;
 import io.quarkus.redis.datasource.stream.XPendingArgs;
 import io.quarkus.redis.datasource.stream.XPendingSummary;
+import io.quarkus.redis.datasource.stream.XTrimArgs;
 import io.quarkus.redis.datasource.transactions.TransactionResult;
 import io.quarkus.redis.runtime.datasource.BlockingRedisDataSourceImpl;
 import io.quarkus.redis.runtime.datasource.ReactiveRedisDataSourceImpl;
@@ -49,30 +50,44 @@ public class TransactionalStreamCommandsTest extends DatasourceTestBase {
             assertThat(stream.getDataSource()).isEqualTo(tx);
 
             stream.xadd(key, payload);
+            stream.xtrim(key, new XTrimArgs().maxlen(0));
+
+            stream.xgroupCreate(key, "g1", "0");
+
+            stream.xpending(key, "g1");
+
+            stream.xadd(key, payload);
             stream.xadd(key, new XAddArgs().nomkstream(), payload);
 
             stream.xread(key, "0"); // 3 -> 2 messages
-            stream.xgroupCreate(key, "g1", "0");
             stream.xreadgroup("g1", "c1", key, ">");
             stream.xpending(key, "g1");
             stream.xpending(key, "g1", StreamRange.of("-", "+"), 10, new XPendingArgs().consumer("c1"));
 
         });
-        assertThat(result.size()).isEqualTo(7);
+        assertThat(result.size()).isEqualTo(10);
         assertThat(result.discarded()).isFalse();
+
         assertThat((String) result.get(0)).isNotBlank();
-        assertThat((String) result.get(1)).isNotBlank();
+        assertThat((Long) result.get(1)).isEqualTo(1);
 
-        String id1 = result.get(0);
-        String id2 = result.get(1);
+        assertThat(result.<XPendingSummary> get(3).getPendingCount()).isEqualTo(0);
+        assertThat(result.<XPendingSummary> get(3).getHighestId()).isNull();
+        assertThat(result.<XPendingSummary> get(3).getLowestId()).isNull();
+        assertThat(result.<XPendingSummary> get(3).getConsumers()).isEmpty();
 
-        assertThat((List<StreamMessage<String, String, String>>) result.get(2)).hasSize(2);
-        assertThat((List<StreamMessage<String, String, String>>) result.get(4)).hasSize(2);
+        String id1 = result.get(4);
+        String id2 = result.get(5);
 
-        assertThat(((XPendingSummary) result.get(5)).getPendingCount()).isEqualTo(2);
-        List<PendingMessage> list = result.get(6);
+        assertThat((List<StreamMessage<String, String, String>>) result.get(6)).hasSize(2);
+        assertThat((List<StreamMessage<String, String, String>>) result.get(7)).hasSize(2);
 
-        assertThat(((List<PendingMessage>) result.get(6))).hasSize(2);
+        assertThat((result.<XPendingSummary> get(8)).getPendingCount()).isEqualTo(2);
+        assertThat((result.<XPendingSummary> get(8)).getHighestId()).isNotNull();
+        assertThat((result.<XPendingSummary> get(8)).getLowestId()).isNotNull();
+        assertThat((result.<XPendingSummary> get(8)).getConsumers()).hasSize(1);
+        List<PendingMessage> list = result.get(9);
+        assertThat(list).hasSize(2);
         List<String> ids = list.stream().map(PendingMessage::getMessageId).collect(Collectors.toList());
         assertThat(ids).containsExactly(id1, id2);
     }
@@ -84,29 +99,41 @@ public class TransactionalStreamCommandsTest extends DatasourceTestBase {
             assertThat(stream.getDataSource()).isEqualTo(tx);
 
             return stream.xadd(key, payload)
-                    .chain((x) -> stream.xadd(key, new XAddArgs().nomkstream(), payload))
-                    .chain(x -> stream.xread(key, "0"))
+                    .chain(x -> stream.xtrim(key, new XTrimArgs().maxlen(0)))
                     .chain(x -> stream.xgroupCreate(key, "g1", "0"))
+                    .chain(x -> stream.xpending(key, "g1"))
+                    .chain(x -> stream.xadd(key, payload))
+                    .chain(x -> stream.xadd(key, new XAddArgs().nomkstream(), payload))
+                    .chain(x -> stream.xread(key, "0"))
                     .chain(x -> stream.xreadgroup("g1", "c1", key, ">"))
                     .chain(x -> stream.xpending(key, "g1"))
                     .chain(x -> stream.xpending(key, "g1", StreamRange.of("-", "+"), 10));
 
         }).await().indefinitely();
-        assertThat(result.size()).isEqualTo(7);
+        assertThat(result.size()).isEqualTo(10);
         assertThat(result.discarded()).isFalse();
+
         assertThat((String) result.get(0)).isNotBlank();
-        assertThat((String) result.get(1)).isNotBlank();
+        assertThat((Long) result.get(1)).isEqualTo(1);
 
-        String id1 = result.get(0);
-        String id2 = result.get(1);
+        assertThat(result.<XPendingSummary> get(3).getPendingCount()).isEqualTo(0);
+        assertThat(result.<XPendingSummary> get(3).getHighestId()).isNull();
+        assertThat(result.<XPendingSummary> get(3).getLowestId()).isNull();
+        assertThat(result.<XPendingSummary> get(3).getConsumers()).isEmpty();
 
-        assertThat((List<StreamMessage<String, String, String>>) result.get(2)).hasSize(2);
-        assertThat((List<StreamMessage<String, String, String>>) result.get(4)).hasSize(2);
+        String id1 = result.get(4);
+        String id2 = result.get(5);
 
-        assertThat(((XPendingSummary) result.get(5)).getPendingCount()).isEqualTo(2);
-        List<PendingMessage> list = result.get(6);
+        assertThat((List<StreamMessage<String, String, String>>) result.get(6)).hasSize(2);
+        assertThat((List<StreamMessage<String, String, String>>) result.get(7)).hasSize(2);
 
-        assertThat(((List<PendingMessage>) result.get(6))).hasSize(2);
+        assertThat((result.<XPendingSummary> get(8)).getPendingCount()).isEqualTo(2);
+        assertThat((result.<XPendingSummary> get(8)).getHighestId()).isNotNull();
+        assertThat((result.<XPendingSummary> get(8)).getLowestId()).isNotNull();
+        assertThat((result.<XPendingSummary> get(8)).getConsumers()).hasSize(1);
+
+        List<PendingMessage> list = result.get(9);
+        assertThat(list).hasSize(2);
         List<String> ids = list.stream().map(PendingMessage::getMessageId).collect(Collectors.toList());
         assertThat(ids).containsExactly(id1, id2);
     }

b329543309b5c46f175d29dafa7423bb43dd5625
Guillaume Smet <guillaume.smet@gmail.com>
2025-01-21 17:04:28 +0100

    Use @ConfigMapping in SmallRye Reactive Messaging extensions

--- extensions/smallrye-reactive-messaging-amqp/deployment/src/main/java/io/quarkus/smallrye/reactivemessaging/amqp/deployment/AmqpDevServicesProcessor.java
+++ extensions/smallrye-reactive-messaging-amqp/deployment/src/main/java/io/quarkus/smallrye/reactivemessaging/amqp/deployment/AmqpDevServicesProcessor.java
@@ -236,7 +236,7 @@ public class AmqpDevServicesProcessor {
     }
 
     private AmqpDevServiceCfg getConfiguration(AmqpBuildTimeConfig cfg) {
-        AmqpDevServicesBuildTimeConfig devServicesConfig = cfg.devservices;
+        AmqpDevServicesBuildTimeConfig devServicesConfig = cfg.devservices();
         return new AmqpDevServiceCfg(devServicesConfig);
     }
 
@@ -250,13 +250,13 @@ public class AmqpDevServicesProcessor {
         private final Map<String, String> containerEnv;
 
         public AmqpDevServiceCfg(AmqpDevServicesBuildTimeConfig devServicesConfig) {
-            this.devServicesEnabled = devServicesConfig.enabled.orElse(true);
-            this.imageName = devServicesConfig.imageName;
-            this.fixedExposedPort = devServicesConfig.port.orElse(0);
-            this.extra = devServicesConfig.extraArgs;
-            this.shared = devServicesConfig.shared;
-            this.serviceName = devServicesConfig.serviceName;
-            this.containerEnv = devServicesConfig.containerEnv;
+            this.devServicesEnabled = devServicesConfig.enabled().orElse(true);
+            this.imageName = devServicesConfig.imageName();
+            this.fixedExposedPort = devServicesConfig.port().orElse(0);
+            this.extra = devServicesConfig.extraArgs();
+            this.shared = devServicesConfig.shared();
+            this.serviceName = devServicesConfig.serviceName();
+            this.containerEnv = devServicesConfig.containerEnv();
         }
 
         @Override
--- extensions/smallrye-reactive-messaging-rabbitmq/deployment/src/main/java/io/quarkus/smallrye/reactivemessaging/rabbitmq/deployment/RabbitMQDevServicesBuildTimeConfig.java
+++ extensions/smallrye-reactive-messaging-rabbitmq/deployment/src/main/java/io/quarkus/smallrye/reactivemessaging/rabbitmq/deployment/RabbitMQDevServicesBuildTimeConfig.java
@@ -7,114 +7,109 @@ import java.util.OptionalInt;
 
 import io.quarkus.runtime.annotations.ConfigDocMapKey;
 import io.quarkus.runtime.annotations.ConfigGroup;
-import io.quarkus.runtime.annotations.ConfigItem;
+import io.smallrye.config.WithDefault;
 
 @ConfigGroup
-public class RabbitMQDevServicesBuildTimeConfig {
+public interface RabbitMQDevServicesBuildTimeConfig {
 
     @ConfigGroup
-    public static class Exchange {
+    public interface Exchange {
 
         /**
          * Type of exchange: direct, topic, headers, fanout, etc.
          */
-        @ConfigItem(defaultValue = "direct")
-        public String type;
+        @WithDefault("direct")
+        String type();
 
         /**
          * Should the exchange be deleted when all queues are finished using it?
          */
-        @ConfigItem(defaultValue = "false")
-        public Boolean autoDelete;
+        @WithDefault("false")
+        Boolean autoDelete();
 
         /**
          * Should the exchange remain after restarts?
          */
-        @ConfigItem(defaultValue = "false")
-        public Boolean durable;
+        @WithDefault("false")
+        Boolean durable();
 
         /**
          * What virtual host should the exchange be associated with?
          */
-        @ConfigItem(defaultValue = "/")
-        public String vhost;
+        @WithDefault("/")
+        String vhost();
 
         /**
          * Extra arguments for the exchange definition.
          */
-        @ConfigItem
         @ConfigDocMapKey("argument-name")
-        public Map<String, String> arguments;
+        Map<String, String> arguments();
     }
 
     @ConfigGroup
-    public static class Queue {
+    public interface Queue {
 
         /**
          * Should the queue be deleted when all consumers are finished using it?
          */
-        @ConfigItem(defaultValue = "false")
-        public Boolean autoDelete;
+        @WithDefault("false")
+        Boolean autoDelete();
 
         /**
          * Should the queue remain after restarts?
          */
-        @ConfigItem(defaultValue = "false")
-        public Boolean durable;
+        @WithDefault("false")
+        Boolean durable();
 
         /**
          * What virtual host should the queue be associated with?
          */
-        @ConfigItem(defaultValue = "/")
-        public String vhost;
+        @WithDefault("/")
+        String vhost();
 
         /**
          * Extra arguments for the queue definition.
          */
-        @ConfigItem
         @ConfigDocMapKey("argument-name")
-        public Map<String, String> arguments;
+        Map<String, String> arguments();
     }
 
     @ConfigGroup
-    public static class Binding {
+    public interface Binding {
 
         /**
          * Source exchange to bind to. Defaults to name of binding instance.
          */
-        @ConfigItem
-        public Optional<String> source;
+        Optional<String> source();
 
         /**
          * Routing key specification for the source exchange.
          */
-        @ConfigItem(defaultValue = "#")
-        public String routingKey;
+        @WithDefault("#")
+        String routingKey();
 
         /**
          * Destination exchange or queue to bind to. Defaults to name of binding instance.
          */
-        @ConfigItem
-        public Optional<String> destination;
+        Optional<String> destination();
 
         /**
          * Destination type for binding: queue, exchange, etc.
          */
-        @ConfigItem(defaultValue = "queue")
-        public String destinationType;
+        @WithDefault("queue")
+        String destinationType();
 
         /**
          * What virtual host should the binding be associated with?
          */
-        @ConfigItem(defaultValue = "/")
-        public String vhost;
+        @WithDefault("/")
+        String vhost();
 
         /**
          * Extra arguments for the binding definition.
          */
-        @ConfigItem
         @ConfigDocMapKey("argument-name")
-        public Map<String, String> arguments;
+        Map<String, String> arguments();
     }
 
     /**
@@ -124,32 +119,29 @@ public class RabbitMQDevServicesBuildTimeConfig {
      * with
      * {@code host} or {@code port}.
      */
-    @ConfigItem
-    public Optional<Boolean> enabled = Optional.empty();
+    Optional<Boolean> enabled();
 
     /**
      * Optional fixed port the dev service will listen to.
      * <p>
      * If not defined, the port will be chosen randomly.
      */
-    @ConfigItem
-    public OptionalInt port;
+    OptionalInt port();
 
     /**
      * Optional fixed port for the RabbitMQ management plugin.
      * <p>
      * If not defined, the port will be chosen randomly.
      */
-    @ConfigItem
-    public OptionalInt httpPort;
+    OptionalInt httpPort();
 
     /**
      * The image to use.
      * Note that only official RabbitMQ images are supported.
      * Specifically, the image repository must end with {@code rabbitmq}.
      */
-    @ConfigItem(defaultValue = "rabbitmq:3.12-management")
-    public String imageName;
+    @WithDefault("rabbitmq:3.12-management")
+    String imageName();
 
     /**
      * Indicates if the RabbitMQ broker managed by Quarkus Dev Services is shared.
@@ -162,8 +154,8 @@ public class RabbitMQDevServicesBuildTimeConfig {
      * <p>
      * Container sharing is only used in dev mode.
      */
-    @ConfigItem(defaultValue = "true")
-    public boolean shared;
+    @WithDefault("true")
+    boolean shared();
 
     /**
      * The value of the {@code quarkus-dev-service-rabbitmq} label attached to the started container.
@@ -175,40 +167,35 @@ public class RabbitMQDevServicesBuildTimeConfig {
      * <p>
      * This property is used when you need multiple shared RabbitMQ brokers.
      */
-    @ConfigItem(defaultValue = "rabbitmq")
-    public String serviceName;
+    @WithDefault("rabbitmq")
+    String serviceName();
 
     /**
      * Exchanges that should be predefined after starting the RabbitMQ broker.
      */
-    @ConfigItem
     @ConfigDocMapKey("exchange-name")
-    public Map<String, Exchange> exchanges;
+    Map<String, Exchange> exchanges();
 
     /**
      * Queues that should be predefined after starting the RabbitMQ broker.
      */
-    @ConfigItem
     @ConfigDocMapKey("queue-name")
-    public Map<String, Queue> queues;
+    Map<String, Queue> queues();
 
     /**
      * Bindings that should be predefined after starting the RabbitMQ broker.
      */
-    @ConfigItem
     @ConfigDocMapKey("binding-name")
-    public Map<String, Binding> bindings;
+    Map<String, Binding> bindings();
 
     /**
      * Virtual hosts that should be predefined after starting the RabbitMQ broker.
      */
-    @ConfigItem
-    public Optional<List<String>> vhosts;
+    Optional<List<String>> vhosts();
 
     /**
      * Environment variables that are passed to the container.
      */
-    @ConfigItem
     @ConfigDocMapKey("environment-variable-name")
-    public Map<String, String> containerEnv;
+    Map<String, String> containerEnv();
 }

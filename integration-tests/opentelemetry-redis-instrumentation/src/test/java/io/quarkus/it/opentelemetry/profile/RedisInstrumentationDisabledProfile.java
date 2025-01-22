0b6bf9bc3bdef459fe4ee1f4a128688e5af775fc
Holly Cummins <hcummins@redhat.com>
2025-01-22 16:05:41 +0000

    Workaround: do not have different profiles sharing a port

--- integration-tests/opentelemetry-redis-instrumentation/src/test/java/io/quarkus/it/opentelemetry/profile/RedisInstrumentationDisabledProfile.java
+++ integration-tests/opentelemetry-redis-instrumentation/src/test/java/io/quarkus/it/opentelemetry/profile/RedisInstrumentationDisabledProfile.java
@@ -1,6 +1,6 @@
 package io.quarkus.it.opentelemetry.profile;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import io.quarkus.test.junit.QuarkusTestProfile;
@@ -9,7 +9,11 @@ public class RedisInstrumentationDisabledProfile implements QuarkusTestProfile {
 
     @Override
     public Map<String, String> getConfigOverrides() {
-        return Collections.singletonMap("quarkus.otel.instrument.vertx-redis-client", "false");
+        Map overrides = new HashMap();
+        overrides.put("quarkus.otel.instrument.vertx-redis-client", "false");
+        overrides.put("quarkus.redis.devservices.port", "4001"); // Workaround for #45785; the dev services for distinct dev services cannot share a port
+        return overrides;
+
     }
 
     @Override

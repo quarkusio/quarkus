dbc599b9c5c7f4b9561817cedb4c501a0e7eb337
Clement Escoffier <clement.escoffier@gmail.com>
2025-01-27 14:08:34 +0100

    Replace the id label with a name label for Netty allocator
    The Netty allocator metrics exposed an `id` label using the allocator hashcode. However, this does not make a lot of sense when you need to aggregate metrics from multiple metrics.
    Thus, this commit replaces the `id` with a `name` label with stable names for the different allocators.
    
    Unfortunately, the initial code was in micrometer. This commit forks the 2 classes required to replace `id` with `name` and adds stable names to the different allocators.
    
    Fix https://github.com/quarkusio/quarkus/issues/45847
    
    (cherry picked from commit 2ee2894451881e54e4382f76f031a00137a8fddb)

--- extensions/micrometer/runtime/src/main/java/io/quarkus/micrometer/runtime/binder/netty/NettyMetricsProvider.java
+++ extensions/micrometer/runtime/src/main/java/io/quarkus/micrometer/runtime/binder/netty/NettyMetricsProvider.java
@@ -4,23 +4,26 @@ import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Singleton;
 
 import io.micrometer.core.instrument.binder.MeterBinder;
-import io.micrometer.core.instrument.binder.netty4.NettyAllocatorMetrics;
 import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.buffer.UnpooledByteBufAllocator;
 
 @Singleton
 public class NettyMetricsProvider {
 
+    public static final String NETTY_DEFAULT_POOLED_ALLOCATOR_NAME = "pooled";
+
+    public static final String NETTY_DEFAULT_UNPOOLED_ALLOCATOR_NAME = "unpooled";
+
     @Produces
     @Singleton
     public MeterBinder pooledByteBufAllocatorMetrics() {
-        return new NettyAllocatorMetrics(PooledByteBufAllocator.DEFAULT);
+        return new NettyAllocatorMetrics(NETTY_DEFAULT_POOLED_ALLOCATOR_NAME, PooledByteBufAllocator.DEFAULT);
     }
 
     @Produces
     @Singleton
     public MeterBinder unpooledByteBufAllocatorMetrics() {
-        return new NettyAllocatorMetrics(UnpooledByteBufAllocator.DEFAULT);
+        return new NettyAllocatorMetrics(NETTY_DEFAULT_UNPOOLED_ALLOCATOR_NAME, UnpooledByteBufAllocator.DEFAULT);
     }
 
 }

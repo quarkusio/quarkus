c9b5e617d1121a8586741a144cba801e59aa08ff
Andreas Eberle <andreas.eberle@arconsis.com>
2025-01-28 15:41:32 +0100

    Update Kotlin to version 2.1.10

--- integration-tests/gradle/src/main/resources/conditional-dependencies-kotlin/build.gradle.kts
+++ integration-tests/gradle/src/main/resources/conditional-dependencies-kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.allopen") version "1.9.23"
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.allopen") version "2.1.10"
     id("io.quarkus")
 }
 
--- integration-tests/gradle/src/main/resources/grpc-multi-module-no-java/build.gradle.kts
+++ integration-tests/gradle/src/main/resources/grpc-multi-module-no-java/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.allopen") version "2.0.21"
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.allopen") version "2.1.10"
 
     id("io.quarkus") apply false
 }

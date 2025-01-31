e5d3f1e65d38a06519c6935c5adaa6d670ae077d
Roberto Cortez <radcortez@yahoo.com>
2025-01-31 21:18:29 +0000

    Runtime values from mapping without expansion
    (cherry picked from commit 0e258ac2ebb617824d6dc0a2fcd412dd34a59a0f)

--- integration-tests/test-extension/extension/runtime/src/main/java/io/quarkus/extest/runtime/config/RunTimeConfigBuilder.java
+++ integration-tests/test-extension/extension/runtime/src/main/java/io/quarkus/extest/runtime/config/RunTimeConfigBuilder.java
@@ -1,6 +1,7 @@
 package io.quarkus.extest.runtime.config;
 
 import io.quarkus.runtime.configuration.ConfigBuilder;
+import io.smallrye.config.SecretKeysHandler;
 import io.smallrye.config.SmallRyeConfigBuilder;
 
 public class RunTimeConfigBuilder implements ConfigBuilder {
@@ -8,6 +9,17 @@ public class RunTimeConfigBuilder implements ConfigBuilder {
     public SmallRyeConfigBuilder configBuilder(final SmallRyeConfigBuilder builder) {
         builder.withDefaultValue("skip.build.sources", "true");
         builder.withDefaultValue("additional.builder.property", "1234");
+        builder.withSecretKeysHandlers(new SecretKeysHandler() {
+            @Override
+            public String decode(final String secret) {
+                return "secret";
+            }
+
+            @Override
+            public String getName() {
+                return "handler";
+            }
+        });
         return builder;
     }
 }

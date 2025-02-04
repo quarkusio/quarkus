150f71f043b60f94ed0da7f97d25096ee372ed28
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-02-04 11:22:54 -0300

    Convert schema-registry to use @ConfigMapping

--- extensions/schema-registry/devservice/deployment/src/main/java/io/quarkus/apicurio/registry/devservice/ApicurioRegistryBuildTimeConfig.java
+++ extensions/schema-registry/devservice/deployment/src/main/java/io/quarkus/apicurio/registry/devservice/ApicurioRegistryBuildTimeConfig.java
@@ -2,28 +2,30 @@ package io.quarkus.apicurio.registry.devservice;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalInt;
 
 import io.quarkus.runtime.annotations.ConfigDocMapKey;
 import io.quarkus.runtime.annotations.ConfigDocSection;
 import io.quarkus.runtime.annotations.ConfigGroup;
-import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
-@ConfigRoot(name = "apicurio-registry", phase = ConfigPhase.BUILD_TIME)
-public class ApicurioRegistryBuildTimeConfig {
+@ConfigMapping(prefix = "quarkus.apicurio-registry")
+@ConfigRoot(phase = ConfigPhase.BUILD_TIME)
+public interface ApicurioRegistryBuildTimeConfig {
 
     /**
      * Dev Services
      * <p>
      * Dev Services allows Quarkus to automatically start Apicurio Registry in dev and test mode.
      */
-    @ConfigItem
     @ConfigDocSection(generated = true)
-    public ApicurioRegistryDevServicesBuildTimeConfig devservices;
+    ApicurioRegistryDevServicesBuildTimeConfig devservices();
 
     @ConfigGroup
-    public static class ApicurioRegistryDevServicesBuildTimeConfig {
+    interface ApicurioRegistryDevServicesBuildTimeConfig {
 
         /**
          * If Dev Services for Apicurio Registry has been explicitly enabled or disabled. Dev Services are generally enabled
@@ -31,24 +33,22 @@ public class ApicurioRegistryBuildTimeConfig {
          * unless {@code mp.messaging.connector.smallrye-kafka.apicurio.registry.url} or
          * {@code mp.messaging.connector.smallrye-kafka.schema.registry.url} is set.
          */
-        @ConfigItem
-        public Optional<Boolean> enabled = Optional.empty();
+        Optional<Boolean> enabled();
 
         /**
          * Optional fixed port the dev service will listen to.
          * <p>
          * If not defined, the port will be chosen randomly.
          */
-        @ConfigItem
-        public Optional<Integer> port;
+        OptionalInt port();
 
         /**
          * The Apicurio Registry image to use.
          * Note that only Apicurio Registry 2.x images are supported.
          * Specifically, the image repository must end with {@code apicurio/apicurio-registry-mem}.
          */
-        @ConfigItem(defaultValue = "quay.io/apicurio/apicurio-registry-mem:2.4.2.Final")
-        public String imageName;
+        @WithDefault("quay.io/apicurio/apicurio-registry-mem:2.4.2.Final")
+        String imageName();
 
         /**
          * Indicates if the Apicurio Registry instance managed by Quarkus Dev Services is shared.
@@ -61,8 +61,8 @@ public class ApicurioRegistryBuildTimeConfig {
          * <p>
          * Container sharing is only used in dev mode.
          */
-        @ConfigItem(defaultValue = "true")
-        public boolean shared;
+        @WithDefault("true")
+        boolean shared();
 
         /**
          * The value of the {@code quarkus-dev-service-apicurio-registry} label attached to the started container.
@@ -74,14 +74,13 @@ public class ApicurioRegistryBuildTimeConfig {
          * <p>
          * This property is used when you need multiple shared Apicurio Registry instances.
          */
-        @ConfigItem(defaultValue = "apicurio-registry")
-        public String serviceName;
+        @WithDefault("apicurio-registry")
+        String serviceName();
 
         /**
          * Environment variables that are passed to the container.
          */
-        @ConfigItem
         @ConfigDocMapKey("environment-variable-name")
-        public Map<String, String> containerEnv;
+        Map<String, String> containerEnv();
     }
 }

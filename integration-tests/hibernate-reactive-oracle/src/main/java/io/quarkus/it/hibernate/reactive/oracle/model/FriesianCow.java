e0c832e02635d7ee0a9fde41f063833898201590
i10320 <ivanpetev.petkov@energo-pro.bg>
2025-02-05 17:10:28 +0200

    Add IT for Hibernate reactive with oracle reactive client

---
+++ integration-tests/hibernate-reactive-oracle/src/main/java/io/quarkus/it/hibernate/reactive/oracle/model/FriesianCow.java
@@ -0,0 +1,18 @@
+package io.quarkus.it.hibernate.reactive.oracle.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Cow")
+public class FriesianCow {
+
+    @Id
+    @GeneratedValue
+    public Long id;
+
+    public String name;
+
+}
---
+++ integration-tests/hibernate-reactive-oracle/src/main/java/io/quarkus/it/hibernate/reactive/oracle/model/lazy/Book.java
@@ -0,0 +1,87 @@
+package io.quarkus.it.hibernate.reactive.oracle.model.lazy;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+import java.util.Objects;
+
+import jakarta.json.bind.annotation.JsonbTransient;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Book {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String isbn;
+
+    private String title;
+
+    @JsonbTransient
+    @ManyToOne(fetch = LAZY)
+    private Author author;
+
+    public Book(String isbn, String title, Author author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
+
+    public Book() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return Objects.equals(isbn, book.isbn)
+                && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn, title);
+    }
+}

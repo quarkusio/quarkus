126593c1b91c7ab2c8dbc730aa940d420cffc153
Guillaume Smet <guillaume.smet@gmail.com>
2025-02-10 19:02:44 +0100

    Simplify and fix building the list of framework endpoints
    We already have the full path at some point so there's really no need to
    rebuild it once again, especially since the code was incorrect as it was
    adding the /q/ prefix always if it wasn't there in the path.
    
    It's perfectly valid to not have /q in the path, for instance if you set
    quarkus.smallrye-health.root-path to /something/else.
    
    I added a new test based on the report in addition to the ones we added
    recently for related fixes.
    
    Fixes #46040
    
    (cherry picked from commit d8daa7163caab19e7e4680a44fd06cc61c7a4e71)

--- extensions/vertx-http/deployment/src/main/java/io/quarkus/vertx/http/deployment/VertxHttpProcessor.java
+++ extensions/vertx-http/deployment/src/main/java/io/quarkus/vertx/http/deployment/VertxHttpProcessor.java
@@ -1,7 +1,6 @@
 package io.quarkus.vertx.http.deployment;
 
 import static io.quarkus.deployment.pkg.steps.GraalVM.Version.CURRENT;
-import static io.quarkus.runtime.TemplateHtmlBuilder.adjustRoot;
 import static io.quarkus.vertx.http.deployment.RequireBodyHandlerBuildItem.getBodyHandlerRequiredConditions;
 import static io.quarkus.vertx.http.deployment.RouteBuildItem.RouteType.FRAMEWORK_ROUTE;
 
@@ -69,7 +68,6 @@ import io.quarkus.vertx.http.deployment.devmode.HttpRemoteDevClientProvider;
 import io.quarkus.vertx.http.deployment.devmode.NotFoundPageDisplayableEndpointBuildItem;
 import io.quarkus.vertx.http.deployment.spi.FrameworkEndpointsBuildItem;
 import io.quarkus.vertx.http.deployment.spi.UseManagementInterfaceBuildItem;
-import io.quarkus.vertx.http.runtime.BasicRoute;
 import io.quarkus.vertx.http.runtime.CurrentRequestProducer;
 import io.quarkus.vertx.http.runtime.CurrentVertxRequest;
 import io.quarkus.vertx.http.runtime.HttpBuildTimeConfig;
@@ -139,27 +137,11 @@ class VertxHttpProcessor {
         Set<String> frameworkEndpoints = new HashSet<>();
         for (RouteBuildItem route : routes) {
             if (FRAMEWORK_ROUTE.equals(route.getRouteType())) {
-                if (route.getConfiguredPathInfo() != null) {
-                    String endpointPath = route.getConfiguredPathInfo().getEndpointPath(nonApplicationRootPath,
-                            managementInterfaceBuildTimeConfig, launchModeBuildItem);
-                    frameworkEndpoints.add(endpointPath);
+                if (route.getAbsolutePath() == null) {
                     continue;
                 }
-                if (route.getRouteFunction() != null && route.getRouteFunction() instanceof BasicRoute) {
-                    BasicRoute basicRoute = (BasicRoute) route.getRouteFunction();
-                    if (basicRoute.getPath() != null) {
-                        if (basicRoute.getPath().startsWith(nonApplicationRootPath.getNonApplicationRootPath())) {
-                            // Do not repeat the non application root path.
-                            frameworkEndpoints.add(basicRoute.getPath());
-                        } else {
-                            // Calling TemplateHtmlBuilder does not see very correct here, but it is the underlying API for ConfiguredPathInfo
-                            String adjustRoot = adjustRoot(nonApplicationRootPath.getNonApplicationRootPath(),
-                                    basicRoute.getPath());
-                            frameworkEndpoints.add(adjustRoot);
-                        }
 
-                    }
-                }
+                frameworkEndpoints.add(route.getAbsolutePath());
             }
         }
         return new FrameworkEndpointsBuildItem(frameworkEndpoints);

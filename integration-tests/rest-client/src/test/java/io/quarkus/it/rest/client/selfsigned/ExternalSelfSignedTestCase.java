4db52c66dc0315f9e81ff980975839ce55a5cdee
Peter Palaga <ppalaga@redhat.com>
2025-02-19 22:51:31 +0100

    javax.net.ssl TLS configuration does not work in native mode, fix #46368
    (cherry picked from commit ababb2fe21578071d257e37cef8ba310d9ab3875)

--- integration-tests/rest-client/src/test/java/io/quarkus/it/rest/client/selfsigned/ExternalSelfSignedTestCase.java
+++ integration-tests/rest-client/src/test/java/io/quarkus/it/rest/client/selfsigned/ExternalSelfSignedTestCase.java
@@ -1,6 +1,8 @@
 package io.quarkus.it.rest.client.selfsigned;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.Test;
@@ -15,10 +17,28 @@ public class ExternalSelfSignedTestCase {
     @Test
     public void should_accept_self_signed_certs() {
         when()
-                .get("/self-signed")
+                .get("/self-signed/ExternalSelfSignedClient")
                 .then()
                 .statusCode(200)
-                .body(is("200"));
+                .body(is("Hello self-signed!"));
+    }
+
+    @Test
+    public void javaxNetSsl() {
+        given()
+                .get("/self-signed/HttpClient/javax.net.ssl")
+                .then()
+                .statusCode(200)
+                .body(is("Hello self-signed!"));
+    }
+
+    @Test
+    public void fakeHost() {
+        given()
+                .get("/self-signed/HttpClient/fake-host")
+                .then()
+                .statusCode(500)
+                .body(containsStringIgnoringCase("unable to find valid certification path"));
     }
 
 }

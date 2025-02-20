6d5f196d5f747bfe2d1ed7de8545b3a10a6e9f33
cmoulliard <cmoulliard@redhat.com>
2025-02-20 18:02:55 +0100

    Review the kubernetes dev service documentation to improve it, add a new class KubernetesDevServiceRequestBuildItem to allow to request to launch a kind cluster from and extension. Document the code using javadoc for the the newly created classes. Remove the non needed callout
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>
    Co-authored-by: Ioannis Canellos <iocanel@gmail.com>
    (cherry picked from commit 38882ec4391e916edfe82e151e154d560627adc4)

--- extensions/kubernetes-client/spi/src/main/java/io/quarkus/kubernetes/client/spi/KubernetesDevServiceInfoBuildItem.java
+++ extensions/kubernetes-client/spi/src/main/java/io/quarkus/kubernetes/client/spi/KubernetesDevServiceInfoBuildItem.java
@@ -2,6 +2,9 @@ package io.quarkus.kubernetes.client.spi;
 
 import io.quarkus.builder.item.SimpleBuildItem;
 
+/**
+ * BuildItem packaging the information of the kind test container created
+ */
 public final class KubernetesDevServiceInfoBuildItem extends SimpleBuildItem {
     private final String kubeConfig;
     private final String containerId;
@@ -11,10 +14,16 @@ public final class KubernetesDevServiceInfoBuildItem extends SimpleBuildItem {
         this.containerId = containerId;
     }
 
+    /**
+     * @return the KubeConfig as YAML string
+     */
     public String getKubeConfig() {
         return kubeConfig;
     }
 
+    /**
+     * @return the containerId of the running kind test container
+     */
     public String getContainerId() {
         return containerId;
     }

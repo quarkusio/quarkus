a2e5afc23131ff126e312cd1858837cd1071e41f
Maximilian Zellhofer <max.zellhofer@gmail.com>
2025-02-25 09:51:13 +0100

    chore: add missing docs for maximum values of MemorySize based config values

--- extensions/resteasy-classic/rest-client-config/runtime/src/main/java/io/quarkus/restclient/config/RestClientsConfig.java
+++ extensions/resteasy-classic/rest-client-config/runtime/src/main/java/io/quarkus/restclient/config/RestClientsConfig.java
@@ -275,7 +275,7 @@ public interface RestClientsConfig {
     boolean http2();
 
     /**
-     * The max HTTP chunk size (8096 bytes by default).
+     * The max HTTP chunk size (8096 bytes by default), up to {@code Integer.MAX_VALUE} bytes.
      * <p>
      * Can be overwritten by client-specific settings.
      */
@@ -606,7 +606,7 @@ public interface RestClientsConfig {
         Optional<Boolean> http2();
 
         /**
-         * The max HTTP chunk size (8096 bytes by default).
+         * The max HTTP chunk size (8096 bytes by default), up to {@code Integer.MAX_VALUE} bytes.
          * <p>
          * This property is not applicable to the RESTEasy Client.
          */

5bcbfb9615b93ee2709954272c741a1cebb94b30
chu3la <elmansouri.houssam@gmail.com>
2025-02-27 14:33:04 +0000

    Adjusting switch between Docker Hub and Quay.io

--- test-framework/common/src/main/java/io/quarkus/test/common/ContainerImagesConfig.java
+++ test-framework/common/src/main/java/io/quarkus/test/common/ContainerImagesConfig.java
@@ -3,8 +3,13 @@ package io.quarkus.test.common;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithName;
 
+/**
+ * Centralized configuration for all container images used in Quarkus tests.
+ * This interface allows configuration of all container images in a single place.
+ */
 @ConfigMapping(prefix = "quarkus.container.images")
 public interface ContainerImagesConfig {
+    // Database images
     @WithName("postgres")
     String postgresImage();
 
@@ -26,6 +31,16 @@ public interface ContainerImagesConfig {
     @WithName("mongo")
     String mongoImage();
 
+    @WithName("redis")
+    String redisImage();
+
+    @WithName("derby")
+    String derbyImage();
+
+    @WithName("h2")
+    String h2Image();
+
+    // Elasticsearch stack
     @WithName("elasticsearch")
     String elasticsearchImage();
 
@@ -35,12 +50,49 @@ public interface ContainerImagesConfig {
     @WithName("kibana")
     String kibanaImage();
 
+    // OpenSearch
     @WithName("opensearch")
     String opensearchImage();
 
+    // Message brokers
+    @WithName("kafka")
+    String kafkaImage();
+
+    @WithName("rabbitmq")
+    String rabbitmqImage();
+
+    @WithName("amqp")
+    String amqpImage();
+
+    @WithName("pulsar")
+    String pulsarImage();
+
+    // Security
     @WithName("keycloak")
     String keycloakImage();
 
     @WithName("keycloak-legacy")
     String keycloakLegacyImage();
+
+    @WithName("ldap")
+    String ldapImage();
+
+    // Miscellaneous
+    @WithName("infinispan")
+    String infinispanImage();
+
+    @WithName("wiremock")
+    String wiremockImage();
+
+    @WithName("mailpit")
+    String mailpitImage();
+
+    @WithName("smtp")
+    String smtpImage();
+
+    @WithName("redpanda")
+    String redpandaImage();
+
+    @WithName("otel-lgtm")
+    String otelLgtmImage();
 }

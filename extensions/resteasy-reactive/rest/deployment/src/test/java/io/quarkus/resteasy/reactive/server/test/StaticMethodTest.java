4448d6ff74c7c9143d678bc560248f67053ea79e
Stuart Douglas <stuart.w.douglas@gmail.com>
2025-03-20 16:29:26 +1100

    Allow for static JAX-RS resource methods
    This is really useful in kotlin as it allows you to define methods that are not inside a class.

---
+++ extensions/resteasy-reactive/rest/deployment/src/test/java/io/quarkus/resteasy/reactive/server/test/StaticMethodTest.java
@@ -0,0 +1,65 @@
+package io.quarkus.resteasy.reactive.server.test;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import org.hamcrest.Matchers;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class StaticMethodTest {
+
+    @RegisterExtension
+    static QuarkusUnitTest test = new QuarkusUnitTest()
+            .setArchiveProducer(new Supplier<>() {
+                @Override
+                public JavaArchive get() {
+                    return ShrinkWrap.create(JavaArchive.class)
+                            .addClasses(StaticMethod.class, FooNoClassPathStaticMethod.class);
+                }
+            });
+
+    @Test
+    public void testStaticMethod() {
+        given().when().get("hello")
+                .then()
+                .statusCode(200)
+                .body(Matchers.equalTo("hello"));
+        given().when().get("noclass")
+                .then()
+                .statusCode(200)
+                .body(Matchers.equalTo("noclass"));
+    }
+
+    @Path("/")
+    public static class StaticMethod {
+
+        @Path("hello")
+        @GET
+        public static String hello() {
+            return "hello";
+        }
+    }
+
+    public static class FooNoClassPathStaticMethod {
+
+        public FooNoClassPathStaticMethod(StaticMethodTest someParam, Objects otherParam) { //CDI can't create this class
+
+        }
+
+        @Path("noclass")
+        @GET
+        public static String hello() {
+            return "noclass";
+        }
+    }
+}

43a11c346d29c7a656dc4786c79c0cfcc7bce74b
Andreas Eberle <andreas.eberle@arconsis.com>
2025-03-24 10:53:57 +0100

    Update Kotlin to version 2.1.20

--- integration-tests/gradle/src/main/resources/grpc-multi-module-no-java/build.gradle.kts
+++ integration-tests/gradle/src/main/resources/grpc-multi-module-no-java/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.allopen") version "2.0.21"
+    kotlin("jvm") version "2.1.20"
+    kotlin("plugin.allopen") version "2.1.20"
 
     id("io.quarkus") apply false
 }

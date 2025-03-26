e2690a8a30a65ffd317f7f7e755c243ad10627ec
marko-bekhta <marko.prykladna@gmail.com>
2025-03-26 17:12:56 +0100

    Add Jakarta Data dependency management and tests

---
+++ integration-tests/hibernate-orm-data/src/test/java/io/quarkus/it/hibernate/jpamodelgen/data/HibernateOrmDataInGraalIT.java
@@ -0,0 +1,8 @@
+package io.quarkus.it.hibernate.jpamodelgen.data;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HibernateOrmDataInGraalIT extends HibernateOrmDataTest {
+
+}

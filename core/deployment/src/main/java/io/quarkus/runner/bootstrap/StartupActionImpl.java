0245f231e22a852adc112bce7708834fe168be1d
Holly Cummins <hcummins@redhat.com>
2025-04-01 18:58:55 +0100

    Conditional closing of curated application

--- core/deployment/src/main/java/io/quarkus/runner/bootstrap/StartupActionImpl.java
+++ core/deployment/src/main/java/io/quarkus/runner/bootstrap/StartupActionImpl.java
@@ -340,11 +340,15 @@ public class StartupActionImpl implements StartupAction {
                                 log.error("Failed to run close task", t);
                             }
                         }
-                        if (curatedApplication.getQuarkusBootstrap().getMode() == QuarkusBootstrap.Mode.TEST &&
-                                !curatedApplication.getQuarkusBootstrap().isAuxiliaryApplication()) {
-                            //for tests, we just always shut down the curated application, as it is only used once
-                            //dev mode might be about to restart, so we leave it
-                            curatedApplication.close();
+                        // This will read the state of the curated application at the time of closing;
+                        // If the caller of close knows that the 'next' application shares a curated application, it can set eligible for reuse to true
+                        if (!curatedApplication.isEligibleForReuse()) {
+                            if (curatedApplication.getQuarkusBootstrap().getMode() == QuarkusBootstrap.Mode.TEST
+                                    && !curatedApplication.getQuarkusBootstrap().isAuxiliaryApplication()) {
+                                //for tests, we just always shut down the curated application, as it is only used once
+                                //dev mode might be about to restart, so we leave it
+                                curatedApplication.close();
+                            }
                         }
                     }
                 }

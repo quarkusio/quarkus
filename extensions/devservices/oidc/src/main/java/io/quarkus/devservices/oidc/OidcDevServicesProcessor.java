b7bec4af2b0efa715e19cbaba653c97136dc2b82
Guillaume Smet <guillaume.smet@gmail.com>
2025-04-04 14:28:05 +0200

    Avoid detecting the container runtime in OIDC Dev Services
    We check this condition last so that we can avoid detecting the
    container if not needed.
    
    It will avoid issues with integration-tests/oidc-wiremock on GitHub
    Actions Windows runners.
    
    (cherry picked from commit 85ce4f5dac6264fb681670dc5a16c316e911a649)

--- extensions/devservices/oidc/src/main/java/io/quarkus/devservices/oidc/OidcDevServicesProcessor.java
+++ extensions/devservices/oidc/src/main/java/io/quarkus/devservices/oidc/OidcDevServicesProcessor.java
@@ -145,10 +145,6 @@ public class OidcDevServicesProcessor {
             LOG.debug("Not starting Dev Services for OIDC as it has been disabled in the config");
             return true;
         }
-        if (devServicesConfig.enabled().isEmpty() && dockerStatusBuildItem.isContainerRuntimeAvailable()) {
-            LOG.debug("Not starting Dev Services for OIDC as detected support the container functionality");
-            return true;
-        }
         if (!isOidcEnabled()) {
             LOG.debug("Not starting Dev Services for OIDC as OIDC extension has been disabled in the config");
             return true;
@@ -163,6 +159,11 @@ public class OidcDevServicesProcessor {
         }
         if (ConfigUtils.isPropertyPresent(PROVIDER_CONFIG_KEY)) {
             LOG.debug("Not starting Dev Services for OIDC as 'quarkus.oidc.provider' has been provided");
+            return true;
+        }
+        if (devServicesConfig.enabled().isEmpty() && dockerStatusBuildItem.isContainerRuntimeAvailable()) {
+            LOG.debug(
+                    "Not starting Dev Services for OIDC as a container runtime is available and a Keycloak Dev Services will be started");
             return true;
         }
         return false;

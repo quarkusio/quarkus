0f567308f11ea62c94efc40db701f6d5e36b50a8
Ladislav Thon <lthon@redhat.com>
2025-04-08 15:42:38 +0200

    Jandex: reindex if the index is too new
    We've been bitten by this issue many times in the past: if a dependency
    upgrades to a newer Jandex than what Quarkus contains, build fails
    with an `UnsupportedVersion` exception.
    
    This commit is a proper solution: if the exception is thrown, we swallow it
    and reindex the archive with the Jandex version that Quarkus incorporates.
    
    Further, this commit increments the minimum Jandex version we "trust" from
    Jandex 2.1 to Jandex 3.0. We rely on new features from Jandex 2.4 and 3.0
    on many places, so it makes sense to make 3.0 the new minimum. The changes
    added in 3.2 and 3.3 are also important, but are not used heavily yet.
    
    (cherry picked from commit 97572fa394c7b76742cbcc7a4714baa8b71278bb)

--- extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/boot/StaticInitFailureTest.java
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/boot/StaticInitFailureTest.java
@@ -27,7 +27,9 @@ public class StaticInitFailureTest {
                     .addClass(EntityWithIncorrectMapping.class))
             .withConfigurationResource("application.properties")
             // Expect only one error: the one we triggered
-            .setLogRecordPredicate(record -> record.getLevel().intValue() >= Level.WARNING.intValue())
+            .setLogRecordPredicate(record -> record.getLevel().intValue() >= Level.WARNING.intValue()
+                    // Ignore these particular warnings: they are not relevant to this test.
+                    && !record.getMessage().contains("must be used to index an application dependency")) // too old Jandex
             // In particular we don't want a log telling us JPAConfig could not be created
             // because HibernateOrmRuntimeConfig is not initialized yet.
             // JPAConfig should not be created in the first place!

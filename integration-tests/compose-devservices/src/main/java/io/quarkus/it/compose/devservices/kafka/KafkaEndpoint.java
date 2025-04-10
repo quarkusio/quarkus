5cabf538985ddcc0309446fc3079fdd630aa5955
Ozan Gunalp <ozangunalp@gmail.com>
2025-04-10 15:05:07 +0200

    Improve test stability for compose devservice IT

--- integration-tests/compose-devservices/src/main/java/io/quarkus/it/compose/devservices/kafka/KafkaEndpoint.java
+++ integration-tests/compose-devservices/src/main/java/io/quarkus/it/compose/devservices/kafka/KafkaEndpoint.java
@@ -2,6 +2,7 @@ package io.quarkus.it.compose.devservices.kafka;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 
@@ -15,5 +16,11 @@ public class KafkaEndpoint {
     @Path("/partitions/{topic}")
     public Integer partitions(@PathParam("topic") String topic) {
         return admin.partitions(topic);
+    }
+
+    @PUT
+    @Path("/topics/{topic}/{partitions}")
+    public void partitions(@PathParam("topic") String topic, @PathParam("partitions") int partitions) {
+        admin.createTopic(topic, partitions);
     }
 }

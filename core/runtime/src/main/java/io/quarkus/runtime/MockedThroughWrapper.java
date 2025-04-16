d460100efee9642ab3881875a2c4fa9d86cbcb73
Georgios Andrianakis <geoand@gmail.com>
2025-04-16 13:14:12 +0300

    Polish Javadoc of MockedThroughWrapper

--- core/runtime/src/main/java/io/quarkus/runtime/MockedThroughWrapper.java
+++ core/runtime/src/main/java/io/quarkus/runtime/MockedThroughWrapper.java
@@ -2,8 +2,8 @@ package io.quarkus.runtime;
 
 /**
  * Usually, QuarkusMock mocking replaces a "delegating instance" of a client proxy.
- *
- * In some cases, e.g. for REST Client Reactive, a CDI bean is a wrapper over a delegate.
+ * <p>
+ * In some cases, e.g. for REST Client, a CDI bean is a wrapper over a delegate.
  * This interface allows to replace the delegate instead of the delegating instance of the proxy.
  */
 public interface MockedThroughWrapper {

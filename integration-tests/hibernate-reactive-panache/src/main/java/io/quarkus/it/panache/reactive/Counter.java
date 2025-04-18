5d484425e4fe89b6d5a8c8dae59403a4f0897771
Davide D'Alto <davide@hibernate.org>
2025-04-18 15:10:27 +0200

    Test

---
+++ integration-tests/hibernate-reactive-panache/src/main/java/io/quarkus/it/panache/reactive/Counter.java
@@ -0,0 +1,37 @@
+package io.quarkus.it.panache.reactive;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Counter extends PanacheEntityBase {
+
+    @Id
+    private Long id;
+    private int count = 0;
+
+    public Counter() {
+    }
+
+    public Counter(long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(count);
+    }
+
+    public void increase() {
+        this.count++;
+    }
+}

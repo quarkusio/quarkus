44d9e7499870271faf73686ee33ef3d7a8910b66
Guillaume Smet <guillaume.smet@gmail.com>
2025-04-18 11:09:32 +0200

    Make Stork optional for REST Client
    If you want to use Stork, you now have to add the quarkus-smallrye-stork
    extension, which IMHO makes perfect sense.
    
    This avoids initializing Stork for every application using the REST
    Client.
    
    Fixes #47337

--- independent-projects/resteasy-reactive/client/runtime/src/main/java/org/jboss/resteasy/reactive/client/impl/StorkClientRequestFilter.java
+++ independent-projects/resteasy-reactive/client/runtime/src/main/java/org/jboss/resteasy/reactive/client/impl/StorkClientRequestFilter.java
@@ -20,7 +20,24 @@ import io.smallrye.stork.Stork;
 @Priority(Priorities.AUTHENTICATION)
 @Provider
 public class StorkClientRequestFilter implements ResteasyReactiveClientRequestFilter {
+
     private static final Logger log = Logger.getLogger(StorkClientRequestFilter.class);
+
+    private final Stork stork;
+
+    public StorkClientRequestFilter() {
+        try {
+            stork = Stork.getInstance();
+
+            if (stork == null) {
+                throw new IllegalStateException(
+                        "Trying to use a StorkClientRequestFilter but the quarkus-smallrye-stork extension is missing, please add the extension.");
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(
+                    "Trying to use a StorkClientRequestFilter but the quarkus-smallrye-stork extension is missing, please add the extension.");
+        }
+    }
 
     @Override
     public void filter(ResteasyReactiveClientRequestContext requestContext) {
@@ -34,8 +51,7 @@ public class StorkClientRequestFilter implements ResteasyReactiveClientRequestFi
             requestContext.suspend();
             boolean measureTime = shouldMeasureTime(requestContext.getResponseType());
             try {
-                Stork.getInstance()
-                        .getService(serviceName)
+                stork.getService(serviceName)
                         .selectInstanceAndRecordStart(measureTime)
                         .subscribe()
                         .with(instance -> {

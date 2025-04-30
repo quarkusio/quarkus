7297fd953b0d7f798850d379e2914a56592d4ad1
iedo <iedoduarte1@gmail.com>
2025-04-29 23:47:39 -0300

    Add Javadoc for AppModelProviderBuildItem class

--- core/deployment/src/main/java/io/quarkus/deployment/builditem/AppModelProviderBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/builditem/AppModelProviderBuildItem.java
@@ -11,11 +11,17 @@ import io.quarkus.bootstrap.model.PlatformImports;
 import io.quarkus.builder.item.SimpleBuildItem;
 import io.quarkus.deployment.BootstrapConfig;
 
+/**
+ * A build item used to provide a application dependency model.
+ *
+ * @see ApplicationModel
+ */
 public final class AppModelProviderBuildItem extends SimpleBuildItem {
 
     private static final Logger log = Logger.getLogger(AppModelProviderBuildItem.class);
 
     private final ApplicationModel appModel;
+
     private final Supplier<DependencyInfoProvider> depInfoProvider;
 
     public AppModelProviderBuildItem(ApplicationModel appModel) {
@@ -27,6 +33,20 @@ public final class AppModelProviderBuildItem extends SimpleBuildItem {
         this.depInfoProvider = depInfoProvider;
     }
 
+    /**
+     * Validates the platform imports in the application model against the provided bootstrap configuration.
+     * The behavior in case of misalignment depends on the provided {@link BootstrapConfig#misalignedPlatformImports()}:
+     * <ul>
+     * <li><b>ERROR</b>: Throws a {@link RuntimeException}.</li>
+     * <li><b>WARN</b>: Logs a warning.</li>
+     * <li><b>IGNORE</b>: Skips validation entirely.</li>
+     * </ul>
+     *
+     * @param config the bootstrap configuration
+     * @return the validated application model
+     * @throws RuntimeException if platform imports are misaligned and the configuration is set to {@code ERROR}
+     *         or if the configuration is unrecognized.
+     */
     public ApplicationModel validateAndGet(BootstrapConfig config) {
         final PlatformImports platforms = appModel.getPlatforms();
         if (platforms != null && !BootstrapConfig.MisalignedPlatformImports.IGNORE.equals(config.misalignedPlatformImports())

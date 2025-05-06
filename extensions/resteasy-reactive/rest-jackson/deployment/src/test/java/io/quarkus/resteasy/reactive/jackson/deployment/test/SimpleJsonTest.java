e42e3973b15c88754aa3e0d46789ad28dfc3db1f
mariofusco <mario.fusco@gmail.com>
2025-05-06 17:38:43 +0200

    Fix reflection-free json serialization with @JsonValue
    (cherry picked from commit a60e2734819088cdb7c1cd8afe363d8864631c75)

--- extensions/resteasy-reactive/rest-jackson/deployment/src/test/java/io/quarkus/resteasy/reactive/jackson/deployment/test/SimpleJsonTest.java
+++ extensions/resteasy-reactive/rest-jackson/deployment/src/test/java/io/quarkus/resteasy/reactive/jackson/deployment/test/SimpleJsonTest.java
@@ -27,7 +27,7 @@ public class SimpleJsonTest extends AbstractSimpleJsonTest {
                                     Pond.class, FrogBodyParts.class, FrogBodyParts.BodyPart.class, ContainerDTO.class,
                                     NestedInterface.class, StateRecord.class, MapWrapper.class, GenericWrapper.class,
                                     Fruit.class, Price.class, DogRecord.class, ItemExtended.class, Book.class, LombokBook.class,
-                                    PrimitiveTypesBean.class, PrimitiveTypesRecord.class, TokenResponse.class)
+                                    PrimitiveTypesBean.class, PrimitiveTypesRecord.class, TokenResponse.class, ItemId.class)
                             .addAsResource(new StringAsset("admin-expression=admin\n" +
                                     "user-expression=user\n" +
                                     "birth-date-roles=alice,bob\n"), "application.properties");

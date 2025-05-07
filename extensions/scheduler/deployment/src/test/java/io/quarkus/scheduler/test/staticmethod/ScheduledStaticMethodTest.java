0fd821e71857dacd34fe937ec6b24989674defdf
Martin Kouba <mkouba@redhat.com>
2025-05-07 18:06:16 +0200

    Scheduler: register static Scheduled methods correctly
    - fixes #47738
    
    (cherry picked from commit f30d9e042d38269f048a0affe398576aec99038f)

--- extensions/scheduler/deployment/src/test/java/io/quarkus/scheduler/test/staticmethod/ScheduledStaticMethodTest.java
+++ extensions/scheduler/deployment/src/test/java/io/quarkus/scheduler/test/staticmethod/ScheduledStaticMethodTest.java
@@ -1,14 +1,18 @@
 package io.quarkus.scheduler.test.staticmethod;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import jakarta.inject.Inject;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.scheduler.Scheduled;
+import io.quarkus.scheduler.Scheduler;
 import io.quarkus.test.QuarkusUnitTest;
 
 public class ScheduledStaticMethodTest {
@@ -18,8 +22,12 @@ public class ScheduledStaticMethodTest {
             .withApplicationRoot((jar) -> jar
                     .addClasses(Jobs.class, AbstractJobs.class, InterfaceJobs.class));
 
+    @Inject
+    Scheduler scheduler;
+
     @Test
     public void testSimpleScheduledJobs() throws InterruptedException {
+        assertEquals(3, scheduler.getScheduledJobs().size());
         assertTrue(Jobs.LATCH.await(5, TimeUnit.SECONDS));
         assertTrue(AbstractJobs.LATCH.await(5, TimeUnit.SECONDS));
         assertTrue(InterfaceJobs.LATCH.await(5, TimeUnit.SECONDS));

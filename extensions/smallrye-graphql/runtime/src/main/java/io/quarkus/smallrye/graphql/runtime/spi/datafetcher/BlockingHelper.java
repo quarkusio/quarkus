d43420323b6ab1cb3b3759209a3c22b4f7b3dec5
Robert Pospisil <robert.pospisil@mobilex.eu>
2025-05-12 13:29:07 +0200

    vt for graphql

--- extensions/smallrye-graphql/runtime/src/main/java/io/quarkus/smallrye/graphql/runtime/spi/datafetcher/BlockingHelper.java
+++ extensions/smallrye-graphql/runtime/src/main/java/io/quarkus/smallrye/graphql/runtime/spi/datafetcher/BlockingHelper.java
@@ -1,13 +1,19 @@
 package io.quarkus.smallrye.graphql.runtime.spi.datafetcher;
 
-import java.util.concurrent.Callable;
-
+import io.quarkus.arc.Arc;
+import io.quarkus.virtual.threads.VirtualThreads;
 import io.smallrye.graphql.schema.model.Execute;
 import io.smallrye.graphql.schema.model.Operation;
 import io.vertx.core.Context;
 import io.vertx.core.Promise;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+
 public final class BlockingHelper {
+
+    private static Boolean isVirtualThreadsExecutorAvailable = null; //todo or no need to cache this?
+
 
     private BlockingHelper() {
     }
@@ -22,9 +28,41 @@ public final class BlockingHelper {
         return operation.getExecute().equals(Execute.BLOCKING) && vc.isEventLoopContext();
     }
 
+    public static boolean shouldUseVirtualThread(Operation operation, Context vc) {
+        // Use virtual threads if the operation is marked as blocking and we're on an event loop
+        // and the virtual threads executor is available
+        return isVirtualThreadsExecutorAvailable() && operation.getExecute().equals(Execute.RUN_ON_VIRTUAL_THREAD);
+    }
+
+    private static boolean isVirtualThreadsExecutorAvailable() {
+        if (isVirtualThreadsExecutorAvailable == null) {
+            try {
+                isVirtualThreadsExecutorAvailable = Arc.container()
+                        .instance(ExecutorService.class, VirtualThreads.Literal.INSTANCE).isAvailable();
+            } catch (Exception e) {
+                isVirtualThreadsExecutorAvailable = false;
+            }
+        }
+        return isVirtualThreadsExecutorAvailable;
+    }
+
     @SuppressWarnings("unchecked")
-    public static void runBlocking(Context vc, Callable<Object> contextualCallable, Promise result) {
-        // Here call blocking with context
-        vc.executeBlocking(contextualCallable).onComplete(result);
+    public static void runBlocking(Context vc, Callable<Object> contextualCallable, Promise result, Operation operation) {
+        // Check if we should use virtual threads
+        if (shouldUseVirtualThread(operation, vc)) {
+            ExecutorService virtualThreadsExecutor = Arc.container()
+                    .instance(ExecutorService.class, VirtualThreads.Literal.INSTANCE).get();
+            virtualThreadsExecutor.submit(() -> {
+                try {
+                    Object value = contextualCallable.call();
+                    result.complete(value);
+                } catch (Throwable t) {
+                    result.fail(t);
+                }
+            });
+        } else {
+            // use regular blocking execution
+            vc.executeBlocking(contextualCallable).onComplete(result);
+        }
     }
 }

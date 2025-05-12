23c9905cab70529b47ef3a91dfdda7d487a562bb
Yoann Rodière <yoann@hibernate.org>
2025-05-12 15:24:48 +0200

    Support DB version checks (enabled by default) with Hibernate Reactive

--- extensions/hibernate-reactive/runtime/src/main/java/io/quarkus/hibernate/reactive/runtime/customized/QuarkusReactiveConnectionPoolInitiator.java
+++ extensions/hibernate-reactive/runtime/src/main/java/io/quarkus/hibernate/reactive/runtime/customized/QuarkusReactiveConnectionPoolInitiator.java
@@ -3,9 +3,7 @@ package io.quarkus.hibernate.reactive.runtime.customized;
 import java.util.Map;
 
 import org.hibernate.boot.registry.StandardServiceInitiator;
-import org.hibernate.engine.jdbc.spi.JdbcServices;
 import org.hibernate.reactive.pool.ReactiveConnectionPool;
-import org.hibernate.reactive.pool.impl.ExternalSqlClientPool;
 import org.hibernate.service.spi.ServiceRegistryImplementor;
 
 import io.quarkus.hibernate.orm.runtime.migration.MultiTenancyStrategy;
@@ -33,8 +31,7 @@ public final class QuarkusReactiveConnectionPoolInitiator
             // nothing to do, but given the separate hierarchies have to handle this here.
             return null;
         }
-        final JdbcServices jdbcService = registry.getService(JdbcServices.class);
-        return new ExternalSqlClientPool(pool, jdbcService.getSqlStatementLogger(), jdbcService.getSqlExceptionHelper());
+        return new QuarkusSqlClientPool(pool);
     }
 
 }

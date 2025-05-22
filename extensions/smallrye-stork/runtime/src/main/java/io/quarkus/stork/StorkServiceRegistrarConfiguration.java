f143f3f1daa7bf83126d93f90edea2216b24f24d
Auri Munoz <amunozhe@redhat.com>
2025-05-22 11:52:35 +0200

    Enable automatic registration for Quarkus apps using built-in Stork registrars (Consul, Eureka, Static)

--- extensions/smallrye-stork/runtime/src/main/java/io/quarkus/stork/StorkServiceRegistrarConfiguration.java
+++ extensions/smallrye-stork/runtime/src/main/java/io/quarkus/stork/StorkServiceRegistrarConfiguration.java
@@ -1,26 +1,33 @@
 package io.quarkus.stork;
 
 import java.util.Map;
+import java.util.Optional;
 
 import io.quarkus.runtime.annotations.ConfigGroup;
+import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithParentName;
 
 @ConfigGroup
 public interface StorkServiceRegistrarConfiguration {
 
     /**
+     * Whether automatic registration and deregistration of service instances is enabled.
+     */
+    @WithDefault("true")
+    boolean enabled();
+
+    /**
      * Configures service registrar type, e.g. "consul".
      * A ServiceRegistrarProvider for the type has to be available
      *
      */
-    String type();
+    Optional<String> type();
 
     /**
      * Service Registrar parameters.
      * Check the documentation of the selected registrar type for available parameters
      *
      */
-    //    @ConfigItem(name = ConfigItem.PARENT)
     @WithParentName
     Map<String, String> parameters();
 

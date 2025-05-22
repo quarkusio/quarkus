e6e0f2edd2c49fa72cdff48f477b48dc1f756679
Auri Munoz <amunozhe@redhat.com>
2025-05-22 11:52:35 +0200

    first try

---
+++ extensions/smallrye-stork/deployment/src/main/java/io/quarkus/stork/deployment/StorkRegistrationBuildItem.java
@@ -0,0 +1,19 @@
+package io.quarkus.stork.deployment;
+
+import io.quarkus.builder.item.SimpleBuildItem;
+
+public final class StorkRegistrationBuildItem extends SimpleBuildItem {
+
+    //    public static final String UNSET_VALUE = "<<unset>>";
+    //
+    //    private final List<ServiceConfig> serviceConfigs;
+
+    //    public StorkRegistrationBuildItem(List<ServiceConfig> serviceConfigs) {
+    //        this.serviceConfigs = serviceConfigs;
+    //    }
+
+    //    public List<ServiceConfig> getServiceConfigs() {
+    //        return this.serviceConfigs;
+    //    }
+
+}

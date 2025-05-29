72925e33aa2f262b2031374887090adf32f0fc6e
David M. Lloyd <david.lloyd@redhat.com>
2025-05-29 14:53:40 -0500

    Simplify JVM version detection
    We can use the built-in version mechanism instead of parsing out a string with regex.

--- extensions/awt/runtime/src/main/java/io/quarkus/awt/runtime/graal/DarwinAwtFeature.java
+++ extensions/awt/runtime/src/main/java/io/quarkus/awt/runtime/graal/DarwinAwtFeature.java
@@ -5,13 +5,11 @@ import org.graalvm.nativeimage.Platforms;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
 
-import io.quarkus.runtime.util.JavaVersionUtil;
-
 @Platforms({ Platform.DARWIN_AMD64.class, Platform.DARWIN_AARCH64.class })
 public class DarwinAwtFeature implements Feature {
     @Override
     public void afterRegistration(AfterRegistrationAccess access) {
-        if (JavaVersionUtil.isJava17OrHigher()) {
+        if (Runtime.version().major() >= 17) {
             // Quarkus run time init for AWT in Darwin
             RuntimeClassInitialization.initializeAtRunTime("sun.lwawt.macosx");
         }

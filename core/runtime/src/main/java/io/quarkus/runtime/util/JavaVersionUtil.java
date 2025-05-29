5cc07e9a385b94bd99ff100e446482b984d5d379
David M. Lloyd <david.lloyd@redhat.com>
2025-05-29 14:53:40 -0500

    Simplify JVM version detection
    We can use the built-in version mechanism instead of parsing out a string with regex.

--- core/runtime/src/main/java/io/quarkus/runtime/util/JavaVersionUtil.java
+++ core/runtime/src/main/java/io/quarkus/runtime/util/JavaVersionUtil.java
@@ -1,49 +1,27 @@
 package io.quarkus.runtime.util;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class JavaVersionUtil {
 
-    private static final Pattern PATTERN = Pattern.compile("(?:1\\.)?(\\d+)");
-
-    private static boolean IS_GRAALVM_JDK;
-    private static boolean IS_JAVA_17_OR_NEWER;
-    private static boolean IS_JAVA_19_OR_NEWER;
-    private static boolean IS_JAVA_21_OR_NEWER;
+    private static final boolean IS_GRAALVM_JDK;
 
     static {
-        performChecks();
-    }
-
-    // visible for testing
-    static void performChecks() {
-        Matcher matcher = PATTERN.matcher(System.getProperty("java.specification.version", ""));
-        if (matcher.matches()) {
-            int first = Integer.parseInt(matcher.group(1));
-            IS_JAVA_17_OR_NEWER = (first >= 17);
-            IS_JAVA_19_OR_NEWER = (first >= 19);
-            IS_JAVA_21_OR_NEWER = (first >= 21);
-        } else {
-            IS_JAVA_17_OR_NEWER = false;
-            IS_JAVA_19_OR_NEWER = false;
-            IS_JAVA_21_OR_NEWER = false;
-        }
-
         String vmVendor = System.getProperty("java.vm.vendor");
         IS_GRAALVM_JDK = (vmVendor != null) && vmVendor.startsWith("GraalVM");
     }
 
+    @Deprecated(forRemoval = true)
     public static boolean isJava17OrHigher() {
-        return IS_JAVA_17_OR_NEWER;
+        return Runtime.version().major() >= 17;
     }
 
+    @Deprecated(forRemoval = true)
     public static boolean isJava19OrHigher() {
-        return IS_JAVA_19_OR_NEWER;
+        return Runtime.version().major() >= 19;
     }
 
+    @Deprecated(forRemoval = true)
     public static boolean isJava21OrHigher() {
-        return IS_JAVA_21_OR_NEWER;
+        return Runtime.version().major() >= 21;
     }
 
     public static boolean isGraalvmJdk() {

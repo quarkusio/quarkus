bf8c645f8a79626c09998066f98bed7bc14b7fd3
João Lopes <joao.c.lopes@sapo.pt>
2025-05-30 10:50:59 +0100

    Added SuppressForbidden annotation.

---
+++ independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/SuppressForbidden.java
@@ -0,0 +1,15 @@
+package io.quarkus.arc.processor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used to suppress forbidden-apis errors inside the element it's added to.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.METHOD, ElementType.TYPE })
+public @interface SuppressForbidden {
+    String reason();
+}

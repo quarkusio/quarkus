73a13a345a2e8f672c1a95f9f16329adce5f3a04
marko-bekhta <marko.prykladna@gmail.com>
2025-05-31 11:57:37 +0200

    Bump Elasticsearch/OpenSearch containers to latest-compatible with Hibernate Search

--- integration-tests/hibernate-search-orm-elasticsearch/src/test/java/io/quarkus/it/hibernate/search/orm/elasticsearch/devservices/HibernateSearchElasticsearchDevServicesDisabledExplicitlyTest.java
+++ integration-tests/hibernate-search-orm-elasticsearch/src/test/java/io/quarkus/it/hibernate/search/orm/elasticsearch/devservices/HibernateSearchElasticsearchDevServicesDisabledExplicitlyTest.java
@@ -37,7 +37,7 @@ public class HibernateSearchElasticsearchDevServicesDisabledExplicitlyTest {
                     // But here it doesn't matter as we won't send a request to Elasticsearch anyway,
                     // so we're free to put anything.
                     // Just make sure to set something consistent with what we have in application.properties.
-                    "quarkus.hibernate-search-orm.elasticsearch.version", "8.18"));
+                    "quarkus.hibernate-search-orm.elasticsearch.version", "9.0"));
             return config;
         }
 

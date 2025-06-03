f954d18b7718cebd8a9dd483700bb60b8810a084
Katia Aresti <karesti@redhat.com>
2025-06-03 15:30:27 +0200

    Fixes request context lost in Infinispan cache get/getasync
    (cherry picked from commit 9936f541ea698d51ddea1783e6a54db0d0e83280)

--- integration-tests/infinispan-cache/src/main/java/io/quarkus/it/cache/infinispan/SunriseRestServerResource.java
+++ integration-tests/infinispan-cache/src/main/java/io/quarkus/it/cache/infinispan/SunriseRestServerResource.java
@@ -1,6 +1,9 @@
 package io.quarkus.it.cache.infinispan;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -12,30 +15,37 @@ import org.jboss.resteasy.reactive.RestQuery;
 @Path("sunrise")
 public class SunriseRestServerResource {
 
-    private int sunriseTimeInvocations;
+    private final AtomicInteger invocations = new AtomicInteger(0);
+
+    @Inject
+    ClientRequestService requestService;
 
     @GET
     @Path("time/{city}")
     public String getSunriseTime(@RestPath String city, @RestQuery String date) {
-        sunriseTimeInvocations++;
+        invocations.incrementAndGet();
+        requestService.setData(city);
         return "2020-12-20T10:15:30";
     }
 
     @GET
     @Path("invocations")
     public Integer getSunriseTimeInvocations() {
-        return sunriseTimeInvocations;
+        requestService.setData("invocations");
+        return invocations.get();
     }
 
     @DELETE
     @Path("invalidate/{city}")
     public void invalidate(@RestPath String city, @RestQuery String notPartOfTheCacheKey, @RestQuery String date) {
         // Do nothing. We only need to test the caching annotation on the client side.
+        requestService.setData("invalidate " + city);
     }
 
     @DELETE
     @Path("invalidate")
     public void invalidateAll() {
         // Do nothing. We only need to test the caching annotation on the client side.
+        requestService.setData("invalidate all");
     }
 }

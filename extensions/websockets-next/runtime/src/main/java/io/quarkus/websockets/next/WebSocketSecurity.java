dac23043aba02002a3c55bb67c870770282855e9
Michal Vavřík <mvavrik@redhat.com>
2025-06-04 16:43:30 +0200

    feat(ws-next,oidc): support identity update before token expires

---
+++ extensions/websockets-next/runtime/src/main/java/io/quarkus/websockets/next/WebSocketSecurity.java
@@ -0,0 +1,21 @@
+package io.quarkus.websockets.next;
+
+import java.util.concurrent.CompletionStage;
+
+import io.quarkus.security.identity.SecurityIdentity;
+
+/**
+ * A CDI bean that allows to update {@link SecurityIdentity} before previous a bearer access token expires.
+ */
+public interface WebSocketSecurity {
+
+    /**
+     * Authenticate with the bearer access token and update current {@link SecurityIdentity} attached to the WebSocket
+     * server connection.
+     *
+     * @param accessToken bearer access token
+     * @return authentication result
+     */
+    CompletionStage<SecurityIdentity> updateSecurityIdentity(String accessToken);
+
+}

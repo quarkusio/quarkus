83992866a545e608d61e71415b6ae89b844325b5
David M. Lloyd <david.lloyd@redhat.com>
2025-06-12 15:33:02 -0500

    Convert `io.quarkus.deployment.pkg.steps.NativeImageBuildRunner` to new process API

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/ErrorReplacingProcessReader.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/ErrorReplacingProcessReader.java
@@ -2,12 +2,9 @@ package io.quarkus.deployment.pkg.steps;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.concurrent.CountDownLatch;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,69 +14,57 @@ import java.util.regex.Pattern;
  * This class intercepts them and prints meaningful output instead, so users don't waste hours going on wild goose
  * chases
  */
-public final class ErrorReplacingProcessReader implements Runnable {
+public final class ErrorReplacingProcessReader {
 
     private static final String LINE_START = "Call path from entry point to ";
-    private final InputStream inputStream;
+    private final BufferedReader reader;
     private final File reportdir;
-    private final CountDownLatch doneLatch;
 
     private ReportAnalyzer reportAnalyzer;
 
-    public ErrorReplacingProcessReader(InputStream inputStream, File reportdir, CountDownLatch doneLatch) {
-        this.inputStream = inputStream;
+    public ErrorReplacingProcessReader(final BufferedReader reader, final File reportdir) {
+        this.reader = reader;
         this.reportdir = reportdir;
-        this.doneLatch = doneLatch;
     }
 
-    @Override
-    public void run() {
-        try {
-            Deque<String> fullBuffer = new ArrayDeque<>();
-            boolean buffering = false;
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-                for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-                    if (line.startsWith(LINE_START)) {
-                        buffering = true;
-                    }
-                    if (buffering) {
-                        fullBuffer.add(line);
-                    } else {
-                        System.err.println(line);
-                    }
-                }
-                File reportFile = null;
-                if (reportdir.exists()) {
-                    File[] files = reportdir.listFiles();
-                    if (files != null) {
-                        for (File j : files) {
-                            if (j.getName().startsWith("call_tree")) {
-                                reportFile = j;
-                                break;
-                            }
-                        }
-
-                    }
-                }
-                if (reportFile == null) {
-                    for (String j : fullBuffer) {
-                        System.err.println(j);
-                    }
-                } else {
-                    while (!fullBuffer.isEmpty()) {
-                        String line = fullBuffer.pop();
-                        if (line.startsWith(LINE_START)) {
-                            handleErrorState(reportFile, line, fullBuffer);
-                        } else {
-                            System.err.println(line);
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+    public void run() throws IOException {
+        Deque<String> fullBuffer = new ArrayDeque<>();
+        boolean buffering = false;
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            if (line.startsWith(LINE_START)) {
+                buffering = true;
             }
-        } finally {
-            doneLatch.countDown();
+            if (buffering) {
+                fullBuffer.add(line);
+            } else {
+                System.err.println(line);
+            }
+        }
+        File reportFile = null;
+        if (reportdir.exists()) {
+            File[] files = reportdir.listFiles();
+            if (files != null) {
+                for (File j : files) {
+                    if (j.getName().startsWith("call_tree")) {
+                        reportFile = j;
+                        break;
+                    }
+                }
+            }
+        }
+        if (reportFile == null) {
+            for (String j : fullBuffer) {
+                System.err.println(j);
+            }
+        } else {
+            while (!fullBuffer.isEmpty()) {
+                String line = fullBuffer.pop();
+                if (line.startsWith(LINE_START)) {
+                    handleErrorState(reportFile, line, fullBuffer);
+                } else {
+                    System.err.println(line);
+                }
+            }
         }
     }
 

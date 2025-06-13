40ac82e1c7015681aa2bb6da074978436dd68edc
Ozan Gunalp <ozangunalp@gmail.com>
2025-06-13 12:50:48 +0200

    Simplify RunnableDevService

--- core/runtime/src/main/java/io/quarkus/devservices/crossclassloader/runtime/DevServiceOwner.java
+++ core/runtime/src/main/java/io/quarkus/devservices/crossclassloader/runtime/DevServiceOwner.java
@@ -3,8 +3,9 @@ package io.quarkus.devservices.crossclassloader.runtime;
 /**
  * We store the launch mode as a string to make cross-classloader comparisons easier
  *
+ * @param featureName the name of the feature, e.g. "redis-client" or "kafka-client"
  * @param launchMode use launchMode.name()
- * @param configName
+ * @param configName the name of the config, e.g. "redis"
  */
 
 public record DevServiceOwner(String featureName, String launchMode, String configName) {

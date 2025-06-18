0b9b62bc9c6834d75333f44f8dd4ed408201529c
Erik Mattheis <erikmattheis@me.com>
2025-06-18 12:50:03 -0400

    expose path params and user data to upgrade check

--- extensions/websockets-next/runtime/src/main/java/io/quarkus/websockets/next/runtime/HttpUpgradeContextImpl.java
+++ extensions/websockets-next/runtime/src/main/java/io/quarkus/websockets/next/runtime/HttpUpgradeContextImpl.java
@@ -2,12 +2,18 @@ package io.quarkus.websockets.next.runtime;
 
 import io.quarkus.security.identity.SecurityIdentity;
 import io.quarkus.websockets.next.HttpUpgradeCheck;
+import io.quarkus.websockets.next.UserData;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.ext.web.RoutingContext;
 
-record HttpUpgradeContextImpl(RoutingContext routingContext,
+record HttpUpgradeContextImpl(RoutingContext routingContext, UserData userData,
         Uni<SecurityIdentity> securityIdentity, String endpointId) implements HttpUpgradeCheck.HttpUpgradeContext {
+
+    @Override
+    public String pathParam(String name) {
+        return routingContext.pathParam(name);
+    }
 
     @Override
     public HttpServerRequest httpRequest() {

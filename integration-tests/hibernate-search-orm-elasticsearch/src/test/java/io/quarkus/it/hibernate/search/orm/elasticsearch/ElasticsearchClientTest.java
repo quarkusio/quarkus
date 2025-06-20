5eba3418cdde0b87a273931883674d297f45e010
marko-bekhta <marko.prykladna@gmail.com>
2025-06-20 23:23:10 +0200

    Use dev services instead of maven docker plugin for Hibernate Search tests

--- integration-tests/hibernate-search-orm-elasticsearch/src/test/java/io/quarkus/it/hibernate/search/orm/elasticsearch/ElasticsearchClientTest.java
+++ integration-tests/hibernate-search-orm-elasticsearch/src/test/java/io/quarkus/it/hibernate/search/orm/elasticsearch/ElasticsearchClientTest.java
@@ -2,13 +2,33 @@ package io.quarkus.it.hibernate.search.orm.elasticsearch;
 
 import static org.hamcrest.Matchers.is;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
 import io.restassured.RestAssured;
 
 @QuarkusTest
+@TestProfile(ElasticsearchClientTest.Profile.class)
 public class ElasticsearchClientTest {
+    public static class Profile implements QuarkusTestProfile {
+        @Override
+        public String getConfigProfile() {
+            // Don't use %test properties;
+            // that way, we can control whether quarkus.hibernate-search-orm.elasticsearch.hosts is set or not.
+            // In this test, we do NOT set quarkus.hibernate-search-orm.elasticsearch.hosts.
+            return "someotherprofile";
+        }
+
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            return Map.of(
+                    "quarkus.elasticsearch.devservices.port", "19201");
+        }
+    }
 
     @Test
     public void testConnection() throws Exception {

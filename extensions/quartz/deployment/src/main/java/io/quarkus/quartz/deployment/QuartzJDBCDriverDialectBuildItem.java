b7c72bbfba3adcc9bb99c1f053ad8cdf875a9e65
Matej Novotny <manovotn@redhat.com>
2025-06-24 17:28:56 +0200

    Quartz - Defer driver discovery to runtime in order to provide more flexibility to users

--- extensions/quartz/deployment/src/main/java/io/quarkus/quartz/deployment/QuartzJDBCDriverDialectBuildItem.java
+++ extensions/quartz/deployment/src/main/java/io/quarkus/quartz/deployment/QuartzJDBCDriverDialectBuildItem.java
@@ -1,20 +1,31 @@
 package io.quarkus.quartz.deployment;
 
+import java.util.List;
 import java.util.Optional;
 
 import io.quarkus.builder.item.SimpleBuildItem;
+import io.quarkus.quartz.runtime.jdbc.JDBCDataSource;
 
 /**
- * Holds the SQL driver dialect {@link org.quartz.impl.jdbcjobstore.StdJDBCDelegate driver delegate} to use.
+ * Holds the necessary information to determine SQL driver dialect.
+ * <p>
+ * This can mean either a custom driver registered by user or one of Quarkus built-in drivers.
+ * If it is the latter, we defer discovering the driver to runtime, see also {@link io.quarkus.quartz.runtime.QuartzSupport}.
  */
 final class QuartzJDBCDriverDialectBuildItem extends SimpleBuildItem {
     private final Optional<String> driver;
+    private List<JDBCDataSource> dataSources;
 
-    public QuartzJDBCDriverDialectBuildItem(Optional<String> driver) {
+    public QuartzJDBCDriverDialectBuildItem(Optional<String> driver, List<JDBCDataSource> dataSources) {
         this.driver = driver;
+        this.dataSources = dataSources;
     }
 
     public Optional<String> getDriver() {
         return driver;
+    }
+
+    public List<JDBCDataSource> getDataSources() {
+        return dataSources;
     }
 }

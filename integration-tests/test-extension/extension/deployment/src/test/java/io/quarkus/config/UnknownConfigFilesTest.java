04dca86ba70261cc99531a0d850b65dc5682f6d0
Roberto Cortez <radcortez@yahoo.com>
2025-06-25 17:47:32 +0100

    Warn when a profile aware configuration file cannot be loaded due to the missing main configuration file

--- integration-tests/test-extension/extension/deployment/src/test/java/io/quarkus/config/UnknownConfigFilesTest.java
+++ integration-tests/test-extension/extension/deployment/src/test/java/io/quarkus/config/UnknownConfigFilesTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
-import java.util.stream.Collectors;
 
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.junit.jupiter.api.Test;
@@ -20,15 +19,21 @@ class UnknownConfigFilesTest {
             .withApplicationRoot((jar) -> jar
                     .addAsResource(EmptyAsset.INSTANCE, "application.properties")
                     .addAsResource(EmptyAsset.INSTANCE, "application-prod.properties")
-                    .addAsResource(EmptyAsset.INSTANCE, "application.yaml"))
+                    .addAsResource(EmptyAsset.INSTANCE, "application.yaml")
+                    .addAsResource(EmptyAsset.INSTANCE, "application-test.toml"))
             .setLogRecordPredicate(record -> record.getLevel().intValue() >= Level.WARNING.intValue())
             .assertLogRecords(logRecords -> {
                 List<LogRecord> unknownConfigFiles = logRecords.stream()
                         .filter(l -> l.getMessage().startsWith("Unrecognized configuration file"))
-                        .collect(Collectors.toList());
-
+                        .toList();
                 assertEquals(1, unknownConfigFiles.size());
                 assertTrue(unknownConfigFiles.get(0).getParameters()[0].toString().contains("application.yaml"));
+
+                List<LogRecord> profiledConfigFiles = logRecords.stream()
+                        .filter(l -> l.getMessage().startsWith("Profiled configuration file"))
+                        .toList();
+                assertEquals(1, profiledConfigFiles.size());
+                assertTrue(profiledConfigFiles.get(0).getParameters()[0].toString().contains("application-test.toml"));
             });
 
     @Test

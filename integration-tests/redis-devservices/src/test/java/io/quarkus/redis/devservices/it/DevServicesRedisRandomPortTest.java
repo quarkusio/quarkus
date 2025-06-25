3ae641d2924bf9c73673feeed6e6d867dab9a129
Ozan Gunalp <ozangunalp@gmail.com>
2025-06-25 13:47:50 +0200

    Add QuarkusIntegrationTests to redis-devservices IT
    Run new tests in native build

--- integration-tests/redis-devservices/src/test/java/io/quarkus/redis/devservices/it/DevServicesRedisRandomPortTest.java
+++ integration-tests/redis-devservices/src/test/java/io/quarkus/redis/devservices/it/DevServicesRedisRandomPortTest.java
@@ -1,13 +1,12 @@
 package io.quarkus.redis.devservices.it;
 
-import jakarta.inject.Inject;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.is;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.redis.datasource.RedisDataSource;
 import io.quarkus.redis.devservices.it.profiles.DevServicesRandomPortProfile;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
@@ -16,18 +15,19 @@ import io.quarkus.test.junit.TestProfile;
 @TestProfile(DevServicesRandomPortProfile.class)
 public class DevServicesRedisRandomPortTest {
 
-    @Inject
-    RedisDataSource redisClient;
-
     @BeforeEach
     public void setUp() {
-        redisClient.value(String.class).set("anykey", "anyvalue");
+        when().get("/set/anykey/anyvalue").then()
+                .statusCode(200)
+                .body(is("OK"));
     }
 
     @Test
     @DisplayName("given redis container must communicate with it and return value by key")
     public void shouldReturnAllKeys() {
-        Assertions.assertEquals("anyvalue", redisClient.value(String.class).get("anykey").toString());
+        when().get("/get/anykey").then()
+                .statusCode(200)
+                .body(is("anyvalue"));
     }
 
 }

06972501aa070ddaf5eda3357f34397ca272e923
jghagemann <hagejotage@hotmail.com>
2025-06-26 13:42:55 -0300

    Add javadoc to a few build items

--- core/deployment/src/main/java/io/quarkus/deployment/builditem/ApplicationClassNameBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/builditem/ApplicationClassNameBuildItem.java
@@ -2,6 +2,11 @@ package io.quarkus.deployment.builditem;
 
 import io.quarkus.builder.item.SimpleBuildItem;
 
+
+/**
+ * Returns the name of the application class of the Build Item generated in the Build Step
+ *
+ */
 public final class ApplicationClassNameBuildItem extends SimpleBuildItem {
     private final String className;
 

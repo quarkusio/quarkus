6f6cf8aa0f2c48b69686f92f2e342505e1394828
melloware <mellowaredev@gmail.com>
2025-06-29 11:23:31 -0400

    Liquibase Dev UI Card

--- extensions/liquibase/liquibase/deployment/src/main/java/io/quarkus/liquibase/deployment/devui/LiquibaseDevUIProcessor.java
+++ extensions/liquibase/liquibase/deployment/src/main/java/io/quarkus/liquibase/deployment/devui/LiquibaseDevUIProcessor.java
@@ -1,9 +1,5 @@
 package io.quarkus.liquibase.deployment.devui;
 
-import java.io.InputStream;
-import java.net.URL;
-import java.util.jar.Manifest;
-
 import io.quarkus.deployment.IsLocalDevelopment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
@@ -11,7 +7,6 @@ import io.quarkus.devui.spi.JsonRPCProvidersBuildItem;
 import io.quarkus.devui.spi.page.CardPageBuildItem;
 import io.quarkus.devui.spi.page.Page;
 import io.quarkus.liquibase.runtime.dev.ui.LiquibaseJsonRpcService;
-import liquibase.changelog.DatabaseChangeLog;
 
 /**
  * Dev UI card for displaying important details such as the library version.
@@ -23,14 +18,8 @@ public class LiquibaseDevUIProcessor {
         final CardPageBuildItem card = new CardPageBuildItem();
 
         // card
-        card.setCustomCard("qwc-liquibase-card.js");
-
-        // pages
-        card.addPage(Page.externalPageBuilder("Version")
-                .icon("font-awesome-solid:book")
-                .url("https://www.liquibase.org/")
-                .doNotEmbed()
-                .staticLabel(getManifest(DatabaseChangeLog.class).getMainAttributes().getValue("Bundle-Version")));
+        card.setLogo("liquibase_logo.svg", "liquibase_logo.svg");
+        card.addLibraryVersion("org.liquibase", "liquibase-core", "Liquibase", "https://www.liquibase.com/");
 
         card.addPage(Page.webComponentPageBuilder().title("Datasources")
                 .componentLink("qwc-liquibase-datasources.js")
@@ -43,17 +32,5 @@ public class LiquibaseDevUIProcessor {
     @BuildStep(onlyIf = IsLocalDevelopment.class)
     JsonRPCProvidersBuildItem registerJsonRpcBackend() {
         return new JsonRPCProvidersBuildItem(LiquibaseJsonRpcService.class);
-    }
-
-    private static Manifest getManifest(Class<?> clz) {
-        String resource = "/" + clz.getName().replace(".", "/") + ".class";
-        String fullPath = clz.getResource(resource).toString();
-        String archivePath = fullPath.substring(0, fullPath.length() - resource.length());
-
-        try (InputStream input = new URL(archivePath + "/META-INF/MANIFEST.MF").openStream()) {
-            return new Manifest(input);
-        } catch (Exception e) {
-            throw new RuntimeException("Loading MANIFEST for class " + clz + " failed!", e);
-        }
     }
 }

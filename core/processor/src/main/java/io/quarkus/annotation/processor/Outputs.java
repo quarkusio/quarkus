ea95c24c8736ba492260baf70960046a11c3af75
Guillaume Smet <guillaume.smet@gmail.com>
2025-06-30 19:05:44 +0200

    Config doc - Write the model as JSON in jars
    It comes with a version marker.

--- core/processor/src/main/java/io/quarkus/annotation/processor/Outputs.java
+++ core/processor/src/main/java/io/quarkus/annotation/processor/Outputs.java
@@ -17,8 +17,8 @@ public final class Outputs {
     public static final String META_INF_QUARKUS_CONFIG = "META-INF/" + QUARKUS_CONFIG_DOC;
     public static final String META_INF_QUARKUS_CONFIG_JAVADOC_JSON = META_INF_QUARKUS_CONFIG + "/quarkus-config-javadoc.json";
     public static final String META_INF_QUARKUS_CONFIG_MODEL_JSON = META_INF_QUARKUS_CONFIG + "/quarkus-config-model.json";
-    public static final String META_INF_QUARKUS_CONFIG_JAVADOC_YAML = META_INF_QUARKUS_CONFIG + "/quarkus-config-javadoc.yaml";
-    public static final String META_INF_QUARKUS_CONFIG_MODEL_YAML = META_INF_QUARKUS_CONFIG + "/quarkus-config-model.yaml";
+    public static final String META_INF_QUARKUS_CONFIG_MODEL_VERSION = META_INF_QUARKUS_CONFIG
+            + "/quarkus-config-model-version";
 
     /**
      * Ideally, we should remove this file at some point.

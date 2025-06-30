ac14b411335abb5e9dfe5e60258b420c5db240d0
Chihiro Ito <chiroito107@gmail.com>
2025-06-30 18:12:23 +0900

    Add functionality to capture Quarkus application runtime data using JFR extension

--- extensions/jfr/deployment/src/main/java/io/quarkus/jfr/deployment/JfrProcessor.java
+++ extensions/jfr/deployment/src/main/java/io/quarkus/jfr/deployment/JfrProcessor.java
@@ -1,11 +1,21 @@
 package io.quarkus.jfr.deployment;
 
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.builder.Version;
 import io.quarkus.deployment.Capabilities;
 import io.quarkus.deployment.Capability;
 import io.quarkus.deployment.Feature;
-import io.quarkus.deployment.annotations.*;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.BuildSteps;
+import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.ApplicationInfoBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
 import io.quarkus.jfr.runtime.JfrRecorder;
@@ -16,9 +26,13 @@ import io.quarkus.jfr.runtime.http.rest.classic.ClassicServerRecorderProducer;
 import io.quarkus.jfr.runtime.http.rest.reactive.ReactiveServerFilters;
 import io.quarkus.jfr.runtime.http.rest.reactive.ReactiveServerRecorderProducer;
 import io.quarkus.jfr.runtime.http.rest.reactive.ServerStartRecordingHandler;
+import io.quarkus.jfr.runtime.runtime.JfrRuntimeBean;
+import io.quarkus.jfr.runtime.runtime.QuarkusRuntimeInfo;
 import io.quarkus.resteasy.common.spi.ResteasyJaxrsProviderBuildItem;
 import io.quarkus.resteasy.reactive.server.spi.GlobalHandlerCustomizerBuildItem;
 import io.quarkus.resteasy.reactive.spi.CustomContainerRequestFilterBuildItem;
+import io.quarkus.runtime.ImageMode;
+import io.quarkus.runtime.configuration.ConfigUtils;
 
 @BuildSteps
 public class JfrProcessor {
@@ -26,6 +40,25 @@ public class JfrProcessor {
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(Feature.JFR);
+    }
+
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    void registerVersion(List<FeatureBuildItem> features, BuildProducer<AdditionalBeanBuildItem> additionalBeans,
+            BuildProducer<SyntheticBeanBuildItem> beanProduce, JfrRecorder recorder, ApplicationInfoBuildItem applicationInfo) {
+
+        List<String> featureNames = features.stream().map(f -> f.getName()).toList();
+        String quarkusVersion = Version.getVersion();
+
+        beanProduce.produce(SyntheticBeanBuildItem.configure(QuarkusRuntimeInfo.class)
+                .supplier(recorder.quarkusInfoSupplier(quarkusVersion, featureNames, ImageMode.current().name(),
+                        String.join(",", ConfigUtils.getProfiles())))
+                .scope(ApplicationScoped.class)
+                .setRuntimeInit().done());
+
+        additionalBeans.produce(AdditionalBeanBuildItem.builder().setUnremovable()
+                .addBeanClasses(JfrRuntimeBean.class)
+                .build());
     }
 
     @BuildStep
---
+++ extensions/jfr/deployment/src/test/java/io/quarkus/jfr/test/runtime/JfrRuntimeTest.java
@@ -0,0 +1,84 @@
+package io.quarkus.jfr.test.runtime;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import jakarta.inject.Inject;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.jfr.runtime.runtime.QuarkusRuntimeInfo;
+import io.quarkus.runtime.ApplicationConfig;
+import io.quarkus.test.QuarkusUnitTest;
+import jdk.jfr.Recording;
+import jdk.jfr.consumer.RecordedEvent;
+import jdk.jfr.consumer.RecordingFile;
+
+public class JfrRuntimeTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
+
+    @Inject
+    QuarkusRuntimeInfo quarkusRuntimeInfo;
+
+    @Inject
+    ApplicationConfig applicationConfig;
+
+    @Test
+    public void test() throws IOException {
+        final Path dumpPath = Path.of("./dump.jfr");
+        try {
+            try (Recording r = new Recording()) {
+                r.start();
+                r.stop();
+                r.dump(dumpPath);
+            } catch (Exception e) {
+                Assertions.fail(e);
+            }
+            List<RecordedEvent> recordedEvents = RecordingFile.readAllEvents(dumpPath);
+
+            testApplicationEvent(recordedEvents);
+            testRuntimeEvent(recordedEvents);
+            testExtensionEvent(recordedEvents);
+        } finally {
+            if (Files.exists(dumpPath)) {
+                Files.delete(dumpPath);
+            }
+        }
+    }
+
+    private void testApplicationEvent(List<RecordedEvent> recordedEvents) {
+        List<RecordedEvent> applicationEvents = recordedEvents.stream()
+                .filter(e -> e.getEventType().getName().equals("quarkus.application")).toList();
+        RecordedEvent applicationEvent = applicationEvents.get(0);
+        Assertions.assertTrue(applicationEvents.size() >= 1);
+        Assertions.assertEquals(applicationConfig.name().get(), applicationEvent.getString("name"));
+        Assertions.assertEquals(applicationConfig.version().get(), applicationEvent.getString("version"));
+    }
+
+    private void testRuntimeEvent(List<RecordedEvent> recordedEvents) {
+        List<RecordedEvent> runtimeEvents = recordedEvents.stream()
+                .filter(e -> e.getEventType().getName().equals("quarkus.runtime")).toList();
+        RecordedEvent runtimeEvent = runtimeEvents.get(0);
+        Assertions.assertTrue(runtimeEvents.size() >= 1);
+        Assertions.assertEquals(quarkusRuntimeInfo.imageMode(), runtimeEvent.getString("imageMode"));
+        Assertions.assertEquals(quarkusRuntimeInfo.version(), runtimeEvent.getString("version"));
+        Assertions.assertEquals(quarkusRuntimeInfo.profiles(), runtimeEvent.getString("profiles"));
+    }
+
+    private void testExtensionEvent(List<RecordedEvent> recordedEvents) {
+        List<RecordedEvent> extensionEvents = recordedEvents.stream()
+                .filter(e -> e.getEventType().getName().equals("quarkus.extension")).toList();
+        List<String> recordedExtensionNames = extensionEvents.stream().map(e -> e.getString("name")).distinct().toList();
+        Assertions.assertEquals(quarkusRuntimeInfo.features().size(), recordedExtensionNames.size());
+        Assertions.assertTrue(recordedExtensionNames.containsAll(quarkusRuntimeInfo.features()));
+    }
+}

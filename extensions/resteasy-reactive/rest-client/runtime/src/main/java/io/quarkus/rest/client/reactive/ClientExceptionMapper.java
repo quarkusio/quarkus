5444ba9f9f79a97070a5c48be3ff942bd649a610
Guillaume Smet <guillaume.smet@gmail.com>
2025-06-30 10:07:44 +0200

    Better document priority for ServerExceptionMapper
    And ClientExceptionMapper.
    
    Fixes #48662
    
    (cherry picked from commit 6d1a483122af6c9df4a34fea8badf55deeaf6b0e)

--- extensions/resteasy-reactive/rest-client/runtime/src/main/java/io/quarkus/rest/client/reactive/ClientExceptionMapper.java
+++ extensions/resteasy-reactive/rest-client/runtime/src/main/java/io/quarkus/rest/client/reactive/ClientExceptionMapper.java
@@ -52,7 +52,11 @@ import jakarta.ws.rs.Priorities;
 public @interface ClientExceptionMapper {
 
     /**
-     * The priority with which the exception mapper will be executed
+     * The priority with which the exception mapper will be executed.
+     * <p>
+     * They are sorted in ascending order; the lower the number the higher the priority.
+     *
+     * @see Priorities
      */
     int priority() default Priorities.USER;
 }

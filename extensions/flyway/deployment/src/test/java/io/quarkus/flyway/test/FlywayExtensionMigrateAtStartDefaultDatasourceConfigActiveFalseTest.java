26ff800198d84a64edb7a1af816e13c246d44069
Yoann Rodière <yoann@hibernate.org>
2025-07-03 13:29:52 +0200

    Disable Hibernate in tests that have it on the classpath, but don't need it and have an undefined default datasource
    Because this will cause issues once Hibernate gets started even if there
    no entities.

--- extensions/flyway/deployment/src/test/java/io/quarkus/flyway/test/FlywayExtensionMigrateAtStartDefaultDatasourceConfigActiveFalseTest.java
+++ extensions/flyway/deployment/src/test/java/io/quarkus/flyway/test/FlywayExtensionMigrateAtStartDefaultDatasourceConfigActiveFalseTest.java
@@ -20,7 +20,10 @@ public class FlywayExtensionMigrateAtStartDefaultDatasourceConfigActiveFalseTest
             .withApplicationRoot((jar) -> jar
                     .addAsResource("db/migration/V1.0.0__Quarkus.sql"))
             .overrideConfigKey("quarkus.datasource.active", "false")
-            .overrideConfigKey("quarkus.flyway.migrate-at-start", "true");
+            .overrideConfigKey("quarkus.flyway.migrate-at-start", "true")
+            // Hibernate ORM is in the test classpath for other tests, but it's not useful here and would fail due to missing configuration.
+            // So we just disable it.
+            .overrideConfigKey("quarkus.hibernate-orm.enabled", "false");
 
     @Inject
     Instance<Flyway> flyway;

0e4b85e2ee33f26eae70e5b6a8684e564cec4842
Yoann Rodière <yoann@hibernate.org>
2025-07-04 14:58:12 +0200

    Refactor tests around inactive datasources for Hibernate ORM/Reactive
    So that we can more easily see what the next commit changes.

--- extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/config/datasource/ConfigDefaultPUDatasourceUrlMissingDynamicInjectionTest.java
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/config/datasource/ConfigDefaultPUDatasourceUrlMissingDynamicInjectionTest.java
@@ -2,24 +2,29 @@ package io.quarkus.hibernate.orm.config.datasource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Assertions;
+import jakarta.inject.Inject;
+import jakarta.persistence.SchemaManager;
+
+import org.assertj.core.api.Assertions;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import io.quarkus.arc.InjectableInstance;
 import io.quarkus.hibernate.orm.config.MyEntity;
-import io.quarkus.runtime.configuration.ConfigurationException;
 import io.quarkus.test.QuarkusUnitTest;
 
-public class EntitiesInDefaultPUWithImplicitDatasourceConfigUrlMissingTest {
+public class ConfigDefaultPUDatasourceUrlMissingDynamicInjectionTest {
 
     @RegisterExtension
-    static QuarkusUnitTest runner = new QuarkusUnitTest()
-            .withApplicationRoot((jar) -> jar
-                    .addClass(MyEntity.class))
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withApplicationRoot(jar -> jar.addClass(MyEntity.class))
+            .withConfigurationResource("application.properties")
+            .overrideConfigKey("quarkus.datasource.jdbc.url", "")
             // The URL won't be missing if dev services are enabled
             .overrideConfigKey("quarkus.devservices.enabled", "false")
-            .assertException(t -> assertThat(t)
-                    .isInstanceOf(ConfigurationException.class)
+            .assertException(e -> assertThat(e)
                     .hasMessageContainingAll(
                             "Unable to find datasource '<default>' for persistence unit '<default>'",
                             "Datasource '<default>' was deactivated automatically because its URL is not set.",
@@ -27,10 +32,18 @@ public class EntitiesInDefaultPUWithImplicitDatasourceConfigUrlMissingTest {
                             "To activate the datasource, set configuration property 'quarkus.datasource.jdbc.url'",
                             "Refer to https://quarkus.io/guides/datasource for guidance."));
 
-    @Test
-    public void testInvalidConfiguration() {
-        // deployment exception should happen first
-        Assertions.fail();
-    }
+    @Inject
+    InjectableInstance<SessionFactory> sessionFactory;
 
+    @Inject
+    InjectableInstance<Session> session;
+
+    // Just try another bean type, but not all of them...
+    @Inject
+    InjectableInstance<SchemaManager> schemaManager;
+
+    @Test
+    public void test() {
+        Assertions.fail("Startup should have failed");
+    }
 }

b5bbb14d0db7759b46f95ebe81a4b5ba997a5594
Roberto Cortez <radcortez@yahoo.com>
2025-07-15 12:20:05 +0100

    Share RuntimeOverrideConfigSource between static init and runtime between tests

--- core/runtime/src/main/java/io/quarkus/runtime/configuration/RuntimeOverrideConfigSourceBuilder.java
+++ core/runtime/src/main/java/io/quarkus/runtime/configuration/RuntimeOverrideConfigSourceBuilder.java
@@ -1,10 +1,15 @@
 package io.quarkus.runtime.configuration;
 
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
 import io.smallrye.config.SmallRyeConfigBuilder;
 
 public class RuntimeOverrideConfigSourceBuilder implements ConfigBuilder {
+    private static final ConfigSource RUNTIME_OVERRIDE_CONFIG_SOURCE = new RuntimeOverrideConfigSource(
+            Thread.currentThread().getContextClassLoader());
+
     @Override
     public SmallRyeConfigBuilder configBuilder(final SmallRyeConfigBuilder builder) {
-        return builder.withSources(new RuntimeOverrideConfigSource(builder.getClassLoader()));
+        return builder.withSources(RUNTIME_OVERRIDE_CONFIG_SOURCE);
     }
 }

94954e9b056abd3bc426af07f746bf2608484a9a
Paulo Casaes <pcasaes@gmail.com>
2025-07-22 23:14:11 -0700

    Add Kotlin Coroutines CDI Context Propagation

---
+++ extensions/arc/deployment/src/test/kotlin/io/quarkus/arc/kotlin/RequestContextCoroutineContextTest.kt
@@ -0,0 +1,456 @@
+package io.quarkus.arc.kotlin
+
+import io.quarkus.arc.Arc
+import io.quarkus.test.QuarkusUnitTest
+import jakarta.enterprise.context.RequestScoped
+import jakarta.inject.Inject
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
+import org.jboss.shrinkwrap.api.spec.JavaArchive
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
+import org.junit.jupiter.api.extension.RegisterExtension
+
+class RequestContextCoroutineContextTest {
+
+    companion object {
+        @RegisterExtension
+        @JvmStatic
+        val TEST =
+            QuarkusUnitTest().withApplicationRoot { jar: JavaArchive ->
+                jar.addClasses(RequestData::class.java)
+            }
+    }
+
+    @Inject lateinit var requestData: RequestData
+
+    lateinit var expectedClassLoader: ClassLoader
+
+    @BeforeEach
+    @AfterEach
+    fun setUp() {
+        if (Arc.container().requestContext().isActive) {
+            Arc.container().requestContext().terminate()
+        }
+        this.expectedClassLoader = Thread.currentThread().contextClassLoader
+    }
+
+    private fun assertThatCallersClassLoaderIsExpected() {
+        Assertions.assertEquals(
+            expectedClassLoader,
+            Thread.currentThread().contextClassLoader,
+            "Thread context class loader should be the expected one",
+        )
+    }
+
+    @Test
+    fun `without an active request scope on withContext`() {
+        // GIVEN no active request context
+        Assertions.assertFalse(
+            Arc.container().requestContext().isActive,
+            "Request context should not be active",
+        )
+
+        // WHEN we run a block
+        runTest {
+            withPropagatedContext(Dispatchers.IO) {
+                // THEN the request context should not be active
+                Assertions.assertFalse(
+                    Arc.container().requestContext().isActive,
+                    "Request context should not be active",
+                )
+
+                assertThatCallersClassLoaderIsExpected()
+            }
+        }
+    }
+
+    @Test
+    fun `without an active request scope on async`() {
+        // GIVEN no active request context
+        Assertions.assertFalse(
+            Arc.container().requestContext().isActive,
+            "Request context should not be active",
+        )
+
+        // WHEN we run a block with async
+        runTest {
+            coroutineScope {
+                asyncWithPropagatedContext(Dispatchers.IO) {
+                        // THEN the request context should not be active
+                        Assertions.assertFalse(
+                            Arc.container().requestContext().isActive,
+                            "Request context should not be active",
+                        )
+
+                        assertThatCallersClassLoaderIsExpected()
+                    }
+                    .await()
+            }
+        }
+    }
+
+    @Test
+    fun `with an active request scope on withContext`() {
+        // GIVEN an active request context
+        Arc.container().requestContext().activate()
+        Assertions.assertTrue(
+            Arc.container().requestContext().isActive,
+            "Request context should be active",
+        )
+
+        // AND a given number and an expected post-async number
+        val givenNumber = 1234L
+        val expectedPostAsyncNumber = 5432L
+
+        // AND we set the number value in the request data
+        requestData.numberValue = givenNumber
+
+        // WHEN we run a block with the request context
+        runTest {
+            withPropagatedContext(Dispatchers.IO) {
+                // THEN the request context should be active
+                Assertions.assertTrue(
+                    Arc.container().requestContext().isActive,
+                    "Request context should be active",
+                )
+
+                // AND the number value should match the given number
+                Assertions.assertEquals(givenNumber, requestData.numberValue)
+
+                assertThatCallersClassLoaderIsExpected()
+
+                // AND the number value should match the given number after a short delay
+                delay(10)
+                Assertions.assertEquals(givenNumber, requestData.numberValue)
+
+                assertThatCallersClassLoaderIsExpected()
+
+                // WHEN we set the number value to the expected post-async number
+                requestData.numberValue = expectedPostAsyncNumber
+
+                // THEN the number value should match the expected post-async number after a short
+                // delay
+                delay(10)
+                Assertions.assertEquals(expectedPostAsyncNumber, requestData.numberValue)
+
+                assertThatCallersClassLoaderIsExpected()
+            }
+        }
+
+        // THEN the number value should match the expected post-async number after the block
+        // execution
+        Assertions.assertEquals(expectedPostAsyncNumber, requestData.numberValue)
+    }
+
+    @Test
+    fun `with an active request scope on async`() {
+        // GIVEN an active request context
+        Arc.container().requestContext().activate()
+        Assertions.assertTrue(
+            Arc.container().requestContext().isActive,
+            "Request context should be active",
+        )
+
+        // AND a given number and an expected post-async number
+        val givenNumber = 1234L
+        val expectedPostAsyncNumber = 5432L
+
+        // AND we set the number value in the request data
+        requestData.numberValue = givenNumber
+
+        // WHEN we run a block with async
+        runTest {
+            coroutineScope {
+                asyncWithPropagatedContext(Dispatchers.IO) {
+                        // THEN the request context should be active
+                        Assertions.assertTrue(
+                            Arc.container().requestContext().isActive,
+                            "Request context should be active",
+                        )
+
+                        // AND the number value should match the given number
+                        Assertions.assertEquals(givenNumber, requestData.numberValue)
+
+                        assertThatCallersClassLoaderIsExpected()
+
+                        // AND the number value should match the given number after a short delay
+                        delay(10)
+                        Assertions.assertEquals(givenNumber, requestData.numberValue)
+
+                        assertThatCallersClassLoaderIsExpected()
+
+                        // WHEN we set the number value to the expected post-async number
+                        requestData.numberValue = expectedPostAsyncNumber
+
+                        // THEN the number value should match the expected post-async number after a
+                        // short delay
+                        delay(10)
+                        Assertions.assertEquals(expectedPostAsyncNumber, requestData.numberValue)
+
+                        assertThatCallersClassLoaderIsExpected()
+                    }
+                    .await()
+            }
+        }
+
+        // THEN the number value should match the expected post-async number after the block
+        // execution
+        Assertions.assertEquals(expectedPostAsyncNumber, requestData.numberValue)
+    }
+
+    @Test
+    fun `with an active request scope on inner coroutine scope in async`() {
+        // GIVEN an active request context
+        Arc.container().requestContext().activate()
+        Assertions.assertTrue(
+            Arc.container().requestContext().isActive,
+            "Request context should be active",
+        )
+
+        // AND a given number and an expected post-async number
+        val givenNumber = 1234L
+        val expectedPostAsyncNumber = 5432L
+
+        // AND we set the number value in the request data
+        requestData.numberValue = givenNumber
+
+        // WHEN we run a block with async
+        runTest {
+            coroutineScope {
+                asyncWithPropagatedContext(Dispatchers.IO) {
+                        coroutineScope {
+                            // THEN the request context should be active
+                            Assertions.assertTrue(
+                                Arc.container().requestContext().isActive,
+                                "Request context should be active",
+                            )
+
+                            // AND the number value should match the given number
+                            Assertions.assertEquals(givenNumber, requestData.numberValue)
+
+                            assertThatCallersClassLoaderIsExpected()
+
+                            // AND the number value should match the given number after a short
+                            // delay
+                            delay(10)
+                            Assertions.assertEquals(givenNumber, requestData.numberValue)
+
+                            assertThatCallersClassLoaderIsExpected()
+
+                            // WHEN we set the number value to the expected post-async number
+                            requestData.numberValue = expectedPostAsyncNumber
+
+                            // THEN the number value should match the expected post-async number
+                            // after a short delay
+                            delay(10)
+                            Assertions.assertEquals(
+                                expectedPostAsyncNumber,
+                                requestData.numberValue,
+                            )
+
+                            assertThatCallersClassLoaderIsExpected()
+                        }
+                    }
+                    .await()
+            }
+        }
+
+        // THEN the number value should match the expected post-async number after the block
+        // execution
+        Assertions.assertEquals(expectedPostAsyncNumber, requestData.numberValue)
+    }
+
+    @Test
+    fun `with a terminated request scope while on async (undefined behavior)`() {
+        // GIVEN an active request context
+        Arc.container().requestContext().activate()
+        Assertions.assertTrue(
+            Arc.container().requestContext().isActive,
+            "Request context should be active",
+        )
+
+        // AND a given number
+        val givenNumber = 1234L
+
+        // AND we set the number value in the request data
+        requestData.numberValue = givenNumber
+
+        val asyncStarted = CompletableDeferred<Unit>()
+        val requestScopeTerminated = CompletableDeferred<Unit>()
+
+        // WHEN we run a block with async
+        runTest {
+            val job = launch {
+                asyncWithPropagatedContext(Dispatchers.IO) {
+                        // THEN the request context should be active
+                        Assertions.assertTrue(
+                            Arc.container().requestContext().isActive,
+                            "Request context should be active",
+                        )
+
+                        // AND the number value should match the given number
+                        Assertions.assertEquals(givenNumber, requestData.numberValue)
+
+                        assertThatCallersClassLoaderIsExpected()
+
+                        asyncStarted.complete(Unit)
+
+                        // WHEN we wait for the request context to be terminated by another thread
+                        requestScopeTerminated.await()
+
+                        // THEN the request context should not be active (undefined behavior)
+                        Assertions.assertFalse(
+                            Arc.container().requestContext().isActive,
+                            "Request context should not be active",
+                        )
+                    }
+                    .await()
+            }
+
+            launch {
+                asyncStarted.await()
+
+                Assertions.assertTrue(
+                    Arc.container().requestContext().isActive,
+                    "Request context should be active",
+                )
+                Assertions.assertEquals(givenNumber, requestData.numberValue)
+                Arc.container().requestContext().terminate()
+                Assertions.assertFalse(
+                    Arc.container().requestContext().isActive,
+                    "Request context should be active",
+                )
+
+                requestScopeTerminated.complete(Unit)
+            }
+
+            job.join()
+        }
+    }
+
+    @Test
+    fun `with two active request scope on async on same coroutine`() {
+        // GIVEN two active request contexts with different giveNumbers
+        Arc.container().requestContext().activate()
+
+        val firstRequestState = Arc.container().requestContext().stateIfActive
+        assertNotNull(firstRequestState)
+
+        val firstGivenNumber = 1234L
+        val expectedFirstGivenNumber = 91234L
+        requestData.numberValue = firstGivenNumber
+
+        Arc.container().requestContext().activate()
+
+        val secondRequestState = Arc.container().requestContext().stateIfActive
+        assertNotNull(secondRequestState)
+        Assertions.assertNotEquals(firstGivenNumber, requestData.numberValue)
+
+        val secondGivenNumber = 5432L
+        val expectedSecondGivenNumber = 95432L
+        requestData.numberValue = secondGivenNumber
+
+        Assertions.assertNotEquals(firstRequestState, secondRequestState)
+
+        val waitForFirstEnd = CompletableDeferred<Unit>()
+        val waitForSecondMiddle = CompletableDeferred<Unit>()
+        val waitForSecondEnd = CompletableDeferred<Unit>()
+
+        // WHEN we run a block with async
+        runTest {
+            val jobFirstRequest = launch {
+                Arc.container().requestContext().activate(firstRequestState)
+                asyncWithPropagatedContext(Dispatchers.IO) {
+                        // THEN the request context should be active
+                        Assertions.assertTrue(
+                            Arc.container().requestContext().isActive,
+                            "Request context should be active",
+                        )
+
+                        // AND the number value should match the given number
+                        Assertions.assertEquals(firstGivenNumber, requestData.numberValue)
+
+                        assertThatCallersClassLoaderIsExpected()
+
+                        waitForSecondMiddle.await()
+
+                        // THEN the request context should be active
+                        Assertions.assertTrue(
+                            Arc.container().requestContext().isActive,
+                            "Request context should be active",
+                        )
+
+                        // AND the number value should match the given number
+                        Assertions.assertEquals(firstGivenNumber, requestData.numberValue)
+
+                        assertThatCallersClassLoaderIsExpected()
+
+                        requestData.numberValue = expectedFirstGivenNumber
+
+                        waitForFirstEnd.complete(Unit)
+                    }
+                    .await()
+            }
+
+            val jobSecondRequest = launch {
+                Arc.container().requestContext().activate(secondRequestState)
+                asyncWithPropagatedContext(Dispatchers.IO) {
+                        // THEN the request context should be active
+                        Assertions.assertTrue(
+                            Arc.container().requestContext().isActive,
+                            "Request context should be active",
+                        )
+
+                        // AND the number value should match the given number
+                        Assertions.assertEquals(secondGivenNumber, requestData.numberValue)
+
+                        assertThatCallersClassLoaderIsExpected()
+
+                        waitForSecondMiddle.complete(Unit)
+
+                        waitForFirstEnd.await()
+
+                        // THEN the request context should be active
+                        Assertions.assertTrue(
+                            Arc.container().requestContext().isActive,
+                            "Request context should be active",
+                        )
+
+                        // AND the number value should match the given number
+                        Assertions.assertEquals(secondGivenNumber, requestData.numberValue)
+
+                        assertThatCallersClassLoaderIsExpected()
+
+                        requestData.numberValue = expectedSecondGivenNumber
+
+                        waitForSecondEnd.complete(Unit)
+                    }
+                    .await()
+            }
+
+            jobSecondRequest.join()
+            jobFirstRequest.join()
+        }
+
+        Arc.container().requestContext().activate(secondRequestState)
+        Assertions.assertEquals(expectedSecondGivenNumber, requestData.numberValue)
+        Arc.container().requestContext().terminate()
+
+        Arc.container().requestContext().activate(firstRequestState)
+        Assertions.assertEquals(expectedFirstGivenNumber, requestData.numberValue)
+        Arc.container().requestContext().terminate()
+    }
+
+    @RequestScoped
+    class RequestData {
+        var numberValue = 0L
+    }
+}
---
+++ extensions/arc/kotlin/src/main/kotlin/io/quarkus/arc/kotlin/RequestContextCoroutineContext.kt
@@ -0,0 +1,149 @@
+package io.quarkus.arc.kotlin
+
+import io.quarkus.arc.Arc
+import io.quarkus.arc.InjectableContext
+import io.quarkus.arc.ManagedContext
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.ThreadContextElement
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+
+/**
+ * A suspending function that executes a block of code within the Quarkus Request Context.
+ *
+ * This function captures the current request context and ensures it is activated when the coroutine
+ * resumes on a thread.
+ *
+ * If the request context is finalized before the block completes, it results in undefined behavior.
+ *
+ * Will not start a request context if there is none active at the time of invocation.
+ *
+ * @param context The CoroutineContext to use for the coroutine.
+ * @param block The block of code to execute within the request context.
+ * @return The result of the block execution.
+ */
+suspend fun <T> withPropagatedContext(
+    context: CoroutineContext,
+    block: suspend CoroutineScope.() -> T,
+): T {
+    return withContext(context = context.appendRequestContextToCoroutineContext(), block = block)
+}
+
+/**
+ * An async function that executes a block of code within the Quarkus Request Context.
+ *
+ * This function captures the current request context and ensures it is activated when the coroutine
+ * resumes on a thread.
+ *
+ * If the caller finalizes the request context before the block is executed, results in undefined
+ * behavior.
+ *
+ * Will not start a request context if there is none active at the time of invocation.
+ *
+ * @param context The CoroutineContext to use for the coroutine.
+ * @param block The block of code to execute within the request context.
+ */
+fun <T> CoroutineScope.asyncWithPropagatedContext(
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> T,
+): Deferred<T> {
+    return async(
+        context = context.appendRequestContextToCoroutineContext(),
+        start = start,
+        block = block,
+    )
+}
+
+fun CoroutineContext.appendRequestContextToCoroutineContext(): CoroutineContext {
+    val requestContext: ManagedContext? = Arc.container()?.requestContext()
+    return if (requestContext == null) {
+        this
+    } else {
+        this + RequestContextCoroutineContext(requestContext = requestContext)
+    }
+}
+
+/**
+ * A CoroutineContext.Element to propagate the Quarkus Request Context.
+ *
+ * This element captures the active request context when a coroutine is launched and ensures it is
+ * activated whenever the coroutine resumes on a thread.
+ *
+ * @param requestContext The Quarkus ManagedContext for the request scope.
+ */
+class RequestContextCoroutineContext(private val requestContext: ManagedContext) :
+    ThreadContextElement<RequestContextCoroutineContext.ContextSnapshot> {
+
+    private val state: InjectableContext.ContextState? = requestContext.stateIfActive
+    private val classLoader: ClassLoader = Thread.currentThread().contextClassLoader
+
+    fun InjectableContext.ContextState?.isNullOrInvalid(): Boolean {
+        return this == null || !this.isValid
+    }
+
+    /** A companion object to act as the Key for this context element. */
+    companion object Key : CoroutineContext.Key<RequestContextCoroutineContext>
+
+    /** The key that identifies this element in a CoroutineContext. */
+    override val key: CoroutineContext.Key<*>
+        get() = Key
+
+    /**
+     * This function is invoked when the coroutine resumes execution on a thread. It activates the
+     * captured request context.
+     *
+     * @param context The coroutine context.
+     * @return The state of the request context *before* this element activated its captured state.
+     *   This is used by `restoreThreadContext` to correctly reset the context later.
+     */
+    override fun updateThreadContext(context: CoroutineContext): ContextSnapshot {
+        // Capture the state of the current thread's context before we change it.
+        val oldState = requestContext.stateIfActive
+
+        val oldClassLoader = Thread.currentThread().contextClassLoader
+
+        Thread.currentThread().contextClassLoader = classLoader
+
+        // If the coroutine was launched from a thread without an active request context,
+        // we should deactivate any context that might be active on the current thread.
+        if (state.isNullOrInvalid()) {
+            requestContext.deactivate()
+        } else {
+            // Activate the request context that we captured when the coroutine was created.
+            requestContext.activate(state)
+        }
+
+        return ContextSnapshot(oldState, oldClassLoader)
+    }
+
+    /**
+     * This function is invoked when the coroutine suspends or completes. It restores the request
+     * context of the thread to its original state.
+     *
+     * @param context The coroutine context.
+     * @param oldState The state that was returned by `updateThreadContext`.
+     */
+    override fun restoreThreadContext(context: CoroutineContext, oldState: ContextSnapshot) {
+
+        Thread.currentThread().contextClassLoader = oldState.classLoader
+
+        // We must restore the request context on the thread to whatever it was before
+        // this coroutine resumed.
+        val oldContext = oldState.contextState
+        if (oldContext.isNullOrInvalid()) {
+            requestContext.deactivate()
+        } else {
+            requestContext.activate(oldContext)
+        }
+    }
+
+    data class ContextSnapshot(
+        val contextState: InjectableContext.ContextState? = null,
+        val classLoader: ClassLoader,
+    )
+}

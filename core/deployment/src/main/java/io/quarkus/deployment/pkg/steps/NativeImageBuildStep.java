ff60a93ff9c37f56572a8ae37ad8d536040335fe
Robert Toyonaga <rtoyonag@redhat.com>
2025-08-07 12:46:17 -0400

    more formatting style

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/NativeImageBuildStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/steps/NativeImageBuildStep.java
@@ -984,9 +984,11 @@ public class NativeImageBuildStep {
                     }
 
                     if (graalVMVersion.compareTo(GraalVM.Version.VERSION_24_2_0) >= 0) {
-                        /* After GraalVM/Mandrel 24.2, JCMD becomes available. The Quarkus thread dumper handles
-                        SIGQUIT which interferes with JCMD. To avoid this problem, Quarkus must use the GraalVM
-                        built-in thread dumper instead for versions beyond 24.2. */
+                        /*
+                         * After GraalVM/Mandrel 24.2, JCMD becomes available. The Quarkus thread dumper handles
+                         * SIGQUIT which interferes with JCMD. To avoid this problem, Quarkus must use the GraalVM
+                         * built-in thread dumper instead for versions beyond 24.2.
+                         */
                         monitoringOptions.add(NativeConfig.MonitoringOption.THREADDUMP);
                     }
                 }

ced36c72fc7b7e50f8b29f3e99508d06eb2e7224
Michal Vavřík <mvavrik@redhat.com>
2025-08-20 22:13:30 +0200

    fix(cors): Dont recreate CORS config in DEV UI CORS filter

--- extensions/devui/runtime/src/main/java/io/quarkus/devui/runtime/DevUICORSFilter.java
+++ extensions/devui/runtime/src/main/java/io/quarkus/devui/runtime/DevUICORSFilter.java
@@ -1,6 +1,10 @@
 package io.quarkus.devui.runtime;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -34,25 +38,22 @@ public class DevUICORSFilter implements Handler<RoutingContext> {
 
     private final List<String> hosts;
     private final List<Pattern> hostsPatterns;
+    private volatile CORSConfig baseCorsConfig;
 
     public DevUICORSFilter(List<String> hosts) {
         this.hosts = hosts;
         this.hostsPatterns = DevUIFilterHelper.detectPatterns(this.hosts);
+        this.baseCorsConfig = null;
     }
 
-    private static CORSFilter corsFilter(String allowedHost) {
-        int httpPort = ConfigProvider.getConfig().getValue(HTTP_PORT_CONFIG_PROP, int.class);
-        int httpsPort = ConfigProvider.getConfig().getValue(HTTPS_PORT_CONFIG_PROP, int.class);
-        final var corsBuilder = CORS.origins(Set.of(
-                HTTP_LOCAL_HOST + ":" + httpPort,
-                HTTPS_LOCAL_HOST + ":" + httpsPort,
-                HTTP_LOCAL_HOST_IP + ":" + httpPort,
-                HTTPS_LOCAL_HOST_IP + ":" + httpsPort));
-        if (allowedHost != null) {
-            corsBuilder.origin(allowedHost);
+    private CORSFilter corsFilter(String allowedHost) {
+        final CORSConfig corsConfig;
+        if (allowedHost == null) {
+            corsConfig = getBaseCorsConfig();
+        } else {
+            corsConfig = createCorsConfig(allowedHost);
         }
-        CORSConfig config = (CORSConfig) corsBuilder.build();
-        return new CORSFilter(config);
+        return new CORSFilter(corsConfig);
     }
 
     @Override
@@ -102,5 +103,62 @@ public class DevUICORSFilter implements Handler<RoutingContext> {
             }
         }
         return false;
+    }
+
+    private CORSConfig getBaseCorsConfig() {
+        if (baseCorsConfig == null) {
+            int httpPort = ConfigProvider.getConfig().getValue(HTTP_PORT_CONFIG_PROP, int.class);
+            int httpsPort = ConfigProvider.getConfig().getValue(HTTPS_PORT_CONFIG_PROP, int.class);
+            baseCorsConfig = (CORSConfig) CORS.origins(Set.of(
+                    HTTP_LOCAL_HOST + ":" + httpPort,
+                    HTTPS_LOCAL_HOST + ":" + httpsPort,
+                    HTTP_LOCAL_HOST_IP + ":" + httpPort,
+                    HTTPS_LOCAL_HOST_IP + ":" + httpsPort)).build();
+        }
+        return baseCorsConfig;
+    }
+
+    private CORSConfig createCorsConfig(String allowedHost) {
+        CORSConfig baseCorsconfig = getBaseCorsConfig();
+        List<String> listOfOrigins = new ArrayList<>(baseCorsconfig.origins().get());
+        listOfOrigins.add(allowedHost);
+        Optional<List<String>> origins = Optional.of(Collections.unmodifiableList(listOfOrigins));
+        return new CORSConfig() {
+
+            @Override
+            public boolean enabled() {
+                return true;
+            }
+
+            @Override
+            public Optional<List<String>> origins() {
+                return origins;
+            }
+
+            @Override
+            public Optional<List<String>> methods() {
+                return baseCorsconfig.methods();
+            }
+
+            @Override
+            public Optional<List<String>> headers() {
+                return baseCorsConfig.headers();
+            }
+
+            @Override
+            public Optional<List<String>> exposedHeaders() {
+                return baseCorsConfig.exposedHeaders();
+            }
+
+            @Override
+            public Optional<Duration> accessControlMaxAge() {
+                return baseCorsConfig.accessControlMaxAge();
+            }
+
+            @Override
+            public Optional<Boolean> accessControlAllowCredentials() {
+                return baseCorsConfig.accessControlAllowCredentials();
+            }
+        };
     }
 }

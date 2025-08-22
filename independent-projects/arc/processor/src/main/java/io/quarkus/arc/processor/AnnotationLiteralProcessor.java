756793467ce4dc820331e39e2492b9d9b22d143c
Guillaume Smet <guillaume.smet@gmail.com>
2025-08-22 11:02:11 +0200

    Avoid a useless AnnotationInstance#toString()
    It actually generates quite a lot of allocations when we stress ArC with
    the crazybeans benchmark.

--- independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/AnnotationLiteralProcessor.java
+++ independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/AnnotationLiteralProcessor.java
@@ -82,8 +82,10 @@ public class AnnotationLiteralProcessor {
         if (!annotationInstance.runtimeVisible()) {
             throw new IllegalArgumentException("Annotation does not have @Retention(RUNTIME): " + annotationInstance);
         }
+        if (annotationClass == null) {
+            throw new IllegalArgumentException("Annotation class not available: " + annotationInstance);
+        }
 
-        Objects.requireNonNull(annotationClass, "Annotation class not available: " + annotationInstance);
         AnnotationLiteralClassInfo literal = cache.getValue(new CacheKey(annotationClass));
 
         if (literal.annotationMembers().isEmpty()) {

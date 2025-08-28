a29a09b5a98e3758778f990ac5d4cfcf751d4927
Alexey Loubyansky <olubyans@redhat.com>
2025-08-28 14:47:32 +0200

    Make sure extension runtime artifacts are not flagged as reloadable in Gradle projects
    (cherry picked from commit 1b0b8dd3df84813df96ec0f469d9f8c722189cc9)

---
+++ integration-tests/gradle/src/main/resources/multi-composite-build-extensions-project/application/src/test/java/org/acme/quarkus/sample/GreetingResourceTest.java
@@ -0,0 +1,20 @@
+package org.acme.quarkus.sample;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class GreetingResourceTest {
+    @Test
+    void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("hello from LibB and LibA extension enabled: false"));
+    }
+
+}

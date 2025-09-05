fef34a790f12c0fe3d54c66aa407667ebe5ded61
Yoann Rodière <yoann@hibernate.org>
2025-09-05 14:09:39 +0200

    Test no-entity use case for Hibernate ORM with Jakarta Data

--- integration-tests/hibernate-orm-data/src/test/java/io/quarkus/it/hibernate/processor/data/HibernateOrmDataTest.java
+++ integration-tests/hibernate-orm-data/src/test/java/io/quarkus/it/hibernate/processor/data/HibernateOrmDataTest.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,7 +17,7 @@ public class HibernateOrmDataTest {
 
     @ValueSource(strings = { "/data", "/data/other" })
     @ParameterizedTest
-    public void repositoryDefaultPu(String root) {
+    public void testEntities(String root) {
         // Create/retrieve
         given()
                 .pathParam("name", "foo")
@@ -83,5 +84,29 @@ public class HibernateOrmDataTest {
                 .when().get(root + "/by/name/{name}")
                 .then()
                 .statusCode(404);
+    }
+
+    @Test
+    public void testSqlOnly() {
+        given()
+                .pathParam("name", "admin")
+                .contentType(ContentType.JSON)
+                .when().get("/data/sqlonly/myuser/by/username/{name}")
+                .then()
+                .statusCode(404);
+        given()
+                .pathParam("id", 42)
+                .contentType(ContentType.JSON)
+                .body("{\"username\":\"admin\",\"role\":\"admin\"}")
+                .when().put("/data/sqlonly/myuser/{id}")
+                .then()
+                .statusCode(204);
+        given()
+                .pathParam("name", "admin")
+                .contentType(ContentType.JSON)
+                .when().get("/data/sqlonly/myuser/by/username/{name}")
+                .then()
+                .statusCode(200)
+                .body(equalTo("{\"id\":42,\"username\":\"admin\",\"role\":\"admin\"}"));
     }
 }

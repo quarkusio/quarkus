22c760e9c95f1a53d99c86f11c728a02021bfc89
Phillip Kruger <phillip.kruger@gmail.com>
2025-09-15 12:18:11 +1000

    Dev UI Agroal: Fix jdbc url parsing for url with ; in it
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- extensions/agroal/runtime-dev/src/main/java/io/quarkus/agroal/runtime/dev/ui/DatabaseInspector.java
+++ extensions/agroal/runtime-dev/src/main/java/io/quarkus/agroal/runtime/dev/ui/DatabaseInspector.java
@@ -453,6 +453,9 @@ public final class DatabaseInspector {
             return true;
         }
 
+        if (ads == null)
+            return false;
+
         try {
             AgroalDataSourceConfiguration configuration = ads.getConfiguration();
             String jdbcUrl = configuration.connectionPoolConfiguration().connectionFactoryConfiguration().jdbcUrl();
@@ -464,7 +467,7 @@ public final class DatabaseInspector {
                 return true;
             }
 
-            String cleanUrl = jdbcUrl.replace("jdbc:", "");
+            String cleanUrl = jdbcUrl.replace("jdbc:", "").replaceFirst(";", "?").replace(";", "&");
             URI uri = new URI(cleanUrl);
 
             String host = uri.getHost();

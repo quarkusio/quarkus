9ff1ec103a36e6eaca1565584256e1df10626d9e
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-09-21 19:10:19 -0300

    Add test using binder-enabled-default as false

---
+++ extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/deployment/MicrometerBinderEnableAllPrecedenceOverBinderEnabledDefaultTest.java
@@ -0,0 +1,34 @@
+package io.quarkus.micrometer.deployment;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.quarkus.micrometer.test.Util;
+import io.quarkus.test.QuarkusUnitTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+public class MicrometerBinderEnableAllPrecedenceOverBinderEnabledDefaultTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withConfigurationResource("test-logging.properties")
+            .overrideConfigKey("quarkus.micrometer.binder.enable-all", "true")
+            .overrideConfigKey("quarkus.micrometer.binder.jvm", "false")
+            .overrideConfigKey("quarkus.micrometer.binder-enabled-default", "false")
+            .withApplicationRoot((jar) -> jar
+                    .addClasses(Util.class));
+
+    @Inject
+    MeterRegistry registry;
+
+    @Test
+    @Disabled("Should have any registered MeterRegistry objects when binder-enabled-default is disabled and enabled-all is enabled")
+    public void testMeterRegistryPresent() {
+        Assertions.assertNotNull(registry, "A registry should be configured");
+
+        Assertions.assertNotNull(registry.find("jvm.info").counter(),
+                "JVM Info counter should be present, found: " + Util.listMeters(registry, "jvm.info"));
+    }
+}

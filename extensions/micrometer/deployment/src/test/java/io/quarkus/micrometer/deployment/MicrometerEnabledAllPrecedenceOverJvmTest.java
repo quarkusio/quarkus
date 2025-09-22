b670b0053a59249d8a0184b938ba1169f49be945
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2025-09-21 23:44:47 -0300

    Update MicrometerEnabledAllPrecedenceOverJvmTest.java

--- extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/deployment/MicrometerEnabledAllPrecedenceOverJvmTest.java
+++ extensions/micrometer/deployment/src/test/java/io/quarkus/micrometer/deployment/MicrometerEnabledAllPrecedenceOverJvmTest.java
@@ -18,7 +18,7 @@ public class MicrometerEnabledAllPrecedenceOverJvmTest {
             .withConfigurationResource("test-logging.properties")
             .overrideConfigKey("quarkus.micrometer.binder.enable-all", "true")
             .overrideConfigKey("quarkus.micrometer.binder.jvm", "false")
-            .overrideRuntimeConfigKey("quarkus.redis.devservices.enabled", "false")
+            .overrideConfigKey("quarkus.redis.devservices.enabled", "false")
             .withApplicationRoot((jar) -> jar
                     .addClasses(Util.class));
 

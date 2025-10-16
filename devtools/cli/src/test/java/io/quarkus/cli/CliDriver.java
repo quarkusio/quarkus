b95ccbc90e2d889e4a4605e3f9ac477c012587c0
David M. Lloyd <dmlloyd@ibm.com>
2025-10-16 07:31:16 -0500

    Reapply "Convert `io.quarkus.cli.CliDriver` to new process API"
    This reverts commit 6d593cc8a997b01cd0008ee6d08657016cd6585e.

--- devtools/cli/src/test/java/io/quarkus/cli/CliDriver.java
+++ devtools/cli/src/test/java/io/quarkus/cli/CliDriver.java
@@ -9,6 +9,7 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -33,7 +34,7 @@ public class CliDriver {
     public static class CliDriverBuilder {
 
         private Path startingDir;
-        private List<String> args = new ArrayList<>();
+        private final List<String> args = new ArrayList<>();
         private String mavenLocalRepo;
         private String mavenSettings;
 
@@ -46,9 +47,7 @@ public class CliDriver {
         }
 
         public CliDriverBuilder addArgs(String... args) {
-            for (String s : args) {
-                this.args.add(s);
-            }
+            Collections.addAll(this.args, args);
             return this;
         }
 
@@ -151,34 +150,26 @@ public class CliDriver {
         getMavenSettingsProperty().map(SETTINGS_ARG_FORMATTER).ifPresent(args::add);
     }
 
-    public static Result executeArbitraryCommand(Path startingDir, String... args) throws Exception {
+    public static Result executeArbitraryCommand(Path startingDir, String... args) {
         System.out.println("$ " + String.join(" ", args));
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintStream outPs = new PrintStream(out);
-        System.setOut(outPs);
-
-        ByteArrayOutputStream err = new ByteArrayOutputStream();
-        PrintStream errPs = new PrintStream(err);
-        System.setErr(errPs);
-
+        StringBuilder errorBuilder = new StringBuilder();
         Result result = new Result();
-        try {
-            ProcessBuilder pb = new ProcessBuilder(args);
-            pb.directory(startingDir.toFile());
-            pb.redirectError(ProcessBuilder.Redirect.INHERIT);
-            pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
 
-            Process p = pb.start();
-            p.waitFor();
-            outPs.flush();
-            errPs.flush();
-        } finally {
-            System.setOut(stdout);
-            System.setErr(stderr);
-        }
-        result.stdout = out.toString();
-        result.stderr = err.toString();
+        String output = io.smallrye.common.process.ProcessBuilder.newBuilder(Path.of(args[0]))
+                .arguments(Arrays.copyOfRange(args, 1, args.length))
+                .exitCodeChecker(ec -> {
+                    result.exitCode = ec;
+                    return true;
+                })
+                .directory(startingDir)
+                .output().toSingleString(65536)
+                .error().consumeLinesWith(65536, line -> errorBuilder.append(line).append(System.lineSeparator()))
+                .run();
+        String error = errorBuilder.toString();
+
+        result.stdout = output;
+        result.stderr = error;
         return result;
     }
 

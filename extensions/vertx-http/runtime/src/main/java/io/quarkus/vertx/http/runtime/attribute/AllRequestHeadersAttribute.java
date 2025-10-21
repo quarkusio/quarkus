96d27d7eb7b4d800c8a4fb2c07a2cfa28d0c7ab8
Sergey Beryozkin <sberyozkin@gmail.com>
2025-10-21 18:41:35 +0100

    Mask some HTTP headers in the HTTP access log
    (cherry picked from commit 1f1f4d1e3323f264a78ab5cd9f3a18b9d4122b3a)

--- extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/attribute/AllRequestHeadersAttribute.java
+++ extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/attribute/AllRequestHeadersAttribute.java
@@ -1,34 +1,106 @@
 package io.quarkus.vertx.http.runtime.attribute;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
+import org.eclipse.microprofile.config.ConfigProvider;
+
+import io.quarkus.vertx.http.runtime.AccessLogConfig;
+import io.quarkus.vertx.http.runtime.VertxHttpConfig;
+import io.smallrye.config.SmallRyeConfig;
 import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.ext.web.RoutingContext;
 
 public class AllRequestHeadersAttribute implements ExchangeAttribute {
 
-    public static final AllRequestHeadersAttribute INSTANCE = new AllRequestHeadersAttribute();
+    private static final String AUTHORIZATION_HEADER = String.valueOf(HttpHeaders.AUTHORIZATION).toLowerCase();
+    private static final String COOKIE_HEADER = String.valueOf(HttpHeaders.COOKIE).toLowerCase();
 
-    private AllRequestHeadersAttribute() {
+    private final Set<String> maskedHeaders;
+    private final Set<String> maskedCookies;
 
+    AllRequestHeadersAttribute() {
+        this(Set.of(), Set.of());
+    }
+
+    AllRequestHeadersAttribute(AccessLogConfig config) {
+        this(config.maskedHeaders().orElse(Set.of()), config.maskedCookies().orElse(Set.of()));
+    }
+
+    AllRequestHeadersAttribute(Set<String> maskedHeaders, Set<String> maskedCookies) {
+        this.maskedHeaders = toLowerCaseStringSet(maskedHeaders);
+        this.maskedCookies = toLowerCaseStringSet(maskedCookies);
+    }
+
+    private static Set<String> toLowerCaseStringSet(Set<String> set) {
+        return set.stream().map(String::toLowerCase).collect(Collectors.toSet());
     }
 
     @Override
     public String readAttribute(RoutingContext exchange) {
-        final MultiMap headers = exchange.request().headers();
+        return readAttribute(exchange.request().headers());
+    }
 
+    String readAttribute(MultiMap headers) {
         if (headers.isEmpty()) {
             return null;
         } else {
             final StringJoiner joiner = new StringJoiner(System.lineSeparator());
 
             for (Map.Entry<String, String> header : headers) {
-                joiner.add(header.getKey() + ": " + header.getValue());
+                joiner.add(header.getKey() + ": " + maskHeaderValue(header.getKey(), header.getValue()));
             }
 
             return joiner.toString();
         }
+    }
+
+    String maskHeaderValue(String headerName, String headerValue) {
+        if (headerValue == null) {
+            return null;
+        }
+
+        String headerNameLowerCase = headerName.toLowerCase();
+
+        if (AUTHORIZATION_HEADER.equals(headerNameLowerCase)) {
+            return maskAuthorizationHeaderValue(headerValue);
+        }
+
+        if (COOKIE_HEADER.equals(headerNameLowerCase)) {
+            return maskCookieHeaderValue(headerValue);
+        }
+
+        if (maskedHeaders.contains(headerNameLowerCase)) {
+            return "...";
+        }
+
+        return headerValue;
+    }
+
+    private String maskAuthorizationHeaderValue(String headerValue) {
+        int idx = headerValue.indexOf(' ');
+        final String scheme = idx > 0 ? headerValue.substring(0, idx) : null;
+
+        if (scheme != null) {
+            return scheme + " ...";
+        } else {
+            return "...";
+        }
+    }
+
+    private String maskCookieHeaderValue(String headerValue) {
+        int idx = headerValue.indexOf('=');
+
+        final String cookieName = idx > 0 ? headerValue.substring(0, idx) : null;
+
+        if (cookieName != null && maskedCookies.contains(cookieName.toLowerCase())) {
+            return cookieName + "=...";
+        }
+
+        return headerValue;
     }
 
     @Override
@@ -46,7 +118,7 @@ public class AllRequestHeadersAttribute implements ExchangeAttribute {
         @Override
         public ExchangeAttribute build(final String token) {
             if (token.equals("%{ALL_REQUEST_HEADERS}")) {
-                return INSTANCE;
+                return new AllRequestHeadersAttribute(getConfigMapping());
             }
             return null;
         }
@@ -55,6 +127,12 @@ public class AllRequestHeadersAttribute implements ExchangeAttribute {
         public int priority() {
             return 0;
         }
+
+        private static AccessLogConfig getConfigMapping() {
+            SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
+            return config.getConfigMapping(VertxHttpConfig.class).accessLog();
+        }
+
     }
 
 }

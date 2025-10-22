03f14d95721fddf83471fd36b3486aa3d6e7ab14
Guillaume Smet <guillaume.smet@gmail.com>
2025-10-22 12:58:38 +0200

    Bring back the original implementation of getting version in CLI
    I think the main issue that Ioannis tried to work around was an artifact
    missing.
    This should handle the problem properly.

--- devtools/cli-common/src/main/java/io/quarkus/cli/common/VersionHelper.java
+++ devtools/cli-common/src/main/java/io/quarkus/cli/common/VersionHelper.java
@@ -1,10 +1,12 @@
 package io.quarkus.cli.common;
 
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Properties;
+
+import io.quarkus.runtime.util.ClassPathUtils;
 
 /**
  * Helper class to get client version without circular dependencies
@@ -20,24 +22,31 @@ public class VersionHelper {
         final Properties props = new Properties();
         final URL quarkusPropertiesUrl = Thread.currentThread().getContextClassLoader().getResource("quarkus.properties");
         if (quarkusPropertiesUrl == null) {
-            return "999-SNAPSHOT"; // fallback version
+            throw new RuntimeException("Failed to locate quarkus.properties on the classpath");
         }
 
-        try {
-            // Handle file and jar URLs differently to avoid file locks on Windows
-            if (quarkusPropertiesUrl.getProtocol().equals("file")) {
-                try (InputStream is = Files.newInputStream(Paths.get(quarkusPropertiesUrl.toURI()))) {
-                    props.load(is);
+        // we have a special case for file and jar as using getResourceAsStream() on Windows might cause file locks
+        if ("file".equals(quarkusPropertiesUrl.getProtocol()) || "jar".equals(quarkusPropertiesUrl.getProtocol())) {
+            ClassPathUtils.consumeAsPath(quarkusPropertiesUrl, p -> {
+                try (BufferedReader reader = Files.newBufferedReader(p)) {
+                    props.load(reader);
+                } catch (IOException e) {
+                    throw new RuntimeException("Failed to load quarkus.properties", e);
                 }
-            } else {
-                try (InputStream is = quarkusPropertiesUrl.openStream()) {
-                    props.load(is);
-                }
+            });
+        } else {
+            try {
+                props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("quarkus.properties"));
+            } catch (IOException e) {
+                throw new IllegalStateException("Failed to load quarkus.properties", e);
             }
-            version = props.getProperty("quarkus-core-version", "999-SNAPSHOT");
-        } catch (Exception e) {
-            version = "999-SNAPSHOT"; // fallback version
         }
+
+        version = props.getProperty("quarkus-core-version");
+        if (version == null) {
+            throw new RuntimeException("Failed to locate quarkus-core-version property in the bundled quarkus.properties");
+        }
+
         return version;
     }
 }

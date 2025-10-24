716625787982b41a46379c1345f9434e93a9f453
Alexey Loubyansky <olubyans@redhat.com>
2025-10-24 10:30:02 +0200

    Make sure local quarkus-versions preferences are effective when the default registry provides its own
    (cherry picked from commit 3e5118e50bbbb3791a05c82e319f97c686c947ec)

--- independent-projects/tools/registry-client/src/test/java/io/quarkus/registry/client/maven/MavenRegistryClientCompleteConfigTest.java
+++ independent-projects/tools/registry-client/src/test/java/io/quarkus/registry/client/maven/MavenRegistryClientCompleteConfigTest.java
@@ -11,11 +11,12 @@ import io.quarkus.maven.dependency.ArtifactCoords;
 import io.quarkus.registry.config.RegistryConfig;
 import io.quarkus.registry.config.RegistryDescriptorConfig;
 import io.quarkus.registry.config.RegistryPlatformsConfig;
+import io.quarkus.registry.config.RegistryQuarkusVersionsConfig;
 
 public class MavenRegistryClientCompleteConfigTest {
 
     @Test
-    public void testCompletePlatformsConfig() throws Exception {
+    public void testCompletePlatformsConfig() {
 
         final RegistryDescriptorConfig descriptorConfig = RegistryDescriptorConfig.builder()
                 .setArtifact(ArtifactCoords.fromString("org.acme.registry:acme-registry-descriptor::json:1.0-SNAPSHOT"))
@@ -49,5 +50,48 @@ public class MavenRegistryClientCompleteConfigTest {
         assertThat(completePlatforms.getArtifact())
                 .isEqualTo(ArtifactCoords.fromString("org.acme.registry:acme-platforms::json:1.0-SNAPSHOT"));
         assertThat(completePlatforms.getExtensionCatalogsIncluded()).isTrue();
+    }
+
+    @Test
+    public void testRecognizedVersionOverride() {
+
+        final RegistryDescriptorConfig descriptorConfig = RegistryDescriptorConfig.builder()
+                .setArtifact(ArtifactCoords.fromString("org.acme.registry:acme-registry-descriptor::json:1.0-SNAPSHOT"))
+                .build();
+
+        final RegistryConfig.Mutable originalRegistryConfig = RegistryConfig.builder();
+        originalRegistryConfig.setId("acme-registry")
+                .setDescriptor(descriptorConfig)
+                .setPlatforms(RegistryPlatformsConfig.builder()
+                        .setArtifact(ArtifactCoords.fromString("org.acme.registry:acme-platforms::json:1.0-SNAPSHOT"))
+                        .build())
+                .setQuarkusVersions(RegistryQuarkusVersionsConfig.builder()
+                        .setRecognizedVersionsExpression("*acme-acme*")
+                        .setExclusiveProvider(true));
+
+        final RegistryConfig.Mutable registryDescriptor = RegistryConfig.builder();
+        registryDescriptor.setId("acme-registry")
+                .setDescriptor(descriptorConfig)
+                .setPlatforms(RegistryPlatformsConfig.builder()
+                        .setArtifact(ArtifactCoords.fromString("org.acme.registry:acme-platforms::json:1.0-SNAPSHOT"))
+                        .build())
+                .setQuarkusVersions(RegistryQuarkusVersionsConfig.builder()
+                        .setRecognizedGroupIds(List.of("org.acme"))
+                        .setRecognizedVersionsExpression("*acme*"));
+
+        final RegistryConfig.Mutable completeConfig = MavenRegistryClientFactory.completeRegistryConfig(originalRegistryConfig,
+                registryDescriptor);
+        assertThat(completeConfig.getId()).isEqualTo("acme-registry");
+        assertThat(completeConfig.getDescriptor().getArtifact())
+                .isEqualTo(ArtifactCoords.fromString("org.acme.registry:acme-registry-descriptor::json:1.0-SNAPSHOT"));
+        final RegistryPlatformsConfig completePlatforms = completeConfig.getPlatforms();
+        assertThat(completePlatforms).isNotNull();
+        assertThat(completePlatforms.getArtifact())
+                .isEqualTo(ArtifactCoords.fromString("org.acme.registry:acme-platforms::json:1.0-SNAPSHOT"));
+        assertThat(completeConfig.getQuarkusVersions())
+                .isEqualTo(RegistryQuarkusVersionsConfig.builder()
+                        .setRecognizedVersionsExpression("*acme-acme*")
+                        .setExclusiveProvider(true)
+                        .setRecognizedGroupIds(List.of("org.acme")));
     }
 }

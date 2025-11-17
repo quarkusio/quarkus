4f7467b4505ddc3dcb8b8ef418e60ea522340fcc
Holly Cummins <holly.k.cummins@googlemail.com>
2025-11-17 20:57:54 +0000

    Use dev services instead of docker-maven-plugin for Postgresql

---
+++ integration-tests/jpa-postgresql/src/test/java/io/quarkus/it/jpa/postgresql/PostgresTestResourceLifecycleManager.java
@@ -0,0 +1,35 @@
+package io.quarkus.it.jpa.postgresql;
+
+import java.util.Map;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+public class PostgresTestResourceLifecycleManager implements QuarkusTestResourceLifecycleManager {
+
+    private static PostgreSQLContainer<?> postgres;
+
+    @SuppressWarnings("resource")
+    @Override
+    public Map<String, String> start() {
+        postgres = new PostgreSQLContainer<>("postgres:14") // the exact value doesn't really matter here
+                .withDatabaseName("testdb")
+                .withUsername("test")
+                .withPassword("test");
+
+        postgres.start();
+
+        return Map.of("quarkus.datasource.jdbc.url", postgres.getJdbcUrl(), "quarkus.datasource.username", "test",
+                "quarkus.datasource.password", "test");
+    }
+
+    @Override
+    public void stop() {
+
+        if (postgres != null) {
+            postgres.stop();
+        }
+    }
+
+}

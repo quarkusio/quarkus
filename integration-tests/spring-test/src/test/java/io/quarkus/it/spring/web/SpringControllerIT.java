be9466ceba0993bf644fd0b2944321c36b1792ef
Aurea Munoz <devauriverse@gmail.com>
2025-11-17 11:17:54 +0100

    Spring boot test - it

---
+++ integration-tests/spring-test/src/test/java/io/quarkus/it/spring/web/SpringControllerIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.it.spring.web;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class SpringControllerIT extends SpringControllerTest {
+}

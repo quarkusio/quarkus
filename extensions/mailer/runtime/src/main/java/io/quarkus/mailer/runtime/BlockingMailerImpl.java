a467cd7e094058e84927ccab5119417a938e5705
staillebois <sylvain.taillebois@gmail.com>
2025-11-25 15:22:30 +0100

    Issue-51042: Introduce timeout settings for Blocking mailer

--- extensions/mailer/runtime/src/main/java/io/quarkus/mailer/runtime/BlockingMailerImpl.java
+++ extensions/mailer/runtime/src/main/java/io/quarkus/mailer/runtime/BlockingMailerImpl.java
@@ -1,5 +1,7 @@
 package io.quarkus.mailer.runtime;
 
+import java.time.Duration;
+
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
 import io.quarkus.mailer.reactive.ReactiveMailer;
@@ -10,13 +12,21 @@ import io.quarkus.mailer.reactive.ReactiveMailer;
 public class BlockingMailerImpl implements Mailer {
 
     private final ReactiveMailer mailer;
+    private final Duration timeout;
 
-    BlockingMailerImpl(ReactiveMailer mailer) {
+    BlockingMailerImpl(ReactiveMailer mailer, Duration timeout) {
         this.mailer = mailer;
+        this.timeout = timeout;
     }
 
     @Override
     public void send(Mail... mails) {
-        mailer.send(mails).await().indefinitely();
+        if (timeout == null || timeout.isZero()) {
+            // Backward compatibility: if timeout is 0 or null, wait indefinitely
+            mailer.send(mails).await().indefinitely();
+        } else {
+            // Use the configured timeout
+            mailer.send(mails).await().atMost(timeout);
+        }
     }
 }

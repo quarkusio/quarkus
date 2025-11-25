bd8fca5d088e57d6b5c577a55ee86fcf6ab4913a
Francesco Nigro <nigro.fra@gmail.com>
2025-11-25 06:28:47 +0100

    Stop using named (with counter) VT names due to https://bugs.openjdk.org/browse/JDK-8372410

--- integration-tests/virtual-threads/metrics-virtual-threads/src/test/java/io/quarkus/virtual/vertx/web/RunOnVirtualThreadTest.java
+++ integration-tests/virtual-threads/metrics-virtual-threads/src/test/java/io/quarkus/virtual/vertx/web/RunOnVirtualThreadTest.java
@@ -3,23 +3,40 @@ package io.quarkus.virtual.vertx.web;
 import static io.restassured.RestAssured.get;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
 import io.quarkus.test.junit.virtual.ShouldNotPin;
 import io.quarkus.test.junit.virtual.VirtualThreadUnit;
 
 @QuarkusTest
+@TestProfile(RunOnVirtualThreadTest.CustomVirtualThreadProfile.class)
 @VirtualThreadUnit
 @ShouldNotPin
 class RunOnVirtualThreadTest {
 
+    public static class CustomVirtualThreadProfile implements QuarkusTestProfile {
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            return Map.of("quarkus.virtual-threads.name-prefix", "quarkus-virtual-thread-");
+        }
+    }
+
     @Test
     void testRouteOnVirtualThread() {
         String bodyStr = get("/hello").then().statusCode(200).extract().asString();
+        String secondBodyStr = get("/hello").then().statusCode(200).extract().asString();
+        // Quarkus specific - all VTs shares the same prefix
+        assertTrue(bodyStr.startsWith("quarkus-virtual-thread-"));
+        assertTrue(secondBodyStr.startsWith("quarkus-virtual-thread-"));
         // Each VT has a unique name in quarkus
-        assertNotEquals(bodyStr, get("/hello").then().statusCode(200).extract().asString());
+        assertNotEquals(bodyStr, secondBodyStr);
     }
 
     @Test

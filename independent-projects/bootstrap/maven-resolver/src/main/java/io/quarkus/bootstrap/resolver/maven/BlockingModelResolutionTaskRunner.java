c9f7144e1724f08010f1ef098503a4a2148d5600
Alexey Loubyansky <olubyans@redhat.com>
2025-11-27 17:41:03 +0100

    Re-try Maven resolver lock acquisition errors with a blocking task runner
    (cherry picked from commit 18688d0d535a5f83a00fb17ecd21af7a0f09954a)

--- independent-projects/bootstrap/maven-resolver/src/main/java/io/quarkus/bootstrap/resolver/maven/BlockingModelResolutionTaskRunner.java
+++ independent-projects/bootstrap/maven-resolver/src/main/java/io/quarkus/bootstrap/resolver/maven/BlockingModelResolutionTaskRunner.java
@@ -8,15 +8,36 @@ package io.quarkus.bootstrap.resolver.maven;
 class BlockingModelResolutionTaskRunner implements ModelResolutionTaskRunner {
 
     static BlockingModelResolutionTaskRunner getInstance() {
-        return new BlockingModelResolutionTaskRunner();
+        return new BlockingModelResolutionTaskRunner(null);
+    }
+
+    static BlockingModelResolutionTaskRunner getInstance(ModelResolutionTaskErrorHandler errorHandler) {
+        return new BlockingModelResolutionTaskRunner(errorHandler);
+    }
+
+    private final ModelResolutionTaskErrorHandler errorHandler;
+
+    BlockingModelResolutionTaskRunner(ModelResolutionTaskErrorHandler errorHandler) {
+        this.errorHandler = errorHandler;
     }
 
     @Override
     public void run(ModelResolutionTask task) {
-        task.run();
+        if (errorHandler == null) {
+            task.run();
+            return;
+        }
+        try {
+            task.run();
+        } catch (Exception e) {
+            errorHandler.handleError(task, e);
+        }
     }
 
     @Override
     public void waitForCompletion() {
+        if (errorHandler != null) {
+            errorHandler.allTasksFinished();
+        }
     }
 }

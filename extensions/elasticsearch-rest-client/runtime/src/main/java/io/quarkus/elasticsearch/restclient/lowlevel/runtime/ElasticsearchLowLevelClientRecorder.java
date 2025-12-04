6156230bb928d5c0566fc63f57d4f8714c74c1b7
marko-bekhta <marko.prykladna@gmail.com>
2025-12-04 21:51:48 +0100

    Allow configuring multiple higher level Elasticsearch clients

--- extensions/elasticsearch-rest-client/runtime/src/main/java/io/quarkus/elasticsearch/restclient/lowlevel/runtime/ElasticsearchLowLevelClientRecorder.java
+++ extensions/elasticsearch-rest-client/runtime/src/main/java/io/quarkus/elasticsearch/restclient/lowlevel/runtime/ElasticsearchLowLevelClientRecorder.java
@@ -4,6 +4,7 @@ import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 

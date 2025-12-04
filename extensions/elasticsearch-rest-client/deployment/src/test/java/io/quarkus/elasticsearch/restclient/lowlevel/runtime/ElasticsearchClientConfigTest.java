9085dc98ac72490d9b29b398dab61dc6e0950dd5
marko-bekhta <marko.prykladna@gmail.com>
2025-12-04 16:59:51 +0100

    Mention the Elasticsearch named clients in the documentation

--- extensions/elasticsearch-rest-client/deployment/src/test/java/io/quarkus/elasticsearch/restclient/lowlevel/runtime/ElasticsearchClientConfigTest.java
+++ extensions/elasticsearch-rest-client/deployment/src/test/java/io/quarkus/elasticsearch/restclient/lowlevel/runtime/ElasticsearchClientConfigTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.quarkus.elasticsearch.restclient.lowlevel.ElasticsearchClientConfig;
 import io.quarkus.test.QuarkusUnitTest;
+import io.smallrye.common.annotation.Identifier;
 
 public class ElasticsearchClientConfigTest {
     @RegisterExtension
@@ -32,15 +33,34 @@ public class ElasticsearchClientConfigTest {
     @Inject
     RestClient client;
 
+    @Inject
+    @Identifier("second-client")
+    RestClient secondClient;
+
     @Test
     public void testRestClientBuilderHelperWithElasticsearchClientConfig() {
         assertNotNull(client);
+        assertNotNull(secondClient);
         assertTrue(TestConfigurator.invoked);
+        assertTrue(SecondTestConfigurator.invoked);
     }
 
     @ElasticsearchClientConfig
     @ApplicationScoped
     public static class TestConfigurator implements RestClientBuilder.HttpClientConfigCallback {
+
+        private static boolean invoked = false;
+
+        @Override
+        public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder builder) {
+            invoked = true;
+            return builder;
+        }
+    }
+
+    @ElasticsearchClientConfig("second-client")
+    @ApplicationScoped
+    public static class SecondTestConfigurator implements RestClientBuilder.HttpClientConfigCallback {
 
         private static boolean invoked = false;
 

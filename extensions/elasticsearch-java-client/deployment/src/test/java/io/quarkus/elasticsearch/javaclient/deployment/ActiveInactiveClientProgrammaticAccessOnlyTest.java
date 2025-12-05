ba8cfb52ed1fedeac4c30173d3f22b502aa6e216
marko-bekhta <marko.prykladna@gmail.com>
2025-12-05 11:36:20 +0100

    Add a "marker" config property to force the discovery of named clients

---
+++ extensions/elasticsearch-java-client/deployment/src/test/java/io/quarkus/elasticsearch/javaclient/deployment/ActiveInactiveClientProgrammaticAccessOnlyTest.java
@@ -0,0 +1,35 @@
+package io.quarkus.elasticsearch.javaclient.deployment;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.elasticsearch.client.RestClient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import io.quarkus.arc.Arc;
+import io.quarkus.test.QuarkusUnitTest;
+import io.smallrye.common.annotation.Identifier;
+
+public class ActiveInactiveClientProgrammaticAccessOnlyTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest test = new QuarkusUnitTest()
+            .withApplicationRoot((jar) -> jar
+                    .addAsResource("active-inactive-programmatic.properties", "application.properties"));
+
+    @Test
+    void smoke() {
+        assertTrue(Arc.container().select(RestClient.class).getHandle().getBean().isActive());
+        assertFalse(
+                Arc.container().select(RestClient.class, Identifier.Literal.of("client2")).getHandle().getBean().isActive());
+        assertTrue(Arc.container().select(RestClient.class, Identifier.Literal.of("client3")).getHandle().getBean().isActive());
+
+        assertTrue(Arc.container().select(ElasticsearchClient.class).getHandle().getBean().isActive());
+        assertFalse(Arc.container().select(ElasticsearchClient.class, Identifier.Literal.of("client2")).getHandle().getBean()
+                .isActive());
+        assertFalse(Arc.container().select(ElasticsearchClient.class, Identifier.Literal.of("client3")).getHandle().getBean()
+                .isActive());
+    }
+}

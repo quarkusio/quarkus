98d9eabcb6d6591bd6f8db120070437b134d6742
Marco Belladelli <marcobladel@gmail.com>
2025-12-18 09:45:42 +0100

    Small fixes and removed explicit geolatte-geometry checks/dependencies

--- extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/spatial/HibernateSpatialAvailable.java
+++ extensions/hibernate-orm/deployment/src/main/java/io/quarkus/hibernate/orm/deployment/spatial/HibernateSpatialAvailable.java
@@ -4,7 +4,7 @@ import java.util.function.BooleanSupplier;
 
 /**
  * Supplier that can be used to only run build steps
- * if the Hibernate ORM extension is enabled.
+ * if Hibernate Spatial is available in the classpath.
  */
 public class HibernateSpatialAvailable implements BooleanSupplier {
     static final String HIBERNATE_SPATIAL_SERVICE_CLASS = "org.hibernate.spatial.integration.SpatialService";

1035b30263b0ebf15e0635baa7b3516bbaa1da47
Anis Da Silva Campos <anis.dasilvacampos@getalma.eu>
2025-12-25 18:04:51 +0100

    feat: add PATCH to HttpMethod in io.quarkus.vertx.web.Route
    Adding the missing patch http method
    
    FIXES #51724
    
    test: check that the patch endpoint works

--- extensions/reactive-routes/deployment/src/test/java/io/quarkus/vertx/web/TestRoute.java
+++ extensions/reactive-routes/deployment/src/test/java/io/quarkus/vertx/web/TestRoute.java
@@ -2,13 +2,14 @@ package io.quarkus.vertx.web;
 
 import static io.quarkus.vertx.web.Route.HttpMethod.GET;
 import static io.quarkus.vertx.web.Route.HttpMethod.OPTIONS;
+import static io.quarkus.vertx.web.Route.HttpMethod.PATCH;
 import static io.quarkus.vertx.web.Route.HttpMethod.POST;
 
 import io.vertx.ext.web.RoutingContext;
 
 public class TestRoute {
 
-    @Route(path = "/test", methods = { GET, OPTIONS, POST })
+    @Route(path = "/test", methods = { GET, OPTIONS, PATCH, POST })
     void getRoutes(RoutingContext context) {
         context.response().setStatusCode(200).end("test route");
     }

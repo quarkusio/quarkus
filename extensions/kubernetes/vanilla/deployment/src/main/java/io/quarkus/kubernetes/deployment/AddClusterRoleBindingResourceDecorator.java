4610fd6235d8b4643c6ae17a56d245c2b07175ae
Georgios Andrianakis <geoand@gmail.com>
2026-01-01 08:29:30 +0200

    Remove code duplication in kubernetes role handling

--- extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/AddClusterRoleBindingResourceDecorator.java
+++ extensions/kubernetes/vanilla/deployment/src/main/java/io/quarkus/kubernetes/deployment/AddClusterRoleBindingResourceDecorator.java
@@ -1,54 +1,40 @@
 package io.quarkus.kubernetes.deployment;
 
 import static io.quarkus.kubernetes.deployment.Constants.CLUSTER_ROLE;
-import static io.quarkus.kubernetes.deployment.Constants.CLUSTER_ROLE_BINDING;
 import static io.quarkus.kubernetes.deployment.Constants.RBAC_API_GROUP;
-import static io.quarkus.kubernetes.deployment.Constants.RBAC_API_VERSION;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
-import io.dekorate.kubernetes.decorator.ResourceProvidingDecorator;
 import io.dekorate.utils.Strings;
 import io.fabric8.kubernetes.api.model.KubernetesListBuilder;
-import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.api.model.rbac.ClusterRoleBindingBuilder;
 import io.quarkus.kubernetes.spi.RoleRef;
 import io.quarkus.kubernetes.spi.Subject;
 
-public class AddClusterRoleBindingResourceDecorator extends ResourceProvidingDecorator<KubernetesListBuilder> {
+public class AddClusterRoleBindingResourceDecorator extends AbstractRoleResourceDecorator {
 
-    private final String deploymentName;
-    private final String name;
-    private final Map<String, String> labels;
     private final RoleRef roleRef;
     private final Subject[] subjects;
 
     public AddClusterRoleBindingResourceDecorator(String deploymentName, String name, Map<String, String> labels,
             RoleRef roleRef,
             Subject... subjects) {
-        this.deploymentName = deploymentName;
-        this.name = name;
-        this.labels = labels;
+        super(deploymentName, name, labels);
         this.roleRef = roleRef;
         this.subjects = subjects;
     }
 
     public void visit(KubernetesListBuilder list) {
-        if (contains(list, RBAC_API_VERSION, CLUSTER_ROLE_BINDING, name)) {
+        Optional<Map<String, String>> maybeClusterRoleBindingLabels = preVisit(list);
+        if (maybeClusterRoleBindingLabels.isEmpty()) {
             return;
         }
-
-        Map<String, String> clusterRoleBindingLabels = new HashMap<>();
-        clusterRoleBindingLabels.putAll(labels);
-        getDeploymentMetadata(list, deploymentName)
-                .map(ObjectMeta::getLabels)
-                .ifPresent(clusterRoleBindingLabels::putAll);
 
         ClusterRoleBindingBuilder builder = new ClusterRoleBindingBuilder()
                 .withNewMetadata()
                 .withName(name)
-                .withLabels(clusterRoleBindingLabels)
+                .withLabels(maybeClusterRoleBindingLabels.get())
                 .endMetadata()
                 .withNewRoleRef()
                 .withKind(CLUSTER_ROLE)

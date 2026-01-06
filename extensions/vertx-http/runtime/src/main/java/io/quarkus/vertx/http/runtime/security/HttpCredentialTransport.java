813dcc1f3c4991ffe8f8d3cd1c61900a7e877c0c
Sergey Beryozkin <sberyozkin@gmail.com>
2026-01-06 19:24:53 +0000

    Improve HttpCredentialTransport related docs

--- extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/security/HttpCredentialTransport.java
+++ extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/security/HttpCredentialTransport.java
@@ -20,10 +20,34 @@ public class HttpCredentialTransport {
     private final String typeTarget;
     private final String authenticationScheme;
 
+    /**
+     * HttpCredentialTransport constructor that accepts two parameters.
+     *
+     * @param transportType the transport type, for example, {@link Type#AUTHORIZATION}
+     * @param typeTarget the type target, case-insensitive, for example, "Bearer".
+     *        It also becomes an authentication scheme returned from {@link HttpCredentialTransport#getAuthenticationScheme()}
+     */
     public HttpCredentialTransport(Type transportType, String typeTarget) {
         this(transportType, typeTarget, typeTarget);
     }
 
+    /**
+     * HttpCredentialTransport constructor that accepts three parameters.
+     *
+     * @param transportType the transport type, for example, {@link Type#AUTHORIZATION}
+     * @param typeTarget the type target, case-insensitive, for example, "Bearer" or a Form authentication post location.
+     * @param authenticationScheme case-insensitive. Usually, it is equal to the {@link #typeTarget} but can also take a unique
+     *        value.
+     *        For example, "Form" for a Form authentication mechanism whose {@link #typeTarget} may be set to a form post
+     *        location.
+     *        <p>
+     *        Both {@link io.quarkus.vertx.http.runtime.security.annotation.HttpAuthenticationMechanism#value()}
+     *        and an optional HTTP security policy's `auth-mechanism` property must be set to the "authenticationScheme" value
+     *        to find a matching {@link HttpAuthenticationMechanism} that must secure a specific REST resource method or request
+     *        path.
+     *        For example, '@HttpAuthenticationMechanism("custom-scheme")',
+     *        'quarkus.http.auth.permission.my-policy.auth-mechanism=custom-scheme'.
+     */
     public HttpCredentialTransport(Type transportType, String typeTarget, String authenticationScheme) {
         this.transportType = Objects.requireNonNull(transportType);
         this.typeTarget = Objects.requireNonNull(typeTarget).toLowerCase();

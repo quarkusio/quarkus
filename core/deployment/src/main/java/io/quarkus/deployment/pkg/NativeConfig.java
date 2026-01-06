9a37ab63a40c9ed427d9905ac39725b29d2e6ee3
Michal Vavřík <michal.vavrik@aol.com>
2026-01-06 16:27:12 +0100

    feat(docs): generate enum desc for lists and optional lists
    Signed-off-by: Michal Vavřík <michal.vavrik@aol.com>
    (cherry picked from commit 92e2aab29842f353f41f1c65356380a5be08ebab)

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/NativeConfig.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/NativeConfig.java
@@ -258,7 +258,7 @@ public interface NativeConfig {
      * <ul>
      * <li><code>jfr</code> for JDK flight recorder support</li>
      * <li><code>jvmstat</code> for JVMStat support</li>
-     * <li><code>heapdump</code> for heampdump support</li>
+     * <li><code>heapdump</code> for heapdump support</li>
      * <li><code>jmxclient</code> for JMX client support (experimental)</li>
      * <li><code>jmxserver</code> for JMX server support (experimental)</li>
      * <li><code>nmt</code> for native memory tracking support</li>
@@ -510,13 +510,37 @@ public interface NativeConfig {
     }
 
     enum MonitoringOption {
+        /**
+         * Heapdump support.
+         */
         HEAPDUMP,
+        /**
+         * JVMStat support.
+         */
         JVMSTAT,
+        /**
+         * JDK flight recorder support.
+         */
         JFR,
+        /**
+         * JMX server support (experimental).
+         */
         JMXSERVER,
+        /**
+         * JMX client support (experimental).
+         */
         JMXCLIENT,
+        /**
+         * Native memory tracking support.
+         */
         NMT,
+        /**
+         * All monitoring features.
+         */
         ALL,
+        /**
+         * Explicitly turns off all monitoring features.
+         */
         NONE
     }
 

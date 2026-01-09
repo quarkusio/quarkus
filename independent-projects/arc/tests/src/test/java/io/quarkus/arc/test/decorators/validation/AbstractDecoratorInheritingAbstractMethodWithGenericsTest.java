6393fda8299f8c02e4e28b6d8e22471b6036e133
Ladislav Thon <lthon@redhat.com>
2026-01-09 15:30:43 +0100

    ArC: proper validation of abstract methods on decorators
    Per the CDI specification, an `abstract` decorator may not have
    `abstract` methods that are not declared on one of the decorated
    types (implemented interfaces). Until now, ArC didn't perform this
    validation properly; instead, it just checked whether the decorator
    class declared any `abstract` method.
    
    With this commit, the validation is more precise. The implementation
    doesn't use the existing ArC facilities for dealing with parameterized
    types; instead, it contains a prototype of a Jandex "type system",
    which will hopefully one day be stabilized in Jandex itself.

---
+++ independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/decorators/validation/AbstractDecoratorInheritingAbstractMethodWithGenericsTest.java
@@ -0,0 +1,80 @@
+package io.quarkus.arc.test.decorators.validation;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import jakarta.annotation.Priority;
+import jakarta.decorator.Decorator;
+import jakarta.decorator.Delegate;
+import jakarta.enterprise.inject.spi.DefinitionException;
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.arc.test.ArcTestContainer;
+
+public class AbstractDecoratorInheritingAbstractMethodWithGenericsTest {
+    @RegisterExtension
+    public ArcTestContainer container = ArcTestContainer.builder()
+            .beanClasses(Converter.class, MyGoodDecorator.class, MyBadDecorator.class)
+            .shouldFail()
+            .build();
+
+    @Test
+    public void trigger() {
+        Throwable error = container.getFailure();
+        assertNotNull(error);
+        assertInstanceOf(DefinitionException.class, error);
+        assertTrue(error.getMessage().contains("declares abstract method(s) not present on decorated interface(s)"));
+        assertTrue(error.getMessage().contains("MyBadDecorator"));
+        assertTrue(error.getMessage().contains("convert3"));
+        assertFalse(error.getMessage().contains("MyGoodDecorator"));
+        assertFalse(error.getMessage().contains("convert1"));
+        assertFalse(error.getMessage().contains("convert2"));
+    }
+
+    interface Converter<S, T> {
+        T convert1(S value);
+
+        T convert2(S value);
+    }
+
+    static abstract class GoodAbstractDecorator<S, T> {
+        abstract T convert2(S value);
+    }
+
+    static abstract class BadAbstractDecorator<S, T> {
+        abstract T convert3(S value);
+    }
+
+    // this is OK, the abstract method comes from a decorated type
+    @Decorator
+    @Priority(1)
+    static abstract class MyGoodDecorator extends GoodAbstractDecorator<Integer, String> implements Converter<Integer, String> {
+        @Inject
+        @Delegate
+        Converter<Integer, String> delegate;
+
+        @Override
+        public String convert1(Integer value) {
+            return delegate.convert1(value);
+        }
+    }
+
+    // this is NOT OK, the abstract method does not come from a decorated type
+    @Decorator
+    @Priority(1)
+    static abstract class MyBadDecorator extends BadAbstractDecorator<Integer, String> implements Converter<Integer, String> {
+        @Inject
+        @Delegate
+        Converter<Integer, String> delegate;
+
+        @Override
+        public String convert1(Integer value) {
+            return delegate.convert1(value);
+        }
+    }
+}

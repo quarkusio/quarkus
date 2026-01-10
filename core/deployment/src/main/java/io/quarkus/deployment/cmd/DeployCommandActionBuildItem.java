3f36e51549033ce7afe02edc24499b29641a837a
Matheus Andre <matheusandr2@gmail.com>
2026-01-10 12:08:54 -0300

    Enhancing the DeployCommandActionBuildItem documentation for greater clarity regarding command execution and success status.

--- core/deployment/src/main/java/io/quarkus/deployment/cmd/DeployCommandActionBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/cmd/DeployCommandActionBuildItem.java
@@ -3,17 +3,16 @@ package io.quarkus.deployment.cmd;
 import io.quarkus.builder.item.MultiBuildItem;
 
 /**
- * A build item that indicates that a deploy command was executed.
- * This can be used to trigger additional actions after a deploy command.
+ * A build item that indicates that a deploy command action was executed successfully or not.  
  */
 public final class DeployCommandActionBuildItem extends MultiBuildItem {
     /**
-     * The name of the command that was executed (e.g., "deploy", "redeploy").
+     * The name of the command that was executed (e.g., "azure-function").
      */
     private final String commandName;
 
     /**
-     * Indicates whether the command was successful.
+     * Indicates whether the deploy action was successful.
      */
     private final boolean successful;
 
@@ -25,16 +24,10 @@ public final class DeployCommandActionBuildItem extends MultiBuildItem {
         this.successful = successful;
     }
 
-    /**
-     * Gets the name of the command that was executed.
-     */
     public String getCommandName() {
         return commandName;
     }
-
-    /**
-     * Indicates whether the command was successful.
-     */
+    
     public boolean isSuccessful() {
         return successful;
     }

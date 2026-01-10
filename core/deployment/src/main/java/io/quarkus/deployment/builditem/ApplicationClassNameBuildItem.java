6deb898b562c38f83385ee5b4a8d9655b16a6c21
Matheus Andre <matheusandr2@gmail.com>
2026-01-10 12:01:39 -0300

    Update Javadoc for ApplicationClassNameBuildItem to clarify entry point

--- core/deployment/src/main/java/io/quarkus/deployment/builditem/ApplicationClassNameBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/builditem/ApplicationClassNameBuildItem.java
@@ -4,6 +4,7 @@ import io.quarkus.builder.item.SimpleBuildItem;
 
 /**
  * Holds the fully qualified name of the application's entry point.
+ * (<code>io.quarkus.runner.ApplicationImpl</code>) 
  * <p>
  * Other build steps can consume this item to generate code,
  * configure Native Image options, or adjust runtime behavior

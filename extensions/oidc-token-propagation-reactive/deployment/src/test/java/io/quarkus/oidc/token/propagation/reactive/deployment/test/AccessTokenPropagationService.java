7aa3872bd5ffd7244529035237f2edca28eacf73
Sergey Beryozkin <sberyozkin@gmail.com>
2026-01-11 13:29:45 +0000

    Check @Consumes duplicates in REST Client
    (cherry picked from commit 21222a95646e887bd8707c06aa39c64a5e3c07e9)

--- extensions/oidc-token-propagation-reactive/deployment/src/test/java/io/quarkus/oidc/token/propagation/reactive/deployment/test/AccessTokenPropagationService.java
+++ extensions/oidc-token-propagation-reactive/deployment/src/test/java/io/quarkus/oidc/token/propagation/reactive/deployment/test/AccessTokenPropagationService.java
@@ -1,6 +1,8 @@
 package io.quarkus.oidc.token.propagation.reactive.deployment.test;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
@@ -10,8 +12,13 @@ import io.quarkus.oidc.token.propagation.common.AccessToken;
 @RegisterRestClient
 @AccessToken
 @Path("/")
+@Consumes("text/plain")
 public interface AccessTokenPropagationService {
 
     @GET
     String getUserName();
+
+    @POST
+    @Consumes("text/plain")
+    String echoUserName(String name);
 }

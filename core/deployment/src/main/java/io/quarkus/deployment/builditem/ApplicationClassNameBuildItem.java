08e8c97cc8c7c8c9dcea47e375eebed38b9f51df
Matheus Andre <matheusandr2@gmail.com>
2026-01-12 10:45:57 -0300

    Fix formatting in Javadoc for ApplicationClassNameBuildItem

--- core/deployment/src/main/java/io/quarkus/deployment/builditem/ApplicationClassNameBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/builditem/ApplicationClassNameBuildItem.java
@@ -3,8 +3,7 @@ package io.quarkus.deployment.builditem;
 import io.quarkus.builder.item.SimpleBuildItem;
 
 /**
- * Holds the fully qualified name of the application's entry point.
- * (<code>io.quarkus.runner.ApplicationImpl</code>) 
+ * Holds the fully qualified name of the application's entry point. (<code>io.quarkus.runner.ApplicationImpl</code>) 
  * <p>
  * Other build steps can consume this item to generate code,
  * configure Native Image options, or adjust runtime behavior

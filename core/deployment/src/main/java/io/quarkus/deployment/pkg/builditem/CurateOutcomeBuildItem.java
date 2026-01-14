dadd50f38bc0df63ca7f01b4437b2dc454a07337
Matheus Andre <matheusandr2@gmail.com>
2026-01-14 00:03:49 -0300

    Docs curate outcome build item

--- core/deployment/src/main/java/io/quarkus/deployment/pkg/builditem/CurateOutcomeBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/pkg/builditem/CurateOutcomeBuildItem.java
@@ -3,14 +3,32 @@ package io.quarkus.deployment.pkg.builditem;
 import io.quarkus.bootstrap.model.ApplicationModel;
 import io.quarkus.builder.item.SimpleBuildItem;
 
+/**
+ * Represents the outcome of the application dependency resolution and
+ * model building.
+ * This build item exposes the resulting {@link ApplicationModel}
+ * to subsequent build steps.
+ */
 public final class CurateOutcomeBuildItem extends SimpleBuildItem {
-
+    /*
+     * The application model resulting from the curation process and Dependency resolution.
+     */
     private final ApplicationModel appModel;
 
+    /**
+     * Creates a new CurateOutcomeBuildItem.
+     *
+     * @param appModel the application model
+     */
     public CurateOutcomeBuildItem(ApplicationModel appModel) {
         this.appModel = appModel;
     }
 
+    /**
+     * Gets the application model.
+     *
+     * @return the application model
+     */
     public ApplicationModel getApplicationModel() {
         return appModel;
     }

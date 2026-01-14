7fec9fc06764d30145dd7bbf6d689167aab1d32c
Luca Molteni <volothamp@gmail.com>
2026-01-14 17:40:30 +0100

    Removed empty integration test suite

--- integration-tests/hibernate-reactive-transactions/src/test/java/io/quarkus/hibernate/reactive/transactions/it/HibernateReactiveTransactionsResourceIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkus.hibernate.reactive.transactions.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class HibernateReactiveTransactionsResourceIT extends HibernateReactiveTransactionsResourceTest {
-}

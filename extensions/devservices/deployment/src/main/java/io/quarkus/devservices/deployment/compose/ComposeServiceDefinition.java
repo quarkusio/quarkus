31de7fad812b42e4a583285b8dfe8d5f85076cf1
Ozan Gunalp <ozangunalp@gmail.com>
2026-01-14 16:28:10 +0100

    Support long format ports definitions in Compose files
    Fixes #51763
    
    (cherry picked from commit fbf8e5412c0bd17e28438dd50b1a372498374330)

--- extensions/devservices/deployment/src/main/java/io/quarkus/devservices/deployment/compose/ComposeServiceDefinition.java
+++ extensions/devservices/deployment/src/main/java/io/quarkus/devservices/deployment/compose/ComposeServiceDefinition.java
@@ -31,11 +31,61 @@ public class ComposeServiceDefinition {
     }
 
     public List<ExposedPort> getPorts() {
-        List<String> ports = (List<String>) definitionMap.get("ports");
+        List<?> ports = (List<?>) definitionMap.get("ports");
         if (ports == null) {
             return Collections.emptyList();
         }
-        return ports.stream().map(PortBinding::parse)
+        return ports.stream().map(port -> {
+            String portString;
+            if (port instanceof String) {
+                // Short syntax: "8080:8080" or "8080"
+                portString = (String) port;
+            } else if (port instanceof Map<?, ?> portMap) {
+                // Long syntax:
+                // ports:
+                //  - name: web
+                //    target: 80
+                //    host_ip: 127.0.0.1
+                //    published: "8080"
+                //    protocol: tcp
+                //    app_protocol: http
+                //    mode: host
+                Object target = portMap.get("target");
+                Object published = portMap.get("published");
+                Object hostIp = portMap.get("host_ip");
+                Object protocol = portMap.get("protocol");
+
+                StringBuilder sb = new StringBuilder();
+
+                // host IP "127.0.0.1:"
+                if (hostIp != null) {
+                    sb.append(hostIp).append(":");
+                }
+
+                // published port "127.0.0.1:8080:"
+                if (published != null) {
+                    String publishedStr = String.valueOf(published);
+                    // Port ranges "8083-9000"
+                    if (publishedStr.contains("-")) {
+                        publishedStr = publishedStr.split("-")[0];
+                    }
+                    sb.append(publishedStr).append(":");
+                }
+
+                // target port "127.0.0.1:8080:80"
+                sb.append(target);
+
+                // protocol "127.0.0.1:8080:80/tcp"
+                if (protocol != null) {
+                    sb.append("/").append(protocol);
+                }
+
+                portString = sb.toString();
+            } else {
+                throw new IllegalArgumentException("Unsupported port format: " + port);
+            }
+            return PortBinding.parse(portString);
+        })
                 .map(PortBinding::getExposedPort)
                 .collect(Collectors.toList());
     }

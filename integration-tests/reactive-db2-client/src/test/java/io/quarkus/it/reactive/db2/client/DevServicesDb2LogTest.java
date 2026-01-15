904279825df47d5940e21a501d31ae6a6259bcde
osoohynn <tbr335@naver.com>
2026-01-15 13:36:03 +0900

    Change DB2 dev services test to verify REST endpoint instead of logs

--- integration-tests/reactive-db2-client/src/test/java/io/quarkus/it/reactive/db2/client/DevServicesDb2LogTest.java
+++ integration-tests/reactive-db2-client/src/test/java/io/quarkus/it/reactive/db2/client/DevServicesDb2LogTest.java
@@ -1,34 +1,31 @@
 package io.quarkus.it.reactive.db2.client;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.test.LogCollectingTestResource;
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.common.ResourceArg;
 import io.quarkus.test.junit.QuarkusTest;
 
 /**
  * Tests that Dev Services for DB2 correctly handles datasource names
  * longer than 8 characters (DB2's database name limit).
  * <p>
- * Note LogCollectingTestResource cannot be used in native mode,
- * hence the lack of a corresponding native mode test.
+ * The "additional" datasource (10 characters) exceeds the limit and
+ * triggers automatic truncation. This test verifies the truncated
+ * database name works correctly.
  *
  * @see <a href="https://github.com/quarkusio/quarkus/issues/51225">GitHub Issue #51225</a>
  */
 @QuarkusTest
-@QuarkusTestResource(value = LogCollectingTestResource.class, restrictToAnnotatedClass = true, initArgs = {
-        @ResourceArg(name = LogCollectingTestResource.LEVEL, value = "WARNING"),
-        @ResourceArg(name = LogCollectingTestResource.INCLUDE, value = "io\\.quarkus\\.devservices\\.db2\\..*")
-})
 public class DevServicesDb2LogTest {
+
     @Test
-    public void testLongDatasourceNameWarning() {
-        assertThat(LogCollectingTestResource.current().getRecords())
-                .as("Dev Services DB2 warning about 8 character limit")
-                .extracting(LogCollectingTestResource::format)
-                .anyMatch(log -> log.contains("8 character limit"));
+    public void testLongDatasourceNameWorks() {
+        given()
+                .when().get("/plants/legumes/")
+                .then()
+                .statusCode(200)
+                .body(containsString("Broccoli"));
     }
 }

ea6534896f19928837956c601e7f2b9393f606f8
Roberto Cortez <radcortez@yahoo.com>
2026-01-19 21:06:25 +0000

    Use ValueRegistry to store and retrieve the real runtime values of gRPC ports

--- integration-tests/reactive-messaging-pulsar/src/test/java/io/quarkus/it/pulsar/GreeterTestIT.java
+++ integration-tests/reactive-messaging-pulsar/src/test/java/io/quarkus/it/pulsar/GreeterTestIT.java
@@ -5,9 +5,4 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 @QuarkusIntegrationTest
 public class GreeterTestIT extends GreeterTest {
 
-    public int getPort() {
-        // In native mode, the port is not injected, so we need to retrieve it from the config
-        // I don't know how to set this up without hardcoding
-        return 9000;
-    }
 }

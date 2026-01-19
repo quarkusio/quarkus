bcd62de7bdaf2892092c7a418fd632731e200e53
Roberto Cortez <radcortez@yahoo.com>
2026-01-19 19:42:18 +0000

    Register `HttpServer.getLocalBaseUri` in integration tests
    (cherry picked from commit eb4336ccbef3c736c073277b8429f56282ddb918)

--- test-framework/common/src/main/java/io/quarkus/test/common/ListeningAddress.java
+++ test-framework/common/src/main/java/io/quarkus/test/common/ListeningAddress.java
@@ -1,7 +1,11 @@
 package io.quarkus.test.common;
 
+import java.net.URI;
+
 import io.quarkus.registry.ValueRegistry;
 import io.quarkus.registry.ValueRegistry.RuntimeKey;
+import io.quarkus.test.common.http.TestHTTPResourceManager;
+import io.smallrye.config.SmallRyeConfig;
 
 public record ListeningAddress(Integer port, String protocol) {
 
@@ -9,9 +13,10 @@ public record ListeningAddress(Integer port, String protocol) {
         return "https".equals(protocol);
     }
 
-    public void register(ValueRegistry valueRegistry) {
+    public void register(ValueRegistry valueRegistry, SmallRyeConfig config) {
         valueRegistry.register(isSsl() ? HTTPS_PORT : HTTP_PORT, port);
         valueRegistry.register(isSsl() ? HTTPS_TEST_PORT : HTTP_TEST_PORT, port);
+        valueRegistry.register(LOCAL_BASE_URI, URI.create(TestHTTPResourceManager.testUrl(valueRegistry, config)));
     }
 
     // Compatibility with Config and io.quarkus.vertx.http.HttpServer
@@ -19,4 +24,5 @@ public record ListeningAddress(Integer port, String protocol) {
     public static final RuntimeKey<Integer> HTTP_TEST_PORT = RuntimeKey.intKey("quarkus.http.test-port");
     public static final RuntimeKey<Integer> HTTPS_PORT = RuntimeKey.intKey("quarkus.http.ssl-port");
     public static final RuntimeKey<Integer> HTTPS_TEST_PORT = RuntimeKey.intKey("quarkus.http.test-ssl-port");
+    public static final RuntimeKey<URI> LOCAL_BASE_URI = RuntimeKey.key("quarkus.http.local-base-uri");
 }

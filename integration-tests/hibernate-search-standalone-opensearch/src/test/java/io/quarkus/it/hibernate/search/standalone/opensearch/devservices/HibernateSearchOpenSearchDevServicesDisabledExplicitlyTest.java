af5b8428c1f75317470fb7fa1dafc43eb02b6f78
marko-bekhta <marko.prykladna@gmail.com>
2026-01-22 17:57:45 +0100

    Move devservice tests with "disabled" services into a "profile"
    so that they are executed separately and not influenced by runs with enabled services.

--- integration-tests/hibernate-search-standalone-opensearch/src/test/java/io/quarkus/it/hibernate/search/standalone/opensearch/devservices/HibernateSearchOpenSearchDevServicesDisabledExplicitlyTest.java
+++ integration-tests/hibernate-search-standalone-opensearch/src/test/java/io/quarkus/it/hibernate/search/standalone/opensearch/devservices/HibernateSearchOpenSearchDevServicesDisabledExplicitlyTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.is;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
@@ -16,6 +17,7 @@ import io.quarkus.test.junit.QuarkusTestProfile;
 import io.quarkus.test.junit.TestProfile;
 import io.restassured.RestAssured;
 
+@Tag("disableddevservices")
 @QuarkusTest
 @DisabledOnOs(OS.WINDOWS)
 @TestProfile(HibernateSearchOpenSearchDevServicesDisabledExplicitlyTest.Profile.class)

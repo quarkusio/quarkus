53930af605c234b692deee155e6289825b61b8be
marko-bekhta <marko.prykladna@gmail.com>
2026-01-22 18:18:53 +0100

    Get rid of "force-discovery" config flag
    and ask users to have at least one static injection point for their named clients.

--- extensions/elasticsearch-rest-client/deployment/src/main/java/io/quarkus/elasticsearch/restclient/lowlevel/deployment/ElasticsearchLowLevelClientBuildTimeConfig.java
+++ extensions/elasticsearch-rest-client/deployment/src/main/java/io/quarkus/elasticsearch/restclient/lowlevel/deployment/ElasticsearchLowLevelClientBuildTimeConfig.java
@@ -13,13 +13,4 @@ public interface ElasticsearchLowLevelClientBuildTimeConfig {
     @WithDefault("true")
     boolean healthEnabled();
 
-    /**
-     * This property is to help Quarkus identify any low-level Elasticsearch REST clients
-     * that possibly are not explicitly injected in the application code with their corresponding identifier, e.g.
-     * {@code @Inject @Identifier("client-name") RestClient client;}.
-     * In such cases, this property is required to help Quarkus detect all the necessary clients at build time.
-     * The value of this configuration property is ignored.
-     */
-    @WithDefault("true")
-    boolean forceDiscovery();
 }

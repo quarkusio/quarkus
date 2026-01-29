a7f127a372d609d99e838f48513f99b06dc40ebc
Georgios Andrianakis <geoand@gmail.com>
2026-01-29 09:54:49 +0200

    Revert "Convert HostAndPort to record"
    This reverts commit 6ac10a21f29581dc49f694cee074bb1ae0e30de9.

--- extensions/resteasy-reactive/rest-client/runtime/src/main/java/io/quarkus/rest/client/reactive/runtime/ProxyAddressUtil.java
+++ extensions/resteasy-reactive/rest-client/runtime/src/main/java/io/quarkus/rest/client/reactive/runtime/ProxyAddressUtil.java
@@ -19,6 +19,13 @@ public class ProxyAddressUtil {
         return new HostAndPort(host, port);
     }
 
-    public record HostAndPort(String host, int port) {
+    public static class HostAndPort {
+        public final String host;
+        public final int port;
+
+        public HostAndPort(String host, int port) {
+            this.host = host;
+            this.port = port;
+        }
     }
 }

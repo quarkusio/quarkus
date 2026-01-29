03b568720e49353ec52777209b3108a922ead09f
Foivos Zakkak <zakkak@ibm.com>
2026-01-29 11:10:33 +0200

    Deprecate long obsoleted `JniBuildItem`
    This build item is obsolete since GraalVM 19.3.1
    (released in Q3 of 2019)

--- core/deployment/src/main/java/io/quarkus/deployment/builditem/JniBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/builditem/JniBuildItem.java
@@ -5,23 +5,26 @@ import java.util.List;
 
 import io.quarkus.builder.item.MultiBuildItem;
 
+/**
+ * @deprecated This build item was previously used to enable JNI from Quarkus extensions,
+ *             but JNI is always enabled starting from GraalVM 19.3.1.
+ */
+@Deprecated(since = "3.32", forRemoval = true)
 public final class JniBuildItem extends MultiBuildItem {
 
     private final List<String> libraryPaths;
 
-    /**
-     * @deprecated This method was previously used to enable JNI from Quarkus extensions, but JNI is always enabled starting
-     *             from GraalVM 19.3.1.
-     */
-    @Deprecated
+    @Deprecated(since = "3.32", forRemoval = true)
     public JniBuildItem() {
         this(Collections.emptyList());
     }
 
+    @Deprecated(since = "3.32", forRemoval = true)
     public JniBuildItem(List<String> libraryPaths) {
         this.libraryPaths = libraryPaths;
     }
 
+    @Deprecated(since = "3.32", forRemoval = true)
     public List<String> getLibraryPaths() {
         return libraryPaths;
     }

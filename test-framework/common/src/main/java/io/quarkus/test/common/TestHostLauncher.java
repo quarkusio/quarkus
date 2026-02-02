07efbfb2a5f1ccedc1adabd1be2e587f28df1fa8
Roberto Cortez <radcortez@yahoo.com>
2026-02-02 12:50:53 +0000

    TestHostLauncher to return the default ListeningAddress
    (cherry picked from commit 70c5c0f25714ae5e10a1045d63b3dedcf51f0240)

--- test-framework/common/src/main/java/io/quarkus/test/common/TestHostLauncher.java
+++ test-framework/common/src/main/java/io/quarkus/test/common/TestHostLauncher.java
@@ -3,6 +3,10 @@ package io.quarkus.test.common;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalInt;
+
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 /**
  * A launcher that simply sets the {@code quarkus.http.host} property based on the value {@code quarkus.http.test-host}
@@ -15,9 +19,23 @@ public class TestHostLauncher implements ArtifactLauncher {
 
     @Override
     public Optional<ListeningAddress> start() throws IOException {
+        Config config = ConfigProvider.getConfig();
+        // TODO - Remove this SystemProperty set / get
         // set 'quarkus.http.host' to ensure that RestAssured targets the proper host
         previousHost = System.setProperty("quarkus.http.host", System.getProperty("quarkus.http.test-host"));
-        return Optional.empty();
+
+        // We need to manually query and set defaults, because this runs in IT and VertxHttpConfig is not available
+        boolean testSslEnabled = config.getOptionalValue("quarkus.http.test-ssl-enabled", boolean.class).orElse(false);
+        int port;
+        String protocol;
+        if (testSslEnabled) {
+            port = config.getValue("quarkus.http.test-ssl-port", OptionalInt.class).orElse(8444);
+            protocol = "https";
+        } else {
+            port = config.getValue("quarkus.http.test-port", OptionalInt.class).orElse(8081);
+            protocol = "http";
+        }
+        return Optional.of(new ListeningAddress(port, protocol));
     }
 
     @Override

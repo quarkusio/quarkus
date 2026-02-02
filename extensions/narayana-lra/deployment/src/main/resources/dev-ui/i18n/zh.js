af26e2330b23cca239ccb0e2cc797605e524423c
Char <cxjava@gmail.com>
2026-02-02 12:46:49 +0800

    Update Chinese localization for Dev UI to improve clarity in LRA support description

--- extensions/narayana-lra/deployment/src/main/resources/dev-ui/i18n/zh.js
+++ extensions/narayana-lra/deployment/src/main/resources/dev-ui/i18n/zh.js
@@ -2,5 +2,5 @@ import { str } from '@lit/localize';
 
 export const templates = {
     // Metadata
-    'quarkus-narayana-lra-meta-description':'协调长时间运行的操作 (LRA)'
+    'quarkus-narayana-lra-meta-description':'提供LRA支持'
 };

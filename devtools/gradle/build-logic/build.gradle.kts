4f8663b062d616fc9c2c47ef4fc7565ebdbac43b
Alexey Loubyansky <olubyans@redhat.com>
2026-02-05 13:38:20 +0100

    Set the upper limit to Java 25 in the Gradle plugin project

--- devtools/gradle/build-logic/build.gradle.kts
+++ devtools/gradle/build-logic/build.gradle.kts
@@ -9,9 +9,9 @@ dependencies {
 java { toolchain {
     // this is fine, even for Java 1.x
     val javaMajor = JavaVersion.current().majorVersion.toInt()
-    // Need to limit the Java version for Kotlin to 17, because 20 doesn't work.
+    // Need to limit the Java version for Kotlin to 25.
     // Also prefer the current version to prevent JDK downloads.
-    languageVersion.set(JavaLanguageVersion.of(javaMajor.coerceAtMost(17)))
+    languageVersion.set(JavaLanguageVersion.of(javaMajor.coerceAtMost(25)))
 } }
 
 fun DependencyHandler.plugin(id: String, version: String) =

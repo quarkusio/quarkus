981aeed634b495a38b31364887626f3bc876bca0
Holly Cummins <holly.k.cummins@googlemail.com>
2026-02-05 16:29:07 +0000

    Convert H2 to new dev services model

--- extensions/devservices/h2/src/main/java/io/quarkus/devservices/h2/deployment/H2DevServicesProcessor.java
+++ extensions/devservices/h2/src/main/java/io/quarkus/devservices/h2/deployment/H2DevServicesProcessor.java
@@ -4,8 +4,6 @@ import static io.quarkus.datasource.deployment.spi.DatabaseDefaultSetupConfig.DE
 import static io.quarkus.datasource.deployment.spi.DatabaseDefaultSetupConfig.DEFAULT_DATABASE_PASSWORD;
 import static io.quarkus.datasource.deployment.spi.DatabaseDefaultSetupConfig.DEFAULT_DATABASE_USERNAME;
 
-import java.io.Closeable;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -19,10 +17,14 @@ import org.jboss.logging.Logger;
 
 import io.quarkus.datasource.common.runtime.DataSourceUtil;
 import io.quarkus.datasource.common.runtime.DatabaseKind;
+import io.quarkus.datasource.deployment.spi.DatasourceStartable;
+import io.quarkus.datasource.deployment.spi.DeferredDevServicesDatasourceProvider;
 import io.quarkus.datasource.deployment.spi.DevServicesDatasourceContainerConfig;
 import io.quarkus.datasource.deployment.spi.DevServicesDatasourceProvider;
 import io.quarkus.datasource.deployment.spi.DevServicesDatasourceProviderBuildItem;
+import io.quarkus.deployment.Feature;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.DevServicesComposeProjectBuildItem;
 import io.quarkus.runtime.LaunchMode;
 
 public class H2DevServicesProcessor {
@@ -31,18 +33,22 @@ public class H2DevServicesProcessor {
 
     @BuildStep
     DevServicesDatasourceProviderBuildItem setupH2() {
-        return new DevServicesDatasourceProviderBuildItem(DatabaseKind.H2, new DevServicesDatasourceProvider() {
+        return new DevServicesDatasourceProviderBuildItem(DatabaseKind.H2, new DeferredDevServicesDatasourceProvider() {
             @Override
-            public RunningDevServicesDatasource startDatabase(Optional<String> username, Optional<String> password,
+            public String getFeature() {
+                return Feature.JDBC_H2.getName();
+            }
+
+            @Override
+            public DatasourceStartable createDatasourceStartable(Optional<String> username, Optional<String> password,
                     String datasourceName, DevServicesDatasourceContainerConfig containerConfig,
-                    LaunchMode launchMode, Optional<Duration> startupTimeout) {
+                    LaunchMode launchMode, boolean useSharedNetwork, Optional<Duration> startupTimeout) {
                 try {
                     final Server tcpServer = Server.createTcpServer("-tcpPort",
                             containerConfig.getFixedExposedPort().isPresent()
                                     ? String.valueOf(containerConfig.getFixedExposedPort().getAsInt())
                                     : "0",
                             "-ifNotExists");
-                    tcpServer.start();
 
                     String effectiveUsername = containerConfig.getUsername().orElse(username.orElse(DEFAULT_DATABASE_USERNAME));
                     String effectivePassword = containerConfig.getPassword().orElse(password.orElse(DEFAULT_DATABASE_PASSWORD));
@@ -57,45 +63,100 @@ public class H2DevServicesProcessor {
                         additionalArgs.append(i.getValue());
                     }
 
-                    LOG.info("Dev Services for H2 started.");
+                    return new DatasourceStartable() {
+                        private String connectionUrl;
 
-                    String connectionUrl = "jdbc:h2:tcp://localhost:" + tcpServer.getPort() + "/mem:"
-                            + effectiveDbName
-                            + ";DB_CLOSE_DELAY=-1" + additionalArgs.toString();
-                    return new RunningDevServicesDatasource(null,
-                            connectionUrl,
-                            null,
-                            effectiveUsername,
-                            effectivePassword,
-                            new Closeable() {
-                                @Override
-                                public void close() throws IOException {
-                                    //Test first, to not make too much noise if the Server is dead already
-                                    //(perhaps we failed to start?)
-                                    if (tcpServer.isRunning(false)) {
-                                        //make sure the DB is removed on close
-                                        try (Connection connection = DriverManager.getConnection(
-                                                connectionUrl,
-                                                effectiveUsername,
-                                                effectivePassword)) {
-                                            try (Statement statement = connection.createStatement()) {
-                                                statement.execute("SET DB_CLOSE_DELAY 0");
-                                            }
-                                        } catch (SQLException t) {
-                                            t.printStackTrace();
-                                        }
-                                        tcpServer.stop();
-                                        LOG.info("Dev Services for H2 shut down; server status: " + tcpServer.getStatus());
-                                    } else {
-                                        LOG.info(
-                                                "Dev Services for H2 was NOT shut down as it appears it was down already; server status: "
-                                                        + tcpServer.getStatus());
+                        @Override
+                        public String getPassword() {
+                            return effectivePassword;
+                        }
+
+                        @Override
+                        public String getUsername() {
+                            return effectiveUsername;
+                        }
+
+                        @Override
+                        public String getReactiveUrl() {
+                            return null;
+                        }
+
+                        @Override
+                        public String getEffectiveJdbcUrl() {
+                            return connectionUrl;
+                        }
+
+                        @Override
+                        public void start() {
+                            try {
+                                tcpServer.start();
+                                connectionUrl = "jdbc:h2:tcp://localhost:" + tcpServer.getPort() + "/mem:"
+                                        + effectiveDbName
+                                        + ";DB_CLOSE_DELAY=-1" + additionalArgs;
+                            } catch (SQLException throwables) {
+                                throw new RuntimeException(throwables);
+                            }
+                        }
+
+                        @Override
+                        public void close() {
+                            //Test first, to not make too much noise if the Server is dead already
+                            //(perhaps we failed to start?)
+                            if (tcpServer.isRunning(false)) {
+                                //make sure the DB is removed on close
+                                try (Connection connection = DriverManager.getConnection(
+                                        connectionUrl,
+                                        effectiveUsername,
+                                        effectivePassword)) {
+                                    try (Statement statement = connection.createStatement()) {
+                                        statement.execute("SET DB_CLOSE_DELAY 0");
                                     }
+                                } catch (SQLException t) {
+                                    t.printStackTrace();
                                 }
-                            });
+                                tcpServer.stop();
+                                LOG.info("Dev Services for H2 shut down; server status: " + tcpServer.getStatus());
+                            } else {
+                                LOG.info(
+                                        "Dev Services for H2 was NOT shut down as it appears it was down already; server status: "
+                                                + tcpServer.getStatus());
+                            }
+                        }
+
+                        @Override
+                        public String getConnectionInfo() {
+                            return getEffectiveJdbcUrl();
+                        }
+
+                        @Override
+                        public String getContainerId() {
+                            return null;
+                        }
+
+                        @Override
+                        public DevServicesDatasourceProvider.RunningDevServicesDatasource runningDevServicesDatasource() {
+                            // Override, so we can check the connection url instead of the container id
+                            if (connectionUrl == null) {
+                                throw new IllegalStateException(
+                                        "Cannot create a RunningDevServicesDatasource before the H2 datasource has been started.");
+                            }
+                            // It would be nice to cache this, but since this is an interface, it can't be done at this level, and would have to be done by every implementing class
+                            return new DevServicesDatasourceProvider.RunningDevServicesDatasource(getContainerId(),
+                                    getEffectiveJdbcUrl(),
+                                    getReactiveUrl(), getUsername(), getPassword());
+                        }
+
+                    };
                 } catch (SQLException throwables) {
                     throw new RuntimeException(throwables);
                 }
+            }
+
+            @Override
+            public Optional<DevServicesDatasourceProvider.RunningDevServicesDatasource> findRunningComposeDatasource(
+                    LaunchMode launchMode, boolean useSharedNetwork, DevServicesDatasourceContainerConfig containerConfig,
+                    DevServicesComposeProjectBuildItem composeProjectBuildItem) {
+                return Optional.empty();
             }
 
             @Override

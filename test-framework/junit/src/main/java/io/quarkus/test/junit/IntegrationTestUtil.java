ed4c4e6f78b61ca5e63b58104969e792f22f4a78
Roberto Cortez <radcortez@yahoo.com>
2026-02-07 17:35:39 +0000

    Do not use System Properties to set test profile configuration

--- test-framework/junit/src/main/java/io/quarkus/test/junit/IntegrationTestUtil.java
+++ test-framework/junit/src/main/java/io/quarkus/test/junit/IntegrationTestUtil.java
@@ -9,7 +9,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -21,12 +20,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
-import jakarta.enterprise.inject.Alternative;
 import jakarta.inject.Inject;
 
 import org.apache.commons.lang3.RandomStringUtils;
@@ -122,35 +118,6 @@ public final class IntegrationTestUtil {
         Map<String, String> sysPropRestore = new HashMap<>();
         sysPropRestore.put(LaunchMode.DEVELOPMENT.getProfileKey(), System.getProperty(LaunchMode.TEST.getProfileKey()));
         return sysPropRestore;
-    }
-
-    static TestProfileAndProperties determineTestProfileAndProperties(Class<? extends QuarkusTestProfile> profile)
-            throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        final Map<String, String> properties = new HashMap<>();
-        QuarkusTestProfile testProfile = null;
-        if (profile != null) {
-            testProfile = profile.getDeclaredConstructor().newInstance();
-            properties.putAll(testProfile.getConfigOverrides());
-            final Set<Class<?>> enabledAlternatives = testProfile.getEnabledAlternatives();
-            if (!enabledAlternatives.isEmpty()) {
-                properties.put("quarkus.arc.selected-alternatives", enabledAlternatives.stream()
-                        .peek((c) -> {
-                            if (!c.isAnnotationPresent(Alternative.class)) {
-                                throw new RuntimeException(
-                                        "Enabled alternative " + c + " is not annotated with @Alternative");
-                            }
-                        })
-                        .map(Class::getName).collect(Collectors.joining(",")));
-            }
-            final String configProfile = testProfile.getConfigProfile();
-            if (configProfile != null) {
-                properties.put(LaunchMode.NORMAL.getProfileKey(), configProfile);
-            }
-            properties.put("quarkus.config.build-time-mismatch-at-runtime", "fail");
-        }
-        // recalculate the property names that may have changed
-        ConfigProvider.getConfig().unwrap(SmallRyeConfig.class).getLatestPropertyNames();
-        return new TestProfileAndProperties(testProfile, properties);
     }
 
     static Optional<ListeningAddress> startLauncher(ArtifactLauncher<?> launcher, Map<String, String> additionalProperties)

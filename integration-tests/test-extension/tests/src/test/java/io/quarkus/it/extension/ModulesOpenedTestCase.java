2942e71697ebc348af33deecc2077b4a4c6eb017
Guillaume Smet <guillaume.smet@gmail.com>
2026-02-13 15:09:46 +0100

    Only apply the JvmModulesReconfigurer machinery to JDK >= 25
    (cherry picked from commit 1f2ad7fb3fe764d577c104fc11e4fff53945c729)

--- integration-tests/test-extension/tests/src/test/java/io/quarkus/it/extension/ModulesOpenedTestCase.java
+++ integration-tests/test-extension/tests/src/test/java/io/quarkus/it/extension/ModulesOpenedTestCase.java
@@ -4,12 +4,16 @@ import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
 
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class ModulesOpenedTestCase {
+
     @Test
+    @DisabledForJreRange(max = JRE.JAVA_24)
     public void test() {
         when().get("/core/modulesOpen").then()
                 .body(is("OK"));

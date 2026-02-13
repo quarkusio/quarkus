7a92e9d0c7da1a2e63ba68c727dad78e8d8966c6
Stéphane Épardaud <stef@epardaud.fr>
2026-02-13 11:26:07 +0100

    Hibernate Panache Next: fix findById result types for reactive repositories
    Fixes #52581

--- extensions/panache/hibernate-panache-next/runtime/src/main/java/io/quarkus/hibernate/panache/stateless/reactive/PanacheStatelessReactiveRepositoryQueries.java
+++ extensions/panache/hibernate-panache-next/runtime/src/main/java/io/quarkus/hibernate/panache/stateless/reactive/PanacheStatelessReactiveRepositoryQueries.java
@@ -23,13 +23,13 @@ public interface PanacheStatelessReactiveRepositoryQueries<Entity, Id> extends P
     }
 
     @Override
-    default Entity findById(Id id) {
-        return (Entity) operations().findById(getEntityClass(), id);
+    default Uni<Entity> findById(Id id) {
+        return (Uni) operations().findById(getEntityClass(), id);
     }
 
     @Override
-    default Entity findById(Id id, LockModeType lockModeType) {
-        return (Entity) operations().findById(getEntityClass(), id, lockModeType);
+    default Uni<Entity> findById(Id id, LockModeType lockModeType) {
+        return (Uni) operations().findById(getEntityClass(), id, lockModeType);
     }
 
     @Override

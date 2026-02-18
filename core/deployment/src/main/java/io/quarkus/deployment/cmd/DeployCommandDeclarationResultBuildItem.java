495ab75b9e264973076c18a83e96f3ddafc14f51
Matheus André <92062874+matheusandre1@users.noreply.github.com>
2026-02-18 15:05:26 -0300

    docs: DeployCommandDeclarationResultBuildItem

--- core/deployment/src/main/java/io/quarkus/deployment/cmd/DeployCommandDeclarationResultBuildItem.java
+++ core/deployment/src/main/java/io/quarkus/deployment/cmd/DeployCommandDeclarationResultBuildItem.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 import io.quarkus.builder.item.SimpleBuildItem;
 
+/**
+ * A {@link SimpleBuildItem} that represents the aggregate result of all
+ * deployment actions.
+ * This is used by build tools to report back the overall outcome of the
+ * deployment process.
+ */
 public final class DeployCommandDeclarationResultBuildItem extends SimpleBuildItem {
     private final List<String> commands;
 

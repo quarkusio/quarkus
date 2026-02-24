5ceabb9882edc2607c8fb22f11f49922e66103ee
Roman Huber <26764588+alerosmile@users.noreply.github.com>
2026-02-24 22:27:04 +0100

    Re-enabling O2N and Vert.x tests (bug in Vert.x has been fixed)

--- integration-tests/grpc-mutiny-cancellation/src/test/java/io/quarkus/grpc/examples/cancellation/VertxCancellationTest.java
+++ integration-tests/grpc-mutiny-cancellation/src/test/java/io/quarkus/grpc/examples/cancellation/VertxCancellationTest.java
@@ -1,12 +1,9 @@
 package io.quarkus.grpc.examples.cancellation;
 
-import org.junit.jupiter.api.Disabled;
-
 import io.quarkus.grpc.test.utils.VertxGRPCTestProfile;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 
-@Disabled
 @QuarkusTest
 @TestProfile(VertxGRPCTestProfile.class)
 public class VertxCancellationTest extends CancellationTest {

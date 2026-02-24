504db87ddf0465d16525c7ef8f836af4860b86bd
Alexey Loubyansky <olubyans@redhat.com>
2026-02-24 14:16:31 +0100

    Test for quarkus.test.arg-line using  as value
    (cherry picked from commit 783528c72bac8e93f2d830fbf7350af4d0b4ebdb)

---
+++ integration-tests/maven/src/test/resources-filtered/projects/classic/src/test/java/org/acme/HelloResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HelloResourceIT extends HelloResourceTest {
+    // Execute the same tests but in packaged mode.
+}

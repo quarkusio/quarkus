20d43e2882f3c08526ba59064857c2b5151f66cd
Sergey Beryozkin <sberyozkin@gmail.com>
2026-03-05 19:18:36 +0000

    Add Vert.x HTTP HostValidationFilter

---
+++ extensions/vertx-http/deployment/src/test/java/io/quarkus/vertx/http/management/ManagementHostValidationAnyHostTest.java
@@ -0,0 +1,69 @@
+package io.quarkus.vertx.http.management;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.net.URL;
+
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Singleton;
+
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.vertx.http.ManagementInterface;
+
+public class ManagementHostValidationAnyHostTest {
+
+    private static final String configuration = """
+            quarkus.management.enabled=true
+            quarkus.management.root-path=/management
+            quarkus.management.host=localhost
+            quarkus.management.host-validation.require-localhost=false
+            """;
+
+    @RegisterExtension
+    static QuarkusUnitTest runner = new QuarkusUnitTest()
+            .withApplicationRoot((jar) -> jar
+                    .addAsResource(new StringAsset(configuration), "application.properties")
+                    .addClasses(MyObserver.class));
+
+    @TestHTTPResource(value = "/my-route", management = true)
+    URL myRoute;
+
+    @Test
+    void localHostloopbackRequest() {
+        given().header("Host", "127.0.0.1:9001")
+                .get(myRoute).then()
+                .statusCode(200)
+                .body(equalTo("test route"));
+    }
+
+    @Test
+    void localHostRequest() {
+        given().header("Host", "localhost:9001")
+                .get(myRoute).then()
+                .statusCode(200)
+                .body(equalTo("test route"));
+    }
+
+    @Test
+    void publicHostRequest() {
+        given().header("Host", "public-api.com:9001")
+                .get(myRoute).then()
+                .statusCode(200)
+                .body(equalTo("test route"));
+    }
+
+    @Singleton
+    static class MyObserver {
+
+        public void registerManagementRoutes(@Observes ManagementInterface mi) {
+            mi.router().get("/management/my-route").handler(rc -> rc.response().end("test route"));
+        }
+
+    }
+}

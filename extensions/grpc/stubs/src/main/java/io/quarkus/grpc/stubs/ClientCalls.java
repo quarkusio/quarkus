11ff8fb2db6385ed99bb7fb62b5c01123e716a6c
Arthur Navarro <arnavarr@ibm.com>
2026-03-16 11:08:42 +0100

    feat: add traffic control to quarkus-grpc
    By leveraging grpc experimental apis `ServerCallStreamObserver` and
    `ClientCallStreamObserver`, it is now possible to regulate the rate at
    which grpc server sends data to grpc client.
    On the server side, the sending will be triggered by a `onReady(cb)`
    callback leveraging HTTP2 readiness based on window size.
    On the client, it is now possible to set a `request()` strategy.
    
    It is important to note that the client `request()` api does not request
    data directly from the server. It requests data from the
    `DirectMemoryBuffer` on the client side. The rate at which the client
    frees DirectMemoryBuffer (by paring buffers and instantiating grpc
    types in heap) changes how frequently the HTTP2 window size will be large
    enough to receive data, and thus how frequently the server `onReady()`
    callback will be called.
    
    We also implemented a way to consume MultiEmitters lazily on the server
    side to avoid OOME: if a Multi creates instance eagerly, it might create
    too many instances in heap that are sent slowly to a slow client. They
    accumulate in heap on the server side and crash it.
    
    Example of a 1-N call from a client:
    
    *server side*
    service method definition -> Mutli ----|heap|----> ServerCallStreamObserver
    
    ----> network ---->
    
    *client side*
    direct memory buffer ----> ClientCallStreamObserver --|heap|--> Multi
    consumed by service method definition.
    
    HTTP2 window size is responsible for avoiding direct memory buffer
    running out of memory and ending the session thanks to the
    ServerCallStreamObserver.

--- extensions/grpc/stubs/src/main/java/io/quarkus/grpc/stubs/ClientCalls.java
+++ extensions/grpc/stubs/src/main/java/io/quarkus/grpc/stubs/ClientCalls.java
@@ -1,19 +1,37 @@
 package io.quarkus.grpc.stubs;
 
 import java.util.concurrent.Flow;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+import org.jctools.queues.SpscChunkedArrayQueue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.grpc.stub.ClientCallStreamObserver;
+import io.grpc.stub.ClientResponseObserver;
+import io.grpc.stub.ServerCallStreamObserver;
 import io.grpc.stub.StreamObserver;
+import io.smallrye.mutiny.Context;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.helpers.Subscriptions;
 import io.smallrye.mutiny.subscription.MultiEmitter;
+import io.smallrye.mutiny.subscription.Subscribers;
 import io.smallrye.mutiny.subscription.UniEmitter;
 
 public class ClientCalls {
+
+    final static long INITIAL_BATCH = 100;
+    final static long REQUEST_BATCH = 1;
+
+    final static String ERROR_CAST_STREAM_OBSERVER = String.format("%s can't be casted as a %s", StreamObserver.class,
+            ServerCallStreamObserver.class);
+    private static final Logger log = LoggerFactory.getLogger(ClientCalls.class);
 
     private ClientCalls() {
     }
@@ -31,7 +49,7 @@ public class ClientCalls {
         return Multi.createFrom().emitter(new Consumer<MultiEmitter<? super O>>() { // NOSONAR
             @Override
             public void accept(MultiEmitter<? super O> emitter) {
-                delegate.accept(request, new MultiStreamObserver<>(emitter));
+                delegate.accept(request, new MultiClientCallStreamObserver<>(emitter));
             }
         });
     }
@@ -41,70 +59,143 @@ public class ClientCalls {
             @Override
             public void accept(UniEmitter<? super O> emitter) {
                 AtomicReference<Flow.Subscription> cancellable = new AtomicReference<>();
+                AtomicReference<Runnable> drainRef = new AtomicReference<>();
                 UniStreamObserver<O> observer = new UniStreamObserver<>(emitter.onTermination(() -> {
                     var subscription = cancellable.getAndSet(Subscriptions.CANCELLED);
                     if (subscription != null) {
                         subscription.cancel();
                     }
                 }));
-                StreamObserver<I> request = delegate.apply(observer);
-                subscribeToUpstreamAndForwardToStreamObserver(items, cancellable, request);
+
+                ClientResponseObserver<I, O> responseObserver = new ClientResponseObserver<>() {
+                    @Override
+                    public void beforeStart(ClientCallStreamObserver<I> requestStream) {
+                        requestStream.setOnReadyHandler(() -> {
+                            Runnable drain = drainRef.get();
+                            if (drain != null) {
+                                drain.run();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onNext(O value) {
+                        observer.onNext(value);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        observer.onError(t);
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        observer.onCompleted();
+                    }
+                };
+
+                StreamObserver<I> request = delegate.apply(responseObserver);
+
+                if (request instanceof ClientCallStreamObserver<I>) {
+                    drainLoop((ClientCallStreamObserver<I>) request, items, cancellable, drainRef);
+                } else {
+                    responseObserver.onError(new Throwable(ERROR_CAST_STREAM_OBSERVER));
+                    log.error(ERROR_CAST_STREAM_OBSERVER);
+                }
             }
-
         }));
-    }
-
-    private static <I> void subscribeToUpstreamAndForwardToStreamObserver(Multi<I> items,
-            AtomicReference<Flow.Subscription> cancellable,
-            StreamObserver<I> request) {
-        items.subscribe().with(
-                new Consumer<Flow.Subscription>() {
-                    @Override
-                    public void accept(Flow.Subscription subscription) {
-                        if (!cancellable.compareAndSet(null, subscription)) {
-                            subscription.cancel();
-                        } else {
-                            subscription.request(Long.MAX_VALUE);
-                        }
-                    }
-                },
-
-                new Consumer<I>() {
-                    @Override
-                    public void accept(I v) {
-                        if (cancellable.get() != null && cancellable.get() != Subscriptions.CANCELLED) {
-                            request.onNext(v);
-                        }
-                    }
-                },
-                new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) {
-                        request.onError(throwable);
-                    }
-                },
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        request.onCompleted();
-                    }
-                });
     }
 
     public static <I, O> Multi<O> manyToMany(Multi<I> items, Function<StreamObserver<O>, StreamObserver<I>> delegate) {
+
         return Multi.createFrom().emitter((new Consumer<MultiEmitter<? super O>>() {
+
             @Override
             public void accept(MultiEmitter<? super O> emitter) {
                 AtomicReference<Flow.Subscription> cancellable = new AtomicReference<>();
-                StreamObserver<I> request = delegate.apply(new MultiStreamObserver<>(emitter.onTermination(() -> {
-                    var subscription = cancellable.getAndSet(Subscriptions.CANCELLED);
-                    if (subscription != null) {
-                        subscription.cancel();
+                AtomicReference<Runnable> drainRef = new AtomicReference<>();
+
+                MultiClientCallStreamObserver<I, O> responseObserver = new MultiClientCallStreamObserver<>(
+                        emitter.onTermination(() -> {
+                            var subscription = cancellable.getAndSet(Subscriptions.CANCELLED);
+                            if (subscription != null) {
+                                subscription.cancel();
+                            }
+                        })) {
+                    @Override
+                    public void beforeStart(ClientCallStreamObserver<I> requestStream) {
+                        super.beforeStart(requestStream);
+                        requestStream.setOnReadyHandler(() -> {
+                            Runnable drain = drainRef.get();
+                            if (drain != null) {
+                                drain.run();
+                            }
+                        });
                     }
-                })));
-                subscribeToUpstreamAndForwardToStreamObserver(items, cancellable, request);
+                };
+
+                StreamObserver<I> request = delegate.apply(responseObserver);
+                if (request instanceof ClientCallStreamObserver<I>) {
+                    drainLoop((ClientCallStreamObserver<I>) request, items, cancellable, drainRef);
+                } else {
+                    responseObserver.onError(new Throwable(ERROR_CAST_STREAM_OBSERVER));
+                    log.error(ERROR_CAST_STREAM_OBSERVER);
+                }
             }
         }));
+    }
 
+    private static <I> void drainLoop(ClientCallStreamObserver<I> requestFlow, Multi<I> items,
+            AtomicReference<Flow.Subscription> cancellable, AtomicReference<Runnable> drainRef) {
+        SpscChunkedArrayQueue<I> queue = new SpscChunkedArrayQueue<>(128, 256);
+        AtomicBoolean done = new AtomicBoolean(false);
+        AtomicInteger wip = new AtomicInteger(0);
+
+        AtomicReference<Flow.Subscription> subscription = new AtomicReference<>();
+
+        drainRef.set(() -> {
+            if (subscription.get() == null || wip.getAndIncrement() != 0) {
+                return;
+            }
+            try {
+                do {
+                    while (requestFlow.isReady()) {
+                        I item = queue.poll();
+                        if (item == null) {
+                            if (done.get()) {
+                                requestFlow.onCompleted();
+                                return;
+                            }
+                            break;
+                        }
+                        requestFlow.onNext(item);
+                        subscription.get().request(REQUEST_BATCH);
+                    }
+                } while (wip.decrementAndGet() != 0);
+            } catch (Throwable t) {
+                requestFlow.onError(t);
+            }
+        });
+
+        items.subscribe().withSubscriber(Subscribers.from(
+                Context.empty(),
+                item -> {
+                    if (cancellable.get() != null && cancellable.get() != Subscriptions.CANCELLED) {
+                        queue.offer(item);
+                        drainRef.get().run();
+                    }
+                },
+                requestFlow::onError,
+                () -> {
+                    done.set(true);
+                    drainRef.get().run();
+                },
+                s -> {
+                    if (!cancellable.compareAndSet(null, s)) {
+                        s.cancel();
+                    }
+                    subscription.set(s);
+                    s.request(INITIAL_BATCH);
+                }));
     }
 }
--- extensions/grpc/stubs/src/main/java/io/quarkus/grpc/stubs/ServerCalls.java
+++ extensions/grpc/stubs/src/main/java/io/quarkus/grpc/stubs/ServerCalls.java
@@ -1,20 +1,33 @@
 package io.quarkus.grpc.stubs;
 
+import java.util.concurrent.Flow;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
 import org.jboss.logging.Logger;
+import org.jctools.queues.SpscChunkedArrayQueue;
 
 import io.grpc.Status;
 import io.grpc.stub.ServerCallStreamObserver;
 import io.grpc.stub.StreamObserver;
 import io.quarkus.arc.Arc;
 import io.quarkus.grpc.ExceptionHandlerProvider;
+import io.smallrye.mutiny.Context;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.operators.multi.processors.UnicastProcessor;
-import io.smallrye.mutiny.subscription.Cancellable;
+import io.smallrye.mutiny.subscription.Subscribers;
 
 public class ServerCalls {
+
+    final static long INITIAL_BATCH = 100;
+    final static long REQUEST_BATCH = 1;
+
+    final static String ERROR_CAST_STREAM_OBSERVER = String.format("%s can't be casted as a %s", StreamObserver.class,
+            ServerCallStreamObserver.class);
+
     private static final Logger log = Logger.getLogger(ServerCalls.class);
 
     private static StreamCollector streamCollector = StreamCollector.NO_OP;
@@ -48,22 +61,20 @@ public class ServerCalls {
 
     public static <I, O> void oneToMany(I request, StreamObserver<O> response, String compression,
             Function<I, Multi<O>> implementation) {
-        try {
-            trySetCompression(response, compression);
-            streamCollector.add(response);
-            Multi<O> returnValue = implementation.apply(request);
-            if (returnValue == null) {
-                log.error("gRPC service method returned null instead of Multi. " +
-                        "Please change the implementation to return a Multi object or throw StatusRuntimeException");
-                onError(response, Status.fromCode(Status.Code.INTERNAL).asException());
-                return;
+        if (response instanceof ServerCallStreamObserver<O>) {
+            ServerCallStreamObserver<O> responseFlowControl = (ServerCallStreamObserver<O>) response;
+            try {
+                trySetCompression(responseFlowControl, compression);
+                streamCollector.add(responseFlowControl);
+                var multi = implementation.apply(request);
+
+                handleSubscription(multi, responseFlowControl);
+            } catch (Throwable throwable) {
+                onError(responseFlowControl, throwable);
             }
-            handleSubscription(returnValue.subscribe().with(
-                    response::onNext,
-                    throwable -> onError(response, throwable),
-                    () -> onCompleted(response)), response);
-        } catch (Throwable throwable) {
-            onError(response, throwable);
+        } else {
+            onError(response, new Throwable(ERROR_CAST_STREAM_OBSERVER));
+            log.error(ERROR_CAST_STREAM_OBSERVER);
         }
     }
 
@@ -95,40 +106,89 @@ public class ServerCalls {
         }
     }
 
-    private static <O> void handleSubscription(Cancellable cancellable, StreamObserver<O> response) {
-        if (response instanceof ServerCallStreamObserver) {
-            ServerCallStreamObserver<O> serverCallResponse = (ServerCallStreamObserver<O>) response;
+    public static <I, O> StreamObserver<I> manyToMany(StreamObserver<O> response,
+            Function<Multi<I>, Multi<O>> implementation) {
+        if (response instanceof ServerCallStreamObserver<O>) {
+            ServerCallStreamObserver<O> responseFlowControl = (ServerCallStreamObserver<O>) response;
+            try {
+                streamCollector.add(responseFlowControl);
+                UnicastProcessor<I> input = UnicastProcessor.create();
+                StreamObserver<I> pump = getStreamObserverFeedingProcessor(input);
+                Multi<O> multi = implementation.apply(input);
+                if (multi == null) {
+                    log.error("gRPC service method returned null instead of Multi. " +
+                            "Please change the implementation to return a Multi object or throw StatusRuntimeException");
+                    onError(responseFlowControl, Status.fromCode(Status.Code.INTERNAL).asException());
+                    return null;
+                }
 
-            Runnable cancel = cancellable::cancel;
-
-            serverCallResponse.setOnCloseHandler(cancel);
-            serverCallResponse.setOnCancelHandler(cancel);
+                handleSubscription(multi, responseFlowControl);
+                return pump;
+            } catch (Throwable throwable) {
+                onError(responseFlowControl, throwable);
+                return null;
+            }
+        } else {
+            log.error(ERROR_CAST_STREAM_OBSERVER);
+            onError(response, new Throwable(ERROR_CAST_STREAM_OBSERVER));
+            return null;
         }
     }
 
-    public static <I, O> StreamObserver<I> manyToMany(StreamObserver<O> response,
-            Function<Multi<I>, Multi<O>> implementation) {
-        try {
-            streamCollector.add(response);
-            UnicastProcessor<I> input = UnicastProcessor.create();
-            StreamObserver<I> pump = getStreamObserverFeedingProcessor(input);
-            Multi<O> multi = implementation.apply(input);
-            if (multi == null) {
-                log.error("gRPC service method returned null instead of Multi. " +
-                        "Please change the implementation to return a Multi object or throw StatusRuntimeException");
-                onError(response, Status.fromCode(Status.Code.INTERNAL).asException());
-                return null;
-            }
-            handleSubscription(multi.subscribe().with(
-                    response::onNext,
-                    failure -> onError(response, failure),
-                    () -> onCompleted(response)), response);
+    public static <O> void handleSubscription(Multi<O> items, ServerCallStreamObserver<O> response) {
+        SpscChunkedArrayQueue<O> queue = new SpscChunkedArrayQueue<>(64, 256);
+        AtomicBoolean done = new AtomicBoolean(false);
+        AtomicInteger wip = new AtomicInteger(0);
 
-            return pump;
-        } catch (Throwable throwable) {
-            onError(response, throwable);
-            return null;
-        }
+        AtomicReference<Flow.Subscription> subscription = new AtomicReference<>();
+
+        Runnable drain = new Runnable() {
+            @Override
+            public void run() {
+                if (wip.getAndIncrement() != 0) {
+                    return;
+                }
+                try {
+                    do {
+                        while (response.isReady()) {
+                            O item = queue.poll();
+                            if (item == null) {
+                                if (done.get()) {
+                                    response.onCompleted();
+                                    return;
+                                }
+                                break;
+                            }
+                            response.onNext(item);
+                            subscription.get().request(REQUEST_BATCH);
+                        }
+                    } while (wip.decrementAndGet() != 0);
+                } catch (Throwable t) {
+                    onError(response, t);
+                }
+            }
+        };
+
+        var cancellable = items.subscribe().withSubscriber(Subscribers.from(
+                Context.empty(),
+                item -> {
+                    queue.offer(item);
+                    drain.run();
+                },
+                failure -> onError(response, failure),
+                () -> {
+                    done.set(true);
+                    drain.run();
+                },
+                s -> {
+                    subscription.set(s);
+                    s.request(INITIAL_BATCH);
+                }));
+
+        response.setOnReadyHandler(drain);
+        response.setOnCloseHandler(cancellable::cancel);
+        response.setOnCancelHandler(cancellable::cancel);
+        drain.run();
     }
 
     private static <O> void onCompleted(StreamObserver<O> response) {

c26dfbbe22259fd4307455881e4c86b1ef15ab92
marco sappe griot <msappegr@ibm.com>
2026-03-18 16:52:32 +0100

    issue 28144
    Implement tests and method to get the status of the recoveryService

--- extensions/narayana-jta/runtime/src/main/java/io/quarkus/narayana/jta/runtime/TransactionManagerConfiguration.java
+++ extensions/narayana-jta/runtime/src/main/java/io/quarkus/narayana/jta/runtime/TransactionManagerConfiguration.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 
+import io.quarkus.runtime.annotations.ConfigDocDefault;
 import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
@@ -41,9 +42,12 @@ public interface TransactionManagerConfiguration {
 
     /**
      * Start the recovery service on startup.
+     * <p>
+     * If not set, the recovery service will be started automatically if XA datasources are configured.
+     * Set to {@code true} to always enable recovery, or {@code false} to explicitly disable it.
      */
-    @WithDefault("false")
-    boolean enableRecovery();
+    @ConfigDocDefault("`true` if XA datasources are configured, `false` otherwise")
+    Optional<Boolean> enableRecovery();
 
     /**
      * The list of recovery modules.

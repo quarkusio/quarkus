134cf593b196c1ee844f994641e2e8add6780e06
Stéphane Épardaud <stef@epardaud.fr>
2026-03-19 16:01:28 +0100

    Panache Next: test that toplevel repositories don't produce infinite files
    See https://github.com/hibernate/hibernate-orm/pull/12010
    Fixes #52975

---
+++ extensions/panache/hibernate-panache-next/deployment/src/test/java/io/quarkus/hibernate/panache/deployment/test/MyEntityRepository.java
@@ -0,0 +1,6 @@
+package io.quarkus.hibernate.panache.deployment.test;
+
+import io.quarkus.hibernate.panache.PanacheRepository;
+
+public class MyEntityRepository implements PanacheRepository<MyEntity> {
+}

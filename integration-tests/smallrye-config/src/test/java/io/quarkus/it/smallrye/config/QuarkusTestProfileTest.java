4f59c2627ffc0bc762c0d1b33f4c3c511722a3b4
Roberto Cortez <radcortez@yahoo.com>
2026-04-01 16:09:49 +0100

    Ensure that shared Config build time runtime fixed uses the expected profile when set from @TestProfile
    (cherry picked from commit b057001215cc5e41a7ebc4d35d0b006290b5b94c)

--- integration-tests/smallrye-config/src/test/java/io/quarkus/it/smallrye/config/QuarkusTestProfileTest.java
+++ integration-tests/smallrye-config/src/test/java/io/quarkus/it/smallrye/config/QuarkusTestProfileTest.java
@@ -3,15 +3,19 @@ package io.quarkus.it.smallrye.config;
 import static io.restassured.RestAssured.given;
 import static jakarta.ws.rs.core.Response.Status.OK;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import jakarta.inject.Inject;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.QuarkusTestProfile;
 import io.quarkus.test.junit.TestProfile;
+import io.quarkus.vertx.http.runtime.VertxHttpBuildTimeConfig;
 
 @QuarkusTest
 @TestProfile(QuarkusTestProfileTest.TestProfile.class)
@@ -31,12 +35,21 @@ public class QuarkusTestProfileTest {
                 .body("value", equalTo("custom"));
     }
 
+    @Inject
+    VertxHttpBuildTimeConfig vertxHttpBuildTimeConfig;
+
+    @Test
+    void profiledBuildTimeRuntimeFixed() {
+        assertTrue(vertxHttpBuildTimeConfig.enableCompression());
+    }
+
     public static class TestProfile implements QuarkusTestProfile {
         @Override
         public Map<String, String> getConfigOverrides() {
             Map<String, String> configs = new HashMap<>();
             configs.put("quarkus.config.locations", "test-profile.properties");
             configs.put("smallrye.config.locations", "relocate.properties");
+            configs.put("%custom.quarkus.http.enable-compression", "true");
             return configs;
         }
 

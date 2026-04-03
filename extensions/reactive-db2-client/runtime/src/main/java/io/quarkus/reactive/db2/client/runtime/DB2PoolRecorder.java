b84757f5172cbbca002129b33a469919ad7e41b1
Clement Escoffier <clement@apache.org>
2026-04-03 12:16:13 +0200

    Simplify DB2 client extension to use the new API from reactive-datasource infrastructure

--- extensions/reactive-db2-client/runtime/src/main/java/io/quarkus/reactive/db2/client/runtime/DB2PoolRecorder.java
+++ extensions/reactive-db2-client/runtime/src/main/java/io/quarkus/reactive/db2/client/runtime/DB2PoolRecorder.java
@@ -1,20 +1,8 @@
 package io.quarkus.reactive.db2.client.runtime;
 
-import static io.quarkus.credentials.CredentialsProvider.PASSWORD_PROPERTY_NAME;
-import static io.quarkus.credentials.CredentialsProvider.USER_PROPERTY_NAME;
 import static io.quarkus.reactive.datasource.runtime.ReactiveDataSourceUtil.qualifier;
-import static io.quarkus.reactive.datasource.runtime.UnitisedTime.unitised;
-import static io.quarkus.vertx.core.runtime.SSLConfigHelper.configureJksKeyCertOptions;
-import static io.quarkus.vertx.core.runtime.SSLConfigHelper.configureJksTrustOptions;
-import static io.quarkus.vertx.core.runtime.SSLConfigHelper.configurePemKeyCertOptions;
-import static io.quarkus.vertx.core.runtime.SSLConfigHelper.configurePemTrustOptions;
-import static io.quarkus.vertx.core.runtime.SSLConfigHelper.configurePfxKeyCertOptions;
-import static io.quarkus.vertx.core.runtime.SSLConfigHelper.configurePfxTrustOptions;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -23,28 +11,26 @@ import jakarta.enterprise.util.TypeLiteral;
 
 import org.jboss.logging.Logger;
 
-import io.quarkus.arc.ActiveResult;
 import io.quarkus.arc.SyntheticCreationalContext;
-import io.quarkus.credentials.CredentialsProvider;
-import io.quarkus.credentials.runtime.CredentialsProviderFinder;
-import io.quarkus.datasource.common.runtime.DataSourceUtil;
 import io.quarkus.datasource.runtime.DataSourceRuntimeConfig;
 import io.quarkus.datasource.runtime.DataSourcesRuntimeConfig;
-import io.quarkus.reactive.datasource.runtime.ConnectOptionsSupplier;
+import io.quarkus.reactive.datasource.PoolCreator;
 import io.quarkus.reactive.datasource.runtime.DataSourceReactiveRuntimeConfig;
 import io.quarkus.reactive.datasource.runtime.DataSourcesReactiveRuntimeConfig;
+import io.quarkus.reactive.datasource.runtime.ReactivePoolUtil;
 import io.quarkus.reactive.db2.client.DB2PoolCreator;
 import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.ShutdownContext;
 import io.quarkus.runtime.annotations.Recorder;
+import io.quarkus.tls.TlsConfigurationRegistry;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.core.impl.VertxInternal;
+import io.vertx.core.internal.VertxInternal;
+import io.vertx.db2client.DB2Builder;
 import io.vertx.db2client.DB2ConnectOptions;
-import io.vertx.db2client.DB2Pool;
-import io.vertx.db2client.spi.DB2Driver;
+import io.vertx.sqlclient.Pool;
 import io.vertx.sqlclient.PoolOptions;
-import io.vertx.sqlclient.impl.Utils;
+import io.vertx.sqlclient.SqlConnectOptions;
 
 @Recorder
 public class DB2PoolRecorder {
@@ -52,7 +38,7 @@ public class DB2PoolRecorder {
     private static final boolean SUPPORTS_CACHE_PREPARED_STATEMENTS = true;
 
     private static final Logger log = Logger.getLogger(DB2PoolRecorder.class);
-    private static final TypeLiteral<Instance<DB2PoolCreator>> POOL_CREATOR_TYPE_LITERAL = new TypeLiteral<>() {
+    private static final TypeLiteral<Instance<PoolCreator>> POOL_CREATOR_TYPE_LITERAL = new TypeLiteral<>() {
     };
 
     private final RuntimeValue<DataSourcesRuntimeConfig> runtimeConfig;
@@ -68,119 +54,47 @@ public class DB2PoolRecorder {
         this.reactiveDB2RuntimeConfig = reactiveDB2RuntimeConfig;
     }
 
-    public Supplier<ActiveResult> poolCheckActiveSupplier(String dataSourceName) {
-        return new Supplier<>() {
-            @Override
-            public ActiveResult get() {
-                Optional<Boolean> active = runtimeConfig.getValue().dataSources().get(dataSourceName).active();
-                if (active.isPresent() && !active.get()) {
-                    return ActiveResult.inactive(DataSourceUtil.dataSourceInactiveReasonDeactivated(dataSourceName));
-                }
-                if (reactiveRuntimeConfig.getValue().dataSources().get(dataSourceName).reactive().url().isEmpty()) {
-                    return ActiveResult.inactive(DataSourceUtil.dataSourceInactiveReasonUrlMissing(dataSourceName,
-                            "reactive.url"));
-                }
-                return ActiveResult.active();
-            }
-        };
-    }
-
-    public Function<SyntheticCreationalContext<DB2Pool>, DB2Pool> configureDB2Pool(RuntimeValue<Vertx> vertx,
-            Supplier<Integer> eventLoopCount, String dataSourceName, ShutdownContext shutdown) {
+    public Function<SyntheticCreationalContext<Pool>, Pool> configureDB2Pool(RuntimeValue<Vertx> vertx,
+            Supplier<Integer> eventLoopCount, String dataSourceName, ShutdownContext shutdown,
+            Supplier<TlsConfigurationRegistry> tlsRegistrySupplier) {
         return new Function<>() {
             @Override
-            public DB2Pool apply(SyntheticCreationalContext<DB2Pool> context) {
-                DB2Pool db2Pool = initialize((VertxInternal) vertx.getValue(),
+            public Pool apply(SyntheticCreationalContext<Pool> context) {
+                Pool pool = initialize((VertxInternal) vertx.getValue(),
                         eventLoopCount.get(),
                         dataSourceName,
                         runtimeConfig.getValue().dataSources().get(dataSourceName),
                         reactiveRuntimeConfig.getValue().dataSources().get(dataSourceName).reactive(),
                         reactiveDB2RuntimeConfig.getValue().dataSources().get(dataSourceName).reactive().db2(),
+                        tlsRegistrySupplier != null ? tlsRegistrySupplier.get() : null,
                         context);
 
-                shutdown.addShutdownTask(db2Pool::close);
-                return db2Pool;
+                shutdown.addShutdownTask(pool::close);
+                return pool;
             }
         };
     }
 
-    public Function<SyntheticCreationalContext<io.vertx.mutiny.db2client.DB2Pool>, io.vertx.mutiny.db2client.DB2Pool> mutinyDB2Pool(
-            String dataSourceName) {
-        return new Function<>() {
-            @SuppressWarnings("unchecked")
-            @Override
-            public io.vertx.mutiny.db2client.DB2Pool apply(SyntheticCreationalContext context) {
-                return io.vertx.mutiny.db2client.DB2Pool.newInstance(
-                        (DB2Pool) context.getInjectedReference(DB2Pool.class, qualifier(dataSourceName)));
-            }
-        };
-    }
-
-    private DB2Pool initialize(VertxInternal vertx,
+    private Pool initialize(VertxInternal vertx,
             Integer eventLoopCount,
             String dataSourceName,
             DataSourceRuntimeConfig dataSourceRuntimeConfig,
             DataSourceReactiveRuntimeConfig dataSourceReactiveRuntimeConfig,
             DataSourceReactiveDB2Config dataSourceReactiveDB2Config,
-            SyntheticCreationalContext<DB2Pool> context) {
-        PoolOptions poolOptions = toPoolOptions(eventLoopCount, dataSourceReactiveRuntimeConfig);
+            TlsConfigurationRegistry tlsRegistry,
+            SyntheticCreationalContext<Pool> context) {
+        PoolOptions poolOptions = ReactivePoolUtil.toPoolOptions(eventLoopCount, dataSourceReactiveRuntimeConfig);
         DB2ConnectOptions db2ConnectOptions = toConnectOptions(dataSourceName, dataSourceRuntimeConfig,
-                dataSourceReactiveRuntimeConfig, dataSourceReactiveDB2Config);
-        Supplier<Future<DB2ConnectOptions>> databasesSupplier = toDatabasesSupplier(List.of(db2ConnectOptions),
-                dataSourceRuntimeConfig);
+                dataSourceReactiveRuntimeConfig, dataSourceReactiveDB2Config, tlsRegistry);
+        Supplier<Future<SqlConnectOptions>> databasesSupplier = ReactivePoolUtil.toDatabasesSupplier(
+                List.of(db2ConnectOptions), dataSourceRuntimeConfig, DB2ConnectOptions::new);
         return createPool(vertx, poolOptions, db2ConnectOptions, dataSourceName, databasesSupplier, context);
-    }
-
-    private Supplier<Future<DB2ConnectOptions>> toDatabasesSupplier(List<DB2ConnectOptions> db2ConnectOptionsList,
-            DataSourceRuntimeConfig dataSourceRuntimeConfig) {
-        Supplier<Future<DB2ConnectOptions>> supplier;
-        if (dataSourceRuntimeConfig.credentialsProvider().isPresent()) {
-            String beanName = dataSourceRuntimeConfig.credentialsProviderName().orElse(null);
-            CredentialsProvider credentialsProvider = CredentialsProviderFinder.find(beanName);
-            String name = dataSourceRuntimeConfig.credentialsProvider().get();
-            supplier = new ConnectOptionsSupplier<>(credentialsProvider, name, db2ConnectOptionsList,
-                    DB2ConnectOptions::new);
-        } else {
-            supplier = Utils.roundRobinSupplier(db2ConnectOptionsList);
-        }
-        return supplier;
-    }
-
-    private PoolOptions toPoolOptions(Integer eventLoopCount, DataSourceReactiveRuntimeConfig dataSourceReactiveRuntimeConfig) {
-        PoolOptions poolOptions;
-        poolOptions = new PoolOptions();
-
-        poolOptions.setMaxSize(dataSourceReactiveRuntimeConfig.maxSize());
-
-        if (dataSourceReactiveRuntimeConfig.idleTimeout().isPresent()) {
-            var idleTimeout = unitised(dataSourceReactiveRuntimeConfig.idleTimeout().get());
-            poolOptions.setIdleTimeout(idleTimeout.value).setIdleTimeoutUnit(idleTimeout.unit);
-        }
-
-        if (dataSourceReactiveRuntimeConfig.maxLifetime().isPresent()) {
-            var maxLifetime = unitised(dataSourceReactiveRuntimeConfig.maxLifetime().get());
-            poolOptions.setMaxLifetime(maxLifetime.value).setMaxLifetimeUnit(maxLifetime.unit);
-        }
-
-        if (dataSourceReactiveRuntimeConfig.shared()) {
-            poolOptions.setShared(true);
-            if (dataSourceReactiveRuntimeConfig.name().isPresent()) {
-                poolOptions.setName(dataSourceReactiveRuntimeConfig.name().get());
-            }
-        }
-
-        if (dataSourceReactiveRuntimeConfig.eventLoopSize().isPresent()) {
-            poolOptions.setEventLoopSize(Math.max(0, dataSourceReactiveRuntimeConfig.eventLoopSize().getAsInt()));
-        } else if (eventLoopCount != null) {
-            poolOptions.setEventLoopSize(Math.max(0, eventLoopCount));
-        }
-
-        return poolOptions;
     }
 
     private DB2ConnectOptions toConnectOptions(String dataSourceName, DataSourceRuntimeConfig dataSourceRuntimeConfig,
             DataSourceReactiveRuntimeConfig dataSourceReactiveRuntimeConfig,
-            DataSourceReactiveDB2Config dataSourceReactiveDB2Config) {
+            DataSourceReactiveDB2Config dataSourceReactiveDB2Config,
+            TlsConfigurationRegistry tlsRegistry) {
         DB2ConnectOptions connectOptions;
 
         if (dataSourceReactiveRuntimeConfig.url().isPresent()) {
@@ -191,7 +105,7 @@ public class DB2PoolRecorder {
             }
             String url = urls.get(0);
             // clean up the URL to make migrations easier
-            if (url.matches("^vertx-reactive:db2://.*$")) {
+            if (url.startsWith("vertx-reactive:")) {
                 url = url.substring("vertx-reactive:".length());
             }
             connectOptions = DB2ConnectOptions.fromUri(url);
@@ -199,87 +113,51 @@ public class DB2PoolRecorder {
             connectOptions = new DB2ConnectOptions();
         }
 
-        if (dataSourceRuntimeConfig.username().isPresent()) {
-            connectOptions.setUser(dataSourceRuntimeConfig.username().get());
-        }
-
-        if (dataSourceRuntimeConfig.password().isPresent()) {
-            connectOptions.setPassword(dataSourceRuntimeConfig.password().get());
-        }
-
-        // credentials provider
-        if (dataSourceRuntimeConfig.credentialsProvider().isPresent()) {
-            String beanName = dataSourceRuntimeConfig.credentialsProviderName().orElse(null);
-            CredentialsProvider credentialsProvider = CredentialsProviderFinder.find(beanName);
-            String name = dataSourceRuntimeConfig.credentialsProvider().get();
-            Map<String, String> credentials = credentialsProvider.getCredentialsAsync(name).await().indefinitely();
-            String user = credentials.get(USER_PROPERTY_NAME);
-            String password = credentials.get(PASSWORD_PROPERTY_NAME);
-            if (user != null) {
-                connectOptions.setUser(user);
-            }
-            if (password != null) {
-                connectOptions.setPassword(password);
-            }
-        }
+        ReactivePoolUtil.configureCredentials(connectOptions, dataSourceRuntimeConfig);
 
         connectOptions.setCachePreparedStatements(
                 dataSourceReactiveRuntimeConfig.cachePreparedStatements().orElse(SUPPORTS_CACHE_PREPARED_STATEMENTS));
 
-        connectOptions.setSsl(dataSourceReactiveDB2Config.ssl());
-
-        connectOptions.setTrustAll(dataSourceReactiveRuntimeConfig.trustAll());
-
-        configurePemTrustOptions(connectOptions, dataSourceReactiveRuntimeConfig.trustCertificatePem());
-        configureJksTrustOptions(connectOptions, dataSourceReactiveRuntimeConfig.trustCertificateJks());
-        configurePfxTrustOptions(connectOptions, dataSourceReactiveRuntimeConfig.trustCertificatePfx());
-
-        configurePemKeyCertOptions(connectOptions, dataSourceReactiveRuntimeConfig.keyCertificatePem());
-        configureJksKeyCertOptions(connectOptions, dataSourceReactiveRuntimeConfig.keyCertificateJks());
-        configurePfxKeyCertOptions(connectOptions, dataSourceReactiveRuntimeConfig.keyCertificatePfx());
-
-        connectOptions.setReconnectAttempts(dataSourceReactiveRuntimeConfig.reconnectAttempts());
-
-        connectOptions.setReconnectInterval(dataSourceReactiveRuntimeConfig.reconnectInterval().toMillis());
-
-        var algo = dataSourceReactiveRuntimeConfig.hostnameVerificationAlgorithm();
-        if ("NONE".equalsIgnoreCase(algo)) {
-            connectOptions.setHostnameVerificationAlgorithm("");
-        } else {
-            connectOptions.setHostnameVerificationAlgorithm(algo);
+        if (dataSourceReactiveDB2Config.ssl()) {
+            connectOptions.setSsl(true);
+        } else if (dataSourceReactiveRuntimeConfig.tlsConfigurationName().isPresent()) {
+            // Auto-enable SSL when a named TLS configuration is set
+            connectOptions.setSsl(true);
         }
+
+        ReactivePoolUtil.configureSsl(connectOptions, dataSourceReactiveRuntimeConfig, tlsRegistry);
 
         dataSourceReactiveRuntimeConfig.additionalProperties().forEach(connectOptions::addProperty);
 
         // Use the convention defined by Quarkus Micrometer Vert.x metrics to create metrics prefixed with db2.
         // and the client_name as tag.
-        // See io.quarkus.micrometer.runtime.binder.vertx.VertxMeterBinderAdapter.extractPrefix and
-        // io.quarkus.micrometer.runtime.binder.vertx.VertxMeterBinderAdapter.extractClientName
         connectOptions.setMetricsName("db2|" + dataSourceName);
 
         return connectOptions;
     }
 
-    private DB2Pool createPool(Vertx vertx, PoolOptions poolOptions, DB2ConnectOptions dB2ConnectOptions,
-            String dataSourceName, Supplier<Future<DB2ConnectOptions>> databases,
-            SyntheticCreationalContext<DB2Pool> context) {
-        Instance<DB2PoolCreator> instance = context.getInjectedReference(POOL_CREATOR_TYPE_LITERAL, qualifier(dataSourceName));
+    private Pool createPool(Vertx vertx, PoolOptions poolOptions, DB2ConnectOptions db2ConnectOptions,
+            String dataSourceName, Supplier<Future<SqlConnectOptions>> databases,
+            SyntheticCreationalContext<Pool> context) {
+        Instance<PoolCreator> instance = context.getInjectedReference(POOL_CREATOR_TYPE_LITERAL, qualifier(dataSourceName));
         if (instance.isResolvable()) {
-            DB2PoolCreator.Input input = new DefaultInput(vertx, poolOptions, dB2ConnectOptions);
-            return (DB2Pool) instance.get().create(input);
+            DB2PoolCreator.Input input = new DefaultInput(vertx, poolOptions, db2ConnectOptions);
+            return instance.get().create(input);
         }
-        return (DB2Pool) DB2Driver.INSTANCE.createPool(vertx, databases, poolOptions);
+        return DB2Builder.pool().with(poolOptions)
+                .connectingTo(databases)
+                .using(vertx).build();
     }
 
     private static class DefaultInput implements DB2PoolCreator.Input {
         private final Vertx vertx;
         private final PoolOptions poolOptions;
-        private final DB2ConnectOptions dB2ConnectOptions;
+        private final DB2ConnectOptions connectOptions;
 
-        public DefaultInput(Vertx vertx, PoolOptions poolOptions, DB2ConnectOptions dB2ConnectOptions) {
+        public DefaultInput(Vertx vertx, PoolOptions poolOptions, DB2ConnectOptions connectOptions) {
             this.vertx = vertx;
             this.poolOptions = poolOptions;
-            this.dB2ConnectOptions = dB2ConnectOptions;
+            this.connectOptions = connectOptions;
         }
 
         @Override
@@ -293,12 +171,13 @@ public class DB2PoolRecorder {
         }
 
         @Override
-        public DB2ConnectOptions db2ConnectOptions() {
-            return dB2ConnectOptions;
+        public List<SqlConnectOptions> connectOptionsList() {
+            return List.of(connectOptions);
         }
-    }
 
-    public RuntimeValue<DB2PoolSupport> createDB2PoolSupport(Set<String> db2PoolNames) {
-        return new RuntimeValue<>(new DB2PoolSupport(db2PoolNames));
+        @Override
+        public DB2ConnectOptions db2ConnectOptions() {
+            return connectOptions;
+        }
     }
 }

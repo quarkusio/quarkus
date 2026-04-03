3cfbc79f0c9237fe1ddbedcfb8f03cfc22e4a25a
Clement Escoffier <clement@apache.org>
2026-04-03 11:51:48 +0200

    Migrate reactive Oracle client tests from Quarkus REST to Reactive Routes
    This is a temporary switch to avoid being blocked by the migration of Resteasy Reactive to Vert.x 5

--- extensions/reactive-oracle-client/deployment/src/test/java/io/quarkus/reactive/oracle/client/ChangingCredentialsTestResource.java
+++ extensions/reactive-oracle-client/deployment/src/test/java/io/quarkus/reactive/oracle/client/ChangingCredentialsTestResource.java
@@ -4,17 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 
 import io.quarkus.runtime.StartupEvent;
+import io.quarkus.vertx.web.Route;
 import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.sqlclient.Pool;
 
-@Path("/test")
 public class ChangingCredentialsTestResource {
 
     @Inject
@@ -28,14 +23,12 @@ public class ChangingCredentialsTestResource {
         client.query("GRANT CREATE SESSION TO user2").executeAndAwait();
     }
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public Uni<Response> connect() {
+    @Route(path = "/test", methods = Route.HttpMethod.GET)
+    Uni<String> connect() {
         return client.query("SELECT USER FROM DUAL").execute()
                 .map(rowSet -> {
                     assertEquals(1, rowSet.size());
-                    return Response.ok(rowSet.iterator().next().getString(0)).build();
+                    return rowSet.iterator().next().getString(0);
                 }).eventually(credentialsProvider::changeProperties);
     }
-
 }

5ed1c97aa9781cc938e3af5ea3288efe97bdefba
Clement Escoffier <clement@apache.org>
2026-04-03 08:31:56 +0200

    Add common reactive datasource mechanism to reactive-datasource extension

--- extensions/reactive-datasource/deployment/src/main/java/io/quarkus/reactive/datasource/deployment/ReactiveDataSourceProcessor.java
+++ extensions/reactive-datasource/deployment/src/main/java/io/quarkus/reactive/datasource/deployment/ReactiveDataSourceProcessor.java
@@ -1,27 +1,67 @@
 package io.quarkus.reactive.datasource.deployment;
 
+import static io.quarkus.reactive.datasource.deployment.ReactiveDataSourceBuildUtil.qualifier;
+import static io.quarkus.reactive.datasource.deployment.ReactiveDataSourceBuildUtil.qualifiers;
+import static io.quarkus.reactive.datasource.deployment.ReactiveDataSourceDotNames.INJECT_INSTANCE;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Singleton;
+
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.ClassType;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.ParameterizedType;
+import org.jboss.jandex.Type;
 import org.jboss.logging.Logger;
 
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem;
+import io.quarkus.arc.deployment.SyntheticBeanBuildItem.ExtendedBeanConfigurator;
+import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
+import io.quarkus.arc.deployment.ValidationPhaseBuildItem;
+import io.quarkus.arc.deployment.devui.Name;
+import io.quarkus.arc.processor.BeanInfo;
 import io.quarkus.datasource.common.runtime.DataSourceUtil;
 import io.quarkus.datasource.deployment.spi.DefaultDataSourceDbKindBuildItem;
 import io.quarkus.datasource.runtime.DataSourceBuildTimeConfig;
 import io.quarkus.datasource.runtime.DataSourcesBuildTimeConfig;
+import io.quarkus.deployment.Capabilities;
+import io.quarkus.deployment.Capability;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.pkg.builditem.CurateOutcomeBuildItem;
+import io.quarkus.reactive.datasource.PoolCreator;
 import io.quarkus.reactive.datasource.ReactiveDataSource;
 import io.quarkus.reactive.datasource.runtime.DataSourceReactiveBuildTimeConfig;
 import io.quarkus.reactive.datasource.runtime.DataSourcesReactiveBuildTimeConfig;
+import io.quarkus.reactive.datasource.runtime.ReactivePoolRecorder;
+import io.quarkus.reactive.datasource.runtime.ReactivePoolsHealthConfig;
+import io.quarkus.reactive.datasource.spi.ReactivePoolBuildItem;
 import io.quarkus.runtime.configuration.ConfigurationException;
+import io.quarkus.smallrye.health.deployment.spi.HealthBuildItem;
+import io.vertx.sqlclient.Pool;
 
 class ReactiveDataSourceProcessor {
     private static final Logger log = Logger.getLogger(ReactiveDataSourceProcessor.class);
+
+    private static final Type POOL_CREATOR_TYPE = ClassType.create(DotName.createSimple(PoolCreator.class));
+    private static final ParameterizedType POOL_CREATOR_INJECTION_TYPE = ParameterizedType.create(INJECT_INSTANCE,
+            new Type[] { POOL_CREATOR_TYPE }, null);
+    private static final DotName POOL = DotName.createSimple(Pool.class);
+    private static final Type POOL_TYPE = ClassType.create(POOL);
 
     @BuildStep
     void addQualifierAsBean(BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
@@ -123,6 +163,128 @@ class ReactiveDataSourceProcessor {
                     newDataSourceBuildItem.getDbKind(),
                     newDataSourceBuildItem.isDefault(),
                     newDataSourceBuildItem.getVersion()));
+        }
+    }
+
+    /**
+     * Consume {@link ReactivePoolBuildItem}s produced by each DB-specific extension (PG, MySQL, etc.)
+     * and register generic synthetic CDI beans for {@link Pool} and Mutiny Pool.
+     * Also produces a {@link VertxPoolBuildItem} for backwards compatibility with Hibernate Reactive.
+     */
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    void registerReactivePools(
+            ReactivePoolRecorder recorder,
+            List<ReactivePoolBuildItem> poolBuildItems,
+            BuildProducer<SyntheticBeanBuildItem> syntheticBeans,
+            BuildProducer<VertxPoolBuildItem> vertxPool) {
+
+        if (poolBuildItems.isEmpty()) {
+            return;
+        }
+
+        Map<String, String> healthSqlMap = new HashMap<>();
+
+        for (ReactivePoolBuildItem poolBuildItem : poolBuildItems) {
+            String dataSourceName = poolBuildItem.getDataSourceName();
+
+            // Register the bare Vert.x Pool as a synthetic bean
+            ExtendedBeanConfigurator poolBeanConfigurator = SyntheticBeanBuildItem.configure(Pool.class)
+                    .defaultBean()
+                    .addType(Pool.class)
+                    .scope(ApplicationScoped.class)
+                    .qualifiers(qualifiers(dataSourceName))
+                    .addInjectionPoint(POOL_CREATOR_INJECTION_TYPE, qualifier(dataSourceName))
+                    .checkActive(recorder.poolCheckActiveSupplier(dataSourceName))
+                    .createWith(poolBuildItem.getPool())
+                    .unremovable()
+                    .setRuntimeInit()
+                    .startup();
+            syntheticBeans.produce(poolBeanConfigurator.done());
+
+            // Register the Mutiny Pool as a synthetic bean
+            ExtendedBeanConfigurator mutinyPoolConfigurator = SyntheticBeanBuildItem
+                    .configure(io.vertx.mutiny.sqlclient.Pool.class)
+                    .defaultBean()
+                    .addType(io.vertx.mutiny.sqlclient.Pool.class)
+                    .scope(ApplicationScoped.class)
+                    .qualifiers(qualifiers(dataSourceName))
+                    .addInjectionPoint(POOL_TYPE, qualifier(dataSourceName))
+                    .checkActive(recorder.poolCheckActiveSupplier(dataSourceName))
+                    .createWith(recorder.mutinyPool(dataSourceName))
+                    .unremovable()
+                    .setRuntimeInit()
+                    .startup();
+            syntheticBeans.produce(mutinyPoolConfigurator.done());
+
+            healthSqlMap.put(dataSourceName, poolBuildItem.getHealthCheckSql());
+        }
+
+        // Register the health config bean
+        syntheticBeans.produce(SyntheticBeanBuildItem.configure(ReactivePoolsHealthConfig.class)
+                .scope(Singleton.class)
+                .unremovable()
+                .runtimeValue(recorder.createHealthConfig(healthSqlMap))
+                .setRuntimeInit()
+                .done());
+
+        // Produce VertxPoolBuildItem for backwards compatibility (Hibernate Reactive consumes it)
+        vertxPool.produce(new VertxPoolBuildItem());
+    }
+
+    /**
+     * The health check needs to be produced in a separate method to avoid a circular dependency
+     * (the Vert.x instance creation consumes the AdditionalBeanBuildItems).
+     * We use AggregatedDataSourceBuildTimeConfigBuildItem (build-time) instead of ReactivePoolBuildItem (runtime)
+     * to avoid introducing a cycle.
+     */
+    @BuildStep
+    void addHealthCheck(
+            Capabilities capabilities,
+            BuildProducer<HealthBuildItem> healthChecks,
+            List<AggregatedDataSourceBuildTimeConfigBuildItem> aggregatedBuildTimeConfigBuildItems,
+            DataSourcesBuildTimeConfig dataSourcesBuildTimeConfig) {
+        if (!capabilities.isPresent(Capability.SMALLRYE_HEALTH)) {
+            return;
+        }
+        if (aggregatedBuildTimeConfigBuildItems.isEmpty()) {
+            return;
+        }
+        healthChecks.produce(new HealthBuildItem(
+                "io.quarkus.reactive.datasource.runtime.ReactiveDataSourcesHealthCheck",
+                dataSourcesBuildTimeConfig.healthEnabled()));
+    }
+
+    @BuildStep
+    void unremoveableBeans(BuildProducer<UnremovableBeanBuildItem> producer) {
+        producer.produce(UnremovableBeanBuildItem.beanTypes(PoolCreator.class));
+    }
+
+    @BuildStep
+    void validateBeans(ValidationPhaseBuildItem validationPhase,
+            BuildProducer<ValidationPhaseBuildItem.ValidationErrorBuildItem> errors) {
+        // no two PoolCreator beans can be associated with the same datasource
+        Set<String> seen = new HashSet<>();
+        for (BeanInfo beanInfo : validationPhase.getContext().beans()
+                .matchBeanTypes(new PoolCreatorBeanClassPredicate())) {
+            Set<Name> qualifiers = new TreeSet<>();
+            for (AnnotationInstance q : beanInfo.getQualifiers()) {
+                qualifiers.add(Name.from(q));
+            }
+            String qualifiersStr = qualifiers.stream().map(Name::toString).collect(Collectors.joining("_"));
+            if (!seen.add(qualifiersStr)) {
+                errors.produce(new ValidationPhaseBuildItem.ValidationErrorBuildItem(
+                        new IllegalStateException(
+                                "There can be at most one bean of type '" + PoolCreator.class.getName()
+                                        + "' for each datasource.")));
+            }
+        }
+    }
+
+    private static class PoolCreatorBeanClassPredicate implements Predicate<Set<Type>> {
+        @Override
+        public boolean test(Set<Type> types) {
+            return types.contains(POOL_CREATOR_TYPE);
         }
     }
 }
---
+++ extensions/reactive-datasource/runtime/src/main/java/io/quarkus/reactive/datasource/runtime/ReactivePoolUtil.java
@@ -0,0 +1,220 @@
+package io.quarkus.reactive.datasource.runtime;
+
+import static io.quarkus.credentials.CredentialsProvider.PASSWORD_PROPERTY_NAME;
+import static io.quarkus.credentials.CredentialsProvider.USER_PROPERTY_NAME;
+import static io.quarkus.reactive.datasource.runtime.UnitisedTime.unitised;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+import io.quarkus.credentials.CredentialsProvider;
+import io.quarkus.credentials.runtime.CredentialsProviderFinder;
+import io.quarkus.datasource.runtime.DataSourceRuntimeConfig;
+import io.quarkus.vertx.core.runtime.config.JksConfiguration;
+import io.quarkus.vertx.core.runtime.config.PemKeyCertConfiguration;
+import io.quarkus.vertx.core.runtime.config.PemTrustCertConfiguration;
+import io.quarkus.vertx.core.runtime.config.PfxConfiguration;
+import io.vertx.core.Future;
+import io.vertx.core.net.ClientSSLOptions;
+import io.vertx.core.net.JksOptions;
+import io.vertx.core.net.PemKeyCertOptions;
+import io.vertx.core.net.PemTrustOptions;
+import io.vertx.core.net.PfxOptions;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.SqlConnectOptions;
+import io.vertx.sqlclient.impl.Utils;
+
+/**
+ * Shared utility methods for reactive SQL client pool configuration.
+ * Used by all DB-specific recorders (PG, MySQL, MSSQL, Oracle, DB2).
+ */
+public final class ReactivePoolUtil {
+
+    private ReactivePoolUtil() {
+    }
+
+    /**
+     * Build {@link PoolOptions} from the generic reactive datasource config.
+     */
+    public static PoolOptions toPoolOptions(Integer eventLoopCount,
+            DataSourceReactiveRuntimeConfig config) {
+        PoolOptions poolOptions = new PoolOptions();
+
+        poolOptions.setMaxSize(config.maxSize());
+
+        if (config.idleTimeout().isPresent()) {
+            var idleTimeout = unitised(config.idleTimeout().get());
+            poolOptions.setIdleTimeout(idleTimeout.value).setIdleTimeoutUnit(idleTimeout.unit);
+        }
+
+        if (config.maxLifetime().isPresent()) {
+            var maxLifetime = unitised(config.maxLifetime().get());
+            poolOptions.setMaxLifetime(maxLifetime.value).setMaxLifetimeUnit(maxLifetime.unit);
+        }
+
+        if (config.shared()) {
+            poolOptions.setShared(true);
+            if (config.name().isPresent()) {
+                poolOptions.setName(config.name().get());
+            }
+        }
+
+        if (config.eventLoopSize().isPresent()) {
+            poolOptions.setEventLoopSize(Math.max(0, config.eventLoopSize().getAsInt()));
+        } else if (eventLoopCount != null) {
+            poolOptions.setEventLoopSize(Math.max(0, eventLoopCount));
+        }
+
+        return poolOptions;
+    }
+
+    /**
+     * Build a databases supplier that handles credential rotation via {@link CredentialsProvider}
+     * or falls back to round-robin for multiple connect options.
+     *
+     * @param connectOptionsList the base connect options
+     * @param dataSourceRuntimeConfig the datasource runtime config
+     * @param copyConstructor a function that copies a connect options instance (e.g. {@code PgConnectOptions::new})
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static <CO extends SqlConnectOptions> Supplier<Future<SqlConnectOptions>> toDatabasesSupplier(
+            List<CO> connectOptionsList,
+            DataSourceRuntimeConfig dataSourceRuntimeConfig,
+            UnaryOperator<CO> copyConstructor) {
+        if (dataSourceRuntimeConfig.credentialsProvider().isPresent()) {
+            String beanName = dataSourceRuntimeConfig.credentialsProviderName().orElse(null);
+            CredentialsProvider credentialsProvider = CredentialsProviderFinder.find(beanName);
+            String name = dataSourceRuntimeConfig.credentialsProvider().get();
+            return (Supplier) new ConnectOptionsSupplier<>(credentialsProvider, name, connectOptionsList, copyConstructor);
+        } else {
+            return (Supplier) Utils.roundRobinSupplier(connectOptionsList);
+        }
+    }
+
+    /**
+     * Apply credentials from the datasource config and credentials provider to connect options.
+     */
+    public static void configureCredentials(SqlConnectOptions connectOptions,
+            DataSourceRuntimeConfig dataSourceRuntimeConfig) {
+        dataSourceRuntimeConfig.username().ifPresent(connectOptions::setUser);
+        dataSourceRuntimeConfig.password().ifPresent(connectOptions::setPassword);
+
+        if (dataSourceRuntimeConfig.credentialsProvider().isPresent()) {
+            String beanName = dataSourceRuntimeConfig.credentialsProviderName().orElse(null);
+            CredentialsProvider credentialsProvider = CredentialsProviderFinder.find(beanName);
+            String name = dataSourceRuntimeConfig.credentialsProvider().get();
+            Map<String, String> credentials = credentialsProvider.getCredentialsAsync(name).await().indefinitely();
+            String user = credentials.get(USER_PROPERTY_NAME);
+            String password = credentials.get(PASSWORD_PROPERTY_NAME);
+            if (user != null) {
+                connectOptions.setUser(user);
+            }
+            if (password != null) {
+                connectOptions.setPassword(password);
+            }
+        }
+    }
+
+    /**
+     * Apply SSL/TLS, reconnect, and hostname verification settings from the generic reactive
+     * datasource config to connect options.
+     */
+    public static void configureSsl(SqlConnectOptions connectOptions,
+            DataSourceReactiveRuntimeConfig config) {
+        ClientSSLOptions sslOptions = connectOptions.getSslOptions();
+        if (sslOptions == null) {
+            sslOptions = new ClientSSLOptions();
+        }
+
+        sslOptions.setTrustAll(config.trustAll());
+
+        configurePemTrustOptions(sslOptions, config.trustCertificatePem());
+        configureJksTrustOptions(sslOptions, config.trustCertificateJks());
+        configurePfxTrustOptions(sslOptions, config.trustCertificatePfx());
+
+        configurePemKeyCertOptions(sslOptions, config.keyCertificatePem());
+        configureJksKeyCertOptions(sslOptions, config.keyCertificateJks());
+        configurePfxKeyCertOptions(sslOptions, config.keyCertificatePfx());
+
+        String algo = config.hostnameVerificationAlgorithm();
+        if ("NONE".equalsIgnoreCase(algo)) {
+            sslOptions.setHostnameVerificationAlgorithm("");
+        } else {
+            sslOptions.setHostnameVerificationAlgorithm(algo);
+        }
+
+        connectOptions.setSslOptions(sslOptions);
+
+        connectOptions.setReconnectAttempts(config.reconnectAttempts());
+        connectOptions.setReconnectInterval(config.reconnectInterval().toMillis());
+    }
+
+    private static void configurePemTrustOptions(ClientSSLOptions options, PemTrustCertConfiguration configuration) {
+        if (configuration.enabled() || (configuration.certs().isPresent() && !configuration.certs().get().isEmpty())) {
+            PemTrustOptions pemTrustOptions = new PemTrustOptions();
+            if (configuration.certs().isPresent()) {
+                for (String cert : configuration.certs().get()) {
+                    pemTrustOptions.addCertPath(cert);
+                }
+            }
+            options.setTrustOptions(pemTrustOptions);
+        }
+    }
+
+    private static void configureJksTrustOptions(ClientSSLOptions options, JksConfiguration configuration) {
+        if (configuration.enabled()) {
+            options.setTrustOptions(toJksOptions(configuration));
+        }
+    }
+
+    private static void configurePfxTrustOptions(ClientSSLOptions options, PfxConfiguration configuration) {
+        if (configuration.enabled()) {
+            options.setTrustOptions(toPfxOptions(configuration));
+        }
+    }
+
+    private static void configurePemKeyCertOptions(ClientSSLOptions options, PemKeyCertConfiguration configuration) {
+        if (configuration.enabled()) {
+            PemKeyCertOptions pemKeyCertOptions = new PemKeyCertOptions();
+            if (configuration.certs().isPresent()) {
+                for (String cert : configuration.certs().get()) {
+                    pemKeyCertOptions.addCertPath(cert);
+                }
+            }
+            if (configuration.keys().isPresent()) {
+                for (String key : configuration.keys().get()) {
+                    pemKeyCertOptions.addKeyPath(key);
+                }
+            }
+            options.setKeyCertOptions(pemKeyCertOptions);
+        }
+    }
+
+    private static void configureJksKeyCertOptions(ClientSSLOptions options, JksConfiguration configuration) {
+        if (configuration.enabled()) {
+            options.setKeyCertOptions(toJksOptions(configuration));
+        }
+    }
+
+    private static void configurePfxKeyCertOptions(ClientSSLOptions options, PfxConfiguration configuration) {
+        if (configuration.enabled()) {
+            options.setKeyCertOptions(toPfxOptions(configuration));
+        }
+    }
+
+    private static JksOptions toJksOptions(JksConfiguration configuration) {
+        JksOptions jksOptions = new JksOptions();
+        configuration.path().ifPresent(jksOptions::setPath);
+        configuration.password().ifPresent(jksOptions::setPassword);
+        return jksOptions;
+    }
+
+    private static PfxOptions toPfxOptions(PfxConfiguration configuration) {
+        PfxOptions pfxOptions = new PfxOptions();
+        configuration.path().ifPresent(pfxOptions::setPath);
+        configuration.password().ifPresent(pfxOptions::setPassword);
+        return pfxOptions;
+    }
+}

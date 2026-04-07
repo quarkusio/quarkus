f578eb9667f64873040c0f170579e8a377233f68
brunobat <brunobat@gmail.com>
2026-04-07 12:13:46 +0100

    Bump Otel SDK to 1.60.1

--- extensions/agroal/runtime/src/main/java/io/quarkus/agroal/runtime/AgroalOpenTelemetryWrapper.java
+++ extensions/agroal/runtime/src/main/java/io/quarkus/agroal/runtime/AgroalOpenTelemetryWrapper.java
@@ -2,12 +2,23 @@ package io.quarkus.agroal.runtime;
 
 import java.util.function.Function;
 
+import jakarta.inject.Inject;
+
 import io.agroal.api.AgroalDataSource;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.jdbc.datasource.JdbcTelemetry;
+import io.opentelemetry.instrumentation.jdbc.datasource.OpenTelemetryDataSource;
 
 public class AgroalOpenTelemetryWrapper implements Function<AgroalDataSource, AgroalDataSource> {
 
+    @Inject
+    OpenTelemetry openTelemetry;
+
     @Override
     public AgroalDataSource apply(AgroalDataSource originalDataSource) {
-        return new OpenTelemetryAgroalDataSource(originalDataSource);
+        OpenTelemetryDataSource otelDataSource = (OpenTelemetryDataSource) JdbcTelemetry
+                .create(openTelemetry)
+                .wrap(originalDataSource);
+        return new OpenTelemetryAgroalDataSource(originalDataSource, otelDataSource);
     }
 }

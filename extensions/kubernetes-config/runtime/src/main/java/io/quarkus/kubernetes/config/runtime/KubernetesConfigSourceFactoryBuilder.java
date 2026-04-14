3c607f96c51ad0346e28ae996ba4aa8c16fe79d4
Roberto Cortez <radcortez@yahoo.com>
2026-04-14 11:41:30 +0100

    Ignore unrelated configuration in KubernetesConfigSourceFactory

--- extensions/kubernetes-config/runtime/src/main/java/io/quarkus/kubernetes/config/runtime/KubernetesConfigSourceFactoryBuilder.java
+++ extensions/kubernetes-config/runtime/src/main/java/io/quarkus/kubernetes/config/runtime/KubernetesConfigSourceFactoryBuilder.java
@@ -1,6 +1,8 @@
 package io.quarkus.kubernetes.config.runtime;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
@@ -10,7 +12,8 @@ import io.quarkus.kubernetes.client.runtime.internal.KubernetesClientUtils;
 import io.quarkus.runtime.ApplicationLifecycleManager;
 import io.quarkus.runtime.configuration.ConfigBuilder;
 import io.smallrye.config.ConfigSourceContext;
-import io.smallrye.config.ConfigSourceFactory.ConfigurableConfigSourceFactory;
+import io.smallrye.config.ConfigSourceFactory;
+import io.smallrye.config.SmallRyeConfig;
 import io.smallrye.config.SmallRyeConfigBuilder;
 
 public class KubernetesConfigSourceFactoryBuilder implements ConfigBuilder {
@@ -19,16 +22,28 @@ public class KubernetesConfigSourceFactoryBuilder implements ConfigBuilder {
         return builder.withSources(new KubernetesConfigFactory());
     }
 
-    static class KubernetesConfigFactory implements ConfigurableConfigSourceFactory<KubernetesClientConfig> {
+    static class KubernetesConfigFactory implements ConfigSourceFactory {
         @Override
-        public Iterable<ConfigSource> getConfigSources(final ConfigSourceContext context,
-                final KubernetesClientConfig config) {
+        public Iterable<ConfigSource> getConfigSources(final ConfigSourceContext context) {
             boolean inAppCDsGeneration = Boolean
                     .parseBoolean(System.getProperty(ApplicationLifecycleManager.QUARKUS_APPCDS_GENERATE_PROP, "false"));
             if (inAppCDsGeneration) {
                 return Collections.emptyList();
             }
-            KubernetesClient client = KubernetesClientUtils.createClient(config);
+
+            List<String> profiles = new ArrayList<>(context.getProfiles());
+            Collections.reverse(profiles);
+
+            SmallRyeConfig config = new SmallRyeConfigBuilder()
+                    .withProfiles(profiles)
+                    .withSources(new ConfigSourceContext.ConfigSourceContextConfigSource(context))
+                    .withSources(context.getConfigSources())
+                    .withMapping(KubernetesClientConfig.class)
+                    .withValidateUnknown(false)
+                    .build();
+
+            KubernetesClientConfig kubernetesClientConfig = config.getConfigMapping(KubernetesClientConfig.class);
+            KubernetesClient client = KubernetesClientUtils.createClient(kubernetesClientConfig);
             return new KubernetesConfigSourceFactory(client).getConfigSources(context);
         }
     }

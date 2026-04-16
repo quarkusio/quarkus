8adf8415811371737e3bc2254629753adaa3d4ef
Amal Sebastian <amalsebastian48@gmail.com>
2026-04-16 19:10:25 +0530

    Added QuarkusTestProfile for the OtlpCoexistenceIT Integration Test

--- integration-tests/opentelemetry/src/test/java/io/quarkus/it/opentelemetry/OtlpCoexistenceIT.java
+++ integration-tests/opentelemetry/src/test/java/io/quarkus/it/opentelemetry/OtlpCoexistenceIT.java
@@ -3,11 +3,16 @@ package io.quarkus.it.opentelemetry;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
 
 @QuarkusTest
+@TestProfile(OtlpCoexistenceIT.OtlpEnabledProfile.class)
 public class OtlpCoexistenceIT {
 
     @Test
@@ -25,5 +30,12 @@ public class OtlpCoexistenceIT {
         String[] exporters = response.split(",");
         assertTrue(exporters.length >= 2,
                 "Should have at least 2 exporters registered, but found: " + response);
+    }
+
+    public static class OtlpEnabledProfile implements QuarkusTestProfile {
+        @Override
+        public Map<String, String> getConfigOverrides() {
+            return Map.of("quarkus.otel.experimental.otlp.default.enable", "true");
+        }
     }
 }

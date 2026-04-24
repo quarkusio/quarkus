55f0ed68fa7f0b6cf99388ef58269ce0302e9bb5
Guillaume Smet <guillaume.smet@gmail.com>
2026-04-24 16:14:12 +0200

    Update to Kotlin 2.3.21

--- integration-tests/gradle/src/main/resources/grpc-multi-module-no-java/build.gradle.kts
+++ integration-tests/gradle/src/main/resources/grpc-multi-module-no-java/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.3.10"
-    kotlin("plugin.allopen") version "2.3.10"
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.allopen") version "2.3.21"
 
     id("io.quarkus") apply false
 }

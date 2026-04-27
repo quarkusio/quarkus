b8ac3f8c22548d97a32bcb458cb2354892752d41
Stéphane Épardaud <stef@inforealm.org>
2026-04-27 14:17:19 +0200

    Update extensions/panache/hibernate-panache-next/deployment/src/test/java/io/quarkus/hibernate/panache/deployment/test/MyEntityRepository.java
    Co-authored-by: Yoann Rodière <yoann@hibernate.org>

--- extensions/panache/hibernate-panache-next/deployment/src/test/java/io/quarkus/hibernate/panache/deployment/test/MyEntityRepository.java
+++ extensions/panache/hibernate-panache-next/deployment/src/test/java/io/quarkus/hibernate/panache/deployment/test/MyEntityRepository.java
@@ -2,5 +2,6 @@ package io.quarkus.hibernate.panache.deployment.test;
 
 import io.quarkus.hibernate.panache.PanacheRepository;
 
+// Test for https://github.com/quarkusio/quarkus/issues/52975
 public class MyEntityRepository implements PanacheRepository<MyEntity> {
 }

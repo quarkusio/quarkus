919b80017d85564143a845b38e9cca54aff5b3cc
Clement Escoffier <clement@apache.org>
2026-05-11 09:22:00 +0200

    Handle GHSA-qcxp-gm7m-4j5v

--- extensions/vertx-http/runtime/src/test/java/io/quarkus/vertx/http/runtime/security/AbstractPathMatchingHttpSecurityPolicyTest.java
+++ extensions/vertx-http/runtime/src/test/java/io/quarkus/vertx/http/runtime/security/AbstractPathMatchingHttpSecurityPolicyTest.java
@@ -1,5 +1,6 @@
 package io.quarkus.vertx.http.runtime.security;
 
+import static io.quarkus.vertx.http.runtime.security.HttpSecurityUtils.normalizePath;
 import static io.quarkus.vertx.http.runtime.security.HttpSecurityUtils.pathWithoutMatrixParams;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -92,6 +93,63 @@ public class AbstractPathMatchingHttpSecurityPolicyTest {
         assertEquals("/a/b", pathWithoutMatrixParams("/a;k=v=w/b"));
         assertEquals("/a/b", pathWithoutMatrixParams("/a;k=v;k2=v2/b"));
         assertEquals("/a/b", pathWithoutMatrixParams("/a;k=v;k2=v2/b;k3=v3;k4=v4"));
+    }
+
+    @Test
+    public void testNormalizePathCleanPathUnchanged() {
+        assertEquals("/api/admin/data", normalizePath("/api/admin/data"));
+        assertEquals("/", normalizePath("/"));
+    }
+
+    @Test
+    public void testNormalizePathStripMatrixParams() {
+        assertEquals("/api/admin/data", normalizePath("/api/admin;x=1/data"));
+        assertEquals("/api/admin/data", normalizePath("/api/admin;bypass=true/data"));
+    }
+
+    @Test
+    public void testNormalizePathPercentDecoding() {
+        assertEquals("/api/admin/data", normalizePath("/api/adm%69n/data"));
+        assertEquals("/secret/confidential.html", normalizePath("/secret%2Fconfidential.html"));
+        assertEquals("/api/admin/data", normalizePath("/%61pi/%61dmin/d%61t%61"));
+    }
+
+    @Test
+    public void testNormalizePathDoubleEncoding() {
+        assertEquals("/api/admin/data", normalizePath("/api/adm%2569n/data"));
+        assertEquals("/secret/confidential.html", normalizePath("/secret%252Fconfidential.html"));
+        assertEquals("/secret/confidential.html", normalizePath("/secret%25252Fconfidential.html"));
+    }
+
+    @Test
+    public void testNormalizePathNullByteRemoval() {
+        assertEquals("/api/admin/data", normalizePath("/api/admin\0/data"));
+        assertEquals("/api/admin/data", normalizePath("/api/admin%00/data"));
+    }
+
+    @Test
+    public void testNormalizePathBackslash() {
+        assertEquals("/secret/confidential.html", normalizePath("/secret\\confidential.html"));
+        assertEquals("/api/admin/data", normalizePath("/api\\admin\\data"));
+        assertEquals("/secret/confidential.html", normalizePath("/secret%5Cconfidential.html"));
+    }
+
+    @Test
+    public void testNormalizePathDotSegmentsAfterDecoding() {
+        assertEquals("/etc/passwd", normalizePath("/api/%2e%2e/etc/passwd"));
+        assertEquals("/secret.html", normalizePath("/api/../secret.html"));
+    }
+
+    @Test
+    public void testNormalizePathEncodedSemicolon() {
+        assertEquals("/api/baz", normalizePath("/api/baz%3Bv=1.1"));
+        assertEquals("/api/admin/data", normalizePath("/api/admin%3Bbypass=true/data"));
+    }
+
+    @Test
+    public void testNormalizePathCombinedAttacks() {
+        assertEquals("/api/admin/data", normalizePath("/api/adm%69n;v=1/data"));
+        assertEquals("/secret/file.html", normalizePath("/secret%2Ffile.html\0"));
     }
 
 }

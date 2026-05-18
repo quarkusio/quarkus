5f3d6ecd6b4f9fee389a620a71fa38c6f8159140
George Gastaldi <gegastaldi@gmail.com>
2026-05-18 12:15:34 -0300

    Replace deprecated Runtime.version().major() with Runtime.version().feature()
    Runtime.Version.major() has been deprecated since JDK 10 in favor of
    Runtime.Version.feature(), which better reflects the versioning scheme
    introduced by JEP 322.

--- core/runtime/src/main/java/io/quarkus/runtime/util/JavaVersionUtil.java
+++ core/runtime/src/main/java/io/quarkus/runtime/util/JavaVersionUtil.java
@@ -11,17 +11,17 @@ public class JavaVersionUtil {
 
     @Deprecated(forRemoval = true)
     public static boolean isJava17OrHigher() {
-        return Runtime.version().major() >= 17;
+        return Runtime.version().feature() >= 17;
     }
 
     @Deprecated(forRemoval = true)
     public static boolean isJava19OrHigher() {
-        return Runtime.version().major() >= 19;
+        return Runtime.version().feature() >= 19;
     }
 
     @Deprecated(forRemoval = true)
     public static boolean isJava21OrHigher() {
-        return Runtime.version().major() >= 21;
+        return Runtime.version().feature() >= 21;
     }
 
     public static boolean isGraalvmJdk() {

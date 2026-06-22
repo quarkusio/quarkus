cffca0793cb3a9107f75a66e2fe3a2ea251bcdd4
Alexey Loubyansky <olubyans@redhat.com>
2026-06-22 22:42:53 +0200

    Represent absence of dependencies as an empty array in CycloneDX SBOMs
    (cherry picked from commit db604cad0273bc8918076d9fb80804d08b0f7596)

--- extensions/cyclonedx/generator/src/test/java/io/quarkus/cyclonedx/generator/CycloneDxSbomGeneratorTest.java
+++ extensions/cyclonedx/generator/src/test/java/io/quarkus/cyclonedx/generator/CycloneDxSbomGeneratorTest.java
@@ -169,6 +169,38 @@ class CycloneDxSbomGeneratorTest {
     }
 
     @Test
+    void leafComponentsHaveDependencyEntries() {
+        ComponentDescriptor react = ComponentDescriptor.builder()
+                .setPurl(Purl.npm(null, "react", "18.0.0"))
+                .build();
+        ComponentDescriptor jsTokens = ComponentDescriptor.builder()
+                .setPurl(Purl.npm(null, "js-tokens", "4.0.0"))
+                .build();
+        SbomContribution contribution = SbomContribution.of(
+                List.of(react, jsTokens),
+                List.of(ComponentDependencies.of(
+                        react.getBomRef(),
+                        List.of(jsTokens.getBomRef()))));
+
+        Bom bom = parseBom(CycloneDxSbomGenerator.newInstance()
+                .setFormat("json")
+                .setContributions(List.of(contribution))
+                .generateText().get(0));
+
+        // js-tokens is a leaf with no dependencies of its own,
+        // but it should still have an entry in the dependency graph
+        assertThat(bom.getDependencies())
+                .extracting(Dependency::getRef)
+                .contains(react.getBomRef(), jsTokens.getBomRef());
+
+        Dependency jsTokensDep = bom.getDependencies().stream()
+                .filter(d -> d.getRef().equals(jsTokens.getBomRef()))
+                .findFirst()
+                .orElseThrow();
+        assertThat(jsTokensDep.getDependencies()).isNullOrEmpty();
+    }
+
+    @Test
     void serialNumberIsDeterministicWithOutputTimestamp() {
         Instant timestamp = Instant.parse("2025-01-15T10:00:00Z");
         ComponentDescriptor react = ComponentDescriptor.builder()

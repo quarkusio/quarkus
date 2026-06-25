536d5d4ff79099dbbcac1ac5f66a4936f0571c98
Clement Escoffier <clement@apache.org>
2026-06-25 14:46:39 +0200

    Native Transport - Add integration tests for epoll, io_uring and nio fallback

---
+++ integration-tests/vertx-native-transport/epoll/src/test/java/io/quarkus/it/vertx/nativetransport/EpollTransportTest.java
@@ -0,0 +1,30 @@
+package io.quarkus.it.vertx.nativetransport;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.vertx.core.runtime.config.NativeTransportType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+@EnabledOnOs(OS.LINUX)
+class EpollTransportTest {
+
+    @Test
+    void shouldUseEpollTransport() {
+        Response response = given()
+                .when().get("/transport")
+                .then().statusCode(200)
+                .extract().response();
+
+        String type = response.jsonPath().getString("type");
+        boolean nativeEnabled = response.jsonPath().getBoolean("nativeTransportEnabled");
+
+        assertThat(nativeEnabled).isTrue();
+        assertThat(type).isEqualTo(NativeTransportType.EPOLL.transportName);
+    }
+}

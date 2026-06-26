024eb365819535c88b64ed30bb8bc47aab99243d
Clement Escoffier <clement@apache.org>
2026-06-26 13:45:56 +0200

    Native Transport - Add support for io_uring in native and add ITs for epoll and io_uring
    Also:
    
    - add a new native build category in the CI for native transports.
    - update documentation to cover native compilation of native transports.

---
+++ integration-tests/vertx-native-transport/io-uring/src/test/java/io/quarkus/it/vertx/nativetransport/IoUringTransportIT.java
@@ -0,0 +1,8 @@
+package io.quarkus.it.vertx.nativetransport;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class IoUringTransportIT extends IoUringTransportTest {
+
+}

31f29729fc7abcabf1ab6fd75dbe12e8bbd64c7a
marko-bekhta <marko.prykladna@gmail.com>
2026-06-29 22:46:49 +0200

    Remove the Elasticsearch Java client configs

--- extensions/elasticsearch-java-client/deployment/src/test/java/io/quarkus/elasticsearch/javaclient/deployment/ActiveInactiveClientProgrammaticAccessOnlyTest.java
+++ extensions/elasticsearch-java-client/deployment/src/test/java/io/quarkus/elasticsearch/javaclient/deployment/ActiveInactiveClientProgrammaticAccessOnlyTest.java
@@ -45,7 +45,7 @@ public class ActiveInactiveClientProgrammaticAccessOnlyTest {
         assertTrue(Arc.container().select(ElasticsearchClient.class).getHandle().getBean().isActive());
         assertFalse(Arc.container().select(ElasticsearchClient.class, Identifier.Literal.of("client2")).getHandle().getBean()
                 .isActive());
-        assertFalse(Arc.container().select(ElasticsearchClient.class, Identifier.Literal.of("client3")).getHandle().getBean()
+        assertTrue(Arc.container().select(ElasticsearchClient.class, Identifier.Literal.of("client3")).getHandle().getBean()
                 .isActive());
     }
 }

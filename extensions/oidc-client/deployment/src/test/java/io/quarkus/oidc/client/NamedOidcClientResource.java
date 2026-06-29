4ac94f9c8a5726ae59f7e945786d9dc2329ea50e
Matheus Cruz <matheuscruz.dev@gmail.com>
2026-06-29 11:15:40 -0300

    Add Literal helper to NamedOidcClient qualifier
    Provide an AnnotationLiteral implementation so NamedOidcClient can be
    used as a qualifier for programmatic CDI lookup and instance selection.
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>
    (cherry picked from commit 8fa5e9a30b7393d9254bb03c3d60a1d2cb60a9d9)

--- extensions/oidc-client/deployment/src/test/java/io/quarkus/oidc/client/NamedOidcClientResource.java
+++ extensions/oidc-client/deployment/src/test/java/io/quarkus/oidc/client/NamedOidcClientResource.java
@@ -1,9 +1,13 @@
 package io.quarkus.oidc.client;
 
+import jakarta.enterprise.inject.Any;
+import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
+import io.quarkus.arc.Arc;
 import io.smallrye.mutiny.Uni;
 
 @Path("/")
@@ -24,6 +28,10 @@ public class NamedOidcClientResource {
     @Inject
     @NamedOidcClient("client2")
     Tokens tokens2;
+
+    @Inject
+    @Any
+    Instance<OidcClient> clients;
 
     @GET
     @Path("/client1/token")
@@ -47,5 +55,19 @@ public class NamedOidcClientResource {
     @Path("/client2/token/singleton")
     public String accessToken2() {
         return tokens2.getAccessToken();
+    }
+
+    @GET
+    @Path("/{client}/token/programmatic")
+    public Uni<String> programmaticTokenUni(@PathParam("client") String client) {
+        OidcClient oidcClient = clients.select(NamedOidcClient.Literal.of(client)).get();
+        return oidcClient.getTokens().flatMap(tokens -> Uni.createFrom().item(tokens.getAccessToken()));
+    }
+
+    @GET
+    @Path("/{client}/token/programmatic-arc")
+    public Uni<String> programmaticTokenUniArc(@PathParam("client") String client) {
+        OidcClient oidcClient = Arc.container().instance(OidcClient.class, NamedOidcClient.Literal.of(client)).get();
+        return oidcClient.getTokens().flatMap(tokens -> Uni.createFrom().item(tokens.getAccessToken()));
     }
 }

3efe4edc4edf7d0c20d910b9d3dd53f8f07f0694
cfitzw <20694057+cfitzw@users.noreply.github.com>
2026-07-02 17:50:21 +0000

    sanitize derived group name and improve tests
    (cherry picked from commit 929406dbc39a8d95f778b9573e08de5dae4f414a)

--- extensions/container-image/deployment/src/test/java/io/quarkus/container/image/deployment/ContainerImageConfigEffectiveGroupTest.java
+++ extensions/container-image/deployment/src/test/java/io/quarkus/container/image/deployment/ContainerImageConfigEffectiveGroupTest.java
@@ -12,34 +12,140 @@ class ContainerImageConfigEffectiveGroupTest {
     private final Optional<String> EMPTY = Optional.empty();
 
     @Test
+    void testSingleSegmentRequestedYieldsEmpty() {
+        testWithNewUsername("test", () -> {
+            assertEquals(EMPTY,
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, true));
+        });
+    }
+
+    @Test
+    void testExplicitGroupOverridesUsername() {
+        testWithNewUsername("test", () -> {
+            Optional<String> group = Optional.of("other");
+            assertEquals(Optional.of("other"),
+                    ContainerImageProcessor.getEffectiveGroup(group, false));
+        });
+    }
+
+    @Test
+    void testExplicitGroupIsLowercasedButNotSanitized() {
+        testWithNewUsername("test", () -> {
+            Optional<String> group = Optional.of("USER@domain.com");
+            assertEquals(Optional.of("user@domain.com"),
+                    ContainerImageProcessor.getEffectiveGroup(group, false));
+        });
+    }
+
+    @Test
     void testFromLowercaseUsername() {
         testWithNewUsername("test", () -> {
-            Optional<String> group = Optional.of(System.getProperty(USER_NAME_SYSTEM_PROPERTY));
-            assertEquals(ContainerImageProcessor.getEffectiveGroup(EMPTY, false), Optional.of("test"));
+            assertEquals(Optional.of("test"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
         });
     }
 
     @Test
     void testFromUppercaseUsername() {
         testWithNewUsername("TEST", () -> {
-            Optional<String> group = Optional.of(System.getProperty(USER_NAME_SYSTEM_PROPERTY));
-            assertEquals(ContainerImageProcessor.getEffectiveGroup(EMPTY, false), Optional.of("test"));
+            assertEquals(Optional.of("test"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
+        });
+    }
+
+    @Test
+    void testPreservesDotsUnderscoresAndHyphensInUsername() {
+        testWithNewUsername("t.e-s_t", () -> {
+            assertEquals(Optional.of("t.e-s_t"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
+        });
+    }
+
+    @Test
+    void testPreservesDigitsInUsername() {
+        testWithNewUsername("t3st", () -> {
+            assertEquals(Optional.of("t3st"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
         });
     }
 
     @Test
     void testFromSpaceInUsername() {
-        testWithNewUsername("user name", () -> {
-            Optional<String> group = Optional.of(System.getProperty(USER_NAME_SYSTEM_PROPERTY));
-            assertEquals(ContainerImageProcessor.getEffectiveGroup(EMPTY, false), Optional.of("user-name"));
+        testWithNewUsername("test user", () -> {
+            assertEquals(Optional.of("test-user"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
         });
     }
 
     @Test
-    void testFromOther() {
-        testWithNewUsername("WhateveR", () -> {
-            Optional<String> group = Optional.of("OtheR");
-            assertEquals(ContainerImageProcessor.getEffectiveGroup(group, false), Optional.of("other"));
+    void testFromFullyQualifiedDomainNameInUsername() {
+        testWithNewUsername("test@domain.com", () -> {
+            assertEquals(Optional.of("test-domain.com"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
+        });
+    }
+
+    @Test
+    void testCollapsesRepeatedUnderscores() {
+        testWithNewUsername("test__user", () -> {
+            assertEquals(Optional.of("test_user"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
+        });
+    }
+
+    @Test
+    void testCollapsesRepeatedHyphens() {
+        testWithNewUsername("test--user", () -> {
+            assertEquals(Optional.of("test-user"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
+        });
+    }
+
+    @Test
+    void testCollapsesRepeatedDots() {
+        testWithNewUsername("test..user", () -> {
+            assertEquals(Optional.of("test.user"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
+        });
+    }
+
+    @Test
+    void testReplacesRunOfInvalidCharactersWithSingleDash() {
+        testWithNewUsername("test@#*user", () -> {
+            assertEquals(Optional.of("test-user"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
+        });
+    }
+
+    @Test
+    void testTrimsLeadingSeparators() {
+        testWithNewUsername("__test", () -> {
+            assertEquals(Optional.of("test"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
+        });
+    }
+
+    @Test
+    void testTrimsTrailingSeparators() {
+        testWithNewUsername("test__", () -> {
+            assertEquals(Optional.of("test"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
+        });
+    }
+
+    @Test
+    void testTrimsLeadingAndTrailingMixedSeparators() {
+        testWithNewUsername("_-test-_", () -> {
+            assertEquals(Optional.of("test"),
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
+        });
+    }
+
+    @Test
+    void testSeparatorOnlyUsernameYieldsEmpty() {
+        testWithNewUsername("._-", () -> {
+            assertEquals(EMPTY,
+                    ContainerImageProcessor.getEffectiveGroup(EMPTY, false));
         });
     }
 

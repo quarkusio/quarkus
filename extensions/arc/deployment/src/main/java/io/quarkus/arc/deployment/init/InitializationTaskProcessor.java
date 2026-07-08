594717a019c1d3ef734d8af635e7f928b8ead8c0
David M. Lloyd <dmlloyd@ibm.com>
2026-07-08 11:39:10 -0500

    Service conversion: ArC extension

--- extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/init/InitializationTaskProcessor.java
+++ extensions/arc/deployment/src/main/java/io/quarkus/arc/deployment/init/InitializationTaskProcessor.java
@@ -3,11 +3,12 @@ package io.quarkus.arc.deployment.init;
 import java.util.List;
 
 import io.quarkus.arc.deployment.SyntheticBeansRuntimeInitBuildItem;
+import io.quarkus.core.deployment.action.ActionBuilder;
+import io.quarkus.deployment.Phase;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.Consume;
-import io.quarkus.deployment.annotations.ExecutionTime;
-import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.InitTaskCompletedBuildItem;
+import io.quarkus.runtime.init.InitRuntimeConfig;
 import io.quarkus.runtime.init.InitializationTaskRecorder;
 
 /**
@@ -19,9 +20,13 @@ public class InitializationTaskProcessor {
 
     @BuildStep
     @Consume(SyntheticBeansRuntimeInitBuildItem.class)
-    @Record(ExecutionTime.RUNTIME_INIT)
-    void startApplicationInitializer(InitializationTaskRecorder recorder,
+    void startApplicationInitializer(ActionBuilder action,
             List<InitTaskCompletedBuildItem> initTaskCompletedBuildItems) {
-        recorder.exitIfNeeded();
+        action
+                .forService("io.quarkus.arc.init-task")
+                .atPhase(Phase.INIT)
+                .require(InitRuntimeConfig.class)
+                .afterBuildItem(SyntheticBeansRuntimeInitBuildItem.class)
+                .action((ctx, config) -> InitializationTaskRecorder.exitIfNeeded(config));
     }
 }

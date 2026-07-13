c81a21c0b2599f2c46b46da0ca9b531e2935963f
Luca Molteni <volothamp@gmail.com>
2026-07-13 13:41:06 +0200

    StatelessSessionLazyDelegator revisited

--- extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/StatelessSessionLazyDelegator.java
+++ extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/StatelessSessionLazyDelegator.java
@@ -1,16 +1,22 @@
 package io.quarkus.hibernate.orm.runtime;
 
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
-
+import jakarta.persistence.CacheRetrieveMode;
+import jakarta.persistence.CacheStoreMode;
+import jakarta.persistence.ConnectionConsumer;
+import jakarta.persistence.ConnectionFunction;
+import jakarta.persistence.EntityAgent;
 import jakarta.persistence.EntityGraph;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.FindOption;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.StatementReference;
+import jakarta.persistence.TypedQuery;
 import jakarta.persistence.TypedQueryReference;
-import jakarta.persistence.criteria.CriteriaDelete;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.CriteriaUpdate;
-
+import jakarta.persistence.criteria.CriteriaSelect;
+import jakarta.persistence.criteria.CriteriaStatement;
+import jakarta.persistence.metamodel.Metamodel;
+import jakarta.persistence.sql.ResultSetMapping;
 import org.hibernate.CacheMode;
 import org.hibernate.Filter;
 import org.hibernate.HibernateException;
@@ -25,12 +31,19 @@ import org.hibernate.graph.RootGraph;
 import org.hibernate.jdbc.ReturningWork;
 import org.hibernate.jdbc.Work;
 import org.hibernate.procedure.ProcedureCall;
+import org.hibernate.query.MutationOrSelectionQuery;
 import org.hibernate.query.MutationQuery;
 import org.hibernate.query.NativeQuery;
-import org.hibernate.query.Query;
 import org.hibernate.query.SelectionQuery;
 import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 import org.hibernate.query.criteria.JpaCriteriaInsert;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * Plays the exact same role as {@link org.hibernate.engine.spi.SessionLazyDelegator} for {@link org.hibernate.Session}
@@ -49,8 +62,8 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
-    public Object insert(Object entity) {
-        return delegate.get().insert(entity);
+    public void insert(Object entity) {
+        delegate.get().insert(entity);
     }
 
     @Override
@@ -99,43 +112,93 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
+    public Object get(String entityName, Object key, FindOption... findOptions) {
+        return delegate.get().get(entityName, key, findOptions);
+    }
+
+    @Override
     public <T> T get(Class<T> entityClass, Object id) {
         return delegate.get().get(entityClass, id);
     }
 
     @Override
+    public <T> T get(Class<T> entityClass, Object id, FindOption... options) {
+        return delegate.get().get(entityClass, id, options);
+    }
+
+    @Override
+    public <T> T get(EntityGraph<T> graph, Object id, FindOption... options) {
+        return delegate.get().get(graph, id, options);
+    }
+
+    @Override
+    public <T> List<T> getMultiple(Class<T> entityClass, List<?> ids, FindOption... options) {
+        return delegate.get().getMultiple(entityClass, ids, options);
+    }
+
+    @Override
+    public <T> List<T> getMultiple(EntityGraph<T> graph, List<?> ids, FindOption... options) {
+        return delegate.get().getMultiple(graph, ids, options);
+    }
+
+    @Override
+    public <T> T find(Class<T> entityClass, Object id) {
+        return delegate.get().find(entityClass, id);
+    }
+
+    @Override
+    public <T> T find(Class<T> entityClass, Object id, FindOption... options) {
+        return delegate.get().find(entityClass, id, options);
+    }
+
+    @Override
+    public <T> T find(EntityGraph<T> graph, Object id, FindOption... options) {
+        return delegate.get().find(graph, id, options);
+    }
+
+    @Override
+    public <T> List<T> findMultiple(Class<T> entityClass, List<?> ids, FindOption... options) {
+        return delegate.get().findMultiple(entityClass, ids, options);
+    }
+
+    @Override
+    public <T> List<T> findMultiple(EntityGraph<T> graph, List<?> ids, FindOption... options) {
+        return delegate.get().findMultiple(graph, ids, options);
+    }
+
+    @Override
+    public void setCacheRetrieveMode(CacheRetrieveMode cacheRetrieveMode) {
+        delegate.get().setCacheRetrieveMode(cacheRetrieveMode);
+    }
+
+    @Override
+    public void setCacheStoreMode(CacheStoreMode cacheStoreMode) {
+        delegate.get().setCacheStoreMode(cacheStoreMode);
+    }
+
+    @Override
+    public CacheRetrieveMode getCacheRetrieveMode() {
+        return delegate.get().getCacheRetrieveMode();
+    }
+
+    @Override
+    public CacheStoreMode getCacheStoreMode() {
+        return delegate.get().getCacheStoreMode();
+    }
+
+    @Override
+    public void setProperty(String propertyName, Object value) {
+        delegate.get().setProperty(propertyName, value);
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+        return delegate.get().getProperties();
+    }
+
+    @Override
     public Object get(String entityName, Object id, LockMode lockMode) {
         return delegate.get().get(entityName, id, lockMode);
-    }
-
-    @Override
-    public <T> T get(Class<T> entityClass, Object id, LockMode lockMode) {
-        return delegate.get().get(entityClass, id, lockMode);
-    }
-
-    @Override
-    public <T> T get(EntityGraph<T> graph, Object id) {
-        return delegate.get().get(graph, id);
-    }
-
-    @Override
-    public <T> T get(EntityGraph<T> graph, Object id, LockMode lockMode) {
-        return delegate.get().get(graph, id, lockMode);
-    }
-
-    @Override
-    public <T> List<T> getMultiple(Class<T> entityClass, List<?> ids) {
-        return delegate.get().getMultiple(entityClass, ids);
-    }
-
-    @Override
-    public <T> List<T> getMultiple(Class<T> entityClass, List<?> ids, LockMode lockMode) {
-        return delegate.get().getMultiple(entityClass, ids, lockMode);
-    }
-
-    @Override
-    public <T> List<T> getMultiple(EntityGraph<T> entityGraph, List<?> ids) {
-        return delegate.get().getMultiple(entityGraph, ids);
     }
 
     @Override
@@ -164,6 +227,26 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
+    public void refreshMultiple(List<?> entities) {
+        delegate.get().refreshMultiple(entities);
+    }
+
+    @Override
+    public void refresh(Object entity, LockModeType lockMode) {
+        delegate.get().refresh(entity, lockMode);
+    }
+
+    @Override
+    public void addOption(EntityAgent.Option option) {
+        delegate.get().addOption(option);
+    }
+
+    @Override
+    public Set<Option> getOptions() {
+        return delegate.get().getOptions();
+    }
+
+    @Override
     public void refresh(String entityName, Object entity) {
         delegate.get().refresh(entityName, entity);
     }
@@ -179,8 +262,8 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
-    public void fetch(Object association) {
-        delegate.get().fetch(association);
+    public <T> T fetch(T association) {
+        return delegate.get().fetch(association);
     }
 
     @Override
@@ -195,7 +278,7 @@ class StatelessSessionLazyDelegator implements StatelessSession {
 
     @Override
     public Object getTenantIdentifierValue() {
-        return delegate.get().getTenantIdentifier();
+        return delegate.get().getTenantIdentifierValue();
     }
 
     @Override
@@ -219,6 +302,11 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
+    public EntityManagerFactory getEntityManagerFactory() {
+        return delegate.get().getEntityManagerFactory();
+    }
+
+    @Override
     public void joinTransaction() {
         delegate.get().joinTransaction();
     }
@@ -226,6 +314,11 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     @Override
     public boolean isJoinedToTransaction() {
         return delegate.get().isJoinedToTransaction();
+    }
+
+    @Override
+    public Object find(String entityName, Object key, FindOption... findOptions) {
+        return delegate.get().find(entityName, key, findOptions);
     }
 
     @Override
@@ -284,6 +377,11 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
+    public Metamodel getMetamodel() {
+        return delegate.get().getMetamodel();
+    }
+
+    @Override
     public void doWork(Work work) throws HibernateException {
         delegate.get().doWork(work);
     }
@@ -294,41 +392,42 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
-    @Deprecated(since = "6.0")
-    public Query createQuery(String queryString) {
+    public MutationOrSelectionQuery createQuery(String queryString) {
         return delegate.get().createQuery(queryString);
     }
 
     @Override
-    public <R> Query<R> createQuery(String queryString, Class<R> resultClass) {
+    public <R> SelectionQuery<R> createQuery(String queryString, Class<R> resultClass) {
         return delegate.get().createQuery(queryString, resultClass);
     }
 
     @Override
-    public <R> Query<R> createQuery(CriteriaQuery<R> criteriaQuery) {
-        return delegate.get().createQuery(criteriaQuery);
+    public <T> SelectionQuery<T> createQuery(String hqlString, EntityGraph<T> entityGraph) {
+        return delegate.get().createQuery(hqlString, entityGraph);
     }
 
     @Override
-    @Deprecated(since = "6.0")
-    public Query createQuery(CriteriaUpdate updateQuery) {
-        return delegate.get().createQuery(updateQuery);
-    }
-
-    @Override
-    @Deprecated(since = "6.0")
-    public Query createQuery(CriteriaDelete deleteQuery) {
-        return delegate.get().createQuery(deleteQuery);
-    }
-
-    @Override
-    public <R> Query<R> createQuery(TypedQueryReference<R> typedQueryReference) {
+    public <R> SelectionQuery<R> createQuery(TypedQueryReference<R> typedQueryReference) {
         return delegate.get().createQuery(typedQueryReference);
     }
 
     @Override
-    @Deprecated(since = "6.0")
-    public NativeQuery createNativeQuery(String sqlString) {
+    public <T> SelectionQuery<T> createQuery(CriteriaSelect<T> criteriaSelect) {
+        return delegate.get().createQuery(criteriaSelect);
+    }
+
+    @Override
+    public MutationQuery createStatement(CriteriaStatement<?> criteriaStatement) {
+        return delegate.get().createStatement(criteriaStatement);
+    }
+
+    @Override
+    public <R> SelectionQuery<R> createSelectionQuery(CriteriaSelect<R> criteria) {
+        return delegate.get().createSelectionQuery(criteria);
+    }
+
+    @Override
+    public NativeQuery<?> createNativeQuery(String sqlString) {
         return delegate.get().createNativeQuery(sqlString);
     }
 
@@ -343,19 +442,18 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
-    @Deprecated(since = "6.0")
-    public NativeQuery createNativeQuery(String sqlString, String resultSetMappingName) {
+    public NativeQuery<?> createNativeQuery(String sqlString, String resultSetMappingName) {
         return delegate.get().createNativeQuery(sqlString, resultSetMappingName);
+    }
+
+    @Override
+    public <T> TypedQuery<T> createNativeQuery(String sql, ResultSetMapping<T> resultSetMapping) {
+        return delegate.get().createNativeQuery(sql, resultSetMapping);
     }
 
     @Override
     public <R> NativeQuery<R> createNativeQuery(String sqlString, String resultSetMappingName, Class<R> resultClass) {
         return delegate.get().createNativeQuery(sqlString, resultSetMappingName, resultClass);
-    }
-
-    @Override
-    public SelectionQuery<?> createSelectionQuery(String hqlString) {
-        return delegate.get().createSelectionQuery(hqlString);
     }
 
     @Override
@@ -379,13 +477,18 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
-    public MutationQuery createMutationQuery(CriteriaUpdate updateQuery) {
-        return delegate.get().createMutationQuery(updateQuery);
+    public MutationQuery createStatement(String hqlString) {
+        return delegate.get().createStatement(hqlString);
     }
 
     @Override
-    public MutationQuery createMutationQuery(CriteriaDelete deleteQuery) {
-        return delegate.get().createMutationQuery(deleteQuery);
+    public MutationQuery createStatement(StatementReference statementReference) {
+        return delegate.get().createStatement(statementReference);
+    }
+
+    @Override
+    public MutationQuery createMutationQuery(CriteriaStatement<?> criteriaStatement) {
+        return delegate.get().createMutationQuery(criteriaStatement);
     }
 
     @Override
@@ -399,19 +502,33 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
-    @Deprecated(since = "6.0")
-    public Query createNamedQuery(String name) {
+    public MutationOrSelectionQuery createNamedQuery(String name) {
         return delegate.get().createNamedQuery(name);
     }
 
     @Override
-    public <R> Query<R> createNamedQuery(String name, Class<R> resultClass) {
+    public <R> SelectionQuery<R> createNamedQuery(String name, Class<R> resultClass) {
         return delegate.get().createNamedQuery(name, resultClass);
     }
 
     @Override
-    public SelectionQuery<?> createNamedSelectionQuery(String name) {
-        return delegate.get().createNamedSelectionQuery(name);
+    public MutationQuery createNamedStatement(String name) {
+        return delegate.get().createNamedStatement(name);
+    }
+
+    @Override
+    public <R> NativeQuery<R> createNamedQuery(String name, String resultSetMappingName) {
+        return delegate.get().createNamedQuery(name, resultSetMappingName);
+    }
+
+    @Override
+    public <R> NativeQuery<R> createNamedQuery(String name, String resultSetMappingName, Class<R> resultClass) {
+        return delegate.get().createNamedQuery(name, resultSetMappingName, resultClass);
+    }
+
+    @Override
+    public MutationQuery createNativeStatement(String sql) {
+        return delegate.get().createNativeStatement(sql);
     }
 
     @Override
@@ -422,24 +539,6 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     @Override
     public MutationQuery createNamedMutationQuery(String name) {
         return delegate.get().createNamedMutationQuery(name);
-    }
-
-    @Override
-    @Deprecated(since = "6.0")
-    public Query getNamedQuery(String queryName) {
-        return delegate.get().getNamedQuery(queryName);
-    }
-
-    @Override
-    @Deprecated(since = "6.0")
-    public NativeQuery getNamedNativeQuery(String name) {
-        return delegate.get().getNamedNativeQuery(name);
-    }
-
-    @Override
-    @Deprecated(since = "6.0")
-    public NativeQuery getNamedNativeQuery(String name, String resultSetMapping) {
-        return delegate.get().getNamedNativeQuery(name, resultSetMapping);
     }
 
     @Override
@@ -463,8 +562,23 @@ class StatelessSessionLazyDelegator implements StatelessSession {
     }
 
     @Override
+    public <T> RootGraph<T> getEntityGraph(Class<T> rootType, String graphName) {
+        return delegate.get().getEntityGraph(rootType, graphName);
+    }
+
+    @Override
     public <T> List<EntityGraph<? super T>> getEntityGraphs(Class<T> entityClass) {
         return delegate.get().getEntityGraphs(entityClass);
+    }
+
+    @Override
+    public <C> void runWithConnection(ConnectionConsumer<C> action) {
+        delegate.get().runWithConnection(action);
+    }
+
+    @Override
+    public <C, T> T callWithConnection(ConnectionFunction<C, T> function) {
+        return delegate.get().callWithConnection(function);
     }
 
     @Override

ece04f969fed1273830ee120172950eec8d0ffdd
Thiago Gonzaga <thiago.gonzaga@icloud.com>
2026-07-17 15:42:11 -0300

    Update OpenTelemetry instrumentation to 1.63
    OpenTelemetry 1.63 moved validateEndpoint to EndpointUtil and relocated
    GrpcExporter and HttpExporter under exporter.otlp.internal. Adapt the
    Quarkus OTLP integration so applications can start against the newer SDK.

--- extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/exporter/otlp/tracing/VertxGrpcSpanExporter.java
+++ extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/exporter/otlp/tracing/VertxGrpcSpanExporter.java
@@ -2,8 +2,8 @@ package io.quarkus.opentelemetry.runtime.exporter.otlp.tracing;
 
 import java.util.Collection;
 
-import io.opentelemetry.exporter.internal.grpc.GrpcExporter;
 import io.opentelemetry.exporter.internal.otlp.traces.SpanReusableDataMarshaler;
+import io.opentelemetry.exporter.otlp.internal.GrpcExporter;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.common.export.MemoryMode;
 import io.opentelemetry.sdk.trace.data.SpanData;
--- extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/exporter/otlp/tracing/VertxHttpSpanExporter.java
+++ extensions/opentelemetry/runtime/src/main/java/io/quarkus/opentelemetry/runtime/exporter/otlp/tracing/VertxHttpSpanExporter.java
@@ -2,8 +2,8 @@ package io.quarkus.opentelemetry.runtime.exporter.otlp.tracing;
 
 import java.util.Collection;
 
-import io.opentelemetry.exporter.internal.http.HttpExporter;
 import io.opentelemetry.exporter.internal.otlp.traces.SpanReusableDataMarshaler;
+import io.opentelemetry.exporter.otlp.internal.HttpExporter;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.common.export.MemoryMode;
 import io.opentelemetry.sdk.trace.data.SpanData;

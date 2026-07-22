ceb4315c2d20a88b9cef83f23b33adb1645bed11
Georgios Andrianakis <geoand@gmail.com>
2026-07-22 09:08:01 +0300

    Partly port quarkus-avro to Jackson 3
    The upstream client still depends on Jackson 2

--- extensions/schema-registry/apicurio/common/deployment/src/main/java/io/quarkus/apicurio/registry/common/ApicurioRegistryClientProcessor.java
+++ extensions/schema-registry/apicurio/common/deployment/src/main/java/io/quarkus/apicurio/registry/common/ApicurioRegistryClientProcessor.java
@@ -2,6 +2,10 @@ package io.quarkus.apicurio.registry.common;
 
 import java.util.logging.Level;
 
+import org.jboss.jandex.AnnotationTransformation;
+import org.jboss.jandex.DotName;
+
+import io.quarkus.arc.deployment.AnnotationsTransformerBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.LogCategoryBuildItem;
@@ -9,6 +13,18 @@ import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildI
 import io.quarkus.smallrye.openapi.deployment.spi.IgnoreStaticDocumentBuildItem;
 
 public class ApicurioRegistryClientProcessor {
+
+    private static final DotName JACKSON_DATE_TIME_CUSTOMIZER = DotName
+            .createSimple("io.apicurio.registry.rest.config.JacksonDateTimeCustomizer");
+
+    @BuildStep
+    void vetoJackson2Beans(BuildProducer<AnnotationsTransformerBuildItem> annotationsTransformer) {
+        // Apicurio's JacksonDateTimeCustomizer implements the Jackson 2 ObjectMapperCustomizer;
+        // replaced by ApicurioRegistryDateTimeCustomizer using Jackson 3
+        annotationsTransformer.produce(new AnnotationsTransformerBuildItem(AnnotationTransformation.forClasses()
+                .whenClass(c -> c.name().equals(JACKSON_DATE_TIME_CUSTOMIZER))
+                .transform(c -> c.add(jakarta.enterprise.inject.Vetoed.class))));
+    }
 
     @BuildStep
     void ignoreIncludedOpenAPIDocument(BuildProducer<IgnoreStaticDocumentBuildItem> ignoreStaticDocumentProducer) {

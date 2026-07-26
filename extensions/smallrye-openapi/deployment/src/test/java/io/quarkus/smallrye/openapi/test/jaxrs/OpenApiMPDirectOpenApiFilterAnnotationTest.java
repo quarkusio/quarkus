1a94f4693761ab7952b84143d643b7a2949168f1
Martin Panzer <postremus1996@googlemail.com>
2026-07-26 20:47:09 +0200

    @OpenApiFilter overrides mp.openapi.filter
    A filter configured using mp.openapi.filter always ran at RUNTIME_STARTUP, even when an @OpenApiFilter annotation requested another stage.
    The annotation is now winning.
    
    Thus follows, a filter class annoted with @OpenApiFilter, and configured using mp.openapi.filter was previously run twice - which is now fixed.
    
    Filter names where previously resolved in multiple places. These are now consolidated into a new DocumentFiltersBuildItem, holding resolved filternames.

---
+++ extensions/smallrye-openapi/deployment/src/test/java/io/quarkus/smallrye/openapi/test/jaxrs/OpenApiMPDirectOpenApiFilterAnnotationTest.java
@@ -0,0 +1,75 @@
+package io.quarkus.smallrye.openapi.test.jaxrs;
+
+import static io.restassured.RestAssured.given;
+
+import jakarta.inject.Singleton;
+
+import org.eclipse.microprofile.openapi.OASFilter;
+import org.eclipse.microprofile.openapi.models.OpenAPI;
+import org.hamcrest.Matchers;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.smallrye.openapi.OpenApiFilter;
+import io.quarkus.test.QuarkusExtensionTest;
+
+public class OpenApiMPDirectOpenApiFilterAnnotationTest {
+    private static final String OPEN_API_PATH = "/q/openapi";
+    private static final String OPEN_API_CUSTOM_PATH = "/q/openapi-custom";
+
+    @Singleton
+    @OpenApiFilter(documentNames = "custom", stages = OpenApiFilter.RunStage.RUNTIME_PER_REQUEST)
+    public static class MyOASFilter implements OASFilter {
+
+        int patchVersion = 0;
+
+        @Override
+        public void filterOpenAPI(OpenAPI openAPI) {
+            openAPI.setOpenapi("3.0." + patchVersion);
+            patchVersion++;
+        }
+    }
+
+    @RegisterExtension
+    static QuarkusExtensionTest runner = new QuarkusExtensionTest()
+            .withApplicationRoot((jar) -> jar
+                    .addClass(OpenApiMPDirectOpenApiFilterAnnotationTest.class)
+                    .addAsResource(
+                            new StringAsset(
+                                    """
+                                            mp.openapi.filter=io.quarkus.smallrye.openapi.test.jaxrs.OpenApiMPDirectOpenApiFilterAnnotationTest$MyOASFilter
+                                            quarkus.smallrye-openapi.custom.info-title=Custom documentation
+                                            quarkus.smallrye-openapi.open-api-version=3.1.0"""),
+                            "application.properties"));
+
+    @Test
+    public void testOpenApiFilterResource() {
+        // validating that MyOASFilter does not change version to 3.0.0, but keeps configured verison
+        given().header("Accept", "application/json")
+                .when().get(OPEN_API_PATH)
+                .then()
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body("openapi", Matchers.equalTo("3.1.0"));
+
+        // validating that MyOASFilter on first request returns 0 - so not run during RUNTIME_STARTUP
+        given().header("Accept", "application/json")
+                .when().get(OPEN_API_CUSTOM_PATH)
+                .then()
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body("openapi", Matchers.equalTo("3.0.0"));
+
+        // validating that MyOASFilter executes during request by increasing version numbers
+        given().header("Accept", "application/json")
+                .when().get(OPEN_API_CUSTOM_PATH)
+                .then()
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body("openapi", Matchers.equalTo("3.0.1"));
+
+        given().header("Accept", "application/json")
+                .when().get(OPEN_API_CUSTOM_PATH)
+                .then()
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body("openapi", Matchers.equalTo("3.0.2"));
+    }
+}

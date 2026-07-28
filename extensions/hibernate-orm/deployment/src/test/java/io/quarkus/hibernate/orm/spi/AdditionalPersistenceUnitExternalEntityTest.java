e52ea144d38ad904f9a050c7ac7d0b06d0f1c098
Yoann Rodière <yoann@hibernate.org>
2026-07-28 15:56:39 +0200

    Remove unnecessary db-kind from tests
    With the request-based datasource model, the need for a datasource
    is automatically derived from the presence of a persistence unit,
    and most of the time its db-kind can be inferred
    from the classpath.
    
    Remove redundant db-kind settings from tests across Hibernate ORM,
    Reactive, Envers, Search, Panache, Spring Data JPA.

--- extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/spi/AdditionalPersistenceUnitExternalEntityTest.java
+++ extensions/hibernate-orm/deployment/src/test/java/io/quarkus/hibernate/orm/spi/AdditionalPersistenceUnitExternalEntityTest.java
@@ -40,10 +40,7 @@ class AdditionalPersistenceUnitExternalEntityTest {
     static QuarkusExtensionTest runner = new QuarkusExtensionTest()
             .withApplicationRoot((jar) -> jar
                     .add(new ByteArrayAsset(generateEntityBytecode()), EXTERNAL_INTERNAL_NAME + ".class"))
-            .addBuildChainCustomizer(buildCustomizer())
-            .withConfiguration("""
-                    quarkus.datasource.vector.db-kind=h2
-                    """);
+            .addBuildChainCustomizer(buildCustomizer());
 
     @Inject
     @PersistenceUnit(PERSISTENCE_UNIT_NAME)

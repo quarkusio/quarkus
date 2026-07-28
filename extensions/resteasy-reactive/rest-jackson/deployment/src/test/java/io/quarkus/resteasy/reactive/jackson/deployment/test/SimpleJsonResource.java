65948f813ee0c8b9a67e35da381299887128da72
mariofusco <mario.fusco@gmail.com>
2026-07-28 08:26:35 +0200

    In reflection-free Jackson serializers skip deserialization of a JsonAlias having the same name of the field
    (cherry picked from commit ea6cd05e6019f24be99c318a139944c51305ee82)

--- extensions/resteasy-reactive/rest-jackson/deployment/src/test/java/io/quarkus/resteasy/reactive/jackson/deployment/test/SimpleJsonResource.java
+++ extensions/resteasy-reactive/rest-jackson/deployment/src/test/java/io/quarkus/resteasy/reactive/jackson/deployment/test/SimpleJsonResource.java
@@ -715,6 +715,13 @@ public class SimpleJsonResource extends SuperClass<Person> {
     }
 
     @POST
+    @Path("/json-alias-same-as-field-name-echo")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public JsonAliasSameAsFieldNameBean echoJsonAliasSameAsFieldName(JsonAliasSameAsFieldNameBean record) {
+        return record;
+    }
+
+    @POST
     @Path("/annotation-naming")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)

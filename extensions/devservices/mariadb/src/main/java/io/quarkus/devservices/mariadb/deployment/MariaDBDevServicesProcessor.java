227953e2a1d11e2d957c77c004bd32afa5b5805a
MdTanwer <tanw9004167@gmail.com>
2026-08-04 11:01:56 +0000

    Fix IPv6 host formatting in Dev Services connection URLs issue #54696

--- extensions/devservices/mariadb/src/main/java/io/quarkus/devservices/mariadb/deployment/MariaDBDevServicesProcessor.java
+++ extensions/devservices/mariadb/src/main/java/io/quarkus/devservices/mariadb/deployment/MariaDBDevServicesProcessor.java
@@ -24,6 +24,7 @@ import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.DevServicesComposeProjectBuildItem;
 import io.quarkus.devservices.common.ComposeLocator;
 import io.quarkus.devservices.common.ConfigureUtil;
+import io.quarkus.devservices.common.DevServicesHostUtil;
 import io.quarkus.devservices.common.JBossLoggingConsumer;
 import io.quarkus.devservices.common.Labels;
 import io.quarkus.devservices.common.Volumes;
@@ -133,15 +134,19 @@ public class MariaDBDevServicesProcessor {
             }
         }
 
+        @Override
+        public String getJdbcUrl() {
+            String host = DevServicesHostUtil.publishedPortHost(getContainerId(), useSharedNetwork, hostName, getHost());
+            int port = useSharedNetwork ? PORT : getMappedPort(PORT);
+            String authority = DevServicesHostUtil.formatHostAndPort(host, port);
+            String additionalUrlParams = constructUrlParameters("?", "&");
+            return "jdbc:mariadb://" + authority + "/" + getDatabaseName() + additionalUrlParams;
+        }
+
         // this is meant to be called by Quarkus code and is needed in order to not disrupt testcontainers
         // from being able to determine the status of the container (which it does by trying to acquire a connection)
         public String getEffectiveJdbcUrl() {
-            if (useSharedNetwork) {
-                String additionalUrlParams = constructUrlParameters("?", "&");
-                return "jdbc:mariadb://" + hostName + ":" + PORT + "/" + getDatabaseName() + additionalUrlParams;
-            } else {
-                return super.getJdbcUrl();
-            }
+            return getJdbcUrl();
         }
 
         public String getReactiveUrl() {

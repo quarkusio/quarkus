092a35f28774bff674bf40aef378516f224c36ec
Guillaume Smet <guillaume.smet@gmail.com>
2026-08-06 14:34:02 +0200

    Properly implement reusable
    It was somehow working by chance until now, mostly because the app was
    eligible for reuse and thus things were not stopped.
    
    But it won't work anymore with the new Dev Services infrastructure I
    have been cooking.

--- core/deployment/src/main/java/io/quarkus/deployment/builditem/Startable.java
+++ core/deployment/src/main/java/io/quarkus/deployment/builditem/Startable.java
@@ -10,4 +10,12 @@ public interface Startable extends Closeable {
     // This starts to couple to containers, so we could move it to sub-interface and use that in dev services
     String getContainerId();
 
+    /**
+     * Whether this container is reusable across JVM restarts (Testcontainers reuse).
+     * When {@code true}, the dev services registry will not close this container on
+     * shutdown, allowing it to be reused by the next run.
+     */
+    default boolean isReusable() {
+        return false;
+    }
 }

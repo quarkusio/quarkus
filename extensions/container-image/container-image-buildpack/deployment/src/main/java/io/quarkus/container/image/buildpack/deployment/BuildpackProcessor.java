b494cd505ec93ec86c771ce1ace5d6f36f814083
Ozzy Osborne <bardweller@gmail.com>
2026-08-07 10:01:54 -0400

    Formatting update

--- extensions/container-image/container-image-buildpack/deployment/src/main/java/io/quarkus/container/image/buildpack/deployment/BuildpackProcessor.java
+++ extensions/container-image/container-image-buildpack/deployment/src/main/java/io/quarkus/container/image/buildpack/deployment/BuildpackProcessor.java
@@ -294,7 +294,7 @@ public class BuildpackProcessor {
                         //configure dockerhost/socket if required
                         DockerConfigNested<BuildConfigBuilder> dc = b.editDockerConfig();
                         dc.withHostAndSocketConfig(new HostAndSocketConfig(buildpackConfig.dockerHost().orElse(null),
-                            buildpackConfig.dockerSocket().orElse(null)));
+                                buildpackConfig.dockerSocket().orElse(null)));
                         dc.endDockerConfig();
 
                         //configure lifecycle override image if present

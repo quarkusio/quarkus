fe637582055f82d5c34f18e698f478359c4257c0
Guillaume Smet <guillaume.smet@gmail.com>
2026-08-12 14:01:06 +0200

    Fix reading headers in Spring Web - Quarkus REST
    Headers were read from the query string, which is bad. It could have
    some security implications if the application expected some headers to
    be enforced by a reverse proxy.

---
+++ extensions/spring-web/resteasy-reactive/tests/src/test/java/io/quarkus/spring/web/requestheader/RequestHeaderControllerTest.java
@@ -0,0 +1,53 @@
+package io.quarkus.spring.web.requestheader;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class RequestHeaderControllerTest {
+
+    @RegisterExtension
+    static QuarkusUnitTest runner = new QuarkusUnitTest()
+            .withApplicationRoot((jar) -> jar
+                    .addClasses(RequestHeaderController.class));
+
+    @Test
+    public void testHeaderIsReadFromHeader() {
+        given()
+                .header("X-Custom-Header", "hello")
+                .when().get("/api/header")
+                .then()
+                .statusCode(200)
+                .body(is("Header: hello"));
+    }
+
+    @Test
+    public void testHeaderNotReadFromQueryParam() {
+        given()
+                .queryParam("X-Custom-Header", "hello")
+                .when().get("/api/header")
+                .then()
+                .statusCode(200)
+                .body(is("Header: null"));
+    }
+
+    @Test
+    public void testHeaderWithDefaultValue() {
+        given()
+                .header("X-Optional-Header", "custom")
+                .when().get("/api/header/default")
+                .then()
+                .statusCode(200)
+                .body(is("Header: custom"));
+
+        given()
+                .when().get("/api/header/default")
+                .then()
+                .statusCode(200)
+                .body(is("Header: fallback"));
+    }
+}

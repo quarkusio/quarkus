b34707a2ba1d8b49c9de2af3f9b7b8a281f6c65e
Roberto Cortez <radcortez@yahoo.com>
2026-08-20 15:18:59 +0100

    Remove deprecated TestHTTPConfigSourceProvider and Interceptor

--- integration-tests/opentelemetry/src/main/java/io/quarkus/it/opentelemetry/PingPongResource.java
+++ integration-tests/opentelemetry/src/main/java/io/quarkus/it/opentelemetry/PingPongResource.java
@@ -6,12 +6,12 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import io.opentelemetry.instrumentation.annotations.SpanAttribute;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
+import io.quarkus.vertx.http.HttpServer;
 import io.smallrye.common.annotation.Blocking;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.MultiMap;
@@ -50,8 +50,8 @@ public class PingPongResource {
     @Inject
     Vertx vertx;
 
-    @ConfigProperty(name = "test.url")
-    String testUrl;
+    @Inject
+    HttpServer httpServer;
 
     @GET
     @Path("pong/{message}")
@@ -78,7 +78,7 @@ public class PingPongResource {
         HttpClient httpClient = vertx.createHttpClient();
         RequestOptions options = new RequestOptions()
                 .setMethod(HttpMethod.GET)
-                .setAbsoluteURI(testUrl + "/client/pong/" + message)
+                .setAbsoluteURI(httpServer.getLocalBaseUri() + "/client/pong/" + message)
                 .setHeaders(MultiMap.caseInsensitiveMultiMap())
                 .setTraceOperation("Async Ping");
         return httpClient.request(options)

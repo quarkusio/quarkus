6ccbfdb9c16ed109189242b7f9a80dc38cb8b0fd
Georgios Andrianakis <geoand@gmail.com>
2026-09-04 14:12:03 +0300

    Ensure `SecurityCustomSerialization` uses `ContextResolver`
    Closes: #56402
    (cherry picked from commit f5b30572e55c415ff348556ec57be1f38bcf6e76)

--- extensions/resteasy-reactive/rest-jackson/runtime/src/main/java/io/quarkus/resteasy/reactive/jackson/runtime/security/SecurityCustomSerialization.java
+++ extensions/resteasy-reactive/rest-jackson/runtime/src/main/java/io/quarkus/resteasy/reactive/jackson/runtime/security/SecurityCustomSerialization.java
@@ -1,7 +1,10 @@
 package io.quarkus.resteasy.reactive.jackson.runtime.security;
 
 import java.lang.reflect.Type;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -9,22 +12,21 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 public class SecurityCustomSerialization implements BiFunction<ObjectMapper, Type, ObjectWriter> {
 
-    private static volatile ObjectWriter writer;
+    private static final Map<ObjectMapper, ObjectWriter> WRITERS = new ConcurrentHashMap<>();
 
     @Override
     public ObjectWriter apply(ObjectMapper objectMapper, Type type) {
-        if (writer == null) {
-            synchronized (this) {
-                if (writer == null) {
-                    writer = objectMapper
-                            .copy() // we need to make the copy in order to avoid adding the Introspector to the default mapper
-                            .setAnnotationIntrospector(new SecurityJacksonAnnotationIntrospector()) // needed in order to trigger the inclusion of the filter
-                            .writer(
-                                    new SimpleFilterProvider().addFilter(SecurityPropertyFilter.FILTER_ID,
-                                            new SecurityPropertyFilter())); // register the actual filter
-                }
+        return WRITERS.computeIfAbsent(objectMapper, new Function<>() {
+
+            @Override
+            public ObjectWriter apply(ObjectMapper objectMapper) {
+                return objectMapper
+                        .copy()
+                        .setAnnotationIntrospector(new SecurityJacksonAnnotationIntrospector())
+                        .writer(
+                                new SimpleFilterProvider().addFilter(SecurityPropertyFilter.FILTER_ID,
+                                        new SecurityPropertyFilter()));
             }
-        }
-        return writer; // we can use the same writer for all usages of @SecurityCustomSerialization because there is nothing resource method specific to it
+        });
     }
 }

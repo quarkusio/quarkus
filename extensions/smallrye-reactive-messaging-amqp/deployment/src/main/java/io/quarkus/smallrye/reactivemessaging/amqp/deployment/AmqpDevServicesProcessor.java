7749ccbc7850f99da849b1e5b4f01e8c79605ecb
Guillaume Smet <guillaume.smet@gmail.com>
2026-09-23 15:43:43 +0200

    Keep using the deprecated configProvider() in AmqpDevServicesProcessor
    We can replace it when we fully get rid of the deprecated method.

--- extensions/smallrye-reactive-messaging-amqp/deployment/src/main/java/io/quarkus/smallrye/reactivemessaging/amqp/deployment/AmqpDevServicesProcessor.java
+++ extensions/smallrye-reactive-messaging-amqp/deployment/src/main/java/io/quarkus/smallrye/reactivemessaging/amqp/deployment/AmqpDevServicesProcessor.java
@@ -126,10 +126,11 @@ public class AmqpDevServicesProcessor {
                         .config(Map.of(
                                 AMQP_USER_PROP, DEFAULT_USER,
                                 AMQP_PASSWORD_PROP, DEFAULT_PASSWORD))
-                        .configProvider(s -> Map.of(
-                                AMQP_HOST_PROP, s.getEffectiveHost(),
-                                AMQP_PORT_PROP, String.valueOf(s.getPort()),
-                                AMQP_MAPPED_PORT_PROP, String.valueOf(s.getMappedConsolePort())))
+                        // keep the deprecated call until we remove it entirely
+                        .configProvider(Map.of(
+                                AMQP_HOST_PROP, ArtemisContainer::getEffectiveHost,
+                                AMQP_PORT_PROP, s -> String.valueOf(s.getPort()),
+                                AMQP_MAPPED_PORT_PROP, s -> String.valueOf(s.getMappedConsolePort())))
                         .build());
     }
 
